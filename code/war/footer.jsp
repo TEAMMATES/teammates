a7f133d18bf0e5bafe03a19cc5b2b01349c18060
Wang Sha <wangshasg@gmail.com>
2011-11-02 21:12:21 +0800

    add two additional files footer.jsp, mobile.jsp convert mobile.html and oldIE.html to mobile.jsp, oldIE.jsp

---
+++ code/war/footer.jsp
@@ -0,0 +1,10 @@
+<%@ page import="com.google.appengine.api.utils.SystemProperty"%>
+<div id="contentFooter">
+<% 
+String version = SystemProperty.applicationVersion.get().split("\\.")[0].replace("-", ".");
+String footer = "[TEAMMATES Version "  + version + "] ";
+footer += "Best Viewed In Firefox, Chrome, Safari and Internet Explore 8+. For Enquires:";
+out.println(footer); 
+%>
+<a class="footer" href="http://www.comp.nus.edu.sg/~teams/contact.html" target="_blank">Contact Us</a>
+</div>
--- code/war/js/blockUnsupportedBrowsers.js
+++ code/war/js/blockUnsupportedBrowsers.js
@@ -6,7 +6,7 @@
 var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1; // && ua.indexOf("mobile");
 if(isAndroid) {
-        window.location="../mobile.html"; // URL to redirect to.
+        window.location="../mobile.jsp"; // URL to redirect to.
 }
 
 
@@ -17,12 +17,12 @@ var thestart    = parseFloat(ver.indexOf("MSIE"))+1; // This finds the start of 
 var brow_ver    = parseFloat(ver.substring(thestart+4, thestart+7)); // This cuts out the bit of string we need.
 
 if ((browser == "Microsoft Internet Explorer") && (brow_ver < 8)) {
-        window.location="../oldIE.html"; // URL to redirect to.
+        window.location="../oldIE.jsp"; // URL to redirect to.
 }
 
 
 // Iphone
 if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
-        window.location="../mobile.html"; // URL to redirect to.
+        window.location="../mobile.jsp"; // URL to redirect to.
 }
 //-->
