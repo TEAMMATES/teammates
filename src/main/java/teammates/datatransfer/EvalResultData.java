ce1129c09df1a7c88b93719d6c24a64950aaf056
Damith Rajapakse <damith@gmail.com>
2012-06-06 20:11:04 +0800

    added missing file

---
+++ src/main/java/teammates/datatransfer/EvalResultData.java
@@ -0,0 +1,21 @@
+package teammates.datatransfer;
+
+import java.util.ArrayList;
+
+import teammates.api.Common;
+
+public class EvalResultData {
+	
+	public SubmissionData own;
+	public ArrayList<SubmissionData> incoming = new ArrayList<SubmissionData>();
+	public ArrayList<SubmissionData> outgoing = new ArrayList<SubmissionData>();
+	
+	public int claimedActual = Common.UNINITIALIZED_INT;
+	public int claimedToStudent = Common.UNINITIALIZED_INT;
+	public int claimedToCoord = Common.UNINITIALIZED_INT;
+	public int perceivedActual = Common.UNINITIALIZED_INT;
+	public int perceivedToCoord = Common.UNINITIALIZED_INT;
+	public int perceivedToStudent = Common.UNINITIALIZED_INT;
+	
+
+}
