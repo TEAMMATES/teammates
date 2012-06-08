3182ad893bc25c517cf6bbcbcfd52236fd2c9fdd
Damith C. Rajapakse <damith@gmail.com>
2012-06-08 18:28:09 +0800

    temp snapshot before introducing factor variable

--- src/main/java/teammates/datatransfer/EvalResultData.java
+++ src/main/java/teammates/datatransfer/EvalResultData.java
@@ -8,54 +8,171 @@ import teammates.api.Common;
 import teammates.api.NotImplementedException;
 
 public class EvalResultData {
-	
+
+	private static int NA = Common.UNINITIALIZED_INT;
+
 	public SubmissionData own;
 	public ArrayList<SubmissionData> incoming = new ArrayList<SubmissionData>();
 	public ArrayList<SubmissionData> outgoing = new ArrayList<SubmissionData>();
-	
+
 	public int claimedActual = Common.UNINITIALIZED_INT;
 	public int claimedToStudent = Common.UNINITIALIZED_INT;
 	public int claimedToCoord = Common.UNINITIALIZED_INT;
 	public int perceivedToCoord = Common.UNINITIALIZED_INT;
 	public int perceivedToStudent = Common.UNINITIALIZED_INT;
-	
-	public void sortOutgoingByStudentNameAscending(){
+
+	public void sortOutgoingByStudentNameAscending() {
 		Collections.sort(outgoing, new Comparator<SubmissionData>() {
 			public int compare(SubmissionData s1, SubmissionData s2) {
 				return s1.revieweeName.compareTo(s2.revieweeName);
 			}
 		});
 	}
-	
-	public void sortIncomingByStudentNameAscending(){
+
+	public void sortIncomingByStudentNameAscending() {
 		Collections.sort(incoming, new Comparator<SubmissionData>() {
 			public int compare(SubmissionData s1, SubmissionData s2) {
 				return s1.reviewerName.compareTo(s2.reviewerName);
 			}
 		});
 	}
-	
-	public void sortIncomingByFeedbackAscending(){
+
+	public void sortIncomingByFeedbackAscending() {
 		Collections.sort(incoming, new Comparator<SubmissionData>() {
 			public int compare(SubmissionData s1, SubmissionData s2) {
-				return s1.p2pFeedback.getValue().compareTo(s2.p2pFeedback.getValue());
+				return s1.p2pFeedback.getValue().compareTo(
+						s2.p2pFeedback.getValue());
 			}
 		});
 	}
 
 	public static int[][] calculatePoints(int[][] input) {
-		 int[][] output= 
-			    {{ 100, 100, 100, 100 }, 
-				 { 100, 100, 100, 100 },
-				 { 100, 100, 100, 100 },
-				 { 100, 100, 100, 100 },
-				 
-				 { 100, 100, 100, 100 },
-				 
-				 { 100, 100, 100, 100 }, 
-				 { 100, 100, 100, 100 },
-				 { 100, 100, 100, 100 },
-				 { 100, 100, 100, 100 }};
+		int teamSize = input.length;
+		int containerSize = teamSize * 2 + 1;
+		int[][] output = new int[containerSize][teamSize];
+
+		// create the three sub containers
+		int[][] actualInput = new int[teamSize][teamSize];
+		int[] perceivedForCoord = new int[teamSize];
+		int[][] perceivedForStudent =  new int[teamSize][teamSize];
+		
+		//fill first sub-container
+		for (int i = 0; i < teamSize; i++) {
+			actualInput[i] = input[i];
+		}
+		
+		//fill second sub-container
+		int[][] normalizedInput = new int[teamSize][teamSize];
+		for (int i = 0; i < teamSize; i++) {
+			normalizedInput[i] = normalizeValues(input[i]);
+		}
+		int[][] selfRatingsRemoved = excludeSelfRatings(normalizedInput);
+
+		int[][] selfRatingRemovedAndNormalized = new int[teamSize][teamSize];
+		for (int i = 0; i < teamSize; i++) {
+			selfRatingRemovedAndNormalized[i] = normalizeValues(selfRatingsRemoved[i]);
+		}
+		perceivedForCoord = normalizeValues(averageColumns(selfRatingRemovedAndNormalized));
+		
+		//fill third sub-container
+		for (int k = 0; k < teamSize; k++) {
+			perceivedForStudent[k] = calculatePerceivedForStudent(actualInput[k],
+					perceivedForCoord);
+		}
+		
+		//transfer values to output container
+		int i=0;
+		for (; i < teamSize; i++) {
+			output[i] = normalizedInput[i];
+		}
+		output[i] = perceivedForCoord;
+		i++;
+		for (int k = 0; k < teamSize; k++) {
+			output[i] = perceivedForStudent[k];
+			i++;
+		}
+		
+		return output;
+	}
+
+	public static int[] calculatePerceivedForStudent(int[] actualInput,
+			int[] perceivedForCoord) {
+		int[] perceivedForStudent = new int[actualInput.length];
+		for (int i = 0; i < perceivedForStudent.length; i++) {
+			perceivedForStudent[i] = perceivedForCoord[i] * sum(actualInput)/ sum(perceivedForCoord);
+		}
+		return perceivedForStudent;
+	}
+
+	public static int sum(int[] input) {
+		int sum = NA;
+		if (input.length == 0) {
+			return 0;
+		}
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] != NA) {
+				sum = (sum == NA ? input[i] : sum + input[i]);
+			}
+		}
+		return sum;
+	}
+
+	// TODO: make this private and use reflection to test
+	public static int[][] excludeSelfRatings(int[][] input) {
+		int[][] output = new int[input.length][input.length];
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[i].length; j++) {
+				if (i == j) {
+					output[i][j] = Common.UNINITIALIZED_INT;
+				} else {
+					output[i][j] = input[i][j];
+				}
+			}
+		}
+		return output;
+	}
+
+	// TODO: make this private and use reflection to test
+	public static int[] normalizeValues(int[] input) {
+		int sum = 0;
+		int count = 0;
+		int[] output = new int[input.length];
+
+		for (int j = 0; j < input.length; j++) {
+			if (input[j] == NA) {
+				continue;
+			}
+			sum += input[j];
+			count++;
+		}
+
+		double factor = (sum == 0 ? 0 : count * 100.0 / sum);
+
+		for (int j = 0; j < input.length; j++) {
+			output[j] = (int) (input[j] == NA ? NA : Math.round(input[j]
+					* factor));
+		}
+		return output;
+	}
+
+	// TODO: make this private and use reflection to test
+	public static int[] averageColumns(int[][] input) {
+		int[] output = new int[input.length];
+
+		for (int i = 0; i < input.length; i++) {
+			int sum = 0;
+			int count = 0;
+			for (int j = 0; j < input.length; j++) {
+				int value = input[j][i];
+				if (value == NA) {
+					continue;
+				} else {
+					sum += value;
+					count++;
+				}
+			}
+			output[i] = (count == 0 ? NA : Math.round(sum / count));
+		}
 		return output;
 	}
 
--- src/test/java/teammates/testing/testcases/EvalResultDataTest.java
+++ src/test/java/teammates/testing/testcases/EvalResultDataTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.appengine.api.datastore.Text;
@@ -13,6 +14,7 @@ import teammates.datatransfer.*;
 
 public class EvalResultDataTest {
 
+	private static int NA = Common.UNINITIALIZED_INT;;
 	@Test
 	public void testSortOutgoingByStudentNameAscending() {
 		EvalResultData result = new EvalResultData();
@@ -83,8 +85,9 @@ public class EvalResultDataTest {
 	}
 
 	@Test
+	
 	// @formatter:off
-	public void testCalculateInputNormalizer() {
+	public void testCalculatePoints() {
 		
 		int[][] input = 
 			{{ 100, 100, 100, 100 }, 
@@ -107,13 +110,151 @@ public class EvalResultDataTest {
 		
 		int[][] output = EvalResultData.calculatePoints(input);
 		assertEquals(pointsToString(expected), pointsToString(output));
+		
+		int[][] input3 = 
+			{{ 100, 100, 100, 100 }, 
+			 { 110, 110, 110, 110 },
+			 {  90,  90,  90,  90 },
+			 {  10,  10,  10,  10 }};
+		
+		int[][] expected3 = 
+			{{ 100, 100, 100, 100 }, 
+			 { 100, 100, 100, 100 },
+			 { 100, 100, 100, 100 },
+			 { 100, 100, 100, 100 },
+			 
+			 { 100, 100, 100, 100 },
+			 
+			 { 100, 100, 100, 100 }, 
+			 { 110, 110, 110, 110 },
+			 {  90,  90,  90,  90 },
+			 {  10,  10,  10,  10 }};
+		assertEquals(pointsToString(expected3),
+				pointsToString(EvalResultData.calculatePoints(input3)));
+		
+		int[][] input2 = 
+			{{ 100, 100, 100, 100 }, 
+			 { 110, 110, 110, 110 },
+			 {  90,  90,  90,  90 },
+			 {  70,  80, 110, 120 }};
+		
+		int[][] expected2 = 
+			{{ 100, 100, 100, 100 }, 
+			 { 100, 100, 100, 100 },
+			 { 100, 100, 100, 100 },
+			 {  74,  84, 116, 126 },
+			 
+			 {  93, 97, 109, 100 },
+			 
+			 {  93,  97, 109, 100 }, 
+			 { 102, 106, 120, 110 },
+			 {  83,  87,  98,  90 },
+			 {  88,  92, 103,  95 }};
+		assertEquals(pointsToString(expected2),
+				pointsToString(EvalResultData.calculatePoints(input2)));
+	}
+	
+	@Test
+	public void testNormalizeValues(){
+		verifyNormalized(new int[] {}, new int[] {});
+		verifyNormalized(new int[] {100}, new int[] {100});
+		verifyNormalized(new int[] {100}, new int[] {50});
+		verifyNormalized(new int[] {150,90,60}, new int[] {50,30,20});
+		verifyNormalized(new int[] {0,0,0}, new int[] {0,0,0});
+		verifyNormalized(new int[] {0,0,300},new int[] {0,0,100});
+		verifyNormalized(new int[] {0,NA,200},new int[] {0,NA,100});
+		verifyNormalized(new int[] {100,100,100},new int[] {110,110,110});
+		verifyNormalized(new int[]{NA,NA},new int[]{NA,NA});
+	}
+
+
+	@Test 
+	public void testExcludeSelfRatings(){
+		
+		int[][] input = 
+			{{ 11, 12, 13, 14 }, 
+			 { 21, 22, 23, 24 },
+			 { 31, 32, 33, 34 },
+			 { 41, 42, 43, 44 }};
+		
+		int[][] expected = 
+			{{ NA, 12, 13, 14 }, 
+			 { 21, NA, 23, 24 },
+			 { 31, 32, NA, 34 },
+			 { 41, 42, 43, NA }};
+		assertEquals(pointsToString(expected),
+				pointsToString(EvalResultData.excludeSelfRatings(input)));
+	}
+	
+	@Test
+	public void testAverageColumns(){
+		int[][] input = 
+			{{ 10, 20,  0, NA }, 
+			 { 10, NA,  0, NA },
+			 { 10, 20, NA, NA },
+			 { 10, 20,  0, NA }};
+		int[] expected = {10, 20, 0, NA};
+		assertEquals(Arrays.toString(expected), 
+				Arrays.toString(EvalResultData.averageColumns(input)));
+		
+	}
+	
+	@Test
+	public void testSum(){
+		assertEquals(6,EvalResultData.sum(new int[]{1,2,3}));
+		assertEquals(0,EvalResultData.sum(new int[]{}));
+		assertEquals(6,EvalResultData.sum(new int[]{NA, 2, 4}));
+		assertEquals(0,EvalResultData.sum(new int[]{NA, 0, 0}));
+		assertEquals(NA,EvalResultData.sum(new int[]{NA, NA, NA}));
+	}
+	
+	@Test
+	public void testCalculatePerceivedForStudent(){
+		
+		assertEquals(Arrays.toString(new int[]{}),
+				Arrays.toString(EvalResultData.calculatePerceivedForStudent
+						(new int[]{}, new int[]{})));
+		
+		assertEquals(Arrays.toString(new int[]{10}),
+				Arrays.toString(EvalResultData.calculatePerceivedForStudent
+						(new int[]{10}, new int[]{5})));
+		
+		assertEquals(Arrays.toString(new int[]{100,50,50}),
+				Arrays.toString(EvalResultData.calculatePerceivedForStudent
+						(new int[]{50,100,50}, new int[]{50,25,25})));
+		
+		assertEquals(Arrays.toString(new int[]{100,50,50}),
+				Arrays.toString(EvalResultData.calculatePerceivedForStudent
+						(new int[]{NA,150,50}, new int[]{50,25,25})));
+		
+		assertEquals(Arrays.toString(new int[]{NA,NA,NA}),
+				Arrays.toString(EvalResultData.calculatePerceivedForStudent
+						(new int[]{NA,NA,NA}, new int[]{NA,NA,NA})));
+		
+//		assertEquals(Arrays.toString(new int[]{100,50,50}),
+//				Arrays.toString(EvalResultData.calculatePerceivedForStudent
+//						(new int[]{NA,NA,NA}, new int[]{100,50,50})));
+
 	}
 	// @formatter:on
-
+	
+	//--------------------------------------------------------------------
+	
+	private void verifyNormalized(int[] expected, int[] input) {
+		assertEquals(Arrays.toString(expected), 
+				Arrays.toString(EvalResultData.normalizeValues(input)));
+	}
+	
 	private String pointsToString(int[][] array) {
 		String returnValue = "";
-		for(int i=0; i<array.length; i++){
-			returnValue = returnValue + Arrays.toString(array[i])+Common.EOL;
+		int firstDividerLocation = (array.length - 1) / 2 - 1;
+		int secondDividerLocation = firstDividerLocation + 1;
+		for (int i = 0; i < array.length; i++) {
+			returnValue = returnValue + Arrays.toString(array[i]) + Common.EOL;
+			if ((i == firstDividerLocation) || (i == secondDividerLocation)) {
+				returnValue = returnValue + "======================="
+						+ Common.EOL;
+			}
 		}
 		return returnValue;
 	}
