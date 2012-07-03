9bad480331a52c817e713cae71a2d1d597c3fd4d
Damith Rajapakse <damith@gmail.com>
2012-07-03 17:50:55 +0800

    removed unused batch files

--- prepDebugServer.bat
+++
@@ -1,5 +0,0 @@
-xcopy  /E /Y .\target\classes\teammates .\target\teammates-v4\WEB-INF\classes\teammates
-xcopy  /E /Y .\src\main\webapp .\target\teammates-v4
-del target\teammates-v4\WEB-INF\appengine-generated\local_db.bin
-mkdir target\teammates-v4\WEB-INF\appengine-generated
-copy local_db.bin target\teammates-v4\WEB-INF\appengine-generated\
--- startLocalServer.bat
+++
@@ -1,4 +0,0 @@
-del target\teammates-v4\WEB-INF\appengine-generated\local_db.bin
-mkdir target\teammates-v4\WEB-INF\appengine-generated\
-copy local_db.bin target\teammates-v4\WEB-INF\appengine-generated\
-mvn gae:run
