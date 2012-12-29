68625957b7cb1027a310141d452863c6d70b786b
Kenny <whisperingrwind@gmail.com>
2012-12-29 20:56:27 +0800

    work Issue 433 Update Issue 433 Forgot to add AccountData for commit
    --HG--
    branch : Issue433
    
    
    Former-commit-id: f87a32d3d75d1438091a1e168a5e3b255c363c37
    Former-commit-id: 9b6e32d531654af65886e3b61c79ebaa9bc74445
    Former-commit-id: 1e552cf3977753d9a056856e5c5a21efe9278934

---
+++ src/main/java/teammates/common/datatransfer/AccountData.java
@@ -0,0 +1,68 @@
+package teammates.common.datatransfer;
+
+import teammates.common.Common;
+import teammates.storage.entity.Account;
+
+public class AccountData extends BaseData {
+	public String googleId;
+	public String name;
+	public boolean isInstructor;
+	
+	// Other Information
+	public String email;
+	public String institute;
+	
+	public static final String ERROR_FIELD_ID = "GoogleID Field is invalid\n";
+	public static final String ERROR_FIELD_NAME = "Name field cannot be null or empty\n";
+	public static final String ERROR_FIELD_EMAIL = "Email Field is invalid\n";
+	public static final String ERROR_FIELD_INSTITUTE = "Institute field cannot be null or empty\n";
+	
+	public AccountData(Account a) {
+		googleId = a.getGoogleId();
+		name = a.getName();
+		isInstructor = a.isInstructor();
+		email = a.getEmail();
+		institute = a.getInstitute();
+	}
+	
+	public AccountData() {
+		
+	}
+	
+	public AccountData(String googleId, String name, boolean isInstructor,
+				String email, String institute) {
+		this.googleId = trimIfNotNull(googleId);
+		this.name = trimIfNotNull(name);
+		this.isInstructor = isInstructor;
+		this.email = trimIfNotNull(email);
+		this.institute = trimIfNotNull(institute);
+	}
+	
+	public Account toEntity() {
+		return new Account(googleId, name, isInstructor,
+						email, institute);
+	}
+	
+	public String getInvalidStateInfo() {
+		String errorMessage = "";
+
+		if (!Common.isValidGoogleId(googleId)) {
+			errorMessage += ERROR_FIELD_ID;
+		}
+	/*
+		if (!Common.isValidName(name)) {
+			errorMessage += ERROR_FIELD_NAME;
+		}
+		
+		if (!Common.isValidEmail(email)) {
+			errorMessage += ERROR_FIELD_EMAIL;
+		}
+		
+		if (!Common.isValidName(institute)) {
+			errorMessage += ERROR_FIELD_INSTITUTE;
+		}
+	*/
+		return errorMessage;
+
+	}
+}
