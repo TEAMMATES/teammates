706c634ea46cc1b50f8fb63868c2048375224700
Luk Ming Kit <A0074064@nus.edu.sg>
2013-01-29 11:13:27 +0800

    Continuing Issue 8 [Allow sorting by descending order] Update Issue 8 Cleaned up on consistency and added comments for the javadoc
    --HG--
    branch : Issue8
    
    
    Former-commit-id: 81d5de45cc4b4f590fd5395da4ae21a969c1188b
    Former-commit-id: 02fca4ac9e2b380cb5f31d7b89109633995de79e
    Former-commit-id: 02815a4a36ff26a0cb2b737078db5f0a067c7f93

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -108,9 +108,7 @@ $.fn.sortElements = (function(){
 * 		The column index (1-based) as key for the sort
 */
 function toggleSort(divElement,colIdx,comparator) {
-	
-	if($(divElement).attr("class")=="buttonSortNone")
-	{
+	if($(divElement).attr("class")=="buttonSortNone"){
 		sortTable(divElement,colIdx,comparator);
 		$(".buttonSortAscending").attr("class","buttonSortNone");
 		$(".buttonSortDescending").attr("class","buttonSortNone");
@@ -147,20 +145,23 @@ function sortTable(oneOfTableCell, colIdx, comparator){
 }
 
 /**
-* The base comparator for a cell
+* The base comparator for a cell (ascending)
 * @param cell1
 * @param cell2
-* @returns
+* @returns returns if cell1.innerHTML is larger than cell2.innerHTML
 */
 function sortBaseCell(cell1, cell2){
-	cell1 = cell1.innerHTML;
-	cell2 = cell2.innerHTML;
-	return sortBase(cell1,cell2);
+	return sortBase(cell1.innerHTML,cell2.innerHTML);
 }
 
-//Just the opposite of what sortBaseCell is doing
+/**
+* The base comparator for a cell (descending)
+* @param cell1
+* @param cell2
+* @returns returns if cell2.innerHTML is larger than cell1.innerHTML
+*/
 function sortBaseCellDescending(cell1, cell2){
-	return sortBaseCell(cell2,cell1);
+	return sortBaseCell(cell2.innerHTML,cell1.innerHTML);
 }
 
 /**
