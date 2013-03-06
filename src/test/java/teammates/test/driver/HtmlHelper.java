bf54efc2bced71722ccde2fef6004fe5b2eb9187
Goh Chun Teck <chunteck.88@gmail.com>
2013-03-06 11:52:45 +0800

    Continuing Issue 659[print view is blank] Update Issue 659 Ensuring that all ui tests still work after merging. Resolved IE UI test bug from Issue 646 There is still one Chrome/IE test bug from Issue 674, that can be fixed along the respective branch
    --HG--
    branch : Issue659
    
    
    Former-commit-id: e48d7e834cfd5cba789ad66b93f498b09c9a6b7d
    Former-commit-id: 82d331777c735be1ec48f3a63531c56feef095d3
    Former-commit-id: f26344adcf2910f85cad560ff56570b0096b0681

--- src/test/java/teammates/test/driver/HtmlHelper.java
+++ src/test/java/teammates/test/driver/HtmlHelper.java
@@ -68,7 +68,7 @@ public class HtmlHelper {
 		htmlString = htmlString.replaceAll("height=\"([0-9]+)\"", "height=\"$1px\"");
 		htmlString = htmlString.replaceAll("width=\"([0-9]+)\"", "width=\"$1px\"");
 		htmlString = htmlString.replaceAll("(?s)<noscript>.*</noscript>", "");
-		htmlString = htmlString.replaceAll("<script type=\"text/javascript\" src=\"http://www.google-analytics.com/ga.js\"></script>", "<script async=\"\"  type=\"text/javascript\" src=\"http://www.google-analytics.com/ga.js\"></script>");
+		htmlString = htmlString.replaceAll("src=\"https://ssl.google-analytics.com/ga.js\"", "async=\"\" src=\"https://ssl.google-analytics.com/ga.js\"");
 
 		if (!htmlString.contains("<!DOCTYPE")){
 			htmlString = "<!DOCTYPE html>\n" + htmlString;
