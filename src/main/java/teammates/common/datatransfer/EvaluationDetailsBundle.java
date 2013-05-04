da03dd8ac78d4936dd5910c026f0a02483496e98
Damith Rajapakse <damith@gmail.com>
2013-05-04 10:55:46 +0800

    Issue 779: Remove redundant association from EvaluationDetailsBundle to TeamDetailsBundle Update Issue 779
    Former-commit-id: 8721e9ed28b52bc4418ab9e535411cf3c472ae63
    Former-commit-id: c3b6a056363f68013b4a6359362441c60778a74c
    Former-commit-id: f6f327627b7f649b23f6a837d8aa1a26fb71d752

--- src/main/java/teammates/common/datatransfer/EvaluationDetailsBundle.java
+++ src/main/java/teammates/common/datatransfer/EvaluationDetailsBundle.java
@@ -1,7 +1,5 @@
 package teammates.common.datatransfer;
 
-import java.util.ArrayList;
-
 import teammates.common.Common;
 
 public class EvaluationDetailsBundle {
@@ -11,19 +9,16 @@ public class EvaluationDetailsBundle {
 	public int submittedTotal = Common.UNINITIALIZED_INT;
 	public int expectedTotal = Common.UNINITIALIZED_INT;
 
-	public ArrayList<TeamDetailsBundle> teams = new ArrayList<TeamDetailsBundle>();
 
 	public EvaluationDetailsBundle(EvaluationData evaluation) {
 		this.evaluation = evaluation;
 	}
 
-	
-
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("course:" + evaluation.course + ", name:" + evaluation.name
 				+ Common.EOL);
-		//TODO: add student details here
+		//TODO: add statistics
 		return sb.toString();
 	}
 
