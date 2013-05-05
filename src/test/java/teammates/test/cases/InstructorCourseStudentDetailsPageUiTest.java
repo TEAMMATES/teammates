cffb30a3d4d81d940f829684676fe7c0f171e234
Damith C. Rajapakse <damith@gmail.com>
2013-05-05 11:11:10 +0800

    Issue 793: Make test independent: InstructorCourseStudentDetailsPageUiTest Update Issue 793
    Former-commit-id: 041ce7f506dacc11f499775bc14454baf32c393c
    Former-commit-id: 504b8117d4d6eecdacfdbc489696eee315b6830d
    Former-commit-id: 6d4f56e12804704e8bb0b365217fffdf0f6fc500

--- src/test/java/teammates/test/cases/InstructorCourseStudentDetailsPageUiTest.java
+++ src/test/java/teammates/test/cases/InstructorCourseStudentDetailsPageUiTest.java
@@ -64,7 +64,7 @@ public class InstructorCourseStudentDetailsPageUiTest extends BaseTestCase {
 		String link = appUrl+Common.PAGE_INSTRUCTOR_COURSE_STUDENT_DETAILS;
 		link = Common.addParamToUrl(link,Common.PARAM_COURSE_ID,scn.courses.get("CCSDetailsUiT.CS2104").id);
 		link = Common.addParamToUrl(link,Common.PARAM_STUDENT_EMAIL,scn.students.get("registeredStudent").email);
-		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("teammates.test").googleId);
+		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("CCSDetailsUiT.instr").googleId);
 		bi.goToUrl(link);
 		
 		bi.verifyCurrentPageHTML(Common.TEST_PAGES_FOLDER+"/instructorCourseStudentDetailsPage.html");
@@ -74,7 +74,7 @@ public class InstructorCourseStudentDetailsPageUiTest extends BaseTestCase {
 		link = appUrl+Common.PAGE_INSTRUCTOR_COURSE_STUDENT_DETAILS;
 		link = Common.addParamToUrl(link,Common.PARAM_COURSE_ID,scn.courses.get("CCSDetailsUiT.CS2104").id);
 		link = Common.addParamToUrl(link,Common.PARAM_STUDENT_EMAIL,scn.students.get("unregisteredStudent").email);
-		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("teammates.test").googleId);
+		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("CCSDetailsUiT.instr").googleId);
 		bi.goToUrl(link);
 		
 		bi.verifyCurrentPageHTML(Common.TEST_PAGES_FOLDER+"/instructorCourseStudentDetailsUnregisteredPage.html");
@@ -88,7 +88,7 @@ public class InstructorCourseStudentDetailsPageUiTest extends BaseTestCase {
 		String link = appUrl+Common.PAGE_INSTRUCTOR_COURSE_STUDENT_EDIT;
 		link = Common.addParamToUrl(link,Common.PARAM_COURSE_ID,scn.courses.get("CCSDetailsUiT.CS2104").id);
 		link = Common.addParamToUrl(link,Common.PARAM_STUDENT_EMAIL,scn.students.get("unregisteredStudent").email);
-		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("teammates.test").googleId);
+		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("CCSDetailsUiT.instr").googleId);
 		bi.goToUrl(link);
 		
 		bi.verifyCurrentPageHTML(Common.TEST_PAGES_FOLDER+"/instructorCourseStudentEditUnregisteredPage.html");
@@ -98,7 +98,7 @@ public class InstructorCourseStudentDetailsPageUiTest extends BaseTestCase {
 		link = appUrl+Common.PAGE_INSTRUCTOR_COURSE_STUDENT_EDIT;
 		link = Common.addParamToUrl(link,Common.PARAM_COURSE_ID,scn.courses.get("CCSDetailsUiT.CS2104").id);
 		link = Common.addParamToUrl(link,Common.PARAM_STUDENT_EMAIL,scn.students.get("registeredStudent").email);
-		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("teammates.test").googleId);
+		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("CCSDetailsUiT.instr").googleId);
 		bi.goToUrl(link);
 		
 		//check the default view
