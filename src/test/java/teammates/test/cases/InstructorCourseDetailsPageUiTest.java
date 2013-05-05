e18c82b71c41563b9be7550d49d315aca7b9592d
Damith C. Rajapakse <damith@gmail.com>
2013-05-05 13:20:12 +0800

    Issue 797: Make test independent: InstructorCourseDetailsPageUiTest Update Issue 797
    Former-commit-id: cded43a88f977bf573411009d783fae851e6e822
    Former-commit-id: 05abf3a4464cd6ccc2c375e64a14f6bc09be84ed
    Former-commit-id: f2ca78a235bbe09c449ee30a4f12df25770988c0

--- src/test/java/teammates/test/cases/InstructorCourseDetailsPageUiTest.java
+++ src/test/java/teammates/test/cases/InstructorCourseDetailsPageUiTest.java
@@ -42,7 +42,7 @@ public class InstructorCourseDetailsPageUiTest extends BaseTestCase {
 		bi.loginAdmin(TestProperties.inst().TEST_ADMIN_ACCOUNT, TestProperties.inst().TEST_ADMIN_PASSWORD);
 		String link = appUrl+Common.PAGE_INSTRUCTOR_COURSE_DETAILS;
 		link = Common.addParamToUrl(link,Common.PARAM_COURSE_ID,scn.courses.get("CCDetailsUiT.CS2104").id);
-		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("teammates.test").googleId);
+		link = Common.addParamToUrl(link,Common.PARAM_USER_ID,scn.instructors.get("CCDetailsUiT.instr").googleId);
 		bi.goToUrl(link);
 	}
 	
@@ -100,7 +100,7 @@ public class InstructorCourseDetailsPageUiTest extends BaseTestCase {
 		String otherStudentEmail = scn.students
 				.get("charlie.tmms@CCDetailsUiT.CS2104").email;
 		String registeredStudentEmail = scn.students
-				.get("alice.tmms@CCDetailsUiT.CS2104").email;
+				.get("CCDetailsUiT.alice.tmms@CCDetailsUiT.CS2104").email;
 
 		int studentRowId = bi.getStudentRowId(studentName);
 
