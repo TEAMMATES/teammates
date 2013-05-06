712109c2fc0b62f73122f885a0444828cbc38a6d
Damith Rajapakse <damith@gmail.com>
2013-05-06 13:24:59 +0800

    Issue 805: Make test independent: AdminAccountManagementUiTest Update Issue 805
    Former-commit-id: 92f11b863d605abda1f3574121a315bece4111b3
    Former-commit-id: 42aa14cefbe62702f3469145b9ba4b6239843813
    Former-commit-id: 9bfc2599eed433989c23c4a7f74a31f89726511c

--- src/test/java/teammates/test/cases/AdminAccountManagementUiTest.java
+++ src/test/java/teammates/test/cases/AdminAccountManagementUiTest.java
@@ -1,6 +1,5 @@
 package teammates.test.cases;
 
-import static org.testng.AssertJUnit.assertNull;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 import org.testng.annotations.AfterClass;
@@ -50,9 +49,9 @@ public class AdminAccountManagementUiTest extends BaseTestCase{
 		String link = appUrl+Common.PAGE_ADMIN_ACCOUNT_MANAGEMENT;
 		bi.goToUrl(link);
 
-		bi.click(By.id("teammates.instr1_delete"));
+		bi.click(By.id("AAMgtUiT.instr1_delete"));
 		bi.waitForStatusMessage(Common.MESSAGE_INSTRUCTOR_STATUS_DELETED);
-		String instructor = BackDoor.getAccountAsJson("teammates.instr1");
+		String instructor = BackDoor.getAccountAsJson("AAMgtUiT.instr1");
 		assertTrue("Account is still an instructor", instructor.contains("\"isInstructor\": false"));
 	}
 	
@@ -60,24 +59,24 @@ public class AdminAccountManagementUiTest extends BaseTestCase{
 	@Test
 	public void testRemoveInstructorFromCourse(){
 		String link = appUrl+Common.PAGE_ADMIN_ACCOUNT_DETAILS;
-		link = Common.addParamToUrl(link, Common.PARAM_INSTRUCTOR_ID, "teammates.instr2");
+		link = Common.addParamToUrl(link, Common.PARAM_INSTRUCTOR_ID, "AAMgtUiT.instr2");
 		bi.goToUrl(link);
 		
-		bi.click(By.id("instructor_CS2104"));
+		bi.click(By.id("instructor_AAMgtUiT.CS2104"));
 		bi.waitForStatusMessage(Common.MESSAGE_INSTRUCTOR_REMOVED_FROM_COURSE);
-		String instructor = BackDoor.getInstructorAsJson("teammates.instr2", "CS2104");
+		String instructor = BackDoor.getInstructorAsJson("AAMgtUiT.instr2", "AAMgtUiT.CS2104");
 		assertEquals("Instructor was not properly removed from the course", instructor, "null");
 	}
 	
 	@Test
 	public void testRemoveStudentFromCourse(){
 		String link = appUrl+Common.PAGE_ADMIN_ACCOUNT_DETAILS;
-		link = Common.addParamToUrl(link, Common.PARAM_INSTRUCTOR_ID, "teammates.instr2");
+		link = Common.addParamToUrl(link, Common.PARAM_INSTRUCTOR_ID, "AAMgtUiT.instr2");
 		bi.goToUrl(link);
 		
-		bi.click(By.id("student_CS1101"));
+		bi.click(By.id("student_AAMgtUiT.CS1101"));
 		bi.waitForStatusMessage(Common.MESSAGE_INSTRUCTOR_REMOVED_FROM_COURSE);
-		String instructor = BackDoor.getStudentAsJson("CS1101", "teammates.instr2@gmail.com");
+		String instructor = BackDoor.getStudentAsJson("AAMgtUiT.CS1101", "AAMgtUiT.instr2@gmail.com");
 		assertEquals("Instructor was not properly removed from the course", instructor, "null");
 	}
 	
@@ -86,13 +85,13 @@ public class AdminAccountManagementUiTest extends BaseTestCase{
 		String link = appUrl+Common.PAGE_ADMIN_ACCOUNT_MANAGEMENT;
 		bi.goToUrl(link);
 		
-		bi.clickAndCancel(By.id("teammates.instr3_deleteAccount"));
-		String instructor = BackDoor.getAccountAsJson("teammates.instr3");
-		assertTrue("Instructor Account was deleted when it is not supposed to be", instructor.contains("\"googleId\": \"teammates.instr3\""));
+		bi.clickAndCancel(By.id("AAMgtUiT.instr3_deleteAccount"));
+		String instructor = BackDoor.getAccountAsJson("AAMgtUiT.instr3");
+		assertTrue("Instructor Account was deleted when it is not supposed to be", instructor.contains("\"googleId\": \"AAMgtUiT.instr3\""));
 
-		bi.clickAndConfirm(By.id("teammates.instr3_deleteAccount"));
+		bi.clickAndConfirm(By.id("AAMgtUiT.instr3_deleteAccount"));
 		bi.waitForStatusMessage(Common.MESSAGE_INSTRUCTOR_ACCOUNT_DELETED);
-		instructor = BackDoor.getAccountAsJson("teammates.instr3");
+		instructor = BackDoor.getAccountAsJson("AAMgtUiT.instr3");
 		assertEquals("Instructor Account was not deleted properly", instructor, "null");
 		
 	}
