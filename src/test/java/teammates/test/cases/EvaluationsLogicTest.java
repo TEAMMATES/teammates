0d3a09f23684e14b764722514877c8f38a3d4742
Damith Rajapakse <damith@gmail.com>
2013-05-17 10:40:49 +0800

    Issue 859: Extract class SubmissionsLogicTest from EvaluationsLogicTest Update Issue 859
    Former-commit-id: 6b6359b4ab31cfb6f4de4e5b26a72f352d323af0
    Former-commit-id: e14a5484d3bbfaa8a26304be374c521de0baf27f
    Former-commit-id: 48be907e25aba2797b781a60a253aaf1882e4051

--- src/test/java/teammates/test/cases/EvaluationsLogicTest.java
+++ src/test/java/teammates/test/cases/EvaluationsLogicTest.java
@@ -2,31 +2,26 @@ package teammates.test.cases;
 
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeClass;
+
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 import javax.servlet.ServletException;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import teammates.common.Common;
-import teammates.common.datatransfer.CourseAttributes;
 import teammates.common.datatransfer.CourseDetailsBundle;
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.EvaluationAttributes;
 import teammates.common.datatransfer.EvaluationAttributes.EvalStatus;
-import teammates.common.datatransfer.StudentAttributes;
-import teammates.common.datatransfer.SubmissionAttributes;
 import teammates.logic.EvaluationsLogic;
-import teammates.logic.SubmissionsLogic;
 import teammates.logic.automated.EvaluationOpeningRemindersServlet;
 import teammates.logic.backdoor.BackDoorLogic;
 import teammates.storage.datastore.Datastore;
-import teammates.storage.entity.Submission;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
@@ -34,7 +29,6 @@ import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 public class EvaluationsLogicTest extends BaseTestCase{
 	
 	private static final EvaluationsLogic evaluationsLogic = EvaluationsLogic.inst();
-	protected static SubmissionsLogic submissionsLogic = SubmissionsLogic.inst();
 	
 	@BeforeClass
 	public static void classSetUp() throws Exception {
@@ -136,68 +130,6 @@ public class EvaluationsLogicTest extends BaseTestCase{
 		// Other variations of ready/not-ready states should be checked at
 		// Evaluation level
 	}
-	
-	
-	@Test
-	public void testAddSubmissionsForIncomingMember() throws Exception {
-		loginAsAdmin("admin.user");
-
-		______TS("typical case");
-
-		restoreTypicalDataInDatastore();
-		DataBundle dataBundle = getTypicalDataBundle();
-
-		CourseAttributes course = dataBundle.courses.get("typicalCourse1");
-		EvaluationAttributes evaluation1 = dataBundle.evaluations
-				.get("evaluation1InCourse1");
-		EvaluationAttributes evaluation2 = dataBundle.evaluations
-				.get("evaluation2InCourse1");
-		StudentAttributes student = dataBundle.students.get("student1InCourse1");
-
-		invokeAddSubmissionsForIncomingMember(course.id,
-				evaluation1.name, "incoming@student.com", student.team);
-
-		// We have a 5-member team and a 1-member team.
-		// Therefore, we expect (5*5)+(1*1)=26 submissions.
-		List<SubmissionAttributes> submissions = submissionsLogic.getSubmissionsForEvaluation(course.id, evaluation1.name);
-		assertEquals(26, submissions.size());
-		
-		// Check the same for the other evaluation, to detect any state leakage
-		invokeAddSubmissionsForIncomingMember(course.id,
-				evaluation2.name, "incoming@student.com", student.team);
-		submissions = submissionsLogic.getSubmissionsForEvaluation(course.id, evaluation2.name);
-		assertEquals(26, submissions.size());
-		
-		______TS("moving to new team");
-		
-		invokeAddSubmissionsForIncomingMember(course.id,
-				evaluation1.name, "incoming@student.com", "new team");
-		//There should be one more submission now.
-		submissions = submissionsLogic.getSubmissionsForEvaluation(course.id, evaluation1.name);
-		assertEquals(27, submissions.size());
-		
-		// Check the same for the other evaluation
-		invokeAddSubmissionsForIncomingMember(course.id,
-				evaluation2.name, "incoming@student.com", "new team");
-		//There should be one more submission now.
-		submissions = submissionsLogic.getSubmissionsForEvaluation(course.id, evaluation2.name);
-		assertEquals(27, submissions.size());
-
-		//TODO: test invalid inputs
-
-	}
-	
-	private void invokeAddSubmissionsForIncomingMember(String courseId,
-			String evaluationName, String studentEmail, String newTeam)throws Exception {
-		Method privateMethod = EvaluationsLogic.class.getDeclaredMethod(
-				"addSubmissionsForIncomingMember", new Class[] { String.class,
-						String.class, String.class, String.class });
-		privateMethod.setAccessible(true);
-		Object[] params = new Object[] {courseId,
-				 evaluationName,  studentEmail, newTeam };
-		privateMethod.invoke(EvaluationsLogic.inst(), params);
-	}
-	
 	
 	
 	@AfterClass()
