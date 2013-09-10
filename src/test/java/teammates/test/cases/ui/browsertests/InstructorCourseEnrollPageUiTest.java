829ee83357b32f9f204364b8cd70c36fb71a15bc
Hunur Khanna <hunur.khanna@gmail.com>
2013-09-10 15:18:43 +0800

    Issue 1174: InstructorCourseEnrollPageUiTest fails on some machines due to EOL mismatch Update Issue 1174
    Former-commit-id: 5f0d22d1dae3afbe76cf6117387b429caa799c76
    Former-commit-id: 915dfdcc3b42f266e243a5b1deaf02cc6a3bb456
    Former-commit-id: fd9bbb416ffd86d6c90c150f38832139e7b64988

--- src/test/java/teammates/test/cases/ui/browsertests/InstructorCourseEnrollPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorCourseEnrollPageUiTest.java
@@ -73,17 +73,8 @@ public class InstructorCourseEnrollPageUiTest extends BaseUiTestCase {
 	private void testSampleLink() throws Exception {
 		
 		______TS("link for the sample spreadsheet");
-		/*
-		 * TODO: The "\n" used below are just used temporarily for passing the test cases.
-		 * 	Originally the line break character should be Const.EOL
-		 */
-		String sampleSpreadsheetContent = "Team 1,Tom Jacobs,tom@email.com,\n" +
-					"Team 1,Jean Wong,jean@email.com,Exchange Student\n" +
-					"Team 1,Ravi Kumar,ravi@email.com,\n" +
-					"Team 2,Chun Ling,ling@coolmai.com,\n" +
-					"Team 2,Desmond Wu,desmond@email.com,\n" +
-					"Team 2,Harsha Silva,harsha@school.com,";
-		enrollPage.verifyDownloadableFile(enrollPage.getSpreadsheetLink(),sampleSpreadsheetContent);
+		
+		enrollPage.verifyDownloadableFile(enrollPage.getSpreadsheetLink(),"B2F8A93F24ACAC5713BCBC42DAF1FDA59F7AE04B");
 	}
 
 	private void testInputValidation() {
