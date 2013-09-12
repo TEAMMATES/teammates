9cc9feb0adbddc8ba567d93612a13e7f4df02ebc
Qiao Liang <QiaoLiang89@gmail.com>
2013-09-12 16:09:04 +0800

    Issue 1130: Use a better strategy to generate course ID for demo courses Update Issue 1130
    Former-commit-id: d0b249f4a8d58628af4a892261fda7e60f42f7bd
    Former-commit-id: 64bbd9fbab1357b56fed155e8b555f03200e1173
    Former-commit-id: b1aefc055434f1fc058efa824255af3bba31daa5

--- src/main/java/teammates/ui/controller/AdminInstructorAccountAddAction.java
+++ src/main/java/teammates/ui/controller/AdminInstructorAccountAddAction.java
@@ -14,20 +14,20 @@ import teammates.common.util.Config;
 import teammates.common.util.Const;
 import teammates.common.util.FileHelper;
 import teammates.common.util.Utils;
+import teammates.common.util.FieldValidator;
 import teammates.logic.api.GateKeeper;
 import teammates.logic.backdoor.BackDoorLogic;
-
 import com.google.gson.Gson;
 
 public class AdminInstructorAccountAddAction extends Action {
 
 	@Override
 	protected ActionResult execute() throws EntityDoesNotExistException {
-		
+
 		new GateKeeper().verifyAdminPrivileges(account);
-		
+
 		AdminHomePageData data = new AdminHomePageData(account);
-		
+
 		data.instructorId = getRequestParam(Const.ParamsNames.INSTRUCTOR_ID);
 		Assumption.assertNotNull(data.instructorId);
 		data.instructorName = getRequestParam(Const.ParamsNames.INSTRUCTOR_NAME);
@@ -36,9 +36,9 @@ public class AdminInstructorAccountAddAction extends Action {
 		Assumption.assertNotNull(data.instructorEmail);
 		data.instructorInstitution = getRequestParam(Const.ParamsNames.INSTRUCTOR_INSTITUTION);
 		Assumption.assertNotNull(data.instructorInstitution);
-		
+
 		String importSampleData = getRequestParam(Const.ParamsNames.INSTRUCTOR_IMPORT_SAMPLE);
-		
+
 		data.instructorId = data.instructorId.trim();
 		data.instructorName = data.instructorName.trim();
 		data.instructorEmail = data.instructorEmail.trim();
@@ -46,19 +46,20 @@ public class AdminInstructorAccountAddAction extends Action {
 
 		if (logic.isInstructor(data.instructorId)) {
 			isError = true;
-			String errorMessage = "The Google ID " + data.instructorId + " is already registered as an instructor";
+			String errorMessage = "The Google ID " + data.instructorId
+					+ " is already registered as an instructor";
 			statusToUser.add(errorMessage);
 			statusToAdmin = Const.ACTION_RESULT_FAILURE + " : " + errorMessage;
 			return createShowPageResult(Const.ViewURIs.ADMIN_HOME, data);
 		}
-		
+
 		try {
-			
+
 			logic.createAccount(data.instructorId,
 					data.instructorName, true,
 					data.instructorEmail,
 					data.instructorInstitution);
-			
+
 			if (importSampleData != null) {
 				importDemoData(data);
 			}
@@ -66,29 +67,31 @@ public class AdminInstructorAccountAddAction extends Action {
 		} catch (Exception e) {
 			isError = true;
 			statusToUser.add(e.getMessage());
-			statusToAdmin = Const.ACTION_RESULT_FAILURE + " : " + e.getMessage();
+			statusToAdmin = Const.ACTION_RESULT_FAILURE + " : "
+					+ e.getMessage();
 			return createShowPageResult(Const.ViewURIs.ADMIN_HOME, data);
 		}
 
-		statusToUser.add("Instructor " + data.instructorName + " has been successfully created");
-		statusToAdmin = "A New Instructor <span class=\"bold\">" + data.instructorName + "</span> has been created.<br>"
-				+ "<span class=\"bold\">Id: </span>" + data.instructorId + "<br>"
-				+ "<span class=\"bold\">Email: </span>" + data.instructorEmail 
-				+ "<span class=\"bold\">Institution: </span>" + data.instructorInstitution;
-		
+		statusToUser.add("Instructor " + data.instructorName
+				+ " has been successfully created");
+		statusToAdmin = "A New Instructor <span class=\"bold\">"
+				+ data.instructorName + "</span> has been created.<br>"
+				+ "<span class=\"bold\">Id: </span>" + data.instructorId
+				+ "<br>"
+				+ "<span class=\"bold\">Email: </span>" + data.instructorEmail
+				+ "<span class=\"bold\">Institution: </span>"
+				+ data.instructorInstitution;
+
 		return createRedirectResult(Const.ActionURIs.ADMIN_HOME_PAGE);
 	}
-	
+
 	private void importDemoData(AdminHomePageData helper)
 			throws EntityAlreadyExistsException,
 			InvalidParametersException, EntityDoesNotExistException {
-		
+
 		String jsonString;
-		String courseId = helper.instructorId.concat("-demo").replace("@", "-at-");
-		if (courseId.length() > 20) {
-			courseId = courseId.substring(courseId.length() - 20);
-		}
-		
+		String courseId = generateDemoCourseId(helper.instructorEmail); 
+
 		jsonString = FileHelper.readStream(Config.class.getClassLoader()
 				.getResourceAsStream("InstructorSampleData.json"));
 
@@ -118,10 +121,107 @@ public class AdminInstructorAccountAddAction extends Action {
 
 		Gson gson = Utils.getTeammatesGson();
 		DataBundle data = gson.fromJson(jsonString, DataBundle.class);
-		
+
 		new BackDoorLogic().persistDataBundle(data);
-		
+
+	}
+
+	/**
+	* Strategy to Generate New Demo Course Id:
+	* 	a.  keep the part of email before "@"
+	* 		replace "@" with "."
+	* 	    replace email host with their first 3 chars. eg, gmail.com -> gma
+	* 	    append "-demo"
+	*       to sum up: lebron@gmail.com -> lebron.gma-demo
+	*       
+	*   b.  if the generated courseId already exists, create another one by appending a integer to the previous courseId.
+	*   	if the newly generate id still exists, increment the id, until we find a feasible one
+	*   	eg.
+	*   	lebron@gmail.com -> lebron.gma-demo  // already exists!
+	*   	lebron@gmail.com -> lebron.gma-demo0 // already exists!
+	*   	lebron@gmail.com -> lebron.gma-demo1 // already exists!
+	*   	...
+	*   	lebron@gmail.com -> lebron.gma-demo99 // already exists!
+	*   	lebron@gmail.com -> lebron.gma-demo100 // found! a feasible id
+	*   
+	*   c.  in any cases(a or b), if generated Id is longer than FieldValidator.COURSE_ID_MAX_LENGTH, shorten the part 
+	*   	before "@" of the intial input email, by continuously remove its last character
+	*	
+	*	@see #generateDemoCourseId(String)  
+	*	@see #generateNextDemoCourseId(String, int)
+	*/
+	
+	/**    
+	* Generate a course ID for demo course, and if the generated id already exists, try another one
+	*   	
+	* @param instructorEmail is the instructor email.
+	* @return generated course id 
+	*/
+	private String generateDemoCourseId(String instructorEmail) {
+		String proposedCourseId = generateNextDemoCourseId(instructorEmail, FieldValidator.COURSE_ID_MAX_LENGTH);
+		while(logic.getCourse(proposedCourseId) != null){
+			proposedCourseId = generateNextDemoCourseId(proposedCourseId, FieldValidator.COURSE_ID_MAX_LENGTH);
+		}
+		return proposedCourseId;
 	}
 	
+	/**    
+	* Generate a course ID for demo course from a given email
+	*   	
+	* @param instructorEmail is the instructor email.
+	* @return the first proposed course id. eg.lebron@gmail.com -> lebron.gma-demo
+	*/
+	private String getDemoCourseIdRoot(String instructorEmail){
+		final String[] splitedEmail = instructorEmail.split("@");
+		final String head = splitedEmail[0];
+		final String emailAbbreviation = splitedEmail[1].substring(0, 3);
+		return head + "." + emailAbbreviation
+				+ "-demo";
+	}
+	
+	/**    
+	* Generate a course ID for demo course from a given email or a generated course Id
+	* here we check the input string is a email or course Id and handle them accordingly
+	* check the resulting course id, and if bigger than maximumIdLength, cut it so that it equals maximumIdLength
+	* 
+	* @param instructorEmailOrProposedCourseId is the instructor email or a proposed course id that already exists.
+	* @param maximumIdLength is the maximum resulting id length allowed, above which we will cut the part before "@" 
+	* @return the proposed course id. 
+	* 	eg.
+	* 		lebron@gmail.com -> lebron.gma-demo
+	* 		lebron.gma-demo -> lebron.gma-demo0
+	* 		lebron.gma-demo0 -> lebron.gma-demo1
+	* 		012345678901234567890123456789.gma-demo9 -> 01234567890123456789012345678.gma-demo10 (being cut)
+	*/
+	private String generateNextDemoCourseId(String instructorEmailOrProposedCourseId, int maximumIdLength){
+		final boolean isFirstCourseId = instructorEmailOrProposedCourseId.contains("@");
+		if(isFirstCourseId){
+			return trimCourseIdToMaximumLengthIfNecessary(getDemoCourseIdRoot(instructorEmailOrProposedCourseId)
+					, maximumIdLength);
+		} else {
+			final boolean isFirstTimeDuplicate = instructorEmailOrProposedCourseId.endsWith("-demo"); 
+			if(isFirstTimeDuplicate){
+				return trimCourseIdToMaximumLengthIfNecessary(instructorEmailOrProposedCourseId + "0"
+						, maximumIdLength);
+			} else {
+				final int lastIndexOfDemo = instructorEmailOrProposedCourseId.lastIndexOf("-demo");
+				final String root = instructorEmailOrProposedCourseId.substring(0, lastIndexOfDemo);
+				final int previousDedupSuffix = Integer.parseInt(instructorEmailOrProposedCourseId.substring(lastIndexOfDemo + 5));
+				
+				return trimCourseIdToMaximumLengthIfNecessary(root + "-demo" + (previousDedupSuffix+1)
+						, maximumIdLength);
+			}
+		}
+	}
+
+	private String trimCourseIdToMaximumLengthIfNecessary(String demoCourseId, final int maximumIdLength) {
+		final int courseIdLength = demoCourseId.length();
+		if (courseIdLength <= maximumIdLength) {
+			return demoCourseId;
+		} else {
+			return demoCourseId.substring(courseIdLength
+					- maximumIdLength);
+		}
+	}
 
 }
--- src/test/java/teammates/test/cases/ui/AdminInstructorAccountAddActionTest.java
+++ src/test/java/teammates/test/cases/ui/AdminInstructorAccountAddActionTest.java
@@ -1,17 +1,21 @@
 package teammates.test.cases.ui;
 
-import static org.testng.AssertJUnit.assertEquals;
 
+import static org.testng.AssertJUnit.assertEquals;
+import java.lang.reflect.Method;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.util.Const;
+import teammates.common.util.StringHelper;
+import teammates.ui.controller.AdminInstructorAccountAddAction;
 import teammates.ui.controller.Action;
 import teammates.ui.controller.AdminHomePageData;
 import teammates.ui.controller.RedirectResult;
 import teammates.ui.controller.ShowPageResult; 
+
 
 public class AdminInstructorAccountAddActionTest extends BaseActionTest {
 
@@ -36,10 +40,15 @@ public class AdminInstructorAccountAddActionTest extends BaseActionTest {
 		verifyOnlyAdminsCanAccess(submissionParams);
 	}
 
+	@Test
+	public void testGenerateNextDemoCourseId() throws Exception{
+		testGenerateNextDemoCourseIdForLengthLimit(40);
+		testGenerateNextDemoCourseIdForLengthLimit(20);
+	}
 	
 	@Test
 	public void testExecuteAndPostProcess() throws Exception{
-		
+
 		final String newInstructorId = "JamesBond89";
 		final String name = "JamesBond";
 		final String email = "jamesbond89@gmail.com";
@@ -118,8 +127,34 @@ public class AdminInstructorAccountAddActionTest extends BaseActionTest {
 
 	}
 	
+
+	private void testGenerateNextDemoCourseIdForLengthLimit(int maximumIdLength) throws Exception{
+		AdminInstructorAccountAddAction a = new AdminInstructorAccountAddAction();
+		final Method generateNextDemoCourseId;
+		generateNextDemoCourseId = a.getClass().getDeclaredMethod("generateNextDemoCourseId", String.class, int.class);
+		generateNextDemoCourseId.setAccessible(true);
+		final String normalIdSuffix = ".gma-demo";
+		final String atEmail = "@gmail.com";
+		final int normalIdSuffixLength = normalIdSuffix.length();  //9
+		final String strShortWithWordDemo = StringHelper.generateStringOfLength((maximumIdLength - normalIdSuffixLength)/2) + "-demo";
+		final String strWayShorterThanMaxium = StringHelper.generateStringOfLength((maximumIdLength - normalIdSuffixLength)/2);
+		final String strOneCharShorterThanMaximum = StringHelper.generateStringOfLength(maximumIdLength - normalIdSuffixLength);
+		final String strOneCharLongerThanMaximum = StringHelper.generateStringOfLength(maximumIdLength - normalIdSuffixLength + 1); 
+		assertEquals("Case email input: normal short email with word 'demo' with maximumIdLength:" + maximumIdLength,strShortWithWordDemo + normalIdSuffix, generateNextDemoCourseId.invoke(a, strShortWithWordDemo + atEmail, maximumIdLength));
+		assertEquals("Case courseId input: normal short email with word 'demo', no index with maximumIdLength:" + maximumIdLength,strShortWithWordDemo + normalIdSuffix + "0", generateNextDemoCourseId.invoke(a, strShortWithWordDemo + normalIdSuffix, maximumIdLength));
+		assertEquals("Case courseId input: normal short email with word 'demo', index is '0' with maximumIdLength:" + maximumIdLength,strShortWithWordDemo + normalIdSuffix + "1", generateNextDemoCourseId.invoke(a, strShortWithWordDemo + normalIdSuffix + "0", maximumIdLength));
+		assertEquals("Case email input: normal short email with maximumIdLength:" + maximumIdLength,strWayShorterThanMaxium + normalIdSuffix, generateNextDemoCourseId.invoke(a, strWayShorterThanMaxium + atEmail, maximumIdLength));
+		assertEquals("Case email input: one char shorter than maximumIdLength:" + maximumIdLength,strOneCharShorterThanMaximum + normalIdSuffix,generateNextDemoCourseId.invoke(a, strOneCharShorterThanMaximum + atEmail, maximumIdLength));
+		assertEquals("Case email input: one char longer than maximumIdLength:" + maximumIdLength,strOneCharLongerThanMaximum.substring(1) + normalIdSuffix,generateNextDemoCourseId.invoke(a, strOneCharLongerThanMaximum + atEmail, maximumIdLength));
+		assertEquals("Case courseId input: no index with maximumIdLength:" + maximumIdLength,strWayShorterThanMaxium + normalIdSuffix + "0",generateNextDemoCourseId.invoke(a, strWayShorterThanMaxium + normalIdSuffix, maximumIdLength));
+		assertEquals("Case courseId input: index is '0' with maximumIdLength:" + maximumIdLength,strWayShorterThanMaxium + normalIdSuffix + "1",generateNextDemoCourseId.invoke(a, strWayShorterThanMaxium + normalIdSuffix + "0", maximumIdLength));
+		assertEquals("Case courseId input: index is '9', short ID with maximumIdLength:" + maximumIdLength,strWayShorterThanMaxium + normalIdSuffix + "10",generateNextDemoCourseId.invoke(a, strWayShorterThanMaxium + normalIdSuffix + "9", maximumIdLength));
+		assertEquals("Case courseId input: index is '9', short ID boundary with maximumIdLength:" + maximumIdLength,strOneCharShorterThanMaximum.substring(2) + normalIdSuffix + "10",generateNextDemoCourseId.invoke(a, strOneCharShorterThanMaximum.substring(1) + normalIdSuffix + "9", maximumIdLength));
+	}
+
 	private Action getAction(String... parameters) throws Exception {
 		return (Action)gaeSimulation.getActionObject(uri, parameters);
 	}
+
 
 }
