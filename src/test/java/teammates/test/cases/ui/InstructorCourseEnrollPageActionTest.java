bbeee275745e737cc548d026c8eef6d2ac0612ac
Theong Siang Oo <OTS.Nexus@gmail.com>
2013-09-19 15:23:47 +0800

    Issue 1186: Complete InstructorCourseEnrollPageActionTest Update Issue 1186
    Former-commit-id: f0e46bd8c776af765eaf7ad2a83612f8c9e0c72b
    Former-commit-id: a737e7433b78dcda1701782e248e654ee99d5ed4
    Former-commit-id: 8b655f85f00a1316748ce079d12fce815f58983a

--- src/test/java/teammates/test/cases/ui/InstructorCourseEnrollPageActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorCourseEnrollPageActionTest.java
@@ -1,11 +1,17 @@
 package teammates.test.cases.ui;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.util.Const;
+import teammates.test.driver.AssertHelper;
+import teammates.ui.controller.InstructorCourseEnrollPageAction;
+import teammates.ui.controller.InstructorCourseEnrollPageData;
+import teammates.ui.controller.ShowPageResult;
 
 public class InstructorCourseEnrollPageActionTest extends BaseActionTest {
 
@@ -36,7 +42,33 @@ public class InstructorCourseEnrollPageActionTest extends BaseActionTest {
 	@Test
 	public void testExecuteAndPostProcess() throws Exception{
 		
-		//TODO: implement this
+		______TS("Typical case: open the enroll page");
+		
+		String instructorId = dataBundle.instructors.get("instructor1OfCourse1").googleId;
+		gaeSimulation.loginAsInstructor(instructorId);
+		
+		String courseId = dataBundle.instructors.get("instructor1OfCourse1").courseId;
+		String[] submissionParams = new String[]{
+				Const.ParamsNames.COURSE_ID, courseId
+		};
+		InstructorCourseEnrollPageAction action = getAction(submissionParams);
+		
+		ShowPageResult result = getShowPageResult(action);
+		assertEquals(Const.ViewURIs.INSTRUCTOR_COURSE_ENROLL, result.destination);
+		assertEquals(false, result.isError);
+		assertEquals("", result.getStatusMessage());
+		
+		InstructorCourseEnrollPageData data = (InstructorCourseEnrollPageData) result.data;
+		assertEquals(courseId, data.courseId);
+		assertEquals("", data.enrollStudents);
+		
+		String expectedLogSegment = "instructorCourseEnroll Page Load<br>"
+				+ "Enrollment for Course <span class=\"bold\">[" + courseId + "]</span>"; 
+		AssertHelper.assertContains(expectedLogSegment, action.getLogMessage());
+		
 	}
 
+	private InstructorCourseEnrollPageAction getAction(String... params) throws Exception {
+		return (InstructorCourseEnrollPageAction) (gaeSimulation.getActionObject(uri, params));
+	}
 }
