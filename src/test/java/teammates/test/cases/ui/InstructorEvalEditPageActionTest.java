83054ca9304de27fd8fe4039708b13b282e85d90
Hunur Khanna <hunur.khanna@gmail.com>
2013-09-19 15:25:53 +0800

    Issue 1182: Complete TODO for InstructorEvalEditPageActionTest Update Issue 1182
    Former-commit-id: eab61b5fd30e1424564940e9dd2032d3241366ef
    Former-commit-id: e751977dcf6e49d22bdedf14e32e23dcdf2fab87
    Former-commit-id: 6b561bc2fc3e68b41c448457e0a1916a2d4d1fcd

--- src/test/java/teammates/test/cases/ui/InstructorEvalEditPageActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorEvalEditPageActionTest.java
@@ -1,12 +1,19 @@
 package teammates.test.cases.ui;
 
+import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.assertEquals;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.EvaluationAttributes;
+import teammates.common.datatransfer.InstructorAttributes;
+import teammates.common.exception.UnauthorizedAccessException;
 import teammates.common.util.Const;
+import teammates.ui.controller.InstructorEvalEditPageAction;
+import teammates.ui.controller.InstructorEvalEditPageData;
+import teammates.ui.controller.ShowPageResult;
 
 public class InstructorEvalEditPageActionTest extends BaseActionTest {
 
@@ -42,8 +49,57 @@ public class InstructorEvalEditPageActionTest extends BaseActionTest {
 	@Test
 	public void testExecuteAndPostProcess() throws Exception{
 		
-		//TODO: implement this
+		InstructorAttributes instructor1OfCourse1 = dataBundle.instructors.get("instructor1OfCourse1");
+		String instructorId = instructor1OfCourse1.googleId;
+		
+		______TS("Typical case, open edit page for existing evaluation");
+		
+		EvaluationAttributes evaluationInCourse1 = dataBundle.evaluations.get("evaluation1InCourse1");
+
+		gaeSimulation.loginAsInstructor(instructorId);
+		
+		InstructorEvalEditPageAction validPageAction = getAction(
+				Const.ParamsNames.COURSE_ID,evaluationInCourse1.courseId,
+				Const.ParamsNames.EVALUATION_NAME,evaluationInCourse1.name);
+		ShowPageResult successPageResult = getShowPageResult(validPageAction);
+		
+		//Verify URL and user status messages
+		assertEquals(
+				Const.ViewURIs.INSTRUCTOR_EVAL_EDIT+"?error="+successPageResult.isError+"&user="+instructorId,
+				successPageResult.getDestinationWithParams());
+		assertEquals(false,successPageResult.isError);
+		assertEquals("", successPageResult.getStatusMessage());
+		
+		//Verify InstructorEvalEditPageData contents
+		InstructorEvalEditPageData pageData = (InstructorEvalEditPageData) successPageResult.data;
+		
+		assertEquals(instructorId, pageData.account.googleId);
+		assertEquals(evaluationInCourse1.name, pageData.evaluation.name);
+		assertEquals(evaluationInCourse1.courseId, pageData.evaluation.courseId);
+		
+		______TS("Error case, edit a non-existing evaluation");
+		
+		String evalName = "DoesNotExist";
+		EvaluationAttributes nonExistingEvaluation = dataBundle.evaluations.get(evalName);
+		ShowPageResult nonExistingEvalPageResult = null;
+		
+		assertNull(nonExistingEvaluation);
+		
+		try {
+			InstructorEvalEditPageAction nonExistingEvalPageAction = getAction(
+					Const.ParamsNames.COURSE_ID,evaluationInCourse1.courseId,
+					Const.ParamsNames.EVALUATION_NAME,evalName);
+			nonExistingEvalPageResult = getShowPageResult(nonExistingEvalPageAction);
+		} catch(UnauthorizedAccessException e) {
+			assertEquals("Trying to access system using a non-existent evaluation entity", e.getMessage());
+		}
+		
+		assertNull(nonExistingEvalPageResult);
+		
 	}
 	
+	private InstructorEvalEditPageAction getAction(String... params) throws Exception{
+		return (InstructorEvalEditPageAction) (gaeSimulation.getActionObject(uri, params));
+	}
 
 }
