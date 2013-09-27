234f7cf2b5504fa75525c06b3b96f5b5cd0e52e0
Oo Theong Siang <none@none>
2013-09-27 09:49:04 +0800

    Issue 1206: Complete InstructorCourseEditPageActionTest Update Issue 1206
    Former-commit-id: a648c0bdf34e155c8704707be36f95307983ec4a
    Former-commit-id: a3944aaa6574bb19032bb0cf589b85727b7db4cd
    Former-commit-id: 6e1436b9c25262d2a155c351ceb52d5705a72a99

--- src/test/java/teammates/test/cases/ui/InstructorCourseEditPageActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorCourseEditPageActionTest.java
@@ -1,11 +1,23 @@
 package teammates.test.cases.ui;
 
+import static org.testng.AssertJUnit.assertEquals;
+
+import java.util.List;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.InstructorAttributes;
+import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.util.Const;
+import teammates.logic.core.CoursesLogic;
+import teammates.logic.core.InstructorsLogic;
+import teammates.test.driver.AssertHelper;
+import teammates.ui.controller.InstructorCourseEditPageAction;
+import teammates.ui.controller.InstructorCourseEditPageData;
+import teammates.ui.controller.ShowPageResult;
 
 public class InstructorCourseEditPageActionTest extends BaseActionTest {
 
@@ -26,7 +38,6 @@ public class InstructorCourseEditPageActionTest extends BaseActionTest {
 	@Test
 	public void testAccessControl() throws Exception{
 		
-		
 		String[] submissionParams = new String[]{
 				Const.ParamsNames.COURSE_ID, dataBundle.instructors.get("instructor1OfCourse1").courseId
 		};
@@ -38,7 +49,91 @@ public class InstructorCourseEditPageActionTest extends BaseActionTest {
 	@Test
 	public void testExecuteAndPostProcess() throws Exception{
 		
-		//TODO: implement this
+		______TS("Typical case: open the course edit page");
+		
+		InstructorAttributes instructor1OfCourse1 = dataBundle.instructors.get("instructor1OfCourse1");
+		String instructorId = instructor1OfCourse1.googleId;
+		gaeSimulation.loginAsInstructor(instructorId);
+		
+		String courseId = instructor1OfCourse1.courseId;
+		String[] submissionParams = new String[]{
+				Const.ParamsNames.COURSE_ID, courseId
+		};
+		
+		InstructorCourseEditPageAction action = getAction(submissionParams);
+		ShowPageResult result = getShowPageResult(action);
+		assertEquals(Const.ViewURIs.INSTRUCTOR_COURSE_EDIT, result.destination);
+		assertEquals(false, result.isError);
+		assertEquals("", result.getStatusMessage());
+		
+		InstructorCourseEditPageData data = (InstructorCourseEditPageData) result.data;
+		assertEquals(CoursesLogic.inst().getCourse(courseId).toString(), data.course.toString());
+		verifySameInstructorList(InstructorsLogic.inst().getInstructorsForCourse(courseId), data.instructorList);
+		
+		String expectedLogSegment = "instructorCourseEdit Page Load<br>"
+				+ "Editing information for Course <span class=\"bold\">["
+				+ courseId + "]</span>";
+		AssertHelper.assertContains(expectedLogSegment, action.getLogMessage());
+		
+		______TS("Masquerade mode");
+		
+		InstructorAttributes instructor = dataBundle.instructors.get("instructor4");
+		instructorId = instructor.googleId;
+		
+		gaeSimulation.loginAsAdmin("admin.user");
+		
+		courseId = instructor.courseId;
+		submissionParams = new String[]{
+				Const.ParamsNames.USER_ID, instructorId,
+				Const.ParamsNames.COURSE_ID, courseId
+		};
+		
+		action = getAction(submissionParams);
+		result = getShowPageResult(action);
+		assertEquals(Const.ViewURIs.INSTRUCTOR_COURSE_EDIT, result.destination);
+		assertEquals(false, result.isError);
+		assertEquals("", result.getStatusMessage());
+		
+		data = (InstructorCourseEditPageData) result.data;
+		assertEquals(CoursesLogic.inst().getCourse(courseId).toString(), data.course.toString());
+		verifySameInstructorList(InstructorsLogic.inst().getInstructorsForCourse(courseId), data.instructorList);
+		
+		expectedLogSegment = "instructorCourseEdit Page Load<br>"
+				+ "Editing information for Course <span class=\"bold\">["
+				+ courseId + "]</span>";
+		AssertHelper.assertContains(expectedLogSegment, action.getLogMessage());
+		
+		______TS("Failure case: edit a non-existing course");
+		
+		CoursesLogic.inst().deleteCourseCascade(courseId);
+		
+		submissionParams = new String[]{
+				Const.ParamsNames.USER_ID, instructorId,
+				Const.ParamsNames.COURSE_ID, courseId
+		};
+			
+		try {
+			action = getAction(submissionParams);
+			result = getShowPageResult(action);
+			signalFailureToDetectException();
+		} catch (EntityDoesNotExistException e) {
+			assertEquals("Course "+courseId+" does not exist", e.getMessage());
+		}
+
 	}
 
+	private InstructorCourseEditPageAction getAction(String... params) throws Exception {
+		return (InstructorCourseEditPageAction) (gaeSimulation.getActionObject(uri, params));
+	}
+	
+	private void verifySameInstructorList(
+			List<InstructorAttributes> list1,
+			List<InstructorAttributes> list2) {
+		
+		assertEquals(list1.size(), list2.size());
+		
+		for (int i = 0; i < list1.size(); i++) {
+			assertEquals(list1.get(i).toString(), list2.get(i).toString());
+		}
+	}
 }
