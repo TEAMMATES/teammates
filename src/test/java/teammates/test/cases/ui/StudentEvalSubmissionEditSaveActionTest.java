84e024e540627a382fcedc3a87011b784e86d93b
Yuan Rui <yuanruiLYR@gmail.com>
2013-11-16 01:16:55 +0800

    Issue 1200: studenEvalSubmission: give better error message when submitting after deadline Update Issue 1200
    Former-commit-id: a02396544de99b991c8c7f92238fb42e381d3ec9
    Former-commit-id: e13a53ba49559eb8b73fc7a51b809b695485e166
    Former-commit-id: 69b0f1aa90adc394ab0f3e4d0f327ed32ef0314f

--- src/test/java/teammates/test/cases/ui/StudentEvalSubmissionEditSaveActionTest.java
+++ src/test/java/teammates/test/cases/ui/StudentEvalSubmissionEditSaveActionTest.java
@@ -12,8 +12,12 @@ import teammates.common.datatransfer.EvaluationAttributes.EvalStatus;
 import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.datatransfer.SubmissionAttributes;
+import teammates.common.exception.UnauthorizedAccessException;
 import teammates.common.util.Const;
+import teammates.common.util.TimeHelper;
 import teammates.storage.api.EvaluationsDb;
+import teammates.ui.controller.ActionResult;
+import teammates.ui.controller.StudentEvalSubmissionEditSaveAction;
 
 public class StudentEvalSubmissionEditSaveActionTest extends BaseActionTest {
 
@@ -84,7 +88,102 @@ public class StudentEvalSubmissionEditSaveActionTest extends BaseActionTest {
 		
 		//TODO: implement this
 		//TODO: ensure uneditable if not OPEN
+		gaeSimulation.loginAsStudent(studentId);
+		EvaluationAttributes eval = dataBundle.evaluations.get("evaluation1InCourse1");
+		SubmissionAttributes sub = dataBundle.submissions.get("submissionFromS1C1ToS2C1");
+		StudentAttributes student1InCourse1 = dataBundle.students.get("student1InCourse1");
 		
+		String submissionFailMessage = new String();
+		
+		String[] submissionParams = new String[]{
+				Const.ParamsNames.COURSE_ID, eval.courseId,
+				Const.ParamsNames.EVALUATION_NAME, eval.name,
+				Const.ParamsNames.FROM_EMAIL, dataBundle.students.get("student1InCourse1").email,
+				Const.ParamsNames.TEAM_NAME, sub.team,
+				Const.ParamsNames.TO_EMAIL, sub.reviewee,
+				Const.ParamsNames.POINTS, sub.points+"",
+				Const.ParamsNames.JUSTIFICATION, sub.justification.toString(),
+				Const.ParamsNames.COMMENTS, sub.p2pFeedback.toString()
+				
+				};
+		StudentEvalSubmissionEditSaveAction a= getAction(submissionParams);
+		______TS("opened");
+		
+		eval.endTime = TimeHelper.getDateOffsetToCurrentTime(1);
+		evaluationsDb.updateEvaluation(eval);
+		
+		assertEquals(true, eval.getStatus()==EvalStatus.OPEN);
+		assertEquals(false, eval.getStatus()==EvalStatus.CLOSED);
+		assertEquals(false, eval.getStatus()==EvalStatus.PUBLISHED);
+		assertEquals(false, eval.getStatus()==EvalStatus.AWAITING);
+		assertEquals(false, eval.getStatus()==EvalStatus.DOES_NOT_EXIST);
+
+		ActionResult r = a.executeAndPostProcess();
+		
+		assertEquals(
+				Const.ActionURIs.STUDENT_HOME_PAGE+"?message=Your+submission+for+evaluation1+In+Course1+"
+				+"in+course+"+eval.courseId +"+has+been+saved+successfully&error=false&user="+ 
+						student1InCourse1.googleId,r.getDestinationWithParams());
+		
+		assertEquals(false, r.isError);
+		
+		______TS("closed");
+		eval.endTime = TimeHelper.getDateOffsetToCurrentTime(-10);
+		
+		evaluationsDb.updateEvaluation(eval);
+		assertEquals(true, eval.getStatus()==EvalStatus.CLOSED);
+		assertEquals(false, eval.getStatus()==EvalStatus.OPEN);
+		assertEquals(false, eval.getStatus()==EvalStatus.PUBLISHED);
+		assertEquals(false, eval.getStatus()==EvalStatus.AWAITING);
+		assertEquals(false, eval.getStatus()==EvalStatus.DOES_NOT_EXIST);
+			
+		try{
+			r = a.executeAndPostProcess();
+		}
+		catch(UnauthorizedAccessException e){
+			submissionFailMessage= e.getMessage();
+		}
+		assertEquals(Const.Tooltips.EVALUATION_STATUS_CLOSED, submissionFailMessage);
+		
+	
+	______TS("published");
+	
+		eval.published = true;
+		assertEquals(EvalStatus.PUBLISHED, eval.getStatus());
+		assertEquals(false, eval.getStatus()==EvalStatus.OPEN);
+		assertEquals(false, eval.getStatus()==EvalStatus.CLOSED);
+		assertEquals(false, eval.getStatus()==EvalStatus.AWAITING);
+		assertEquals(false, eval.getStatus()==EvalStatus.DOES_NOT_EXIST);
+		evaluationsDb.updateEvaluation(eval);
+		try{
+			r = a.executeAndPostProcess();
+		}
+		catch(UnauthorizedAccessException e){
+			submissionFailMessage= e.getMessage();
+		}
+		assertEquals(Const.Tooltips.EVALUATION_STATUS_PUBLISHED, submissionFailMessage);
+	
+	______TS("awaiting");
+	
+		eval.startTime = TimeHelper.getDateOffsetToCurrentTime(1);
+		eval.endTime = TimeHelper.getDateOffsetToCurrentTime(2);
+		eval.setDerivedAttributes();
+		assertEquals(EvalStatus.AWAITING, eval.getStatus());
+		assertEquals(false, eval.getStatus()==EvalStatus.OPEN);
+		assertEquals(false, eval.getStatus()==EvalStatus.PUBLISHED);
+		assertEquals(false, eval.getStatus()==EvalStatus.CLOSED);
+		assertEquals(false, eval.getStatus()==EvalStatus.DOES_NOT_EXIST);
+		evaluationsDb.updateEvaluation(eval);
+		try{
+			r = a.executeAndPostProcess();
+		}
+		catch(UnauthorizedAccessException e){
+			submissionFailMessage= e.getMessage();
+		}
+		assertEquals(Const.Tooltips.EVALUATION_STATUS_AWAITING, submissionFailMessage);
+	}
+	private StudentEvalSubmissionEditSaveAction getAction(String... params) throws Exception{
+		return (StudentEvalSubmissionEditSaveAction) (gaeSimulation.getActionObject(uri, params));
 	}
 	
 }
