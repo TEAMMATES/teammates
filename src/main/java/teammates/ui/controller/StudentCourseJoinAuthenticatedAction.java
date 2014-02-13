426f07a234816dd2296127eb00be08fbf2b00df2
Arnold Koroa <arnold.koroa@gmail.com>
2014-02-13 14:55:44 +0800

    Issue 1610: Add more logging to join action Update Issue 1610 Status: Delivered
    Former-commit-id: 6f3fe08f2963ab6deb36cd4a0cedc377cd6138a8
    Former-commit-id: d88a7cd271fe10916e4b6143d5a786bcfbf2907d
    Former-commit-id: 1c65653ff5f66a465cdecf142fe035bca697f069

--- src/main/java/teammates/ui/controller/StudentCourseJoinAuthenticatedAction.java
+++ src/main/java/teammates/ui/controller/StudentCourseJoinAuthenticatedAction.java
@@ -1,5 +1,6 @@
 package teammates.ui.controller;
 
+import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.exception.EntityAlreadyExistsException;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
@@ -7,7 +8,6 @@ import teammates.common.exception.JoinCourseException;
 import teammates.common.util.Assumption;
 import teammates.common.util.Const;
 import teammates.common.util.Sanitizer;
-import teammates.common.util.StringHelper;
 import teammates.logic.api.GateKeeper;
 
 /**
@@ -19,27 +19,46 @@ import teammates.logic.api.GateKeeper;
 public class StudentCourseJoinAuthenticatedAction extends Action {
 	@Override
 	protected ActionResult execute() throws EntityDoesNotExistException {
+		//TODO Remove excessive logging from this method
 		String key = getRequestParamValue(Const.ParamsNames.REGKEY);
 		Assumption.assertNotNull(key);
 		
 		new GateKeeper().verifyLoggedInUserPrivileges();
+		
+		String logMsg = null;
 		try {
 			logic.joinCourse(account.googleId, key);
 		} catch (InvalidParametersException
 				| EntityAlreadyExistsException e) {
 			setStatusForException(e, Sanitizer.sanitizeForHtml(e.getMessage()));
+			logMsg = "GAE-level exception not thrown explicitly by Logic <br/>" + e.toString();
 		} catch (JoinCourseException e) {
 			// Does not sanitize for html to allow insertion of mailto link
 			setStatusForException(e, e.getMessage());
+			
+			StudentAttributes student = logic.getStudentForRegistrationKey(key);
+			if (student != null) {
+				logMsg = "Student object for key exists.<br/>Student object information:"
+					+ "<br/>Course: " + student.course
+					+ "<br/>Name: " + student.name 
+					+ "<br/>Email: " + student.email
+					+ "<br/>Id: " + student.googleId;
+			} else {
+				logMsg = "Student object for key not found.";
+			}
 		}
 		
-		final String studentInfo = "Action Student joins course<br>" +
-				"Student (GoogleID): " + account.googleId + "<br>" +
-				"With Key : " + key;
-		if (statusToAdmin != null && !StringHelper.isWhiteSpace(statusToAdmin)){
-			statusToAdmin += ("<br>" + studentInfo);
+		final String studentInfo = "Action Student Joins Course"
+				+ "<br/>Google ID: " + account.googleId
+				+ "<br/>Key : " + key; 
+		if(statusToAdmin != null && !statusToAdmin.trim().isEmpty()) {
+			statusToAdmin += "<br/><br/>" + studentInfo;
 		} else {
-			statusToAdmin = studentInfo;	
+			statusToAdmin = studentInfo;
+		}
+		
+		if(logMsg != null){
+			log.info(logMsg);
 		}
 		
 		RedirectResult response = createRedirectResult(Const.ActionURIs.STUDENT_HOME_PAGE);
