8443f6f709749ff3b5d297ae6ece068fddcfd257
Damith Rajapakse <damith@gmail.com>
2014-02-27 14:41:25 +0800

    Backed out changeset: 234aecf7b1ce (Issue 1545) due to serious flaws
    Former-commit-id: 001e2026fdd7498b1a19b8cc635bc1b783fbbe6f
    Former-commit-id: c00a9998279cfa47554102e7d8bc60957d1e8f83
    Former-commit-id: 3954fb026a1413cd244fb9e900f694a54edc9b1d

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -28,14 +28,8 @@ public class Const {
 		
 		public static final String EMAIL_TASK_QUEUE = "email-queue";
 		public static final String SUBMISSION_TASK_QUEUE = "submission-queue";
-		public static final String EVAL_SUBMISSION_ADJUSTMENT_TASK_QUEUE =
-								"eval-submission-adjust-queue";
-		
 		public static final String EVAL_PUBLISH_EMAIL_TASK_QUEUE = "evaluation-publish-email-queue";
 		public static final String EVAL_REMIND_EMAIL_TASK_QUEUE = "evaluation-remind-email-queue";
-		
-		public static final String FEEDBACK_SUBMISSION_ADJUSTMENT_TASK_QUEUE = 
-								"feedback-submission-adjust-queue";
 		
 		public static final String QUEUE_XML_PATH = "src/main/webapp/WEB-INF/queue.xml";
 	}
@@ -332,12 +326,9 @@ public class Const {
 		public static final String COMMENT_EDITTYPE = "commentedittype";
 		public static final String COMMENT_ID = "commentid";
 		public static final String COMMENT_TEXT = "commenttext";
-		
 		//Submission parameters for Task Queue
 		public static final String SUBMISSION_COURSE = "course";
 		public static final String SUBMISSION_EVAL = "evaluation";
-		
-		public static final String ENROLLMENT_DETAILS = "enrollmentdetails";
 		
 		public static final String SEARCH_KEY ="searchkey";
 		
@@ -448,10 +439,6 @@ public class Const {
 		//Task Queue Worker Servlets URI
 		public static final String EMAIL_WORKER = "/emailWorker";
 		public static final String SUBMISSION_WORKER = "/submissionWorker";
-		public static final String EVAL_SUBMISSION_ADJUSTMENT_WORKER = 
-									"/evalSubmissionAdjustmentWorker";
-		public static final String FEEDBACK_SUBMISSION_ADJUSTMENT_WORKER = 
-									"/feedbackSubmissionAdjustmentWorker";
 		public static final String EVAL_PUBLISH_EMAIL_WORKER = "/evalPublishEmailWorker";
 		public static final String EVAL_REMIND_EMAIL_WORKER = "/evalRemindEmailWorker";
 	}
