56f93d7802c19caff41026f3d9c8ca54de9c1c8c
Huynh Van Quang <none@none>
2014-04-09 20:05:47 +0800

    Issue 1519: InstructorCourseEnrollPage: detect and warn when instructors try to use space as separator Update Issue 1519 Status: Delivered
    Former-commit-id: 34c23bcd91576ad5456bb87a8c240d64931d2018
    Former-commit-id: ea7f03e98042a17f7cd7fe0aad0d214ea8b421a1
    Former-commit-id: d86870d8bd1b7f2b6de33b245a6d4b49268f1093

---
+++ src/test/javascript/InstructorEnrolmentJsTest.js
@@ -0,0 +1,12 @@
+
+
+module('instructorCourseEnrollPage.js');
+
+
+test('isUserTyping(strText)', function(){
+	equal(isUserTyping("no separator \n"),true,"Manually typing, no pipe");
+	equal(isUserTyping("field1 | field2 \n"),false,"Manually typing, with pipe");
+	equal(isUserTyping("navie 	quahuynh@gmail.com 	coder \n"),false,"Copied from spreadsheet");
+	
+});
+
