0c41b83ef56b45c3bea690e15a000deb1659d05e
Kai33 <gigikie@gmail.com>
2014-05-08 23:43:38 +0800

    handle the case, when users unpublish an unpublished session

--- src/main/java/teammates/ui/controller/InstructorFeedbackUnpublishAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackUnpublishAction.java
@@ -1,5 +1,7 @@
 package teammates.ui.controller;
 
+import teammates.common.datatransfer.FeedbackSessionAttributes;
+import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Assumption;
@@ -13,22 +15,36 @@ public class InstructorFeedbackUnpublishAction extends InstructorFeedbacksPageAc
         
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
+        
+        InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, account.googleId);
+        FeedbackSessionAttributes session = logic.getFeedbackSession(feedbackSessionName, courseId);
         boolean isCreatorOnly = true;
         
         new GateKeeper().verifyAccessible(
-                logic.getInstructorForGoogleId(courseId, account.googleId),
-                logic.getFeedbackSession(feedbackSessionName, courseId),
+                instructor,
+                session,
                 isCreatorOnly);
         
-        try {
-            logic.unpublishFeedbackSession(feedbackSessionName, courseId);
-        } catch (InvalidParametersException e) {
-            Assumption.fail("InvalidParametersException not expected at this point");
+        if(session.isPublished()){
+            try {
+                logic.unpublishFeedbackSession(feedbackSessionName, courseId);
+            } catch (InvalidParametersException e) {
+                Assumption.fail("InvalidParametersException not expected at this point");
+            }
+
+            statusToUser.add(Const.StatusMessages.FEEDBACK_SESSION_UNPUBLISHED);
+            statusToAdmin = "Evaluation <span class=\"bold\">("
+                    + feedbackSessionName + ")</span> " +
+                    "for Course <span class=\"bold\">[" + courseId
+                    + "]</span> unpublished.";
+        } else {
+            isError = true;
+            statusToUser.add(Const.StatusMessages.FEEDBACK_SESSION_UNPUBLISH_FAILED);
+            statusToAdmin = "Evaluation <span class=\"bold\">("
+                    + feedbackSessionName + ")</span> " +
+                    "for Course <span class=\"bold\">[" + courseId
+                    + "]</span> unpublished for more than one time.";
         }
-        
-        statusToUser.add(Const.StatusMessages.FEEDBACK_SESSION_UNPUBLISHED);
-        statusToAdmin = "Evaluation <span class=\"bold\">(" + feedbackSessionName + ")</span> " +
-                "for Course <span class=\"bold\">[" + courseId + "]</span> unpublished.";
         
         return createRedirectResult(Const.ActionURIs.INSTRUCTOR_FEEDBACKS_PAGE);
     }
