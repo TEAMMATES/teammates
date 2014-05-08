bea42a0065460f7aa9b993a2d227b57dda3a5c0e
Kai33 <gigikie@gmail.com>
2014-05-08 22:03:11 +0800

    Revert "clean up InstructorFeedbackUnpublishActionTest.java"
    This reverts commit 13e34031b3958aa984e2e50bfd26d10c573a8d95.

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackUnpublishActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackUnpublishActionTest.java
@@ -1,8 +1,6 @@
 package teammates.test.cases.ui;
 
-import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
-import static org.testng.AssertJUnit.assertFalse;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -13,14 +11,8 @@ import teammates.common.datatransfer.FeedbackSessionAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.TimeHelper;
 import teammates.storage.api.FeedbackSessionsDb;
-import teammates.ui.controller.InstructorFeedbackUnpublishAction;
-import teammates.ui.controller.RedirectResult;
 
 public class InstructorFeedbackUnpublishActionTest extends BaseActionTest {
-    private static final boolean PUBLISHED = true;
-    private static final boolean UNPUBLISHED = false;
-    private static final boolean PRIVATE = true;
-    private static final boolean PUBLIC = false;
     DataBundle dataBundle;
         
     @BeforeClass
@@ -59,101 +51,16 @@ public class InstructorFeedbackUnpublishActionTest extends BaseActionTest {
     
     @Test
     public void testExecuteAndPostProcess() throws Exception{
-        gaeSimulation.loginAsInstructor(dataBundle.instructors.get("instructor1OfCourse1").googleId);
-        FeedbackSessionAttributes session = dataBundle.feedbackSessions.get("session2InCourse1");
-        String[] unpublishParamsForTypicalCases = {
-                Const.ParamsNames.COURSE_ID, session.courseId,
-                Const.ParamsNames.FEEDBACK_SESSION_NAME, session.feedbackSessionName
-        };
-        
-        ______TS("Success Case 1: unpublish published feedback session");
-        
-        makeFeedbackSessionPublished(session);
-        
-        InstructorFeedbackUnpublishAction unpublishAction = getAction(unpublishParamsForTypicalCases);
-        RedirectResult result = (RedirectResult) unpublishAction.executeAndPostProcess();
-        
-        assertEquals(Const.ActionURIs.INSTRUCTOR_FEEDBACKS_PAGE + 
-                "?message=The+feedback+session+has+been+unpublished.&error=false&user=idOfInstructor1OfCourse1"
-                , result.getDestinationWithParams());
-        assertEquals(Const.StatusMessages.FEEDBACK_SESSION_UNPUBLISHED, result.getStatusMessage());
-        assertFalse(result.isError);
-        
-        ______TS("Failed Case 1: unpublish unpublished feedback session");
-        
-        makeFeedbackSessionUnpublished(session);
-        
-        String errorMessage = "";
-        unpublishAction = getAction(unpublishParamsForTypicalCases);
-        try{
-            unpublishAction.executeAndPostProcess();
-        } catch(Throwable e){
-            assertTrue(e instanceof AssertionError);
-            errorMessage = e.getMessage();
-        }
-        assertEquals(errorMessage, "InvalidParametersException not expected at this point");
-        
-        makeFeedbackSessionPublished(session);
-        
-        ______TS("Failed Case 2: unpublish private feedback session");
-
-        makeFeedbackSessionPrivate(session);
-        
-        errorMessage = "";
-        unpublishAction = getAction(unpublishParamsForTypicalCases);
-        try{
-            unpublishAction.executeAndPostProcess();
-        } catch(Throwable e){
-            assertTrue(e instanceof AssertionError);
-            errorMessage = e.getMessage();
-        }
-        assertEquals(errorMessage, "InvalidParametersException not expected at this point");
-        
-        makeFeedbackSessionPublic(session);
-    }
-    
-    private void modifyFeedbackSessionPublishState(FeedbackSessionAttributes session, boolean isPublished) throws Exception {
-        session.startTime = TimeHelper.getDateOffsetToCurrentTime(-2);
-        session.endTime = TimeHelper.getDateOffsetToCurrentTime(-1);
-        if(isPublished){
-            session.resultsVisibleFromTime = TimeHelper.getDateOffsetToCurrentTime(-1);
-            assertTrue(session.isPublished());
-        } else {
-            session.resultsVisibleFromTime = Const.TIME_REPRESENTS_LATER;
-            assertFalse(session.isPublished());
-        }
-        session.sentPublishedEmail = true;
-        new FeedbackSessionsDb().updateFeedbackSession(session);
+        //TODO: implement this
+        //TODO: ensure cannot unpublish if not published already
     }
 
     private void makeFeedbackSessionPublished(FeedbackSessionAttributes session) throws Exception {
-        modifyFeedbackSessionPublishState(session, PUBLISHED);
-    }
-    
-    private void makeFeedbackSessionUnpublished(FeedbackSessionAttributes session) throws Exception {
-        modifyFeedbackSessionPublishState(session, UNPUBLISHED);
-    }
-    
-    private void modifyFeedbackSessionPrivateState(FeedbackSessionAttributes session, boolean isPrivate) throws Exception {
-        if(isPrivate){
-            session.sessionVisibleFromTime = Const.TIME_REPRESENTS_NEVER;
-            assertTrue(session.isPrivateSession());
-        } else {
-            session.sessionVisibleFromTime = TimeHelper.getDateOffsetToCurrentTime(-3);
-            assertFalse(session.isPrivateSession());
-        }
+        session.startTime = TimeHelper.getDateOffsetToCurrentTime(-2);
+        session.endTime = TimeHelper.getDateOffsetToCurrentTime(-1);
+        session.resultsVisibleFromTime = TimeHelper.getDateOffsetToCurrentTime(-1);
+        session.sentPublishedEmail = true;
+        assertTrue(session.isPublished());
         new FeedbackSessionsDb().updateFeedbackSession(session);
-    }
-    
-    private void makeFeedbackSessionPublic(FeedbackSessionAttributes session) throws Exception {
-        modifyFeedbackSessionPrivateState(session, PUBLIC);
-    }
-    
-    private void makeFeedbackSessionPrivate(FeedbackSessionAttributes session) throws Exception {
-        modifyFeedbackSessionPrivateState(session, PRIVATE);
-    }
-    
-    private InstructorFeedbackUnpublishAction getAction(String[] params){
-        return (InstructorFeedbackUnpublishAction) gaeSimulation.getActionObject(uri, params);
     }
 }
