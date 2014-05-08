77fd4428ca905266d88d5999cce25bb80e8310be
Kai33 <gigikie@gmail.com>
2014-05-08 22:21:02 +0800

    for unpublishActionTest, I only test success and failure (due to accessibility). Other failure due to logic will be tested in the logic test

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackUnpublishActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackUnpublishActionTest.java
@@ -1,6 +1,8 @@
 package teammates.test.cases.ui;
 
 import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertEquals;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -11,6 +13,8 @@ import teammates.common.datatransfer.FeedbackSessionAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.TimeHelper;
 import teammates.storage.api.FeedbackSessionsDb;
+import teammates.ui.controller.InstructorFeedbackUnpublishAction;
+import teammates.ui.controller.RedirectResult;
 
 public class InstructorFeedbackUnpublishActionTest extends BaseActionTest {
     DataBundle dataBundle;
@@ -51,8 +55,27 @@ public class InstructorFeedbackUnpublishActionTest extends BaseActionTest {
     
     @Test
     public void testExecuteAndPostProcess() throws Exception{
-        //TODO: implement this
-        //TODO: ensure cannot unpublish if not published already
+        gaeSimulation.loginAsInstructor(dataBundle.instructors.get("instructor1OfCourse1").googleId);
+        FeedbackSessionAttributes session = dataBundle.feedbackSessions.get("session2InCourse1");
+        String[] unpublishParams = {
+                Const.ParamsNames.COURSE_ID, session.courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, session.feedbackSessionName
+        };
+        
+        ______TS("Typical Case: ensure Unpublish Action can be executed");
+        
+        makeFeedbackSessionPublished(session);
+        
+        InstructorFeedbackUnpublishAction unpublishAction = getAction(unpublishParams);
+        RedirectResult result = (RedirectResult) unpublishAction.executeAndPostProcess();
+        
+        String expectedDestination = Const.ActionURIs.INSTRUCTOR_FEEDBACKS_PAGE + 
+                "?message=The+feedback+session+has+been+unpublished." + 
+                "&error=false" + 
+                "&user=idOfInstructor1OfCourse1";
+        assertEquals(expectedDestination, result.getDestinationWithParams());
+        assertEquals(Const.StatusMessages.FEEDBACK_SESSION_UNPUBLISHED, result.getStatusMessage());
+        assertFalse(result.isError);
     }
 
     private void makeFeedbackSessionPublished(FeedbackSessionAttributes session) throws Exception {
@@ -62,5 +85,9 @@ public class InstructorFeedbackUnpublishActionTest extends BaseActionTest {
         session.sentPublishedEmail = true;
         assertTrue(session.isPublished());
         new FeedbackSessionsDb().updateFeedbackSession(session);
+    }
+    
+    private InstructorFeedbackUnpublishAction getAction(String[] params){
+        return (InstructorFeedbackUnpublishAction) gaeSimulation.getActionObject(uri, params);
     }
 }
