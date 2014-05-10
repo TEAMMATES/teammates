919d8630f4e70178f2d21ea842719e9f98e84648
franklingu <franklingujunchao@gmail.com>
2014-05-10 13:57:35 +0800

    full test coverage for this action
    test new instructor to be done further

--- src/test/java/teammates/test/cases/ui/InstructorHomePageActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorHomePageActionTest.java
@@ -1,17 +1,21 @@
 package teammates.test.cases.ui;
 
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.CourseAttributes;
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.EvaluationAttributes;
 import teammates.common.datatransfer.EvaluationAttributes.EvalStatus;
 import teammates.common.util.Const;
 import teammates.common.util.TimeHelper;
 import teammates.logic.core.EvaluationsLogic;
+import teammates.logic.api.Logic;
 import teammates.test.cases.common.EvaluationAttributesTest;
 import teammates.test.driver.AssertHelper;
 import teammates.ui.controller.InstructorHomePageAction;
@@ -68,6 +72,7 @@ public class InstructorHomePageActionTest extends BaseActionTest {
                 "|||iwc@yahoo.com|||instructorHome Page Load<br>Total Courses: 0|||/page/instructorHomePage" ;
         assertEquals(expectedLogMessage, a.getLogMessage());
         
+        
         ______TS("instructor with multiple courses, sort by course id, masquerade mode");
         
         submissionParams = new String[]{
@@ -75,6 +80,7 @@ public class InstructorHomePageActionTest extends BaseActionTest {
         };
         gaeSimulation.loginAsAdmin("admin.user");
         
+        // what is this block of code doing here?
         //create a CLOSED evaluation
         EvaluationAttributes eval = EvaluationAttributesTest.generateValidEvaluationAttributesObject();
         String IdOfCourse2 = dataBundle.courses.get("typicalCourse2").id;
@@ -87,6 +93,7 @@ public class InstructorHomePageActionTest extends BaseActionTest {
         EvaluationsLogic evaluationsLogic = new EvaluationsLogic();
         evaluationsLogic.createEvaluationCascade(eval);
         
+        // what is this block of code doing here?
         //create a PUBLISHED evaluation
         eval.name = "published eval";
         eval.startTime = TimeHelper.getDateOffsetToCurrentTime(-2);
@@ -98,6 +105,13 @@ public class InstructorHomePageActionTest extends BaseActionTest {
         
         //access page in masquerade mode
         String instructorWithMultipleCourses = dataBundle.accounts.get("instructor3").googleId;
+        
+        //create another course for sorting
+        Logic logic = new Logic();
+        String newCourseIdForSorting = "idOfTypicalCourse"; // should be 1st if sort by course id
+        String newCourseNameForSorting = "Typical Course 3"; //should be 3rd if sort by course name 
+        logic.createCourseAndInstructor(instructorWithMultipleCourses, newCourseIdForSorting, newCourseNameForSorting);
+        
         a = getAction(addUserIdToParams(instructorWithMultipleCourses, submissionParams));
         r = getShowPageResult(a);
         
@@ -106,13 +120,64 @@ public class InstructorHomePageActionTest extends BaseActionTest {
         assertEquals("",r.getStatusMessage());
         
         data = (InstructorHomePageData)r.data;
-        assertEquals(2, data.courses.size());
+        assertEquals(3, data.courses.size());
+        String expectedCourse1IdAfterSortByCourseId = "idOfTypicalCourse";
+        String expectedCourse2IdAfterSortByCourseId = "idOfTypicalCourse1";
+        String expectedCourse3IdAfterSortByCourseId = "idOfTypicalCourse2";
+        CourseAttributes actualCourse1AfterSortByCourseId = data.courses.get(0).course;
+        CourseAttributes actualCourse2AfterSortByCourseId = data.courses.get(1).course;
+        CourseAttributes actualCourse3AfterSortByCourseId = data.courses.get(2).course;
+        assertEquals(expectedCourse1IdAfterSortByCourseId, actualCourse1AfterSortByCourseId.id);
+        assertEquals(expectedCourse2IdAfterSortByCourseId, actualCourse2AfterSortByCourseId.id);
+        assertEquals(expectedCourse3IdAfterSortByCourseId, actualCourse3AfterSortByCourseId.id);
         assertEquals(Const.SORT_BY_COURSE_ID, data.sortCriteria);
         
         expectedLogMessage = "TEAMMATESLOG|||instructorHomePage|||instructorHomePage|||true" +
                 "|||Instructor(M)|||Instructor 3 of Course 1 and 2|||idOfInstructor3" +
-                "|||instr3@course1n2.com|||instructorHome Page Load<br>Total Courses: 2|||/page/instructorHomePage" ;
+                "|||instr3@course1n2.com|||instructorHome Page Load<br>Total Courses: 3|||/page/instructorHomePage" ;
         assertEquals(expectedLogMessage, a.getLogMessage());
+        
+        
+        ______TS("instructor with multiple courses, sort by course name, masquerade mode");
+        
+        submissionParams = new String[]{
+                Const.ParamsNames.COURSE_SORTING_CRITERIA, Const.SORT_BY_COURSE_NAME
+        };
+        
+        a = getAction(addUserIdToParams(instructorWithMultipleCourses, submissionParams));
+        r = getShowPageResult(a);
+        
+        assertEquals("/jsp/instructorHome.jsp?error=false&user="+instructorWithMultipleCourses, r.getDestinationWithParams());
+        assertEquals(false, r.isError);
+        assertEquals("",r.getStatusMessage());
+        
+        data = (InstructorHomePageData)r.data;
+        assertEquals(3, data.courses.size());
+        String expectedCourse1IdAfterSortByCourseName = "idOfTypicalCourse1";
+        String expectedCourse2IdAfterSortByCourseName = "idOfTypicalCourse2";
+        String expectedCourse3IdAfterSortByCourseName = "idOfTypicalCourse";
+        CourseAttributes actualCourse1AfterSortByCourseName = data.courses.get(0).course;
+        CourseAttributes actualCourse2AfterSortByCourseName = data.courses.get(1).course;
+        CourseAttributes actualCourse3AfterSortByCourseName = data.courses.get(2).course;
+        assertEquals(expectedCourse1IdAfterSortByCourseName, actualCourse1AfterSortByCourseName.id);
+        assertEquals(expectedCourse2IdAfterSortByCourseName, actualCourse2AfterSortByCourseName.id);
+        assertEquals(expectedCourse3IdAfterSortByCourseName, actualCourse3AfterSortByCourseName.id);
+        assertEquals(Const.SORT_BY_COURSE_NAME, data.sortCriteria);
+        
+        
+        ______TS("instructor with multiple courses, sort by course name, masquerade mode");
+        
+        submissionParams = new String[]{
+                Const.ParamsNames.COURSE_SORTING_CRITERIA, "haha"
+        };
+        
+        try {
+            a = getAction(addUserIdToParams(instructorWithMultipleCourses, submissionParams));
+            r = getShowPageResult(a);
+            fail("The run time exception is not thrown as expected");
+        } catch(RuntimeException e) {
+            assertNotNull(e);
+        }
         
     }
 
