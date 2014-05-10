4cdc96b3dc49084c7badedf76af83d403ca5b80c
Kai33 <gigikie@gmail.com>
2014-05-10 17:21:50 +0800

    clean up *feedbackResultsDownloadActionTest

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackResultsDownloadActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackResultsDownloadActionTest.java
@@ -1,5 +1,8 @@
 package teammates.test.cases.ui;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -7,6 +10,8 @@ import org.testng.annotations.Test;
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.FeedbackSessionAttributes;
 import teammates.common.util.Const;
+import teammates.ui.controller.ActionResult;
+import teammates.ui.controller.InstructorFeedbackResultsDownloadAction;
 
 public class InstructorFeedbackResultsDownloadActionTest extends BaseActionTest {
     DataBundle dataBundle;
@@ -37,6 +42,40 @@ public class InstructorFeedbackResultsDownloadActionTest extends BaseActionTest 
     
     @Test
     public void testExecuteAndPostProcess() throws Exception{
-        //TODO: implement this
+        gaeSimulation.loginAsInstructor(dataBundle.instructors.get("instructor1OfCourse1").googleId);
+        FeedbackSessionAttributes session = dataBundle.feedbackSessions.get("session2InCourse1");
+        String[] paramsNormal = {
+                Const.ParamsNames.COURSE_ID, session.courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, session.feedbackSessionName
+        };
+        String[] paramsWithNullCourseId = {
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, session.feedbackSessionName
+        };
+        String[] paramsWithNullFeedbackSessionName = {
+                Const.ParamsNames.COURSE_ID, session.courseId
+        };
+        
+        ______TS("Typical successful case: results downloadable");
+        
+        InstructorFeedbackResultsDownloadAction action = getAction(paramsNormal);
+        ActionResult result = action.executeAndPostProcess();
+        
+        String expectedDestination = "filedownload?" +
+                "error=false" +
+                "&user=idOfInstructor1OfCourse1";
+        assertEquals(expectedDestination, result.getDestinationWithParams());
+        assertFalse(result.isError);
+        
+        ______TS("Unsuccessful case 1: params with null course id");
+        
+        verifyAssumptionFailure(paramsWithNullCourseId);
+        
+        ______TS("Unsuccessful case 2: params with null feedback session name");
+        
+        verifyAssumptionFailure(paramsWithNullFeedbackSessionName);
+    }
+    
+    private InstructorFeedbackResultsDownloadAction getAction(String[] params){
+        return (InstructorFeedbackResultsDownloadAction) gaeSimulation.getActionObject(uri, params);
     }
 }
