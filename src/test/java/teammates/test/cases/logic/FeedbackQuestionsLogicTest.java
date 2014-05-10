b9f3a6187a78812be4886e63f872901c84a71a8c
Low WeiLin <lwl1991@gmail.com>
2014-05-11 00:23:03 +0800

    Cover branch for invalidParametersException

--- src/test/java/teammates/test/cases/logic/FeedbackQuestionsLogicTest.java
+++ src/test/java/teammates/test/cases/logic/FeedbackQuestionsLogicTest.java
@@ -18,7 +18,10 @@ import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.FeedbackParticipantType;
 import teammates.common.datatransfer.FeedbackQuestionAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
+import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Const;
+import teammates.common.util.FieldValidator;
+import teammates.common.util.StringHelper;
 import teammates.logic.core.AccountsLogic;
 import teammates.logic.core.FeedbackQuestionsLogic;
 import teammates.logic.core.FeedbackResponsesLogic;
@@ -330,6 +333,19 @@ public class FeedbackQuestionsLogicTest extends BaseComponentTestCase {
             assertEquals(e.getMessage(), "Trying to update a feedback question that does not exist.");
         }
         
+        ______TS("failure: invalid parameters");
+        
+        questionToUpdate = getQuestionFromDatastore("qn3InSession1InCourse1");
+        questionToUpdate.giverType = FeedbackParticipantType.TEAMS;
+        questionToUpdate.recipientType = FeedbackParticipantType.OWN_TEAM_MEMBERS;
+        try {
+            fqLogic.updateFeedbackQuestion(questionToUpdate);
+            signalFailureToDetectException("Expected InvalidParametersException not caught.");
+        } catch (InvalidParametersException e){
+            assertEquals(e.getMessage(), String.format(FieldValidator.PARTICIPANT_TYPE_TEAM_ERROR_MESSAGE,
+                                                       questionToUpdate.recipientType.toDisplayRecipientName(),
+                                                       questionToUpdate.giverType.toDisplayGiverName()));
+        }
     }
 
     @Test
