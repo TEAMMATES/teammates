43c63bb0556e58ed518c71bbfff909d895fcb477
Kai33 <gigikie@gmail.com>
2014-05-10 13:37:55 +0800

    add coverage testing for 2 assumptions

--- src/main/java/teammates/ui/controller/InstructorFeedbackPublishAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackPublishAction.java
@@ -15,8 +15,8 @@ public class InstructorFeedbackPublishAction extends InstructorFeedbacksPageActi
         
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
-        Assumption.assertNotNull(courseId);
-        Assumption.assertNotNull(feedbackSessionName);
+        Assumption.assertNotNull(Const.StatusCodes.NULL_PARAMETER, courseId);
+        Assumption.assertNotNull(Const.StatusCodes.NULL_PARAMETER, feedbackSessionName);
         
         InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, account.googleId);
         FeedbackSessionAttributes session = logic.getFeedbackSession(feedbackSessionName, courseId);
--- src/test/java/teammates/test/cases/ui/InstructorFeedbackPublishActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackPublishActionTest.java
@@ -61,16 +61,22 @@ public class InstructorFeedbackPublishActionTest extends BaseActionTest {
     public void testExecuteAndPostProcess() throws Exception{
         gaeSimulation.loginAsInstructor(dataBundle.instructors.get("instructor1OfCourse1").googleId);
         FeedbackSessionAttributes session = dataBundle.feedbackSessions.get("session2InCourse1");
-        String[] publishParams = {
+        String[] paramsNormal = {
                 Const.ParamsNames.COURSE_ID, session.courseId,
                 Const.ParamsNames.FEEDBACK_SESSION_NAME, session.feedbackSessionName
+        };
+        String[] paramsWithNullCourseId = {
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, session.feedbackSessionName
+        };
+        String[] paramsWithNullFeedbackSessionName = {
+                Const.ParamsNames.COURSE_ID, session.courseId
         };
         
         ______TS("Typical successful case: session publishable");
         
         makeFeedbackSessionUnpublished(session);
         
-        InstructorFeedbackPublishAction publishAction = getAction(publishParams);
+        InstructorFeedbackPublishAction publishAction = getAction(paramsNormal);
         RedirectResult result = (RedirectResult) publishAction.executeAndPostProcess();
         
         String expectedDestination = Const.ActionURIs.INSTRUCTOR_FEEDBACKS_PAGE +
@@ -82,11 +88,35 @@ public class InstructorFeedbackPublishActionTest extends BaseActionTest {
                 result.getStatusMessage());
         assertFalse(result.isError);
         
-        ______TS("Unsuccessful case: trying to publish a session not currently unpublished");
+        ______TS("Unsuccessful case 1: params with null course id");
+        
+        String errorMessage = "";
+        publishAction = getAction(paramsWithNullCourseId);
+        try{
+            publishAction.executeAndPostProcess();
+        } catch (Throwable e){
+            assertTrue(e instanceof AssertionError);
+            errorMessage = e.getMessage();
+        }
+        assertEquals(Const.StatusCodes.NULL_PARAMETER, errorMessage);
+        
+        ______TS("Unsuccessful case 2: params with null feedback session name");
+        
+        errorMessage = "";
+        publishAction = getAction(paramsWithNullFeedbackSessionName);
+        try{
+            publishAction.executeAndPostProcess();
+        } catch (Throwable e){
+            assertTrue(e instanceof AssertionError);
+            errorMessage = e.getMessage();
+        }
+        assertEquals(Const.StatusCodes.NULL_PARAMETER, errorMessage);
+        
+        ______TS("Unsuccessful case 3: trying to publish a session not currently unpublished");
         
         makeFeedbackSessionPublished(session);
         
-        publishAction = getAction(publishParams);
+        publishAction = getAction(paramsNormal);
         result = (RedirectResult) publishAction.executeAndPostProcess();
         
         expectedDestination = Const.ActionURIs.INSTRUCTOR_FEEDBACKS_PAGE +
