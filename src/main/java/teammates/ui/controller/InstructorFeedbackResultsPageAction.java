7bc123680dbb87111454d4975c920c58f64bf949
Kai33 <gigikie@gmail.com>
2014-05-13 17:30:58 +0800

    refactor insFbResultsPageAction

--- src/main/java/teammates/ui/controller/InstructorFeedbackResultsPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackResultsPageAction.java
@@ -1,5 +1,7 @@
 package teammates.ui.controller;
 
+import teammates.common.datatransfer.FeedbackSessionAttributes;
+import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.util.Const;
 import teammates.logic.api.GateKeeper;
@@ -15,36 +17,33 @@ public class InstructorFeedbackResultsPageAction extends Action {
         statusToAdmin = "Show instructor feedback result page<br>" +
                 "Session Name: " + feedbackSessionName + "<br>" + 
                 "Course ID: " + courseId;
-        if(courseId==null || feedbackSessionName == null) {
+        if(courseId == null || feedbackSessionName == null) {
             return createRedirectResult(Const.ActionURIs.INSTRUCTOR_HOME_PAGE);
         }
         
+        InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, account.googleId);
+        FeedbackSessionAttributes session = logic.getFeedbackSession(feedbackSessionName, courseId);
+        boolean isCreatorOnly = true;
         new GateKeeper().verifyAccessible(
-                logic.getInstructorForGoogleId(courseId, account.googleId), 
-                logic.getFeedbackSession(feedbackSessionName, courseId),
-                false);
+                instructor, 
+                session,
+                !isCreatorOnly);
         
         InstructorFeedbackResultsPageData data = new InstructorFeedbackResultsPageData(account);
-        data.instructor = logic.getInstructorForGoogleId(courseId, account.googleId);
+        data.instructor = instructor;
         data.bundle = logic.getFeedbackSessionResultsForInstructor(feedbackSessionName, courseId, data.instructor.email);
-        data.sortType = getRequestParamValue(Const.ParamsNames.FEEDBACK_RESULTS_SORTTYPE);
-        
         if(data.bundle == null) {
-            throw new EntityDoesNotExistException("Feedback session "+feedbackSessionName+" does not exist in "+courseId+".");
+            throw new EntityDoesNotExistException(
+                    "Feedback session " + feedbackSessionName + " does not exist in " + courseId + ".");
         }
         
-        if (data.sortType == null) {
-            // default: sort by recipients
-            return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_RECIPIENT, data);
-        }
-        if (data.sortType.equals("table")){
+        data.sortType = getRequestParamValue(Const.ParamsNames.FEEDBACK_RESULTS_SORTTYPE);
+        if (data.sortType != null && data.sortType.equals("table")){
             return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_TABLE, data);
-        } else if (data.sortType.equals("recipient")) {
-            return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_RECIPIENT, data);
-        } else if (data.sortType.equals("giver")) {
+        } else if (data.sortType != null && data.sortType.equals("giver")) {
             return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_GIVER, data);
         } else {
-            // default: sort by recipients
+            // data.sortType == null OR data.sortType.equals("recipient"): sort by recipients
             return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_RECIPIENT, data);
         }
     }
