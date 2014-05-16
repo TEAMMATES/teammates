2d8b645678dcff255a30bb697d9a98a77b08f701
ymoctavia <a0091836@nus.edu.sg>
2014-05-16 15:38:17 +0800

    New: Cleaned up StudentDetailsPageAction, correct the previous code   #1916

--- src/main/java/teammates/ui/controller/StudentCourseDetailsPageAction.java
+++ src/main/java/teammates/ui/controller/StudentCourseDetailsPageAction.java
@@ -6,25 +6,26 @@ import teammates.common.datatransfer.CourseDetailsBundle;
 import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.datatransfer.TeamDetailsBundle;
 import teammates.common.exception.EntityDoesNotExistException;
+import teammates.common.exception.UnauthorizedAccessException;
 import teammates.common.util.Assumption;
 import teammates.common.util.Const;
 import teammates.logic.api.GateKeeper;
 
 public class StudentCourseDetailsPageAction extends Action {
-    
+
     private StudentCourseDetailsPageData data;
-    
 
     @Override
     public ActionResult execute() throws EntityDoesNotExistException {
 
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         Assumption.assertNotNull(courseId);
-        
-        if(!isJoinedCourse(courseId, account.googleId)){
+
+        if (!isJoinedCourse(courseId, account.googleId)) {
             return createPleaseJoinCourseResponse(courseId);
         }
 
+       
         new GateKeeper().verifyAccessible(
                 logic.getStudentForGoogleId(courseId, account.googleId),
                 logic.getCourse(courseId));
@@ -35,11 +36,12 @@ public class StudentCourseDetailsPageAction extends Action {
         data.instructors = logic.getInstructorsForCourse(courseId);
 
         data.student = logic.getStudentForGoogleId(courseId, account.googleId);
-        
+
         data.team = getTeam(logic.getTeamsForCourse(courseId), data.student);
 
         statusToAdmin = "studentCourseDetails Page Load<br>" +
-                "Viewing team details for <span class=\"bold\">[" + courseId + "] " +
+                "Viewing team details for <span class=\"bold\">[" + courseId
+                + "] " +
                 data.courseDetails.course.name + "</span>";
 
         ShowPageResult response = createShowPageResult(
@@ -47,20 +49,19 @@ public class StudentCourseDetailsPageAction extends Action {
         return response;
 
     }
-    
-    private TeamDetailsBundle getTeam(List<TeamDetailsBundle> teams, StudentAttributes student){
-        if(student.team == null || student.team.trim().isEmpty()){
+
+    private TeamDetailsBundle getTeam(List<TeamDetailsBundle> teams,
+            StudentAttributes student) {
+        if (student.team == null || student.team.trim().isEmpty()) {
             return null;
-        }
-        else{
-            for(TeamDetailsBundle team: teams){
-                if(team.name.equals(student.team)){
+        } else {
+            for (TeamDetailsBundle team : teams) {
+                if (team.name.equals(student.team)) {
                     return team;
                 }
             }
             return null;
         }
     }
-    
 
 }
