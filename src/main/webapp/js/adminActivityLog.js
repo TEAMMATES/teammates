acfb30b46f16c3605c8413774742da789942d0e3
ymoctavia <a0091836@nus.edu.sg>
2014-05-28 15:16:52 +0800

    1.applied BootStrap to admin ActivityLog 2.changed only header and footer for admin search

--- src/main/webapp/js/adminActivityLog.js
+++ src/main/webapp/js/adminActivityLog.js
@@ -1,10 +1,23 @@
-function submitForm(offset){
-    $('input[name=offset]').val(offset);
-    $('input[name=pageChange]').val("true");
-    $("#activityLogFilter").submit();
+function submitForm(offset) {
+	$('input[name=offset]').val(offset);
+	$('input[name=pageChange]').val("true");
+	$("#activityLogFilter").submit();
 }
 
-
-function toggleReference(){
-    $("#filterReference").toggle();
+function toggleReference() {
+	$("#filterReference").toggle();
+	
+	var button = $("#detailButton").attr("class");
+	
+	if(button == "glyphicon glyphicon-chevron-down"){
+	$("#detailButton").attr("class","glyphicon glyphicon-chevron-up");
+	$("#referenceText").text("Hide Reference");
+	}else{
+		$("#detailButton").attr("class","glyphicon glyphicon-chevron-down");
+		$("#referenceText").text("Show Reference");
+	}
 }
+
+$(function() {
+	$("#filterReference").toggle();
+});
