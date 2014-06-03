b602b829416dba19bfbf0272c50c3151363375aa
Kai33 <gigikie@gmail.com>
2014-06-03 12:09:48 +0800

    done 'quick access for comment' prototype

--- src/main/webapp/js/omniComment.js
+++ src/main/webapp/js/omniComment.js
@@ -1,7 +1,50 @@
-$('#omni-comment-recipient-input').parent().focusin(function(){
-	$('#omni-comment-recipient-select').show();
+$(function () {
+	focusOmniCommentRecipientInput();
 });
 
-$('#omni-comment-recipient-input').parent().focusout(function(){
-	$('#omni-comment-recipient-select').hide();
+function focusOmniCommentRecipientInput(){
+	$('#omni-comment-dropdown-trigger').click(function () {
+		setTimeout(function(){$('#omni-comment-recipient-input').focus();}, 0);
+	});
+	$('#omni-comment-recipient-input').click(function(e){
+		e.stopPropagation();
+	});
+}
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+ 
+    cb(matches);
+  };
+};
+ 
+var students = ['Kai (Team 1) in CS2103', 'Ashray (Team 2) in CS2103'
+];
+
+$('#omni-comment-recipient-input').typeahead({
+	hint: true,
+	highlight: true,
+	minLength: 1
+},
+{
+	name: 'students',
+	displayKey: 'value',
+	source: substringMatcher(students)
 });
