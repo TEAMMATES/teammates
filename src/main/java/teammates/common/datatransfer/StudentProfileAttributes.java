a26f063e2c76b1f8ddb8406abefd087c28820f35
Thyagesh Manikandan <thyageshm@gmail.com>
2014-06-05 23:36:12 +0800

    Completed StudentProfileAttributes and its test 100%

---
+++ src/main/java/teammates/common/datatransfer/StudentProfileAttributes.java
@@ -0,0 +1,98 @@
+package teammates.common.datatransfer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import teammates.common.util.FieldValidator;
+import teammates.common.util.Sanitizer;
+import teammates.storage.entity.StudentProfile;
+
+public class StudentProfileAttributes extends EntityAttributes {
+
+    public String googleId;
+    public String shortName;
+    public String email;
+    public String institute;
+    public String country;
+    /* only accepts "male", "female" or "other" */
+    public String gender;
+    public String moreInfo;
+
+    public StudentProfileAttributes(String googleId, String shortName, String email,
+            String institute, String country, String gender, String moreInfo) {
+        this.googleId = googleId;
+        this.shortName = shortName;
+        this.email = email;
+        this.institute = institute;
+        this.country = country;
+        this.gender = gender;
+        this.moreInfo = moreInfo;
+    }
+    
+    public StudentProfileAttributes() {
+        // just a container so all can be null
+        this.googleId = null;
+        this.shortName = null;
+        this.email = null;
+        this.institute = null;
+        this.country = null;
+        this.gender = null;
+        this.moreInfo = null;
+    }
+
+    @Override
+    public List<String> getInvalidityInfo() {
+        FieldValidator validator = new FieldValidator();
+        List<String> errors = new ArrayList<String>();
+        String error;
+        
+        error= validator.getInvalidityInfo(FieldValidator.FieldType.GOOGLE_ID, googleId);
+        if(!error.isEmpty()) { errors.add(error); }
+        
+        error= validator.getInvalidityInfo(FieldValidator.FieldType.PERSON_NAME, shortName);
+        if(!error.isEmpty()) { errors.add(error); }
+        
+        error= validator.getInvalidityInfo(FieldValidator.FieldType.EMAIL, email);
+        if(!error.isEmpty()) { errors.add(error); }
+        
+        error= validator.getInvalidityInfo(FieldValidator.FieldType.INSTITUTE_NAME, institute);
+        if(!error.isEmpty()) { errors.add(error); }
+        
+        error= validator.getInvalidityInfo(FieldValidator.FieldType.COUNTRY, country);
+        if(!error.isEmpty()) { errors.add(error); }
+        
+        error= validator.getInvalidityInfo(FieldValidator.FieldType.GENDER, gender);
+        if(!error.isEmpty()) { errors.add(error); }
+        
+        // No validation for More Info. It will properly sanitized.
+        
+        return errors;
+    }
+
+    @Override
+    public Object toEntity() {
+        return new StudentProfile(googleId, shortName, email, institute, country, gender, moreInfo);
+    }
+
+    @Override
+    public String getIdentificationString() {
+        return googleId;
+    }
+
+    @Override
+    public String getEntityTypeAsString() {
+        return "StudentProfile";
+    }
+
+    @Override
+    public void sanitizeForSaving() {
+        this.googleId = Sanitizer.sanitizeGoogleId(this.googleId);
+        this.shortName = Sanitizer.sanitizeForHtml(this.shortName);
+        this.email = Sanitizer.sanitizeForHtml(this.email);
+        this.institute = Sanitizer.sanitizeForHtml(this.institute);
+        this.country = Sanitizer.sanitizeForHtml(this.country);
+        this.gender = Sanitizer.sanitizeForHtml(this.gender);
+        this.moreInfo = Sanitizer.sanitizeForHtml(this.moreInfo);
+    }
+
+}
---
+++ src/test/java/teammates/test/cases/common/StudentProfileAttributesTest.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package teammates.test.cases.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertEquals;
+import teammates.common.datatransfer.StudentProfileAttributes;
+import teammates.common.util.FieldValidator;
+import teammates.common.util.Sanitizer;
+import teammates.common.util.StringHelper;
+import teammates.storage.entity.StudentProfile;
+import teammates.test.cases.BaseTestCase;
+import teammates.test.util.TestHelper;
+
+public class StudentProfileAttributesTest extends BaseTestCase {
+
+    private static StudentProfileAttributes profile;
+    
+    @BeforeClass
+    public static void setupClass() throws Exception {
+        printTestClassHeader();
+        profile = new StudentProfileAttributes();
+        profile.googleId = "valid.google.id";
+        profile.shortName = "shor";
+        profile.institute = "institute";
+        profile.email = "valid@email.com";
+        profile.country = "country";
+        profile.gender = "female";
+        profile.moreInfo = "moreInfo can have a lot more than this...";
+    }
+    
+    @Test
+    public void testGetInvalidityInfo() {
+        StudentProfileAttributes validProfile = profile;
+        StudentProfileAttributes invalidProfile = getInvalidStudentProfileAttributes();
+        
+        ______TS("Valid Profile Attributes");
+        assertTrue("valid: all valid info", validProfile.isValid());
+        assertEquals(new ArrayList<String>(), validProfile.getInvalidityInfo());
+        
+        ______TS("Invalid Profile Attributes");
+        assertFalse("valid: all valid info", invalidProfile.isValid());
+        List<String> expectedErrorMessages = new ArrayList<String>();
+
+        //tests both the constructor and the invalidity info
+        
+        expectedErrorMessages.add(String.format(FieldValidator.WHITESPACE_ONLY_OR_EXTRA_WHITESPACE_ERROR_MESSAGE, profile.googleId));
+        expectedErrorMessages.add(String.format(FieldValidator.PERSON_NAME_ERROR_MESSAGE, profile.shortName, FieldValidator.REASON_EMPTY));
+        expectedErrorMessages.add(String.format(FieldValidator.EMAIL_ERROR_MESSAGE, profile.email, FieldValidator.REASON_INCORRECT_FORMAT));
+        expectedErrorMessages.add(String.format(FieldValidator.INSTITUTE_NAME_ERROR_MESSAGE, profile.institute, FieldValidator.REASON_TOO_LONG));
+        expectedErrorMessages.add(String.format(FieldValidator.COUNTRY_ERROR_MESSAGE, profile.country, FieldValidator.REASON_START_WITH_NON_ALPHANUMERIC_CHAR));
+        expectedErrorMessages.add(String.format(FieldValidator.GENDER_ERROR_MESSAGE, profile.gender));
+        
+        TestHelper.isSameContentIgnoreOrder(expectedErrorMessages, invalidProfile.getInvalidityInfo());
+    }
+    
+    @Test
+    public void testeGetIdentificationString() {
+        assertEquals(profile.googleId, profile.getIdentificationString());
+    }
+    
+    @Test
+    public void testSanitizeForSaving() {
+        StudentProfileAttributes profileToSanitize = getStudentProfileAttributesToSanitize();
+        StudentProfileAttributes profileToSanitizeExpected = getStudentProfileAttributesToSanitize();
+        profileToSanitize.sanitizeForSaving();
+        
+        assertEquals(Sanitizer.sanitizeGoogleId(profileToSanitizeExpected.googleId), profileToSanitize.googleId);
+        assertEquals(Sanitizer.sanitizeForHtml(profileToSanitizeExpected.shortName), profileToSanitize.shortName);
+        assertEquals(Sanitizer.sanitizeForHtml(profileToSanitizeExpected.institute), profileToSanitize.institute);
+        assertEquals(Sanitizer.sanitizeForHtml(profileToSanitizeExpected.email), profileToSanitize.email);
+        assertEquals(Sanitizer.sanitizeForHtml(profileToSanitizeExpected.country), profileToSanitize.country);
+        assertEquals(Sanitizer.sanitizeForHtml(profileToSanitizeExpected.gender), profileToSanitize.gender);
+        assertEquals(Sanitizer.sanitizeForHtml(profileToSanitizeExpected.moreInfo), profileToSanitize.moreInfo);
+    }
+    
+    public StudentProfileAttributes getInvalidStudentProfileAttributes() {
+        
+        String googleId = "invalid ";
+        String shortName = "";
+        String email = "invalid@email@com";
+        String institute = StringHelper.generateStringOfLength(FieldValidator.INSTITUTE_NAME_MAX_LENGTH+1);
+        String country = "$invalid country ";
+        String gender = "invalidGender";
+        String moreInfo = "Ooops no validation for this one...";
+        
+        return new StudentProfileAttributes(googleId, shortName, email, institute, country, gender, moreInfo);
+    }
+    
+    public StudentProfileAttributes getStudentProfileAttributesToSanitize() {
+        
+        String googleId = " samitize@gmail.com ";
+        String shortName = "<name>";
+        String email = " 'toSanitize@email.com'";
+        String institute = "institute/\"";
+        String country = "&\"invalid country &";
+        String gender = "'\"'invalidGender";
+        String moreInfo = "<<script> alert('hi!'); </script>";
+        
+        return new StudentProfileAttributes(googleId, shortName, email, institute, country, gender, moreInfo);
+    }
+    
+    @Test
+    public void testToEntity() {
+        StudentProfile entity = (StudentProfile) profile.toEntity();
+        assertEquals(profile.googleId, entity.getGoogleId());
+        assertEquals(profile.shortName, entity.getShortName());
+        assertEquals(profile.institute, entity.getInstitute());
+        assertEquals(profile.email, entity.getEmail());
+        assertEquals(profile.country, entity.getCountry());
+        assertEquals(profile.gender, entity.getGender());
+        assertEquals(profile.moreInfo, entity.getMoreInfo());
+    }
+    
+    @Test
+    public void testGetEntityTypeAsString() {
+        assertEquals("StudentProfile", profile.getEntityTypeAsString());
+    }
+    
+    @AfterClass
+    public static void tearDown() {
+        printTestClassFooter();
+    }
+
+}
