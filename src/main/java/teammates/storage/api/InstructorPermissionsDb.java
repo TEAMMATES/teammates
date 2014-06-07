550452be663d2a14c1639de220c026b3d64c725d
franklingu <franklingujunchao@gmail.com>
2014-06-07 17:22:38 +0800

    create db file

---
+++ src/main/java/teammates/storage/api/InstructorPermissionsDb.java
@@ -0,0 +1,18 @@
+package teammates.storage.api;
+
+import java.util.logging.Logger;
+
+import teammates.common.datatransfer.EntityAttributes;
+import teammates.common.util.Utils;
+
+public class InstructorPermissionsDb extends EntitiesDb {
+
+    private static final Logger log = Utils.getLogger();
+    
+    @Override
+    protected Object getEntity(EntityAttributes attributes) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
