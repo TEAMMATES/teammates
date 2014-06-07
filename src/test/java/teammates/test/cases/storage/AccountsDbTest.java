5ef1a2fd9e897a819edb54f92474d0922e83baf2
Thyagesh Manikandan <thyageshm@gmail.com>
2014-06-07 12:32:08 +0800

    AccountsDbTest covers all the new functions added to AccountsDb 100%

--- src/test/java/teammates/test/cases/storage/AccountsDbTest.java
+++ src/test/java/teammates/test/cases/storage/AccountsDbTest.java
@@ -7,6 +7,7 @@ import static org.testng.AssertJUnit.assertTrue;
 import static org.testng.AssertJUnit.assertFalse;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.testng.Assert;
@@ -15,6 +16,7 @@ import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.StudentProfileAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Const;
@@ -42,10 +44,12 @@ public class AccountsDbTest extends BaseComponentTestCase {
        ______TS("typical success case");
         AccountAttributes retrieved = accountsDb.getAccount(a.googleId);
         assertNotNull(retrieved);
+        accountsDb.closePM();
         
         ______TS("expect null for non-existent account");
         retrieved = accountsDb.getAccount("non.existent");
         assertNull(retrieved);
+        accountsDb.closePM();
         
         ______TS("failure: null parameter");
         try {
@@ -81,11 +85,21 @@ public class AccountsDbTest extends BaseComponentTestCase {
         
         ______TS("typical success case");
         AccountAttributes a = new AccountAttributes();
+        StudentProfileAttributes spa = new StudentProfileAttributes();
+        spa.shortName = "test acc na";
+        spa.email = "test@personal.com";
+        spa.gender = Const.GenderTypes.MALE;
+        spa.country = "test.country";
+        spa.institute = "institute";
+        spa.moreInfo = "this is more info";
+        
         a.googleId = "test.account";
         a.name = "Test account Name";
         a.isInstructor = false;
         a.email = "fresh-account@email.com";
         a.institute = "National University of Singapore";
+        a.studentProfile = spa;
+        
         accountsDb.createAccount(a);
             
         ______TS("success case: duplicate account");
@@ -93,22 +107,48 @@ public class AccountsDbTest extends BaseComponentTestCase {
         
         ______TS("test persistence of latest entry");
         AccountAttributes accountDataTest = accountsDb.getAccount(a.googleId);
+        
+        assertEquals(spa.shortName, accountDataTest.studentProfile.shortName);
+        // ensure that the institutes are different for now...
+        assertEquals(spa.institute, accountDataTest.studentProfile.institute);
+        assertEquals(a.institute, accountDataTest.institute);
+        assertEquals(spa.email, accountDataTest.studentProfile.email);
+        
         assertFalse(accountDataTest.isInstructor);
         // Change a field
         accountDataTest.isInstructor = true;
+        accountDataTest.studentProfile.gender = Const.GenderTypes.FEMALE;
         accountsDb.updateAccount(accountDataTest);
         // Re-retrieve
         accountDataTest = accountsDb.getAccount(a.googleId);
         assertTrue(accountDataTest.isInstructor);
+        assertEquals(Const.GenderTypes.FEMALE, accountDataTest.studentProfile.gender);
         
-        accountsDb.deleteAccount(a.googleId);
+        accountsDb.closePM();
+        
+        ______TS("success: modified date does not change if profile is not changed");
+        
+        accountDataTest = accountsDb.getAccount(a.googleId);
+        accountDataTest.institute = "new institute";
+        Date expectedModifiedDate = accountDataTest.studentProfile.modifiedDate;
+        
+        accountsDb.updateAccount(accountDataTest);
+        a = accountsDb.getAccount(a.googleId);
+        
+        // ensure update was successful
+        assertEquals(accountDataTest.institute, a.institute);
+        // ensure profile was not updated
+        assertEquals(expectedModifiedDate, a.studentProfile.modifiedDate);
 
+        accountsDb.closePM();
+        
+        
         // Should we not allow empty fields?
         ______TS("failure case: invalid parameter");
         a.email = "invalid email";
         try {
             accountsDb.createAccount(a);
-            signalFailureToDetectException();
+            signalFailureToDetectException(" - InvalidParametersException");
         } catch (InvalidParametersException e) {
             AssertHelper.assertContains(
                     String.format(FieldValidator.EMAIL_ERROR_MESSAGE,
@@ -120,7 +160,7 @@ public class AccountsDbTest extends BaseComponentTestCase {
         ______TS("failure: null parameter");
         try {
             accountsDb.createAccount(null);
-            Assert.fail();
+            signalFailureToDetectException(" - AssertionError");
         } catch (AssertionError ae) {
             assertEquals(Const.StatusCodes.DBLEVEL_NULL_INPUT, ae.getMessage());
         }
@@ -139,7 +179,7 @@ public class AccountsDbTest extends BaseComponentTestCase {
         try {
             a.googleId = "non.existent";
             accountsDb.updateAccount(a);
-            signalFailureToDetectException();
+            signalFailureToDetectException(" - EntityDoesNotExistException");
         } catch (EntityDoesNotExistException edne) {
             AssertHelper.assertContains(AccountsDb.ERROR_UPDATE_NON_EXISTENT_ACCOUNT, edne.getMessage());
         }
@@ -153,6 +193,7 @@ public class AccountsDbTest extends BaseComponentTestCase {
         
         try {
             accountsDb.updateAccount(a);
+            signalFailureToDetectException(" - InvalidParametersException");
         } catch (InvalidParametersException ipe) {
             assertEquals(StringHelper.toString(a.getInvalidityInfo()), ipe.getMessage());
         }
@@ -161,10 +202,19 @@ public class AccountsDbTest extends BaseComponentTestCase {
         ______TS("failure: null parameter");
         try {
             accountsDb.updateAccount(null);
-            Assert.fail();
+            signalFailureToDetectException(" - AssertionError");
         } catch (AssertionError ae) {
             assertEquals(Const.StatusCodes.DBLEVEL_NULL_INPUT, ae.getMessage());
         }
+    }
+
+    @Test 
+    public void testGetStudentProfile() throws Exception{
+        AccountAttributes a = createNewAccount();
+        a = accountsDb.getAccount(a.googleId);
+        StudentProfileAttributes spa = accountsDb.getStudentProfile(a.googleId);
+        
+        assertEquals(a.studentProfile.toString(), spa.toString());
     }
     
     @Test
@@ -184,12 +234,11 @@ public class AccountsDbTest extends BaseComponentTestCase {
         
         try {
             accountsDb.deleteAccount(null);
-            Assert.fail();
+            signalFailureToDetectException(" - AssertionError");
         } catch (AssertionError ae) {
             assertEquals(Const.StatusCodes.DBLEVEL_NULL_INPUT, ae.getMessage());
         }
     }
-
 
     private AccountAttributes createNewAccount() throws Exception {
         AccountAttributes a = new AccountAttributes();
@@ -198,6 +247,8 @@ public class AccountsDbTest extends BaseComponentTestCase {
         a.isInstructor = false;
         a.email = "valid@email.com";
         a.institute = "National University of Singapore";
+        a.studentProfile = new StudentProfileAttributes();
+        a.studentProfile.institute = "National University of Singapore";
         
         accountsDb.createAccount(a);
         return a;
