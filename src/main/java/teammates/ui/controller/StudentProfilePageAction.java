3e9f06cc9030ed351539e6edf622c814f2b49622
Thyagesh Manikandan <thyageshm@gmail.com>
2014-06-09 16:38:17 +0800

    added status message to admin

--- src/main/java/teammates/ui/controller/StudentProfilePageAction.java
+++ src/main/java/teammates/ui/controller/StudentProfilePageAction.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package teammates.ui.controller;
 
 import teammates.common.exception.EntityDoesNotExistException;
@@ -10,19 +7,15 @@ import teammates.logic.api.GateKeeper;
 public class StudentProfilePageAction extends Action {
 
     private PageData data;
-    
-    public StudentProfilePageAction() {
-        // TODO Auto-generated constructor stub
-    }
 
     @Override
     protected ActionResult execute() throws EntityDoesNotExistException {
         new GateKeeper().verifyLoggedInUserPrivileges();
         
         data = new PageData(this.account);
+        statusToAdmin = "studentProfile Page Load <br> Account: " + this.account;
         
         ShowPageResult response = createShowPageResult(Const.ViewURIs.STUDENT_PROFILE_PAGE, data);
-        
         return response;
     }
 }
