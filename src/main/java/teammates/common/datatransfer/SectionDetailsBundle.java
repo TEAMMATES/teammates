026a9874573afc80f683df4d726f40cd4599852f
imouto1994 <imouto1994@gmail.com>
2014-06-09 11:03:10 +0800

    Edit mistake in header comment

--- src/main/java/teammates/common/datatransfer/SectionDetailsBundle.java
+++ src/main/java/teammates/common/datatransfer/SectionDetailsBundle.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /** 
  * Represents details of teams in a section.
  * <br> Contains: 
- * <br> * The team name .
+ * <br> * The section name .
  * <br> * {@link TeamDetailsBundle} objects for all teams in the section.
  */
 public class SectionDetailsBundle {
