c9060982e2ec01906bbb3ed6771267fd3327c3a3
franklingu <franklingujunchao@gmail.com>
2014-06-13 21:06:33 +0800

    ready for merging into dev

--- src/main/java/teammates/logic/backdoor/BackDoorLogic.java
+++ src/main/java/teammates/logic/backdoor/BackDoorLogic.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import teammates.common.datatransfer.AccountAttributes;
