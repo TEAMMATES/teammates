c7a19f61760b94ce666da0ab4c3375f0dba93c4f
Low WeiLin <lwl1991@gmail.com>
2014-06-20 15:39:42 +0800

    Initial code for contribution question

---
+++ src/main/java/teammates/common/datatransfer/FeedbackContributionQuestionDetails.java
@@ -0,0 +1,132 @@
+package teammates.common.datatransfer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import teammates.common.util.Const;
+
+public class FeedbackContributionQuestionDetails extends FeedbackAbstractQuestionDetails {
+    
+    public FeedbackContributionQuestionDetails() {
+        super(FeedbackQuestionType.CONTRIB);
+    }
+
+    public FeedbackContributionQuestionDetails(String questionText) {
+        super(FeedbackQuestionType.CONTRIB, questionText);
+        
+    }
+
+    @Override
+    public String getQuestionTypeDisplayName() {
+        return Const.FeedbackQuestionTypeNames.CONTRIB;
+    }
+    
+    @Override
+    public boolean isChangesRequiresResponseDeletion(FeedbackAbstractQuestionDetails newDetails) {
+        return false;
+    }
+
+    @Override
+    public String getQuestionWithExistingResponseSubmissionFormHtml(boolean sessionIsOpen, int qnIdx,
+            int responseIdx, String courseId, FeedbackAbstractResponseDetails existingResponseDetails) {
+        /*
+        FeedbackMcqResponseDetails existingMcqResponse = (FeedbackMcqResponseDetails) existingResponseDetails;
+        List<String> choices = null;
+        StringBuilder optionListHtml = new StringBuilder();
+        String optionFragmentTemplate = FeedbackQuestionFormTemplates.CONTRIB_SUBMISSION_FORM_OPTIONFRAGMENT;
+        for(int i = 0; i < choices.size(); i++) {
+            String optionFragment = 
+                    FeedbackQuestionFormTemplates.populateTemplate(optionFragmentTemplate,
+                            "${qnIdx}", Integer.toString(qnIdx),
+                            "${responseIdx}", Integer.toString(responseIdx),
+                            "${disabled}", sessionIsOpen ? "" : "disabled=\"disabled\"",
+                            "${checked}", existingMcqResponse.getAnswerString().equals(choices.get(i)) ? "checked=\"checked\"" : "",
+                            "${Const.ParamsNames.FEEDBACK_RESPONSE_TEXT}", Const.ParamsNames.FEEDBACK_RESPONSE_TEXT,
+                            "${mcqChoiceValue}", choices.get(i));
+            optionListHtml.append(optionFragment + Const.EOL);
+        }
+        
+        String html = FeedbackQuestionFormTemplates.populateTemplate(
+                FeedbackQuestionFormTemplates.CONTRIB_SUBMISSION_FORM,
+                "${mcqSubmissionFormOptionFragments}", optionListHtml.toString());
+        */
+        return "";
+    }
+
+    @Override
+    public String getQuestionWithoutExistingResponseSubmissionFormHtml(
+            boolean sessionIsOpen, int qnIdx, int responseIdx, String courseId) {
+        /*
+        List<String> choices = null;
+        
+        StringBuilder optionListHtml = new StringBuilder();
+        String optionFragmentTemplate = FeedbackQuestionFormTemplates.CONTRIB_SUBMISSION_FORM_OPTIONFRAGMENT;
+        for(int i = 0; i < choices.size(); i++) {
+            String optionFragment = 
+                    FeedbackQuestionFormTemplates.populateTemplate(optionFragmentTemplate,
+                            "${qnIdx}", Integer.toString(qnIdx),
+                            "${responseIdx}", Integer.toString(responseIdx),
+                            "${disabled}", sessionIsOpen ? "" : "disabled=\"disabled\"",
+                            "${checked}", "",
+                            "${Const.ParamsNames.FEEDBACK_RESPONSE_TEXT}", Const.ParamsNames.FEEDBACK_RESPONSE_TEXT,
+                            "${mcqChoiceValue}", choices.get(i));
+            optionListHtml.append(optionFragment + Const.EOL);
+        }
+        
+        String html = FeedbackQuestionFormTemplates.populateTemplate(
+                FeedbackQuestionFormTemplates.CONTRIB_SUBMISSION_FORM,
+                "${mcqSubmissionFormOptionFragments}", optionListHtml.toString());
+        */
+        return "";
+    }
+
+    @Override
+    public String getQuestionSpecificEditFormHtml(int questionNumber) {
+        return "";
+    }
+
+    @Override
+    public String getQuestionAdditionalInfoHtml(int questionNumber, String additionalInfoId) {
+        return "";
+    }
+    
+    @Override
+    public String getQuestionResultStatisticsHtml(List<FeedbackResponseAttributes> responses,
+            FeedbackSessionResultsBundle bundle) {
+        if(responses.size() == 0){
+            return "";
+        }
+        
+        String html = "";
+        
+        return html;
+    }
+    
+    @Override
+    public String getCsvHeader() {
+        return "Feedback";
+    }
+
+    @Override
+    public List<String> validateQuestionDetails() {
+        List<String> errors = new ArrayList<String>();
+        return errors;
+    }
+
+    final String ERROR_INVALID_OPTION = "Invalid option for the " + Const.FeedbackQuestionTypeNames.CONTRIB + ".";
+    
+    @Override
+    public List<String> validateResponseAttributes(
+            List<FeedbackResponseAttributes> responses,
+            int numRecipients) {
+        List<String> errors = new ArrayList<String>();
+        for(FeedbackResponseAttributes response : responses){
+            FeedbackContributionResponseDetails frd = (FeedbackContributionResponseDetails) response.getResponseDetails();
+            if(frd.getAnswer() > 21 || frd.getAnswer()<0){
+                errors.add(ERROR_INVALID_OPTION);
+            }
+        }
+        return errors;
+    }
+
+}
---
+++ src/main/java/teammates/common/datatransfer/FeedbackContributionResponseDetails.java
@@ -0,0 +1,42 @@
+package teammates.common.datatransfer;
+
+import teammates.common.util.Const;
+import teammates.common.util.Sanitizer;
+
+public class FeedbackContributionResponseDetails extends FeedbackAbstractResponseDetails {
+    private int answer;
+    
+    public FeedbackContributionResponseDetails() {
+        super(FeedbackQuestionType.CONTRIB);
+        answer = Const.CONTRIB_NOT_SUBMITTED;
+    }
+    
+    public FeedbackContributionResponseDetails(String answer) {
+        super(FeedbackQuestionType.CONTRIB);
+        this.answer = Integer.parseInt(answer);
+    }
+    
+    public FeedbackContributionResponseDetails(int answer) {
+        super(FeedbackQuestionType.CONTRIB);
+        this.answer = answer;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+    
+    @Override
+    public String getAnswerString() {
+        return Integer.toString(answer);
+    }
+
+    @Override
+    public String getAnswerHtml(FeedbackAbstractQuestionDetails questionDetails) {
+        return Sanitizer.sanitizeForHtml(getAnswerString());
+    }
+
+    @Override
+    public String getAnswerCsv(FeedbackAbstractQuestionDetails questionDetails) {
+        return Sanitizer.sanitizeForCsv(getAnswerString());
+    }
+}
