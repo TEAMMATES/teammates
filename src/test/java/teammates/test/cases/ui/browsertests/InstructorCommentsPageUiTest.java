33167dc4b69ce71aecc3befbb9a33d81e5d3b943
Kai33 <gigikie@gmail.com>
2014-06-24 04:36:22 +0800

    done insCommentsPageUiTest

--- src/test/java/teammates/test/cases/ui/browsertests/InstructorCommentsPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorCommentsPageUiTest.java
@@ -1,5 +1,7 @@
 package teammates.test.cases.ui.browsertests;
 
+import static org.testng.AssertJUnit.assertTrue;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -27,18 +29,93 @@ public class InstructorCommentsPageUiTest extends BaseUiTestCase {
     @Test 
     public void allTests() throws Exception{
         testConent();
-        testLinks();
+        testScripts();
         testActions();
     }
 
     private void testActions() {
-        // TODO Auto-generated method stub
+        ______TS("action: edit student comment");
+        commentsPage.clickStudentCommentRow(1);
+        commentsPage.clickStudentCommentEditForRow(1);
+        commentsPage.fillTextareaToEditStudentCommentForRow(1, "");
+        commentsPage.saveEditStudentCommentForRow(1);
+        commentsPage.verifyStatus("Please enter a valid comment. The comment can't be empty.");
+        commentsPage.fillTextareaToEditStudentCommentForRow(1, "edited student comment");
+        commentsPage.saveEditStudentCommentForRow(1);
+        commentsPage.verifyContains("edited student comment");
+        commentsPage.verifyStatus(Const.StatusMessages.COMMENT_EDITED);
         
+        ______TS("action: delete student comment");
+        commentsPage.clickStudentCommentRow(1);
+        commentsPage.clickAndCancel(commentsPage.getStudentCommentDeleteForRow(1));
+        commentsPage.clickAndConfirm(commentsPage.getStudentCommentDeleteForRow(1));
+        commentsPage.verifyStatus(Const.StatusMessages.COMMENT_DELETED);
+        
+        ______TS("action: add feedback response comment");
+        commentsPage.clickResponseCommentAdd(1, 1, 1);
+        commentsPage.fillTextareaToEditResponseComment(1, 1, 1, "");
+        commentsPage.addResponseComment(1, 1, 1);
+        commentsPage.fillTextareaToEditResponseComment(1, 1, 1, "added response comment");
+        commentsPage.addResponseComment(1, 1, 1);
+        commentsPage.reloadPage();
+        commentsPage.verifyContains("added response comment");
+        
+        ______TS("action: edit feedback response comment");
+        commentsPage.clickResponseCommentRow(1, 1, 1, 1);
+        commentsPage.clickResponseCommentEdit(1, 1, 1, 1);
+        commentsPage.fillTextareaToEditResponseComment(1, 1, 1, 1, "");
+        commentsPage.saveResponseComment(1, 1, 1, 1);
+        commentsPage.fillTextareaToEditResponseComment(1, 1, 1, 1, "edited response comment");
+        commentsPage.saveResponseComment(1, 1, 1, 1);
+        commentsPage.reloadPage();
+        commentsPage.verifyContains("edited response comment");
+        
+        ______TS("action: delete feedback response comment");
+        commentsPage.clickResponseCommentRow(1, 1, 1, 1);
+        commentsPage.clickResponseCommentDelete(1, 1, 1, 1);
+        commentsPage.reloadPage();
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageAfterTestScript.html");
     }
 
-    private void testLinks() {
-        // TODO Auto-generated method stub
+    private void testScripts() {
+        ______TS("script: include archived course");
         
+        commentsPage.clickIsIncludeArchivedCoursesCheckbox();
+        commentsPage.verifyContains("comments.idOfArchivedCourse");
+        
+        commentsPage.clickNextCourseLink();
+        assertTrue(browser.driver.getCurrentUrl().contains(Const.ActionURIs.INSTRUCTOR_COMMENTS_PAGE + 
+                "?user=comments.idOfInstructor1OfCourse1&courseid=comments.idOfArchivedCourse"));
+        
+        commentsPage.clickPreviousCourseLink();
+        assertTrue(browser.driver.getCurrentUrl().contains(Const.ActionURIs.INSTRUCTOR_COMMENTS_PAGE + 
+                "?user=comments.idOfInstructor1OfCourse1&courseid=comments.idOfTypicalCourse1"));
+        
+        commentsPage.clickIsIncludeArchivedCoursesCheckbox();
+        
+        ______TS("script: filter comments");
+        
+        commentsPage.clickShowMoreOptions();
+        
+        commentsPage.showCommentsForAll();
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageShowCommentsForAll.html");
+        
+        int studentCommentPanelIdx = 1;
+        commentsPage.showCommentsForPanel(studentCommentPanelIdx);
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageShowCommentsForStudentCommentPanel.html");
+        
+        int sessionCommentPanelIdx = 2;
+        commentsPage.showCommentsForPanel(sessionCommentPanelIdx);
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageShowCommentsForSessionCommentPanel.html");
+        
+        commentsPage.showCommentsFromAll();
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageShowCommentsFromAll.html");
+        
+        commentsPage.showCommentsFromGiver("you");
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageShowCommentsFromYou.html");
+        
+        commentsPage.showCommentsFromGiver("others");
+        commentsPage.verifyHtmlMainContent("/instructorCommentsPageShowCommentsFromOthers.html");
     }
 
     private void testConent() {
--- src/test/java/teammates/test/pageobjects/InstructorCommentsPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorCommentsPage.java
@@ -1,7 +1,17 @@
 package teammates.test.pageobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class InstructorCommentsPage extends AppPage {
 
+    @FindBy(id = "option-check")
+    private WebElement showMoreOptionsCheckbox;
+    
+    @FindBy(id = "displayArchivedCourses_check")
+    private WebElement isIncludeArchivedCoursesCheckbox;
+    
     public InstructorCommentsPage(Browser browser) {
         super(browser);
     }
@@ -10,5 +20,127 @@ public class InstructorCommentsPage extends AppPage {
     protected boolean containsExpectedPageContents() {
         return getPageSource().contains("<h1>Comments from Instructors</h1>");
     }
+    
+    public void clickShowMoreOptions(){
+        showMoreOptionsCheckbox.click();
+        waitForPageToLoad();
+    }
+    
+    public void clickIsIncludeArchivedCoursesCheckbox(){
+        isIncludeArchivedCoursesCheckbox.click();
+        waitForPageToLoad();
+    }
+    
+    public void clickPreviousCourseLink(){
+        getPreviousCourseLink().click();
+        waitForPageToLoad();
+    }
+    
+    public void clickNextCourseLink(){
+        getNextCourseLink().click();
+        waitForPageToLoad();
+    }
+    
+    public void clickStudentCommentRow(int rowIdx){
+        getStudentCommentRow(rowIdx).click();
+    }
+    
+    public void showCommentsForAll(){
+        browser.driver.findElement(By.id("panel_all")).click();;
+    }
+    
+    public void showCommentsFromAll(){
+        browser.driver.findElement(By.id("giver_all")).click();;
+    }
+    
+    public void showCommentsForPanel(int panelIdx){
+        browser.driver.findElement(By.id("panel_check-" + panelIdx)).click();;
+    }
+    
+    public void showCommentsFromGiver(String giverIdx){
+        browser.driver.findElement(By.id("giver_check-by-" + giverIdx)).click();;
+    }
 
+    public WebElement getNextCourseLink() {
+        String xpathExp = "//*[@id=\"frameBodyWrapper\"]/ul[1]/li[4]/a";
+        return browser.driver.findElement(By.xpath(xpathExp));
+    }
+    
+    public WebElement getPreviousCourseLink() {
+        String xpathExp = "//*[@id=\"frameBodyWrapper\"]/ul[1]/li[1]/a";
+        return browser.driver.findElement(By.xpath(xpathExp));
+    }
+    
+    public WebElement getStudentCommentRow(int rowIdx) {
+        return browser.driver.findElement(By.id("form_commentedit-" + rowIdx));
+    }
+    
+    public WebElement getStudentCommentEditForRow(int rowIdx) {
+        return browser.driver.findElement(By.id("commentedit-" + rowIdx));
+    }
+
+    public void clickStudentCommentEditForRow(int i) {
+        getStudentCommentEditForRow(i).click();
+    }
+    
+    public WebElement getStudentCommentDeleteForRow(int i) {
+        return browser.driver.findElement(By.id("commentdelete-" + i));
+    }
+
+    public void fillTextareaToEditStudentCommentForRow(int i, String text){
+        WebElement textarea = browser.driver.findElement(By.id("commentText" + i));
+        textarea.click();
+        textarea.clear();
+        textarea.sendKeys(text);
+    }
+    
+    public void saveEditStudentCommentForRow(int i){
+        browser.driver.findElement(By.id("commentsave-" + i)).click();
+        waitForPageToLoad();
+    }
+
+    public void clickResponseCommentAdd(int sessionIdx, int questionIdx, int responseIdx) {
+        browser.driver.findElement(By.id("button_add_comment-" + sessionIdx + "-" + questionIdx + "-" + responseIdx)).click();
+        waitForPageToLoad();
+    }
+
+    public void fillTextareaToEditResponseComment(int sessionIdx, int questionIdx, int responseIdx, String text) {
+        WebElement textarea = browser.driver.findElement(By.id("responseCommentAddForm-" + sessionIdx + "-" + questionIdx + "-" + responseIdx));
+        textarea.click();
+        textarea.clear();
+        textarea.sendKeys(text);
+    }
+    
+    public void fillTextareaToEditResponseComment(int sessionIdx, int questionIdx, int responseIdx, Integer commentIdx, String text) {
+        WebElement textarea = browser.driver.findElement(By.id("responsecommenttext-" + sessionIdx + "-" + questionIdx + "-" + responseIdx
+                 + "-" + commentIdx));
+        textarea.click();
+        textarea.clear();
+        textarea.sendKeys(text);
+    }
+
+    public void addResponseComment(int sessionIdx, int questionIdx, int responseIdx) {
+        browser.driver.findElement(By.id("button_save_comment_for_add-" + sessionIdx + "-" + questionIdx + "-" + responseIdx)).click();
+        waitForPageToLoad();
+    }
+
+    public void clickResponseCommentRow(int sessionIdx, int questionIdx, int responseIdx, int commentIdx) {
+        browser.driver.findElement(By.id("responseCommentRow-" + sessionIdx + "-" + questionIdx + "-" + responseIdx + "-" + commentIdx)).click();
+        waitForPageToLoad();
+    }
+
+    public void clickResponseCommentEdit(int sessionIdx, int questionIdx, int responseIdx, int commentIdx) {
+        browser.driver.findElement(By.id("commentedit-" + sessionIdx + "-" + questionIdx + "-" + responseIdx + "-" + commentIdx)).click();
+        waitForPageToLoad();
+    }
+
+    public void saveResponseComment(int sessionIdx, int questionIdx, int responseIdx, int commentIdx) {
+        browser.driver.findElement(By.id("button_save_comment_for_edit-" + sessionIdx + "-" + questionIdx + "-" + responseIdx + "-" + commentIdx)).click();
+        waitForPageToLoad();
+    }
+
+    public void clickResponseCommentDelete(int sessionIdx, int questionIdx, int responseIdx, int commentIdx) {
+        browser.driver.findElement(By.id("commentdelete-" + sessionIdx + "-" + questionIdx + "-" + responseIdx + "-" + commentIdx)).click();
+        waitForPageToLoad();
+    }
 }
