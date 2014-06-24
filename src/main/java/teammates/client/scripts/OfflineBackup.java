251306e809d40bedda19334636ad5f0f8b7bc4aa
Shawn Lee <leeshawn89@gmail.com>
2014-06-24 13:47:50 +0800

    Able to save data to csv format for some entities

--- src/main/java/teammates/client/scripts/OfflineBackup.java
+++ src/main/java/teammates/client/scripts/OfflineBackup.java
@@ -8,6 +8,7 @@ import java.net.URLConnection;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -30,6 +31,7 @@ import teammates.common.datatransfer.FeedbackSessionAttributes;
 import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.datatransfer.SubmissionAttributes;
+import teammates.common.util.Const;
 import teammates.logic.api.Logic;
 import teammates.storage.api.CommentsDb;
 import teammates.storage.api.FeedbackQuestionsDb;
@@ -37,6 +39,8 @@ import teammates.storage.api.FeedbackResponseCommentsDb;
 import teammates.storage.api.FeedbackResponsesDb;
 import teammates.storage.api.SubmissionsDb;
 import teammates.storage.datastore.Datastore;
+import teammates.test.driver.TestProperties;
+import teammates.test.util.FileHelper;
 
 public class OfflineBackup extends RemoteApiClient {
     
@@ -54,10 +58,12 @@ public class OfflineBackup extends RemoteApiClient {
     
     private Vector<String> getModifiedLogs() {
         Vector<String> modifiedLogs = new Vector<String>();
-
+        TestProperties testProperties = TestProperties.inst();
         try {
             //Opens a URL connection to obtain the entity modified logs
-            URL myURL = new URL("http://4-18-dot-teammates-shawn.appspot.com/entityModifiedLogs");
+            
+            URL myURL = new URL(testProperties.TEAMMATES_URL + "/entityModifiedLogs");
+                    //"http://4-18-dot-teammates-shawn2.appspot.com/entityModifiedLogs");
             
             URLConnection myURLConnection = myURL.openConnection();        
         
@@ -98,7 +104,7 @@ public class OfflineBackup extends RemoteApiClient {
             String entity = it.next();
             String tokens[] = entity.split("::");
             String type = tokens[0];
-            String[] id = tokens[1].split("[,|/]");
+            String id = tokens[1];
             entitiesMap.put(type, id);
         }
         
@@ -113,15 +119,15 @@ public class OfflineBackup extends RemoteApiClient {
         
         while(it.hasNext()) {
             String entityType = it.next();
-            Collection<String[]> ids = (Collection<String[]>) entityMap.get(entityType);
+            Collection<String> ids = (Collection<String>) entityMap.get(entityType);
             
-            Iterator<String[]> idit = ids.iterator();
+            Iterator<String> idit = ids.iterator();
             
             while(idit.hasNext()) {
-                String[] id = idit.next();
+                String id = idit.next();
                 try {
-                    EntityAttributes ea = retrieveEntity(entityType,id);
-                    System.out.println("Retrieving:" + ea.toString());  
+                    retrieveEntity(entityType,id);
+                    
                 } catch (ParseException e) {
                     System.out.println("Error while retrieving entities: " + e.getMessage());
                 }     
@@ -129,11 +135,28 @@ public class OfflineBackup extends RemoteApiClient {
         }
     }
     
-    private EntityAttributes retrieveEntity(String type, String[] id) throws ParseException {
+    private void retrieveEntity(String type, String id) throws ParseException {
         System.out.println(type);
-        Logic logic = new Logic();
+        
         switch(type) {
+            
             case "Account":
+                retrieveAndSaveAccount(id);
+                break;
+                
+            case "Comment":
+                retrieveAndSaveComment(id);
+                break;
+               
+            case "Course":
+                retrieveAndSaveCourse(id);
+                break;
+                
+            case "Evaluation":
+                retrieveAndSaveEvaluation(id);
+                break;
+               
+            /*case "Account":
                 AccountAttributes account = logic.getAccount(id[0].trim());
                 return account;
                 
@@ -180,8 +203,51 @@ public class OfflineBackup extends RemoteApiClient {
             case "Submission":
                 SubmissionsDb submissionsDb = new SubmissionsDb();
                 SubmissionAttributes submission = submissionsDb.getSubmission(id[0].trim(), id[1].trim(), id[2].split("to:")[1].trim(), id[3].split("from:")[1].trim());
-                return submission;
+                return submission;*/
         }
-        return null;
+        
+    }
+    
+    private void retrieveAndSaveAccount(String id) {
+        Logic logic = new Logic();
+        String googleId = id.trim();
+        AccountAttributes account = logic.getAccount(googleId);
+        String accountCsv = account.googleId + "," + account.name + "," + account.isInstructor + "," + account.email + "," + account.institute + "," + account.createdAt + Const.EOL;
+        FileHelper.appendToFile("account.csv", accountCsv);
+    }
+    
+    private void retrieveAndSaveComment(String id) throws ParseException {
+        CommentsDb commentDb = new CommentsDb();
+        String[] idTokens = id.split("|");
+        String courseId = idTokens[0].trim();
+        String giverEmail = idTokens[1].trim();
+        String receiverEmail = idTokens[2].trim();
+        Text commentText = new Text(idTokens[3].trim());
+        Date date = new SimpleDateFormat("EEE MMM d HH:mm:ss.SSSSSS zzz yyyy").parse(idTokens[4].trim());
+        CommentAttributes comment = commentDb.getComment(courseId, giverEmail, receiverEmail, commentText, date);
+        String commentCsv = comment.getCommentId() + "," + comment.courseId + "," + comment.giverEmail + "," + comment.receiverEmail + "," + date + "," + commentText + Const.EOL;
+        FileHelper.appendToFile("comment.csv", commentCsv);
+    }
+    
+    private void retrieveAndSaveCourse(String id) {
+        Logic logic = new Logic();
+        String courseId = id.trim();
+        CourseAttributes course = logic.getCourse(courseId);
+        String createdAt = new SimpleDateFormat("EEE MMM d HH:mm:ss.SSSSSS zzz yyyy").format(course.createdAt);
+        String courseCsv = course.id + "," + course.name + "," + createdAt + "," + course.isArchived + Const.EOL;
+        FileHelper.appendToFile("course.csv", courseCsv);
+    }
+    
+    private void retrieveAndSaveEvaluation(String id) {
+        Logic logic = new Logic();
+        String[] idTokens = id.split("|");
+        String courseId = idTokens[0].trim();
+        String evaluationName = idTokens[1].trim();
+        EvaluationAttributes evaluation = logic.getEvaluation(courseId, evaluationName);
+        String startTime = new SimpleDateFormat("EEE MMM d HH:mm:ss.SSSSSS zzz yyyy").format(evaluation.startTime);
+        String endTime = new SimpleDateFormat("EEE MMM d HH:mm:ss.SSSSSS zzz yyyy").format(evaluation.endTime);
+        String evaluationCsv = evaluation.courseId + "," + evaluation.name + "," + evaluation.instructions.getValue() + "," + startTime + "," + endTime + "," +
+                evaluation.timeZone + "," + evaluation.gracePeriod + "," + evaluation.p2pEnabled + "," + evaluation.published + "," + evaluation.activated + Const.EOL;
+        FileHelper.appendToFile("evaluation.csv", evaluationCsv);
     }
 }
--- src/test/java/teammates/test/util/FileHelper.java
+++ src/test/java/teammates/test/util/FileHelper.java
@@ -30,5 +30,29 @@ public class FileHelper {
             e.printStackTrace();
         }
     }
+    
+    public static void appendToFile(String fileName, String fileContent) {
+        try {
 
+            File file = new File(fileName);
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(fileContent);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void deleteFile(String fileName) {
+       File file = new File(fileName);
+       file.delete();
+    }
 }
