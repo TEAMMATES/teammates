c849b13ba81d3c6d6141c8193c572deb30b61ae4
Thyagesh Manikandan <thyageshm@gmail.com>
2014-06-24 10:34:20 +0800

    added authentication checks in StudentProfilePictureAction

--- src/main/java/teammates/ui/controller/StudentProfilePictureAction.java
+++ src/main/java/teammates/ui/controller/StudentProfilePictureAction.java
@@ -1,9 +1,12 @@
 package teammates.ui.controller;
 
+import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.UnauthorizedAccessException;
 import teammates.common.util.Assumption;
 import teammates.common.util.Const;
+import teammates.logic.api.GateKeeper;
+import teammates.storage.api.EntitiesDb;
 
 public class StudentProfilePictureAction extends Action {
 
@@ -19,21 +22,32 @@ public class StudentProfilePictureAction extends Action {
         } else if (getRequestParamValue(Const.ParamsNames.STUDENT_EMAIL) != null) {
             result = handleRequestWithEmailAndCourse();
         } else {
-            Assumption.assertPostParamNotNull("expected blob-key or student email", null);
+            Assumption.fail("expected blob-key, or student email with courseId");
         }
         
         return result;
     }
 
-    private ActionResult handleRequestWithEmailAndCourse() {
+    private ActionResult handleRequestWithEmailAndCourse() throws EntityDoesNotExistException {
         String email = getRequestParamValue(Const.ParamsNames.STUDENT_EMAIL);
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         
         Assumption.assertPostParamNotNull(Const.ParamsNames.STUDENT_EMAIL, email);
         Assumption.assertPostParamNotNull(Const.ParamsNames.COURSE_ID, courseId);
+
+        if(logic.getInstructorForGoogleId(courseId, account.googleId) == null) {
+            throw new UnauthorizedAccessException(
+                    "User is not instructor of the course that student belongs to");
+        }
         
-        String googleId = logic.getStudentForEmail(courseId, email).googleId;
-        String blobKey = logic.getStudentProfile(googleId).pictureKey;
+        StudentAttributes student = logic.getStudentForEmail(courseId, email);
+        if (student == null) {
+            throw new EntityDoesNotExistException(EntitiesDb.ERROR_UPDATE_NON_EXISTENT_STUDENT +
+                    courseId + "/" + email);
+        }
+        
+        // googleId == null is handled at logic level
+        String blobKey = logic.getStudentProfile(student.googleId).pictureKey;
         
         return createImageResult(blobKey);
     }
