6fc08b9e364f78d183c7e581cc9bb248d3982dc4
Damith C. Rajapakse <damith@gmail.com>
2014-06-24 19:46:45 +0800

    Added line break to test case to make it pass

--- src/test/java/teammates/test/cases/ui/browsertests/InstructorCourseEnrollPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorCourseEnrollPageUiTest.java
@@ -162,7 +162,7 @@ public class InstructorCourseEnrollPageUiTest extends BaseUiTestCase {
 
         enrollPage.enrollUnsuccessfully(enrollString);
         enrollPage.verifyStatus("The team \"Team 1\" is in multiple sections. The team ID should be unique across the entire course and a team cannot be spread across multiple sections."
-                + "Please use the enroll page to edit multiple students");
+                + "\nPlease use the enroll page to edit multiple students");
 
         ______TS("enroll action: fail to enroll due to invalid header");
 
