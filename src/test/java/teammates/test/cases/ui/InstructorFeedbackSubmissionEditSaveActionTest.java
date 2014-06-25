2744eda19ba5e912aadffe4503bbdfe0d5726b46
franklingu <franklingujunchao@gmail.com>
2014-06-25 17:46:25 +0800

    submit link is disabled for people who cannot submit for the session

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackSubmissionEditSaveActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackSubmissionEditSaveActionTest.java
@@ -56,7 +56,7 @@ public class InstructorFeedbackSubmissionEditSaveActionTest extends BaseActionTe
                 Const.ParamsNames.COURSE_ID, fs.courseId,
                 Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.feedbackSessionName
         };
-        
+        verifyUnaccessibleWithoutSubmitSessionInSectionsPrivilege(submissionParams);
         verifyOnlyInstructorsOfTheSameCourseCanAccess(submissionParams);
     }
     
