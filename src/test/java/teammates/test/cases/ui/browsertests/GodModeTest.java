b76ac75ca95c8fc66529464e6f0969ed8c20b929
Low WeiLin <lwl1991@gmail.com>
2014-06-26 16:11:16 +0800

    Release broswer for godmode test

--- src/test/java/teammates/test/cases/ui/browsertests/GodModeTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/GodModeTest.java
@@ -117,6 +117,7 @@ public class GodModeTest extends BaseUiTestCase {
 
     @AfterClass
     public static void closeClass() throws Exception {
+        BrowserPool.release(browser);
         System.clearProperty("godmode");
         writeToFile(TestProperties.TEST_PAGES_FOLDER + "/godmode.html", initialContent);
     }
