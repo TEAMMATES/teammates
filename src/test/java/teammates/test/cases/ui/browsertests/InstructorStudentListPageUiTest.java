bc68e0a2e580e3ee5ed747076237e9a23ae4fd28
Thyagesh Manikandan <thyageshm@gmail.com>
2014-06-30 13:52:04 +0800

    fixing test cases

--- src/test/java/teammates/test/cases/ui/browsertests/InstructorStudentListPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorStudentListPageUiTest.java
@@ -2,7 +2,6 @@ package teammates.test.cases.ui.browsertests;
 
 import static org.testng.AssertJUnit.assertNotNull;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
