72b3724e91065f2b66d98c6b40c7118777402602
Kai33 <gigikie@gmail.com>
2014-07-02 04:55:33 +0800

    add instructor verification for search page

--- src/main/java/teammates/ui/controller/InstructorSearchPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorSearchPageAction.java
@@ -1,19 +1,19 @@
 package teammates.ui.controller;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import teammates.common.datatransfer.CommentAttributes;
 import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.util.Const;
+import teammates.logic.api.GateKeeper;
 
 public class InstructorSearchPageAction extends Action {
 
     @Override
     protected ActionResult execute() throws EntityDoesNotExistException {
+        new GateKeeper().verifyInstructorPrivileges(account);
+        
         String key = getRequestParamValue(Const.ParamsNames.SEARCH_KEY);
         if(key == null) key = "";
 
