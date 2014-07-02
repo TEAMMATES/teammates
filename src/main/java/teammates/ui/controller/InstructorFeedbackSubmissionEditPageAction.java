ac4768e27a2061495f58f5bc33b934f6d6d631f8
franklingu <franklingujunchao@gmail.com>
2014-07-02 10:18:19 +0800

    adjustment of one type of submission

--- src/main/java/teammates/ui/controller/InstructorFeedbackSubmissionEditPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackSubmissionEditPageAction.java
@@ -34,7 +34,7 @@ public class InstructorFeedbackSubmissionEditPageAction extends FeedbackSubmissi
         for (String section : sectionsInCourse) {
             if (!instructor.isAllowedForPrivilege(section, session.feedbackSessionName, 
                     Const.ParamsNames.INSTRUCTOR_PERMISSION_SUBMIT_SESSION_IN_SECTIONS)) {
-                shouldEnableSubmit = false;
+                shouldEnableSubmit = true;
                 break;
             }
         }
