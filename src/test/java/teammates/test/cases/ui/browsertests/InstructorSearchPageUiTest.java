4c0989a7b0f00192c32ad909fd8226d2ccd8af18
franklingu <franklingujunchao@gmail.com>
2014-07-29 17:55:16 +0800

    add search ui page test

---
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorSearchPageUiTest.java
@@ -0,0 +1,60 @@
+package teammates.test.cases.ui.browsertests;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.DataBundle;
+import teammates.common.util.Const;
+import teammates.common.util.Url;
+import teammates.test.pageobjects.Browser;
+import teammates.test.pageobjects.BrowserPool;
+import teammates.test.pageobjects.InstructorCommentsPage;
+import teammates.test.pageobjects.InstructorSearchPage;
+
+public class InstructorSearchPageUiTest extends BaseUiTestCase {
+    private static Browser browser;
+    private static InstructorSearchPage searchPage;
+    private static DataBundle testData;
+
+    @BeforeClass
+    public static void classSetup() throws Exception {
+        printTestClassHeader();
+        testData = loadDataBundle("/InstructorSearchPageUiTest.json");
+        removeAndRestoreTestDataOnServer(testData);
+        browser = BrowserPool.getBrowser(true);
+    }
+    
+    @Test 
+    public void allTests() throws Exception{
+        
+        testContent();
+        
+    }
+    
+    private void testContent() {
+        
+        ______TS("content: default search page");
+        System.setProperty("godmode", "true");
+        String instructorId = testData.accounts.get("instructor1OfCourse1").googleId;
+        searchPage = getInstructorSearchPage(instructorId);
+        searchPage.verifyHtml("/InstructorSearchPageDefault.html");
+    }
+    
+    private void testSearchComments() {
+        
+    }
+
+    private InstructorSearchPage getInstructorSearchPage(String instructorId) {
+        Url commentsPageUrl = createUrl(Const.ActionURIs.INSTRUCTOR_SEARCH_PAGE)
+                .withUserId(instructorId);
+
+        return loginAdminToPage(browser, commentsPageUrl, InstructorSearchPage.class);
+    }
+    
+    @AfterClass
+    public static void classTearDown() throws Exception {
+        BrowserPool.release(browser);
+    }
+
+}
---
+++ src/test/java/teammates/test/pageobjects/InstructorSearchPage.java
@@ -0,0 +1,57 @@
+package teammates.test.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class InstructorSearchPage extends AppPage {
+    
+    public InstructorSearchPage(Browser browser) {
+        super(browser);
+    }
+
+    @Override
+    protected boolean containsExpectedPageContents() {
+        return getPageSource().contains("<h1>Search</h1>");
+    }
+    
+    public void inputSearchContent(String content) {
+        this.getSearchBox().sendKeys(content);
+    }
+    
+    public void clickSearchButton() {
+        this.getSearchButton().click();
+    }
+    
+    public void clickStudentCommentCheckBox() {
+        this.getStudentCommentCheckBox().click();
+    }
+    
+    public void clickFeedbackResponseCommentCheckBox() {
+        this.getFeedbackResponseCommentCheckBox().click();
+    }
+    
+    public void clickStudentCheckBox() {
+        this.getStudentCheckBox().click();
+    }
+    
+    private WebElement getSearchBox() {
+        return browser.driver.findElement(By.id("searchBox"));
+    }
+    
+    private WebElement getSearchButton() {
+        return browser.driver.findElement(By.id("buttonSearch"));
+    }
+    
+    private WebElement getStudentCommentCheckBox() {
+        return browser.driver.findElement(By.id("comments-for-student-check"));
+    }
+    
+    private WebElement getFeedbackResponseCommentCheckBox() {
+        return browser.driver.findElement(By.id("comments-for-responses-check"));
+    }
+    
+    private WebElement getStudentCheckBox() {
+        return browser.driver.findElement(By.id("students-check"));
+    }
+
+}
