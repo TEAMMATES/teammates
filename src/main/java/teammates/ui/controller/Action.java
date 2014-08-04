1d9fac51eda4973d211cecc4a219ac1f6dbf812d
Thyagesh Manikandan <thyageshm@gmail.com>
2014-08-04 17:25:29 +0800

    added a end date for the legacy parameter handling branch

--- src/main/java/teammates/ui/controller/Action.java
+++ src/main/java/teammates/ui/controller/Action.java
@@ -126,6 +126,7 @@ public abstract class Action {
     protected String getRegkeyFromRequest() {
         String regkey = getRequestParamValue(Const.ParamsNames.REGKEY);
         if (regkey == null) {
+            //TODO: remove this branch on October 15th 2014.
             return getRequestParamValue(Const.ParamsNames.REGKEY_LEGACY);
         } else {
             return regkey;
