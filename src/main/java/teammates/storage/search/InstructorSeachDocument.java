a1fafd467bc25f7f4e6054d1872d424e3b4824d1
ymoctavia <a0091836@nus.edu.sg>
2014-08-22 11:11:33 +0800

    1. temp push

---
+++ src/main/java/teammates/storage/search/InstructorSeachDocument.java
@@ -0,0 +1,19 @@
+package teammates.storage.search;
+
+import com.google.appengine.api.search.Document;
+
+public class InstructorSeachDocument extends SearchDocument {
+
+    @Override
+    protected void prepareData() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected Document toDocument() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
