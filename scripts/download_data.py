1a64021a7acacef4576692a3940e9fb2bd2b8123
Shawn Lee <leeshawn89@gmail.com>
2014-08-30 14:56:25 +0800

    Added some comments to using the scripts

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,13 +1,20 @@
 import os
 import datetime
 
+#This script should be placed in the GAE Python SDK directory.
+#The path of the SDK will look like C:\Program Files (x86)\Google\google_appengine
+#The script is to be used in conjunction with the generated_bulkloader.yaml file
+
+#The script will download all types of entities from the GAE datastore except the StudentProfile entity type.
+#The backup files will be stored on the Desktop with a timestamp of when the the backup is performed.
+
+
 #Creates a folder on desktop with a timestamp of the backup
 desktopFile = os.path.expanduser("~/Desktop/TM_Backup/")
 mydir = os.path.join(desktopFile, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
 os.makedirs(mydir)
 
-#Downloads all the types of entities
-#Student profiles will be handled separately
+#Runs a set of commands to download all the different types of entities from the datastore
 os.system("bulkloader.py --download --url https://teammatesv4.appspot.com/remote_api  --config_file generated_bulkloader.yaml --kind Account --filename %s/accounts.csv" %mydir)
 os.system("bulkloader.py --download --url https://teammatesv4.appspot.com/remote_api  --config_file generated_bulkloader.yaml --kind Comment --filename %s/comment.csv" %mydir)
 os.system("bulkloader.py --download --url https://teammatesv4.appspot.com/remote_api  --config_file generated_bulkloader.yaml --kind Course --filename %s/course.csv" %mydir)
--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -1,13 +1,22 @@
 import os
 import datetime
 
-#Creates a folder on desktop with a timestamp of the backup
+#This script should be placed in the GAE Python SDK directory.
+#The path of the SDK will look like C:\Program Files (x86)\Google\google_appengine
+#The script is to be used in conjunction with the generated_bulkloader.yaml file
+
+#The script will upload all types of entities from the backup files to the GAE datastore.
+#The only entity type that is not handled here is the StudentProfile entity type.
+#As many backups would have been performed, the timestamp of which backup files to be used for uploading must be specified.
+#The format of the timestamp is YYYY-MM-DD_HH-MM-SS.
+
+
+#Obtain the timestamp from the user and forms the upload file path
 date_time = raw_input("Enter the date and time of the backup files to be uploaded. Format is YYYY-MM-DD_HH-MM-SS: ")
 desktopPath = os.path.expanduser("~/Desktop/TM_Backup/")
 backupFileDirectory = os.path.join(desktopPath, date_time)
 
-#Downloads all the types of entities
-#Student profiles will be handled separately
+#Runs a set of commands to upload all the data to the GAE datastore
 os.system("appcfg.py upload_data --url https://teammatesv4.appspot.com/remote_api  --config_file generated_bulkloader.yaml --kind Account --filename %s/accounts.csv" %backupFileDirectory)
 os.system("appcfg.py upload_data --url https://teammatesv4.appspot.com/remote_api  --config_file generated_bulkloader.yaml --kind Comment --filename %s/comment.csv" %backupFileDirectory)
 os.system("appcfg.py upload_data --url https://teammatesv4.appspot.com/remote_api  --config_file generated_bulkloader.yaml --kind Course --filename %s/course.csv" %backupFileDirectory)
