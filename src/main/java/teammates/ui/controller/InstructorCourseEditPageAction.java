e546aee990a5dceea70600337789d66d1b7228b4
Bui Trong Nhan <imouto1994@gmail.com>
2014-09-06 06:26:54 +0800

    Remove redunant logger

--- src/main/java/teammates/ui/controller/InstructorCourseEditPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorCourseEditPageAction.java
@@ -19,8 +19,6 @@ import teammates.logic.api.GateKeeper;
  */
 public class InstructorCourseEditPageAction extends Action {
     
-    protected static final Logger log = Utils.getLogger();
-    
     //TODO: display privileges in the database properly
     @Override
     public ActionResult execute() throws EntityDoesNotExistException { 
