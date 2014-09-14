9150b439e7b86c130acf91dff1b542febd02fd98
ymoctavia <a0091836@nus.edu.sg>
2014-09-14 16:21:52 +0800

    1. get data from Students for student counting 2. add unique instructor email counting

--- src/main/java/teammates/client/scripts/StatisticsPerInstitute.java
+++ src/main/java/teammates/client/scripts/StatisticsPerInstitute.java
@@ -9,11 +9,18 @@ import java.util.HashSet;
 import java.util.List;
 
 import javax.jdo.JDOHelper;
+import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 import teammates.client.remoteapi.RemoteApiClient;
 import teammates.storage.entity.Account;
+import teammates.storage.entity.Instructor;
 import teammates.storage.entity.Student;
+
 
 /**
  * Generate list of institutes and number of users per institute.
@@ -27,6 +34,9 @@ public class StatisticsPerInstitute extends RemoteApiClient {
     
     private static final int INSTRUCTOR_INDEX = 0;
     private static final int STUDENT_INDEX = 1;
+    private static final String UNKNOWN_INSTITUTE = "Unkonwn Institute";
+    
+    private HashMap<String, String> courseIdToInstituteMap = new HashMap<String, String>();
     
     public static void main(String[] args) throws IOException {
         StatisticsPerInstitute statistics = new StatisticsPerInstitute();
@@ -37,10 +47,56 @@ public class StatisticsPerInstitute extends RemoteApiClient {
     protected void doOperation() {
         List<InstituteStats> statsPerInstituteList = generateStatsPerInstitute();
         String statsForUniqueStudentEmail =  generateUniqueStudentEmailStatsInWholeSystem();
+        String statsForUniqueInstructorEmail = generateUniqueInstructorEmailStatsInWholeSystem();
+        
         print(statsPerInstituteList);
         System.out.println("\n\n" + "***************************************************" + "\n\n");
         System.out.println(statsForUniqueStudentEmail);
+        
+        System.out.println("\n\n" + "***************************************************" + "\n\n");
+        System.out.println(statsForUniqueInstructorEmail);
     }
+    
+    
+    @SuppressWarnings("unchecked")
+    private String generateUniqueInstructorEmailStatsInWholeSystem(){
+        String q = "SELECT FROM " + Instructor.class.getName();
+        List<Instructor> allInstructors = (List<Instructor>) pm.newQuery(q).execute();
+        HashSet<String> set = new HashSet<String>();
+        int totalRealInstructor = 0;
+        
+        for(Instructor i: allInstructors){
+            if(!isTestingInstructorData(i)){
+                set.add(i.getEmail().toLowerCase());
+                totalRealInstructor ++;
+            }
+            
+        }
+        
+        String result = "===============Unique Instructor Emails===============\n"
+                        + "Format=> Total Unique Emails [Total Emails]\n"
+                        + "===================================================\n"
+                        + set.size() + " [ " + totalRealInstructor + " ]\n";
+        return result;
+    }
+    
+    
+    private boolean isTestingInstructorData(Instructor instructor){
+        boolean isTestingData = false;
+        
+        if(instructor.getEmail().toLowerCase().endsWith(".tmt")){
+            isTestingData = true;
+        }       
+        
+        if(getInstituteForInstructor(instructor).contains("TEAMMATES Test Institute")){
+            isTestingData = true;
+        } 
+        
+        return isTestingData;
+    }
+    
+    
+    
     
     @SuppressWarnings("unchecked")
     private String generateUniqueStudentEmailStatsInWholeSystem(){
@@ -70,6 +126,10 @@ public class StatisticsPerInstitute extends RemoteApiClient {
         if(student.getEmail().toLowerCase().endsWith(".tmt")){
             isTestingData = true;
         }       
+        
+        if(getInstituteForStudent(student).contains("TEAMMATES Test Institute")){
+            isTestingData = true;
+        } 
         
         return isTestingData;
     }
@@ -104,17 +164,119 @@ public class StatisticsPerInstitute extends RemoteApiClient {
             // Increase the appropriate slot
             if (a.isInstructor()) {
                 institutes.get(a.getInstitute()).put(INSTRUCTOR_INDEX,
-                        institutes.get(a.getInstitute()).get(INSTRUCTOR_INDEX) + 1);
-            } else {
-                institutes.get(a.getInstitute()).put(STUDENT_INDEX,
-                        institutes.get(a.getInstitute()).get(STUDENT_INDEX) + 1);
-            }
+                                                     institutes.get(a.getInstitute()).get(INSTRUCTOR_INDEX) + 1);
+            } 
+            
         }
         
+        
+        q = "SELECT FROM " + Student.class.getName();
+        
+        List<Student> allStudents = (List<Student>) pm.newQuery(q).execute();
+        
+        for(Student student : allStudents){
+            
+            if(isTestingStudentData(student)){
+                continue;
+            }
+            
+            String institute = getInstituteForStudent(student);
+            
+            if(!institutes.containsKey(institute)){               
+                institutes.put(institute,
+                        new HashMap<Integer, Integer>());
+                institutes.get(institute).put(INSTRUCTOR_INDEX, 0);
+                institutes.get(institute).put(STUDENT_INDEX, 0);
+            }
+            
+            institutes.get(institute).put(STUDENT_INDEX,
+                                          institutes.get(institute).get(STUDENT_INDEX) + 1);
+                                          
+          
+        }
+                
         List<InstituteStats> statList = convertToList(institutes);
         sortByTotalStudentsDescending(statList);
         return statList;
     }
+    
+    @SuppressWarnings("unchecked")
+    private String getInstituteForStudent(Student student){
+        
+        String institute = courseIdToInstituteMap.get(student.getCourseId());
+        
+        if(institute != null){
+            return institute;
+        } else{
+            institute = UNKNOWN_INSTITUTE;
+        }
+        
+        
+        Query q = pm.newQuery(Instructor.class);
+        q.declareParameters("String courseIdParam");
+        q.setFilter("courseId == courseIdParam");
+        List<Instructor> instructorList = (List<Instructor>) q.execute(student.getCourseId());        
+        
+        institute = getInstituteForInstructors(instructorList);
+        
+        courseIdToInstituteMap.put(student.getCourseId(), institute);
+        
+        return institute;
+        
+    }
+    
+    private String getInstituteForInstructors(List<Instructor> instructorList){
+        String institute = UNKNOWN_INSTITUTE;
+        
+        for(Instructor instructor : instructorList){
+           
+            if(getInstituteForInstructor(instructor) != null){
+                institute = getInstituteForInstructor(instructor);
+                break;
+            }
+           
+        }
+                
+        return institute;
+    }
+    
+    private String getInstituteForInstructor(Instructor instructor){
+        
+        String institute = UNKNOWN_INSTITUTE;
+        
+        if(instructor.getGoogleId() == null){
+            return institute;
+        }
+        
+        Account account = getAccountEntity(instructor.getGoogleId());
+        if(account != null) {
+            return account.getInstitute();
+            
+        } else {
+            return institute;
+        }
+    }
+    
+    
+    private Account getAccountEntity(String googleId) {
+        
+        try {
+            Key key = KeyFactory.createKey(Account.class.getSimpleName(), googleId);
+            Account account = pm.getObjectById(Account.class, key);
+            
+            if (JDOHelper.isDeleted(account)) {
+                return null;
+            } 
+            
+            return account;
+            
+        } catch (IllegalArgumentException iae){
+            return null;            
+        } catch(JDOObjectNotFoundException je) {
+            return null;
+        }
+    }
+    
     
     private boolean isTestingAccount(Account account){
         boolean isTestingAccount = false;
