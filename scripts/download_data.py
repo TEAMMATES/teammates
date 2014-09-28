476563925589bc09d850e556306982c047453522
Shawn Lee <leeshawn89@gmail.com>
2014-09-28 14:40:49 +0800

    Removed uploading to dev server and updated upload script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -14,11 +14,6 @@ import datetime
 #Remember to add /remote_api at the end of the URL
 liveServerUrl = "https://teammatesv4.appspot.com/remote_api"
 
-#URL of local dev server where the downloaded data is uploaded to
-#Change this to connect to a different dev server
-#Remember to add /remote_api at the end of the URL
-devServerUrl = "http://localhost:8888/remote_api"
-
 #Creates a folder on desktop with a timestamp of the backup
 desktopFile = os.path.expanduser("~/Desktop/TM_Backup/")
 fileLocation = os.path.join(desktopFile, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
@@ -37,18 +32,3 @@ os.system("bulkloader.py --download --url %s --config_file generated_bulkloader.
 os.system("bulkloader.py --download --url %s --config_file generated_bulkloader.yaml --kind Instructor --filename %s/instructor.csv" %(liveServerUrl,fileLocation))
 os.system("bulkloader.py --download --url %s --config_file generated_bulkloader.yaml --kind Student --filename %s/student.csv" %(liveServerUrl,fileLocation))
 os.system("bulkloader.py --download --url %s --config_file generated_bulkloader.yaml --kind Submission --filename %s/submission.csv" %(liveServerUrl,fileLocation))
-
-
-#Uploads the downloaded data to the local dev server
-#User name and password is arbitrary, but you will not be prompted for it as the script will handle it
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Account --filename %s/accounts.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Comment --filename %s/comment.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Course --filename %s/course.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Evaluation --filename %s/evaluation.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind FeedbackQuestion --filename %s/feedbackQuestion.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind FeedbackResponse --filename %s/feedbackResponse.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind FeedbackResponseComment --filename %s/feedbackResponseComment.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind FeedbackSession --filename %s/feedbackSession.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Instructor --filename %s/instructor.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Student --filename %s/student.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
-os.system("echo 'XX' | appcfg.py upload_data --url %s --config_file generated_bulkloader.yaml --kind Submission --filename %s/submission.csv --email=aaa@gmail.com --passin" %(devServerUrl,fileLocation))
