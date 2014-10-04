1110a814e4a4091c1e382e6679881e60239b97e8
ymoctavia <a0091836@nus.edu.sg>
2014-10-05 03:43:03 +0800

    write a script

---
+++ src/main/java/teammates/client/scripts/InstructorStudentInstitueMigrator.java
@@ -0,0 +1,90 @@
+package teammates.client.scripts;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import teammates.client.remoteapi.RemoteApiClient;
+import teammates.storage.entity.Account;
+
+public class InstructorStudentInstitueMigrator extends RemoteApiClient {
+    
+    private static String fromInstitute = null;
+    private static String toInstitute = null;
+    
+    private Scanner reader = new Scanner(System.in);
+    
+    private static final String NO_MATCHING_INSTITUTE = "No Matching Accounts Found for Institue: %s [q: exit | r: re-enter]";
+    private static final int PROGRESS_STEP = 1000;
+    private static int counter = 0;
+    
+    
+    protected static final PersistenceManager pm = JDOHelper
+                                                   .getPersistenceManagerFactory("transactions-optional")
+                                                   .getPersistenceManager();
+    
+    public static void main(String[] args) throws IOException {
+
+        InstructorStudentInstitueMigrator migrator = new InstructorStudentInstitueMigrator();
+        migrator.doOperationRemotely();
+    }
+    
+    
+    @Override
+    protected void doOperation() {
+        String command = "q";
+        readInput();
+        
+        Query q = pm.newQuery(Account.class);
+        q.declareParameters("String instituteName");
+        q.setFilter("institute == instituteName" );
+        
+        @SuppressWarnings("unchecked")
+        List<Account> accountsList = (List<Account>) q.execute(fromInstitute);
+        
+        for (Account a : accountsList) {
+            a.setInstitute(toInstitute);
+            updateProgressIndicator(accountsList.size());
+        }
+              
+        
+        if(accountsList.size() == 0){
+            System.out.printf(NO_MATCHING_INSTITUTE, fromInstitute);      
+            command = reader.next();
+            while(!command.contentEquals("q") && !command.contentEquals("r")){
+                System.out.printf(NO_MATCHING_INSTITUTE, fromInstitute);
+                command = reader.next();
+            }
+        }
+        
+        if(command.contentEquals("q")){
+          pm.close();
+        } else {
+          doOperation();
+        }
+
+    }
+    
+    private void updateProgressIndicator(int total){
+        counter ++;
+        if(counter % PROGRESS_STEP == 0 || counter == total){
+            System.out.printf("total accounts modified %d/%d \n", counter, total);
+        }
+    }
+    
+    private void readInput(){
+        
+        if(fromInstitute != null){
+        reader.nextLine(); 
+        }
+        System.out.print("From:");      
+        fromInstitute = reader.nextLine();
+        System.out.print("To:");      
+        toInstitute = reader.nextLine();
+    }
+
+}
