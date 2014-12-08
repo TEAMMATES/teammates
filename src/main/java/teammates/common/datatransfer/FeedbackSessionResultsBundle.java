e4a4af7cc56e5c2645c9c6718a67ce3ac0a9890f
Kang HJ <kanghongjin@gmail.com>
2014-12-08 13:22:53 +0800

    Added extra row that displays Perceived Contribution if user did not respond to self

--- src/main/java/teammates/common/datatransfer/FeedbackSessionResultsBundle.java
+++ src/main/java/teammates/common/datatransfer/FeedbackSessionResultsBundle.java
@@ -253,6 +253,35 @@ public class FeedbackSessionResultsBundle implements SessionResultsBundle{
         return responseAnswerHtml;
     }
     
+    public boolean hasTargetGivenResponseToSelfInContributionQuestion(FeedbackQuestionAttributes question, String targetEmail) {
+        Map<String, StudentResultSummary> stats = getContribQnStudentResultSummary(question);
+        StudentResultSummary studentResult = stats.get(targetEmail);
+        
+        return studentResult.claimedFromStudent != Const.INT_UNINITIALIZED && 
+               studentResult.claimedFromStudent != Const.POINTS_NOT_SUBMITTED;
+    }
+    
+    public String getContributionQuestionPerceivedContributionHtml(FeedbackQuestionAttributes question,
+            String targetEmail) {
+        FeedbackAbstractQuestionDetails questionDetails = question.getQuestionDetails();
+        Map<String, StudentResultSummary> stats = getContribQnStudentResultSummary(question);
+        
+        StudentResultSummary studentResult = stats.get(targetEmail);
+        String responseAnswerHtml = FeedbackContributionQuestionDetails.convertToEqualShareFormatHtml(
+                studentResult.claimedToInstructor);
+        
+        int pc = studentResult.perceivedToInstructor;
+        @SuppressWarnings("static-access")
+        String pcHtml = ((FeedbackContributionQuestionDetails) questionDetails).convertToEqualShareFormatHtml(pc);
+        responseAnswerHtml += "<span>&nbsp;&nbsp;["
+                + "Perceived Contribution: "
+                + pcHtml
+                + "]</span>";
+        
+        return responseAnswerHtml;
+    }
+    
+    
     private String getContributionQuestionResponseAnswerCsv(
             FeedbackResponseAttributes response,
             FeedbackQuestionAttributes question,
