222060668c069df21087c22813e4015c88134f17
rcyq <quan0203@gmail.com>
2014-12-30 03:31:40 +0800

    Created another course for unregistered student session

--- src/test/java/teammates/test/cases/ui/StudentFeedbackSubmissionEditSaveActionTest.java
+++ src/test/java/teammates/test/cases/ui/StudentFeedbackSubmissionEditSaveActionTest.java
@@ -538,11 +538,11 @@ public class StudentFeedbackSubmissionEditSaveActionTest extends BaseActionTest 
         
         StudentAttributes unregisteredStudent = dataBundle.students.get("unregisteredStudentInCourse1");
         
-        fq = fqDb.getFeedbackQuestion("Unregistered Student Session", "FSQTT.idOfTypicalCourse1", 1);
+        fq = fqDb.getFeedbackQuestion("Unregistered Student Session", "FSQTT.idOfTypicalCourse2", 1);
         assertNotNull("Feedback question not found in database", fq);
         fqd = (FeedbackNumericalScaleQuestionDetails) fq.getQuestionDetails();
         
-        fr = dataBundle.feedbackResponses.get("response1ForQ1S6C1");
+        fr = dataBundle.feedbackResponses.get("response1ForQ1S1C2");
         fr = frDb.getFeedbackResponse(fq.getId(), fr.giverEmail, fr.recipientEmail);
         // ensure correct response id is retrieved
         assertNotNull("Feedback response not found in database", fr);
@@ -581,8 +581,8 @@ public class StudentFeedbackSubmissionEditSaveActionTest extends BaseActionTest 
         
         assertFalse(redirectResult.isError);
         assertEquals(Const.ActionURIs.STUDENT_FEEDBACK_SUBMISSION_EDIT_PAGE + 
-                "?studentemail=unregisteredStudentInCourse1%40gmail.tmt&error="+redirectResult.isError+"&courseid="+unregisteredStudent.course
-                + "&fsname=Unregistered+Student+Session&key=B1C228E80034757806E898A30004811F1D48E908E4FEBC58E5E636B8A972F075", 
+                "?studentemail=unregisteredStudentInCourse2%40gmail.tmt&error="+redirectResult.isError+"&courseid="+unregisteredStudent.course
+                + "&fsname=Unregistered+Student+Session&key=9358DCAD3C1AE18F2D60BE06770D2148AE249011E9A28BD2CDA26EF18CCF8F5D", 
                 redirectResult.getDestinationWithParams());
         assertEquals("All responses submitted succesfully!", redirectResult.getStatusMessage());
         gaeSimulation.logoutUser();
@@ -621,8 +621,8 @@ public class StudentFeedbackSubmissionEditSaveActionTest extends BaseActionTest 
         
         assertTrue(redirectResult.isError);
         assertEquals(Const.ActionURIs.STUDENT_FEEDBACK_SUBMISSION_EDIT_PAGE + 
-                "?studentemail=unregisteredStudentInCourse1%40gmail.tmt&error="+redirectResult.isError+"&courseid="+unregisteredStudent.course
-                + "&fsname=Unregistered+Student+Session&key=B1C228E80034757806E898A30004811F1D48E908E4FEBC58E5E636B8A972F075", 
+                "?studentemail=unregisteredStudentInCourse2%40gmail.tmt&error="+redirectResult.isError+"&courseid="+unregisteredStudent.course
+                + "&fsname=Unregistered+Student+Session&key=9358DCAD3C1AE18F2D60BE06770D2148AE249011E9A28BD2CDA26EF18CCF8F5D", 
                 redirectResult.getDestinationWithParams());
         assertEquals("100 is out of the range for Numerical-scale question.(min=1, max=5)", redirectResult.getStatusMessage());
         gaeSimulation.logoutUser();
