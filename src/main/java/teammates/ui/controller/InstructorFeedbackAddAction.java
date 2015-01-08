b8c8db826cd1654b268dde40c623e4259712932b
Low WeiLin <lwl1991@gmail.com>
2015-01-08 11:23:28 +0800

    Change form UI when no active courses

--- src/main/java/teammates/ui/controller/InstructorFeedbackAddAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackAddAction.java
@@ -31,6 +31,7 @@ public class InstructorFeedbackAddAction extends InstructorFeedbacksPageAction {
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         
         Assumption.assertPostParamNotNull(Const.ParamsNames.COURSE_ID, courseId);
+        Assumption.assertNotEmpty(courseId);
         
         InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, account.googleId); 
         
