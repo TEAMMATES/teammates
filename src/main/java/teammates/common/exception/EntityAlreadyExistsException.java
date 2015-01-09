a8595d046d1030451dd85d1bba9838297c0e23ac
Kang HJ <kanghongjin@gmail.com>
2015-01-09 09:42:56 +0800

    Revert "Added attribute to EntityAlreadyExistsException"
    This reverts commit f88f1c8849d5cb27e41db9417eaaafb663d50a5e.

--- src/main/java/teammates/common/exception/EntityAlreadyExistsException.java
+++ src/main/java/teammates/common/exception/EntityAlreadyExistsException.java
@@ -1,22 +1,9 @@
 package teammates.common.exception;
 
-import teammates.common.datatransfer.EntityAttributes;
-
 
 @SuppressWarnings("serial")
 public class EntityAlreadyExistsException extends TeammatesException {
-    private EntityAttributes offendingEntity;
-    
     public EntityAlreadyExistsException(String message) {
         super(message);
-    }
-    
-    public EntityAlreadyExistsException(String message, EntityAttributes obj) {
-        super(message);
-        offendingEntity = obj;
-    }
-    
-    public EntityAttributes getOffendingEntity() {
-        return offendingEntity;
     }
 }
