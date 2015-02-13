9678c737a89d770fe5b22c7c82f5c3d7bddc648f
Kang HJ <kanghongjin@gmail.com>
2015-02-13 23:00:48 +0800

    Fix incorrect comments

--- src/main/webapp/js/feedbackSubmissionsEdit.js
+++ src/main/webapp/js/feedbackSubmissionsEdit.js
@@ -62,26 +62,26 @@ function prepareContribQuestions() {
 
 // Prepare MSQ questions 
 function prepareMSQQuestions() {
-    // Get index of contribution questions
-    var MsqQuestionNum = getQuestionTypeNumbers("MSQ");
-    for(var i=0 ; i<MsqQuestionNum.length ; i++) {
-        var qnNum = MsqQuestionNum[i];
+    // Get index of MSQ questions
+    var msqQuestionNums = getQuestionTypeNumbers("MSQ");
+    for(var i=0 ; i<msqQuestionNums.length ; i++) {
+        var qnNum = msqQuestionNums[i];
         
-        // Get blank option
         var noneOfTheAboveOption = $("input[name^='responsetext-" + qnNum + "'][value='']");
 
-        // onclick event to reset other options 
+        // reset other options when "none of the above" is clicked
         noneOfTheAboveOption.click(function() {
-            var options = $(this).closest("table").find("input[value!='']");
+            var options = $(this).closest("table").find("input[name^='responsetext-'][value!='']");
             
             options.each(function() {
                 $(this).prop('checked', false);
             });
         });
 
+        // reset "none of the above" if any option is clicked
         var options = $("input[name^='responsetext-" + qnNum + "'][value!='']");
         options.click(function() {
-            var noneOfTheAboveOption = $(this).closest("table").find("input[value='']");
+            var noneOfTheAboveOption = $(this).closest("table").find("input[name^='responsetext-'][value='']");
             noneOfTheAboveOption.prop('checked', false);
         });
 
