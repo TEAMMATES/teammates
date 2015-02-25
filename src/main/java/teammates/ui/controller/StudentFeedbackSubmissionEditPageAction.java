9b1f88cf8d6d5ca6279448639a1590009e44770a
Thyagesh Manikandan <thyageshm@gmail.com>
2015-02-25 20:51:25 +0800

    refactored isRegistered() function to AccountAttributes

--- src/main/java/teammates/ui/controller/StudentFeedbackSubmissionEditPageAction.java
+++ src/main/java/teammates/ui/controller/StudentFeedbackSubmissionEditPageAction.java
@@ -127,7 +127,6 @@ public class StudentFeedbackSubmissionEditPageAction extends FeedbackSubmissionE
     }
     
     protected boolean isRegisteredStudent(){
-        // a registered student must have an associated google Id
-        return (account.googleId != null) && (!account.googleId.isEmpty());
+        return account.isUserRegistered();
     }
 }
