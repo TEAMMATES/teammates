ad83477f64b46b0cfd1a63cabc84a0422cf367e9
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-03-27 13:46:14 +0800

    added UI test to ensure that the 3-dp restriction works

--- src/test/java/teammates/test/cases/ui/browsertests/InstructorFeedbackEditPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorFeedbackEditPageUiTest.java
@@ -635,6 +635,13 @@ public class InstructorFeedbackEditPageUiTest extends BaseUiTestCase {
         
         assertEquals("Please enter valid options. The interval is not divisible by the specified increment.", feedbackEditPage.getStatus());
         
+        ______TS("more than three dp step rounding test");
+
+        feedbackEditPage.fillMaxNumScaleBox("1002", -1);
+        feedbackEditPage.fillStepNumScaleBox("1.00123456789", -1);
+
+        assertEquals("[Based on the above settings, acceptable responses are: 1, 2.001, 3.002, ..., 999.998, 1000.999, 1002]",
+                feedbackEditPage.getNumScalePossibleValuesString(-1));
         
         ______TS("NUMSCALE: min >= max test");
         //Tests javascript that automatically makes max = min+1 when max is <= min.
