accfd1c1bd90342b1e71671cab62d446a4d5acb9
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-04-02 06:31:32 +0800

    added new test case for numscale options with floating point error. failed miserably.

--- src/test/java/teammates/test/cases/ui/browsertests/InstructorFeedbackEditPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/InstructorFeedbackEditPageUiTest.java
@@ -625,7 +625,6 @@ public class InstructorFeedbackEditPageUiTest extends BaseUiTestCase {
         feedbackEditPage.fillQuestionBox("NumScale qn");
         feedbackEditPage.fillMinNumScaleBox("1", -1);
         feedbackEditPage.fillStepNumScaleBox("0.3", -1);
-        feedbackEditPage.fillMaxNumScaleBox("", -1);
         feedbackEditPage.fillMaxNumScaleBox("5", -1);
         
         assertEquals("[The interval 1 - 5 is not divisible by the specified increment.]",
@@ -634,6 +633,16 @@ public class InstructorFeedbackEditPageUiTest extends BaseUiTestCase {
         feedbackEditPage.clickAddQuestionButton();
         
         assertEquals("Please enter valid options. The interval is not divisible by the specified increment.", feedbackEditPage.getStatus());
+        
+        ______TS("possible floating point error");
+        
+        feedbackEditPage.fillQuestionBox("NumScale qn");
+        feedbackEditPage.fillMinNumScaleBox("1", -1);
+        feedbackEditPage.fillStepNumScaleBox("0.001", -1);
+        feedbackEditPage.fillMaxNumScaleBox("5555", -1);
+        
+        assertEquals("[Based on the above settings, acceptable responses are: 1, 1.001, 1.002, ..., 5554.998, 5554.999, 5555]",
+                feedbackEditPage.getNumScalePossibleValuesString(-1));
         
         ______TS("more than three dp step rounding test");
 
