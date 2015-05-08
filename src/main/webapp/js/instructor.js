a3c5ce69cc5b6c95654da2384e2d229464a10840
MrFool <lordyyap@gmail.com>
2015-05-09 01:54:58 +0800

    Added quotes around feedback session name

--- src/main/webapp/js/instructor.js
+++ src/main/webapp/js/instructor.js
@@ -73,10 +73,10 @@ function toggleDeleteFeedbackSessionConfirmation(courseID, name) {
  */
 function togglePublishEvaluation(name, willSendPublishEmail) {
     if (willSendPublishEmail) {
-        return confirm("Are you sure you want to publish the responses for the session " + name + "?" + 
-                " An email will be sent to students to inform them that the responses are ready for viewing.");
+        return confirm("Are you sure you want to publish the responses for the session \"" + name + "\"?" +
+               " An email will be sent to students to inform them that the responses are ready for viewing.");
     } else {
-        return confirm("Are you sure you want to publish the responses for the session " + name + "?");
+        return confirm("Are you sure you want to publish the responses for the session \"" + name + "\"?");
     }
 }
 
