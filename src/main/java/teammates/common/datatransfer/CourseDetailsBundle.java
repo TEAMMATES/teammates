b05f6610b163a0c9352f18e560b38f4b69291db0
Kang HJ <kanghongjin@gmail.com>
2015-05-11 12:12:00 +0800

    Update various files which were missed out

--- src/main/java/teammates/common/datatransfer/CourseDetailsBundle.java
+++ src/main/java/teammates/common/datatransfer/CourseDetailsBundle.java
@@ -6,10 +6,9 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Represents details of a course, including its students and evaluations.
+ * Represents details of a course, including its students and feedback sessions.
  * <br> Contains:
  * <br> * statistics of teams, enrollments, registrations
- * <br> * Details of its evaluations (as {@link EvaluationDetailsBundle} objects)
  * <br> * Details of its feedback sessions (as {@link FeedbackSessionDetailsBundle} objects)
  * <br> * Details of its teams (as {@link TeamDetailsBundle} objects)
  * <br> * Details of students without teams i.e. 'loners'  (as {@link StudentAttributes} objects)
