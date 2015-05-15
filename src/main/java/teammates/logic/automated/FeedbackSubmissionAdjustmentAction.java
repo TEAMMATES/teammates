83397347e960ee698dcce670468052b64d744994
Kang HJ <kanghongjin@gmail.com>
2015-05-15 15:44:08 +0800

    Modify FeedbackSubmissionAdjustmentAction to account for renaming and moving of entire team

--- src/main/java/teammates/logic/automated/FeedbackSubmissionAdjustmentAction.java
+++ src/main/java/teammates/logic/automated/FeedbackSubmissionAdjustmentAction.java
@@ -2,16 +2,22 @@ package teammates.logic.automated;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 import teammates.common.datatransfer.FeedbackResponseAttributes;
 import teammates.common.datatransfer.FeedbackSessionAttributes;
+import teammates.common.datatransfer.StudentAttributes.UpdateStatus;
 import teammates.common.datatransfer.StudentEnrollDetails;
 import teammates.common.util.ActivityLogEntry;
 import teammates.common.util.Assumption;
 import teammates.common.util.HttpRequestHelper;
+import teammates.common.util.StringHelper;
 import teammates.common.util.Utils;
 import teammates.common.util.Const.ParamsNames;
 import teammates.logic.core.FeedbackResponsesLogic;
@@ -87,11 +93,99 @@ public class FeedbackSubmissionAdjustmentAction extends TaskQueueWorkerAction {
                     return false;
                 }
             } 
+            adjustResponsesForTeamChange(enrollmentList, feedbackSession.feedbackSessionName);
+            
             return true;
         } else {
             log.severe(String.format(errorString, sessionName, courseId, "feedback session is null", ""));
             return false;
         }    
     }
+
+    private void adjustResponsesForTeamChange(ArrayList<StudentEnrollDetails> enrolmentList, String feedbackSessionName) {
+        FeedbackResponsesLogic frLogic = FeedbackResponsesLogic.inst();
+        // check if all members of the same team got moved to the same team
+        // if it is, then the team is renamed
+        Map<String, String> oldTeamToNewTeamMap = new HashMap<String, String>();
+        Map<String, Boolean> isGivenTeamRenamed = new HashMap<String, Boolean>();
+        
+        // initialise isGivenTeamRenamed 
+        for (StudentEnrollDetails details : enrolmentList) {
+            String originalSectionTeam = constructOriginalSectionTeamStr(details);
+            
+            if (details.updateStatus == UpdateStatus.UNMODIFIED) {
+                // if someone from the same team is not modified, then
+                // the team is not renamed
+                isGivenTeamRenamed.put(originalSectionTeam, false);
+                continue;
+            }
+            if (details.oldSection == null && details.oldTeam == null) {
+                continue; // team was not modified
+            }
+            
+            if (!isGivenTeamRenamed.containsKey(originalSectionTeam)) {
+                isGivenTeamRenamed.put(originalSectionTeam, true);
+            }
+        }
+        
+        // For every old team, check if there exists another member of the old team
+        // who got a different new team
+        for (StudentEnrollDetails details : enrolmentList) {
+            String originalSectionTeam = constructOriginalSectionTeamStr(details);
+            String newSectionTeam = details.newSection + "|" + details.newTeam;
+            
+            if (oldTeamToNewTeamMap.containsKey(originalSectionTeam)) {
+                if (!oldTeamToNewTeamMap.get(originalSectionTeam).equals(newSectionTeam)) {
+                    isGivenTeamRenamed.put(originalSectionTeam, false);
+                }
+            } else {
+                oldTeamToNewTeamMap.put(originalSectionTeam, newSectionTeam);
+            }
+        }
+        
+        for (Entry<String, Boolean> entry : isGivenTeamRenamed.entrySet()) {
+            Boolean isTeamRenamed = entry.getValue();
+            if (!isTeamRenamed) {
+                continue;
+            }
+            
+            String sectionTeamToRename = entry.getKey();
+            String[] splitsectionTeamToRename = sectionTeamToRename.split("\\|");
+            String oldSection = splitsectionTeamToRename[0];
+            String oldTeam = splitsectionTeamToRename[1];
+            
+            String newSectionTeam = oldTeamToNewTeamMap.get(sectionTeamToRename);
+            String[] splitNewSectionTeam = newSectionTeam.split("\\|");
+            String newSection = splitNewSectionTeam[0];
+            String newTeam = splitNewSectionTeam[1];
+            
+            frLogic.updateFeedbackResponsesForRenamingTeam(courseId, feedbackSessionName, oldTeam, oldSection, newTeam, newSection);
+        }
+    }
+
+    private String constructOriginalSectionTeamStr(StudentEnrollDetails details) {
+        boolean isSectionChanged = details.oldSection != null;
+        String oldSectionName;
+        if (isSectionChanged) { 
+            oldSectionName = details.oldSection;
+        } else {
+            // if the section is not changed, details.oldSection is null
+            // and newSection contains the original value
+            oldSectionName = details.newSection;
+        }
+        
+        boolean isTeamChanged = details.oldTeam != null;
+        String oldTeamName;
+        if (isTeamChanged) {
+            oldTeamName = details.oldTeam;
+        } else {
+            // if the team is not changed, details.oldteam is null
+            // and newTeam contains the original value
+            oldTeamName = details.newTeam;
+        }
+        
+        return oldSectionName + "|" + oldTeamName;
+    }
+    
 
 }
