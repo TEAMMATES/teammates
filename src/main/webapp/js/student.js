6f75e45a28db1b8254245dc0701ed47a8ccf3754
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-05-15 18:05:57 +0800

    reverted student.js (covered in other slice)

--- src/main/webapp/js/student.js
+++ src/main/webapp/js/student.js
@@ -3,14 +3,14 @@
  * should be common to the student pages.
  */
 
-// Initial load-up
-// -----------------------------------------------------------------------------
-$(function() {
-    $("[data-toggle='tooltip']").tooltip({
-        html: true
-    });
+
+
+//Initial load-up
+//-----------------------------------------------------------------------------
+$(function() { 
+    $("[data-toggle='tooltip']").tooltip({html: true});
     $("[data-unreg].navLinks").click(function() {
-        return confirm("You have to register using a google account "
-                + "in order to access this page. Would you like to proceed and register?");
+    	return confirm("You have to register using a google account " +
+    			"in order to access this page. Would you like to proceed and register?");
     });
 });
