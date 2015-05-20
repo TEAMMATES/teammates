6aaba367c0b8ce538ca9536704377688d92ed56e
MrFool <lordyyap@gmail.com>
2015-05-20 15:40:20 +0800

    All teammates exception code formmating

--- src/main/java/teammates/common/exception/EntityDoesNotExistException.java
+++ src/main/java/teammates/common/exception/EntityDoesNotExistException.java
@@ -2,7 +2,6 @@ package teammates.common.exception;
 
 @SuppressWarnings("serial")
 public class EntityDoesNotExistException extends Exception {
-
     public EntityDoesNotExistException(String message) {
         super(message);
     }
