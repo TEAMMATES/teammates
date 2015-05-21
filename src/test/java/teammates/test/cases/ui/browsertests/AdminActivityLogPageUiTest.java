92468b64f5aa40bcedb213a843a2584c21cb39ad
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-05-21 11:08:59 +0800

    renamed the exceptions for accuracy sake

--- src/test/java/teammates/test/cases/ui/browsertests/AdminActivityLogPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/AdminActivityLogPageUiTest.java
@@ -52,12 +52,12 @@ public class AdminActivityLogPageUiTest extends BaseUiTestCase {
             logPage.clickViewActionsButtonOfFirstEntry();
             String actualPersonInfo = logPage.getFilterBoxString();
             assertEqualsIfQueryStringNotEmpty(expectedPersonInfo, actualPersonInfo);            
-        } catch (NoSuchElementException emptylogs) {
+        } catch (NoSuchElementException exceptionFromEmptyLogs) {
             /*
              * This can happen if this test is run right after the server is started.
              * In this case, no view actions can be done.
              */
-        } catch (IndexOutOfBoundsException invisibletmtlogs) {
+        } catch (IndexOutOfBoundsException exceptionFromInvisibleTmtLogs) {
             /*
              * This can happen if all the log entries are from test accounts
              * (i.e emails ending with .tmt) because they are invisible.
