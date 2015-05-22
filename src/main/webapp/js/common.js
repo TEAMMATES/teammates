e98935372362f2d2846268c3f32773a1523d4a9a
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-05-22 13:30:42 +0800

    used innerText instead of innerHTML to ignore HTML wrappers

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -189,7 +189,7 @@ function sortTable(oneOfTableCell, colIdx, comparator, ascending, row) {
             continue;
         }
         
-        var innerText = $RowList[i].cells[colIdx - 1].innerHTML;
+        var innerText = $RowList[i].cells[colIdx - 1].innerText;
         
         // Store rows together with the innerText to compare
         store.push([innerText, $RowList[i], i]);
