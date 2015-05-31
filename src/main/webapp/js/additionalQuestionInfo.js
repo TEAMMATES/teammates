1d798df0c07be1b72eded1b0e0061809582e0ef1
MrFool <lordyyap@gmail.com>
2015-05-31 23:06:12 +0800

    additionalQuestionInfo.js code formatting and removal of console logs

--- src/main/webapp/js/additionalQuestionInfo.js
+++ src/main/webapp/js/additionalQuestionInfo.js
@@ -1,16 +1,12 @@
-/*
-This function is called to toggle the visibility of additional question information for the specified question.
-*/
-function toggleAdditionalQuestionInfo(identifier){
-    console.log($("#questionAdditionalInfoButton-"+identifier).text());
-    console.log("#questionAdditionalInfoButton-"+identifier);
+// This function is called to toggle the visibility of additional question information for the specified question.
+function toggleAdditionalQuestionInfo(identifier) {
+    var $questionButton = $('#questionAdditionalInfoButton-' + identifier);
 
-    var element = $("#questionAdditionalInfoButton-"+identifier)
-
-    if(element.text() == element.attr('data-more')){
-        element.text(element.attr('data-less'));
+    if ($questionButton.text() == $questionButton.attr('data-more')) {
+        $questionButton.text($questionButton.attr('data-less'));
     } else {
-        element.text(element.attr('data-more'));
+        $questionButton.text($questionButton.attr('data-more'));
     }
-    $("#questionAdditionalInfo-"+identifier).toggle();
+
+    $('#questionAdditionalInfo-' + identifier).toggle();
 }
