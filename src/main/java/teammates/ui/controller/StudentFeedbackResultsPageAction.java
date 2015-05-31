a4da5c1c0ac3cc9a41c40ae741382e38c967fe53
MrFool <lordyyap@gmail.com>
2015-05-31 22:50:20 +0800

    StudentFeedbackResultsPageAction.java code formatting

--- src/main/java/teammates/ui/controller/StudentFeedbackResultsPageAction.java
+++ src/main/java/teammates/ui/controller/StudentFeedbackResultsPageAction.java
@@ -7,50 +7,48 @@ import teammates.common.util.Const;
 import teammates.logic.api.GateKeeper;
 
 public class StudentFeedbackResultsPageAction extends Action {
-
     @Override
     protected ActionResult execute() throws EntityDoesNotExistException {
-        
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
-        
-        if(courseId==null || feedbackSessionName == null) {
+
+        if (courseId==null || feedbackSessionName == null) {
             return createRedirectResult(Const.ActionURIs.STUDENT_HOME_PAGE);
         }
-        
-        if(!isJoinedCourse(courseId)){
+
+        if (!isJoinedCourse(courseId)) {
             return createPleaseJoinCourseResponse(courseId);
         }
-        
-        new GateKeeper().verifyAccessible(
-                getCurrentStudent(courseId), 
-                logic.getFeedbackSession(feedbackSessionName, courseId));
-        
+
+        new GateKeeper().verifyAccessible(getCurrentStudent(courseId),
+                                          logic.getFeedbackSession(feedbackSessionName, courseId));
+
         StudentFeedbackResultsPageData data = new StudentFeedbackResultsPageData(account, student);
-        
+
         data.student = getCurrentStudent(courseId);
         data.bundle = logic.getFeedbackSessionResultsForStudent(feedbackSessionName, courseId, data.student.email);
-        if(data.bundle == null) {
-            throw new EntityDoesNotExistException("Feedback session "+feedbackSessionName+" does not exist in "+courseId+".");
+
+        if (data.bundle == null) {
+            throw new EntityDoesNotExistException("Feedback session " + feedbackSessionName
+                                                  + " does not exist in " + courseId + ".");
         }
-        
+
         if (data.bundle.feedbackSession.isPublished() == false) {
-            throw new UnauthorizedAccessException(
-                    "This feedback session is not yet visible.");
+            throw new UnauthorizedAccessException("This feedback session is not yet visible.");
         }
-        
+
         if (data.bundle.isStudentHasSomethingNewToSee(data.student)) {
             statusToUser.add(Const.StatusMessages.FEEDBACK_RESULTS_SOMETHINGNEW);
         } else {
             statusToUser.add(Const.StatusMessages.FEEDBACK_RESULTS_NOTHINGNEW);
         }
-        
-        statusToAdmin = "Show student feedback result page<br>" +
-                "Session Name: " + feedbackSessionName + "<br>" + 
-                "Course ID: " + courseId;
+
+        statusToAdmin = "Show student feedback result page<br>"
+                        + "Session Name: " + feedbackSessionName + "<br>"
+                        + "Course ID: " + courseId;
         return createShowPageResult(Const.ViewURIs.STUDENT_FEEDBACK_RESULTS, data);
     }
-    
+
     // The following methods are overridden by the unregistered version of this action
 
     protected boolean isJoinedCourse(String courseId) {
