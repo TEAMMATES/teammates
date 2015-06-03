05f6e34c520ae8a379a8e728b87840b3cc8297cb
MrFool <lordyyap@gmail.com>
2015-06-03 11:28:51 +0800

    Covered all except one branch

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackQuestionVisibilityMessageActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackQuestionVisibilityMessageActionTest.java
@@ -36,7 +36,7 @@ public class InstructorFeedbackQuestionVisibilityMessageActionTest extends BaseA
                                             .inst()
                                             .getFeedbackQuestion(fs.feedbackSessionName, fs.courseId, 1);
         
-        ______TS("Typical Case");
+        ______TS("Typical Case - max -> constructed params");
 
         String[] typicalParams = new String[]{
                 Const.ParamsNames.COURSE_ID, fs.courseId,
@@ -62,9 +62,9 @@ public class InstructorFeedbackQuestionVisibilityMessageActionTest extends BaseA
         assertEquals("", r.getStatusMessage());
         assertFalse(r.isError);
         
-        ______TS("Custom Case");
+        ______TS("Custom Case Students - constructed params");
         
-        String[] customParams = {
+        String[] customParams = new String[]{
                 Const.ParamsNames.COURSE_ID, fs.courseId,
                 Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.feedbackSessionName,
                 Const.ParamsNames.FEEDBACK_QUESTION_GIVERTYPE, FeedbackParticipantType.STUDENTS.toString(),
@@ -86,6 +86,128 @@ public class InstructorFeedbackQuestionVisibilityMessageActionTest extends BaseA
 
         assertEquals(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_QUESTION + "?error=false"
                      + "&user=idOfInstructor1OfCourse1", r.getDestinationWithParams());
+        assertEquals("", r.getStatusMessage());
+        assertFalse(r.isError);
+        
+        ______TS("Custom Case Teams - data bundle params");
+
+        fs = dataBundle.feedbackSessions.get("session2InCourse1");
+        fq = FeedbackQuestionsLogic.inst().getFeedbackQuestion(fs.feedbackSessionName, fs.courseId, 1);
+        
+        customParams = new String[]{
+                Const.ParamsNames.COURSE_ID, fs.courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.feedbackSessionName,
+                Const.ParamsNames.FEEDBACK_QUESTION_GIVERTYPE, FeedbackParticipantType.TEAMS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_RECIPIENTTYPE, FeedbackParticipantType.TEAMS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBER, "1",
+                Const.ParamsNames.FEEDBACK_QUESTION_TYPE, "TEXT",
+                Const.ParamsNames.FEEDBACK_QUESTION_TEXT, "question",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIESTYPE, "custom",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIES, "2",
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRESPONSESTO, FeedbackParticipantType.RECEIVER.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWGIVERTO, FeedbackParticipantType.RECEIVER.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRECIPIENTTO, FeedbackParticipantType.RECEIVER.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_EDITTYPE, "edit",
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, fq.getId()
+        };
+
+        a = getAction(customParams);
+        r = (ActionResult) a.executeAndPostProcess();
+
+        assertEquals(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_QUESTION + "?error=false"
+                     + "&user=idOfInstructor1OfCourse1", r.getDestinationWithParams());
+        assertEquals("", r.getStatusMessage());
+        assertFalse(r.isError);
+        
+        ______TS("Custom Case Instructor - data bundle params");
+
+        fs = dataBundle.feedbackSessions.get("gracePeriodSession");
+        fq = FeedbackQuestionsLogic.inst().getFeedbackQuestion(fs.feedbackSessionName, fs.courseId, 1);
+        
+        customParams = new String[]{
+                Const.ParamsNames.COURSE_ID, fs.courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.feedbackSessionName,
+                Const.ParamsNames.FEEDBACK_QUESTION_GIVERTYPE, FeedbackParticipantType.INSTRUCTORS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_RECIPIENTTYPE, FeedbackParticipantType.INSTRUCTORS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBER, "2",
+                Const.ParamsNames.FEEDBACK_QUESTION_TYPE, "TEXT",
+                Const.ParamsNames.FEEDBACK_QUESTION_TEXT, "question",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIESTYPE, "custom",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIES, "1",
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRESPONSESTO, FeedbackParticipantType.RECEIVER.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWGIVERTO, FeedbackParticipantType.RECEIVER.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRECIPIENTTO, FeedbackParticipantType.RECEIVER.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_EDITTYPE, "edit",
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, fq.getId()
+        };
+
+        a = getAction(customParams);
+        r = (ActionResult) a.executeAndPostProcess();
+
+        assertEquals(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_QUESTION + "?error=false"
+                     + "&user=idOfInstructor1OfCourse1", r.getDestinationWithParams());
+        assertEquals("", r.getStatusMessage());
+        assertFalse(r.isError);
+        
+        ______TS("Private case, empty participant list - data bundle params");
+
+        String instructor1OfCourse2 = dataBundle.instructors.get("instructor1OfCourse2").googleId;
+
+        gaeSimulation.logoutUser(); // log out of instructor 1
+        gaeSimulation.loginAsInstructor(instructor1OfCourse2);
+
+        fs = dataBundle.feedbackSessions.get("session1InCourse2");
+        fq = FeedbackQuestionsLogic.inst().getFeedbackQuestion(fs.feedbackSessionName, fs.courseId, 1);
+        
+        String[] privateParams = new String[]{
+                Const.ParamsNames.COURSE_ID, fs.courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.feedbackSessionName,
+                Const.ParamsNames.FEEDBACK_QUESTION_GIVERTYPE, FeedbackParticipantType.INSTRUCTORS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_RECIPIENTTYPE, FeedbackParticipantType.STUDENTS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBER, "1",
+                Const.ParamsNames.FEEDBACK_QUESTION_TYPE, "TEXT",
+                Const.ParamsNames.FEEDBACK_QUESTION_TEXT, "question",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIESTYPE, "custom",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIES, "10",
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRESPONSESTO, "",
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWGIVERTO, "",
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRECIPIENTTO, "",
+                Const.ParamsNames.FEEDBACK_QUESTION_EDITTYPE, "edit",
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, fq.getId()
+        };
+
+        a = getAction(privateParams);
+        r = (ActionResult) a.executeAndPostProcess();
+
+        assertEquals(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_QUESTION + "?error=false"
+                     + "&user=idOfInstructor1OfCourse2", r.getDestinationWithParams());
+        assertEquals("", r.getStatusMessage());
+        assertFalse(r.isError);
+        
+        ______TS("Private case, null participant list - constructed params");
+        
+        privateParams = new String[]{
+                Const.ParamsNames.COURSE_ID, fs.courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.feedbackSessionName,
+                Const.ParamsNames.FEEDBACK_QUESTION_GIVERTYPE, FeedbackParticipantType.INSTRUCTORS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_RECIPIENTTYPE, FeedbackParticipantType.STUDENTS.toString(),
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBER, "1",
+                Const.ParamsNames.FEEDBACK_QUESTION_TYPE, "TEXT",
+                Const.ParamsNames.FEEDBACK_QUESTION_TEXT, "question",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIESTYPE, "custom",
+                Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFENTITIES, "10",
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRESPONSESTO, null,
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWGIVERTO, null,
+                Const.ParamsNames.FEEDBACK_QUESTION_SHOWRECIPIENTTO, null,
+                Const.ParamsNames.FEEDBACK_QUESTION_EDITTYPE, "edit",
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, fq.getId()
+        };
+
+        a = getAction(privateParams);
+        r = (ActionResult) a.executeAndPostProcess();
+
+        assertEquals(Const.ViewURIs.INSTRUCTOR_FEEDBACK_RESULTS_BY_QUESTION + "?error=false"
+                     + "&user=idOfInstructor1OfCourse2", r.getDestinationWithParams());
         assertEquals("", r.getStatusMessage());
         assertFalse(r.isError);
     }
