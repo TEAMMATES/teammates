e07088d27ce785f37cf19167e4a90f810d1d9d8e
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-06-12 12:16:50 +0800

    Added init() and parsed values for StudentCourseDetailsPanel in page data

--- src/main/java/teammates/ui/controller/StudentCourseDetailsPageData.java
+++ src/main/java/teammates/ui/controller/StudentCourseDetailsPageData.java
@@ -5,15 +5,48 @@ import java.util.List;
 import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.CourseDetailsBundle;
 import teammates.common.datatransfer.InstructorAttributes;
+import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.datatransfer.TeamDetailsBundle;
+import teammates.ui.template.StudentCourseDetailsPanel;
 
 public class StudentCourseDetailsPageData extends PageData {
     public CourseDetailsBundle courseDetails;
-    public TeamDetailsBundle team;
     public List<InstructorAttributes> instructors;
+    public TeamDetailsBundle team;
+    private StudentCourseDetailsPanel studentCourseDetailsPanel;
     
     public StudentCourseDetailsPageData(AccountAttributes account) {
         super(account);
+    }
+    
+    public void init(CourseDetailsBundle courseDetails, List<InstructorAttributes> instructors, 
+                    StudentAttributes student, TeamDetailsBundle team) {
+        this.courseDetails = courseDetails;
+        this.instructors = instructors;
+        this.student = student;
+        this.team = team;
+        
+        setStudentCourseDetailsPanel();
+    }
+    
+    public StudentCourseDetailsPanel getStudentCourseDetailsPanel() {
+        return studentCourseDetailsPanel;
+    }
+
+    private void setStudentCourseDetailsPanel() {
+        studentCourseDetailsPanel = createStudentCourseDetailsPanel();
+    }
+
+    private StudentCourseDetailsPanel createStudentCourseDetailsPanel() {
+        String courseId = courseDetails.course.id;
+        String courseName = courseDetails.course.name; 
+        String studentTeam = student.team;
+        String studentName = student.name;
+        String studentEmail = student.email;
+        List<StudentAttributes> teammates = team.students;
+        
+        return new StudentCourseDetailsPanel(courseId, courseName, instructors, studentTeam,
+                                                 studentName, studentEmail, teammates);
     }
     
 }
