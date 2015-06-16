b4c260190b5fae54fd062048632723a46aaa2d75
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-06-16 12:51:00 +0800

    Added get methods in FeedbackSessionAttributes

--- src/main/java/teammates/common/datatransfer/FeedbackSessionAttributes.java
+++ src/main/java/teammates/common/datatransfer/FeedbackSessionAttributes.java
@@ -114,6 +114,22 @@ public class FeedbackSessionAttributes extends EntityAttributes implements Sessi
         this.respondingInstructorList = instructorList;
         this.respondingStudentList = studentList;
     }
+    
+    public String getCourseId() {
+        return courseId;
+    }
+    
+    public String getFeedbackSessionName() {
+        return feedbackSessionName;
+    }
+    
+    public String getStartTime() {
+        return TimeHelper.formatTime(startTime);
+    }
+    
+    public String getEndTime() {
+        return TimeHelper.formatTime(endTime);
+    }
 
     @Override
     public FeedbackSession toEntity() {
