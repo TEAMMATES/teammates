9df851fac99270637c2718dad168bcf241a5ef4b
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-06-16 11:58:00 +0800

    removed unused imports and variables

--- src/main/java/teammates/ui/controller/InstructorStudentRecordsAjaxPageData.java
+++ src/main/java/teammates/ui/controller/InstructorStudentRecordsAjaxPageData.java
@@ -3,12 +3,10 @@ package teammates.ui.controller;
 import java.util.List;
 
 import teammates.common.datatransfer.AccountAttributes;
-import teammates.common.datatransfer.SessionAttributes;
 import teammates.common.datatransfer.SessionResultsBundle;
 
 public class InstructorStudentRecordsAjaxPageData extends PageData {
 
-    public List<SessionAttributes> sessions;
     public List<SessionResultsBundle> results;
 
     public InstructorStudentRecordsAjaxPageData(AccountAttributes account) {
--- src/main/java/teammates/ui/controller/InstructorStudentRecordsPageData.java
+++ src/main/java/teammates/ui/controller/InstructorStudentRecordsPageData.java
@@ -6,7 +6,6 @@ import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.CommentAttributes;
 import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.datatransfer.SessionAttributes;
-import teammates.common.datatransfer.SessionResultsBundle;
 import teammates.common.datatransfer.StudentProfileAttributes;
 
 public class InstructorStudentRecordsPageData extends PageData {
@@ -14,10 +13,8 @@ public class InstructorStudentRecordsPageData extends PageData {
     public String courseId;
     public InstructorAttributes currentInstructor;
     public StudentProfileAttributes studentProfile;
-    public String targetSessionName;
     public List<CommentAttributes> comments;
     public List<SessionAttributes> sessions;
-    public List<SessionResultsBundle> results;
     public String showCommentBox;
 
     public InstructorStudentRecordsPageData(AccountAttributes account) {
