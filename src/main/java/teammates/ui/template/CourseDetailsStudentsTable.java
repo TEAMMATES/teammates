b87b5d4c71ccf09ffeecc4a92943277af8f81441
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2015-06-18 13:46:54 +0900

    added tag files, jsp file and structure files used in tag files

---
+++ src/main/java/teammates/ui/template/CourseDetailsStudentsTable.java
@@ -0,0 +1,16 @@
+package teammates.ui.template;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseDetailsStudentsTable {
+    public List<CourseDetailsStudentsTableRow> rows;
+    
+    public CourseDetailsStudentsTable() {
+        this.rows = new ArrayList<CourseDetailsStudentsTableRow>();
+    }
+    
+    public List<CourseDetailsStudentsTableRow> getRows() {
+        return this.rows;
+    }
+}
