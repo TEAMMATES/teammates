8b7f5ef94c0a42959fc5261dffd684ca56391fe1
JosephineKwa <work.and.stuff.jo@gmail.com>
2015-06-19 15:39:30 +0800

    write test for pagedata

--- src/test/java/teammates/test/cases/ui/pagedata/InstructorCommentsPageDataTest.java
+++ src/test/java/teammates/test/cases/ui/pagedata/InstructorCommentsPageDataTest.java
@@ -2,32 +2,31 @@ package teammates.test.cases.ui.pagedata;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
 
 import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.CommentAttributes;
-import teammates.common.datatransfer.CourseAttributes;
 import teammates.common.datatransfer.CourseRoster;
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.FeedbackSessionAttributes;
 import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.datatransfer.StudentAttributes;
-import teammates.test.cases.BaseComponentTestCase;
 import teammates.test.cases.BaseTestCase;
+import teammates.test.util.TestHelper;
 import teammates.ui.controller.InstructorCommentsPageData;
 
-public class InstructorCommentsPageDataTest extends BaseComponentTestCase {
+public class InstructorCommentsPageDataTest extends BaseTestCase {
     private static DataBundle dataBundle = getTypicalDataBundle();
     
     @BeforeClass
     public void classSetUp() throws Exception {
         printTestClassHeader();
-        removeAndRestoreTypicalDataInDatastore();
     }
     
     @Test
@@ -47,7 +46,7 @@ public class InstructorCommentsPageDataTest extends BaseComponentTestCase {
         String courseId = "idOfTypicalCourse1";
         String courseName = "idOfTypicalCourse1 : Typical Course 1 with 2 Evals";
         List<String> coursePaginationList = Arrays.asList("idOfTypicalCourse1", "idOfTypicalCourse2");
-        Map<String, List<CommentAttributes>> comments = new HashMap<String, List<CommentAttributes>>();
+        Map<String, List<CommentAttributes>> comments = new TreeMap<String, List<CommentAttributes>>();
         
         List<CommentAttributes> commentsForGiverList = new ArrayList<CommentAttributes>();
         commentsForGiverList.add(dataBundle.comments.get("comment1FromI3C1toS2C1"));
@@ -57,15 +56,48 @@ public class InstructorCommentsPageDataTest extends BaseComponentTestCase {
         commentsForGiverList.add(dataBundle.comments.get("comment2FromI1C1toS1C1"));
         comments.put("instructor1@course1.tmt", commentsForGiverList);
         
-        InstructorAttributes instructor = dataBundle.instructors.get("instructor3OfCourse1");
+        Map<String, List<Boolean>> commentModifyPermissions = new TreeMap<String, List<Boolean>>();
+        
+        List<Boolean> canModifyCommentList = new ArrayList<Boolean>();
+        canModifyCommentList.add(true);
+        commentModifyPermissions.put(InstructorCommentsPageData.COMMENT_GIVER_NAME_THAT_COMES_FIRST, 
+                                     canModifyCommentList);
+        canModifyCommentList.add(true);
+        canModifyCommentList.add(true);
+        commentModifyPermissions.put("instructor1@course1.tmt", canModifyCommentList);
+        
         CourseRoster roster = new CourseRoster(getStudentsInCourse(courseId), getInstructorsInCourse(courseId));
         List<FeedbackSessionAttributes> feedbackSessions = getFeedbackSessionsForCourse(courseId);
         int numberOfPendingComments = 0;
         
         data.init(isViewingDraft, isDisplayArchive, courseId, courseName, coursePaginationList, 
-                  comments, instructor, roster, feedbackSessions, numberOfPendingComments);
+                  comments, commentModifyPermissions, roster, feedbackSessions, numberOfPendingComments);
         
+        Map<String, List<CommentAttributes>> actualComments = data.getComments();
+        Map<String, List<CommentAttributes>> expectedComments = comments;
         
+        List<String> actualGivers = new ArrayList<String>();
+        actualGivers.addAll(actualComments.keySet());
+        List<String> expectedGivers = new ArrayList<String>();
+        expectedGivers.addAll(expectedComments.keySet());
+        
+        TestHelper.isSameContentIgnoreOrder(expectedGivers, actualGivers);
+        for (String giverEmail : expectedGivers) {
+            TestHelper.isSameContentIgnoreOrder(expectedComments.get(giverEmail), actualComments.get(giverEmail));
+        }
+        
+        assertEquals(courseId, data.getCourseId());
+        assertEquals(courseName, data.getCourseName());
+        TestHelper.isSameContentIgnoreOrder(coursePaginationList, data.getCoursePaginationList());
+        TestHelper.isSameContentIgnoreOrder(feedbackSessions, data.getFeedbackSessions());
+        
+        String expectedNextPageLink = data.getInstructorCommentsLink() + "&courseid=" + "idOfTypicalCourse2";
+        String expectedPreviousPageLink = "javascript:;";
+        
+        assertEquals(data.getNextPageLink(), expectedNextPageLink);
+        assertEquals(data.getPreviousPageLink(), expectedPreviousPageLink);
+        
+        assertEquals(data.getNumberOfPendingComments(), numberOfPendingComments);
         
     }
     
