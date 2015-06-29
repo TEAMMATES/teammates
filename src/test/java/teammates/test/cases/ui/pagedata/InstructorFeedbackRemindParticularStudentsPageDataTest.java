45cdb24d2c32974d1d3d50f5be9bd82df7e965c4
junhaoyap <lordyyap@gmail.com>
2015-06-29 17:39:28 +0800

    Updated testing skeleton

--- src/test/java/teammates/test/cases/ui/pagedata/InstructorFeedbackRemindParticularStudentsPageDataTest.java
+++ src/test/java/teammates/test/cases/ui/pagedata/InstructorFeedbackRemindParticularStudentsPageDataTest.java
@@ -1,7 +1,9 @@
 package teammates.test.cases.ui.pagedata;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.DataBundle;
 import teammates.test.cases.BaseComponentTestCase;
 
@@ -12,5 +14,10 @@ public class InstructorFeedbackRemindParticularStudentsPageDataTest extends Base
     public static void classSetUp() throws Exception {
         printTestClassHeader();
         removeAndRestoreTypicalDataInDatastore();
+    }
+
+    @Test
+    public void testInitWithoutDefaultFormValues() throws Exception {
+        AccountAttributes instructorAccount = dataBundle.accounts.get("instructor1OfCourse1");
     }
 }
