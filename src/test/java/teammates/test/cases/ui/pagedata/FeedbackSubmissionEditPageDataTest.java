01833e25db60e69d6587750487dc9ecd400a57c2
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-06-29 17:40:31 +0800

    Added more tests in FeedbackSubmissionEditPageDataTest

--- src/test/java/teammates/test/cases/ui/pagedata/FeedbackSubmissionEditPageDataTest.java
+++ src/test/java/teammates/test/cases/ui/pagedata/FeedbackSubmissionEditPageDataTest.java
@@ -3,14 +3,25 @@ package teammates.test.cases.ui.pagedata;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertFalse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.FeedbackQuestionAttributes;
+import teammates.common.datatransfer.FeedbackResponseAttributes;
+import teammates.common.datatransfer.FeedbackSessionAttributes;
+import teammates.common.datatransfer.FeedbackSessionQuestionsBundle;
 import teammates.common.datatransfer.StudentAttributes;
+import teammates.common.util.Const;
 import teammates.test.cases.BaseTestCase;
 import teammates.ui.controller.FeedbackSubmissionEditPageData;
+import teammates.ui.template.StudentFeedbackSubmissionEditQuestionsWithResponses;
 
 public class FeedbackSubmissionEditPageDataTest extends BaseTestCase {
     private static DataBundle dataBundle = getTypicalDataBundle();
@@ -27,6 +38,25 @@ public class FeedbackSubmissionEditPageDataTest extends BaseTestCase {
         StudentAttributes student = dataBundle.students.get("student1InCourse1");
         FeedbackSubmissionEditPageData pageData = new FeedbackSubmissionEditPageData(studentAccount, student);
         
+        FeedbackSessionAttributes feedbackSession = dataBundle.feedbackSessions.get("session1InCourse1");
+        FeedbackQuestionAttributes question = dataBundle.feedbackQuestions.get("qn1InSession1InCourse1");
+        List<FeedbackResponseAttributes> responses = new ArrayList<FeedbackResponseAttributes>();
+        
+        responses.add(dataBundle.feedbackResponses.get("response1ForQ1S1C1"));
+        responses.add(dataBundle.feedbackResponses.get("response2ForQ1S1C1"));
+        
+        Map<FeedbackQuestionAttributes, List<FeedbackResponseAttributes>> questionResponseBundle = 
+                                        new HashMap<FeedbackQuestionAttributes, List<FeedbackResponseAttributes>>();
+        questionResponseBundle.put(question, responses);
+        
+        Map<String, Map<String, String>> recipientList = new HashMap<String, Map<String, String>>();
+        Map<String,String> recipients = new HashMap<String,String>();
+        recipients.put(student.email, Const.USER_NAME_FOR_SELF);
+        recipientList.put(question.getId(), recipients);
+        
+        
+        pageData.bundle = new FeedbackSessionQuestionsBundle(feedbackSession, questionResponseBundle, recipientList);
+        pageData.bundle.questionResponseBundle.put(question, responses);
         pageData.init(student.key, student.email, student.course);
         
         assertEquals("You are submitting feedback as <span class='text-danger text-bold text-large'>"
@@ -37,14 +67,25 @@ public class FeedbackSubmissionEditPageDataTest extends BaseTestCase {
         
         assertEquals("/page/studentFeedbackSubmissionEditSave", pageData.getSubmitAction());
         assertEquals("/page/studentFeedbackQuestionSubmissionEditSave", pageData.getSubmitActionQuestion());
-        assertFalse(pageData.isModeration);
-        assertFalse(pageData.isSessionOpenForSubmission);
+        assertFalse(pageData.isModeration());
+        assertFalse(pageData.isSessionOpenForSubmission());
         assertFalse(pageData.isSubmittable());
+        
+        StudentFeedbackSubmissionEditQuestionsWithResponses questionWithResponses = pageData.getQuestionsWithResponses().get(0);
+        
+        assertEquals(question.questionType, questionWithResponses.getQuestion().getQuestionType());
+        assertEquals(question.courseId, questionWithResponses.getQuestion().getCourseId());
+        assertEquals(question.questionNumber, questionWithResponses.getQuestion().getQuestionNumber());
+        assertEquals(question.getQuestionDetails().questionText, questionWithResponses.getQuestion().getQuestionText());
+        assertEquals(question.numberOfEntitiesToGiveFeedbackTo, questionWithResponses.getQuestion().getNumberOfEntitiesToGiveFeedbackTo());
+        assertEquals(question.getId(), questionWithResponses.getQuestion().getQuestionId());
         
         ______TS("student in unregistered course");
         student = dataBundle.students.get("student1InUnregisteredCourse");
         pageData = new FeedbackSubmissionEditPageData(studentAccount, student);
         
+        pageData.bundle = new FeedbackSessionQuestionsBundle(feedbackSession, questionResponseBundle, recipientList);
+        pageData.bundle.questionResponseBundle.put(question, responses);
         pageData.init(student.key, student.email, student.course);
         
         assertEquals("You are submitting feedback as <span class='text-danger text-bold text-large'>student1 "
@@ -56,14 +97,25 @@ public class FeedbackSubmissionEditPageDataTest extends BaseTestCase {
         
         assertEquals("/page/studentFeedbackSubmissionEditSave", pageData.getSubmitAction());
         assertEquals("/page/studentFeedbackQuestionSubmissionEditSave", pageData.getSubmitActionQuestion());
-        assertFalse(pageData.isModeration);
-        assertFalse(pageData.isSessionOpenForSubmission);
+        assertFalse(pageData.isModeration());
+        assertFalse(pageData.isSessionOpenForSubmission());
         assertFalse(pageData.isSubmittable());
+        
+        questionWithResponses = pageData.getQuestionsWithResponses().get(0);
+        
+        assertEquals(question.questionType, questionWithResponses.getQuestion().getQuestionType());
+        assertEquals(question.courseId, questionWithResponses.getQuestion().getCourseId());
+        assertEquals(question.questionNumber, questionWithResponses.getQuestion().getQuestionNumber());
+        assertEquals(question.getQuestionDetails().questionText, questionWithResponses.getQuestion().getQuestionText());
+        assertEquals(question.numberOfEntitiesToGiveFeedbackTo, questionWithResponses.getQuestion().getNumberOfEntitiesToGiveFeedbackTo());
+        assertEquals(question.getId(), questionWithResponses.getQuestion().getQuestionId());
         
         ______TS("student in archived course");
         student = dataBundle.students.get("student1InArchivedCourse");
         pageData = new FeedbackSubmissionEditPageData(studentAccount, student);
         
+        pageData.bundle = new FeedbackSessionQuestionsBundle(feedbackSession, questionResponseBundle, recipientList);
+        pageData.bundle.questionResponseBundle.put(question, responses);
         pageData.init(student.key, student.email, student.course);
         
         assertEquals("You are submitting feedback as <span class='text-danger text-bold text-large'>student1 In Course1"
@@ -74,8 +126,17 @@ public class FeedbackSubmissionEditPageDataTest extends BaseTestCase {
         
         assertEquals("/page/studentFeedbackSubmissionEditSave", pageData.getSubmitAction());
         assertEquals("/page/studentFeedbackQuestionSubmissionEditSave", pageData.getSubmitActionQuestion());
-        assertFalse(pageData.isModeration);
-        assertFalse(pageData.isSessionOpenForSubmission);
+        assertFalse(pageData.isModeration());
+        assertFalse(pageData.isSessionOpenForSubmission());
         assertFalse(pageData.isSubmittable());
+        
+        questionWithResponses = pageData.getQuestionsWithResponses().get(0);
+        
+        assertEquals(question.questionType, questionWithResponses.getQuestion().getQuestionType());
+        assertEquals(question.courseId, questionWithResponses.getQuestion().getCourseId());
+        assertEquals(question.questionNumber, questionWithResponses.getQuestion().getQuestionNumber());
+        assertEquals(question.getQuestionDetails().questionText, questionWithResponses.getQuestion().getQuestionText());
+        assertEquals(question.numberOfEntitiesToGiveFeedbackTo, questionWithResponses.getQuestion().getNumberOfEntitiesToGiveFeedbackTo());
+        assertEquals(question.getId(), questionWithResponses.getQuestion().getQuestionId());
     }
 }
