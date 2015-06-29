418c3300a9e90ecd2c6daa490477cd4d27757fb7
YJSoh <soh.you.jun@gmail.com>
2015-06-29 14:56:49 +0800

    Extracted picture url algo into PageData.

--- src/main/java/teammates/ui/controller/InstructorCourseStudentDetailsPageData.java
+++ src/main/java/teammates/ui/controller/InstructorCourseStudentDetailsPageData.java
@@ -3,7 +3,6 @@ package teammates.ui.controller;
 import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.datatransfer.StudentProfileAttributes;
-import teammates.common.util.Const;
 import teammates.ui.template.StudentInfoTable;
 import teammates.ui.template.StudentProfile;
 
@@ -18,11 +17,7 @@ public class InstructorCourseStudentDetailsPageData extends PageData {
             String commentRecipient) {
         super(account);
         if (studentProfile != null) {
-            String pictureUrl = studentProfile.pictureKey == null || studentProfile.pictureKey.isEmpty()
-                              ? Const.SystemParams.DEFAULT_PROFILE_PICTURE_PATH
-                              : Const.ActionURIs.STUDENT_PROFILE_PICTURE + "?"
-                                + Const.ParamsNames.BLOB_KEY + "=" + studentProfile.pictureKey + "&"
-                                + Const.ParamsNames.USER_ID + "=" + account.googleId;
+            String pictureUrl = getPictureUrl(studentProfile.pictureKey);
             this.studentProfile = new StudentProfile(student.name, studentProfile, pictureUrl);
         }
         this.studentInfoTable = new StudentInfoTable(student, isAbleToAddComment, hasSection);
