8f5b176241779d1944d04f0f3b222857a2c265b1
junhaoyap <lordyyap@gmail.com>
2015-06-30 17:50:22 +0800

    Made fields private and moved attributes for data into constructor

--- src/main/java/teammates/ui/controller/StudentCourseJoinConfirmationPageData.java
+++ src/main/java/teammates/ui/controller/StudentCourseJoinConfirmationPageData.java
@@ -4,12 +4,14 @@ import teammates.common.datatransfer.AccountAttributes;
 import teammates.common.datatransfer.StudentAttributes;
 
 public class StudentCourseJoinConfirmationPageData extends PageData {
-
-    public String confirmUrl;
-    public String logoutUrl;
+    private String confirmUrl;
+    private String logoutUrl;
     
-    public StudentCourseJoinConfirmationPageData(AccountAttributes account, StudentAttributes student) {
+    public StudentCourseJoinConfirmationPageData(AccountAttributes account, StudentAttributes student,
+                                                 String confirmUrl, String logoutUrl) {
         super(account, student);
+        this.confirmUrl = confirmUrl;
+        this.logoutUrl = logoutUrl;
     }
 
     public String getConfirmUrl() {
