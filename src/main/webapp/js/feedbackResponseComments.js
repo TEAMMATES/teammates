91a7500030cf2a6b186923380b1ff365c4347c8d
junhaoyap <lordyyap@gmail.com>
2015-07-03 17:30:31 +0800

    Removed wild tooltipping function in feedbackResponseComments.js

--- src/main/webapp/js/feedbackResponseComments.js
+++ src/main/webapp/js/feedbackResponseComments.js
@@ -255,12 +255,6 @@ function enableHoverToDisplayEditOptions() {
     });
 }
 
-function enableTooltip() {
-    $(function() { 
-        $("[data-toggle='tooltip']").tooltip({html: true, container: 'body'}); 
-    });
-}
-
 $(document).ready(registerResponseCommentsEvent);
 
 function removeUnwantedVisibilityOptions(commentId) {
@@ -397,7 +391,6 @@ function loadFeedbackResponseComments(user, courseId, fsName, fsIndx, sender) {
             registerResponseCommentsEvent();
             registerCheckboxEventForVisibilityOptions();
             enableHoverToDisplayEditOptions();
-            enableTooltip();
       } else {
           panelBody.find('div[class^="placeholder-error-msg"]').removeClass('hidden');
           panelBody.removeClass('hidden');
