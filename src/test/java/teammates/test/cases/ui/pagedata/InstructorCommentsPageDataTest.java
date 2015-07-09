e7043ba81b466a60f35ef7e6f575895efcc54610
JosephineKwa <work.and.stuff.jo@gmail.com>
2015-07-09 17:45:50 +0800

    add another test to improve test coverage

--- src/test/java/teammates/test/cases/ui/pagedata/InstructorCommentsPageDataTest.java
+++ src/test/java/teammates/test/cases/ui/pagedata/InstructorCommentsPageDataTest.java
@@ -71,11 +71,9 @@ public class InstructorCommentsPageDataTest extends BaseTestCase {
         List<FeedbackSessionAttributes> feedbackSessions = getFeedbackSessionsForCourse(courseId);
         int numberOfPendingComments = 0;
         
-        // Setup first instructor data
+        // Setup instructor comments
         String giverEmail = instructor1.email;
         setInstructorComments(giverEmail, instructor1.email, courseId, comments, commentModifyPermissions);
-        
-        // Setup second instructor data
         giverEmail = instructor2.email;
         setInstructorComments(giverEmail, instructor1.email, courseId, comments, commentModifyPermissions);
         
@@ -101,7 +99,7 @@ public class InstructorCommentsPageDataTest extends BaseTestCase {
         for (String email : expectedGivers) {
             TestHelper.isSameContentIgnoreOrder(expectedComments.get(email), actualComments.get(email));
         }
-        
+        // TODO: not sure if feedbackSessions can be compared like this
         TestHelper.isSameContentIgnoreOrder(feedbackSessions, data.getFeedbackSessions());
         String expectedNextPageLink = data.getInstructorCommentsLink() + "&courseid=" + course2.id;
         String expectedPreviousPageLink = "javascript:;";
@@ -118,26 +116,96 @@ public class InstructorCommentsPageDataTest extends BaseTestCase {
                 data.getCommentsForStudentsTables();
         // TODO: use a different method of comparing 
         TestHelper.isSameContentIgnoreOrder(expectedCommentsForStudentsTables, actualCommentsForStudentsTables);
+        
+        ______TS("instructor is in second course page");
+        
+        courseId = course2.id;
+        courseName = course2.name;
+        
+        comments = new TreeMap<String, List<CommentAttributes>>();
+        commentModifyPermissions = new TreeMap<String, List<Boolean>>();
+        
+        roster = new CourseRoster(getStudentsInCourse(courseId), getInstructorsInCourse(courseId));
+        feedbackSessions = getFeedbackSessionsForCourse(courseId);
+        
+        // Setup instructor comments
+        giverEmail = instructor1.email;
+        setInstructorComments(giverEmail, instructor1.email, courseId, comments, commentModifyPermissions);
+        
+        data.init(isViewingDraft, isDisplayArchive, courseId, courseName, coursePaginationList, 
+                  comments, commentModifyPermissions, roster, feedbackSessions, numberOfPendingComments);
+        
+        expectedNextPageLink = "javascript:;";
+        expectedPreviousPageLink = data.getInstructorCommentsLink() + "&courseid=" + course1.id;
+        assertEquals(data.getNextPageLink(), expectedNextPageLink);
+        assertEquals(data.getPreviousPageLink(), expectedPreviousPageLink);
+        
+        assertEquals(data.getNumberOfPendingComments(), numberOfPendingComments);
+        
+        /******************** Assertions for pageData data ********************/
+        assertFalse(data.isDisplayArchive());
+        assertFalse(data.isViewingDraft());
+        assertEquals(courseId, data.getCourseId());
+        assertEquals(courseName, data.getCourseName());
+        TestHelper.isSameContentIgnoreOrder(coursePaginationList, data.getCoursePaginationList());
+        
+        actualComments = data.getComments();
+        expectedComments = comments;
+        
+        actualGivers = new ArrayList<String>();
+        actualGivers.addAll(actualComments.keySet());
+        expectedGivers = new ArrayList<String>();
+        expectedGivers.addAll(expectedComments.keySet());
+        
+        TestHelper.isSameContentIgnoreOrder(expectedGivers, actualGivers);
+        for (String email : expectedGivers) {
+            TestHelper.isSameContentIgnoreOrder(expectedComments.get(email), actualComments.get(email));
+        }
+        // TODO: not sure if feedbackSessions can be compared like this
+        TestHelper.isSameContentIgnoreOrder(feedbackSessions, data.getFeedbackSessions());
+        expectedNextPageLink = "javascript:;";
+        expectedPreviousPageLink = data.getInstructorCommentsLink() + "&courseid=" + course1.id;
+        assertEquals(data.getNextPageLink(), expectedNextPageLink);
+        assertEquals(data.getPreviousPageLink(), expectedPreviousPageLink);
+        
+        assertEquals(numberOfPendingComments, data.getNumberOfPendingComments());
+        
+        /******************** Assertions for data structures ********************/
+        expectedCommentsForStudentsTables =
+                getCommentsForStudentsTables(courseId, commentModifyPermissions, roster, comments, data);
+        actualCommentsForStudentsTables =
+                data.getCommentsForStudentsTables();
+        // TODO: use a different method of comparing 
+        TestHelper.isSameContentIgnoreOrder(expectedCommentsForStudentsTables, actualCommentsForStudentsTables);
     }
 
-    private void setInstructorComments(
-            String giverEmail, String currentInstructorEmail, String courseId, 
-            Map<String, List<CommentAttributes>> comments,
-            Map<String, List<Boolean>> commentModifyPermissions) {
-        List<CommentAttributes> commentsForGiverList;
-        List<Boolean> canModifyCommentList = new ArrayList<Boolean>();
-        commentsForGiverList = getCommentsForGiverInCourse(giverEmail, courseId);
-        for(int i = 0; i < commentsForGiverList.size(); i++) {
-            canModifyCommentList.add(true);
-        }
-        String key = giverEmail;
-        if (giverEmail.equals(currentInstructorEmail)) {
-            key = InstructorCommentsPageData.COMMENT_GIVER_NAME_THAT_COMES_FIRST;
-        }
-        commentModifyPermissions.put(key, canModifyCommentList);
-        comments.put(key, commentsForGiverList);   
+    private List<CommentRow> createCommentRows(
+            String courseId, String giverEmail, String giverName,
+            Map<String, List<Boolean>> commentModifyPermissions, CourseRoster roster, 
+            Map<String, List<CommentAttributes>> comments, InstructorCommentsPageData data) {
+        
+        List<CommentRow> rows = new ArrayList<CommentRow>();
+        List<CommentAttributes> commentsForGiver = comments.get(giverEmail);
+        for (int i = 0; i < commentsForGiver.size(); i++) {            
+            String recipientDetails = 
+                    getRecipientNames(data, courseId, commentsForGiver.get(i).recipients, roster);
+            String creationTime = 
+                    Const.SystemParams.COMMENTS_SIMPLE_DATE_FORMATTER.format(commentsForGiver.get(i).createdAt);          
+            Boolean isInstructorAllowedToModifyCommentInSection = commentModifyPermissions.get(giverEmail).get(i);
+            String typeOfPeopleCanViewComment = data.getTypeOfPeopleCanViewComment(commentsForGiver.get(i));
+            String editedAt = commentsForGiver.get(i).getEditedAtText(giverName.equals("Anonymous"));
+            String showCommentsTo = data.getShowCommentsToForComment(commentsForGiver.get(i));
+            String showGiverNameTo = data.getShowGiverNameToForComment(commentsForGiver.get(i));
+            String showRecipientNameTo = data.getShowRecipientNameToForComment(commentsForGiver.get(i));
+            VisibilityCheckboxes visibilityCheckboxes = new VisibilityCheckboxes(commentsForGiver.get(i));
+            
+            rows.add(new InstructorCommentsCommentRow(giverEmail, commentsForGiver.get(i), recipientDetails, creationTime, 
+                     isInstructorAllowedToModifyCommentInSection, typeOfPeopleCanViewComment, editedAt,
+                     visibilityCheckboxes, showCommentsTo, showGiverNameTo, showRecipientNameTo));
+        }       
+        return rows;
     }
-    
+
     private List<CommentAttributes> getCommentsForGiverInCourse(String giverEmail, String courseId) {
         List<CommentAttributes> commentsForGiverInCourseList = new ArrayList<CommentAttributes>();
         for (CommentAttributes comment : dataBundle.comments.values()) {
@@ -147,27 +215,26 @@ public class InstructorCommentsPageDataTest extends BaseTestCase {
         }
         return commentsForGiverInCourseList;
     }
-    
-    private List<InstructorAttributes> getInstructorsInCourse(String courseId) {
-        List<InstructorAttributes> instructorsInCourse = new ArrayList<InstructorAttributes>();
-        for (InstructorAttributes instructor : dataBundle.instructors.values()) {
-            if (instructor.courseId.equals(courseId)) {
-                instructorsInCourse.add(instructor);
-            }
+
+    private List<InstructorCommentsForStudentsTable> getCommentsForStudentsTables(
+            String courseId, Map<String, List<Boolean>> commentModifyPermissions, CourseRoster roster, 
+            Map<String, List<CommentAttributes>> comments, InstructorCommentsPageData data) {
+        Map<String, String> giverEmailToGiverNameMap = getGiverEmailToGiverNameMap(comments, roster);
+        List<InstructorCommentsForStudentsTable> commentsForStudentsTables = 
+                new ArrayList<InstructorCommentsForStudentsTable>();      
+          
+        for (String giverEmail : comments.keySet()) {
+            String giverName = giverEmailToGiverNameMap.get(giverEmail);
+            commentsForStudentsTables
+                    .add(new InstructorCommentsForStudentsTable(
+                                 giverEmail, 
+                                 giverName, 
+                                 createCommentRows(courseId, giverEmail, giverName, 
+                                                   commentModifyPermissions, roster, comments, data)));
         }
-        return instructorsInCourse;
+        return commentsForStudentsTables;
     }
-    
-    private List<StudentAttributes> getStudentsInCourse(String courseId) {
-        List<StudentAttributes> studentsInCourse = new ArrayList<StudentAttributes>();
-        for (StudentAttributes student : dataBundle.students.values()) {
-            if (student.course.equals(courseId)) {
-                studentsInCourse.add(student);
-            }
-        }
-        return studentsInCourse;
-    }
-    
+
     private List<FeedbackSessionAttributes> getFeedbackSessionsForCourse(String courseId) {
         List<FeedbackSessionAttributes> feedbackSessionsInCourse = new ArrayList<FeedbackSessionAttributes>();
         for (FeedbackSessionAttributes feedbackSession : dataBundle.feedbackSessions.values()) {
@@ -176,6 +243,36 @@ public class InstructorCommentsPageDataTest extends BaseTestCase {
             }
         }
         return feedbackSessionsInCourse;
+    }
+
+    private Map<String, String> getGiverEmailToGiverNameMap(
+            Map<String, List<CommentAttributes>> comments, CourseRoster roster) {
+    
+        Map<String, String> giverEmailToGiverNameMap = new HashMap<String, String>();
+        for (String giverEmail : comments.keySet()) {
+    
+            InstructorAttributes instructor = roster.getInstructorForEmail(giverEmail);
+            String giverDisplay = giverEmail;
+            if (giverEmail.equals(InstructorCommentsPageData.COMMENT_GIVER_NAME_THAT_COMES_FIRST)) {
+                giverDisplay = "You";
+            } else if (instructor != null) {
+                String title = instructor.displayedName;
+                giverDisplay = title + " " + instructor.name;
+            }
+    
+            giverEmailToGiverNameMap.put(giverEmail, giverDisplay);
+        }
+        return giverEmailToGiverNameMap;
+    }
+
+    private List<InstructorAttributes> getInstructorsInCourse(String courseId) {
+        List<InstructorAttributes> instructorsInCourse = new ArrayList<InstructorAttributes>();
+        for (InstructorAttributes instructor : dataBundle.instructors.values()) {
+            if (instructor.courseId.equals(courseId)) {
+                instructorsInCourse.add(instructor);
+            }
+        }
+        return instructorsInCourse;
     }
     
     private String getRecipientNames(PageData data, String courseId, Set<String> recipients, CourseRoster roster) {
@@ -202,70 +299,32 @@ public class InstructorCommentsPageDataTest extends BaseTestCase {
         String namesString = namesStringBuilder.toString();
         return data.removeEndComma(namesString);
     }
-    
-    private Map<String, String> getGiverEmailToGiverNameMap(
-            Map<String, List<CommentAttributes>> comments, CourseRoster roster) {
 
-        Map<String, String> giverEmailToGiverNameMap = new HashMap<String, String>();
-        for (String giverEmail : comments.keySet()) {
-
-            InstructorAttributes instructor = roster.getInstructorForEmail(giverEmail);
-            String giverDisplay = giverEmail;
-            if (giverEmail.equals(InstructorCommentsPageData.COMMENT_GIVER_NAME_THAT_COMES_FIRST)) {
-                giverDisplay = "You";
-            } else if (instructor != null) {
-                String title = instructor.displayedName;
-                giverDisplay = title + " " + instructor.name;
+    private List<StudentAttributes> getStudentsInCourse(String courseId) {
+        List<StudentAttributes> studentsInCourse = new ArrayList<StudentAttributes>();
+        for (StudentAttributes student : dataBundle.students.values()) {
+            if (student.course.equals(courseId)) {
+                studentsInCourse.add(student);
             }
-
-            giverEmailToGiverNameMap.put(giverEmail, giverDisplay);
         }
-        return giverEmailToGiverNameMap;
+        return studentsInCourse;
     }
     
-    private List<InstructorCommentsForStudentsTable> getCommentsForStudentsTables(
-            String courseId, Map<String, List<Boolean>> commentModifyPermissions, CourseRoster roster, 
-            Map<String, List<CommentAttributes>> comments, InstructorCommentsPageData data) {
-        Map<String, String> giverEmailToGiverNameMap = getGiverEmailToGiverNameMap(comments, roster);
-        List<InstructorCommentsForStudentsTable> commentsForStudentsTables = 
-                new ArrayList<InstructorCommentsForStudentsTable>();      
-          
-        for (String giverEmail : comments.keySet()) {
-            String giverName = giverEmailToGiverNameMap.get(giverEmail);
-            commentsForStudentsTables
-                    .add(new InstructorCommentsForStudentsTable(
-                                 giverEmail, 
-                                 giverName, 
-                                 createCommentRows(courseId, giverEmail, giverName, 
-                                                   commentModifyPermissions, roster, comments, data)));
+    private void setInstructorComments(
+            String giverEmail, String currentInstructorEmail, String courseId, 
+            Map<String, List<CommentAttributes>> comments,
+            Map<String, List<Boolean>> commentModifyPermissions) {
+        List<CommentAttributes> commentsForGiverList;
+        List<Boolean> canModifyCommentList = new ArrayList<Boolean>();
+        commentsForGiverList = getCommentsForGiverInCourse(giverEmail, courseId);
+        for(int i = 0; i < commentsForGiverList.size(); i++) {
+            canModifyCommentList.add(true);
         }
-        return commentsForStudentsTables;
-    }
-    
-    private List<CommentRow> createCommentRows(
-            String courseId, String giverEmail, String giverName,
-            Map<String, List<Boolean>> commentModifyPermissions, CourseRoster roster, 
-            Map<String, List<CommentAttributes>> comments, InstructorCommentsPageData data) {
-        
-        List<CommentRow> rows = new ArrayList<CommentRow>();
-        List<CommentAttributes> commentsForGiver = comments.get(giverEmail);
-        for (int i = 0; i < commentsForGiver.size(); i++) {            
-            String recipientDetails = 
-                    getRecipientNames(data, courseId, commentsForGiver.get(i).recipients, roster);
-            String creationTime = 
-                    Const.SystemParams.COMMENTS_SIMPLE_DATE_FORMATTER.format(commentsForGiver.get(i).createdAt);          
-            Boolean isInstructorAllowedToModifyCommentInSection = commentModifyPermissions.get(giverEmail).get(i);
-            String typeOfPeopleCanViewComment = data.getTypeOfPeopleCanViewComment(commentsForGiver.get(i));
-            String editedAt = commentsForGiver.get(i).getEditedAtText(giverName.equals("Anonymous"));
-            String showCommentsTo = data.getShowCommentsToForComment(commentsForGiver.get(i));
-            String showGiverNameTo = data.getShowGiverNameToForComment(commentsForGiver.get(i));
-            String showRecipientNameTo = data.getShowRecipientNameToForComment(commentsForGiver.get(i));
-            VisibilityCheckboxes visibilityCheckboxes = new VisibilityCheckboxes(commentsForGiver.get(i));
-            
-            rows.add(new InstructorCommentsCommentRow(giverEmail, commentsForGiver.get(i), recipientDetails, creationTime, 
-                     isInstructorAllowedToModifyCommentInSection, typeOfPeopleCanViewComment, editedAt,
-                     visibilityCheckboxes, showCommentsTo, showGiverNameTo, showRecipientNameTo));
-        }       
-        return rows;
+        String key = giverEmail;
+        if (giverEmail.equals(currentInstructorEmail)) {
+            key = InstructorCommentsPageData.COMMENT_GIVER_NAME_THAT_COMES_FIRST;
+        }
+        commentModifyPermissions.put(key, canModifyCommentList);
+        comments.put(key, commentsForGiverList);   
     }
 }
