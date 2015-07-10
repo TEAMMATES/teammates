1d44d09ee0b8672068ffc94ffc77de963986cd3c
junhaoyap <lordyyap@gmail.com>
2015-07-10 12:14:46 +0800

    Added correct filepath for helper file

--- src/main/java/teammates/common/util/LocalLibrariesFilePathHelper.java
+++ src/main/java/teammates/common/util/LocalLibrariesFilePathHelper.java
@@ -8,8 +8,8 @@ public class LocalLibrariesFilePathHelper {
         boolean isDevEnvironment = Boolean.parseBoolean(System.getProperty("isDevEnvironment"));
         
         if (isDevEnvironment) {
-            jQueryFilePath = "somelocalfilepath";
-            jQueryUiFilePath = "somelocalfilepath";
+            jQueryFilePath = "<script type=\"text/javascript\" src=\"/js/lib/jquery-1.11.3.min.js\"></script>";
+            jQueryUiFilePath = "<script type=\"text/javascript\" src=\"/js/lib/jquery-ui-1.11.4.min.js\"></script>";
         } else {
             jQueryFilePath = "<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>";
             jQueryUiFilePath = "<script type=\"text/javascript\" src=\"//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js\"></script>";
