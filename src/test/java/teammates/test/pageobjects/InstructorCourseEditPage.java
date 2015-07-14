65bb03550a220a0e6249d11806ca84f6c8fbf420
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2015-07-14 12:31:32 +0900

    add more test

--- src/test/java/teammates/test/pageobjects/InstructorCourseEditPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorCourseEditPage.java
@@ -159,12 +159,12 @@ public class InstructorCourseEditPage extends AppPage {
     
     public void clickViewDetailsLinkForInstructor(int instrNum, int viewLinkNum) {
         // there is one link before view details link group
-        int cssLinkNum = viewLinkNum + 1;
+        int cssLinkNum = viewLinkNum * 3 - 1;
         WebElement viewLink = browser.driver.findElement(By.cssSelector("#accessControlEditDivForInstr" + instrNum +
-                " > div:nth-child(1) > div.col-sm-9 > a:nth-child(" + cssLinkNum + ")"));
+                " > div.form-group > div.col-sm-9 > a:nth-child(" + cssLinkNum + ")"));
         
         viewLink.click();
-        browser.selenium.waitForPageToLoad("500");
+        waitForPageToLoad();
     }
     
     public void closeModal() {
@@ -175,6 +175,20 @@ public class InstructorCourseEditPage extends AppPage {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
         }
+    }
+    
+    public boolean isPrivilegeCheckboxInModalChecked(String privilege) {
+        By selector = By.cssSelector("#tunePermissionsDivForInstructorAll input[type='checkbox'][name='" 
+                                     + privilege + "']");
+        WebElement checkbox = browser.driver.findElement(selector);
+        return checkbox.isSelected();
+    }
+    
+    public boolean isPrivilegeCheckboxInPermissionDivChecked(int instructorIndex, String privilege) {
+        By selector = By.cssSelector("#tunePermissionsDivForInstructor" + instructorIndex 
+                                     + " input[type='checkbox'][name='" + privilege + "']");
+        WebElement checkbox = browser.driver.findElement(selector);
+        return checkbox.isSelected();
     }
     
     public WebElement courseLevelPanel(int instrNum) {
