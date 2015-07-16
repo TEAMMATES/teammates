07daf7ed63fd9a1252591f42a295b06de7a7713a
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-07-16 12:39:57 +0800

    Fixed ElementNotVisibleException for copy to modal in InstructorHomePageUiTest

--- src/test/java/teammates/test/pageobjects/InstructorHomePage.java
+++ src/test/java/teammates/test/pageobjects/InstructorHomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import teammates.common.util.Const;
 import teammates.common.util.ThreadHelper;
@@ -335,6 +336,7 @@ public class InstructorHomePage extends AppPage {
     }
     
     public void clickFsCopySubmitButton() {
+        new WebDriverWait(browser.driver, 300);
         WebElement fsCopySubmitButton = browser.driver.findElement(By.id("fscopy_submit"));
         
         fsCopySubmitButton.click();
