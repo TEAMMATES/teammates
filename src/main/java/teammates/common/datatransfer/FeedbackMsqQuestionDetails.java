0ff94b244d7840e5252a6dcccfca133fdcb7d70a
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-07-20 16:45:34 +0800

    Added other enabled condition for isChangesRequiresResponseDeletion()

--- src/main/java/teammates/common/datatransfer/FeedbackMsqQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/FeedbackMsqQuestionDetails.java
@@ -116,6 +116,10 @@ public class FeedbackMsqQuestionDetails extends FeedbackQuestionDetails {
             return true;
         }
         
+        if (this.otherEnabled != newMsqDetails.otherEnabled) {
+            return true;
+        }
+        
         return false;
     }
 
