4758d2b1b5a5fa278b303a1c2654145f5c0a15f2
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-07-20 16:31:44 +0800

    Called updateMsqOtherOptionField()

--- src/main/webapp/js/feedbackSubmissionsEdit.js
+++ src/main/webapp/js/feedbackSubmissionsEdit.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
         validationStatus &= validateAllAnswersHaveRecipient();
         
         updateMcqOtherOptionField();
+        updateMsqOtherOptionField();
         
         if (!validationStatus) {
             return false;
