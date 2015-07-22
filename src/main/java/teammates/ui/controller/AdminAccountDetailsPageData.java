fd143ef6e2d9aeb11b34ed92e1591234e109d265
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2015-07-22 12:47:12 +0800

    added structure data for the Page

--- src/main/java/teammates/ui/controller/AdminAccountDetailsPageData.java
+++ src/main/java/teammates/ui/controller/AdminAccountDetailsPageData.java
@@ -1,5 +1,6 @@
 package teammates.ui.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import teammates.common.datatransfer.AccountAttributes;
@@ -7,25 +8,64 @@ import teammates.common.datatransfer.CourseAttributes;
 import teammates.common.datatransfer.CourseDetailsBundle;
 import teammates.common.util.Const;
 import teammates.common.util.Url;
+import teammates.ui.template.AdminAccountDetailsInstructorCourseListTableRow;
+import teammates.ui.template.AdminAccountDetailsStudentCourseListTableRow;
 
 public class AdminAccountDetailsPageData extends PageData {
     
-    public AccountAttributes accountInformation;
-    public List<CourseDetailsBundle> instructorCourseList;
-    public List<CourseAttributes> studentCourseList;
-
-    public AdminAccountDetailsPageData(AccountAttributes account) {
+    private AccountAttributes accountInformation;
+    private List<AdminAccountDetailsInstructorCourseListTableRow> instructorCourseListTable;
+    private List<AdminAccountDetailsStudentCourseListTableRow> studentCourseListTable;
+    
+    public AdminAccountDetailsPageData(AccountAttributes account, AccountAttributes accountInformation, 
+                                       List<CourseDetailsBundle> instructorCourseList, 
+                                       List<CourseAttributes> studentCourseList) {
         super(account);
+        this.accountInformation = accountInformation;
+        this.instructorCourseListTable = createInstructorCourseListTable(instructorCourseList);
+        this.studentCourseListTable = createStudentCourseListTable(studentCourseList);
     }
     
-    public String getAdminDeleteInstructorFromCourseLink(String instructorId, String courseId){
+    private List<AdminAccountDetailsStudentCourseListTableRow> createStudentCourseListTable(
+                                    List<CourseAttributes> studentCourseList) {
+        List<AdminAccountDetailsStudentCourseListTableRow> courseListTable = new ArrayList<AdminAccountDetailsStudentCourseListTableRow>();
+        for(CourseAttributes courseDetails : studentCourseList) {
+            AdminAccountDetailsStudentCourseListTableRow row = new AdminAccountDetailsStudentCourseListTableRow(accountInformation.googleId, courseDetails);
+            courseListTable.add(row);
+        }
+        return courseListTable;
+    }
+
+    private List<AdminAccountDetailsInstructorCourseListTableRow> createInstructorCourseListTable(
+                                                            List<CourseDetailsBundle> instructorCourseList) {
+        List<AdminAccountDetailsInstructorCourseListTableRow> courseListTable = new ArrayList<AdminAccountDetailsInstructorCourseListTableRow>();
+        for(CourseDetailsBundle courseDetails : instructorCourseList) {
+            AdminAccountDetailsInstructorCourseListTableRow row = new AdminAccountDetailsInstructorCourseListTableRow(accountInformation.googleId, courseDetails);
+            courseListTable.add(row);
+        }
+        return courseListTable;
+    }
+
+    public AccountAttributes getAccountInformation() {
+        return accountInformation;
+    }
+    
+    public List<AdminAccountDetailsInstructorCourseListTableRow> getInstructorCourseListTable() {
+        return instructorCourseListTable;
+    }
+    
+    public List<AdminAccountDetailsStudentCourseListTableRow> getStudentCourseListTable() {
+        return studentCourseListTable;
+    }
+    
+    public static String getAdminDeleteInstructorFromCourseLink(String instructorId, String courseId){
         String link = Const.ActionURIs.ADMIN_ACCOUNT_DELETE;
         link = Url.addParamToUrl(link,Const.ParamsNames.INSTRUCTOR_ID,instructorId);
         link = Url.addParamToUrl(link,Const.ParamsNames.COURSE_ID,courseId);
         return link;
     }
     
-    public String getAdminDeleteStudentFromCourseLink(String studentId, String courseId){
+    public static String getAdminDeleteStudentFromCourseLink(String studentId, String courseId){
         String link = Const.ActionURIs.ADMIN_ACCOUNT_DELETE;
         link = Url.addParamToUrl(link,Const.ParamsNames.STUDENT_ID,studentId);
         link = Url.addParamToUrl(link,Const.ParamsNames.COURSE_ID,courseId);
