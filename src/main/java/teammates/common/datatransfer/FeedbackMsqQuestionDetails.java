3a157f341116c07263a4db413052855bdb0fea9d
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-07-22 17:53:22 +0800

    Removed unused import

--- src/main/java/teammates/common/datatransfer/FeedbackMsqQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/FeedbackMsqQuestionDetails.java
@@ -2,7 +2,6 @@ package teammates.common.datatransfer;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
