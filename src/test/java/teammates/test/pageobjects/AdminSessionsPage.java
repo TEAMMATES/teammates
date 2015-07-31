290082aca0c462968aebf7844d019a1d977d0b9b
JosephineKwa <work.and.stuff.jo@gmail.com>
2015-07-31 16:24:03 +0800

    create empty page object

---
+++ src/test/java/teammates/test/pageobjects/AdminSessionsPage.java
@@ -0,0 +1,16 @@
+package teammates.test.pageobjects;
+
+public class AdminSessionsPage extends AppPage {
+
+    public AdminSessionsPage(Browser browser) {
+        super(browser);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected boolean containsExpectedPageContents() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
