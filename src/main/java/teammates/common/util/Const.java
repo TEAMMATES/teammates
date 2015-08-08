2f2751599cc0958e9ffcc303a03428bc9677475a
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-08-09 00:22:16 +0800

    Added request parameter STATUS_MESSAGE_COLOR

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -463,6 +463,7 @@ public class Const {
         public static final String TEAMMATES = "teammates";
     
         public static final String STATUS_MESSAGE = "message";
+        public static final String STATUS_MESSAGE_COLOR = "statusmessagecolor";
         public static final String ERROR = "error";
         public static final String NEXT_URL = "next";
         public static final String USER_ID = "user";
