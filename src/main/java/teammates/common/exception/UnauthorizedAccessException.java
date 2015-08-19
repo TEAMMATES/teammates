85a7d1e849c7f25fc8a45f6339e6dc07730aa09c
Low WeiLin <lwl1991@gmail.com>
2015-08-19 20:33:49 +0800

    Remove unnecessary changes

--- src/main/java/teammates/common/exception/UnauthorizedAccessException.java
+++ src/main/java/teammates/common/exception/UnauthorizedAccessException.java
@@ -2,12 +2,11 @@ package teammates.common.exception;
 
 @SuppressWarnings("serial")
 public class UnauthorizedAccessException extends RuntimeException {
-    
     public UnauthorizedAccessException(){
+        super();
     }
 
     public UnauthorizedAccessException(String message) {
         super(message);
     }
-    
 }
