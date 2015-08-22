6beb6993b79a683ccb234f524097df337c69fc8d
yj-soh <soh.you.jun@gmail.com>
2015-08-22 15:13:13 +0800

    Refactor instructorHome.js according to standards.

--- src/main/webapp/js/instructorHome.js
+++ src/main/webapp/js/instructorHome.js
@@ -2,24 +2,24 @@ $(document).ready(function() {
     setupFsCopyModal();
     
     //Click event binding for radio buttons
-    var radiobuttons = $("label[name='sortby']");
-    $.each(radiobuttons, function() {
+    var radioButtons = $('label[name="sortby"]');
+    $.each(radioButtons, function() {
         $(this).click(function () {
             var currentPath = window.location.pathname;
             var query = window.location.search.substring(1);
             var params = {};
             
-            var param_values = query.split("&");
-            for (var i = 0; i < param_values.length; i++) {
-                var param_value = param_values[i].split("=");
-                params[param_value[0]] = param_value[1];
+            var paramValues = query.split('&');
+            for (var i = 0; i < paramValues.length; i++) {
+                var paramValue = paramValues[i].split('=');
+                params[paramValue[0]] = paramValue[1];
             }
 
-            if ("user" in params == false) {
-                params["user"] = $("input[name='user']").val();
+            if ('user' in params == false) {
+                params['user'] = $('input[name="user"]').val();
             }
 
-            window.location.href = currentPath + "?user=" + params["user"] + "&sortby=" + $(this).attr("data");
+            window.location.href = currentPath + '?user=' + params['user'] + '&sortby=' + $(this).attr('data');
         });
     });
 });
