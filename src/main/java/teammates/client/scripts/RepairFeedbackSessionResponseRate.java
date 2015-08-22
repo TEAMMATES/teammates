6ab269d18302e62d049d15c843b993aefd1a76a3
Kang Hong Jin <kanghongjin@gmail.com>
2015-08-22 22:10:38 +0800

    Add additional method to retrieve feedback sessions within the specified date range

--- src/main/java/teammates/client/scripts/RepairFeedbackSessionResponseRate.java
+++ src/main/java/teammates/client/scripts/RepairFeedbackSessionResponseRate.java
@@ -2,11 +2,16 @@ package teammates.client.scripts;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 
 import teammates.client.remoteapi.RemoteApiClient;
 import teammates.common.datatransfer.EntityAttributes;
@@ -18,12 +23,18 @@ import teammates.common.exception.InvalidParametersException;
 import teammates.logic.api.Logic;
 import teammates.storage.api.FeedbackSessionsDb;
 import teammates.storage.datastore.Datastore;
+import teammates.storage.entity.FeedbackSession;
 
 /**
- * Script to go through either every feedback session, or a specified feedback session,
- * and verifies that the non-respondents do not have a response in the feedback session
+ * Script to go through either:
+ * <ul>
+ * <li> every feedback session, </li> 
+ * <li> feedback sessions with start date within a specified range, </li> 
+ * <li> or a specified feedback session, </li>
+ * </ul>
+ * and verifies that the non-respondents do not have a response in the feedback session. <br/>
  * 
- * If isPreview is false, whenever an inconsistency is found, logic.updateRespondants will
+ * If isPreview is false, whenever an inconsistency is found, {@code logic.updateRespondants} will
  * be used to recompute the respondents' set.
  * 
  */
@@ -35,14 +46,22 @@ public class RepairFeedbackSessionResponseRate extends RemoteApiClient {
     // if isPreview is true, then no writes will be done 
     private boolean isPreview = true;
     
-    // if either of courseId or feedbackSessionName is null,
+    
+    // if numDays is set to > 0,
+    // then feedback sessions with start date from (now - numDays) days to the current time
+    // will be retrieved and checked
+    private int numDays = 180;
+    
+    // If numDays is not set, and
+    // and both courseId and feedbackSessionName is specified,
+    // the feedback session specified will be operated upon.
+    // If either of courseId or feedbackSessionName is null,
     // then all feedback sessions will be checked
-    // otherwise the feedback session specified will be operated upon.
     private String courseId = null;
     private String feedbackSessionName = null;
     
-    private Map<String, Set<String>> emailsInCourse = new HashMap<>();
     
+    private Map<String, Set<String>> emailsInCourse = new HashMap<>();
     
     public static void main(String[] args) throws IOException {
         RepairFeedbackSessionResponseRate migrator = new RepairFeedbackSessionResponseRate();
@@ -54,7 +73,9 @@ public class RepairFeedbackSessionResponseRate extends RemoteApiClient {
         Datastore.initialize();
         
         List<FeedbackSessionAttributes> feedbackSessions;
-        if (courseId == null || feedbackSessionName == null) {
+        if (numDays > 0) {
+            feedbackSessions = getFeedbackSessionsWithStartDateNoOlderThan(numDays);
+        } else if (courseId == null || feedbackSessionName == null) {
             feedbackSessions = getAllFeedbackSessions();
         } else {
             feedbackSessions = new ArrayList<FeedbackSessionAttributes>();
@@ -142,5 +163,45 @@ public class RepairFeedbackSessionResponseRate extends RemoteApiClient {
     @SuppressWarnings("deprecation")
     private List<FeedbackSessionAttributes> getAllFeedbackSessions() {
         return fsDb.getAllFeedbackSessions();
+    }
+    
+    /**
+     * Return a list of feedback sessions with start time from (now - numDays) and now 
+     */
+    public List<FeedbackSessionAttributes> getFeedbackSessionsWithStartDateNoOlderThan(int numDays) {
+        Calendar startCal = Calendar.getInstance();
+        startCal.add(Calendar.DAY_OF_YEAR, -1 * numDays);
+        
+        Date now = Calendar.getInstance().getTime();
+        
+        return getFeedbackSessionsWithStartTimeWithinRange(startCal.getTime(), now);
+    }
+    
+    private List<FeedbackSessionAttributes> getFeedbackSessionsWithStartTimeWithinRange(
+                                                    Date startDate, Date endDate) {
+        List<FeedbackSessionAttributes> result = new ArrayList<>();
+        
+        PersistenceManager pm = Datastore.getPersistenceManager();
+        Query query = pm.newQuery("SELECT FROM teammates.storage.entity.FeedbackSession "
+                                + "WHERE this.startTime >= rangeStart && this.startTime < rangeEnd "
+                                + "PARAMETERS java.util.Date rangeStart, "
+                                + "java.util.Date rangeEnd");
+        
+        Calendar startCal = Calendar.getInstance();
+        startCal.setTime(startDate);
+        Calendar endCal = Calendar.getInstance();
+        endCal.setTime(endDate);
+        
+        Date curStart = startCal.getTime();
+        Date curEnd = endCal.getTime();
+        
+        @SuppressWarnings("unchecked")
+        List<FeedbackSession> feedbackSessions = (List<FeedbackSession>)query.execute(curStart, curEnd);
+        for (FeedbackSession feedbackSession : feedbackSessions) {
+            FeedbackSessionAttributes feedbackSessionAttributes = new FeedbackSessionAttributes(feedbackSession);
+            result.add(feedbackSessionAttributes);
+        }
+        
+        return result;
     }
 }
