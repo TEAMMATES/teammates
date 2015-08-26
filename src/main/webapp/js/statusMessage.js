64fffbe52426681094962dd7b39439b5302e66b6
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-08-26 14:12:33 +0800

    Status message is not hidden behind nav bar

--- src/main/webapp/js/statusMessage.js
+++ src/main/webapp/js/statusMessage.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	document.getElementById('statusMessage').scrollIntoView();
+    var statusMessage = document.getElementById('statusMessage');
+    var navbarHeight = '70';
+
+    // scroll to element
+    statusMessage.scrollIntoView(true);
+
+    // now account for fixed header
+    var scrolledY = window.scrollY;
+
+    if (scrolledY) {
+      window.scroll(0, scrolledY - navbarHeight);
+    }
 });
