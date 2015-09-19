65f0caf0aabf60208ac901429332761d7fdf73c8
Thyagesh Manikandan <thyageshm@gmail.com>
2015-09-20 01:13:02 +0800

    Added necessary conditions in Godmode to handle actual web urls of the app.

--- src/test/java/teammates/test/pageobjects/AppPage.java
+++ src/test/java/teammates/test/pageobjects/AppPage.java
@@ -749,6 +749,9 @@ public abstract class AppPage {
         return content
                 .replaceAll("<#comment[ ]*</#comment>", "<!---->")
                 .replace(Config.APP_URL, "${app.url}")
+                .replace("http://localhost:8888", "${web.url}")
+                .replace(Config.APP_URL.replace("http", "https"), "${web.url}")
+                .replace("\"/_ah", "\"${web.url}/_ah")
                 .replaceAll("V[0-9]\\.[0-9]+", "V\\${version}")
                 // photo from instructor
                 .replaceAll(Const.ActionURIs.STUDENT_PROFILE_PICTURE + "\\?" + Const.ParamsNames.STUDENT_EMAIL + "=([a-zA-Z0-9]){1,}\\&amp;"
