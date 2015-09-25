8ef9a77f2c22c8687fc1888b90161c5b427f6c22
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-09-25 18:02:14 +0800

    changed the regex for regkey (and questionid since both are the same)

--- src/test/java/teammates/test/pageobjects/AppPage.java
+++ src/test/java/teammates/test/pageobjects/AppPage.java
@@ -786,7 +786,8 @@ public abstract class AppPage {
                 //regkey
                 .replaceAll(Const.ParamsNames.REGKEY + "=([a-zA-Z0-9]){1,}\\&", Const.ParamsNames.REGKEY + "={*}\\&")
                 .replaceAll(Const.ParamsNames.REGKEY + "%3D([a-zA-Z0-9]){1,}\\%", Const.ParamsNames.REGKEY + "%3D{*}\\%")
-                .replaceAll("\"([a-zA-Z0-9-_]){50,}\"","\"{*}\"")
+                //regkey and questionid (same regex for both)
+                .replaceAll("value=\"([a-zA-Z0-9-_]){50,}\"","value=\"{*}\"")
                 //responseid
                 .replaceAll("([a-zA-Z0-9-_]){30,}%"
                         + "[\\w+-][\\w+!#$%&'*/=?^_`{}~-]*+(\\.[\\w+!#$%&'*/=?^_`{}~-]+)*+@([A-Za-z0-9-]+\\.)*[A-Za-z]+%"
