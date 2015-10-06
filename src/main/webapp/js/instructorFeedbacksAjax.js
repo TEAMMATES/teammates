48309263e36f0986a4d30b348bb028437a8e52fb
yj-soh <soh.you.jun@gmail.com>
2015-10-07 01:25:30 +0800

    Extract ajaxRequest() outside of document ready.

--- src/main/webapp/js/instructorFeedbacksAjax.js
+++ src/main/webapp/js/instructorFeedbacksAjax.js
@@ -1,39 +1,41 @@
 var isSessionsAjaxSending = false;
-$(document).ready(function(){
-    var ajaxRequest = function(e) {
-        e.preventDefault();
-        
-        if (isSessionsAjaxSending) {
-            return;
-        }
-        
-        var formData = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            cache: false,
-            url: $(this).attr('action') + '?' + formData,
-            beforeSend: function() {
-                isSessionsAjaxSending = true;
-                $('#sessionList').html('<img height="75" width="75" class="margin-center-horizontal" src="/images/ajax-preload.gif"/>');
-            },
-            error: function() {
-                isSessionsAjaxSending = false;
-                $('#sessionList').html('');
-                var msg = 'Failed to load sessions. Please <a href="#" onclick="loadSessionsByAjax()">click here</a> to retry.';
-                setStatusMessage(msg, true);
-            },
-            success: function(data) {
-                clearStatusMessage();
-                var appendedModalBody = $(data).find('#copySessionsBody').html();
-                var appendedSessionTable = $(data).find('#sessionList').html();
 
-                $('#button_copy').text('Copy from previous feedback sessions');
-                $('#copySessionsBody').html(appendedModalBody);
-                $('#sessionList').removeClass('align-center')
-                                 .html(appendedSessionTable);
-                bindEventsAfterAjax();
-            }
-        });
-    };
+var ajaxRequest = function(e) {
+    e.preventDefault();
+    
+    if (isSessionsAjaxSending) {
+        return;
+    }
+    
+    var formData = $(this).serialize();
+    $.ajax({
+        type: 'POST',
+        cache: false,
+        url: $(this).attr('action') + '?' + formData,
+        beforeSend: function() {
+            isSessionsAjaxSending = true;
+            $('#sessionList').html('<img height="75" width="75" class="margin-center-horizontal" src="/images/ajax-preload.gif"/>');
+        },
+        error: function() {
+            isSessionsAjaxSending = false;
+            $('#sessionList').html('');
+            var msg = 'Failed to load sessions. Please <a href="#" onclick="loadSessionsByAjax()">click here</a> to retry.';
+            setStatusMessage(msg, true);
+        },
+        success: function(data) {
+            clearStatusMessage();
+            var appendedModalBody = $(data).find('#copySessionsBody').html();
+            var appendedSessionTable = $(data).find('#sessionList').html();
+
+            $('#button_copy').text('Copy from previous feedback sessions');
+            $('#copySessionsBody').html(appendedModalBody);
+            $('#sessionList').removeClass('align-center')
+                             .html(appendedSessionTable);
+            bindEventsAfterAjax();
+        }
+    });
+};
+
+$(document).ready(function(){
     $('#ajaxForSessions').submit(ajaxRequest);
 });
