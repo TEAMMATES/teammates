08d0951db1343959571ca381858ba9aaf8562410
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-10-12 10:52:20 +0800

    Added methods to test email compose, sent, draft and trash pages

--- src/test/java/teammates/test/cases/ui/browsertests/AdminEmailPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/AdminEmailPageUiTest.java
@@ -44,12 +44,12 @@ public class AdminEmailPageUiTest extends BaseUiTestCase {
         
         emailPage = loginAdminToPageForAdminUiTests(
                         browser, createUrl(Const.ActionURIs.ADMIN_EMAIL_COMPOSE_PAGE), AdminEmailPage.class);
-        emailPage.verifyHtml("/adminEmailCompose.html");
+        assertTrue(isEmailComposePageCorrect());
         
         ______TS("send email - no recipient");
         
         emailPage.clickSendButton();
-        emailPage.verifyHtmlMainContent("/adminEmailNoRecipient.html");
+        assertTrue(hasStatusMessageNoRecipient());
         
         ______TS("send email - recipient email format error");
         
@@ -57,20 +57,21 @@ public class AdminEmailPageUiTest extends BaseUiTestCase {
         emailPage.inputSubject("Email Subject");
         emailPage.inputContent("Email Content");
         emailPage.clickSendButton();
-        emailPage.verifyHtmlMainContent("/adminEmailRecipientEmailFormatError.html");
+        assertTrue(hasStatusMessageRecipientEmailFormatError());
         
         ______TS("send email - no subject");
         
         emailPage.inputRecipient("recipient@email.tmt");
         emailPage.clearSubjectBox();
         emailPage.clickSendButton();
-        emailPage.verifyHtmlMainContent("/adminEmailNoSubject.html");
+        assertTrue(hasStatusMessageNoSubject());
         
         ______TS("send email - success");
         
         emailPage.inputSubject("Email Subject");
         emailPage.clickSendButton();
-        emailPage.verifyHtmlMainContent("/adminEmailSendSuccess.html");
+        assertTrue(hasNoErrorMessage());
+        assertTrue(isEmailComposePageCorrect());
         
         ______TS("save email - success");
         
@@ -78,22 +79,169 @@ public class AdminEmailPageUiTest extends BaseUiTestCase {
         emailPage.inputSubject("Email Subject");
         emailPage.inputContent("Email to save");
         emailPage.clickSaveButton();
-        emailPage.verifyHtmlMainContent("/adminEmailSaveSuccess.html");
+        assertTrue(hasStatusMessageSaveSuccess());
     }
 
     private void testSent() {
         emailPage.clickSentTab();
-        emailPage.verifyHtmlMainContent("/adminEmailSentPage.html");
+        assertTrue(isEmailSentDataDisplayCorrect());
     }
 
     private void testDraft() {
         emailPage.clickDraftTab();
-        emailPage.verifyHtmlMainContent("/adminEmailDraftPage.html");
+        assertTrue(isEmailDraftDataDisplayCorrect());
     }
 
     private void testTrash() {
         emailPage.clickTrashTab();
-        emailPage.verifyHtmlMainContent("/adminEmailTrashPage.html");
+        assertTrue(isEmailTrashDataDisplayCorrect());
+    }
+    
+    private boolean isEmailComposePageCorrect() {
+        return emailPage.isElementPresent(By.id("addressReceiverEmails"))
+            && emailPage.isElementPresent(By.name("emailsubject"))
+            && emailPage.isElementPresent(By.className("mce-tinymce"))
+            && emailPage.isElementPresent(By.id("adminEmailBox"))
+            && emailPage.isElementPresent(By.id("composeSubmitButton"))
+            && emailPage.isElementPresent(By.id("composeSaveButton"));
+    }
+    
+    private boolean hasStatusMessageNoRecipient() {
+        return emailPage.getStatus().equals("Error : No reciver address or file given");
+    }
+    
+    private boolean hasStatusMessageRecipientEmailFormatError() {
+        return emailPage.getStatus().contains("\"recipient\" is not acceptable to TEAMMATES as an email "
+                                            + "because it is not in the correct format. An email address "
+                                            + "contains some text followed by one '@' sign followed by "
+                                            + "some more text. It cannot be longer than 45 characters. "
+                                            + "It cannot be empty and it cannot have spaces.");
+    }
+    
+    private boolean hasStatusMessageNoSubject() {
+        return emailPage.getStatus().equals("\"\" is not acceptable to TEAMMATES as email subject because "
+                                          + "it is empty. The value of email subject should be no longer "
+                                          + "than 200 characters. It should not be empty.");
+    }
+    
+    private boolean hasNoErrorMessage() {
+        return !(emailPage.isElementPresent(By.className("alert-danger")));
+    }
+    
+    private boolean hasStatusMessageSaveSuccess() {
+        return emailPage.getStatus().equals("Email draft has been saved");
+    }
+    
+    /**
+     * This method only checks if the email sent data table is displayed correctly
+     * i.e, table headers are correct
+     * It does not test for the table content
+     */
+    private boolean isEmailSentDataDisplayCorrect() {
+        if (emailPage.isElementPresent(By.className("table"))) {
+            return isEmailSentTableHeaderCorrect();
+        } else {     
+            return false;
+        }
+    }
+
+    private boolean isEmailSentTableHeaderCorrect() {
+        int numColumns = emailPage.getNumberOfColumnsFromDataTable(0); // 1 table
+        
+        if (numColumns != ADMIN_EMAIL_SENT_TABLE_NUM_COLUMNS) {
+            return false;
+        }
+        
+        List<String> expectedSessionTableHeaders = Arrays.asList("Action",
+                                                                 "Address Receiver",
+                                                                 "Group Receiver",
+                                                                 "Subject",
+                                                                 "Date");
+        List<String> actualSessionTableHeaders = new ArrayList<String>();
+        
+        for (int i = 0 ; i < numColumns ; i++) {
+            actualSessionTableHeaders.add(emailPage.getHeaderValueFromDataTable(0, 0, i));
+        }
+        
+        return actualSessionTableHeaders.equals(expectedSessionTableHeaders);
+    }
+    
+    /**
+     * This method only checks if the email draft data table is displayed correctly
+     * i.e, table headers are correct
+     * It does not test for the table content
+     */
+    private boolean isEmailDraftDataDisplayCorrect() {
+        if (emailPage.isElementPresent(By.className("table"))) {
+            return isEmailDraftTableHeaderCorrect();
+        } else {     
+            return false;
+        }
+    }
+
+    private boolean isEmailDraftTableHeaderCorrect() {
+        int numColumns = emailPage.getNumberOfColumnsFromDataTable(0); // 1 table
+        
+        if (numColumns != ADMIN_EMAIL_DRAFT_TABLE_NUM_COLUMNS) {
+            return false;
+        }
+        
+        List<String> expectedSessionTableHeaders = Arrays.asList("Action",
+                                                                 "Address Receiver",
+                                                                 "Group Receiver",
+                                                                 "Subject",
+                                                                 "Date");
+        List<String> actualSessionTableHeaders = new ArrayList<String>();
+        
+        for (int i = 0 ; i < numColumns ; i++) {
+            actualSessionTableHeaders.add(emailPage.getHeaderValueFromDataTable(0, 0, i));
+        }
+        
+        return actualSessionTableHeaders.equals(expectedSessionTableHeaders);
+    }
+    
+    /**
+     * This method only checks if the email trash data table is displayed correctly
+     * i.e, table headers are correct
+     * It does not test for the table content
+     */
+    private boolean isEmailTrashDataDisplayCorrect() {
+        if (emailPage.isElementPresent(By.className("table")) && isEmptyTrashButtonPresent()) {
+            return isEmailTrashTableHeaderCorrect();
+        } else {     
+            return false;
+        }
+    }
+
+    private boolean isEmailTrashTableHeaderCorrect() {
+        int numColumns = emailPage.getNumberOfColumnsFromDataTable(0); // 1 table
+        
+        if (numColumns != ADMIN_EMAIL_TRASH_TABLE_NUM_COLUMNS) {
+            return false;
+        }
+        
+        List<String> expectedSessionTableHeaders = Arrays.asList("Action",
+                                                                 "Address Receiver",
+                                                                 "Group Receiver",
+                                                                 "Subject",
+                                                                 "Date");
+        List<String> actualSessionTableHeaders = new ArrayList<String>();
+        
+        for (int i = 0 ; i < numColumns ; i++) {
+            actualSessionTableHeaders.add(emailPage.getHeaderValueFromDataTable(0, 0, i));
+        }
+        
+        return actualSessionTableHeaders.equals(expectedSessionTableHeaders);
+    }
+    
+    private boolean isEmptyTrashButtonPresent() {
+        if (!emailPage.isElementPresent(By.className("btn-danger"))) {
+            return false;
+        }
+        
+        WebElement trashButton = browser.driver.findElement(By.className("btn-danger"));
+        
+        return trashButton.getText().contains("Empty Trash");
     }
     
     @AfterClass
