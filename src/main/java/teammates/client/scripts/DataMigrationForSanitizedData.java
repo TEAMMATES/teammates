05d324cf9e19b15cab2f1061acc3a682b189a73f
bamboo3250 <bambooVN_3250@yahoo.com>
2015-12-07 21:22:44 +0800

    add isPreview boolean

--- src/main/java/teammates/client/scripts/DataMigrationForSanitizedData.java
+++ src/main/java/teammates/client/scripts/DataMigrationForSanitizedData.java
@@ -15,7 +15,8 @@ import teammates.logic.core.StudentsLogic;
 import teammates.storage.datastore.Datastore;
 
 public class DataMigrationForSanitizedData extends RemoteApiClient {
-
+    private final boolean isPreview = true;
+    
     public static void main(String[] args) throws IOException {
         DataMigrationForSanitizedData migrator = new DataMigrationForSanitizedData();
         migrator.doOperationRemotely();
@@ -26,9 +27,66 @@ public class DataMigrationForSanitizedData extends RemoteApiClient {
         Datastore.initialize();
 
         List<StudentAttributes> allStudents = getAllStudents();
-        for (StudentAttributes student : allStudents) {
-            fixSanitizedDataForStudent(student);
+        if (isPreview) {
+            System.out.println("Fixing Sanitization for students...");
         }
+        int numberOfAffectedStudents = 0;
+        for (StudentAttributes student : allStudents) {
+            if (!isPreview) {
+                fixSanitizedDataForStudent(student);
+            } else {
+                if (previewSanitizedDataForStudent(student)) {
+                    numberOfAffectedStudents++;
+                }
+            }
+        }
+        if (isPreview) {
+            System.out.println("There are/is " + numberOfAffectedStudents + " student(s) affected!");
+        } else {
+            System.out.println("Sanitization fixing done!");
+        }
+    }
+
+    private boolean previewSanitizedDataForStudent(StudentAttributes student) {
+        boolean hasSanitizedData = checkStudentHasSanitizedData(student);
+        if (hasSanitizedData) {
+            System.out.println("Updating student having email: " + student.email);
+            
+            if (isSanitizedString(student.comments)) {
+                System.out.println("comments: " + student.comments);
+                System.out.println("new comments: " + fixSanitization(student.comments));
+            }
+            if (isSanitizedString(student.course)) {
+                System.out.println("course: " + student.course);
+                System.out.println("new course: " + fixSanitization(student.course));
+            }
+            if (isSanitizedString(student.email)) {
+                System.out.println("email: " + student.email);
+                System.out.println("new email: " + fixSanitization(student.email));
+            }
+            if (isSanitizedString(student.googleId)) {
+                System.out.println("googleId: " + student.googleId);
+                System.out.println("new googleId: " + fixSanitization(student.googleId));
+            }
+            if (isSanitizedString(student.lastName)) {
+                System.out.println("lastName: " + student.lastName);
+                System.out.println("new lastName: " + fixSanitization(student.lastName));
+            }
+            if (isSanitizedString(student.name)) {
+                System.out.println("name: " + student.name);
+                System.out.println("new name: " + fixSanitization(student.name));
+            }
+            if (isSanitizedString(student.section)) {
+                System.out.println("section: " + student.section);
+                System.out.println("new section: " + fixSanitization(student.section));
+            }
+            if (isSanitizedString(student.team)) {
+                System.out.println("team: " + student.team);
+                System.out.println("new team: " + fixSanitization(student.team));
+            }
+            System.out.println();
+        }
+        return hasSanitizedData;
     }
 
     private boolean isSanitizedString(String s){
@@ -50,20 +108,31 @@ public class DataMigrationForSanitizedData extends RemoteApiClient {
         return s;
     }
     
-    private void fixSanitizedDataForStudent(StudentAttributes student) {
+    private boolean checkStudentHasSanitizedData(StudentAttributes student) {
+        return isSanitizedString(student.comments) || isSanitizedString(student.course) || 
+               isSanitizedString(student.email) || isSanitizedString(student.googleId) || 
+               isSanitizedString(student.lastName) || isSanitizedString(student.name) || 
+               isSanitizedString(student.section) || isSanitizedString(student.team);
+    }
+    
+    private void fixSanitizationForStudent(StudentAttributes student) {
         student.comments = fixSanitization(student.comments);
         student.course = fixSanitization(student.course);
         student.email = fixSanitization(student.email);
         student.googleId = fixSanitization(student.googleId);
-        student.key = fixSanitization(student.key);
         student.lastName = fixSanitization(student.lastName);
         student.name = fixSanitization(student.name);
         student.section = fixSanitization(student.section);
         student.team = fixSanitization(student.team);
-        
+    }
+    
+    private void fixSanitizedDataForStudent(StudentAttributes student) {
         try {
-            StudentsLogic.inst().updateStudentCascade(student.email, student);
-            
+            boolean hasSanitizedData = checkStudentHasSanitizedData(student);
+            if (hasSanitizedData) {
+                fixSanitizationForStudent(student);
+                StudentsLogic.inst().updateStudentCascade(student.email, student);
+            }
         } catch (InvalidParametersException e) {
             Utils.getLogger().log(Level.INFO, "Student " + student.email + " invalid!");
             e.printStackTrace();
@@ -71,7 +140,6 @@ public class DataMigrationForSanitizedData extends RemoteApiClient {
             Utils.getLogger().log(Level.INFO, "Student " + student.email + " does not exist!");
             e.printStackTrace();
         }
-        
     }
 
     protected PersistenceManager getPM() {
