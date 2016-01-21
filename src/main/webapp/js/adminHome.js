b99dbfcabde323e81828a9abb5aba9c14e4ad276
bamboo3250 <bambooVN_3250@yahoo.com>
2016-01-22 02:55:29 +0800

    replace tab by spaces

--- src/main/webapp/js/adminHome.js
+++ src/main/webapp/js/adminHome.js
@@ -1,121 +1,121 @@
 function createRowForResultTable(shortName, name, email, institution, isSuccess, status) {
-	var result = "<td>" + shortName + "</td>";
-	result += "<td>" + name + "</td>";
-	result += "<td>" + email + "</td>";
-	result += "<td>" + institution + "</td>";
-	if (isSuccess) {
-		result += "<td>Success</td>";
-	} else {
-		result += "<td>Fail</td>";
-	}
-	result += "<td>" + status + "</td>";
-	if (isSuccess) {
-		result = "<tr class=\"success\">" + result + "</tr>";
-	} else {
-		result = "<tr class=\"danger\">" + result + "</tr>";
-	}
-	return result;
+    var result = "<td>" + shortName + "</td>";
+    result += "<td>" + name + "</td>";
+    result += "<td>" + email + "</td>";
+    result += "<td>" + institution + "</td>";
+    if (isSuccess) {
+        result += "<td>Success</td>";
+    } else {
+        result += "<td>Fail</td>";
+    }
+    result += "<td>" + status + "</td>";
+    if (isSuccess) {
+        result = "<tr class=\"success\">" + result + "</tr>";
+    } else {
+        result = "<tr class=\"danger\">" + result + "</tr>";
+    }
+    return result;
 }
 
 var paramsCounter = 0;
-var paramsList = [];	// list of parameter strings that will be sent via ajax
+var paramsList = [];    // list of parameter strings that will be sent via ajax
 var instructorDetailsList = [];
 var isInputFromFirstPanel = false;
 
 function disableAddInstructorForm() {
-	$(".addInstructorBtn").each(function() {
-		$(this).html("<img src='/images/ajax-loader.gif'/>");
-	});
-	$(".addInstructorFormControl").each(function() {
-		$(this).prop('disabled', true);
-	});
-	
+    $(".addInstructorBtn").each(function() {
+        $(this).html("<img src='/images/ajax-loader.gif'/>");
+    });
+    $(".addInstructorFormControl").each(function() {
+        $(this).prop('disabled', true);
+    });
+    
 }
 
 function enableAddInstructorForm() {
-	$(".addInstructorBtn").each(function() {
-		$(this).html("Add Instructor");
-	});
-	$(".addInstructorFormControl").each(function() {
-		$(this).prop('disabled', false);
-	});
+    $(".addInstructorBtn").each(function() {
+        $(this).html("Add Instructor");
+    });
+    $(".addInstructorFormControl").each(function() {
+        $(this).prop('disabled', false);
+    });
 }
 
 function addInstructorByAjaxRecursively() {
-	$.ajax({
+    $.ajax({
         type : 'POST',
         url :   "/admin/adminInstructorAccountAdd?" + paramsList[paramsCounter],
         beforeSend : function() {
-        	disableAddInstructorForm();
+            disableAddInstructorForm();
         },
         error : function() {
-        	var rowText = createRowForResultTable(data.instructorShortName, data.instructorName, 
-        										  data.instructorEmail,data.instructorInstitution,
-        										  false, data.statusForAjax);
-    		$("#addInstructorResultTable tbody").append(rowText);
-    		if (isInputFromFirstPanel) {
-    			var errorInstructorsText = $("#addInstructorDetailsSingleLine").val() + instructorDetailsList[paramsCounter] + "\n";
-            	$("#addInstructorDetailsSingleLine").val(errorInstructorsText);
-    		}
-        	paramsCounter++;
-    		var panelHeader = "<strong>Result (" + paramsCounter + "/" + paramsList.length + ")</strong>";
-			$("#addInstructorResultPanel div.panel-heading").html(panelHeader);
-    		if (paramsCounter < paramsList.length) {
-    			addInstructorByAjaxRecursively();
-    		} else {
-    			enableAddInstructorForm();
-    		}
+            var rowText = createRowForResultTable(data.instructorShortName, data.instructorName, 
+                                                  data.instructorEmail,data.instructorInstitution,
+                                                  false, data.statusForAjax);
+            $("#addInstructorResultTable tbody").append(rowText);
+            if (isInputFromFirstPanel) {
+                var errorInstructorsText = $("#addInstructorDetailsSingleLine").val() + instructorDetailsList[paramsCounter] + "\n";
+                $("#addInstructorDetailsSingleLine").val(errorInstructorsText);
+            }
+            paramsCounter++;
+            var panelHeader = "<strong>Result (" + paramsCounter + "/" + paramsList.length + ")</strong>";
+            $("#addInstructorResultPanel div.panel-heading").html(panelHeader);
+            if (paramsCounter < paramsList.length) {
+                addInstructorByAjaxRecursively();
+            } else {
+                enableAddInstructorForm();
+            }
         },
         success : function(data) {
-        	var rowText = createRowForResultTable(data.instructorShortName, data.instructorName, 
-												  data.instructorEmail,data.instructorInstitution,
-												  data.instructorAddingResultForAjax, data.statusForAjax);
-        	$("#addInstructorResultTable tbody").append(rowText);
-        	if ((!data.instructorAddingResultForAjax) && (isInputFromFirstPanel)) {
-        		var errorInstructorsText = $("#addInstructorDetailsSingleLine").val() + instructorDetailsList[paramsCounter] + "\n";
-        		$("#addInstructorDetailsSingleLine").val(errorInstructorsText);
-        	}
-        	paramsCounter++;
-        	var panelHeader = "<strong>Result (" + paramsCounter + "/" + paramsList.length + ")</strong>";
-			$("#addInstructorResultPanel div.panel-heading").html(panelHeader);
-    		if (paramsCounter < paramsList.length) {
-    			addInstructorByAjaxRecursively();
-    		} else {
-    			enableAddInstructorForm();
-    		}
+            var rowText = createRowForResultTable(data.instructorShortName, data.instructorName, 
+                                                  data.instructorEmail,data.instructorInstitution,
+                                                  data.instructorAddingResultForAjax, data.statusForAjax);
+            $("#addInstructorResultTable tbody").append(rowText);
+            if ((!data.instructorAddingResultForAjax) && (isInputFromFirstPanel)) {
+                var errorInstructorsText = $("#addInstructorDetailsSingleLine").val() + instructorDetailsList[paramsCounter] + "\n";
+                $("#addInstructorDetailsSingleLine").val(errorInstructorsText);
+            }
+            paramsCounter++;
+            var panelHeader = "<strong>Result (" + paramsCounter + "/" + paramsList.length + ")</strong>";
+            $("#addInstructorResultPanel div.panel-heading").html(panelHeader);
+            if (paramsCounter < paramsList.length) {
+                addInstructorByAjaxRecursively();
+            } else {
+                enableAddInstructorForm();
+            }
         }
     });
 }
 
 function addInstructorByAjax() {
-	$("#addInstructorResultPanel").show();	// show the hidden panel
-	
-	var multipleLineText = $("#addInstructorDetailsSingleLine").val();	// get input from the first panel
-	multipleLineText = multipleLineText.trim();
-	
-	if (multipleLineText.length == 0) {
-		var instructorDetails = $("#instructorName").val() + "|" + $("#instructorEmail").val() + "|" + $("#instructorInstitution").val();
-		instructorDetailsList = [instructorDetails];
-		var params = "instructorshortname=" + $("#instructorShortName").val() +
-					 "&instructorname=" + $("#instructorName").val() +
-					 "&instructoremail=" + $("#instructorEmail").val() +
-					 "&instructorinstitution=" + $("#instructorInstitution").val();
-		paramsList = [params];
-		isInputFromFirstPanel = false;
-	} else {
-		instructorDetailsList = multipleLineText.split("\n");
-		paramsList = [];
-		for(var i = 0; i < instructorDetailsList.length; i++) {
-			instructorDetailsList[i] = instructorDetailsList[i].replace(/\t/g,"|");
-			paramsList[i] = "instructordetailssingleline=" + instructorDetailsList[i];
-		}
-		isInputFromFirstPanel = true;
-	}
-	paramsCounter = 0;
-	$("#addInstructorResultTable tbody").html("");	// clear table
-	$("#addInstructorDetailsSingleLine").val("");	// clear input form
-	$("#addInstructorResultPanel div.panel-heading").html("<strong>Result</strong>");	// clear panel header
-	if (paramsCounter < paramsList.length) {
-		addInstructorByAjaxRecursively();
-	}
+    $("#addInstructorResultPanel").show();    // show the hidden panel
+    
+    var multipleLineText = $("#addInstructorDetailsSingleLine").val();    // get input from the first panel
+    multipleLineText = multipleLineText.trim();
+    
+    if (multipleLineText.length == 0) {
+        var instructorDetails = $("#instructorName").val() + "|" + $("#instructorEmail").val() + "|" + $("#instructorInstitution").val();
+        instructorDetailsList = [instructorDetails];
+        var params = "instructorshortname=" + $("#instructorShortName").val() +
+                     "&instructorname=" + $("#instructorName").val() +
+                     "&instructoremail=" + $("#instructorEmail").val() +
+                     "&instructorinstitution=" + $("#instructorInstitution").val();
+        paramsList = [params];
+        isInputFromFirstPanel = false;
+    } else {
+        instructorDetailsList = multipleLineText.split("\n");
+        paramsList = [];
+        for(var i = 0; i < instructorDetailsList.length; i++) {
+            instructorDetailsList[i] = instructorDetailsList[i].replace(/\t/g,"|");
+            paramsList[i] = "instructordetailssingleline=" + instructorDetailsList[i];
+        }
+        isInputFromFirstPanel = true;
+    }
+    paramsCounter = 0;
+    $("#addInstructorResultTable tbody").html("");    // clear table
+    $("#addInstructorDetailsSingleLine").val("");    // clear input form
+    $("#addInstructorResultPanel div.panel-heading").html("<strong>Result</strong>");    // clear panel header
+    if (paramsCounter < paramsList.length) {
+        addInstructorByAjaxRecursively();
+    }
 }
