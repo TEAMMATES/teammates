05e0fae4067f689f4d8701c849d5297058fbf0a2
Sumei <susumaibaobao@gmail.com>
2016-01-31 16:24:40 +0800

    fix the typo

--- src/main/webapp/js/instructorStudentEdit.js
+++ src/main/webapp/js/instructorStudentEdit.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $("#button_submit").on('click', changeStudnetInformationConfirmation);
 });
 
-function changeStudnetInformationConfirmation() {
+function changeStudentInformationConfirmation() {
     return confirm('Editing these fields will result in some existing responses from this student to be deleted. You may download the data before you make the changes. Are you sure you want to continue?');
 }
