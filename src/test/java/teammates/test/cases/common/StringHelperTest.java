8c6647931e29f11eb150cd2c94d917c29d4197bf
Tarun <tarun14110@iiitd.ac.in>
2016-02-17 20:03:30 +0530

    added unit test for truncateHead function

--- src/test/java/teammates/test/cases/common/StringHelperTest.java
+++ src/test/java/teammates/test/cases/common/StringHelperTest.java
@@ -246,5 +246,12 @@ public class StringHelperTest extends BaseTestCase {
             assertEquals(expectedResult, StringHelper.isAnyMatching(str, regexArray));
         }
     }
-
+    
+    @Test
+    public void testTruncateHead() {
+        assertEquals("1234567890", StringHelper.truncateHead("xxxx1234567890", 10));
+        assertEquals("1234567890", StringHelper.truncateHead("1234567890", 10));
+        assertEquals("123456789", StringHelper.truncateHead("123456789", 10));
+        assertEquals("567890", StringHelper.truncateHead("1234567890", 6));
+    }
 }
