9eba8ad955e131c71e18b5d5d33d668b7f070669
Tarun <tarun14110@iiitd.ac.in>
2016-02-17 20:09:55 +0530

    moved comma(,) before line break

--- src/main/java/teammates/ui/controller/AdminInstructorAccountAddAction.java
+++ src/main/java/teammates/ui/controller/AdminInstructorAccountAddAction.java
@@ -298,20 +298,20 @@ public class AdminInstructorAccountAddAction extends Action {
     private String generateNextDemoCourseId(String instructorEmailOrProposedCourseId, int maximumIdLength){
         final boolean isFirstCourseId = instructorEmailOrProposedCourseId.contains("@");
         if(isFirstCourseId){
-            return StringHelper.truncateHead(getDemoCourseIdRoot(instructorEmailOrProposedCourseId)
-                                            , maximumIdLength);
+            return StringHelper.truncateHead(getDemoCourseIdRoot(instructorEmailOrProposedCourseId),
+                                            maximumIdLength);
         } else {
             final boolean isFirstTimeDuplicate = instructorEmailOrProposedCourseId.endsWith("-demo"); 
             if(isFirstTimeDuplicate){
-                return StringHelper.truncateHead(instructorEmailOrProposedCourseId + "0"
-                                                , maximumIdLength);
+                return StringHelper.truncateHead(instructorEmailOrProposedCourseId + "0",
+                                                maximumIdLength);
             } else {
                 final int lastIndexOfDemo = instructorEmailOrProposedCourseId.lastIndexOf("-demo");
                 final String root = instructorEmailOrProposedCourseId.substring(0, lastIndexOfDemo);
                 final int previousDedupSuffix = Integer.parseInt(instructorEmailOrProposedCourseId.substring(lastIndexOfDemo + 5));
 
-                return StringHelper.truncateHead(root + "-demo" + (previousDedupSuffix + 1)
-                                                , maximumIdLength);
+                return StringHelper.truncateHead(root + "-demo" + (previousDedupSuffix + 1),
+                                                maximumIdLength);
             }
         }
     }
