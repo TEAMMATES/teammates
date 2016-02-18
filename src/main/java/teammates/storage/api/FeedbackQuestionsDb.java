04f21e115d8b9aa5aafd25b383b61e8130e71a3f
John Kevin Tjahjadi <jk_john_kevin@hotmail.com>
2016-02-18 12:46:53 +0800

    updated header comments for updateFeedbackQuestion method in FeedbackQuestionsDb.java

--- src/main/java/teammates/storage/api/FeedbackQuestionsDb.java
+++ src/main/java/teammates/storage/api/FeedbackQuestionsDb.java
@@ -148,6 +148,16 @@ public class FeedbackQuestionsDb extends EntitiesDb {
         return fqList;
     }
     
+    /**
+     * Updates the feedback question identified by `{@code newAttributes.getId()} 
+     * For the remaining parameters, the existing value is preserved 
+     *   if the parameter is null (due to 'keep existing' policy).<br> 
+     * This will call the method {@code updateFeedbackQuestion} with {@code keepUpdateTimestamp = false}
+     * so that every changes to the timestamp will be reflected in the FeedbackQuestion.
+     * Preconditions: <br>
+     * * {@code newAttributes.getId()} is non-null and
+     *  correspond to an existing feedback question. <br>
+     */
     public void updateFeedbackQuestion(FeedbackQuestionAttributes newAttributes) throws InvalidParametersException, EntityDoesNotExistException {
         updateFeedbackQuestion(newAttributes, false);
     }
@@ -156,6 +166,8 @@ public class FeedbackQuestionsDb extends EntitiesDb {
      * Updates the feedback question identified by `{@code newAttributes.getId()} 
      * For the remaining parameters, the existing value is preserved 
      *   if the parameter is null (due to 'keep existing' policy).<br> 
+     * The boolean variable {@code keepUpdateTimestamp} would be set so that it will prevent
+     * changes / keep changes to the updated timestamp of the FeedbackQuestion.<br>
      * Preconditions: <br>
      * * {@code newAttributes.getId()} is non-null and
      *  correspond to an existing feedback question. <br>
