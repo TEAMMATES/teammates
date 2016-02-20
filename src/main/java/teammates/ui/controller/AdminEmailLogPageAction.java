5c582c3afc18031b4cbcc054d619ec91754ccc2d
bamboo3250 <bambooVN_3250@yahoo.com>
2016-02-20 22:08:24 +0800

    Integrate LogHelper into EmailLogPage

--- src/main/java/teammates/ui/controller/AdminEmailLogPageAction.java
+++ src/main/java/teammates/ui/controller/AdminEmailLogPageAction.java
@@ -4,32 +4,42 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.google.appengine.api.log.AppLogLine;
+
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.util.Const;
 import teammates.common.util.EmailLogEntry;
 import teammates.common.util.LogHelper;
 import teammates.common.util.StatusMessage;
+import teammates.common.util.TimeHelper;
 import teammates.common.util.Const.StatusMessageColor;
 import teammates.logic.api.GateKeeper;
 
 public class AdminEmailLogPageAction extends Action {
     
     private static final int LOGS_PER_PAGE = 50;
-    private static final int MAX_LOGSEARCH_LIMIT = 15000;
+    private static final int MAX_SEARCH_TIMES = 12; // search maximum 12 times with time increment
+    
+    private Long nextEndTimeToSearch;
     
     @Override
     protected ActionResult execute() throws EntityDoesNotExistException {
         
         new GateKeeper().verifyAdminPrivileges(account);
+        String timeOffset = getRequestParamValue("offset");
+        Long endTimeToSearch;
+        if ((timeOffset != null) && (!timeOffset.isEmpty())) {
+            endTimeToSearch = Long.parseLong(timeOffset);
+        } else {
+            endTimeToSearch = TimeHelper.now(0.0).getTimeInMillis();
+        }
         
-        AdminEmailLogPageData data = new AdminEmailLogPageData(account, getRequestParamValue("offset"),
-                                        getRequestParamValue("filterQuery"), getRequestParamAsBoolean("all"));
+        AdminEmailLogPageData data = new AdminEmailLogPageData(account, getRequestParamValue("filterQuery"), getRequestParamAsBoolean("all"));
         
         String pageChange = getRequestParamValue("pageChange");
-       
-        if (pageChange != null && pageChange.equals("true")) {
+        boolean isPageChanged = (pageChange != null && pageChange.equals("true")) || (timeOffset == null);
+        if (isPageChanged) {
             //Reset the offset because we are performing a new search, so we start from the beginning of the logs
-            data.setOffset(null);
+            endTimeToSearch = TimeHelper.now(0.0).getTimeInMillis();
         }
         
         if (data.getFilterQuery() == null) {
@@ -39,56 +49,63 @@ public class AdminEmailLogPageAction extends Action {
         //This is used to parse the filterQuery. If the query is not parsed, the filter function would ignore the query
         data.generateQueryParameters(data.getFilterQuery());
         
-        LogHelper logHelper = new LogHelper();
-        logHelper.setQuery(data.getVersions(), null, null, data.getOffset());
-        data.setLogs(getEmailLogs(logHelper, data));
+        
+        data.setLogs(getEmailLogs(endTimeToSearch, data));
         
         statusToAdmin = "adminEmailLogPage Page Load";
         
-        if (data.getOffset() == null) {
+        if (isPageChanged) {
             return createShowPageResult(Const.ViewURIs.ADMIN_EMAIL_LOG, data);
         }
         
         return createAjaxResult(data);
     }
     
-    private List<EmailLogEntry> getEmailLogs(LogHelper logHelper, AdminEmailLogPageData data) {
+    private List<EmailLogEntry> getEmailLogs(Long endTimeToSearch, AdminEmailLogPageData data) {
         List<EmailLogEntry> emailLogs = new LinkedList<EmailLogEntry>();
+        
+        LogHelper logHelper = new LogHelper();
+        logHelper.setQuery(data.getVersions(), null, endTimeToSearch);
+        
         int totalLogsSearched = 0;
-        int currentLogsInPage = 0;
         
-        String lastOffset = null;
-        
-        List<AppLogLine> appLogLines = logHelper.fetchLogs();
-        for (AppLogLine appLog : appLogLines) {
-            if (currentLogsInPage >= LOGS_PER_PAGE) break;
-            String logMsg = appLog.getLogMessage();
-            boolean isNotEmailLog = (!logMsg.contains("TEAMMATESEMAILLOG"));
-            if (isNotEmailLog) continue;
-            
-            EmailLogEntry emailLogEntry = new EmailLogEntry(appLog);
-            if(!data.shouldShowLog(emailLogEntry)) continue;
-            
-            emailLogs.add(emailLogEntry);
-            currentLogsInPage++;
+        for(int i = 0; i < MAX_SEARCH_TIMES; i++) {
+            if (emailLogs.size() >= LOGS_PER_PAGE) {
+                break;
+            }
+            List<AppLogLine> searchResult = logHelper.fetchLogsInNextHours();
+            List<EmailLogEntry> filteredLogs = filterLogsForEmailLogPage(searchResult, data);
+            emailLogs.addAll(filteredLogs);
+            totalLogsSearched += searchResult.size();
         }
+        nextEndTimeToSearch = logHelper.getEndTime();
         
-        totalLogsSearched = appLogLines.size();
         String status="&nbsp;&nbsp;Total Logs gone through in last search: " + totalLogsSearched + "<br>";
         //link for Next button, will fetch older logs
-        if (totalLogsSearched >= MAX_LOGSEARCH_LIMIT){
-            status += "<br><span class=\"red\">&nbsp;&nbsp;Maximum amount of logs per requst have been searched.</span><br>";
-            status += "<button class=\"btn-link\" id=\"button_older\" onclick=\"submitFormAjax('" + lastOffset + "');\">Search More</button>";           
-        }
-        
-        if (currentLogsInPage >= LOGS_PER_PAGE) {   
-            status += "<button class=\"btn-link\" id=\"button_older\" onclick=\"submitFormAjax('" + lastOffset + "');\">Older Logs </button>";              
-        }
-        
+        status += "<button class=\"btn-link\" id=\"button_older\" onclick=\"submitFormAjax('" + nextEndTimeToSearch + "');\">Search More</button>";           
         data.setStatusForAjax(status);
-        statusToUser.add(new StatusMessage(status, 
-                           totalLogsSearched >= MAX_LOGSEARCH_LIMIT ? StatusMessageColor.WARNING : StatusMessageColor.INFO));
+        statusToUser.add(new StatusMessage(status, StatusMessageColor.INFO));
+        return emailLogs;
+    }
+
+    private List<EmailLogEntry> filterLogsForEmailLogPage(List<AppLogLine> appLogLines,
+                                                          AdminEmailLogPageData data) {
+        List<EmailLogEntry> emailLogs = new LinkedList<EmailLogEntry>();
         
+        for (AppLogLine appLog : appLogLines) {
+            String logMsg = appLog.getLogMessage();
+            boolean isNotEmailLog = (!logMsg.contains("TEAMMATESEMAILLOG"));
+            if (isNotEmailLog) {
+                continue;
+            }
+            
+            EmailLogEntry emailLogEntry = new EmailLogEntry(appLog);
+            if(!data.shouldShowLog(emailLogEntry)) {
+                continue;
+            }
+            
+            emailLogs.add(emailLogEntry);
+        }
         return emailLogs;
     }
 }
