d666f89be16fa8b09714abc27e1d695c38a89364
Manish <manish.vishnoi2@gmail.com>
2016-03-01 21:17:27 +0530

    fixed wrong status message

--- src/test/java/teammates/test/cases/ui/browsertests/AdminAccountDetailsPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/AdminAccountDetailsPageUiTest.java
@@ -68,7 +68,7 @@ public class AdminAccountDetailsPageUiTest extends BaseUiTestCase{
         
         courseId = "AAMgtUiT.CS1101";
         detailsPage.clickRemoveStudentFromCourse(courseId)
-            .verifyStatus(Const.StatusMessages.INSTRUCTOR_REMOVED_FROM_COURSE);
+            .verifyStatus(Const.StatusMessages.STUDENT_DELETED);
         assertNull(BackDoor.getStudent(courseId, "AAMgtUiT.instr2@gmail.com"));
         detailsPage.verifyHtmlMainContent("/adminAccountDetailsRemoveStudent.html");
     }
