84e3f6e65a7219e575502359322dc402e1d8a570
Low WeiLin <lwl1991@gmail.com>
2016-03-02 16:41:49 +0800

    Use cast in test

--- src/test/java/teammates/test/cases/common/StudentAttributesTest.java
+++ src/test/java/teammates/test/cases/common/StudentAttributesTest.java
@@ -60,18 +60,18 @@ public class StudentAttributesTest extends BaseTestCase {
         expected = generateTypicalStudentObject();
         studentUnderTest = new StudentAttributes("  sect 1 ", "  team 1   ", "   name 1   ",
                                                  "   email@email.com  ", "  comment 1  ", "courseId1");
-        verifyStudentContent(expected, studentUnderTest.toEntity());
+        verifyStudentContent(expected, (Student)studentUnderTest.toEntity());
 
         ______TS("Typical case: contains google id");
         expected = generateTypicalStudentObject();
         studentUnderTest = new StudentAttributes("googleId.1", "email@email.com", "name 1", "comment 1",
                                                  "courseId1", "team 1", "section 1");
-        verifyStudentContentIncludingID(expected, studentUnderTest.toEntity());
+        verifyStudentContentIncludingID(expected, (Student)studentUnderTest.toEntity());
 
         ______TS("Typical case: initialize from entity");
         expected = generateTypicalStudentObject();
         studentUnderTest = new StudentAttributes(expected);
-        verifyStudentContentIncludingID(expected, studentUnderTest.toEntity());
+        verifyStudentContentIncludingID(expected, (Student)studentUnderTest.toEntity());
 
         ______TS("Failure case: empty course id");
         invalidStudent = new StudentAttributes("section", "team", "name", "e@e.com", "c", "");
