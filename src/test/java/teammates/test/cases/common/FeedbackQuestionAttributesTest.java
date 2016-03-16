62d48951af901f97a2ded9f852ec15ba4abffb9c
YongJieYongJie <KhooYongJie@gmx.com>
2016-03-16 15:13:22 +0800

    Updates test expected errorMessage in FeedbackQuestionAttributesTest.java

--- src/test/java/teammates/test/cases/common/FeedbackQuestionAttributesTest.java
+++ src/test/java/teammates/test/cases/common/FeedbackQuestionAttributesTest.java
@@ -54,7 +54,7 @@ public class FeedbackQuestionAttributesTest extends BaseTestCase {
 
         assertFalse(fq.isValid());
 
-        String errorMessage = String.format(FEEDBACK_SESSION_NAME_ERROR_MESSAGE, fq.creatorEmail, REASON_EMPTY) + EOL
+        String errorMessage = FEEDBACK_SESSION_NAME_ERROR_MESSAGE + EOL
                               + String.format(COURSE_ID_ERROR_MESSAGE, fq.courseId, REASON_EMPTY) + EOL
                               + String.format("Invalid creator's email: " + EMAIL_ERROR_MESSAGE, fq.creatorEmail, REASON_EMPTY) + EOL
                               + String.format(PARTICIPANT_TYPE_ERROR_MESSAGE, fq.giverType.toString(), GIVER_TYPE_NAME) + EOL
