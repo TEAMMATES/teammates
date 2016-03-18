444d0f6ea914dd2c97255a49cd654a23b5ba1d2c
YongJieYongJie <KhooYongJie@gmx.com>
2016-03-18 12:24:10 +0800

    Updates outdated comment and adds null guard for sanitizeForJs()

--- src/main/java/teammates/common/util/Sanitizer.java
+++ src/main/java/teammates/common/util/Sanitizer.java
@@ -90,9 +90,13 @@ public class Sanitizer {
 
     /**
      * Escape the string for inserting into javascript code.
-     * This automatically calls {@link #escapeHTML} so make it safe for HTML too.
+     * This automatically calls {@link #sanitizeForHtml} so make it safe for HTML too.
+     *
+     * @param string
+     * @return the sanitized string or null (if the parameter was null).
      */
     public static String sanitizeForJs(String str){ 
+        if(str == null) return null;
         return Sanitizer.sanitizeForHtml(
                 str.replace("\\", "\\\\")
                 .replace("\"", "\\\"")
