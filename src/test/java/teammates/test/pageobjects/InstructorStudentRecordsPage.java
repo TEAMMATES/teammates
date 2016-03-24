70a993cf46f290f6a38d864989741a6338c0a96d
Serhii Beliablia <Serhii.Beliablia@portaone.com>
2016-03-24 23:31:09 +0200

    Added test for 'all records' expand/collapse functionality

--- src/test/java/teammates/test/pageobjects/InstructorStudentRecordsPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorStudentRecordsPage.java
@@ -3,6 +3,8 @@ package teammates.test.pageobjects;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -110,6 +112,68 @@ public class InstructorStudentRecordsPage extends AppPage {
 
     private WebElement getCommentSaveLink(int id) {
         return browser.driver.findElement(By.id("commentsave-" + id));
+    }
+
+    /**
+     * Clicks all the headings of the record panels to either expand/collapse the panel body.
+     */
+    public void clickAllRecordPanelHeadings() {
+        for (WebElement e : browser.driver.findElements(By.cssSelector("div[id^='studentFeedback-']"))) {
+            e.findElement(By.cssSelector(".panel-heading")).click();
+        }
+    }
+
+    /**
+     * Checks if the body of all the record panels are visible.
+     * @return true if all record panel bodies are visible
+     */
+    public boolean areRecordsVisible() {
+        return areAllRecordPanelBodiesVisibilityEquals(true);
+    }
+
+    /**
+     * Checks if the body of all the record panels are hidden
+     * @return true if all record panel bodies are hidden
+     */
+    public boolean areRecordsHidden() {
+        return areAllRecordPanelBodiesVisibilityEquals(false);
+    }
+
+    /**
+     * Checks if the bodies of all the record panels are collapsed or expanded.
+     * @param isVisible true to check for expanded, false to check for collapsed.
+     * @return true if all record panel bodies are equals to the visibility being checked.
+     */
+    private boolean areAllRecordPanelBodiesVisibilityEquals(boolean isVisible) {
+        By panelCollapseSelector = By.cssSelector(".panel-collapse");
+        List<WebElement> webElements = browser.driver.findElements(panelCollapseSelector);
+
+        for(WebElement e : webElements) {
+            if(e.isDisplayed() != isVisible) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Waits for all the panels to collapse.
+     */
+    public void waitForPanelsToCollapse() {
+        By panelCollapseSelector = By.cssSelector(".panel-collapse");
+
+        waitForElementToDisappear(panelCollapseSelector);
+    }
+
+    /**
+     * Waits for all the panels to expand.
+     */
+    public void waitForPanelsToExpand() {
+        By panelExpandSelector = By.cssSelector(".panel-collapse");
+        List<WebElement> webElements = browser.driver.findElements(panelExpandSelector);
+
+        waitForElementsVisibility(webElements);
     }
 
 }
