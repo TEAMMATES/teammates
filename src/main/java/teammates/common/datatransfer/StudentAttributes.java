f71a4390f42fc94d379a4521e3b607276a73c47c
Wilson Kurniawan <wkurniawan.92@gmail.com>
2016-05-13 10:40:04 +0800

    fixed the PMD error

--- src/main/java/teammates/common/datatransfer/StudentAttributes.java
+++ src/main/java/teammates/common/datatransfer/StudentAttributes.java
@@ -286,7 +286,6 @@ public class StudentAttributes extends EntityAttributes {
         if (this.email == null) {
             this.email = originalStudent.email;
         }
-        String abc;
 
         if (this.name == null) {
             this.name = originalStudent.name;
