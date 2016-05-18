d79b5124eda58d4e87817d8bba689d3ab4e58fa0
tarun <tarun14110@iiitd.ac.in>
2016-05-18 10:34:34 +0530

    removed unsused import form FeedbackQuestionsLogicTest.java

--- src/test/java/teammates/test/cases/logic/FeedbackQuestionsLogicTest.java
+++ src/test/java/teammates/test/cases/logic/FeedbackQuestionsLogicTest.java
@@ -15,7 +15,6 @@ import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.FeedbackQuestionDetails;
 import teammates.common.datatransfer.FeedbackParticipantType;
 import teammates.common.datatransfer.FeedbackQuestionAttributes;
-import teammates.common.datatransfer.FeedbackResponseAttributes;
 import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
