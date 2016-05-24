06805ef819b59782ef9abda5cae8ed85ed474a5a
JosephineKwa <work.and.stuff.jo@gmail.com>
2016-05-24 14:31:02 +0800

    remove unnecessary parentheses

--- src/main/java/teammates/logic/core/TeamEvalResult.java
+++ src/main/java/teammates/logic/core/TeamEvalResult.java
@@ -249,7 +249,7 @@ public class TeamEvalResult {
         double[][] output = new double[input.length][input.length];
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < input[i].length; j++) {
-                output[i][j] = (i == j) ? NA : input[i][j];
+                output[i][j] = i == j ? NA : input[i][j];
             }
         }
         return output;
