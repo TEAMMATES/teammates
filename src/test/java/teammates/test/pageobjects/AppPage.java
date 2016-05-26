3be9c3e80034bb714381c74697b4fbd1bd17beaa
Yong Jie <KhooYongJie@gmx.com>
2016-05-26 11:56:30 +0800

    [5422] Misleading file extension of course enroll sample spreadsheet

--- src/test/java/teammates/test/pageobjects/AppPage.java
+++ src/test/java/teammates/test/pageobjects/AppPage.java
@@ -886,6 +886,14 @@ public abstract class AppPage {
     }
     
     /**
+     * Verifies that the title of the loaded page is the same as {@code expectedTitle}
+     * @param expectedTitle
+     */
+    public void verifyTitle(String expectedTitle) {
+        assertEquals(expectedTitle, browser.driver.getTitle());
+    }
+
+    /**
      * Also supports the expression "{*}" which will match any text.
      * e.g. "team 1{*}team 2" will match "team 1 xyz team 2"
      */
