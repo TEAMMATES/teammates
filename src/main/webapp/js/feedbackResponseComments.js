a51afb9e2d53b37edbb48766276924b1ae994acd
Yuan Bin <chowwars@hotmail.com>
2016-06-28 16:13:14 +0800

    fix bug in feedback response comments

--- src/main/webapp/js/feedbackResponseComments.js
+++ src/main/webapp/js/feedbackResponseComments.js
@@ -386,19 +386,13 @@ function feedbackResetVisibilityHiddenFields(form, table) {
     table.find('.answerCheckbox:checked').each(function() {
         visibilityOptions.push($(this).val());
     });
-    form.find("input[name='showcommentsto']").val(visibilityOptions.join(', '));
+    form.find("input[name='showresponsecommentsto']").val(visibilityOptions.join(', '));
     
     visibilityOptions = [];
     table.find('.giverCheckbox:checked').each(function() {
         visibilityOptions.push($(this).val());
     });
-    form.find("input[name='showgiverto']").val(visibilityOptions.join(', '));
-    
-    visibilityOptions = [];
-    table.find('.recipientCheckbox:checked').each(function() {
-        visibilityOptions.push($(this).val());
-    });
-    form.find("input[name='showrecipientto']").val(visibilityOptions.join(', '));
+    form.find("input[name='showresponsegiverto']").val(visibilityOptions.join(', '));
 }
 
 function showNewlyAddedResponseCommentEditForm(addedIndex) {
