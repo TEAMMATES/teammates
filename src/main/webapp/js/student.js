6e95da02d105947bd62188721654d3aa7a5899b9
YongJieYongJie <KhooYongJie@gmx.com>
2016-06-29 16:54:20 +0800

    Replace browser dialogs with modals in student.js | AWAITING GODMODE

--- src/main/webapp/js/student.js
+++ src/main/webapp/js/student.js
@@ -4,8 +4,17 @@
  */
 
 $(document).ready(function() {
-    $('[data-unreg].navLinks').click(function() {
-        return confirm('You have to register using a google account '
-                       + 'in order to access this page. Would you like to proceed and register?');
+    $('[data-unreg].navLinks').click(function(event) {
+        event.preventDefault();
+        $clickedLink = $(event.target);
+
+        var messageText = 'You have to register using a google account in order to access this page. Would '
+                          + 'you like to proceed and register?';
+        var okCallback = function() {
+            window.location = $clickedLink.attr('href');
+        };
+
+        BootboxWrapper.showModalConfirmation('Register for TEAMMATES', messageText, okCallback, null,
+                BootboxWrapper.DEFAULT_OK_TEXT, BootboxWrapper.DEFAULT_CANCEL_TEXT, StatusType.INFO);
     });
 });
