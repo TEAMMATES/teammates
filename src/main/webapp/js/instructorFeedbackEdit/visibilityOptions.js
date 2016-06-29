24e55d22597367d9046c1d6713caf18e63358e56
Wilson Kurniawan <wkurniawan.92@gmail.com>
2016-06-29 22:43:22 +0800

    removed "hide" element

--- src/main/webapp/js/instructorFeedbackEdit/visibilityOptions.js
+++ src/main/webapp/js/instructorFeedbackEdit/visibilityOptions.js
@@ -130,11 +130,38 @@ function disableRow($containingForm, row) {
     var $table = $editTab.find('table');
     var $tdElements = $($table.children().children()[row]).children();
     
-    if ($tdElements.parent().prop('tagName') === 'hide') {
+    if ($tdElements.parent().prop('tagName') === 'tr') {
         return;
     }
-    $tdElements.unwrap().wrapAll('<hide>');
+    $tdElements.unwrap().wrapAll('<tr>');
     $tdElements.parent().hide();
+}
+
+function feedbackRecipientUpdateVisibilityOptions(elem) {
+    var $elem = $(elem);
+    if (isRecipientsTeamMembersVisibilityOptionInvalidForRecipientType($elem.val())) {
+        // show the row Recipient(s) and hide the row Recipient's Team Members
+        enableRow($elem, 1);
+        disableRow($elem, 3);
+        return;
+    } else if ($elem.val() === 'NONE') {
+        // hide both the row Recipient(s) and the row Recipient's Team Members
+        disableRow($elem, 3);
+        disableRow($elem, 1);
+        return;
+    }
+    
+    enableRow($elem, 1);
+    enableRow($elem, 3);
+}
+
+function feedbackGiverUpdateVisibilityOptions(elem) {
+    var $elem = $(elem);
+    if ($elem.val() === 'INSTRUCTORS' || $elem.val() === 'TEAMS') {
+        disableRow($elem, 2);
+        return;
+    }
+    enableRow($elem, 2);
 }
 
 function updateEditTabAccordingToRecipient($containingForm) {
