584fae24cefd44c99720645b153fe2b2ffd2db75
Yuan Bin <chowwars@hotmail.com>
2016-07-14 12:34:21 +0800

    fix comments

--- src/main/java/teammates/logic/core/CommentsLogic.java
+++ src/main/java/teammates/logic/core/CommentsLogic.java
@@ -177,7 +177,7 @@ public class CommentsLogic {
     }
     
     /**
-     * update comment's giver email (assume to be an instructor)
+     * update comment's giver and last editor email (assume to be an instructor)
      * @param courseId
      * @param oldInstrEmail
      * @param updatedInstrEmail
