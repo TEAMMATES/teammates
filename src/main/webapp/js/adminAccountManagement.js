463d42cb14cb46bf0108ecdd226f63cda43bb7f0
Wilson Kurniawan <wkurniawan.92@gmail.com>
2016-11-02 14:49:07 +0800

    moved code only used once to the relevant file

--- src/main/webapp/js/adminAccountManagement.js
+++ src/main/webapp/js/adminAccountManagement.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
     updatePagination();
     showFirstPage();
     updateEntriesCount();
+    bindDeleteAccountAction();
 });
 
 function updatePagination() {
@@ -139,6 +140,27 @@ function goToNextPage() {
     showEntriesForSelectedPage();
     updateEntriesCount();
     updatePagination();
+}
+
+function bindDeleteAccountAction() {
+    $('.admin-delete-account-link').on('click', function(event) {
+        event.preventDefault();
+
+        var $clickedLink = $(event.target);
+        var googleId = $clickedLink.data('googleId');
+        var existingCourses = document.getElementById('courses_' + googleId).innerHTML;
+
+        var messageText = 'Are you sure you want to delete the account ' + googleId + '?'
+                          + '<br><br>' + existingCourses
+                          + '<br><br>This operation will delete ALL information about this account from the system.';
+
+        var okCallback = function() {
+            window.location = $clickedLink.attr('href');
+        };
+
+        BootboxWrapper.showModalConfirmation('Confirm deletion', messageText, okCallback, null,
+                BootboxWrapper.DEFAULT_OK_TEXT, BootboxWrapper.DEFAULT_CANCEL_TEXT, StatusType.DANGER);
+    });
 }
 
 $(document).keydown(function(e) {
