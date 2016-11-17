c2c9c9654f662636cbd101fdb13b58913cb67e16
sznicci <sznicci@gmail.com>
2016-11-17 15:15:12 +0000

    Add new time for feedback session submission email test case
    Added a new time for the instructor so the timestamp will not
    change it twice.
    Modified the time for the student and the instructor to UTC timezone.

--- src/test/java/teammates/test/cases/logic/EmailGeneratorTest.java
+++ src/test/java/teammates/test/cases/logic/EmailGeneratorTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -17,7 +18,6 @@ import teammates.common.datatransfer.UserType;
 import teammates.common.util.Config;
 import teammates.common.util.EmailType;
 import teammates.common.util.EmailWrapper;
-import teammates.common.util.TimeHelper;
 import teammates.logic.core.CoursesLogic;
 import teammates.logic.core.EmailGenerator;
 import teammates.logic.core.FeedbackSessionsLogic;
@@ -205,19 +205,24 @@ public class EmailGeneratorTest extends BaseComponentTestCase {
         
         ______TS("feedback session submission email");
 
-        Calendar time = Calendar.getInstance();
-        time.set(Calendar.DATE, 4);
-        time.set(Calendar.MONTH, 8);
-        time.set(Calendar.HOUR_OF_DAY, 5);
-        time.set(Calendar.MINUTE, 30);
+        Calendar timeForStudent = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        timeForStudent.set(Calendar.DATE, 4);
+        timeForStudent.set(Calendar.MONTH, 8);
+        timeForStudent.set(Calendar.HOUR_OF_DAY, 5);
+        timeForStudent.set(Calendar.MINUTE, 30);
         EmailWrapper email = new EmailGenerator().generateFeedbackSubmissionConfirmationEmailForStudent(
-                session, student1, TimeHelper.formatTime12H(time.getTime()));
+                session, student1, timeForStudent);
         subject = String.format(EmailType.FEEDBACK_SUBMISSION_CONFIRMATION.getSubject(), course.getName(),
                                 session.getFeedbackSessionName());
         verifyEmail(email, student1.email, subject, "/sessionSubmissionConfirmationEmailForStudent.html");
 
+        Calendar timeForInstructor = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        timeForInstructor.set(Calendar.DATE, 4);
+        timeForInstructor.set(Calendar.MONTH, 8);
+        timeForInstructor.set(Calendar.HOUR_OF_DAY, 5);
+        timeForInstructor.set(Calendar.MINUTE, 30);
         email = new EmailGenerator().generateFeedbackSubmissionConfirmationEmailForInstructor(session,
-                instructor1, TimeHelper.formatTime12H(time.getTime()));
+                instructor1, timeForInstructor);
         subject = String.format(EmailType.FEEDBACK_SUBMISSION_CONFIRMATION.getSubject(), course.getName(),
                                 session.getFeedbackSessionName());
         verifyEmail(email, instructor1.email, subject, "/sessionSubmissionConfirmationEmailForInstructor.html");
