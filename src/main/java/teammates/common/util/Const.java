4ff61e1ff984e699cd87f38f0b53907dc4015bab
sznicci <sznicci@gmail.com>
2016-12-07 13:01:32 +0000

    Change STUDENT_NATIONALITY value in Const.java
    Changed the const value because of inconsistency
    in StudentProfilePage.java

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -760,7 +760,7 @@ public final class Const {
         public static final String STUDENT_SHORT_NAME = "studentshortname";
         public static final String STUDENT_PROFILE_EMAIL = "studentprofileemail";
         public static final String STUDENT_PROFILE_INSTITUTION = "studentprofileinstitute";
-        public static final String STUDENT_NATIONALITY = "studentnationality";
+        public static final String STUDENT_NATIONALITY = "studentNationality";
         public static final String STUDENT_GENDER = "studentgender";
         public static final String STUDENT_PROFILE_MOREINFO = "studentprofilemoreinfo";
         public static final String STUDENT_PROFILE_PHOTO = "studentprofilephoto";
