3565f61bb3a06bb51e89e1e81d95f64837d1d8fe
Wilson Kurniawan <wkurniawan.92@gmail.com>
2016-12-12 23:15:26 +0800

    [6201] Test failures in enrollment result due to line endings in HTML attribute value

--- src/test/java/teammates/test/driver/HtmlHelper.java
+++ src/test/java/teammates/test/driver/HtmlHelper.java
@@ -85,7 +85,8 @@ public final class HtmlHelper {
     }
     
     private static boolean areSameHtmls(String expected, String actual) {
-        return expected.equals(actual);
+        // accounts for the variations in line breaks
+        return expected.replaceAll("[\r\n]", "").equals(actual.replaceAll("[\r\n]", ""));
     }
     
     /**
