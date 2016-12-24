2b336ec37890f5e52e6580325e2c37aaa85f52f7
Serhii Beliablia <belyabl9@gmail.com>
2016-12-24 16:25:19 +0200

    Add image upload support for rich-text editor (back-end part)

--- src/main/java/teammates/ui/controller/AdminEmailImageUploadAction.java
+++ src/main/java/teammates/ui/controller/AdminEmailImageUploadAction.java
@@ -1,47 +1,38 @@
 package teammates.ui.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import teammates.common.util.Config;
 import teammates.common.util.Const;
 import teammates.logic.api.GateKeeper;
 
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.BlobstoreFailureException;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 
-public class AdminEmailImageUploadAction extends Action {
+public class AdminEmailImageUploadAction extends ImageUploadAction {
     
     AdminEmailComposePageData data;
     
     @Override
     protected ActionResult execute() {
-        
         GateKeeper.inst().verifyAdminPrivileges(account);
-       
+
         data = new AdminEmailComposePageData(account);
-        BlobInfo blobInfo = extractImageKey();
-        
+        BlobInfo blobInfo = extractImageKey(Const.ParamsNames.ADMIN_EMAIL_IMAGE_TO_UPLOAD);
+
         if (blobInfo == null) {
             data.isFileUploaded = false;
             data.fileSrcUrl = null;
-            log.info("Image Upload Failed");
+            log.warning("Image Upload Failed");
             statusToAdmin = "Image Upload Failed";
-            
+
             return createAjaxResult(data);
         }
-        
-        
+
         BlobKey blobKey = blobInfo.getBlobKey();
-        
-      
+
         data.isFileUploaded = true;
-        data.fileSrcUrl = Const.ActionURIs.PUBLIC_EMAIL_FILE_SERVE + "?blob-key="
-                + blobKey.getKeyString();
+        data.fileSrcUrl = Const.ActionURIs.PUBLIC_EMAIL_FILE_SERVE + "?blob-key=" + blobKey.getKeyString();
         String absoluteFileSrcUrl = Config.getAppUrl(data.fileSrcUrl).toAbsoluteString();
-        
+
         log.info("New Image Uploaded : " + absoluteFileSrcUrl);
         statusToAdmin = "New Image Uploaded : " + "<a href="
                 + data.fileSrcUrl + " target=blank>" + absoluteFileSrcUrl + "</a>";
@@ -49,55 +40,8 @@ public class AdminEmailImageUploadAction extends Action {
 
         return createAjaxResult(data);
     }
-
-    private BlobInfo extractImageKey() {
-        try {
-            Map<String, List<BlobInfo>> blobsMap = BlobstoreServiceFactory.getBlobstoreService().getBlobInfos(request);
-            List<BlobInfo> blobs = blobsMap.get(Const.ParamsNames.ADMIN_EMAIL_IMAGE_TO_UPLOAD);
-            
-            if (blobs == null || blobs.isEmpty()) {
-                data.ajaxStatus = Const.StatusMessages.NO_IMAGE_GIVEN;
-                isError = true;
-                return null;
-            }
-            
-            BlobInfo image = blobs.get(0);
-            return validateImage(image);
-        } catch (IllegalStateException e) {
-            return null;
-        }
-    }
-
-    private BlobInfo validateImage(BlobInfo image) {
-        if (image.getSize() > Const.SystemParams.MAX_PROFILE_PIC_SIZE) {
-            deleteImage(image.getBlobKey());
-            isError = true;
-            data.ajaxStatus = Const.StatusMessages.IMAGE_TOO_LARGE;
-            return null;
-        } else if (!image.getContentType().contains("image/")) {
-            deleteImage(image.getBlobKey());
-            isError = true;
-            data.ajaxStatus = Const.StatusMessages.FILE_NOT_A_PICTURE;
-            return null;
-        }
-           
-        return image;
-    }
     
-    private void deleteImage(BlobKey blobKey) {
-        if (blobKey.equals(new BlobKey(""))) {
-            return;
-        }
-        
-        try {
-            logic.deleteAdminEmailUploadedFile(blobKey);
-        } catch (BlobstoreFailureException bfe) {
-            statusToAdmin = Const.ACTION_RESULT_FAILURE
-                    + " : Unable to delete picture (possible unused picture with key: "
-                    + blobKey.getKeyString()
-                    + " || Error Message: "
-                    + bfe.getMessage() + Const.EOL;
-        }
+    protected void deleteUploadedFile(BlobKey blobKey) {
+        logic.deleteAdminEmailUploadedFile(blobKey);
     }
-    
 }
---
+++ src/main/java/teammates/ui/controller/FileUploadPageData.java
@@ -0,0 +1,14 @@
+package teammates.ui.controller;
+
+import teammates.common.datatransfer.AccountAttributes;
+
+public class FileUploadPageData extends PageData {
+    public boolean isFileUploaded;
+    public String fileSrcUrl;
+    public String ajaxStatus;
+
+    public FileUploadPageData(AccountAttributes account) {
+        super(account);
+    }
+
+}
