a2d9de57d6ebfdcc3335a62b229afeeaf87af7a3
tarun <tarun14110@iiitd.ac.in>
2017-01-07 05:12:05 +0530

    added year to the session time tests

--- src/test/java/teammates/test/cases/logic/EmailGeneratorTest.java
+++ src/test/java/teammates/test/cases/logic/EmailGeneratorTest.java
@@ -226,6 +226,7 @@ public class EmailGeneratorTest extends BaseComponentTestCase {
         time.set(Calendar.MONTH, 8);
         time.set(Calendar.HOUR_OF_DAY, 5);
         time.set(Calendar.MINUTE, 30);
+        time.set(Calendar.YEAR, 2016);
         email = new EmailGenerator()
                 .generateFeedbackSubmissionConfirmationEmailForStudent(session, student1, time);
         subject = String.format(EmailType.FEEDBACK_SUBMISSION_CONFIRMATION.getSubject(), course.getName(),
