aae61fed69dd6f5f115b7b5085af5e3a6c01277b
Wilson Kurniawan <wkurniawan.92@gmail.com>
2017-01-06 17:02:17 +0800

    improve the automated actions tests involving emails
    Tests that the email subjects are accurate, and in some cases, the recipients are accurate.

--- src/test/java/teammates/test/cases/automated/FeedbackSessionOpeningRemindersActionTest.java
+++ src/test/java/teammates/test/cases/automated/FeedbackSessionOpeningRemindersActionTest.java
@@ -1,11 +1,18 @@
 package teammates.test.cases.automated;
 
+import java.util.List;
+import java.util.Map;
+
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.FeedbackSessionAttributes;
 import teammates.common.util.Const;
+import teammates.common.util.Const.ParamsNames;
+import teammates.common.util.EmailType;
+import teammates.common.util.TaskWrapper;
 import teammates.common.util.TimeHelper;
+import teammates.logic.core.CoursesLogic;
 import teammates.logic.core.FeedbackSessionsLogic;
 import teammates.ui.automated.FeedbackSessionOpeningRemindersAction;
 
@@ -14,6 +21,7 @@ import teammates.ui.automated.FeedbackSessionOpeningRemindersAction;
  */
 public class FeedbackSessionOpeningRemindersActionTest extends BaseAutomatedActionTest {
     
+    private static final CoursesLogic coursesLogic = CoursesLogic.inst();
     private static final FeedbackSessionsLogic fsLogic = FeedbackSessionsLogic.inst();
     private static final DataBundle dataBundle = getTypicalDataBundle();
     
@@ -58,6 +66,21 @@ public class FeedbackSessionOpeningRemindersActionTest extends BaseAutomatedActi
         
         // 5 students and 5 instructors in course1
         verifySpecifiedTasksAdded(action, Const.TaskQueue.SEND_EMAIL_QUEUE_NAME, 20);
+        
+        String courseName = coursesLogic.getCourse(session1.getCourseId()).getName();
+        List<TaskWrapper> tasksAdded = action.getTaskQueuer().getTasksAdded();
+        for (TaskWrapper task : tasksAdded) {
+            Map<String, String[]> paramMap = task.getParamMap();
+            try {
+                assertEquals(String.format(EmailType.FEEDBACK_OPENING.getSubject(), courseName,
+                                           session1.getSessionName()),
+                             paramMap.get(ParamsNames.EMAIL_SUBJECT)[0]);
+            } catch (AssertionError ae) {
+                assertEquals(String.format(EmailType.FEEDBACK_OPENING.getSubject(), courseName,
+                                           session2.getSessionName()),
+                             paramMap.get(ParamsNames.EMAIL_SUBJECT)[0]);
+            }
+        }
         
         ______TS("2 sessions opened with emails sent");
         
--- src/test/java/teammates/test/cases/automated/FeedbackSessionRemindParticularUsersEmailWorkerActionTest.java
+++ src/test/java/teammates/test/cases/automated/FeedbackSessionRemindParticularUsersEmailWorkerActionTest.java
@@ -1,5 +1,8 @@
 package teammates.test.cases.automated;
 
+import java.util.List;
+import java.util.Map;
+
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.DataBundle;
@@ -8,6 +11,9 @@ import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.Const.ParamsNames;
+import teammates.common.util.EmailType;
+import teammates.common.util.TaskWrapper;
+import teammates.logic.core.CoursesLogic;
 import teammates.ui.automated.FeedbackSessionRemindParticularUsersEmailWorkerAction;
 
 /**
@@ -15,6 +21,7 @@ import teammates.ui.automated.FeedbackSessionRemindParticularUsersEmailWorkerAct
  */
 public class FeedbackSessionRemindParticularUsersEmailWorkerActionTest extends BaseAutomatedActionTest {
     
+    private static final CoursesLogic coursesLogic = CoursesLogic.inst();
     private static final DataBundle dataBundle = getTypicalDataBundle();
     
     @Override
@@ -42,8 +49,19 @@ public class FeedbackSessionRemindParticularUsersEmailWorkerActionTest extends B
         FeedbackSessionRemindParticularUsersEmailWorkerAction action = getAction(submissionParams);
         action.execute();
         
+        // send 2 emails as specified in the submission parameters
         verifySpecifiedTasksAdded(action, Const.TaskQueue.SEND_EMAIL_QUEUE_NAME, 2);
         
+        String courseName = coursesLogic.getCourse(session1.getCourseId()).getName();
+        List<TaskWrapper> tasksAdded = action.getTaskQueuer().getTasksAdded();
+        for (TaskWrapper task : tasksAdded) {
+            Map<String, String[]> paramMap = task.getParamMap();
+            assertEquals(String.format(EmailType.FEEDBACK_SESSION_REMINDER.getSubject(), courseName,
+                                       session1.getSessionName()),
+                         paramMap.get(ParamsNames.EMAIL_SUBJECT)[0]);
+            String recipient = paramMap.get(ParamsNames.EMAIL_RECEIVER)[0];
+            assertTrue(recipient.equals(student1.email) || recipient.equals(instructor1.email));
+        }
     }
     
     @Override
