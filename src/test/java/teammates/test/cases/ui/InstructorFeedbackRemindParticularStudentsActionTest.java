45439fb7df04984442d83e95c944690cfda8cbe1
Pu Xiao <xp.xiaopu@gmail.com>
2017-01-13 19:37:20 +0800

    add InstructorFeedbackRemindParticularStudentsActionTest

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackRemindParticularStudentsActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackRemindParticularStudentsActionTest.java
@@ -3,21 +3,78 @@ package teammates.test.cases.ui;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.FeedbackSessionAttributes;
+import teammates.common.datatransfer.InstructorAttributes;
+import teammates.common.datatransfer.StudentAttributes;
 import teammates.common.util.Const;
+import teammates.common.util.Const.TaskQueue;
+import teammates.ui.controller.InstructorFeedbackRemindParticularStudentsAction;
+import teammates.ui.controller.RedirectResult;
 
 public class InstructorFeedbackRemindParticularStudentsActionTest extends BaseActionTest {
+    private static final DataBundle dataBundle = getTypicalDataBundle();
     
     @BeforeClass
-    public static void classSetUp() {
+    public static void classSetUp() throws Exception {
         printTestClassHeader();
+        removeAndRestoreTypicalDataInDatastore();
         uri = Const.ActionURIs.INSTRUCTOR_FEEDBACK_REMIND_PARTICULAR_STUDENTS;
     }
     
     @Test
     public void testExecuteAndPostProcess() {
-        // This test case was omitted as the action is executed in the background task queue
-        // so we cannot determine its result.
+        InstructorAttributes instructor1ofCourse1 = dataBundle.instructors.get("instructor1OfCourse1");
+        FeedbackSessionAttributes fs = dataBundle.feedbackSessions.get("session1InCourse1");
+        StudentAttributes studentNotSubmitFeedback = dataBundle.students.get("student5InCourse1");
+        InstructorFeedbackRemindParticularStudentsAction action;
+        RedirectResult rr;
         
-        // The logic under the action is covered with test case to ensure the action works.
+        ______TS("Unsuccessful case: Not ownwed Course/Modify Permission, authethication failure");
+        String[] paramsNotOwnedCourse = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getSessionName(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail()
+        };
+        verifyUnaccessibleForInstructorsOfOtherCourses(paramsNotOwnedCourse);
+        verifyUnaccessibleWithoutModifySessionPrivilege(paramsNotOwnedCourse);
+        
+        gaeSimulation.loginAsInstructor(instructor1ofCourse1.googleId);
+        
+        ______TS("Unsuccessful case: No Parameter");
+        verifyAssumptionFailure();
+        
+        ______TS("Unsuccessful case: No UserToRemind, Warning Message generated");
+        
+        String[] paramsNoUserToRemind = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getSessionName(),
+        };
+        
+        action = getAction(paramsNoUserToRemind);
+        
+        rr = (RedirectResult) action.executeAndPostProcess();
+        assertTrue(rr.getStatusMessage().contains(Const.StatusMessages.FEEDBACK_SESSION_REMINDERSEMPTYRECIPIENT));
+        
+        ______TS("Successful case: Typical case");
+        
+        String[] paramsTypical = new String[]{
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getSessionName(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail()
+        };
+        
+        action = getAction(paramsTypical);
+        
+        rr = (RedirectResult) action.executeAndPostProcess();
+        assertTrue(rr.getStatusMessage().contains(Const.StatusMessages.FEEDBACK_SESSION_REMINDERSSENT));
+        
+        verifySpecifiedTasksAdded(action,
+                TaskQueue.FEEDBACK_SESSION_REMIND_PARTICULAR_USERS_EMAIL_QUEUE_NAME, 1);
+        
+    }
+    
+    private InstructorFeedbackRemindParticularStudentsAction getAction(String... params) {
+        return (InstructorFeedbackRemindParticularStudentsAction) gaeSimulation.getActionObject(uri, params);
     }
 }
