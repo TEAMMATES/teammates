2652fac40d5372e11186781b6fc82230b9425eb1
Pu Xiao <xp.xiaopu@gmail.com>
2017-01-13 19:46:16 +0800

    add InstructorFeedbackRemindActionTest

--- src/test/java/teammates/test/cases/ui/InstructorFeedbackRemindActionTest.java
+++ src/test/java/teammates/test/cases/ui/InstructorFeedbackRemindActionTest.java
@@ -3,21 +3,70 @@ package teammates.test.cases.ui;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.FeedbackSessionAttributes;
+import teammates.common.datatransfer.InstructorAttributes;
 import teammates.common.util.Const;
+import teammates.common.util.Const.TaskQueue;
+import teammates.ui.controller.InstructorFeedbackRemindAction;
+import teammates.ui.controller.RedirectResult;
 
 public class InstructorFeedbackRemindActionTest extends BaseActionTest {
+    private static final DataBundle dataBundle = getTypicalDataBundle();
     
     @BeforeClass
-    public static void classSetUp() {
+    public static void classSetUp() throws Exception {
         printTestClassHeader();
+        removeAndRestoreTypicalDataInDatastore();
         uri = Const.ActionURIs.INSTRUCTOR_FEEDBACK_REMIND;
     }
     
     @Test
     public void testExecuteAndPostProcess() {
-        // This test case was omitted as the action is executed in the background task queue
-        // so we cannot determine its result.
-          
-        // The logic under the action is covered with test case to ensure the action works.
+        InstructorAttributes instructor1ofCourse1 = dataBundle.instructors.get("instructor1OfCourse1");
+        FeedbackSessionAttributes fs = dataBundle.feedbackSessions.get("session1InCourse1");
+        InstructorFeedbackRemindAction action;
+        RedirectResult rr;
+        
+        ______TS("Unsuccessful case: Not ownwed Course/Modify Permission, authethication failure");
+        String[] paramsNotOwnedCourse = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getSessionName(),
+        };
+        verifyUnaccessibleForInstructorsOfOtherCourses(paramsNotOwnedCourse);
+        verifyUnaccessibleWithoutModifySessionPrivilege(paramsNotOwnedCourse);
+        
+        gaeSimulation.loginAsInstructor(instructor1ofCourse1.googleId);
+        
+        ______TS("Unsuccessful case: Not enough Parameter");
+        verifyAssumptionFailure();
+        String[] paramsNoCourseId = new String[] {
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getSessionName(),
+        };
+        verifyAssumptionFailure(paramsNoCourseId);
+        String[] paramsNoFeedback = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId()
+        };
+        verifyAssumptionFailure(paramsNoFeedback);
+        
+        ______TS("Successful case: Typical case");
+        
+        String[] paramsTypical = new String[]{
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getSessionName(),
+        };
+        
+        action = getAction(paramsTypical);
+        
+        rr = (RedirectResult) action.executeAndPostProcess();
+        assertTrue(rr.getStatusMessage().contains(Const.StatusMessages.FEEDBACK_SESSION_REMINDERSSENT));
+        
+        verifySpecifiedTasksAdded(action,
+                TaskQueue.FEEDBACK_SESSION_REMIND_EMAIL_QUEUE_NAME, 1);
+        
+    }
+    
+    private InstructorFeedbackRemindAction getAction(String... params) {
+        return (InstructorFeedbackRemindAction) gaeSimulation.getActionObject(uri, params);
     }
 }
