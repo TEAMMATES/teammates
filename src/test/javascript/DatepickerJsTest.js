37f92975d57005fcd3321712360e7ad643a5ae82
nsaiisasidhar <sasinimmagadda@gmail.com>
2017-01-13 19:50:30 -0500

    Update DatepickerJsTest.js

--- src/test/javascript/DatepickerJsTest.js
+++ src/test/javascript/DatepickerJsTest.js
@@ -1,10 +1,10 @@
-QUnit.module('datePicker.js');
+QUnit.module('datepicker.js');
 
 QUnit.test('getMinDateForEndDate(startDate)', function(assert) {
-    assert.deepEqual(getMinDateForEndDate(newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Return date given as input argument');
+    assert.deepEqual(getMinDateForEndDate(newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Return startDate input argument');
 
 QUnit.test('getMaxDateForStartDate(endDate)', function(assert) {
-    assert.deepEqual(getMaxDateForStartDate(newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Return date given as input argument');
+    assert.deepEqual(getMaxDateForStartDate(newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Return endDate input argument');
 
 QUnit.test('getMaxDateForVisibleDate(startDate, publishDate)', function(assert) {
     assert.deepEqual(getMaxDateForVisibleDate(newDate("02 Apr 2012, 23:59"), null), newDate("02 Apr 2012, 23:59"), 'NULL value test');
@@ -14,5 +14,5 @@ QUnit.test('getMaxDateForVisibleDate(startDate, publishDate)', function(assert) 
     assert.deepEqual(getMaxDateForVisibleDate(newDate("02 Apr 2012, 23:59"), newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Test for startDate = publishDate');
     
 QUnit.test('getMinDateForPublishDate(visibleDate)', function(assert) {
-    assert.deepEqual(getMinDateForPublishDate(newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Return date given as input argument');
+    assert.deepEqual(getMinDateForPublishDate(newDate("02 Apr 2012, 23:59")), newDate("02 Apr 2012, 23:59"), 'Return visibleDate input argument');
     
