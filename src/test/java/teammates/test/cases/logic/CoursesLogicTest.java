433030f06c02c77a58da7469709c50a83b5ab8ba
boxin-yang <a0107354@u.nus.edu>
2017-01-18 20:09:24 +0800

    remove unused import after deleting methods and tests

--- src/test/java/teammates/test/cases/logic/CoursesLogicTest.java
+++ src/test/java/teammates/test/cases/logic/CoursesLogicTest.java
@@ -2,10 +2,8 @@ package teammates.test.cases.logic;
 
 import static teammates.common.util.Const.EOL;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.BeforeClass;
