9a1bae10c49dd880f7ed56a5c0f4b300deffb644
Alexander Levy <alexander.levy3@gmail.com>
2017-01-21 14:56:03 +0100

    typo

--- src/test/java/teammates/test/cases/ui/browsertests/AppPageUiTest.java
+++ src/test/java/teammates/test/cases/ui/browsertests/AppPageUiTest.java
@@ -18,7 +18,7 @@ public class AppPageUiTest extends BaseUiTestCase {
     public void classSetup() throws Exception {
         printTestClassHeader();
         browser = BrowserPool.getBrowser();
-        page = AppPage.getNewPageInstance(browser).navigateTo(createLocalUrl("/VerifyTablePatternTestPage.html"));
+        page = AppPage.getNewPageInstance(browser).navigateTo(createLocalUrl("/appVerifyTablePatternTestPage.html"));
     }
     
     @Test
