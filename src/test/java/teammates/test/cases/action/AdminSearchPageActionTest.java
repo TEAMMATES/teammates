c81aff169eb464f59f3b642faef499596642fe67
Wilson Kurniawan <wkurniawan.92@gmail.com>
2017-01-22 21:17:13 +0800

    [#6360] Reorganize parts of the test package (#6384)

---
+++ src/test/java/teammates/test/cases/action/AdminSearchPageActionTest.java
@@ -0,0 +1,19 @@
+package teammates.test.cases.action;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AdminSearchPageActionTest extends BaseActionTest {
+    
+    @BeforeClass
+    public void classSetup() {
+        printTestClassHeader();
+    }
+    
+    @Test
+    public void testExecuteAndPostProcess() {
+        // Test is done as a browser test,
+        // because otherwise there are problems when rebuilding the document
+    }
+    
+}
