972224be5bf87b8479e24d2743be78207175e77b
Li Kai <falconets25601@gmail.com>
2017-02-02 14:10:47 +0800

    Add minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
-var path = require('path');
+const path = require('path');
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/main/webapp/js/index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build/exploded-app/js/')
-  }
+    entry: path.resolve(__dirname, 'src/main/webapp/js/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build/exploded-app/js/')
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 };
