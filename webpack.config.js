0034b13cfec88a5c7f75d24c20c10fb3e1654b4b
Li Kai <falconets25601@gmail.com>
2017-02-03 23:53:29 +0800

    Parse subdirectories

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
-const glob = require("glob");
+const glob = require('glob');
 const webpack = require('webpack');
 
+const ROOT = './src/main/webapp/js';
+const BUILD = 'build/exploded-app/js';
+
 const entry = {};
-glob.sync("./src/main/webapp/js/*.js").forEach((file) => {
-    const filename = path.basename(file, '.js');
-    entry[filename] = file
+const files = glob.sync(`${ROOT}/**/*.js`, {
+    ignore: `${ROOT}/lib/**`    // ignore /lib/
+});
+files.forEach((file) => {
+    const { dir, name } = path.parse(file);
+    const filePath = path.join(dir.replace(ROOT, ''), name);
+    entry[filePath] = file;
 });
 
 module.exports = {
     entry,
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'build/exploded-app/js/')
+        path: path.resolve(__dirname, BUILD)
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin()
     ],
-    stats: "errors-only"
+    stats: 'errors-only'
 };
