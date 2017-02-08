672aafd738e07eea8bec7cdb4a8a18a9cb226c97
Pu Xiao <xp.xiaopu@gmail.com>
2017-02-09 00:12:43 +0800

    change js file line warpping indentation.

--- src/main/webapp/js/instructorStudentList.js
+++ src/main/webapp/js/instructorStudentList.js
@@ -45,8 +45,8 @@ $(document).ready(function() {
         clearTimeout(copyEmailPopoverTimeout);
         var $copyEmailButton = $(this);
         var tips = 'Emails now are copied. If it doesn\'t work, you can also use <kbd>Ctrl + C</kbd> to COPY.<br>'
-                 + 'You may use <kbd>Ctrl + V</kbd> to PASTE to your email client. <br>'
-                 + '<small class="text-muted">This message will disappear in 10 seconds</small>';
+                   + 'You may use <kbd>Ctrl + V</kbd> to PASTE to your email client. <br>'
+                   + '<small class="text-muted">This message will disappear in 10 seconds</small>';
         
         $copyEmailButton
             .popover('destroy')
