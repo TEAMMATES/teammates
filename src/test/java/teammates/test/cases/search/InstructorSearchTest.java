c635afe61ab18f6e58a2eac67941fe722cdfcd7c
Neurrone <neurrone@users.noreply.github.com>
2017-02-09 13:16:42 +0800

    add new test case class

---
+++ src/test/java/teammates/test/cases/search/InstructorSearchTest.java
@@ -0,0 +1,20 @@
+package teammates.test.cases.search;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.StudentSearchResultBundle;
+import teammates.storage.api.InstructorsDb;
+import teammates.storage.search.InstructorSearchDocument;
+import teammates.storage.search.InstructorSearchQuery;
+import teammates.test.driver.AssertHelper;
+
+/**
+ * SUT: {@link InstructorsDb},
+ *      {@link InstructorSearchDocument},
+ *      {@link InstructorSearchQuery}.
+ */
+public class InstructorSearchTest extends BaseSearchTest {
+    }
