43f7996ad5a484ee64ea6240f71802a615a403f8
Pu Xiao <xp.xiaopu@gmail.com>
2017-02-14 10:10:32 +0800

    add more rigid verification for comment search result

--- src/test/java/teammates/test/cases/search/CommentSearchTest.java
+++ src/test/java/teammates/test/cases/search/CommentSearchTest.java
@@ -41,12 +41,10 @@ public class CommentSearchTest extends BaseSearchTest {
         
         CommentSearchResultBundle bundle = commentsDb.search("non-existent", Arrays.asList(ins1InCourse1));
         
-        assertEquals(0, bundle.numberOfResults);
         assertEmptyBundle(bundle);
         
         bundle = commentsDb.search("", Arrays.asList(ins1InCourse1));
         
-        assertEquals(0, bundle.numberOfResults);
         assertEmptyBundle(bundle);
         
         ______TS("success: search for comments; query string matches some comments");
@@ -54,83 +52,63 @@ public class CommentSearchTest extends BaseSearchTest {
         // comment content
         bundle = commentsDb.search("\"Comment 1\"", Arrays.asList(ins1InCourse1));
         
-        assertEquals(1, bundle.numberOfResults);
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI1C1toS1C1),
-                     bundle.giverCommentTable.get(ins1InCourse1.email + ins1InCourse1.courseId));
+        List<CommentAttributes> expectedComments = Arrays.asList(comment1FromI1C1toS1C1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse1));
         
-        // giver email
+        // giver's email
         bundle = commentsDb.search("instructor1@course1.tmt", Arrays.asList(ins1InCourse1, ins1InCourse2));
         
-        assertEquals(5, bundle.numberOfResults);
-        assertSameCommentContentIgnoreOrder(
-                Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
-                        comment1FromI1C1toSE1C1, comment1FromI1C1toC1),
-                bundle.giverCommentTable.get(ins1InCourse1.email + ins1InCourse1.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
+                comment1FromI1C1toSE1C1, comment1FromI1C1toC1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse1));
         
-        // recipients email
+        // recipient's email
         bundle = commentsDb.search("student1InCourse1@gmail.tmt", Arrays.asList(ins1InCourse1, ins1InCourse2));
         
-        assertEquals(4, bundle.numberOfResults);
-        assertEquals(1, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(
-                 Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
-                         comment1FromI1C1toSE1C1), // recipients also in the section and team
-                 bundle.giverCommentTable.get(ins1InCourse1.email + ins1InCourse1.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
+                comment1FromI1C1toSE1C1); // recipients also in the section and team
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse1));
         
-        // recipients name
+        // recipient's name
         bundle = commentsDb.search("student1", Arrays.asList(ins3InCourse1, ins1InCourse2));
         
-        assertEquals(1, bundle.numberOfResults);
-        assertEquals(1, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI1C2toS1C2),
-                 bundle.giverCommentTable.get(ins1InCourse2.email + ins1InCourse2.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C2toS1C2);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse2));
         
-        // recipients team
+        // recipient's team
         bundle = commentsDb.search("\"Team 1.1\"", Arrays.asList(ins1InCourse1, ins1InCourse2));
         
-        assertEquals(1, bundle.numberOfResults);
-        assertEquals(1, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI1C1toT11C1),
-                 bundle.giverCommentTable.get(ins1InCourse1.email + ins1InCourse1.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C1toT11C1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse1));
         
-        // recipients section
+        // recipient's section
         bundle = commentsDb.search("\"Section 1\"", Arrays.asList(ins1InCourse1, ins1InCourse2));
         
-        assertEquals(4, bundle.numberOfResults);
-        assertEquals(1, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(
-                 Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
-                         comment1FromI1C1toSE1C1),
-                 bundle.giverCommentTable.get(ins1InCourse1.email + ins1InCourse1.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
+                comment1FromI1C1toSE1C1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse1));
                 
         // course id
         bundle = commentsDb.search("idOfTypicalCourse2", Arrays.asList(ins1InCourse1, ins1InCourse2));
         
-        assertEquals(1, bundle.numberOfResults);
-        assertEquals(1, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI1C2toS1C2),
-                 bundle.giverCommentTable.get(ins1InCourse2.email + ins1InCourse2.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C2toS1C2);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse2));
         
         // course name
         bundle = commentsDb.search("Course", Arrays.asList(ins1InCourse2, ins3InCourse1));
         
-        assertEquals(3, bundle.numberOfResults);
-        assertEquals(2, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI1C2toS1C2),
-                 bundle.giverCommentTable.get(ins1InCourse2.email + ins1InCourse2.courseId));
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI3C1toS2C1, comment1FromI3C1toC1),
-                 bundle.giverCommentTable.get(ins3InCourse1.email + ins3InCourse1.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C2toS1C2);
+        List<CommentAttributes> expectedCommentsForI3 = Arrays.asList(comment1FromI3C1toS2C1, comment1FromI3C1toC1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments, expectedCommentsForI3),
+                Arrays.asList(ins1InCourse2, ins3InCourse1));
         
         ______TS("success: search for comments; query string should be case-insensitive");
 
         bundle = commentsDb.search("sTuDeNt1INCourSe1@gMaIl.Tmt", Arrays.asList(ins1InCourse1, ins1InCourse2));
         
-        assertEquals(4, bundle.numberOfResults);
-        assertEquals(1, bundle.giverCommentTable.size());
-        assertSameCommentContentIgnoreOrder(
-                 Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
-                         comment1FromI1C1toSE1C1),
-                 bundle.giverCommentTable.get(ins1InCourse1.email + ins1InCourse1.courseId));
+        expectedComments = Arrays.asList(comment1FromI1C1toS1C1, comment2FromI1C1toS1C1, comment1FromI1C1toT11C1,
+                comment1FromI1C1toSE1C1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins1InCourse1));
 
         ______TS("success: search for comments; query string matches some comments based on comment visibility");
         
@@ -140,45 +118,66 @@ public class CommentSearchTest extends BaseSearchTest {
 
         // The above cases are tested in the previous test case
         
-        // instructors beside the giver can view the comment due the visibility setting
+        // instructors beside the giver can view the comment due to the visibility setting
         bundle = commentsDb.search("idOfTypicalCourse2", Arrays.asList(ins2InCourse2));
         
+        String giverAsKey = "Anonymous" + ins2InCourse2.courseId;
         assertEquals(1, bundle.numberOfResults);
         assertEquals(1, bundle.giverCommentTable.size());
         assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI1C2toS1C2),
-                 bundle.giverCommentTable.get("AnonymousidOfTypicalCourse2"));
-        assertEquals(1, bundle.giverTable.size());
-        assertEquals("Anonymous (idOfTypicalCourse2)", bundle.giverTable.get("AnonymousidOfTypicalCourse2"));
-
+                 bundle.giverCommentTable.get(giverAsKey));
+        assertEquals("Anonymous" + " (" + ins2InCourse2.courseId + ")", bundle.giverTable.get(giverAsKey));
+        
         ______TS("success: search for comments; deleted comment no longer searchable");
         
         commentsDb.deleteCommentsByInstructorEmail(ins1InCourse1.courseId, ins1InCourse1.email);
         
         bundle = commentsDb.search("comment", Arrays.asList(ins1InCourse1));
         
-        assertEquals(0, bundle.numberOfResults);
         assertEmptyBundle(bundle);
         
-        ______TS("success: search for comments; deleted comment without deleted comment: the document "
+        ______TS("success: search for comments; deleted comment without deleting document: the document "
                  + "will be deleted during the search");
         
         commentsDb.deleteEntity(comment1FromI3C1toS2C1);
         
         bundle = commentsDb.search("instructor3@course1.tmt", Arrays.asList(ins3InCourse1));
-        
-        assertEquals(1, bundle.numberOfResults);
-        assertSameCommentContentIgnoreOrder(Arrays.asList(comment1FromI3C1toC1),
-                bundle.giverCommentTable.get(ins3InCourse1.email + ins3InCourse1.courseId));
+            
+        expectedComments = Arrays.asList(comment1FromI3C1toC1);
+        assertHasOwnComments(bundle, Arrays.asList(expectedComments), Arrays.asList(ins3InCourse1));
     }
 
     private void assertEmptyBundle(CommentSearchResultBundle bundle) {
+        assertEquals(0, bundle.numberOfResults);
         assertTrue(bundle.giverCommentTable.isEmpty());
         assertTrue(bundle.giverTable.isEmpty());
         assertTrue(bundle.recipientTable.isEmpty());
     }
     
+    private void assertHasOwnComments(CommentSearchResultBundle bundle, List<List<CommentAttributes>> commentsList,
+            List<InstructorAttributes> instructorsList) {
+        assertEquals(commentsList.size(), instructorsList.size()); // every instructor has one comment List
+        int totalComments = 0;
+        for (int i = 0; i < commentsList.size(); i++) {
+            List<CommentAttributes> comments = commentsList.get(i);
+            InstructorAttributes instructor = instructorsList.get(i);
+            String giverAsKey = instructor.email + instructor.courseId;
+            
+            assertSameCommentContentIgnoreOrder(comments, bundle.giverCommentTable.get(giverAsKey));
+            assertEquals(bundle.giverTable.get(giverAsKey), "You (" + instructor.courseId + ")");
+            
+            // Not testable for bundle.recipientTable as the key is comment id
+            
+            totalComments += comments.size();
+        }
+        
+        assertEquals(totalComments, bundle.numberOfResults);
+    }
+    
     private void assertSameCommentContentIgnoreOrder(List<CommentAttributes> commentsListA,
             List<CommentAttributes> commentListB) {
+        // as comment content is unique for every comment in test data,
+        // we can assume same contents means same comments.
         AssertHelper.assertSameContentIgnoreOrder(getCommentContentAsList(commentsListA),
                 getCommentContentAsList(commentListB));
     }
