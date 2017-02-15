4715e2d99c3ced7c9684877a892ff0cb53651508
Wilson Kurniawan <wkurniawan.92@gmail.com>
2017-02-15 16:10:31 +0800

    remove unused function in index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -16,13 +16,6 @@ var TESTIMONIALS = [
 var LOOP_INTERVAL = '5000'; // in milliseconds
 var CURRENT_TESTIMONIAL = 0;
 
-function instructorLogin() {
-    // send request
-    requestInstructorLogin();
-    // handle response
-    handleInstructorLogin();
-}
-
 function submissionCounter(currentDate, baseDate, submissionPerHour, baseCount) {
     var errorMsg = 'Thousands of';
     if (!currentDate || !baseDate) {
