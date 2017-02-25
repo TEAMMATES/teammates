9ed7b7044fa33692bc348bfe0cdea5ca26a80345
Anagh Hegde <h.v.anagh@gmail.com>
2017-02-25 16:17:35 +0530

    resolve merge conflicts

--- src/main/java/teammates/ui/controller/InstructorFeedbacksPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbacksPageAction.java
@@ -1,6 +1,9 @@
 package teammates.ui.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +15,8 @@ import teammates.common.util.StatusMessage;
 import teammates.common.util.StatusMessageColor;
 import teammates.ui.pagedata.InstructorFeedbacksPageData;
 
-public class InstructorFeedbacksPageAction extends InstructorFeedbackAbstractAction {
-    
+public class InstructorFeedbacksPageAction extends Action {
+
     @Override
     protected ActionResult execute() {
         // This can be null. Non-null value indicates the page is being loaded
@@ -21,9 +24,9 @@ public class InstructorFeedbacksPageAction extends InstructorFeedbackAbstractAct
         String courseIdForNewSession = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionToHighlight = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
         String isUsingAjax = getRequestParamValue(Const.ParamsNames.IS_USING_AJAX);
-        
+
         gateKeeper.verifyInstructorPrivileges(account);
-                
+
         if (courseIdForNewSession != null) {
             gateKeeper.verifyAccessible(
                     logic.getInstructorForGoogleId(courseIdForNewSession, account.googleId),
@@ -33,16 +36,15 @@ public class InstructorFeedbacksPageAction extends InstructorFeedbackAbstractAct
 
         InstructorFeedbacksPageData data = new InstructorFeedbacksPageData(account);
         data.setUsingAjax(isUsingAjax != null);
-        
-        
+
         boolean omitArchived = true; // TODO: implement as a request parameter
         // HashMap with courseId as key and InstructorAttributes as value
         Map<String, InstructorAttributes> instructors = loadCourseInstructorMap(omitArchived);
-        
+
         List<InstructorAttributes> instructorList =
                 new ArrayList<InstructorAttributes>(instructors.values());
         List<CourseAttributes> courses = loadCoursesList(instructorList);
-        
+
         List<FeedbackSessionAttributes> existingFeedbackSessions;
         if (courses.isEmpty() || !data.isUsingAjax()) {
             existingFeedbackSessions = new ArrayList<FeedbackSessionAttributes>();
@@ -52,20 +54,53 @@ public class InstructorFeedbacksPageAction extends InstructorFeedbackAbstractAct
                 statusToUser.add(new StatusMessage(Const.StatusMessages.FEEDBACK_SESSION_EMPTY, StatusMessageColor.WARNING));
             }
         }
-        
+
         if (courses.isEmpty()) {
             statusToUser.add(new StatusMessage(Const.StatusMessages.COURSE_EMPTY_IN_INSTRUCTOR_FEEDBACKS
                                                        .replace("${user}", "?user=" + account.googleId),
                                                StatusMessageColor.WARNING));
         }
-        
+
         statusToAdmin = "Number of feedback sessions: " + existingFeedbackSessions.size();
-        
-        
+
         data.initWithoutDefaultFormValues(courses, courseIdForNewSession, existingFeedbackSessions,
                                         instructors, feedbackSessionToHighlight);
-        
+
         return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACKS, data);
     }
 
+    protected List<FeedbackSessionAttributes> loadFeedbackSessionsList(
+            List<InstructorAttributes> instructorList) {
+
+        List<FeedbackSessionAttributes> sessions =
+                logic.getFeedbackSessionsListForInstructor(instructorList);
+        return sessions;
+    }
+
+    protected List<CourseAttributes> loadCoursesList(List<InstructorAttributes> instructorList) {
+
+        List<CourseAttributes> courses = logic.getCoursesForInstructor(instructorList);
+
+        Collections.sort(courses, new Comparator<CourseAttributes>() {
+            @Override
+            public int compare(CourseAttributes c1, CourseAttributes c2) {
+                return c1.getId().compareTo(c2.getId());
+            }
+        });
+
+        return courses;
+    }
+
+    /**
+     * Gets a Map with courseId as key, and InstructorAttributes as value.
+     * @return
+     */
+    protected HashMap<String, InstructorAttributes> loadCourseInstructorMap(boolean omitArchived) {
+        HashMap<String, InstructorAttributes> courseInstructorMap = new HashMap<String, InstructorAttributes>();
+        List<InstructorAttributes> instructors = logic.getInstructorsForGoogleId(account.googleId, omitArchived);
+        for (InstructorAttributes instructor : instructors) {
+            courseInstructorMap.put(instructor.courseId, instructor);
+        }
+        return courseInstructorMap;
+    }
 }
