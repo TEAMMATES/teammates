ee563686cfa3fbf06d437018d360b1c2dc51d7bf
Anagh Hegde <h.v.anagh@gmail.com>
2017-02-26 23:37:55 +0530

    fix trailing spaces

--- src/main/java/teammates/ui/controller/InstructorFeedbackAddAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackAddAction.java
@@ -29,14 +29,14 @@ public class InstructorFeedbackAddAction extends InstructorFeedbackAbstractActio
 
     @Override
     protected ActionResult execute() {
-        
+
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
-        
+
         Assumption.assertPostParamNotNull(Const.ParamsNames.COURSE_ID, courseId);
         Assumption.assertNotEmpty(courseId);
-        
+
         InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, account.googleId);
-        
+
         gateKeeper.verifyAccessible(
                 instructor, logic.getCourse(courseId), Const.ParamsNames.INSTRUCTOR_PERMISSION_MODIFY_SESSION);
 
@@ -44,18 +44,17 @@ public class InstructorFeedbackAddAction extends InstructorFeedbackAbstractActio
 
         // Set creator email as instructors' email
         fs.setCreatorEmail(instructor.email);
-        
+
         // A session opening reminder email is always sent as students
         // without accounts need to receive the email to be able to respond
         fs.setOpeningEmailEnabled(true);
-        
-     
+
         String feedbackSessionType = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_TYPE);
 
         InstructorFeedbacksPageData data = new InstructorFeedbacksPageData(account);
         try {
             logic.createFeedbackSession(fs);
-            
+
             try {
                 createTemplateFeedbackQuestions(fs.getCourseId(), fs.getFeedbackSessionName(),
                                                 fs.getCreatorEmail(), feedbackSessionType);
@@ -64,7 +63,7 @@ public class InstructorFeedbackAddAction extends InstructorFeedbackAbstractActio
                 //TODO: let the user know an error has occurred? delete the feedback session?
                 log.severe(TeammatesException.toStringWithStackTrace(e));
             }
-            
+
             statusToUser.add(new StatusMessage(Const.StatusMessages.FEEDBACK_SESSION_ADDED, StatusMessageColor.SUCCESS));
             statusToAdmin =
                     "New Feedback Session <span class=\"bold\">(" + fs.getFeedbackSessionName() + ")</span> for Course "
@@ -74,13 +73,13 @@ public class InstructorFeedbackAddAction extends InstructorFeedbackAbstractActio
                     + "<span class=\"bold\">Session visible from:</span> " + fs.getSessionVisibleFromTime() + "<br>"
                     + "<span class=\"bold\">Results visible from:</span> " + fs.getResultsVisibleFromTime() + "<br><br>"
                     + "<span class=\"bold\">Instructions:</span> " + fs.getInstructions();
-            
+
             //TODO: add a condition to include the status due to inconsistency problem of database
             //      (similar to the one below)
             return createRedirectResult(
                     data.getInstructorFeedbackEditLink(
                             fs.getCourseId(), fs.getFeedbackSessionName()));
-            
+
         } catch (EntityAlreadyExistsException e) {
             setStatusForException(e, Const.StatusMessages.FEEDBACK_SESSION_EXISTS);
         } catch (InvalidParametersException e) {
@@ -94,54 +93,54 @@ public class InstructorFeedbackAddAction extends InstructorFeedbackAbstractActio
         List<CourseAttributes> courses = loadCoursesList(instructorList);
         List<FeedbackSessionAttributes> feedbackSessions = loadFeedbackSessionsList(instructorList);
         FeedbackSessionAttributes.sortFeedbackSessionsByCreationTimeDescending(feedbackSessions);
-        
+
         if (feedbackSessions.isEmpty()) {
             statusToUser.add(new StatusMessage(Const.StatusMessages.FEEDBACK_SESSION_ADD_DB_INCONSISTENCY,
                                                StatusMessageColor.WARNING));
         }
-        
+
         data.initWithoutHighlightedRow(courses, courseId, feedbackSessions, instructors, fs,
                                        feedbackSessionType);
-        
+
         return createShowPageResult(Const.ViewURIs.INSTRUCTOR_FEEDBACKS, data);
     }
-    
+
     private void createTemplateFeedbackQuestions(String courseId, String feedbackSessionName,
             String creatorEmail, String feedbackSessionType) throws InvalidParametersException {
         if (feedbackSessionType == null) {
             return;
         }
-        
+
         List<FeedbackQuestionAttributes> questions =
                 getFeedbackSessionTemplateQuestions(feedbackSessionType, courseId, feedbackSessionName, creatorEmail);
-        
+
         int questionNumber = 1;
         for (FeedbackQuestionAttributes fqa : questions) {
             logic.createFeedbackQuestionForTemplate(fqa, questionNumber);
             questionNumber++;
         }
     }
-    
+
     /** 
      * Get the list of questions for the specified feedback session template
      */
     private static List<FeedbackQuestionAttributes> getFeedbackSessionTemplateQuestions(
             String templateType, String courseId, String feedbackSessionName, String creatorEmail) {
         Assumption.assertNotNull(templateType);
-        
+
         if ("TEAMEVALUATION".equals(templateType)) {
             String jsonString = Templates.populateTemplate(FeedbackSessionTemplates.TEAM_EVALUATION,
                     "${courseId}", courseId,
                     "${feedbackSessionName}", feedbackSessionName,
                     "${creatorEmail}", creatorEmail);
-            
+
             Type listType = new TypeToken<ArrayList<FeedbackQuestionAttributes>>(){}.getType();
             return JsonUtils.fromJson(jsonString, listType);
         }
-        
+
         return new ArrayList<FeedbackQuestionAttributes>();
     }
-    
+
     protected FeedbackSessionAttributes extractFeedbackSessionDataHelper(
             FeedbackSessionAttributes newSession, List<String> sendRemainderEmailsList) {
         newSession.setCreatedTime(new Date());
