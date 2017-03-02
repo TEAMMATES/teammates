0180cad5d65a474ae6cee98c2ba458b5e93f01b2
= <=>
2017-03-02 18:33:25 +0530

    removed test cases for unused functions

--- src/main/webapp/dev/DatePickerJsTest.js
+++ src/main/webapp/dev/DatePickerJsTest.js
@@ -1,15 +1,7 @@
 QUnit.module('datepicker.js');
 
-QUnit.test('getMinDateForEndDate(startDate)', function(assert) {
-    assert.deepEqual(getMinDateForEndDate(new Date('02/25/2014')), new Date('02/25/2014'), 'Return startDate');
-});
-
-QUnit.test('getMaxDateForStartDate(endDate)', function(assert) {
-    assert.deepEqual(getMaxDateForStartDate(new Date('02/04/2014')), new Date('02/04/2014'), 'Return endDate');
-});
-
 QUnit.test('getMinDateForPublishDate(visibleDate)', function(assert) {
-    assert.deepEqual(getMinDateForPublishDate(new Date('10/13/2014')), new Date('10/13/2014'), 'Return visibleDate');
+    assert.deepEqual(getMinDateForPublishDate(new Date('10/13/2014')), new Date('10/13/2014'), 'returns visibleDate');
 });
 
 QUnit.test('getMaxDateForVisibleDate(startDate,publishDate)', function(assert) {
@@ -22,9 +14,9 @@ QUnit.test('getMaxDateForVisibleDate(startDate,publishDate)', function(assert) {
     assert.deepEqual(getMaxDateForVisibleDate(new Date('08/25/2014'), undefined), new Date('08/25/2014'),
             'When publishDate is undefined, returns startDate');
     assert.deepEqual(getMaxDateForVisibleDate(new Date('10/13/2014'), new Date('08/25/2014')), new Date('08/25/2014'),
-            'When startDate > publishDate, returns minDate');
+            'When startDate > publishDate, returns publishDate');
     assert.deepEqual(getMaxDateForVisibleDate(new Date('08/25/2014'), new Date('08/25/2014')), new Date('08/25/2014'),
-            'When startDate = publishDate, returns minDate');
+            'When startDate = publishDate, returns startDate');
     assert.deepEqual(getMaxDateForVisibleDate(new Date('08/25/2014'), new Date('08/26/2014')), new Date('08/25/2014'),
-            'When startDate < publishDate, returns minDate');
+            'When startDate < publishDate, returns startDate');
 });
