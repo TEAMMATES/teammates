dcfeb67e843547bc10a21adc38594852518b7bd5
Miguel Araújo <miguelarauj1o@gmail.com>
2017-03-02 22:02:39 -0300

    Remove unnecessary classes in common.datatransfer package
    * Remove FeedbackQuestionBundle class
    * Remove SessionResultsBundle interface
    * Move FeedbackSessionState class to AdminSearchPageData as private enum instead.
    
    related to #6640

--- src/main/java/teammates/ui/controller/AdminSearchPageAction.java
+++ src/main/java/teammates/ui/controller/AdminSearchPageAction.java
@@ -20,6 +20,10 @@ import teammates.ui.pagedata.AdminSearchPageData;
 
 public class AdminSearchPageAction extends Action {
 
+    private enum FeedbackSessionState {
+        OPEN, CLOSED, PUBLISHED, AWAITING;
+    }
+
     private HashMap<String, String> tempCourseIdToInstituteMap = new HashMap<String, String>();
     private HashMap<String, String> tempCourseIdToInstructorGoogleIdMap = new HashMap<String, String>();
 
