4a6d2ce45868313aea44ca052fc9b8eff676ec90
= <=>
2017-03-03 16:54:02 +0530

    Edited getMinDateForPublishDate test message'

--- src/main/webapp/dev/DatePickerJsTest.js
+++ src/main/webapp/dev/DatePickerJsTest.js
@@ -1,10 +1,10 @@
 QUnit.module('datepicker.js');
 
 QUnit.test('getMinDateForPublishDate(visibleDate)', function(assert) {
-    assert.deepEqual(getMinDateForPublishDate(new Date('10/13/2014')), new Date('10/13/2014'), 'returns visibleDate');
+    assert.deepEqual(getMinDateForPublishDate(new Date('10/13/2014')), new Date('10/13/2014'), 'returns visibleDate(parameter) itself');
 });
 
-QUnit.test('getMaxDateForVisibleDate(startDate,publishDate)', function(assert) {
+QUnit.test('getMaxDateForVisibleDate(startDate, publishDate)', function(assert) {
     assert.deepEqual(getMaxDateForVisibleDate(null, null), null,
             'When startDate and publishDate are null, returns null');
     assert.deepEqual(getMaxDateForVisibleDate(new Date('08/25/2014'), null), new Date('08/25/2014'),
