081e9eafe83f6a64139823bc750e4d9fbe3150dd
Yi Min <imleeyimin@gmail.com>
2017-03-04 22:24:37 +0800

    Add test cases to AdminEMailComposePageActionTest

--- src/test/java/teammates/test/cases/action/AdminEmailComposePageActionTest.java
+++ src/test/java/teammates/test/cases/action/AdminEmailComposePageActionTest.java
@@ -6,13 +6,20 @@ import teammates.common.datatransfer.attributes.AdminEmailAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.SanitizationHelper;
 import teammates.logic.core.AdminEmailsLogic;
-import teammates.storage.api.AdminEmailsDb;
 import teammates.test.driver.AssertHelper;
 import teammates.ui.controller.AdminEmailComposePageAction;
 import teammates.ui.controller.ShowPageResult;
 import teammates.ui.pagedata.AdminEmailComposePageData;
 
-public class AdminEmailComposePageActionTest extends BaseActionTest{
+public class AdminEmailComposePageActionTest extends BaseActionTest {
+
+    protected AdminEmailsLogic adminEmailsLogic = AdminEmailsLogic.inst();
+
+    @Override
+    protected void prepareTestData() {
+        dataBundle = loadDataBundle("/AdminEmailComposePageTest.json");
+        removeAndRestoreDataBundle(dataBundle);
+    }
 
     @Override
     protected String getActionUri() {
@@ -45,5 +52,36 @@ public class AdminEmailComposePageActionTest extends BaseActionTest{
         AdminEmailComposePageData data = (AdminEmailComposePageData) pageResult.data;
         assertNull(data.emailToEdit);
 
+        ______TS("case: edit existing email");
+        // retrieve email id from logic
+        AdminEmailAttributes email = adminEmailsLogic.getAllAdminEmails().get(0);
+        action = getAction(Const.ParamsNames.ADMIN_EMAIL_ID, email.emailId);
+        pageResult = getShowPageResult(action);
+        assertEquals(Const.ViewURIs.ADMIN_EMAIL + "?error=false&user=admin.user",
+                     pageResult.getDestinationWithParams());
+
+        String expectedLogSegment = normalLogSegment + " : Edit Email ["
+                                    + SanitizationHelper.sanitizeForHtml(email.getSubject()) + "]";
+        AssertHelper.assertContains(expectedLogSegment, action.getLogMessage());
+
+        assertEquals("", pageResult.getStatusMessage());
+
+        data = (AdminEmailComposePageData) pageResult.data;
+        assertNotNull(data.emailToEdit);
+
+        ______TS("case: edit non-existing email");
+        String emailId = "nonexistingEmailId";
+        action = getAction(Const.ParamsNames.ADMIN_EMAIL_ID, emailId);
+        pageResult = getShowPageResult(action);
+        assertEquals(Const.ViewURIs.ADMIN_EMAIL + "?error=true&user=admin.user",
+                pageResult.getDestinationWithParams());
+
+        expectedLogSegment = normalLogSegment + " : " + Const.StatusMessages.EMAIL_NOT_FOUND;
+        AssertHelper.assertContains(expectedLogSegment, action.getLogMessage());
+
+        assertEquals(Const.StatusMessages.EMAIL_NOT_FOUND, pageResult.getStatusMessage());
+
+        data = (AdminEmailComposePageData) pageResult.data;
+        assertNull(data.emailToEdit);
     }
 }
