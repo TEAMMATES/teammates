d67a660e4c9212a182e504b81e8c02d1e39a6fe9
AbhishekChd <acechd97@gmail.com>
2017-03-05 01:48:47 +0530

    Revert "Removed unused parameters"
    This reverts commit 84eb24e0634c1afe5b768178d02bdef065231a03.

--- src/main/java/teammates/logic/core/FeedbackQuestionsLogic.java
+++ src/main/java/teammates/logic/core/FeedbackQuestionsLogic.java
@@ -88,6 +88,7 @@ public final class FeedbackQuestionsLogic {
     }
 
     public FeedbackQuestionAttributes copyFeedbackQuestion(
+            String oldCourseId, String oldFeedbackSessionName,
             String feedbackQuestionId,
             String feedbackSessionName, String courseId, String instructorEmail)
             throws InvalidParametersException {
