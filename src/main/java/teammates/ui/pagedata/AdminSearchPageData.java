3bfade69a008b4e756c668f5e4ccf93ef7d162f4
Tiffany White <tiffanywhitedev412@gmail.com>
2017-03-05 19:35:26 -0500

    Adds FeedbackSessionState enum. Removes FeedbackSessionState class

--- src/main/java/teammates/ui/pagedata/AdminSearchPageData.java
+++ src/main/java/teammates/ui/pagedata/AdminSearchPageData.java
@@ -22,6 +22,10 @@ import teammates.ui.template.AdminSearchStudentTable;
 
 public class AdminSearchPageData extends PageData {
 
+    private enum FeedbackSessionState {
+        OPEN, CLOSED, PUBLISHED, AWAITING;
+    }
+
     public String searchKey = "";
 
     /*
