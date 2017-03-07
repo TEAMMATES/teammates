0efd827ea9e17c0ba1a6ab208f8475b7579cec3b
Tiffany White <tiffanywhitedev412@gmail.com>
2017-03-06 20:43:39 -0500

    fixed indentation

--- src/main/java/teammates/ui/pagedata/AdminSearchPageData.java
+++ src/main/java/teammates/ui/pagedata/AdminSearchPageData.java
@@ -22,9 +22,9 @@ import teammates.ui.template.AdminSearchStudentTable;
 
 public class AdminSearchPageData extends PageData {
 
-  private enum FeedbackSessionState {
-      OPEN, CLOSED, PUBLISHED, AWAITING
-  }
+      private enum FeedbackSessionState {
+          OPEN, CLOSED, PUBLISHED, AWAITING
+      }
 
     public String searchKey = "";
 
