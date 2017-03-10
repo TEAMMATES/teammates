c3efb7dac436687582340e2ca7b7ed5c0d9705ab
Pu Xiao <xp.xiaopu@gmail.com>
2017-03-10 23:51:43 +0800

    EmailLogEntry: Add log severe message when email log message is not in expected format

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -1482,6 +1482,9 @@ public final class Const {
         public static final String FIELD_SEPARATOR = "|||";
 
         public static final String TEST_DATA_POSTFIX = ".tmt";
+
+        public static final String ERROR_LOG_FORMAT = "Email log message is not in expected format."
+                + " Raw log message: %1$s";
     }
 
 }
