19c0fd63ac1b2fd01c0a1da5ea80cb5f403fa829
Anagh Hegde <h.v.anagh@gmail.com>
2017-03-10 22:10:26 +0530

    fix alignment issues

--- src/main/java/teammates/common/datatransfer/questions/FeedbackMsqQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/questions/FeedbackMsqQuestionDetails.java
@@ -430,8 +430,8 @@ public class FeedbackMsqQuestionDetails extends FeedbackQuestionDetails {
         return (numChoice == 0) ? 0 : (entryValue / numChoice);
     }
 
-    private int getQuestionResultStatistics(List<FeedbackResponseAttributes> responses,
-            Map<String, Integer> answerFrequency) {
+    private int getQuestionResultStatistics(
+            List<FeedbackResponseAttributes> responses, Map<String, Integer> answerFrequency) {
         containsNonEmptyResponse = false; // we will only show stats if there is at least one nonempty response
 
         for (String option : msqChoices) {
@@ -454,8 +454,8 @@ public class FeedbackMsqQuestionDetails extends FeedbackQuestionDetails {
         return numChoicesSelected;
     }
 
-    private int getNumOfChoicesSelected(FeedbackResponseAttributes response,
-            Map<String, Integer> answerFrequency) {
+    private int getNumOfChoicesSelected(
+            FeedbackResponseAttributes response, Map<String, Integer> answerFrequency) {
         int numChoices = 0;
         List<String> answerStrings =
                 ((FeedbackMsqResponseDetails) response.getResponseDetails()).getAnswerStrings();
