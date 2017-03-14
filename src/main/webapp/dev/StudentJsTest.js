9bb28f4fd98b49e047e79719596c39da8745b9ce
Li Kai <falconets25601@gmail.com>
2017-03-14 15:11:32 +0800

    update syntax to es6

--- src/main/webapp/dev/StudentJsTest.js
+++ src/main/webapp/dev/StudentJsTest.js
@@ -5,7 +5,7 @@ QUnit.module('student.js');
 QUnit.test('bindLinkInUnregisteredPage(selector)', function(assert) {
 
     clearBootboxButtonClickEvent();
-    StudentCommon.bindLinksInUnregisteredPage('#test-bootbox-button');
+    bindLinksInUnregisteredPage('#test-bootbox-button');
     ensureCorrectModal(assert, '#test-bootbox-button', Const.ModalDialogHeader.UNREGISTERED_STUDENT,
                        Const.ModalDialogText.UNREGISTERED_STUDENT);
     clearBootboxModalStub();
