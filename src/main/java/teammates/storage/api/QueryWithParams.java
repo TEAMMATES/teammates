78f3deb9a20da53baae112fbaedb4ac9738f2ece
John Yong <whipermr5@gmail.com>
2017-03-18 17:35:20 +0800

    Fix checkstyle violations

--- src/main/java/teammates/storage/api/QueryWithParams.java
+++ src/main/java/teammates/storage/api/QueryWithParams.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import javax.jdo.Query;
 
+/**
+ * Struct for bundling a Query together with parameters and expected result field.
+ */
 public class QueryWithParams {
+    public Query query;
+    public Object[] params;
+    public String expectedResultField;
     public QueryWithParams(Query query, Object[] params, String expectedResult) {
         this.query = query;
         this.params = params;
         this.expectedResultField = expectedResult;
     }
-    public Query query;
-    public Object[] params;
-    public String expectedResultField;
     public List<?> execute() {
         query.setResult(expectedResultField);
         return (List<?>) query.executeWithArray(params);
