3ab52f1e01d5ee8c66a7753d535a6a8d198ce1d4
John Yong <whipermr5@gmail.com>
2017-03-18 17:36:19 +0800

    Add line separators

--- src/main/java/teammates/storage/api/QueryWithParams.java
+++ src/main/java/teammates/storage/api/QueryWithParams.java
@@ -8,18 +8,22 @@ import javax.jdo.Query;
  * Struct for bundling a Query together with parameters and expected result field.
  */
 public class QueryWithParams {
+
     public Query query;
     public Object[] params;
     public String expectedResultField;
+
     public QueryWithParams(Query query, Object[] params, String expectedResult) {
         this.query = query;
         this.params = params;
         this.expectedResultField = expectedResult;
     }
+
     public List<?> execute() {
         query.setResult(expectedResultField);
         return (List<?>) query.executeWithArray(params);
     }
+
     public long delete() {
         return query.deletePersistentAll(params);
     }
