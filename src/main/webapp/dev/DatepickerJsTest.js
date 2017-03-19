f5f98d2ba184bf2bb640f346b1e0c4ec13c492c6
Vamsi Sangam <vamsisangam@live.com>
2017-03-19 17:05:05 +0530

    Added tests for datepicker.js

---
+++ src/main/webapp/dev/DatepickerJsTest.js
@@ -0,0 +1,34 @@
+'use strict';
+
+QUnit.module('datepicker.js');
+
+QUnit.test('triggerDatepickerOnClick(datepickerDivs)', function(assert) {
+    assert.expect(1);
+
+    $('#date-picker-div').datepicker();
+    triggerDatepickerOnClick([$('#date-picker-div')]);
+    $('#date-picker-div').click();
+
+    assert.equal($('.ui-datepicker-calendar:visible').length, 1, 'Displays datepickers after trigger');
+});
+
+QUnit.test('getMaxDateForVisibleDate(startDate, publishDate)', function(assert) {
+    assert.expect(5);
+
+    var startDate = new Date(2017, 3, 19, 2, 31, 0, 0);
+    var publishDate = new Date(2017, 3, 19, 2, 30, 0, 0);
+
+    assert.equal(getMaxDateForVisibleDate(startDate, null), startDate, 'Returns startDate when publishDate is null');
+    assert.equal(getMaxDateForVisibleDate(startDate, undefined), startDate, 'Returns startDate when publishDate is undefined');
+    assert.equal(getMaxDateForVisibleDate(startDate, publishDate), publishDate, 'Returns publishDate when startDate > publishDate');
+    assert.equal(getMaxDateForVisibleDate(startDate, startDate), startDate, 'Returns startDate when startDate = publishDate');
+    assert.equal(getMaxDateForVisibleDate(publishDate, startDate), publishDate, 'Returns startDate when startDate < publishDate');
+});
+
+QUnit.test('getMinDateForPublishDate(visibleDate)', function(assert) {
+    assert.expect(1);
+
+    var visibleDate = new Date(2017, 3, 19, 2, 31, 0, 0);
+
+    assert.equal(getMinDateForPublishDate(visibleDate), visibleDate, 'Returns visibleDate');
+});
