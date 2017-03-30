98ecc5f13392e0c2af8cea5d568da055da832d47
ZhaoDingfan <zhaodingfanhaha@gmail.com>
2017-03-30 22:46:58 +0800

    Update MissingResponsesHidden

--- src/test/java/teammates/test/cases/action/InstructorFeedbackResultsDownloadActionTest.java
+++ src/test/java/teammates/test/cases/action/InstructorFeedbackResultsDownloadActionTest.java
@@ -334,15 +334,15 @@ public class InstructorFeedbackResultsDownloadActionTest extends BaseActionTest 
 
         String[] expected = {
                 // CHECKSTYLE.OFF:LineLength csv lines can exceed character limit
-                "Course,\"" + session.getCourseId() + "\"",
-                "Session Name,\"" + session.getFeedbackSessionName() + "\"",
+                CourseID(session),
+                sessionName(session),
                 "",
                 "",
-                "Question 1,\"What is the best selling point of your product?\"",
+                question1(),
                 "",
-                "Team,Giver's Full Name,Giver's Last Name,Giver's Email,Recipient's Team,Recipient's Full Name,Recipient's Last Name,Recipient's Email,Feedback",
-                "\"Team 1.1</td></div>'\"\"\",\"student1 In Course1</td></div>'\"\"\",\"Course1</td></div>'\"\"\",\"student1InCourse1@gmail.tmt\",\"Team 1.1</td></div>'\"\"\",\"student1 In Course1</td></div>'\"\"\",\"Course1</td></div>'\"\"\",\"student1InCourse1@gmail.tmt\",\"Student 1 self feedback.\"",
-                "\"Team 1.1</td></div>'\"\"\",\"student2 In Course1\",\"Course1\",\"student2InCourse1@gmail.tmt\",\"Team 1.1</td></div>'\"\"\",\"student2 In Course1\",\"Course1\",\"student2InCourse1@gmail.tmt\",\"I'm cool'\""
+                contentHeader(),
+                team1Student1Course1(),
+                team1Student2Course1()
                 // CHECKSTYLE.ON:LineLength
         };
 
