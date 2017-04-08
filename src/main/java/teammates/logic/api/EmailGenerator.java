61b25811a2fcf6d723fd935b2083b0e5fa40e2ea
HirdayGupta <hirday.gupta@gmail.com>
2017-04-09 00:01:06 +0530

    Add Session Instructions to generateFeedbackSessionEmailBaseForStudents
    Added the key-value pair for Session Instructions to the
    EmailBaseForStudents method, so that this is included in all the emails
    sent to students that contains the key in its HTML body.

--- src/main/java/teammates/logic/api/EmailGenerator.java
+++ src/main/java/teammates/logic/api/EmailGenerator.java
@@ -413,6 +413,7 @@ public class EmailGenerator {
                 "${feedbackSessionName}", SanitizationHelper.sanitizeForHtml(session.getFeedbackSessionName()),
                 "${deadline}", SanitizationHelper.sanitizeForHtml(TimeHelper.formatTime12H(session.getEndTime())),
                 "${instructorFragment}", "",
+                "${sessionInstructions}", session.getInstructionsString(),
                 "${submitUrl}", submitUrl,
                 "${reportUrl}", reportUrl,
                 "${supportEmail}", Config.SUPPORT_EMAIL);
