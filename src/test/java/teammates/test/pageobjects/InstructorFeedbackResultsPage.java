08031ac3ca5398d2f79f5dc8966137a76600dab2
John Yong <whipermr5@gmail.com>
2017-04-10 04:52:37 +0800

    Build #3

--- src/test/java/teammates/test/pageobjects/InstructorFeedbackResultsPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorFeedbackResultsPage.java
@@ -341,7 +341,7 @@ public class InstructorFeedbackResultsPage extends AppPage {
         click(ajaxPanels);
     }
 
-    // Build #2
+    // Build #3
 
     public void clickViewPhotoLink(String panelBodyIndex, String urlRegex) {
         String panelBodySelector = "#panelBodyCollapse-" + panelBodyIndex;
