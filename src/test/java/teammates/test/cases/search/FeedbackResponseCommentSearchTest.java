d57c5c02d1a98ea4bcd127942f8c308c541cab0e
wkk91193 <wkk91193@yahoo.com>
2017-04-11 23:20:43 +0530

    added standardizedFeedbackResponseCommentsForComparison,verifyingSearchResult

--- src/test/java/teammates/test/cases/search/FeedbackResponseCommentSearchTest.java
+++ src/test/java/teammates/test/cases/search/FeedbackResponseCommentSearchTest.java
@@ -1,6 +1,7 @@
 package teammates.test.cases.search;
 
-//import java.util.ArrayList;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import teammates.common.datatransfer.FeedbackResponseCommentSearchResultBundle;
 import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.storage.api.FeedbackResponseCommentsDb;
-//import teammates.test.driver.AssertHelper;
+import teammates.test.driver.AssertHelper;
 
 /**
  * SUT: {@link FeedbackResponseCommentsDb},
@@ -20,13 +21,13 @@ import teammates.storage.api.FeedbackResponseCommentsDb;
 public class FeedbackResponseCommentSearchTest extends BaseSearchTest {
 
     @Test
-    public void allTests() {
+    public void allTests() throws InvalidParameterException {
 
         FeedbackResponseCommentsDb feedbackResponseCommentsDb = new FeedbackResponseCommentsDb();
-        // FeedbackResponseCommentAttributes
-        // fdbrI1S1=dataBundle.feedbackResponseComments.get("comment1FromT1C1ToR1Q1S1C1");
-        // FeedbackResponseCommentAttributes fdbrI1S1Q2 =
-        // dataBundle.feedbackResponseComments.get("comment1FromT1C1ToR1Q2S1C1");
+        FeedbackResponseCommentAttributes fdbrI1S1 = dataBundle.feedbackResponseComments
+                .get("comment1FromT1C1ToR1Q1S1C1");
+        FeedbackResponseCommentAttributes fdbrI1S1Q2 = dataBundle.feedbackResponseComments
+                .get("comment1FromT1C1ToR1Q2S1C1");
         FeedbackResponseCommentAttributes fdbrI1S1Q3 = dataBundle.feedbackResponseComments
                 .get("comment1FromT1C1ToR1Q3S1C1");
         List<InstructorAttributes> ins1OfCourse1 = Arrays.asList(
@@ -39,17 +40,13 @@ public class FeedbackResponseCommentSearchTest extends BaseSearchTest {
 
         FeedbackResponseCommentSearchResultBundle bundle = feedbackResponseCommentsDb.search("non-existent",
                 ins1OfCourse1);
-        assertEquals(0, bundle.numberOfResults);
-        assertTrue(bundle.comments.isEmpty());
-
+        verifySearchResults(bundle);
         ______TS(
                 "success: search for feedbackresponsecomments; query string matches"
                         + " some feedbackresponsecomments; results restricted based on instructor's privilege");
 
         bundle = feedbackResponseCommentsDb.search("self feedback Question 2", ins1OfCourse1);
-        assertEquals(2, bundle.numberOfResults);
-        // AssertHelper.assertSameContentIgnoreOrder(Arrays.asList(fdbrI1S1,fdbrI1S1Q2),
-        // new ArrayList<>(bundle.comments.values()));
+        verifySearchResults(bundle, fdbrI1S1, fdbrI1S1Q2);
 
         ______TS(
                 "success: search for feedbackresponsecomments; query string should be case-insensitive;results restricted "
@@ -57,16 +54,70 @@ public class FeedbackResponseCommentSearchTest extends BaseSearchTest {
 
         bundle = feedbackResponseCommentsDb
                 .search("Instructor 1 COMMENT to student 1 self feedback Question 2", ins1OfCourse1);
-        assertEquals(2, bundle.numberOfResults);
-        // AssertHelper.assertSameContentIgnoreOrder(Arrays.asList(fdbrI1S1Q2),
-        // new ArrayList<>(bundle.comments.values()));
+        verifySearchResults(bundle, fdbrI1S1Q2, fdbrI1S1);
+
+        ______TS(
+                "success: search for feedbackresponsecomments by searchable by courseId; results restricted "
+                        + "based on instructor's privilege");
+
+        bundle = feedbackResponseCommentsDb.search("idOfTypicalCourse1", ins1OfCourse1);
+        verifySearchResults(bundle, fdbrI1S1, fdbrI1S1Q2);
 
         ______TS("success: search for students; deleted student no longer searchable");
 
         feedbackResponseCommentsDb.deleteFeedbackResponseCommentsForCourse(fdbrI1S1Q3.courseId);
         bundle = feedbackResponseCommentsDb.search("feedback Question 3", ins1OfCourse1);
-        assertEquals(0, bundle.numberOfResults);
-        assertTrue(bundle.comments.isEmpty());
+        verifySearchResults(bundle);
 
     }
+
+    /*
+     * Verifies that search results match with expected output. Parameters are
+     * modified to standardize {@link FeedbackResponseCommentAttributes} for
+     * comparison.
+     *
+     * @param actual the results from the search query.
+     *
+     * @param expected the expected results for the search query.
+     */
+    private static void verifySearchResults(FeedbackResponseCommentSearchResultBundle actual,
+            FeedbackResponseCommentAttributes... expected) {
+        assertEquals(expected.length, actual.numberOfResults);
+        standardizedFeedbackResponseCommentsForComparison(expected);
+        List<FeedbackResponseCommentAttributes> feedbackCommentListNormalised =
+                new ArrayList<FeedbackResponseCommentAttributes>();
+        for (List<FeedbackResponseCommentAttributes> feedbackResponseCommentAttributesList : actual.comments
+                .values()) {
+            standardizedFeedbackResponseCommentsForComparison(feedbackResponseCommentAttributesList
+                    .toArray(
+                            new FeedbackResponseCommentAttributes[feedbackResponseCommentAttributesList
+                                                                  .size()]));
+            feedbackCommentListNormalised.add(feedbackResponseCommentAttributesList.get(0));
+
+        }
+
+        AssertHelper.assertSameContentIgnoreOrder(Arrays.asList(expected),
+                feedbackCommentListNormalised);
+    }
+
+    /*
+     * Standardizes feedbackResponsesComments for comparison by setting fields
+     * to null
+     *
+     * @param feedbackResponsesComments the FeedbackResponseCommentAttributes to
+     * standardize.
+     */
+    private static void standardizedFeedbackResponseCommentsForComparison(
+            FeedbackResponseCommentAttributes[] feedbackResponsesComments) {
+
+        for (FeedbackResponseCommentAttributes feedbackResponseComment : feedbackResponsesComments) {
+            feedbackResponseComment.feedbackQuestionId = null;
+            feedbackResponseComment.feedbackResponseId = null;
+            feedbackResponseComment.feedbackResponseCommentId = null;
+            feedbackResponseComment.lastEditorEmail = null;
+            feedbackResponseComment.lastEditedAt = null;
+        }
+
+    }
+
 }
