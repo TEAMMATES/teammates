baac7c5328a8ca9d44a586307449f3c2f511d1f5
tarun <tarun14110@iiitd.ac.in>
2017-04-13 01:39:15 +0530

    used ajax for remindEmail

---
+++ src/main/java/teammates/ui/pagedata/InstructorFeedbackRemindAjaxPageData.java
@@ -0,0 +1,14 @@
+package teammates.ui.pagedata;
+
+import teammates.common.datatransfer.attributes.AccountAttributes;
+
+/**
+ * Page data for a page with created image URL.
+ */
+public class InstructorFeedbackRemindAjaxPageData extends PageData {
+    public String ajaxStatus;
+
+    public InstructorFeedbackRemindAjaxPageData(AccountAttributes account) {
+        super(account);
+    }
+}
