2c624bdf1f7e629431d4eda6231ea3f589233848
--replace-all <--replace-all>
2017-04-19 13:12:53 +0530

    used redirectResult after successfully adding

--- src/main/java/teammates/ui/controller/RedirectResult.java
+++ src/main/java/teammates/ui/controller/RedirectResult.java
@@ -8,17 +8,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import teammates.common.datatransfer.attributes.AccountAttributes;
 import teammates.common.util.StatusMessage;
+import teammates.ui.pagedata.PageData;
 
 /** A 'redirect' type result. That is, the Browser will be required to make
  * another request to the specified {@code destination}.
  */
 public class RedirectResult extends ActionResult {
 
+    /** The data that will be used to render the page. */
+    public PageData data;
+
     public RedirectResult(
             String destination,
             AccountAttributes account,
             List<StatusMessage> status) {
         super(destination, account, status);
+    }
+
+    public RedirectResult(
+            String destination,
+            AccountAttributes account,
+            PageData data,
+            List<StatusMessage> status) {
+        super(destination, account, status);
+        this.data = data;
     }
 
     @Override
