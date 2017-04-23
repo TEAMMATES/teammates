28504a60d0372b635546f74ff12efb446970bc55
wkk91193 <wkk91193@yahoo.com>
2017-04-23 14:07:25 +0530

    fix changes requested 1

--- src/test/java/teammates/test/cases/search/FeedbackResponseCommentSearchTest.java
+++ src/test/java/teammates/test/cases/search/FeedbackResponseCommentSearchTest.java
@@ -14,8 +14,7 @@ import teammates.storage.api.FeedbackResponseCommentsDb;
 import teammates.test.driver.AssertHelper;
 
 /**
- * SUT: {@link FeedbackResponseCommentsDb},
- * {@link FeedbackResponseCommentSearchResult}.
+ * SUT: {@link FeedbackResponseCommentsDb},{@link FeedbackResponseCommentSearchResult}.
  */
 
 public class FeedbackResponseCommentSearchTest extends BaseSearchTest {
@@ -24,49 +23,60 @@ public class FeedbackResponseCommentSearchTest extends BaseSearchTest {
     public void allTests() throws InvalidParameterException {
 
         FeedbackResponseCommentsDb feedbackResponseCommentsDb = new FeedbackResponseCommentsDb();
-        FeedbackResponseCommentAttributes fdbrI1S1 = dataBundle.feedbackResponseComments
-                .get("comment1FromT1C1ToR1Q1S1C1");
-        FeedbackResponseCommentAttributes fdbrI1S1Q2 = dataBundle.feedbackResponseComments
-                .get("comment1FromT1C1ToR1Q2S1C1");
-        FeedbackResponseCommentAttributes fdbrI1S1Q3 = dataBundle.feedbackResponseComments
-                .get("comment1FromT1C1ToR1Q3S1C1");
-        List<InstructorAttributes> ins1OfCourse1 = Arrays.asList(
-                new InstructorAttributes[] { dataBundle.instructors.get("instructor1OfCourse1") });
+        FeedbackResponseCommentAttributes fdbrI1S1 =
+                dataBundle.feedbackResponseComments.get("comment1FromT1C1ToR1Q1S1C1");
+        FeedbackResponseCommentAttributes fdbrI1S1Q2 =
+                dataBundle.feedbackResponseComments.get("comment1FromT1C1ToR1Q2S1C1");
+        FeedbackResponseCommentAttributes fdbrI1S1Q3 =
+                dataBundle.feedbackResponseComments.get("comment1FromT1C1ToR1Q3S1C1");
+        List<InstructorAttributes> ins1OfCourse1 =
+                Arrays.asList(new InstructorAttributes[] { dataBundle.instructors.get("instructor1OfCourse1") });
 
-        ______TS(
-                "success: search for feedbackresponsecomments; query string does not match any "
-                        + "feedbackresponsecomments; results restricted "
-                        + "based on instructor's privilege");
+        ______TS("success: search for feedbackResponseComments; query string does not match any "
+                + "feedbackResponseComments; results restricted "
+                + "based on instructor's privilege");
 
-        FeedbackResponseCommentSearchResultBundle bundle = feedbackResponseCommentsDb.search("non-existent",
-                ins1OfCourse1);
+        FeedbackResponseCommentSearchResultBundle bundle =
+                feedbackResponseCommentsDb.search("non-existent", ins1OfCourse1);
         verifySearchResults(bundle);
-        ______TS(
-                "success: search for feedbackresponsecomments; query string matches"
-                        + " some feedbackresponsecomments; results restricted based on instructor's privilege");
 
-        bundle = feedbackResponseCommentsDb.search("self feedback Question 2", ins1OfCourse1);
-        verifySearchResults(bundle, fdbrI1S1, fdbrI1S1Q2);
+        ______TS("success: search for feedbackResponseComments; query string matches"
+                + " some feedbackResponseComments; results restricted based on instructor's privilege");
 
-        ______TS(
-                "success: search for feedbackresponsecomments; query string should be case-insensitive;results restricted "
-                        + "based on instructor's privilege");
+        bundle =
+                feedbackResponseCommentsDb.search("\"self feedback Question 2\"", ins1OfCourse1);
+        verifySearchResults(bundle, fdbrI1S1Q2);
+
+        ______TS("success: search for feedbackResponseComments; query string should be case-insensitive;results restricted "
+                + "based on instructor's privilege");
 
         bundle = feedbackResponseCommentsDb
-                .search("Instructor 1 COMMENT to student 1 self feedback Question 2", ins1OfCourse1);
-        verifySearchResults(bundle, fdbrI1S1Q2, fdbrI1S1);
+                .search("\"Instructor 1 COMMENT to student 1 self feedback Question 2\"", ins1OfCourse1);
+        verifySearchResults(bundle, fdbrI1S1Q2);
 
-        ______TS(
-                "success: search for feedbackresponsecomments by searchable by courseId; results restricted "
-                        + "based on instructor's privilege");
+        ______TS("success: search for feedbackResponseComments searchable by courseId; results restricted "
+                + "based on instructor's privilege");
 
         bundle = feedbackResponseCommentsDb.search("idOfTypicalCourse1", ins1OfCourse1);
         verifySearchResults(bundle, fdbrI1S1, fdbrI1S1Q2);
 
-        ______TS("success: search for students; deleted student no longer searchable");
+        ______TS("success: search for feedbackResponseComments searchable by feedbackSessionName; results restricted "
+                + "based on instructor's privilege");
+
+        bundle = feedbackResponseCommentsDb.search("\"First feedback session\"", ins1OfCourse1);
+        verifySearchResults(bundle, fdbrI1S1, fdbrI1S1Q2);
+
+        ______TS("success: search for feedbackResponseComments searchable by feedbackResponseId; results restricted "
+                + "based on instructor's privilege");
+
+        bundle = feedbackResponseCommentsDb
+                .search("1%student1InCourse1@gmail.tmt%student1InCourse1@gmail.tmt", ins1OfCourse1);
+        verifySearchResults(bundle, fdbrI1S1, fdbrI1S1Q2);
+
+        ______TS("success: search for feedbackResponseComments; deleted feedbackResponseComments no longer searchable");
 
         feedbackResponseCommentsDb.deleteFeedbackResponseCommentsForCourse(fdbrI1S1Q3.courseId);
-        bundle = feedbackResponseCommentsDb.search("feedback Question 3", ins1OfCourse1);
+        bundle = feedbackResponseCommentsDb.search("\"feedback Question 3\"", ins1OfCourse1);
         verifySearchResults(bundle);
 
     }
@@ -86,18 +96,14 @@ public class FeedbackResponseCommentSearchTest extends BaseSearchTest {
         standardizedFeedbackResponseCommentsForComparison(expected);
         List<FeedbackResponseCommentAttributes> feedbackCommentListNormalised =
                 new ArrayList<FeedbackResponseCommentAttributes>();
-        for (List<FeedbackResponseCommentAttributes> feedbackResponseCommentAttributesList : actual.comments
-                .values()) {
+        for (List<FeedbackResponseCommentAttributes> feedbackResponseCommentAttributesList
+                : actual.comments.values()) {
             standardizedFeedbackResponseCommentsForComparison(feedbackResponseCommentAttributesList
-                    .toArray(
-                            new FeedbackResponseCommentAttributes[feedbackResponseCommentAttributesList
-                                                                  .size()]));
+                    .toArray(new FeedbackResponseCommentAttributes[feedbackResponseCommentAttributesList.size()]));
             feedbackCommentListNormalised.add(feedbackResponseCommentAttributesList.get(0));
-
         }
 
-        AssertHelper.assertSameContentIgnoreOrder(Arrays.asList(expected),
-                feedbackCommentListNormalised);
+        AssertHelper.assertSameContentIgnoreOrder(Arrays.asList(expected), feedbackCommentListNormalised);
     }
 
     /*
