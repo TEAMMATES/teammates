6a0435a09ec8857e06af670d9a424c97446e3367
John Yong <whipermr5@gmail.com>
2017-05-14 18:34:08 +0800

    Extract MAX_DELAY_IN_S constant in RetryManager; update documentation to mention exponential backoff

--- src/test/java/teammates/test/driver/RetryManager.java
+++ src/test/java/teammates/test/driver/RetryManager.java
@@ -7,12 +7,14 @@ import teammates.common.util.ThreadHelper;
  */
 public final class RetryManager {
 
+    private static final int MAX_DELAY_IN_S = TestProperties.PERSISTENCE_RETRY_PERIOD_IN_S / 2;
+
     private RetryManager() {
         // utility class
     }
 
     /**
-     * Runs {@code task} and retries if needed.
+     * Runs {@code task} and retries if needed using exponential backoff.
      * Returns {@code task} result.
      */
     public static <T, E extends Throwable> T runWithRetry(RetryableWithResult<T, E> task) throws E {
@@ -21,11 +23,11 @@ public final class RetryManager {
     }
 
     /**
-     * Runs {@code task} and retries if needed.
+     * Runs {@code task} and retries if needed using exponential backoff.
      */
     public static <E extends Throwable> void runWithRetry(Retryable<E> task) throws E {
         boolean isSuccessful = task.run();
-        for (int delay = 1; !isSuccessful && delay <= TestProperties.PERSISTENCE_RETRY_PERIOD_IN_S / 2; delay *= 2) {
+        for (int delay = 1; !isSuccessful && delay <= MAX_DELAY_IN_S; delay *= 2) {
             System.out.println(task.getName() + " failed; waiting " + delay + "s before retry");
             ThreadHelper.waitFor(delay * 1000);
             task.beforeRetry();
