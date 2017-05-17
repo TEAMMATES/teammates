2dda82eafcea0b641d919108552e1fa08a12d263
Andy Daehn <andydaehn@gmail.com>
2017-05-17 15:18:11 -0600

    Fixed typo in FieldValidatorTest #7315
    Corrected the spelling of 'string' in the message of assertEquals in
    invalidityInfoFor_invalidGender_returnErrorString

--- src/test/java/teammates/test/cases/util/FieldValidatorTest.java
+++ src/test/java/teammates/test/cases/util/FieldValidatorTest.java
@@ -378,7 +378,7 @@ public class FieldValidatorTest extends BaseTestCase {
     public void invalidityInfoFor_invalidGender_returnErrorString() {
         String invalidGender = "alpha male";
         String actual = validator.getInvalidityInfoForGender(invalidGender);
-        assertEquals("Invalid gender should return appropriate error stirng",
+        assertEquals("Invalid gender should return appropriate error string",
                      String.format(GENDER_ERROR_MESSAGE, invalidGender),
                      actual);
 
