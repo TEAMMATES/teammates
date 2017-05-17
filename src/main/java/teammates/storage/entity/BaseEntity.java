e661289ff2545b0f6867802e58174f7f6340ceae
John Yong <whipermr5@gmail.com>
2017-05-17 17:06:31 +0800

    Update getFieldWithPrimaryKeyAnnotation method to recognise Objectify Id annotation

--- src/main/java/teammates/storage/entity/BaseEntity.java
+++ src/main/java/teammates/storage/entity/BaseEntity.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Field;
 
 import javax.jdo.annotations.PrimaryKey;
 
+import com.googlecode.objectify.annotation.Id;
+
 import teammates.common.util.Assumption;
 
 /**
@@ -14,11 +16,11 @@ public abstract class BaseEntity {
     protected static String getFieldWithPrimaryKeyAnnotation(Class<?> cls) {
         Field[] fs = cls.getDeclaredFields();
         for (Field f : fs) {
-            if (f.isAnnotationPresent(PrimaryKey.class)) {
+            if (f.isAnnotationPresent(PrimaryKey.class) || f.isAnnotationPresent(Id.class)) {
                 return f.getName();
             }
         }
-        Assumption.fail("There should be a field annotated with @PrimaryKey");
+        Assumption.fail("There should be a field annotated with @PrimaryKey or @Id");
         return null;
     }
 }
