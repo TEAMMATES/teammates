102c590d29b3f53466f2479727d05b5df50a29ec
John Yong <whipermr5@gmail.com>
2017-05-23 17:52:50 +0800

    Remove unused method from BaseEntity

--- src/main/java/teammates/storage/entity/BaseEntity.java
+++ src/main/java/teammates/storage/entity/BaseEntity.java
@@ -1,26 +1,7 @@
 package teammates.storage.entity;
 
-import java.lang.reflect.Field;
-
-import javax.jdo.annotations.PrimaryKey;
-
-import com.googlecode.objectify.annotation.Id;
-
-import teammates.common.util.Assumption;
-
 /**
  * Base class for all entities persisted to the Datastore.
  */
-@SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
-public abstract class BaseEntity {
-    protected static String getFieldWithPrimaryKeyAnnotation(Class<?> cls) {
-        Field[] fs = cls.getDeclaredFields();
-        for (Field f : fs) {
-            if (f.isAnnotationPresent(PrimaryKey.class) || f.isAnnotationPresent(Id.class)) {
-                return f.getName();
-            }
-        }
-        Assumption.fail("There should be a field annotated with @PrimaryKey or @Id");
-        return null;
-    }
+public abstract class BaseEntity { // NOPMD
 }
