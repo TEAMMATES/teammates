26bbf7d318600a789031fe7198a423a0b920b9d9
HirdayGupta <hirday.gupta@gmail.com>
2017-06-05 01:07:29 +0530

    Add the link text to getQuestionWithExistingResponseSubmissionFormHtml()
    Something I missed in my original commits! This adds the link to forms
    that are being edited as well. Apologies for the oversight earlier!

--- src/main/java/teammates/common/datatransfer/questions/FeedbackContributionQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/questions/FeedbackContributionQuestionDetails.java
@@ -86,7 +86,8 @@ public class FeedbackContributionQuestionDetails extends FeedbackQuestionDetails
                 Slots.RESPONSE_INDEX, Integer.toString(responseIdx),
                 Slots.FEEDBACK_RESPONSE_TEXT, Const.ParamsNames.FEEDBACK_RESPONSE_TEXT,
                 Slots.DISABLED, sessionIsOpen ? "" : "disabled",
-                Slots.CONTRIB_SELECT_FRAGMENTS_HTML, optionSelectFragmentsHtml);
+                Slots.CONTRIB_SELECT_FRAGMENTS_HTML, optionSelectFragmentsHtml,
+        		"${equalShareHelp}", responseIdx == 0 ? "<span class=\"glyphicon glyphicon-info-sign\"></span> More info about the equal share scale" : "");
     }
 
     @Override
