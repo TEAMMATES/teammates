e34c1f9555aa956f8ca55286c97ce682e9d7895a
HirdayGupta <hirday.gupta@gmail.com>
2017-06-11 18:01:11 +0530

    Fix line wrapping
    As per suggestion by @damithc

--- src/test/java/teammates/test/pageobjects/FeedbackSubmitPage.java
+++ src/test/java/teammates/test/pageobjects/FeedbackSubmitPage.java
@@ -202,8 +202,8 @@ public class FeedbackSubmitPage extends AppPage {
     }
 
     public void closeMoreInfoAboutEqualShareModal() {
-        WebElement closeButton = browser.driver.findElement(By
-                .xpath("//div[@id='more-info-equal-share-modal']//button[@class='close']"));
+        WebElement closeButton = browser.driver
+                .findElement(By.xpath("//div[@id='more-info-equal-share-modal']//button[@class='close']"));
         waitForElementToBeClickable(closeButton);
         click(closeButton);
         waitForElementToDisappear(By.id("more-info-equal-share-modal"));
