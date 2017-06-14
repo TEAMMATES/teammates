d43d8cceded9317028a6a6dfaa4c3d67ee721994
Mynk96 <mayank.harsani@gmail.com>
2017-06-14 08:54:52 +0000

    removed unused imports

--- src/main/java/teammates/ui/controller/InstructorFeedbackResponseCommentAddAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackResponseCommentAddAction.java
@@ -7,7 +7,6 @@ import com.google.appengine.api.datastore.Text;
 
 import teammates.common.datatransfer.FeedbackParticipantType;
 import teammates.common.datatransfer.FeedbackSessionResultsBundle;
-import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
 import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
 import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
