da694407edddf053996e11dbb332ac4728102426
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2017-06-15 23:59:07 +0800

    Change to a working check for `messageStubs`

--- src/test/java/teammates/test/driver/EmailAccount.java
+++ src/test/java/teammates/test/driver/EmailAccount.java
@@ -80,7 +80,7 @@ public final class EmailAccount {
                 .setQ("is:unread").execute();
 
         final List<Message> messageStubs = listMessagesResponse.getMessages();
-        if (!messageStubs.isEmpty()) {
+        if (messageStubs != null) {
             for (Message messageStub : messageStubs) {
                 final Message message = service.users().messages().get(user, messageStub.getId()).setFormat("raw").execute();
 
