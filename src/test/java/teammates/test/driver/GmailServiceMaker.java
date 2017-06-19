d1e046359fa8f37052cbcad54a3345d5405b220b
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2017-06-19 12:32:01 +0800

    Update message to be hosting platform agnostic

--- src/test/java/teammates/test/driver/GmailServiceMaker.java
+++ src/test/java/teammates/test/driver/GmailServiceMaker.java
@@ -92,9 +92,8 @@ final class GmailServiceMaker {
         try {
             in = new FileInputStream(new File(TestProperties.TEST_GMAIL_API_FOLDER, "client_secret.json"));
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("You need to set up your client_secret.json." + Const.EOL
-                    + "See Gmail API section at https://github.com/TEAMMATES/teammates/tree/master/docs/"
-                    + "development.md#deploying-to-a-staging-server", e);
+            throw new AssertionError("You need to set up your client_secret.json." + Const.EOL
+                    + "See Gmail API section in the documentation (development.md#deploying-to-a-staging-server)", e);
         }
         return GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
     }
