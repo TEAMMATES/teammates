d2d56d1b65891a63f6230ddd134620c935218bf8
sujeet14108 <sujeet14108@iiitd.ac.in>
2017-06-20 23:47:21 +0530

    InstructorEditInstructorFeedbackPageActionTest

--- src/test/java/teammates/test/cases/action/InstructorEditInstructorFeedbackPageActionTest.java
+++ src/test/java/teammates/test/cases/action/InstructorEditInstructorFeedbackPageActionTest.java
@@ -29,6 +29,7 @@ public class InstructorEditInstructorFeedbackPageActionTest extends BaseActionTe
     @Override
     @Test
     public void testExecuteAndPostProcess() {
+        prepareTestData();
         InstructorAttributes instructor = dataBundle.instructors.get("IEIFPTCourseinstr");
         InstructorAttributes moderatedInstructor = dataBundle.instructors.get("IEIFPTCoursehelper1");
         InstructorEditInstructorFeedbackPageAction editInstructorFpAction;
@@ -139,6 +140,21 @@ public class InstructorEditInstructorFeedbackPageActionTest extends BaseActionTe
     @Override
     @Test
     protected void testAccessControl() throws Exception {
-        //TODO: implement this
+        dataBundle = getTypicalDataBundle();
+        removeAndRestoreDataBundle(dataBundle);
+        InstructorAttributes moderatedInstructor = dataBundle.instructors.get("instructor1OfCourse1");
+        String courseId = moderatedInstructor.courseId;
+        String feedbackSessionName = "";
+        String moderatedInstructorEmail = "instructor1@course1.tmt";
+        String[] submissionParams;
+
+        feedbackSessionName = "First feedback session";
+        submissionParams = new String[]{
+                Const.ParamsNames.COURSE_ID, courseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, feedbackSessionName,
+                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, moderatedInstructorEmail
+        };
+        verifyOnlyInstructorsOfTheSameCourseCanAccess(submissionParams);
+        verifyUnaccessibleWithoutModifyCoursePrivilege(submissionParams);
     }
 }
