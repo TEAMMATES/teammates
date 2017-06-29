6f6440ced20b9841c48a26faff0cddd1826a0a90
Vamsi Sangam <vamsisangam@live.com>
2017-06-29 20:17:58 +0530

    Added methods to operate on select question number field

--- src/test/java/teammates/test/pageobjects/InstructorFeedbackEditPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorFeedbackEditPage.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 import com.google.appengine.api.datastore.Text;
 
@@ -567,6 +568,16 @@ public class InstructorFeedbackEditPage extends AppPage {
     public void selectQuestionNumber(int qnNumber, int newQnNumber) {
         WebElement qnNumSelect = browser.driver.findElement(By.id("questionnum-" + qnNumber));
         selectDropdownByVisibleValue(qnNumSelect, String.valueOf(newQnNumber));
+    }
+
+    public boolean isSelectQuestionNumberEnabled(int qnNumber) {
+        WebElement qnNumSelect = browser.driver.findElement(By.id("questionnum-" + qnNumber));
+        return qnNumSelect.isEnabled();
+    }
+
+    public int getSelectedQuestionNumber(int qnNumber) {
+        Select qnNumSelect = new Select(browser.driver.findElement(By.id("questionnum-" + qnNumber)));
+        return Integer.parseInt(qnNumSelect.getFirstSelectedOption().getText());
     }
 
     /**
