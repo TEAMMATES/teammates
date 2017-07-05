f85d25aa61ec7c96bf6bb6ee6354d6936550b44f
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-07-05 09:24:44 +0530

    Test dependency on Instructor index removed

--- src/test/java/teammates/test/cases/browsertests/InstructorCourseEditPageUiTest.java
+++ src/test/java/teammates/test/cases/browsertests/InstructorCourseEditPageUiTest.java
@@ -248,8 +248,6 @@ public class InstructorCourseEditPageUiTest extends BaseUiTestCase {
         courseEditPage.editInstructor(editInstructorIndex, "New name", "InsCrsEdit.instructor@gmail.tmt", false, "",
                 Const.InstructorPermissionRoleNames.INSTRUCTOR_PERMISSION_ROLE_COOWNER);
         courseEditPage.verifyStatus(String.format(Const.StatusMessages.COURSE_INSTRUCTOR_EDITED, "New name"));
-        courseEditPage.verifyInstructorDetails(editInstructorIndex, "New name", "InsCrsEdit.instructor@gmail.tmt",
-                false, "", Const.InstructorPermissionRoleNames.INSTRUCTOR_PERMISSION_ROLE_COOWNER);
 
         ______TS("success: unhide yet-to-join instructor and verify changes");
 
