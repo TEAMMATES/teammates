8744a948eedd169397d1389f787c6563b27f56b9
Tanya Pascal <josephinex21@gmail.com>
2017-07-13 22:11:21 +0300

    Fix lint errors

--- src/test/java/teammates/test/cases/util/AdminLogQueryTest.java
+++ src/test/java/teammates/test/cases/util/AdminLogQueryTest.java
@@ -21,9 +21,9 @@ public class AdminLogQueryTest extends BaseTestCase {
         versionList.add("5-44");
         Calendar cal = new GregorianCalendar();
         cal.set(1994, Calendar.MAY, 7, 15, 30, 12);
-        long startTime = cal.getTimeInMillis();                
+        long startTime = cal.getTimeInMillis();
         cal.add(Calendar.YEAR, 22); // about 22 years later
-        long endTime = cal.getTimeInMillis(); 
+        long endTime = cal.getTimeInMillis();
         AdminLogQuery query = new AdminLogQuery(versionList, startTime, endTime);
         assertEquals(startTime, query.getStartTime());
         assertEquals(endTime, query.getEndTime());
