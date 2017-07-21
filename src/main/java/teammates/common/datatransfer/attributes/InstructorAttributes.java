f43dbfc98a91962ddabec6ed273b1111bc919f4c
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-07-21 07:20:22 +0530

    Updated comment changed `Sort` to `Sorts`

--- src/main/java/teammates/common/datatransfer/attributes/InstructorAttributes.java
+++ src/main/java/teammates/common/datatransfer/attributes/InstructorAttributes.java
@@ -19,7 +19,7 @@ public class InstructorAttributes extends EntityAttributes<Instructor> {
     public static final String DEFAULT_DISPLAY_NAME = "Instructor";
 
     /**
-     * Sort the Instructors list alphabetically by name.
+     * Sorts the Instructors list alphabetically by name.
      */
     public static Comparator<InstructorAttributes> compareByName = new Comparator<InstructorAttributes>() {
         public int compare(InstructorAttributes one, InstructorAttributes other) {
