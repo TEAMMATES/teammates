3596f2883119d046fe8d9c41cfaada345ae85588
Brian Coveney <coveneybrian@gmail.com>
2017-07-28 18:43:32 +0100

    Fix checkstyle issues

--- src/main/java/teammates/common/datatransfer/attributes/GenderType.java
+++ src/main/java/teammates/common/datatransfer/attributes/GenderType.java
@@ -1,30 +1,24 @@
 package teammates.common.datatransfer.attributes;
 
-
 public enum GenderType {
-
     male {
         public String toString() {
             return "male";
         }
     },
-
     female {
         public String toString() {
             return "female";
         }
     },
-
     other {
         public String toString() {
             return "other";
         }
      },
-
     invalid {
         public String toString() {
             return "invalid gender";
         }
      }
-
 }
