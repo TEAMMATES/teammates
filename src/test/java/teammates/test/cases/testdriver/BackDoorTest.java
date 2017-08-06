2c25566f366ec10d5f30b862f3f88bc4531a88f1
Melky Salem <melkysalem@gmail.com>
2017-08-06 16:01:24 -0300

    [#7901] BackDoorTest: Remove `@SuppressWarnings("deprecation")`

--- src/test/java/teammates/test/cases/testdriver/BackDoorTest.java
+++ src/test/java/teammates/test/cases/testdriver/BackDoorTest.java
@@ -132,7 +132,6 @@ public class BackDoorTest extends BaseTestCaseWithBackDoorApiAccess {
         String courseId = "tmapitt.tcc.course";
         String name = "Tmapitt testInstr Name";
         String email = "tmapitt@tci.tmt";
-        
         InstructorAttributes instructor = InstructorAttributes.builder(instructorId, courseId, name, email)
                 .build();
 
