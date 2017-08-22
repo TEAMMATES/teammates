d295f3f4d6762e6615f0d1338fab77644e301108
Brian Coveney <coveneybrian@gmail.com>
2017-08-22 09:44:32 +0100

    Remove more unrelated changes in AccountLogicTest

--- src/test/java/teammates/test/cases/logic/AccountsLogicTest.java
+++ src/test/java/teammates/test/cases/logic/AccountsLogicTest.java
@@ -271,14 +271,14 @@ public class AccountsLogicTest extends BaseLogicTest {
             signalFailureToDetectException();
         } catch (JoinCourseException e) {
             assertEquals("The join link used belongs to a different user whose "
-                            + "Google ID is corre..dentId (only part of the Google ID is "
-                            + "shown to protect privacy). If that Google ID is owned by you, "
-                            + "please logout and re-login using that Google account. "
-                            + "If it doesn’t belong to you, please "
-                            + "<a href=\"mailto:" + Config.SUPPORT_EMAIL + "?"
-                            + "body=Your name:%0AYour course:%0AYour university:\">"
-                            + "contact us</a> so that we can investigate.",
-                    e.getMessage());
+                                          + "Google ID is corre..dentId (only part of the Google ID is "
+                                          + "shown to protect privacy). If that Google ID is owned by you, "
+                                          + "please logout and re-login using that Google account. "
+                                          + "If it doesn’t belong to you, please "
+                                          + "<a href=\"mailto:" + Config.SUPPORT_EMAIL + "?"
+                                          + "body=Your name:%0AYour course:%0AYour university:\">"
+                                          + "contact us</a> so that we can investigate.",
+                                  e.getMessage());
         }
 
         ______TS("success: with encryption and new account to be created");
@@ -446,14 +446,14 @@ public class AccountsLogicTest extends BaseLogicTest {
             signalFailureToDetectException();
         } catch (JoinCourseException e) {
             assertEquals("The join link used belongs to a different user whose "
-                            + "Google ID is stude..ourse1 (only part of the Google ID is "
-                            + "shown to protect privacy). If that Google ID is owned by you, "
-                            + "please logout and re-login using that Google account. "
-                            + "If it doesn’t belong to you, please "
-                            + "<a href=\"mailto:" + Config.SUPPORT_EMAIL + "?"
-                            + "body=Your name:%0AYour course:%0AYour university:\">"
-                            + "contact us</a> so that we can investigate.",
-                    e.getMessage());
+                                          + "Google ID is stude..ourse1 (only part of the Google ID is "
+                                          + "shown to protect privacy). If that Google ID is owned by you, "
+                                          + "please logout and re-login using that Google account. "
+                                          + "If it doesn’t belong to you, please "
+                                          + "<a href=\"mailto:" + Config.SUPPORT_EMAIL + "?"
+                                          + "body=Your name:%0AYour course:%0AYour university:\">"
+                                          + "contact us</a> so that we can investigate.",
+                                  e.getMessage());
         }
 
         ______TS("failure: invalid key");
