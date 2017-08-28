9dd8b5e15b2e4853cd69f3f0527890e545c09ab4
sujeet14108 <sujeet14108@iiitd.ac.in>
2017-08-28 13:40:53 +0530

    Conflict

--- src/main/java/teammates/ui/controller/InstructorFeedbackRemindParticularStudentsAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackRemindParticularStudentsAction.java
@@ -28,7 +28,7 @@ public class InstructorFeedbackRemindParticularStudentsAction extends Action {
         if (!feedbackSession.isOpened()) {
             statusToUser.add(new StatusMessage(
                     Const.StatusMessages.FEEDBACK_SESSION_REMINDERSSESSIONNOTOPEN, StatusMessageColor.DANGER));
-            statusToAdmin = "Reminder email could not be sent out as the feedback session is not open for submissions.";
+            statusToAdmin.add("Reminder email could not be sent out as the feedback session is not open for submissions.");
             return createRedirectResult(nextUrl);
         }
 
