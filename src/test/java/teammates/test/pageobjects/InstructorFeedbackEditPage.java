9369b8bf291d957b8574cb9557723396da6e6af4
Mac Knight <macknight@Macs-MacBook-Pro.local>
2017-09-22 13:08:19 -0400

    Added documention for selectConstSumPointsOption method

--- src/test/java/teammates/test/pageobjects/InstructorFeedbackEditPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorFeedbackEditPage.java
@@ -1005,6 +1005,14 @@ public class InstructorFeedbackEditPage extends AppPage {
                 generateFor);
     }
 
+    /**
+     * Selects the points distribution scheme for a const sum question.
+     * 
+     * @param pointsOption Value of "Total" will select total number of points for all options points distribution scheme,
+     *                     or "PerOption" will select number of points per option points distribution scheme.
+     * @param questionNumber question number of the const sum question.
+     */
+
     public void selectConstSumPointsOptions(String pointsOption, int questionNumber) {
         markRadioButtonAsChecked(
                 browser.driver.findElement(By.id("constSumPoints" + pointsOption + "-" + questionNumber)));
