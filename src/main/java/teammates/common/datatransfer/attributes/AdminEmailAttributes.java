d9636e28f90e10ad7a5fe6b391a911b879f2aeb9
Brian Coveney <coveneybrian@gmail.com>
2017-09-23 10:08:12 +0100

    [#7190] Use builder instead of telescoping constructor antipattern in AdminEmailAttributes class (#7701)

--- src/main/java/teammates/common/datatransfer/attributes/AdminEmailAttributes.java
+++ src/main/java/teammates/common/datatransfer/attributes/AdminEmailAttributes.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import com.google.appengine.api.datastore.Text;
 
+import teammates.common.util.Assumption;
 import teammates.common.util.Const;
 import teammates.common.util.FieldValidator;
 import teammates.common.util.JsonUtils;
@@ -15,37 +16,46 @@ import teammates.common.util.TimeHelper;
 import teammates.storage.entity.AdminEmail;
 
 public class AdminEmailAttributes extends EntityAttributes<AdminEmail> {
-
-    public String emailId;
+    // Required fields
     public List<String> addressReceiver;
     public List<String> groupReceiver;
     public String subject;
+    public Text content;
+
+    // Optional fields
     public Date sendDate;
     public Date createDate;
-    public Text content;
+    public String emailId;
     public boolean isInTrashBin;
 
-    public AdminEmailAttributes(AdminEmail ae) {
-        this.emailId = ae.getEmailId();
-        this.addressReceiver = ae.getAddressReceiver();
-        this.groupReceiver = ae.getGroupReceiver();
-        this.subject = ae.getSubject();
-        this.sendDate = ae.getSendDate();
-        this.createDate = ae.getCreateDate();
-        this.content = ae.getContent();
-        this.isInTrashBin = ae.getIsInTrashBin();
+    AdminEmailAttributes() {
+        createDate = Const.TIME_REPRESENTS_DEFAULT_TIMESTAMP;
+        emailId = Const.ParamsNames.ADMIN_EMAIL_ID;
     }
 
-    public AdminEmailAttributes(String subject,
-                                List<String> addressReceiver,
-                                List<String> groupReceiver,
-                                Text content,
-                                Date sendDate) {
-        this.subject = subject;
-        this.addressReceiver = addressReceiver;
-        this.groupReceiver = groupReceiver;
-        this.content = content;
-        this.sendDate = sendDate;
+    /**
+     * Creates a new AdminEmailAttributes with default values for optional fields.
+     *
+     * <p>Following default values are set to corresponding attributes:
+     * <ul>
+     * <li>{@code null} for {@code sendDate}</li>
+     * <li>{@code Const.TIME_REPRESENTS_DEFAULT_TIMESTAMP} for {@code createDate}</li>
+     * <li>{@code Const.ParamsNames.ADMIN_EMAIL_ID} for {@code emailId}</li>
+     * <li>{@code false} for {@code isInTrashBin}</li>
+     * </ul>
+     */
+    public static Builder builder(String subject, List<String> addressReceiver, List<String> groupReceiver, Text content) {
+        return new Builder(subject, addressReceiver, groupReceiver, content);
+    }
+
+    public static AdminEmailAttributes valueOf(AdminEmail adminEmail) {
+        return new Builder(adminEmail.getSubject(), adminEmail.getAddressReceiver(),
+                        adminEmail.getGroupReceiver(), adminEmail.getContent())
+                .withSendDate(adminEmail.getSendDate())
+                .withCreateDate(adminEmail.getCreateDate())
+                .withEmailId(adminEmail.getEmailId())
+                .withIsInTrashBin(adminEmail.getIsInTrashBin())
+                .build();
     }
 
     @Override
@@ -150,5 +160,53 @@ public class AdminEmailAttributes extends EntityAttributes<AdminEmail> {
 
     public String getFirstGroupReceiver() {
         return getGroupReceiver().get(0);
+    }
+
+    public static class Builder {
+        private final AdminEmailAttributes adminEmailAttributes;
+
+        public Builder(String subject, List<String> addressReceiver, List<String> groupReceiver, Text content) {
+
+            Assumption.assertNotNull(Const.StatusCodes.NULL_PARAMETER, subject);
+            Assumption.assertNotNull(Const.StatusCodes.NULL_PARAMETER, addressReceiver);
+            Assumption.assertNotNull(Const.StatusCodes.NULL_PARAMETER, groupReceiver);
+            Assumption.assertNotNull(Const.StatusCodes.NULL_PARAMETER, content);
+
+            adminEmailAttributes = new AdminEmailAttributes();
+            adminEmailAttributes.addressReceiver = addressReceiver;
+            adminEmailAttributes.groupReceiver = groupReceiver;
+            adminEmailAttributes.subject = subject;
+            adminEmailAttributes.content = content;
+        }
+
+        public Builder withSendDate(Date sendDate) {
+            if (sendDate != null) {
+                adminEmailAttributes.sendDate = sendDate;
+            }
+            return this;
+        }
+
+        public Builder withCreateDate(Date createDate) {
+            if (createDate != null) {
+                adminEmailAttributes.createDate = createDate;
+            }
+            return this;
+        }
+
+        public Builder withEmailId(String emailId) {
+            if (emailId != null) {
+                adminEmailAttributes.emailId = emailId;
+            }
+            return this;
+        }
+
+        public Builder withIsInTrashBin(boolean isInTrashBin) {
+            adminEmailAttributes.isInTrashBin = isInTrashBin;
+            return this;
+        }
+
+        public AdminEmailAttributes build() {
+            return adminEmailAttributes;
+        }
     }
 }
