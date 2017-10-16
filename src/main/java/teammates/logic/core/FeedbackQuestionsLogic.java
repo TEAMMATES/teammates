15b458742f9c8d293a006f71cf0ad3726be2577c
Daniel Esponda <danielresponda@gmail.com>
2017-10-15 20:47:03 -0400

    Removing unused import - teammates.common.datatransfer.attributes.FeedbackResponseAttributes

--- src/main/java/teammates/logic/core/FeedbackQuestionsLogic.java
+++ src/main/java/teammates/logic/core/FeedbackQuestionsLogic.java
@@ -13,7 +13,6 @@ import teammates.common.datatransfer.FeedbackParticipantType;
 import teammates.common.datatransfer.TeamDetailsBundle;
 import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
-import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
