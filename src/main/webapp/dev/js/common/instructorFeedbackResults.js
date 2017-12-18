a6f0aa58a902854d594d4a1f8b99ff733121dca9
Shubham Garg <gargshubham49@gmail.com>
2017-12-18 17:44:40 +0530

    replacing var

--- src/main/webapp/dev/js/common/instructorFeedbackResults.js
+++ src/main/webapp/dev/js/common/instructorFeedbackResults.js
@@ -358,9 +358,9 @@ function prepareInstructorFeedbackResultsPage() {
 
 function bindRubicQuestionSelfExcludingCheck() {
     $('#self-excluding-check').on('change', (e) => {
-        var responseSummary = $(e.target).next();
-        var includingSelfBody = responseSummary.parent().parent().parent().find('.body-including-self');
-        var excludingSelfBody = responseSummary.parent().parent().parent().find('.body-excluding-self');
+        const responseSummary = $(e.target).next();
+        const includingSelfBody = responseSummary.parent().parent().parent().find('.body-including-self');
+        const excludingSelfBody = responseSummary.parent().parent().parent().find('.body-excluding-self');
         if (responseSummary.html().trim() === 'Response Summary') {
             responseSummary.html('Response Summary Excluding Self');
         } else {
