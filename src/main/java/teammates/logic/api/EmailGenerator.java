95d640e00660bb3795e577610892e08d648c3614
Sukanta Roy <sukanta.raj.27@gmail.com>
2017-12-20 01:50:43 +0530

    Revert

--- src/main/java/teammates/logic/api/EmailGenerator.java
+++ src/main/java/teammates/logic/api/EmailGenerator.java
@@ -305,7 +305,7 @@ public class EmailGenerator {
             }
         }
 
-        String template = EmailTemplates.USER_FEEDBACK_SESSION
+        String template = EmailTemplates.USER_FEEDBACK_SESSION_CLOSING
                 .replace("${status}", FEEDBACK_STATUS_SESSION_CLOSING);
         CourseAttributes course = coursesLogic.getCourse(session.getCourseId());
         List<InstructorAttributes> instructors = isEmailNeeded
@@ -313,8 +313,7 @@ public class EmailGenerator {
                                                  : new ArrayList<InstructorAttributes>();
 
         return generateFeedbackSessionEmailBases(course, session, students, instructors, template,
-                EmailType.FEEDBACK_CLOSING.getSubject(), EmailTemplates.FRAGMENT_CLOSED_SESSION_ADDITIONAL_INSTRUCTIONS,
-                FEEDBACK_ACTION_SUBMIT);
+                EmailType.FEEDBACK_CLOSING.getSubject());
     }
 
     /**
