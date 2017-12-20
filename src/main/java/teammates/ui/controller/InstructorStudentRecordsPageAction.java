57d7ea343cd0a8c6103ddf7d7ce44058c4ef8d61
Haozhe <phaozhe@hotmail.com>
2017-12-20 21:41:18 +0800

    Remove unused import for Iterator

--- src/main/java/teammates/ui/controller/InstructorStudentRecordsPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorStudentRecordsPageAction.java
@@ -2,7 +2,6 @@ package teammates.ui.controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
