03801751edae2bfd108bb2e9d9015d56da497209
bjrodrig <barbarajrodriguez542@gmail.com>
2017-12-21 13:51:01 -0500

    style changes

--- src/test/java/teammates/test/cases/browsertests/InstructorCourseEditPageUiTest.java
+++ src/test/java/teammates/test/cases/browsertests/InstructorCourseEditPageUiTest.java
@@ -583,42 +583,42 @@ public class InstructorCourseEditPageUiTest extends BaseUiTestCase {
         courseEditPage.editInstructor(1, "New name", "new_email@email.tmt",
                                       false, "Instructor", "Custom");
         courseEditPage.editInstructor(2, "New name", "InsCrsEdit.instructor@gmail.tmt",
-                false, "New display name", "Co-owner");
+                                      false, "New display name", "Co-owner");
         courseEditPage.editInstructor(3, "New name", "InsCrsEdit.instructor@gmail.tmt",
-                false, "Instructor", "Co-owner");
+                                      false, "Instructor", "Co-owner");
         courseEditPage.editInstructor(4, "Teammates New Instructor", "InsCrsEdit.Helper@gmail.tmt",
-                false, "Instructor", "Custom");
+                                      false, "Instructor", "Custom");
         courseEditPage.editInstructor(5, "Teammates New Instructor", "InsCrsEdit.newInstr@gmail.tmt",
-                false, "Instructor", "Custom");
+                                      false, "Instructor", "Custom");
         courseEditPage.editInstructor(6, "Teammates Observer", "InsCrsEdit.observer@gmail.tmt",
-                false, "Instructor", "Observer");
+                                      false, "Instructor", "Observer");
         courseEditPage.editInstructor(7, "Teammates Test", "InsCrsEdit.test@gmail.tmt",
-                false, "Instructor", "Co-owner");
+                                      false, "Instructor", "Co-owner");
         courseEditPage.editInstructor(8, "Teammates Do Not Save", "InsCrsEdit.tutor@gmail.tmt",
-                false, "Instructor", "Tutor");
+                                      false, "Instructor", "Tutor");
 
         // Test to verify that changes made to instructor are not saved if this is the only instructor displayed to students
         // and you try to uncheck the box for "Display to students as:"
         // The information in "verifyInstructorDetails" method below comes from InstructorCourseEditPageUiTest.json,
         // googleId: InsCrsEdit.tutor
         courseEditPage.verifyInstructorDetails(8, "Teammates Tutor", "InsCrsEdit.tutor@gmail.tmt",
-                true, "Instructor", "Tutor");
+                                               true, "Instructor", "Tutor");
         courseEditPage.verifyStatus(String.format(Const.StatusMessages.COURSE_INSTRUCTOR_NO_INSTRUCTOR_DISPLAYED));
 
         // Test that you can successfully edit a non-displayed instructor without receiving an error message,
         // if at least one instructor is visible.
         courseEditPage.editInstructor(1, "New name", "new_email_number_2@email.tmt",
-                false, "Instructor", "Custom");
+                                      false, "Instructor", "Custom");
         courseEditPage.verifyInstructorDetails(1, "New name", "new_email_number_2@email.tmt",
-                false, "Instructor", "Custom");
+                                               false, "Instructor", "Custom");
         courseEditPage.verifyStatus(String.format(Const.StatusMessages.COURSE_INSTRUCTOR_EDITED, "New name"));
-        
+
         // after test, need to change Instructor 7 back to visible so that future tests work correctly
         courseEditPage.editInstructor(7, "Teammates Test", "InsCrsEdit.test@gmail.tmt",
-                true, "Instructor", "Co-owner");
+                                      true, "Instructor", "Co-owner");
         // after test, need to change Instructor 1 email back so future tests work correctly
         courseEditPage.editInstructor(1, "New name", "new_email@email.tmt",
-                false, "Instructor", "Custom");
+                                      false, "Instructor", "Custom");
     }
 
     private void testCancelEditInstructorAction() throws Exception {
