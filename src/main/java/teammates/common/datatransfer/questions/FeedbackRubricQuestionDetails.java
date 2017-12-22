d07c31d7333ec42bbd601d615492cd33f8a8b069
Ahchuang <E0036072@u.nus.edu>
2017-12-22 19:58:05 +0800

    Retry Feedback Rubric Question Details

--- src/main/java/teammates/common/datatransfer/questions/FeedbackRubricQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/questions/FeedbackRubricQuestionDetails.java
@@ -2,13 +2,11 @@ package teammates.common.datatransfer.questions;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import teammates.common.datatransfer.FeedbackParticipantType;
 import teammates.common.datatransfer.FeedbackSessionResultsBundle;
@@ -818,18 +816,15 @@ public class FeedbackRubricQuestionDetails extends FeedbackQuestionDetails {
 
         List<Map.Entry<String, RubricRecipientStatistics>> recipientStatsList =
                 new LinkedList<>(recipientToRecipientStats.entrySet());
-        Collections.sort(recipientStatsList, new Comparator<Map.Entry<String, RubricRecipientStatistics>>() {
-            @Override
-            public int compare(Entry<String, RubricRecipientStatistics> o1,
-                    Entry<String, RubricRecipientStatistics> o2) {
-                RubricRecipientStatistics a = o1.getValue();
-                RubricRecipientStatistics b = o2.getValue();
+        recipientStatsList.sort((Map.Entry<String, RubricRecipientStatistics> obj1,
+                                 Map.Entry<String, RubricRecipientStatistics> obj2) -> {
+            RubricRecipientStatistics a = obj1.getValue();
+            RubricRecipientStatistics b = obj2.getValue();
 
-                if (a.recipientTeam.equalsIgnoreCase(b.recipientTeam)) {
-                    return a.recipientName.compareTo(b.recipientName);
-                } else {
-                    return a.recipientTeam.compareTo(b.recipientTeam);
-                }
+            if (a.recipientTeam.equalsIgnoreCase(b.recipientTeam)) {
+                return a.recipientName.compareTo(b.recipientName);
+            } else {
+                return a.recipientTeam.compareTo(b.recipientTeam);
             }
         });
 
