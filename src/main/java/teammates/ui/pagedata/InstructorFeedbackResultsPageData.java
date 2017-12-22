0dd388ebb42fa577313890590268c07609aba384
Ahchuang <E0036072@u.nus.edu>
2017-12-22 14:15:56 +0800

    Update UI pagedata Update test driver

--- src/main/java/teammates/ui/pagedata/InstructorFeedbackResultsPageData.java
+++ src/main/java/teammates/ui/pagedata/InstructorFeedbackResultsPageData.java
@@ -162,7 +162,7 @@ public class InstructorFeedbackResultsPageData extends PageData {
             }
         }
 
-        Collections.sort(sectionNames);
+        sectionNames.sort(null);
         return sectionNames;
     }
 
@@ -649,7 +649,7 @@ public class InstructorFeedbackResultsPageData extends PageData {
         // create for every remaining team in the section, participantResultsPanels for every team member
         for (String teamWithoutResponses : teamsWithoutResponses) {
             List<String> teamMembers = new ArrayList<>(bundle.getTeamMembersFromRoster(teamWithoutResponses));
-            Collections.sort(teamMembers);
+            teamMembers.sort(null);
             if (viewType.isPrimaryGroupingOfGiverType()) {
                 addMissingParticipantsPanelsWithModerationButtonForTeam(
                                                 sectionPanel, teamWithoutResponses, teamMembers);
@@ -687,7 +687,7 @@ public class InstructorFeedbackResultsPageData extends PageData {
 
         // Create missing participants panels for the previous team
         List<String> sortedTeamMembersWithoutResponses = new ArrayList<>(teamMembersWithoutResponses);
-        Collections.sort(sortedTeamMembersWithoutResponses);
+        sortedTeamMembersWithoutResponses.sort(null);
 
         if (viewType.isPrimaryGroupingOfGiverType()) {
             addMissingParticipantsPanelsWithModerationButtonForTeam(sectionPanel,
