33d3b64fc3ff38b242eac56df9c2d420c958f44f
Zhi Xuan Chng <e0032135@u.nus.edu>
2018-01-02 23:09:36 +0800

    Remove temp data structure in sanitizeListForCsv method

--- src/main/java/teammates/common/util/SanitizationHelper.java
+++ src/main/java/teammates/common/util/SanitizationHelper.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
@@ -316,11 +317,10 @@ public final class SanitizationHelper {
      * @see <a href="http://tools.ietf.org/html/rfc4180">http://tools.ietf.org/html/rfc4180</a>
      */
     public static List<String> sanitizeListForCsv(List<String> strList) {
-        List<String> sanitizedStrList = new ArrayList<>();
 
-        strList.forEach(string -> sanitizedStrList.add(sanitizeForCsv(string)));
+        return strList.stream().map(string -> sanitizeForCsv(string))
+                               .collect(Collectors.toList());
 
-        return sanitizedStrList;
     }
 
     /**
