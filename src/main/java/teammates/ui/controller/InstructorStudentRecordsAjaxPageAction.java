81c3d93222dce685751117a6d523d2cbd7d887ab
Zhi Xuan Chng <e0032135@u.nus.edu>
2018-01-02 23:35:04 +0800

    Fix unused import statement

--- src/main/java/teammates/ui/controller/InstructorStudentRecordsAjaxPageAction.java
+++ src/main/java/teammates/ui/controller/InstructorStudentRecordsAjaxPageAction.java
@@ -1,7 +1,6 @@
 package teammates.ui.controller;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import teammates.common.datatransfer.FeedbackSessionResultsBundle;
