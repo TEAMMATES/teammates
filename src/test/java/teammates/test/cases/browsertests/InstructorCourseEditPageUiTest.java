0d4cde7eeaf4655858933e827eab2c04110a8f7a
bjrodrig <barbarajrodriguez542@gmail.com>
2018-01-09 21:32:22 -0500

    browser tests

--- src/test/java/teammates/test/cases/browsertests/InstructorCourseEditPageUiTest.java
+++ src/test/java/teammates/test/cases/browsertests/InstructorCourseEditPageUiTest.java
@@ -825,7 +825,7 @@ public class InstructorCourseEditPageUiTest extends BaseUiTestCase {
         // set instructor 1 to invisible and also change email (for variety)
         courseEditPage.editInstructor(1, "CS2105 Instructor 1", "new_email@email.tmt", false,
                 "CS2105 Instructor 1", "Co-owner");
-        
+
         courseEditPage.verifyInstructorDetails(1, "CS2105 Instructor 1", "new_email@email.tmt",
                 false, "CS2105 Instructor 1", "Co-owner");
         courseEditPage.verifyStatus(String.format(Const.StatusMessages.COURSE_INSTRUCTOR_EDITED, "CS2105 Instructor 1"));
