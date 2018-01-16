7b10798f956896be82226adf7ab5b684e8349452
sweety98 <manoghna10@gmail.com>
2018-01-16 20:08:40 +0530

    Update CoursesLogic.java

--- src/main/java/teammates/logic/core/CoursesLogic.java
+++ src/main/java/teammates/logic/core/CoursesLogic.java
@@ -101,8 +101,8 @@ public final class CoursesLogic {
         } catch (EntityAlreadyExistsException | InvalidParametersException e) {
             //roll back the transaction
             coursesDb.deleteCourse(courseId);
-            String errorMessage = "Unexpected exception while trying to create instructor for a new course " + Const.EOL
-                                  + instructor.toString() + Const.EOL
+            String errorMessage = "Unexpected exception while trying to create instructor for a new course " + System.lineSeparator()
+                                  + instructor.toString() +System.lineSeparator()
                                   + TeammatesException.toStringWithStackTrace(e);
             Assumption.fail(errorMessage);
         }
@@ -517,7 +517,7 @@ public final class CoursesLogic {
             for (CourseAttributes ca : courseList) {
                 courseIdList.remove(ca.getId());
             }
-            log.severe("Course(s) was deleted but the instructor still exists: " + Const.EOL + courseIdList.toString());
+            log.severe("Course(s) was deleted but the instructor still exists: " + System.lineSeparator() + courseIdList.toString());
         }
 
         return courseList;
@@ -565,7 +565,7 @@ public final class CoursesLogic {
             for (CourseAttributes ca : courseList) {
                 courseIdList.remove(ca.getId());
             }
-            log.severe("Course(s) was deleted but the instructor still exists: " + Const.EOL + courseIdList.toString());
+            log.severe("Course(s) was deleted but the instructor still exists: " + System.lineSeparator() + courseIdList.toString());
         }
 
         for (CourseAttributes ca : courseList) {
@@ -634,7 +634,7 @@ public final class CoursesLogic {
             for (CourseAttributes ca : courseList) {
                 courseIdList.remove(ca.getId());
             }
-            log.severe("Course(s) was deleted but the instructor still exists: " + Const.EOL + courseIdList.toString());
+            log.severe("Course(s) was deleted but the instructor still exists: " + System.lineSeparator() + courseIdList.toString());
         }
 
         for (CourseAttributes ca : courseList) {
@@ -654,12 +654,12 @@ public final class CoursesLogic {
         boolean hasSection = hasIndicatedSections(courseId);
 
         StringBuilder export = new StringBuilder(100);
-        String courseInfo = "Course ID," + SanitizationHelper.sanitizeForCsv(courseId) + Const.EOL
-                      + "Course Name," + SanitizationHelper.sanitizeForCsv(course.course.getName()) + Const.EOL
-                      + Const.EOL + Const.EOL;
+        String courseInfo = "Course ID," + SanitizationHelper.sanitizeForCsv(courseId) + System.lineSeparator()
+                      + "Course Name," + SanitizationHelper.sanitizeForCsv(course.course.getName()) + System.lineSeparator()
+                      + System.lineSeparator() + System.lineSeparator();
         export.append(courseInfo);
 
-        String header = (hasSection ? "Section," : "") + "Team,Full Name,Last Name,Status,Email" + Const.EOL;
+        String header = (hasSection ? "Section," : "") + "Team,Full Name,Last Name,Status,Email" + System.lineSeparator();
         export.append(header);
 
         for (SectionDetailsBundle section : course.sections) {
@@ -680,7 +680,7 @@ public final class CoursesLogic {
                             + SanitizationHelper.sanitizeForCsv(StringHelper.removeExtraSpace(student.name)) + ','
                             + SanitizationHelper.sanitizeForCsv(StringHelper.removeExtraSpace(student.lastName)) + ','
                             + SanitizationHelper.sanitizeForCsv(studentStatus) + ','
-                            + SanitizationHelper.sanitizeForCsv(student.email) + Const.EOL);
+                            + SanitizationHelper.sanitizeForCsv(student.email) + System.lineSeparator());
                 }
             }
         }
