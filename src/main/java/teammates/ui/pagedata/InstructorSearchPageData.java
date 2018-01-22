d6884ce371591df46e3ef7b532d9b8c4c287370c
ss3681755 <singh_8.cse14@nituk.ac.in>
2018-01-23 01:05:32 +0530

    Minor changes

--- src/main/java/teammates/ui/pagedata/InstructorSearchPageData.java
+++ src/main/java/teammates/ui/pagedata/InstructorSearchPageData.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import teammates.common.datatransfer.FeedbackResponseCommentSearchResultBundle;
 import teammates.common.datatransfer.SectionDetailsBundle;
