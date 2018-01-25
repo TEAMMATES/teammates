2f83dbc01d598ecbb8ca8ee91f599e6afb91c982
Mushroom Wen <Wen Xin@DESKTOP-IVHHP1V.(none)>
2018-01-25 13:13:02 +0800

    Content/test:change AverageRank calculation in RankOptionQn to OverallRank
    And modify the expected test results data correspondingly

--- src/main/java/teammates/common/datatransfer/questions/FeedbackRankQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/questions/FeedbackRankQuestionDetails.java
@@ -144,6 +144,35 @@ public abstract class FeedbackRankQuestionDetails extends FeedbackQuestionDetail
         return normalisedRankForSingleSetOfRankings;
     }
 
+    /**
+     * Generate the normalized overall ranking from the ranks of the recipients or options
+     * by comparing the average ranks of the recipients or options
+     * E.g. A and B received (1,2) and C received (1,2,3),
+     * so A and B have the average rank of 1.5 and C's average rank is 2
+     * After normalization, the overall rank of A, B and C will be 1, 1 and 3
+     * @param recipientRanks is a map
+     *                       with key being the recipient identifier and the value the list of ranks of the recipient
+     */
+    protected Map<String, Integer> generateNormalizedOverallRankMapping(Map<String, List<Integer>> recipientRanks) {
+        TreeMap<Double, List<String>> recipientAverageRank = new TreeMap<>();
+        recipientRanks.forEach((recipientIdentifier, ranks) -> {
+            double average = computeAverage(ranks);
+            recipientAverageRank.computeIfAbsent(average, key -> new ArrayList<>())
+                    .add(recipientIdentifier);
+        });
+
+        Map<String, Integer> normalizedOverallRanking = new HashMap<>();
+        int currentRank = 1;
+        for (List<String> recipientsWithSameRank : recipientAverageRank.values()) {
+            for (String recipient : recipientsWithSameRank) {
+                normalizedOverallRanking.put(recipient, currentRank);
+            }
+            currentRank += recipientsWithSameRank.size();
+        }
+
+        return normalizedOverallRanking;
+    }
+
     public boolean isAreDuplicatesAllowed() {
         return areDuplicatesAllowed;
     }
--- src/main/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetails.java
+++ src/main/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetails.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 import teammates.common.datatransfer.FeedbackSessionResultsBundle;
 import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
@@ -359,35 +358,6 @@ public class FeedbackRankRecipientsQuestionDetails extends FeedbackRankQuestionD
         }
 
         return optionRanks;
-    }
-
-    /**
-     * Generate the normalized overall ranking from the ranks of the recipients
-     * by comparing the average ranks of the recipients
-     * E.g. A and B received (1,2) and C received (1,2,3),
-     * so A and B have the average rank of 1.5 and C's average rank is 2
-     * After normalization, the overall rank of A, B and C will be 1, 1 and 3
-     * @param recipientRanks is a map
-     *                       with key being the recipient identifier and the value the list of ranks of the recipient
-     */
-    public Map<String, Integer> generateNormalizedOverallRankMapping(Map<String, List<Integer>> recipientRanks) {
-        TreeMap<Double, List<String>> recipientAverageRank = new TreeMap<>();
-        recipientRanks.forEach((recipientIdentifier, ranks) -> {
-            double average = computeAverage(ranks);
-            recipientAverageRank.computeIfAbsent(average, key -> new ArrayList<>())
-                    .add(recipientIdentifier);
-        });
-
-        Map<String, Integer> normalizedOverallRanking = new HashMap<>();
-        int currentRank = 1;
-        for (List<String> recipientsWithSameRank : recipientAverageRank.values()) {
-            for (String recipient : recipientsWithSameRank) {
-                normalizedOverallRanking.put(recipient, currentRank);
-            }
-            currentRank += recipientsWithSameRank.size();
-        }
-
-        return normalizedOverallRanking;
     }
 
     /**
