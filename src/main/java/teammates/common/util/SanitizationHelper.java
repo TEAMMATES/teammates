36dc215daf88a99d75a51a349541e82092a57b99
Darren Wee <darren.wee@u.nus.edu>
2018-01-25 19:39:11 +0800

    Remove unused TreeSet import

--- src/main/java/teammates/common/util/SanitizationHelper.java
+++ src/main/java/teammates/common/util/SanitizationHelper.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.owasp.html.HtmlPolicyBuilder;
