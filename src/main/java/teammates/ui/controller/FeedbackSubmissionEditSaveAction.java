6b4aca3070440079af233b99d0cb682a0865fd10
John Yong <whipermr5@gmail.com>
2018-02-01 13:30:28 +0800

    Severe log unrecognised responses

--- src/main/java/teammates/ui/controller/FeedbackSubmissionEditSaveAction.java
+++ src/main/java/teammates/ui/controller/FeedbackSubmissionEditSaveAction.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.google.appengine.api.datastore.Text;
 
@@ -171,6 +172,23 @@ public abstract class FeedbackSubmissionEditSaveAction extends Action {
 
                 statusToUser.addAll(errorMessages);
                 isError = true;
+            }
+        }
+
+        for (int questionIndx = numOfQuestionsToGet + 1; questionIndx <= 1000; questionIndx++) {
+            String totalResponsesForQuestion = getRequestParamValue(
+                    Const.ParamsNames.FEEDBACK_QUESTION_RESPONSETOTAL + "-" + questionIndx);
+
+            if (totalResponsesForQuestion != null) {
+                String questionsResponses = data.bundle.getSortedQuestions().stream()
+                        .map(q -> q.toString() + "\n" + data.bundle.questionResponseBundle.get(q))
+                        .collect(Collectors.joining("\n\n"));
+
+                log.severe("Encountered responses in submission to questions that were not part of "
+                        + "the feedback session saved in the datastore!"
+                        + "\n\nParameters: " + HttpRequestHelper.printRequestParameters(request)
+                        + "\n\nQuestions and responses in datastore:\n\n" + questionsResponses + "\n");
+                break;
             }
         }
 
