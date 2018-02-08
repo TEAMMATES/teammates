a392916dd2c56a9804b7171c5fdbc3cfe9aaabbb
Sukanta Roy <sukanta.raj.27@gmail.com>
2018-02-08 12:32:09 +0530

    Modify isClosedWithinPastHour method

--- src/main/java/teammates/common/datatransfer/attributes/FeedbackSessionAttributes.java
+++ src/main/java/teammates/common/datatransfer/attributes/FeedbackSessionAttributes.java
@@ -274,9 +274,13 @@ public class FeedbackSessionAttributes extends EntityAttributes<FeedbackSession>
     }
 
     /**
-     * Returns true if the session is closed within the past hour of calling this function.
+     * Returns true if the session is closed within the past hour of calling this function,
+     * {@code false} otherwise, or if end time is {@code null}.
      */
     public boolean isClosedWithinPastHour() {
+        if (endTime == null) {
+            return false;
+        }
         long timeZoneOffset = (long) timeZone * 60 * 60 * 1000;
         Date date = new Date(endTime.getTime() + gracePeriod * 60000L - timeZoneOffset);
         return TimeHelper.isWithinPastHourFromNow(date);
