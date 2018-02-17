799771bd16f1f7d1d337e4f4e92a3ab474c6fd4d
Nidhi <nidhi98gupta@gmail.com>
2018-02-17 12:32:06 +0530

    pass checkstyle

--- src/main/java/teammates/common/datatransfer/TeamEvalResult.java
+++ src/main/java/teammates/common/datatransfer/TeamEvalResult.java
@@ -223,11 +223,11 @@ public class TeamEvalResult {
         double[] returnValue = new double[filterArray.length];
         for (int i = 0; i < filterArray.length; i++) {
             int filterValue = (int) filterArray[i];
-                if (filterValue == NA || filterValue == NSU || !isSanitized(filterValue)) {
-                    returnValue[i] = filterValue == NSU ? NSU : NA;
-                } else {
-                    returnValue[i] = valueArray[i];
-                }
+            if (filterValue == NA || filterValue == NSU || !isSanitized(filterValue)) {
+                returnValue[i] = filterValue == NSU ? NSU : NA;
+            } else {
+                returnValue[i] = valueArray[i];
+            }
         }
         return returnValue;
     }
