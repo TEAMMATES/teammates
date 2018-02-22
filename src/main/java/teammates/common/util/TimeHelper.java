160dc747aad65c2f60d87cbb26bc94ea23c72ecc
Tran Tien Dat <peter.trantiendat@gmail.com>
2018-02-22 12:17:33 +0800

    Remove logging when handling parse exception

--- src/main/java/teammates/common/util/TimeHelper.java
+++ src/main/java/teammates/common/util/TimeHelper.java
@@ -151,8 +151,6 @@ public final class TimeHelper {
         try {
             return LocalDateTime.parse(dateTimeString, formatter);
         } catch (DateTimeParseException e) {
-            log.warning("Failed to parse datetime string: \""
-                    + dateTimeString + "\" with format " + formatter);
             return null;
         }
     }
