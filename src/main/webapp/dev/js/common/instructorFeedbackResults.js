9afaf47a61a0af8f965090496894306583d4d217
Shradheya Thakre <tshradheya@gmail.com>
2018-02-23 19:17:12 +0800

    Fixes JS coding style

--- src/main/webapp/dev/js/common/instructorFeedbackResults.js
+++ src/main/webapp/dev/js/common/instructorFeedbackResults.js
@@ -68,11 +68,19 @@ function showHideStats() {
 // Toggles Rubric Questions excluding self
 function switchExcludingSelfResultsForRubricStatistics(checkbox) {
     if (checkbox.checked) {
-        $(checkbox).parent().parent().parent().find('.table-body-including-self').addClass('hidden');
-        $(checkbox).parent().parent().parent().find('.table-body-excluding-self').removeClass('hidden');
+        $(checkbox).parent().parent().parent()
+                .find('.table-body-including-self')
+                .addClass('hidden');
+        $(checkbox).parent().parent().parent()
+                .find('.table-body-excluding-self')
+                .removeClass('hidden');
     } else {
-        $(checkbox).parent().parent().parent().find('.table-body-including-self').removeClass('hidden');
-        $(checkbox).parent().parent().parent().find('.table-body-excluding-self').addClass('hidden');
+        $(checkbox).parent().parent().parent()
+                .find('.table-body-including-self')
+                .removeClass('hidden');
+        $(checkbox).parent().parent().parent()
+                .find('.table-body-excluding-self')
+                .addClass('hidden');
     }
 }
 
