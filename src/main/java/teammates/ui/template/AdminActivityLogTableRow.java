b6d6df8ab962d16c426d597ac5dace640f8c948b
Tran Tien Dat <peter.trantiendat@gmail.com>
2018-02-28 13:07:23 +0800

    [#8362] TimeHelper: migrate to Java 8's `java.time` - migrating conversion and formatting methods (#8428)

--- src/main/java/teammates/ui/template/AdminActivityLogTableRow.java
+++ src/main/java/teammates/ui/template/AdminActivityLogTableRow.java
@@ -1,7 +1,6 @@
 package teammates.ui.template;
 
-import java.util.Calendar;
-import java.util.TimeZone;
+import java.time.Instant;
 
 import teammates.common.util.ActivityLogEntry;
 import teammates.common.util.Const;
@@ -111,10 +110,9 @@ public class AdminActivityLogTableRow {
     }
 
     public String getDisplayedLogTime() {
-        Calendar appCal = Calendar.getInstance(TimeZone.getTimeZone(Const.DEFAULT_TIMEZONE));
-        appCal.setTimeInMillis(activityLog.getLogTime());
-        appCal = TimeHelper.convertToUserTimeZone(appCal, Const.SystemParams.ADMIN_TIME_ZONE_DOUBLE);
-        return TimeHelper.calendarToString(appCal);
+        Instant logInstant = Instant.ofEpochMilli(activityLog.getLogTime());
+        return TimeHelper.formatActivityLogTime(logInstant,
+                TimeHelper.convertToZoneId(Const.SystemParams.ADMIN_TIME_ZONE_DOUBLE));
     }
 
     public String getDisplayedRole() {
