6f797bccc4802971d6945cda66a316959ae9fc1d
Shradheya Thakre <tshradheya@gmail.com>
2018-03-06 13:36:54 +0800

    Correct checkstyle errors

--- src/main/java/teammates/common/util/Templates.java
+++ src/main/java/teammates/common/util/Templates.java
@@ -230,8 +230,10 @@ public final class Templates {
                     "${Const.ParamsNames.FEEDBACK_QUESTION_NUMBEROFCHOICECREATED}";
             public static final String QUESTION_NUMBER = "${questionNumber}";
             public static final String CHECKED_OTHER_OPTION_ENABLED = "${checkedOtherOptionEnabled}";
-            public static final String MCQ_GENERATED_OPTIONS = "${Const.ParamsNames.FEEDBACK_QUESTION_MCQ_GENERATED_OPTIONS}";
-            public static final String MSQ_GENERATED_OPTIONS = "${Const.ParamsNames.FEEDBACK_QUESTION_MSQ_GENERATED_OPTIONS}";
+            public static final String MCQ_GENERATED_OPTIONS =
+                    "${Const.ParamsNames.FEEDBACK_QUESTION_MCQ_GENERATED_OPTIONS}";
+            public static final String MSQ_GENERATED_OPTIONS =
+                    "${Const.ParamsNames.FEEDBACK_QUESTION_MSQ_GENERATED_OPTIONS}";
 
             public static final String GENERATE_OPTIONS_FOR_VALUE = "${generateOptionsForValue}";
             public static final String STUDENT_SELECTED = "${studentSelected}";
