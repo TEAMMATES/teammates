d6b08bb7553813f2881df168a70d70a174fd2a9c
shaharyar-shamshi <shaharyarshamshi@gmail.com>
2018-03-13 12:26:51 +0530

    logic changed

--- src/main/java/teammates/ui/controller/InstructorCourseInstructorEditSaveAction.java
+++ src/main/java/teammates/ui/controller/InstructorCourseInstructorEditSaveAction.java
@@ -19,7 +19,6 @@ public class InstructorCourseInstructorEditSaveAction extends InstructorCourseIn
 
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         Assumption.assertPostParamNotNull(Const.ParamsNames.COURSE_ID, courseId);
-        String instructorId = getRequestParamValue(Const.ParamsNames.INSTRUCTOR_ID);
         String instructorName = getRequestParamValue(Const.ParamsNames.INSTRUCTOR_NAME);
         Assumption.assertPostParamNotNull(Const.ParamsNames.INSTRUCTOR_NAME, instructorName);
         String instructorEmail = getRequestParamValue(Const.ParamsNames.INSTRUCTOR_EMAIL);
@@ -29,22 +28,21 @@ public class InstructorCourseInstructorEditSaveAction extends InstructorCourseIn
         gateKeeper.verifyAccessible(instructor, logic.getCourse(courseId),
                                     Const.ParamsNames.INSTRUCTOR_PERMISSION_MODIFY_INSTRUCTOR);
 
-        InstructorAttributes instructorToEdit = instructorId == null ? logic.getInstructorForEmail(courseId, instructorEmail)
+        String instructorId = getRequestParamValue(Const.ParamsNames.INSTRUCTOR_ID);
+        InstructorAttributes instructorToEdit = instructorId == null
+                ? logic.getInstructorForEmail(courseId, instructorEmail)
                 : logic.getInstructorForGoogleId(courseId, instructorId);
-        String instructorvisibilityInfo = "true";
-        boolean isVisibilityChanged = false;
-        if (getRequestParamValue(Const.ParamsNames.INSTRUCTOR_IS_DISPLAYED_TO_STUDENT) != null) {
-            isVisibilityChanged = true;
-        }
-        
-        if (isVisibilityChanged) {
-            instructorvisibilityInfo = getRequestParamValue(Const.ParamsNames.INSTRUCTOR_IS_DISPLAYED_TO_STUDENT);
-        }
-        if ("false".equalsIgnoreCase(instructorvisibilityInfo)) {
-            long totalInstructorDisplayed = getNumberOfInstructorsDisplayed(courseId);
-            if (totalInstructorDisplayed == 1) {
+
+        boolean isDisplayedToStudentsAfterChanges =
+                getRequestParamValue(Const.ParamsNames.INSTRUCTOR_IS_DISPLAYED_TO_STUDENT) != null;
+        boolean isMakingInstructorInvisible = instructorToEdit.isDisplayedToStudents
+                && !isDisplayedToStudentsAfterChanges;
+        if (isMakingInstructorInvisible) {
+            long numberOfInstructorsDisplayed = getNumberOfInstructorsDisplayedToStudents(courseId);
+            boolean isSoleVisibleInstructor = numberOfInstructorsDisplayed == 1;
+            if (isSoleVisibleInstructor) {
                 statusToUser.add(new StatusMessage(String.format(
-                                Const.StatusMessages.COURSE_INSTRUCTOR_DISPLAYED), StatusMessageColor.DANGER));
+                        Const.StatusMessages.COURSE_INSTRUCTOR_DISPLAYED), StatusMessageColor.DANGER));
                 RedirectResult result = createRedirectResult(Const.ActionURIs.INSTRUCTOR_COURSE_EDIT_PAGE);
                 result.addResponseParam(Const.ParamsNames.COURSE_ID, courseId);
                 return result;
@@ -53,7 +51,6 @@ public class InstructorCourseInstructorEditSaveAction extends InstructorCourseIn
 
         instructorToEdit = extractUpdatedInstructor(courseId, instructorId, instructorName, instructorEmail);
         updateToEnsureValidityOfInstructorsForTheCourse(courseId, instructorToEdit);
-
         try {
             if (instructorId == null) {
                 logic.updateInstructorByEmail(instructorEmail, instructorToEdit);
@@ -62,7 +59,7 @@ public class InstructorCourseInstructorEditSaveAction extends InstructorCourseIn
             }
 
             statusToUser.add(new StatusMessage(String.format(Const.StatusMessages.COURSE_INSTRUCTOR_EDITED, instructorName),
-                                               StatusMessageColor.SUCCESS));
+                    StatusMessageColor.SUCCESS));
             statusToAdmin = "Instructor <span class=\"bold\"> " + instructorName + "</span>"
                     + " for Course <span class=\"bold\">[" + courseId + "]</span> edited.<br>"
                     + "New Name: " + instructorName + "<br>New Email: " + instructorEmail;
@@ -76,6 +73,11 @@ public class InstructorCourseInstructorEditSaveAction extends InstructorCourseIn
         return result;
     }
 
+    private long getNumberOfInstructorsDisplayedToStudents(String courseId) {
+        List<InstructorAttributes> instructors = logic.getInstructorsForCourse(courseId);
+        return instructors.stream().filter(instructor -> instructor.isDisplayedToStudents).count();
+    }
+
     /**
      * Checks if there are any other registered instructors that can modify instructors.
      * If there are none, the instructor currently being edited will be granted the privilege
@@ -85,12 +87,6 @@ public class InstructorCourseInstructorEditSaveAction extends InstructorCourseIn
      * @param instructorToEdit Instructor that will be edited.
      *                             This may be modified within the method.
      */
-
-    private long getNumberOfInstructorsDisplayed(String courseId) {
-        List<InstructorAttributes> instructors = logic.getInstructorsForCourse(courseId);
-        return instructors.stream().filter(instructor -> instructor.isDisplayedToStudents).count();
-    }
-
     private void updateToEnsureValidityOfInstructorsForTheCourse(String courseId, InstructorAttributes instructorToEdit) {
         List<InstructorAttributes> instructors = logic.getInstructorsForCourse(courseId);
         int numOfInstrCanModifyInstructor = 0;
