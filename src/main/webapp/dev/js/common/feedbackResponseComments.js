5eb79d87d89c5f5e47f006d2f895d75dd98bc045
Nashmia Riaz <nashmia-riaz@hotmail.com>
2018-03-27 20:10:21 +0500

    fixes 8723, yet to test

--- src/main/webapp/dev/js/common/feedbackResponseComments.js
+++ src/main/webapp/dev/js/common/feedbackResponseComments.js
@@ -169,7 +169,7 @@ function showResponseCommentAddForm(recipientIndex, giverIndex, qnIndex, section
     }
     $(`#showResponseCommentAddForm${id}`).show();
 
-    $(`#responseCommentAddForm${id}`).empty();
+    // $(`#responseCommentAddForm${id}`).empty();
 
     if (typeof richTextEditorBuilder !== 'undefined') {
         /* eslint-disable camelcase */ // The property names are determined by external library (tinymce)
