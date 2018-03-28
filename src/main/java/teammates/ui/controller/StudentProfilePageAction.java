8e102024c56c962161bf734899932bf44c9e26a7
gthd <gtheodorou035@gmail.com>
2018-03-28 08:50:16 +0300

    Fixes the issue #8717 Inconsistent Student Profile Page

--- src/main/java/teammates/ui/controller/StudentProfilePageAction.java
+++ src/main/java/teammates/ui/controller/StudentProfilePageAction.java
@@ -2,6 +2,7 @@ package teammates.ui.controller;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import teammates.common.util.Const;
 import teammates.common.util.Logger;
 import teammates.common.util.SanitizationHelper;
@@ -27,16 +28,16 @@ public class StudentProfilePageAction extends Action {
             return createRedirectResult(Const.ActionURIs.STUDENT_HOME_PAGE);
         }
 
-        String whole_message = SanitizationHelper.sanitizeForHtmlTag(account.studentProfile.toString());
-        String part_of_message = null;
+        String wholeMessage = SanitizationHelper.sanitizeForHtmlTag(account.studentProfile.toString());
+        String partOfMessage = null;
         String googleId = null;
         String pattern = "(googleId\")(.*)";
         Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(whole_message);
+        Matcher m = r.matcher(wholeMessage);
         if (m.find()) {
-            part_of_message = m.group(2);
+            partOfMessage = m.group(2);
             Pattern ptrn = Pattern.compile("\\B(:.*?\")\\B");
-            Matcher matcher = ptrn.matcher(part_of_message);
+            Matcher matcher = ptrn.matcher(partOfMessage);
             if (matcher.find()) {
                 googleId = matcher.group();
                 googleId = googleId.substring(3, googleId.length() - 1);
