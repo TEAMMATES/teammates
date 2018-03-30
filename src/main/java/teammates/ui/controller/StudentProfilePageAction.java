9de0f633a1a7e6dd264a9dc32619a192068dfdd0
gthd <gtheodorou035@gmail.com>
2018-03-30 11:00:32 +0300

    Cleaner method for getting only the googleId

--- src/main/java/teammates/ui/controller/StudentProfilePageAction.java
+++ src/main/java/teammates/ui/controller/StudentProfilePageAction.java
@@ -1,11 +1,7 @@
 package teammates.ui.controller;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import teammates.common.util.Const;
 import teammates.common.util.Logger;
-import teammates.common.util.SanitizationHelper;
 import teammates.ui.pagedata.StudentProfilePageData;
 
 /**
@@ -28,25 +24,8 @@ public class StudentProfilePageAction extends Action {
             return createRedirectResult(Const.ActionURIs.STUDENT_HOME_PAGE);
         }
 
-        String wholeMessage = SanitizationHelper.sanitizeForHtmlTag(account.studentProfile.toString());
-        String partOfMessage = null;
-        String googleId = null;
-        String pattern = "(googleId\")(.*)";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(wholeMessage);
-        if (m.find()) {
-            partOfMessage = m.group(2);
-            Pattern ptrn = Pattern.compile("\\B(:.*?\")\\B");
-            Matcher matcher = ptrn.matcher(partOfMessage);
-            if (matcher.find()) {
-                googleId = matcher.group();
-                googleId = googleId.substring(3, googleId.length() - 1);
-            }
-        }
-
         StudentProfilePageData data = new StudentProfilePageData(account, sessionToken, isEditingPhoto);
-        statusToAdmin = "studentProfile Page Load <br> Profile: "
-                + googleId;
+        statusToAdmin = "studentProfile Page Load <br> Profile: " + account.googleId;
 
         return createShowPageResult(Const.ViewURIs.STUDENT_PROFILE_PAGE, data);
     }
