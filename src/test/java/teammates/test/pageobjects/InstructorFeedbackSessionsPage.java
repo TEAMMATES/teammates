70dd5bc96c30df987e5d3b8df9821b4d6398867b
Darren Wee <darren.wee@u.nus.edu>
2018-04-01 20:54:34 +0800

    Remove unused import

--- src/test/java/teammates/test/pageobjects/InstructorFeedbackSessionsPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorFeedbackSessionsPage.java
@@ -4,7 +4,6 @@ import static org.testng.AssertJUnit.fail;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.Calendar;
 import java.util.List;
 
