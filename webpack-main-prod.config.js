346bcfc3e57eb2bc6e3bef0394b7cbc1e7b1d2e6
Crphang <crphang@gmail.com>
2018-04-01 22:21:53 +0800

    Add full source map to production mode. Modify docs

--- webpack-main-prod.config.js
+++ webpack-main-prod.config.js
@@ -3,6 +3,7 @@ const common = require('./webpack-main.config.js');
 
 module.exports = merge(common, {
     mode: 'production',
+    devtool: 'source-map',
     optimization: {
         minimize: true,
     },
