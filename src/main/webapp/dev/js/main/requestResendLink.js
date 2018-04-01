b8e629a1ec58752f9463ae3da09f4f071e088266
Ahchuang <E0036072@u.nus.edu>
2018-04-01 12:29:07 +0800

    Fix lint errors

--- src/main/webapp/dev/js/main/requestResendLink.js
+++ src/main/webapp/dev/js/main/requestResendLink.js
@@ -1,25 +1,30 @@
-import {setStatusMessage} from "../common/statusMessage";
-import {BootstrapContextualColors} from "../common/const";
+import {
+    setStatusMessage,
+} from '../common/statusMessage';
+import {
+    BootstrapContextualColors,
+} from '../common/const';
 
 function validateEmail(event) {
-    var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var isValidEmail = emailRegex.test($('#email').val());
+    const emailRegex = new RegExp(['^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)|(".+"))@',
+        '((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'].join(''));
+    const isValidEmail = emailRegex.test($('#email').val());
 
-    if (!isValidEmail) {
-        alert("Invalid Email Address");
-        event.preventDefault();
-        setStatusMessage('Invalid Email Address', BootstrapContextualColors.DANGER);
-        return false;
-    } else {
+    if (isValidEmail) {
         setStatusMessage('Email Sent Successfully', BootstrapContextualColors.SUCCESS);
         return true;
     }
+
+    alert('Invalid Email Address');
+    event.preventDefault();
+    setStatusMessage('Invalid Email Address', BootstrapContextualColors.DANGER);
+    return false;
 }
 
 $(document).ready(() => {
-    $('#submitButton').on("click", function(event) {
+    $('#submitButton').on('click', (event) => {
         validateEmail(event);
     });
-})
+});
 
 window.validateEmail = validateEmail;
