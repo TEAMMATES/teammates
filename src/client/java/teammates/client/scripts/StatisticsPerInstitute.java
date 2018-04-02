b61c3cb9d6d9ca3db78a69977a8744b1e03ca2d8
gthd <gtheodorou035@gmail.com>
2018-04-02 20:22:37 +0300

    New changes for mapper-to-java-8-syntax

--- src/client/java/teammates/client/scripts/StatisticsPerInstitute.java
+++ src/client/java/teammates/client/scripts/StatisticsPerInstitute.java
@@ -120,12 +120,10 @@ public class StatisticsPerInstitute extends RemoteApiClient {
 
             String institute = getInstituteForInstructor(instructor, allAccounts);
 
-            if (!institutes.containsKey(institute)) {
-                institutes.put(institute, new HashMap<Integer, Set<String>>());
-                institutes.get(institute).put(INSTRUCTOR_INDEX, new HashSet<String>());
-                institutes.get(institute).put(STUDENT_INDEX, new HashSet<String>());
-            }
+            institutes.computeIfAbsent(institute, key -> new HashMap<>()).put(INSTRUCTOR_INDEX, new HashSet<>());
+            institutes.get(institute).put(STUDENT_INDEX, new HashSet<>());
             institutes.get(institute).get(INSTRUCTOR_INDEX).add(instructor.getEmail().toLowerCase());
+
             allInstructorEmailSet.add(instructor.getEmail().toLowerCase());
             instructorEmailCounter++;
             updateProgressIndicator();
@@ -139,14 +137,8 @@ public class StatisticsPerInstitute extends RemoteApiClient {
 
             String institute = getInstituteForStudent(student, allInstructors, allAccounts);
 
-            if (!institutes.containsKey(institute)) {
-                institutes.put(institute, new HashMap<Integer, Set<String>>());
-
-                institutes.get(institute).put(INSTRUCTOR_INDEX, new HashSet<String>());
-                institutes.get(institute).put(STUDENT_INDEX, new HashSet<String>());
-            }
-
-            institutes.get(institute).get(STUDENT_INDEX).add(student.getEmail().toLowerCase());
+            institutes.computeIfAbsent(institute, key -> new HashMap<>()).put(INSTRUCTOR_INDEX, new HashSet<>());
+            institutes.get(institute).put(STUDENT_INDEX, new HashSet<>());
             allStudentEmailSet.add(student.getEmail().toLowerCase());
             studentEmailCounter++;
             updateProgressIndicator();
--- src/main/java/teammates/logic/backdoor/BackDoorLogic.java
+++ src/main/java/teammates/logic/backdoor/BackDoorLogic.java
@@ -283,11 +283,9 @@ public class BackDoorLogic extends Logic {
 
             courseInstructorsMap.put(instructor.courseId, instructor);
 
-            if (StringHelper.isEmpty(instructor.googleId) || googleIdAccountMap.containsKey(instructor.googleId)) {
-                // No account, or account already exists in the data bundle
-                continue;
+            if (!StringHelper.isEmpty(instructor.googleId)) {
+                googleIdAccountMap.putIfAbsent(instructor.googleId, makeAccount(instructor));
             }
-            googleIdAccountMap.put(instructor.googleId, makeAccount(instructor));
         }
     }
 
@@ -296,12 +294,9 @@ public class BackDoorLogic extends Logic {
         for (StudentAttributes student : students) {
             populateNullSection(student);
 
-            if (StringHelper.isEmpty(student.googleId) || googleIdAccountMap.containsKey(student.googleId)) {
-                // No account, account already exists in the data bundle,
-                // or instructor account already exists (i.e. instructor is also a student)
-                continue;
+            if (!StringHelper.isEmpty(student.googleId)) {
+                googleIdAccountMap.putIfAbsent(student.googleId, makeAccount(student));
             }
-            googleIdAccountMap.put(student.googleId, makeAccount(student));
         }
     }
 
