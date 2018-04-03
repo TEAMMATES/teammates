9f21003d0996f93896cf2f7df36441836e1a6a5e
Nidhi <nidhi98gupta@gmail.com>
2018-04-03 15:31:36 +0530

    Indent

--- src/main/java/teammates/ui/pagedata/AdminSessionsPageData.java
+++ src/main/java/teammates/ui/pagedata/AdminSessionsPageData.java
@@ -186,9 +186,8 @@ public class AdminSessionsPageData extends PageData {
 
     private void setFilter() {
         filter = new AdminFilter(TimeHelper.formatDateForSessionsForm(rangeStart), getHourOptionsAsHtml(rangeStart),
-                                 getMinuteOptionsAsHtml(rangeStart), TimeHelper.formatDateForSessionsForm(rangeEnd),
-                                 getHourOptionsAsHtml(rangeEnd), getMinuteOptionsAsHtml(rangeEnd),
-                                 getTimeZoneOptionsAsHtml());
+                         getMinuteOptionsAsHtml(rangeStart), TimeHelper.formatDateForSessionsForm(rangeEnd),
+                         getHourOptionsAsHtml(rangeEnd), getMinuteOptionsAsHtml(rangeEnd), getTimeZoneOptionsAsHtml());
     }
 
     public void setInstitutionPanels(
