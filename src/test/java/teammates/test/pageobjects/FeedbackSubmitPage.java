b981eadfa8f5423a8fecc7cb03b8e65b2d28899c
tanhengyeow <E0032242@u.nus.edu>
2018-04-12 15:42:11 +0800

    Fixed some typos

--- src/test/java/teammates/test/pageobjects/FeedbackSubmitPage.java
+++ src/test/java/teammates/test/pageobjects/FeedbackSubmitPage.java
@@ -202,16 +202,16 @@ public class FeedbackSubmitPage extends AppPage {
     }
 
     public void submitWithoutConfirmationEmail() {
-        WebElement sendEmailChecbox = browser.driver.findElement(By.name(Const.ParamsNames.SEND_SUBMISSION_EMAIL));
-        if (sendEmailChecbox.isSelected()) {
-            click(sendEmailChecbox);
+        WebElement sendEmailCheckbox = browser.driver.findElement(By.name(Const.ParamsNames.SEND_SUBMISSION_EMAIL));
+        if (sendEmailCheckbox.isSelected()) {
+            click(sendEmailCheckbox);
         }
         clickSubmitButton();
     }
 
     public boolean isConfirmationEmailBoxTicked() {
-        WebElement sendEmailChecbox = browser.driver.findElement(By.name(Const.ParamsNames.SEND_SUBMISSION_EMAIL));
-        return sendEmailChecbox.isSelected();
+        WebElement sendEmailCheckbox = browser.driver.findElement(By.name(Const.ParamsNames.SEND_SUBMISSION_EMAIL));
+        return sendEmailCheckbox.isSelected();
     }
 
     public void clickSubmitButton() {
