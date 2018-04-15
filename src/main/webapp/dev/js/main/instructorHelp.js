5a1e648d03d9919a7b62a1a2932dfb763c417bee
Cara Leong <heycraa@gmail.com>
2018-04-15 13:11:49 +0800

    Update instructorHelp.js
    Added ability to expand a collapsed panel if an inbound link is trying
    to access a panel using an anchor. For example, if trying to access
    instructorHelp#question-contrib, the question-contrib panel will now
    expand once the document loads.
    
    Refactored and added method documentation.

--- src/main/webapp/dev/js/main/instructorHelp.js
+++ src/main/webapp/dev/js/main/instructorHelp.js
@@ -1,4 +1,30 @@
-$('.collapse-link').on('click', function () {
-    const x = this.getAttribute('data-target');
-    $(x).collapse('show');
+/**
+ * If the incoming link to this site targets a panel,
+ * expands the targeted collapsed panel
+ */
+function openAnchorPanel() {
+    if (window.location.hash) {
+        const target = $('body').find(window.location.hash);
+        if ($(target).hasClass('panel')) {
+            const targetCollapse = $(target).find('.collapse');
+            $(targetCollapse).collapse('show');
+        }
+    }
+}
+
+/**
+ * To be used by links on the instructorHelp.jsp page itself.
+ * If a link is of the class 'collapse-link', expands the
+ * collapse with the ID 'data-target'.
+ */
+function handleCollapseLinks() {
+    $('.collapse-link').on('click', function () {
+        const x = this.getAttribute('data-target');
+        $(x).collapse('show');
+    });
+}
+
+$(document).ready(() => {
+    openAnchorPanel();
+    handleCollapseLinks();
 });
