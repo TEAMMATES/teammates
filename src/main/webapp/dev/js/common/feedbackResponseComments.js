5444ee6094b827557e69cdf0c5d421cf36e6e563
Nashmia Riaz <nashmia-riaz@hotmail.com>
2018-04-16 21:13:31 +0500

    changes to take care of asynchronous tinymce init

--- src/main/webapp/dev/js/common/feedbackResponseComments.js
+++ src/main/webapp/dev/js/common/feedbackResponseComments.js
@@ -169,17 +169,19 @@ function showResponseCommentAddForm(recipientIndex, giverIndex, qnIndex, section
     }
     $(`#showResponseCommentAddForm${id}`).show();
 
-    if (typeof richTextEditorBuilder !== 'undefined') {
-        /* eslint-disable camelcase */ // The property names are determined by external library (tinymce)
-        richTextEditorBuilder.initEditor(`#responseCommentAddForm${id}`, {
+    const responseCommentAddFormId = `responseCommentAddForm${id}`;
+    const responseCommentEditor = tinymce.get(responseCommentAddFormId);
+    if (responseCommentEditor === null) {
+        richTextEditorBuilder.initEditor(responseCommentAddFormId, {
             inline: true,
         });
-        /* eslint-enable camelcase */
+    } else {
+        responseCommentEditor.setContent('');
     }
+    
     saveInitialVisibilityOfCheckboxes(`showResponseCommentAddForm${id}`,
             $(`#showResponseCommentAddForm${id}`).children('.responseCommentAddForm'));
     $(`#responseCommentAddForm${id}`).focus();
-    tinymce.get(`responseCommentAddForm${id}`).setContent('');
 }
 
 function hideResponseCommentAddForm(recipientIndex, giverIndex, qnIndex, sectionIndex) {
