9efe6e79c2a5fc359bc81562619103b015403489
Shradheya Thakre <tshradheya@gmail.com>
2018-05-22 16:15:21 +0800

    Fixed static analysis issues

--- src/test/java/teammates/test/cases/browsertests/ScrollUnitTest.java
+++ src/test/java/teammates/test/cases/browsertests/ScrollUnitTest.java
@@ -3,18 +3,23 @@ package teammates.test.cases.browsertests;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import teammates.common.util.ThreadHelper;
 import teammates.test.driver.TestProperties;
 
+/**
+ * Unit Test for functions in scrollTo.js
+ * All tests are done on About Us page to avoid regression
+ */
 public class ScrollUnitTest extends BaseUiTestCase {
 
-    public final String SCROLL_TO_POSITION_SCRIPT = "if (arguments[1] === undefined || arguments[1] === null) {\n" +
-            "        $(window).scrollTop(arguments[0]);\n" +
-            "    } else {\n" +
-            "        $('html, body').animate({ scrollTop: arguments[0] }, arguments[1]);\n" +
-            "    }";
+    public static final String SCROLL_TO_POSITION_SCRIPT = "if (arguments[1] === undefined || arguments[1] === null) {\n"
+            + "        $(window).scrollTop(arguments[0]);\n"
+            + "    } else {\n"
+            + "        $('html, body').animate({ scrollTop: arguments[0] }, arguments[1]);\n"
+            + "    }";
 
-    public final String VERTICAL_SCROLL_VALUE = "return window.scrollY";
+    public static final String VERTICAL_SCROLL_VALUE = "return window.scrollY";
 
     @Override
     protected void prepareTestData() {
@@ -32,7 +37,7 @@ public class ScrollUnitTest extends BaseUiTestCase {
         scrollToTop_aboutUsPage_scrolledCorrectly();
     }
 
-    public void scrollToPosition_aboutUsPage_scrolledCorrectly() {
+    private void scrollToPosition_aboutUsPage_scrolledCorrectly() {
         int scrollPos = 1500;
         String duration = "400"; // 400ms which gives enough time to scroll
 
@@ -43,7 +48,7 @@ public class ScrollUnitTest extends BaseUiTestCase {
         assertEquals((long) scrollPos, javascriptExecutor.executeScript(VERTICAL_SCROLL_VALUE));
     }
 
-    public void scrollToTop_aboutUsPage_scrolledCorrectly() {
+    private void scrollToTop_aboutUsPage_scrolledCorrectly() {
         String duration = "400"; // 400ms which gives enough time to scroll
 
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) browser.driver;
