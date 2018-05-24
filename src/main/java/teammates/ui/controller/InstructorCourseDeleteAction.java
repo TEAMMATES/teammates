51d505ccbbb5745cd76d3ea6b117d4ab5f302aae
dalessr <dale.sunshengran@gmail.com>
2018-05-24 14:49:01 +0800

    Set deletedAt value instead of actually deleting the course

--- src/main/java/teammates/ui/controller/InstructorCourseDeleteAction.java
+++ src/main/java/teammates/ui/controller/InstructorCourseDeleteAction.java
@@ -21,7 +21,7 @@ public class InstructorCourseDeleteAction extends Action {
                                     Const.ParamsNames.INSTRUCTOR_PERMISSION_MODIFY_COURSE);
 
         /* Delete the course and setup status to be shown to user and admin */
-        logic.deleteCourse(idOfCourseToDelete);
+        logic.moveCourseToRecovery(idOfCourseToDelete);
         String statusMessage = String.format(Const.StatusMessages.COURSE_DELETED, idOfCourseToDelete);
         statusToUser.add(new StatusMessage(statusMessage, StatusMessageColor.SUCCESS));
         statusToAdmin = "Course deleted: " + idOfCourseToDelete;
