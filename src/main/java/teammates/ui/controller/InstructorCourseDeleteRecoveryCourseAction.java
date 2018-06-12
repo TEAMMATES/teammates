5f10a70d719b2a6e7c89d41aa8d8e66ba36c5ebc
dalessr <dale.sunshengran@gmail.com>
2018-06-12 11:43:45 +0800

    Rename action files for delete/restore deleted courses

---
+++ src/main/java/teammates/ui/controller/InstructorCourseDeleteRecoveryCourseAction.java
@@ -0,0 +1,46 @@
+package teammates.ui.controller;
+
+import teammates.common.util.Assumption;
+import teammates.common.util.Const;
+import teammates.common.util.StatusMessage;
+import teammates.common.util.StatusMessageColor;
+
+/**
+ * Action: Permanently delete a course from Recycle Bin for an instructor.
+ */
+public class InstructorCourseDeleteRecoveryCourseAction extends Action {
+
+    @Override
+    public ActionResult execute() {
+
+        String idOfCourseToDelete = getRequestParamValue(Const.ParamsNames.COURSE_ID);
+        Assumption.assertPostParamNotNull(Const.ParamsNames.COURSE_ID, idOfCourseToDelete);
+
+        gateKeeper.verifyAccessible(logic.getInstructorForGoogleId(idOfCourseToDelete, account.googleId),
+                logic.getCourse(idOfCourseToDelete),
+                Const.ParamsNames.INSTRUCTOR_PERMISSION_MODIFY_COURSE);
+
+        try {
+            /* Permanently delete the course and setup status to be shown to user and admin */
+            logic.deleteCourse(idOfCourseToDelete);
+            String statusMessage = String.format(Const.StatusMessages.COURSE_DELETED, idOfCourseToDelete);
+            statusToUser.add(new StatusMessage(statusMessage, StatusMessageColor.SUCCESS));
+            statusToAdmin = "Course deleted: " + idOfCourseToDelete;
+        } catch (Exception e) {
+            setStatusForException(e);
+        }
+
+        if (isRedirectedToHomePage()) {
+            return createRedirectResult(Const.ActionURIs.INSTRUCTOR_HOME_PAGE);
+        }
+        return createRedirectResult(Const.ActionURIs.INSTRUCTOR_COURSES_PAGE);
+    }
+
+    /**
+     * Checks if the action is executed in homepage or 'Recovery' page based on its redirection.
+     */
+    private boolean isRedirectedToHomePage() {
+        String nextUrl = getRequestParamValue(Const.ParamsNames.NEXT_URL);
+        return nextUrl != null && nextUrl.equals(Const.ActionURIs.INSTRUCTOR_HOME_PAGE);
+    }
+}
---
+++ src/main/java/teammates/ui/controller/InstructorCourseRestoreAllRecoveryCoursesAction.java
@@ -0,0 +1,47 @@
+package teammates.ui.controller;
+
+import java.util.List;
+
+import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.util.Const;
+import teammates.common.util.StatusMessage;
+import teammates.common.util.StatusMessageColor;
+import teammates.ui.pagedata.InstructorCoursesPageData;
+
+/**
+ * Action: Restore all courses from Recycle Bin for an instructor.
+ */
+public class InstructorCourseRestoreAllRecoveryCoursesAction extends Action {
+
+    @Override
+    public ActionResult execute() {
+
+        gateKeeper.verifyInstructorPrivileges(account);
+
+        InstructorCoursesPageData data = new InstructorCoursesPageData(account, sessionToken);
+
+        try {
+            /* Restore all courses and setup status to be shown to user and admin */
+            List<InstructorAttributes> instructorList = logic.getInstructorsForGoogleId(data.account.googleId);
+            logic.restoreAllCoursesFromRecovery(instructorList);
+            String statusMessage = Const.StatusMessages.COURSE_ALL_RESTORED;
+            statusToUser.add(new StatusMessage(statusMessage, StatusMessageColor.SUCCESS));
+            statusToAdmin = "All courses restored";
+        } catch (Exception e) {
+            setStatusForException(e);
+        }
+
+        if (isRedirectedToHomePage()) {
+            return createRedirectResult(Const.ActionURIs.INSTRUCTOR_HOME_PAGE);
+        }
+        return createRedirectResult(Const.ActionURIs.INSTRUCTOR_COURSES_PAGE);
+    }
+
+    /**
+     * Checks if the action is executed in homepage or 'Recovery' page based on its redirection.
+     */
+    private boolean isRedirectedToHomePage() {
+        String nextUrl = getRequestParamValue(Const.ParamsNames.NEXT_URL);
+        return nextUrl != null && nextUrl.equals(Const.ActionURIs.INSTRUCTOR_HOME_PAGE);
+    }
+}
