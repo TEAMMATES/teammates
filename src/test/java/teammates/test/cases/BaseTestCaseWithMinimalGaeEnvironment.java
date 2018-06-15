e04152e4f1d97c4394375e9adf1654f28bd4cd5b
Sukanta Roy <sukanta.raj.27@gmail.com>
2018-06-15 21:31:19 +0530

    Add static method in GaeSimulation

--- src/test/java/teammates/test/cases/BaseTestCaseWithMinimalGaeEnvironment.java
+++ src/test/java/teammates/test/cases/BaseTestCaseWithMinimalGaeEnvironment.java
@@ -1,16 +1,11 @@
 package teammates.test.cases;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
-import teammates.test.driver.TestProperties;
+import teammates.test.driver.GaeSimulation;
 
 /**
  * Base class for all test cases which require a minimal GAE API environment registered.
@@ -21,24 +16,13 @@ public class BaseTestCaseWithMinimalGaeEnvironment extends BaseTestCase {
 
     @BeforeClass
     public void setUpGae() {
-        helper.setEnvAttributes(generateEnvironmentAttributesWithApplicationHostnameSet());
+        helper.setEnvAttributes(GaeSimulation.getEnvironmentAttributesWithApplicationHostname());
         helper.setUp();
     }
 
     @AfterClass
     public void tearDownGae() {
         helper.tearDown();
-    }
-
-    public Map<String, Object> generateEnvironmentAttributesWithApplicationHostnameSet() {
-        Map<String, Object> attributes = new HashMap<>();
-        try {
-            attributes.put("com.google.appengine.runtime.default_version_hostname",
-                    new URL(TestProperties.TEAMMATES_URL).getAuthority());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-        return attributes;
     }
 
 }
