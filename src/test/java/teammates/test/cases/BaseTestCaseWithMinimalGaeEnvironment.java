e6e269d0b1d319f55b83aec8431832f30b6ce067
Sukanta Roy <sukanta.raj.27@gmail.com>
2018-06-15 19:37:27 +0530

    Add the hostname setup logic to BaseTestCaseWithMinimalGaeEnvironment

--- src/test/java/teammates/test/cases/BaseTestCaseWithMinimalGaeEnvironment.java
+++ src/test/java/teammates/test/cases/BaseTestCaseWithMinimalGaeEnvironment.java
@@ -1,9 +1,16 @@
 package teammates.test.cases;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+import teammates.test.driver.TestProperties;
 
 /**
  * Base class for all test cases which require a minimal GAE API environment registered.
@@ -14,12 +21,24 @@ public class BaseTestCaseWithMinimalGaeEnvironment extends BaseTestCase {
 
     @BeforeClass
     public void setUpGae() {
+        helper.setEnvAttributes(generateEnvironmentAttributesWithApplicationHostnameSet());
         helper.setUp();
     }
 
     @AfterClass
     public void tearDownGae() {
         helper.tearDown();
+    }
+
+    public Map<String, Object> generateEnvironmentAttributesWithApplicationHostnameSet() {
+        Map<String, Object> attributes = new HashMap<>();
+        try {
+            attributes.put("com.google.appengine.runtime.default_version_hostname",
+                    new URL(TestProperties.TEAMMATES_URL).getAuthority());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        return attributes;
     }
 
 }
