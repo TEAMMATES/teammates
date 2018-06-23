a07eb522fffe6561f3db8512a633103b0321ff67
Nidhi <nidhi98gupta@gmail.com>
2018-06-24 03:16:00 +0530

    Add unit test

---
+++ src/test/java/teammates/test/cases/datatransfer/FeedbackSessionResultsBundleTest.java
@@ -0,0 +1,60 @@
+package teammates.test.cases.datatransfer;
+
+import org.testng.annotations.Test;
+import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.FeedbackSessionResultsBundle;
+import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
+import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
+import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
+import teammates.common.exception.EntityDoesNotExistException;
+import teammates.logic.core.FeedbackQuestionsLogic;
+import teammates.logic.core.FeedbackResponsesLogic;
+import teammates.logic.core.FeedbackSessionsLogic;
+import teammates.storage.entity.FeedbackResponse;
+import teammates.test.cases.logic.BaseLogicTest;
+import teammates.test.driver.BackDoor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SUT: {@link teammates.common.datatransfer.FeedbackSessionResultsBundle}.
+ */
+public class FeedbackSessionResultsBundleTest extends BaseLogicTest {
+
+    private static FeedbackSessionsLogic fsLogic = FeedbackSessionsLogic.inst();
+    private static FeedbackQuestionsLogic fqLogic = FeedbackQuestionsLogic.inst();
+    private static FeedbackResponsesLogic frLogic = FeedbackResponsesLogic.inst();
+
+    @Test
+    public void testGetAllResponses() throws EntityDoesNotExistException {
+        DataBundle responseBundle = loadDataBundle("/FeedbackSessionQuestionTypeTest.json");
+        removeAndRestoreDataBundle(responseBundle);
+
+        ______TS("test getAllResponsesForQuestion function");
+
+        FeedbackSessionAttributes session =
+                responseBundle.feedbackSessions.get("contribSession");
+        StudentAttributes student =
+                responseBundle.students.get("student1InCourse1");
+        FeedbackSessionResultsBundle results =
+                fsLogic.getFeedbackSessionResultsForStudent(session.getFeedbackSessionName(),
+                        session.getCourseId(), student.email);
+        FeedbackQuestionAttributes fqa = responseBundle.feedbackQuestions.get("qn1InSession5InCourse1");
+        FeedbackQuestionAttributes questionToGet = fqLogic.getFeedbackQuestion(fqa.feedbackSessionName, fqa.courseId, fqa.questionNumber);
+        List<FeedbackResponseAttributes> allResponses =
+                results.getActualResponses(questionToGet);
+        List<String> allResponsesString = new ArrayList<>();
+
+        List<String> allExpectedResponses = new ArrayList<>();
+        FeedbackResponseAttributes fra;
+        for (int i =1; i<=4; i++) {
+            fra = responseBundle.feedbackResponses.get("response" + i + "ForQ1S5C1");
+            allExpectedResponses.add(frLogic.getFeedbackResponse(questionToGet.getId(), fra.giver, fra.recipient).toString());
+            allResponsesString.add(allResponses.get(i-1).toString());
+        }
+        assertEquals(4, allResponses.size());
+        assertTrue("Responses are missing", allResponsesString.containsAll(allExpectedResponses));
+    }
+}
