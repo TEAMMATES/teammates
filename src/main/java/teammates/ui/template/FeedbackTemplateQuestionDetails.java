7c57d198b19d22f8b6f72f9085fbfbd83553d856
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2018-06-24 15:16:39 +0800

    handle the get functions of the class to be populated in the modal tag

--- src/main/java/teammates/ui/template/FeedbackTemplateQuestionDetails.java
+++ src/main/java/teammates/ui/template/FeedbackTemplateQuestionDetails.java
@@ -23,4 +23,45 @@ public class FeedbackTemplateQuestionDetails {
         this.qnDescription = getQnDescription();
     }
 
+    public int getQnNumber() {
+        return qnNumber;
+    }
+
+    public String getQnType() {
+        return qnType;
+    }
+
+    public String getQnText() {
+        return qnText;
+    }
+
+    public String getQnFeedbackPath() {
+        return qnFeedbackPath;
+    }
+
+    public String getQnVisibilityOption() {
+        return qnVisibilityOption;
+    }
+
+    public List<String> getQnVisibilityHints() {
+        return qnVisibilityHints;
+    }
+
+    public String getQnDescription() {
+        switch (qnNumber) {
+        case 1:
+            return "Use <b>peer estimates</b> to determine the <b>work distribution percentage</b> "
+                    + "among team members in <b>a team activity</b>";
+        case 2:
+            return "Ask each student to describe something related to themselves i.e., <b>self-reflection</b>";
+        case 3:
+            return "Ask each student to give <b>confidential peer feedback (qualitative)</b> to other <b>team members</b>";
+        case 4:
+            return "Ask each student to give <b>confidential feedback (qualitative)</b> about the team behaviour";
+        case 5:
+            return "Ask each student to give <b>comments about other team members, in confidence</b>";
+        default:
+            return null;
+        }
+    }
 }
