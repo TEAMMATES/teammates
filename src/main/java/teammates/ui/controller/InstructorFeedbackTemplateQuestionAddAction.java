a5a9f027babd4d39675debf82b4219a8a2dc1ca7
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2018-06-24 15:46:06 +0800

    ensure that the current session exist and the instructor has the right to access the details in the feedback session

--- src/main/java/teammates/ui/controller/InstructorFeedbackTemplateQuestionAddAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackTemplateQuestionAddAction.java
@@ -4,5 +4,12 @@ public class InstructorFeedbackTemplateQuestionAddAction extends Action {
 
     @Override
     protected ActionResult execute() {
+        String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
+        String feedbackSessionName = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
+        InstructorAttributes instructorDetailForCourse = logic.getInstructorForGoogleId(courseId, account.googleId);
+
+        gateKeeper.verifyAccessible(instructorDetailForCourse,
+                logic.getFeedbackSession(feedbackSessionName, courseId),
+                false, Const.ParamsNames.INSTRUCTOR_PERMISSION_MODIFY_SESSION);
     }
 }
