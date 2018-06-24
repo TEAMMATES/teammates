1d247817ed03fdd98d5cb5be8ebfcf654dc0cf14
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2018-06-24 15:15:47 +0800

    initialise the variables of FeedbackTemplateQuestionDetails to be used for the template modal tag

--- src/main/java/teammates/ui/template/FeedbackTemplateQuestionDetails.java
+++ src/main/java/teammates/ui/template/FeedbackTemplateQuestionDetails.java
@@ -1,6 +1,15 @@
 package teammates.ui.template;
 
+import java.util.List;
+
 public class FeedbackTemplateQuestionDetails {
 
+    private int qnNumber;
+    private String qnType;
+    private String qnDescription;
+    private String qnText;
+    private String qnFeedbackPath;
+    private String qnVisibilityOption;
+    private List<String> qnVisibilityHints;
 
 }
