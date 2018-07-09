c3cd19e8fb2119e7724bb9a07ba8580e8dedbfdc
Shradheya Thakre <tshradheya@gmail.com>
2018-07-09 14:09:23 +0800

    CI  fail?

--- src/test/java/teammates/test/cases/BaseTestCase.java
+++ src/test/java/teammates/test/cases/BaseTestCase.java
@@ -45,7 +45,7 @@ public class BaseTestCase {
      */
     @BeforeSuite
     public void setLocale() {
-        Locale.setDefault(new Locale("en", "US"));
+        //Locale.setDefault(new Locale("en", "US"));
     }
 
     @BeforeClass
