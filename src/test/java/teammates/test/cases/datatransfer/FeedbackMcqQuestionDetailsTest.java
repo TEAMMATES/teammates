2aade0e68628b4062d1d7f1f506add173700b8b2
Shradheya Thakre <tshradheya@gmail.com>
2018-07-10 13:44:09 +0800

    Refactored test to follow convention

--- src/test/java/teammates/test/cases/datatransfer/FeedbackMcqQuestionDetailsTest.java
+++ src/test/java/teammates/test/cases/datatransfer/FeedbackMcqQuestionDetailsTest.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.FeedbackParticipantType;
+import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.datatransfer.questions.FeedbackMcqQuestionDetails;
+import teammates.common.datatransfer.questions.FeedbackQuestionDetails;
 import teammates.common.datatransfer.questions.FeedbackQuestionType;
+import teammates.common.datatransfer.questions.FeedbackResponseDetails;
 import teammates.common.util.Const;
 import teammates.test.cases.BaseTestCase;
 
@@ -410,5 +414,88 @@ public class FeedbackMcqQuestionDetailsTest extends BaseTestCase {
         assertFalse(mcqDetails.hasAssignedWeights());
         assertTrue(mcqDetails.getMcqWeights().isEmpty());
         assertEquals(0.0, mcqDetails.getMcqOtherWeight());
+    }
+
+    @Test
+    public void testCreateQuestionDetails_mcqTypeQuestion_createdSuccessfully() {
+        HashMap<String, String[]> requestParameters = new HashMap<>();
+
+        requestParameters.put("questiontext", new String[] { "MCQ" });
+        requestParameters.put("mcqGeneratedOptions", new String[] {FeedbackParticipantType.NONE.toString()});
+        requestParameters.put("noofchoicecreated", new String[] { "1" });
+        requestParameters.put("mcqOption-0", new String[] { "mcq option" });
+
+        FeedbackQuestionDetails feedbackQuestionDetails =
+                FeedbackQuestionDetails.createQuestionDetails(requestParameters, FeedbackQuestionType.MCQ);
+
+        assertEquals(feedbackQuestionDetails.getQuestionType(), FeedbackQuestionType.MCQ);
+        assertTrue(feedbackQuestionDetails instanceof FeedbackMcqQuestionDetails);
+        assertEquals(feedbackQuestionDetails.getQuestionText(), "MCQ");
+    }
+
+    @Test
+    public void testGetQuestionWithExistingResponseSubmissionFormHtml_responsePresent_htmlTagsPresent() {
+        HashMap<String, String[]> requestParameters = new HashMap<>();
+
+        requestParameters.put("questiontext", new String[] { "MCQ" });
+        requestParameters.put("mcqGeneratedOptions", new String[] {FeedbackParticipantType.NONE.toString()});
+        requestParameters.put("noofchoicecreated", new String[] { "1" });
+        requestParameters.put("mcqOption-0", new String[] { "mcq option" });
+        requestParameters.put("questiontype-1", new String[] { "MCQ" });
+        requestParameters.put("responsetext-1-0", new String[] { "mcq option" });
+
+        FeedbackQuestionDetails feedbackQuestionDetails =
+                FeedbackQuestionDetails.createQuestionDetails(requestParameters, FeedbackQuestionType.MCQ);
+
+        FeedbackResponseDetails responseDetails =
+                FeedbackResponseDetails.createResponseDetails(
+                        new String[] { "mcq option" },
+                        FeedbackQuestionType.MCQ,
+                        feedbackQuestionDetails, requestParameters, 1, 0);
+
+        String result = feedbackQuestionDetails.getQuestionWithExistingResponseSubmissionFormHtml(true, 1,
+                0, "test.course", 1, responseDetails,
+                StudentAttributes.builder("test.course", "test", "test@gmail.com").build());
+        assertTrue(result.contains("checked")); // Checking if response is selected
+        assertTrue(result.contains("mcq option")); // Check if option is showed in HTML
+    }
+
+    @Test
+    public void testGetQuestionWithoutExistingResponseSubmissionFormHtml_noResponse_htmlTagsPresent() {
+        HashMap<String, String[]> requestParameters = new HashMap<>();
+
+        requestParameters.put("questiontext", new String[] { "MCQ" });
+        requestParameters.put("mcqGeneratedOptions", new String[] {FeedbackParticipantType.NONE.toString()});
+        requestParameters.put("noofchoicecreated", new String[] { "1" });
+        requestParameters.put("mcqOption-0", new String[] { "mcq option" });
+        requestParameters.put("questiontype-1", new String[] { "MCQ" });
+        requestParameters.put("responsetext-1-0", new String[] { "mcq option" });
+
+        FeedbackQuestionDetails feedbackQuestionDetails =
+                FeedbackQuestionDetails.createQuestionDetails(requestParameters, FeedbackQuestionType.MCQ);
+
+        String result = feedbackQuestionDetails.getQuestionWithoutExistingResponseSubmissionFormHtml(true, 1,
+                0, "test.course", 0,
+                StudentAttributes.builder("test.course", "test", "test@gmail.com").build());
+        assertFalse(result.contains("checked")); // Checking if response is not present now
+
+    }
+
+    @Test
+    public void testGetQuestionAdditionalInfo_additionalInfoPresent_htmlTagsPresent() {
+        HashMap<String, String[]> requestParameters = new HashMap<>();
+
+        requestParameters.put("questiontext", new String[] { "MCQ" });
+        requestParameters.put("mcqGeneratedOptions", new String[] {FeedbackParticipantType.NONE.toString()});
+        requestParameters.put("noofchoicecreated", new String[] { "1" });
+        requestParameters.put("mcqOption-0", new String[] { "mcq option" });
+        requestParameters.put("questiontype-1", new String[] { "MCQ" });
+
+        FeedbackQuestionDetails feedbackQuestionDetails =
+                FeedbackQuestionDetails.createQuestionDetails(requestParameters, FeedbackQuestionType.MCQ);
+
+        String result = feedbackQuestionDetails.getQuestionAdditionalInfoHtml(1, "");
+        assertTrue(result.contains("[more]"));
+        assertTrue(result.contains("<li>mcq option</li>"));
     }
 }
