0d46be8b4681f5eb4c6dd1ba8290a60e2cfb30ea
Shradheya Thakre <tshradheya@gmail.com>
2018-07-10 14:18:31 +0800

    Add test

--- src/test/java/teammates/test/cases/datatransfer/FeedbackRubricQuestionDetailsTest.java
+++ src/test/java/teammates/test/cases/datatransfer/FeedbackRubricQuestionDetailsTest.java
@@ -40,7 +40,6 @@ public class FeedbackRubricQuestionDetailsTest extends BaseTestCase {
         requestParams.put(Const.ParamsNames.FEEDBACK_QUESTION_RUBRIC_WEIGHTS_ASSIGNED, new String[] { "off" });
         requestParams.put("responsetext-1-0", new String[] { "0-0,1-1" });
 
-
         FeedbackQuestionDetails rubricQuestionDetails =
                 FeedbackRubricQuestionDetails.createQuestionDetails(requestParams, FeedbackQuestionType.RUBRIC);
 
@@ -51,6 +50,28 @@ public class FeedbackRubricQuestionDetailsTest extends BaseTestCase {
 
         String result = rubricQuestionDetails.getQuestionWithExistingResponseSubmissionFormHtml(true, 1,
                 0, "test.course", 0, rubricResponseDetails,
+                StudentAttributes.builder("test.course", "test", "test@gmail.com").build());
+
+        assertTrue(result.contains("id=\"rubricResponseTable-1-0\""));
+        assertTrue(result.contains("id=\"rubricResponse-1-0\""));
+    }
+
+    @Test
+    public void testGetQuestionWithoutExistingResponseSubmissionFormHtml_responsePresent_htmlTagsPresent() {
+
+        HashMap<String, String[]> requestParams = new HashMap<>();
+
+        requestParams.put("questiontype-1", new String[] { "RUBRIC" });
+        requestParams.put(Const.ParamsNames.FEEDBACK_QUESTION_TEXT, new String[] { "rubricQuestion" });
+        requestParams.put(Const.ParamsNames.FEEDBACK_QUESTION_RUBRIC_NUM_COLS, new String[] { "2" });
+        requestParams.put(Const.ParamsNames.FEEDBACK_QUESTION_RUBRIC_NUM_ROWS, new String[] { "2" });
+        requestParams.put(Const.ParamsNames.FEEDBACK_QUESTION_RUBRIC_WEIGHTS_ASSIGNED, new String[] { "off" });
+
+        FeedbackQuestionDetails rubricQuestionDetails =
+                FeedbackRubricQuestionDetails.createQuestionDetails(requestParams, FeedbackQuestionType.RUBRIC);
+
+        String result = rubricQuestionDetails.getQuestionWithoutExistingResponseSubmissionFormHtml(false, 1,
+                0, "test.course", 0,
                 StudentAttributes.builder("test.course", "test", "test@gmail.com").build());
 
         assertTrue(result.contains("id=\"rubricResponseTable-1-0\""));
