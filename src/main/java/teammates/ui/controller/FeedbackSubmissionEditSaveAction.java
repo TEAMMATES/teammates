63184c3cf9db792eb781814c4cd9b60431cd141a
Sukanta Roy <sukanta.raj.27@gmail.com>
2018-07-12 06:22:40 +0530

    Fix lint error

--- src/main/java/teammates/ui/controller/FeedbackSubmissionEditSaveAction.java
+++ src/main/java/teammates/ui/controller/FeedbackSubmissionEditSaveAction.java
@@ -3,7 +3,6 @@ package teammates.ui.controller;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
