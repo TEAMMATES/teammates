70fae8f987bd09773fcca4ef7c3e2945af7ab1d0
Jacob Li Peng Cheng <JacobLI@90-175.priv31.nus.edu.sg>
2018-07-13 15:44:01 +0800

    Initializes getActionUri and getAction function first

--- src/test/java/teammates/test/cases/action/InstructorFeedbackTemplateQuestionAddActionTest.java
+++ src/test/java/teammates/test/cases/action/InstructorFeedbackTemplateQuestionAddActionTest.java
@@ -1,5 +1,9 @@
 package teammates.test.cases.action;
 
+import org.testng.annotations.Test;
+import teammates.common.util.Const;
+import teammates.ui.controller.InstructorFeedbackTemplateQuestionAddAction;
+
 /**
  * SUT: {@link teammates.ui.controller.InstructorFeedbackTemplateQuestionAddAction}.
  */
@@ -7,10 +11,12 @@ public class InstructorFeedbackTemplateQuestionAddActionTest extends BaseActionT
 
     @Override
     protected String getActionUri() {
+        return Const.ActionURIs.INSTRUCTOR_FEEDBACK_TEMPLATE_QUESTION_ADD;
     }
 
     @Override
     protected InstructorFeedbackTemplateQuestionAddAction getAction(String[] params) {
+        return (InstructorFeedbackTemplateQuestionAddAction) gaeSimulation.getActionObject(getActionUri(), params);
     }
 
     @Override
