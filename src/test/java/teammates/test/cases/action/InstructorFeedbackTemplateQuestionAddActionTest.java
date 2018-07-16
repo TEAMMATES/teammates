147e588ae4f7102a124c8e797f8004afa3d6fd3c
Jacob Li Peng Cheng <JacobLI@90-201.priv31.nus.edu.sg>
2018-07-17 00:32:49 +0800

    no message

---
+++ src/test/java/teammates/test/cases/action/InstructorFeedbackTemplateQuestionAddActionTest.java
@@ -0,0 +1,27 @@
+package teammates.test.cases.action;
+
+/**
+ * SUT: {@link teammates.ui.controller.InstructorFeedbackTemplateQuestionAddAction}.
+ */
+public class InstructorFeedbackTemplateQuestionAddActionTest extends BaseActionTest {
+
+    @Override
+    protected String getActionUri() {
+    }
+
+    @Override
+    protected InstructorFeedbackTemplateQuestionAddAction getAction(String[] params) {
+    }
+
+    @Override
+    @Test
+    public void testExecuteAndPostProcess() {
+
+    }
+
+    @Override
+    @Test
+    public void testAccessControl() {
+
+    }
+}
