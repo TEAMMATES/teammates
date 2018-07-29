71cbd4035099b87d55bfd3d9d2623688d2d8a5c0
tanhengyeow <E0032242@u.nus.edu>
2018-07-29 23:26:08 +0800

    Removed unnecessary parameter

--- src/test/java/teammates/test/cases/action/InstructorCourseEnrollAjaxPageActionTest.java
+++ src/test/java/teammates/test/cases/action/InstructorCourseEnrollAjaxPageActionTest.java
@@ -65,7 +65,6 @@ public class InstructorCourseEnrollAjaxPageActionTest extends BaseActionTest {
         InstructorAttributes instructor = typicalBundle.instructors.get("instructor1OfCourse1");
         String[] submissionParams = new String[] {
                 Const.ParamsNames.COURSE_ID, instructor.courseId,
-                Const.ParamsNames.COURSE_INDEX, "1",
         };
         verifyOnlyInstructorsOfTheSameCourseCanAccess(submissionParams);
     }
