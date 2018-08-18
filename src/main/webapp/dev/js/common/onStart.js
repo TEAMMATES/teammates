023c97495cff7c8733a3818b96be418ce075c7a4
mg14777 <mg14777@my.bristol.ac.uk>
2018-08-18 10:22:19 +0530

    use exponentiation in Number.EPSILON polyfill

--- src/main/webapp/dev/js/common/onStart.js
+++ src/main/webapp/dev/js/common/onStart.js
@@ -24,11 +24,9 @@ if (!String.prototype.includes) {
 /**
  * Polyfills the Number.EPSILON property finalized in ES6 for browsers that do not yet support the property.
  */
-/* eslint-disable no-restricted-properties */ // needed to avoid ** exponentiation operator since Math.pow is more clear
 if (!Number.EPSILON) {
-    Number.EPSILON = Math.pow(2, -52);
+    Number.EPSILON = 2 ** -52;
 }
-/* eslint-enable no-restricted-properties */
 /* eslint-enable no-extend-native */
 
 /**
