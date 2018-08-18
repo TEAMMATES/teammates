93a0a03686eb4e7cd26ddb49f76748a9fa726d15
Mudit Gupta <mg14777@my.bristol.ac.uk>
2018-08-18 21:28:49 +0530

    [#9052] InstructorHelpPageUiTest failing on AppVeyor master (#9055)

--- src/main/webapp/dev/js/main/instructorCourses.js
+++ src/main/webapp/dev/js/main/instructorCourses.js
@@ -100,7 +100,7 @@ $(document).ready(() => {
             url: `${$(this).attr('action')}?${formData}`,
             beforeSend() {
                 $('#coursesList').html(
-                        '<img height="75" width="75" class="margin-center-horizontal" src="/images/ajax-preload.gif"/>'
+                        '<img height="75" width="75" class="margin-center-horizontal" src="/images/ajax-preload.gif"/>',
                 );
                 isFetchingCourses = true;
             },
@@ -110,7 +110,7 @@ $(document).ready(() => {
                 $('#coursesList').html('');
                 setStatusMessage(
                         'Courses could not be loaded. Click <a href="javascript:;" id="retryAjax">here</a> to retry.',
-                        BootstrapContextualColors.WARNING
+                        BootstrapContextualColors.WARNING,
                 );
                 $('#retryAjax').click((ev) => {
                     ev.preventDefault();
