32b11a3b9f59da409b96ceb09a790308be75453b
Wilson Kurniawan <wilson@visenze.com>
2018-08-28 20:11:00 +0800

    [#9079] Set up Angular 6 project (#9080)
    * Set up minimal Angular 6 project
    
    * Migrate static pages, disregard design
    
    * Change minimum browser versions
    
    Minimum versions are taken based on Angular and Bootstrap 4's support.
    
    * Fix tracking website configs
    
    * Use jsonlint-cli to validate json
    
    This frees up our dependency to ESLint which is an overkill if only used for linting JSON.
    
    * Move Java and SCSS space linting to their own lint tools
    
    * Use tslint-config-airbnb
    
    * Remove references to JavaScript and JSP in docs
    
    * Update setup guide
    
    * Sort out TSLint rules and fix violations
    
    * Separate CI test to component tests and E2E tests
    
    Component tests: white-box tests
    E2E tests: black-box tests, like how the current browser tests have always been
    
    * Upgrade Selenium to 3.14.0
    
    * Update some IntelliJ-specific values
    
    Ref: https://github.com/xpdavid/teammates/commit/76e59d1a0b7f999407510eda832e171a1749fa58
    
    * Update build instructions
    
    * Add placeholder Selenium test

---
+++ src/web/app/pages-static/usermap-page/usermap-page.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Usermap page.
+ */
+@Component({
+  selector: 'tm-usermap-page',
+  templateUrl: './usermap-page.component.html',
+  styleUrls: ['./usermap-page.component.scss'],
+})
+export class UsermapPageComponent implements OnInit {
+
+  /**
+   * The date in which the usermap is last updated.
+   */
+  lastUpdated: string = '';
+
+  /**
+   * The number of institutions.
+   */
+  nInstitutions: number = 0;
+
+  /**
+   * The number of countries.
+   */
+  nCountries: number = 0;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get('./assets/data/userMapData.json').subscribe((res: any) => {
+      this.lastUpdated = res.lastUpdated;
+      this.nInstitutions = 0;
+      const dataTable: (string | number)[][] = [['Country', 'Institutions']];
+      for (const country of Object.keys(res.institutes)) {
+        const nInstitutionsInCountry: number = res.institutes[country].length;
+        this.nInstitutions += nInstitutionsInCountry;
+        dataTable.push([country, nInstitutionsInCountry]);
+      }
+      this.nCountries = Object.keys(res.institutes).length;
+
+      const { google }: any = window as any;
+
+      google.charts.load('current', {
+        packages: ['geochart'],
+      });
+
+      google.charts.setOnLoadCallback(() => {
+        const data: any = google.visualization.arrayToDataTable(dataTable);
+        const chart: any = new google.visualization.GeoChart(document.getElementById('world-map'));
+        chart.draw(data, {});
+      });
+
+    });
+  }
+
+}
---
+++ src/web/typings.d.ts
@@ -0,0 +1,1 @@
+declare module 'ua-parser-js';
