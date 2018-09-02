8ea39e99ac5a5ae9f697b1522eb9cbfeb58be98a
Nidhi <nidhi98gupta@gmail.com>
2018-09-02 19:02:36 +0530

    Use new framework

--- src/client/java/teammates/client/scripts/DataMigrationForInstructorFeedbackResponseComments.java
+++ src/client/java/teammates/client/scripts/DataMigrationForInstructorFeedbackResponseComments.java
@@ -1,11 +1,11 @@
 package teammates.client.scripts;
 
 import java.io.IOException;
-import java.util.List;
 
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
 import teammates.common.datatransfer.FeedbackParticipantType;
-import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
-import teammates.storage.api.FeedbackResponseCommentsDb;
+import teammates.storage.entity.FeedbackResponseComment;
 
 /**
  * Script to set commentGiverType as INSTRUCTOR in all comments by instructor.
@@ -14,59 +14,43 @@ import teammates.storage.api.FeedbackResponseCommentsDb;
  */
 
 public class DataMigrationForInstructorFeedbackResponseComments extends
-        DataMigrationForEntities<FeedbackResponseCommentAttributes> {
-
-    private FeedbackResponseCommentsDb commentsDb = new FeedbackResponseCommentsDb();
+        DataMigrationWithCheckpointForEntities<FeedbackResponseComment> {
 
     public static void main(String[] args) throws IOException {
-        DataMigrationForInstructorFeedbackResponseComments migrator =
-                new DataMigrationForInstructorFeedbackResponseComments();
-        migrator.doOperationRemotely();
+        new DataMigrationForInstructorFeedbackResponseComments().doOperationRemotely();
     }
 
-    /**
-     * If true, the script will not perform actual data migration.
-     */
+    @Override
+    protected Query<FeedbackResponseComment> getFilterQuery() {
+        return ofy().load().type(FeedbackResponseComment.class);
+    }
+
     @Override
     protected boolean isPreview() {
         return true;
     }
 
-    /**
-     * Gets all the entities that are to be filtered for migration.
-     */
     @Override
-    protected List<FeedbackResponseCommentAttributes> getEntities() {
-        return commentsDb.getAllFeedbackResponseComments();
-    }
-
-    /**
-     * Checks whether data migration is needed.
-     */
-    @Override
-    protected boolean isMigrationNeeded(FeedbackResponseCommentAttributes entity) {
-        return entity.commentGiverType == null;
-    }
-
-    /**
-     * Prints information necessary for previewing the migration process.
-     */
-    @Override
-    protected void printPreviewInformation(FeedbackResponseCommentAttributes entity) {
-        // nothing to do
-    }
-
-    /**
-     * Migrates the entity.
-     */
-    @Override
-    protected void migrate(FeedbackResponseCommentAttributes entity) {
-        entity.commentGiverType = FeedbackParticipantType.INSTRUCTORS;
-        entity.isCommentFromFeedbackParticipant = false;
+    protected String getLastPositionOfCursor() {
+        return "";
     }
 
     @Override
-    protected void postAction() {
-        // nothing to do
+    protected int getCursorInformationPrintCycle() {
+        return 100;
+    }
+
+    @Override
+    protected boolean isMigrationNeeded(Key<FeedbackResponseComment> entity) {
+        FeedbackResponseComment comment = ofy().load().key(entity).now();
+        return comment.getCommentGiverType() == null;
+    }
+
+    @Override
+    protected void migrateEntity(Key<FeedbackResponseComment> entity) {
+        FeedbackResponseComment comment = ofy().load().key(entity).now();
+        comment.setCommentGiverType(FeedbackParticipantType.INSTRUCTORS);
+        comment.setIsCommentFromFeedbackParticipant(false);
+        ofy().save().entity(comment).now();
     }
 }
