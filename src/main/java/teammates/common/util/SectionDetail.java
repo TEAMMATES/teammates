e3cfab46c4c55c7255e3d67a3d6b2126fd29a3c8
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2018-09-13 21:24:07 +0800

    implemented Enum for section details using SectionDetail.java

---
+++ src/main/java/teammates/common/util/SectionDetail.java
@@ -0,0 +1,18 @@
+package teammates.common.util;
+
+public enum SectionDetail {
+    EITHER("Show response if either the giver or evaluee is in the selected section"),
+    GIVER("Show response if the giver is in the selected section"),
+    EVALUEE("Show response if the evaluee is in the selected section"),
+    BOTH("Show response only if both are in the selected section");
+
+    private String sectionDetail;
+
+    SectionDetail(String sectionDetail) {
+        this.sectionDetail = sectionDetail;
+    }
+
+    public String getSectionDetail() {
+        return sectionDetail;
+    }
+}
