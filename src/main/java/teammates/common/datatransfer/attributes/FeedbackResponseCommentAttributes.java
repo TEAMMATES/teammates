13a09187a98e887ded1dd315334419b64a8e7dec
Krishnakantah Jayashree Chandrashekhar <kkanthjc@gmail.com>
2018-09-24 23:05:40 -0700

    9137 - The format of the identification string changed to feedbackResponseId followed by a forwardslash which is thenfollowed by timestamp. A colon seprates the timestamp from userId.

--- src/main/java/teammates/common/datatransfer/attributes/FeedbackResponseCommentAttributes.java
+++ src/main/java/teammates/common/datatransfer/attributes/FeedbackResponseCommentAttributes.java
@@ -176,11 +176,7 @@ public class FeedbackResponseCommentAttributes extends EntityAttributes<Feedback
 
     @Override
     public String getIdentificationString() {
-        return "IdentificationString ["
-                +"feedbackResponseId = " + feedbackResponseId
-                + ", createdAt = " + createdAt
-                + ", commentGiver = " + commentGiver
-                +"]";
+        return  feedbackResponseId + "/" + createdAt + ":" + commentGiver;
     }
 
     @Override
