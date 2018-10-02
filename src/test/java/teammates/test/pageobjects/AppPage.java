a8f41dced5d754c3e5acd005b4084f0ef6ddc9e4
Wilson Kurniawan <wkurniawan.92@gmail.com>
2018-10-03 01:03:47 +0800

    Remove org.junit.jupiter dependency and fall back to org.junit
    assertThrows and its related functions are manually defined

--- src/test/java/teammates/test/pageobjects/AppPage.java
+++ src/test/java/teammates/test/pageobjects/AppPage.java
@@ -4,10 +4,10 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
--- src/test/java/teammates/test/pageobjects/InstructorFeedbackResultsPage.java
+++ src/test/java/teammates/test/pageobjects/InstructorFeedbackResultsPage.java
@@ -2,8 +2,8 @@ package teammates.test.pageobjects;
 
 import static com.google.common.base.Preconditions.checkState;
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
