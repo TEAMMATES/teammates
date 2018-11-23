35ea28c1f20cada9e98a1c5e69dd489066c03e77
saturator22 <f.szczeski@gmail.com>
2018-11-23 13:09:09 +0100

    Fixed method getBackupIdentifier() with returning message and googleId while method is used

--- src/main/java/teammates/common/datatransfer/attributes/AccountAttributes.java
+++ src/main/java/teammates/common/datatransfer/attributes/AccountAttributes.java
@@ -4,10 +4,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-import teammates.common.util.FieldValidator;
-import teammates.common.util.JsonUtils;
-import teammates.common.util.SanitizationHelper;
-import teammates.common.util.StringHelper;
+import teammates.common.util.*;
 import teammates.storage.entity.Account;
 
 /**
@@ -171,7 +168,7 @@ public class AccountAttributes extends EntityAttributes<Account> {
 
     @Override
     public String getBackupIdentifier() {
-        return "Account";
+        return Const.SystemParams.ACCOUNT_ATTRIBUTES_BACKUP_LOG_MSG + this.googleId;
     }
 
     @Override
