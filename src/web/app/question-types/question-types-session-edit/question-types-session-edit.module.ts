1d152cdb658dc18db1e1af3d0d72cfa0e635b6c9
Wilson Kurniawan <wkurniawan.92@gmail.com>
2018-12-13 16:28:23 +0800

    Add base module for all question types

---
+++ src/web/app/question-types/question-types-session-edit/question-types-session-edit.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+/**
+ * Module for all question edit UI in session edit page.
+ */
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [],
+})
+export class QuestionTypesSessionEditModule { }
