69ce971469e4d2ca1216884ed6ee634a0f2bab7f
Tran Tien Dat <peter.trantiendat@gmail.com>
2018-12-31 22:00:07 +0700

    Rough initial frontend

--- src/web/app/pages-instructor/instructor-course-student-details-page/instructor-course-student-details-page.component.ts
+++ src/web/app/pages-instructor/instructor-course-student-details-page/instructor-course-student-details-page.component.ts
@@ -1,5 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpRequestService } from '../../../services/http-request.service';
+import { ErrorMessageOutput } from '../../message-output';
+import { StatusMessageService } from 'src/web/services/status-message.service';
+import { environment } from 'src/web/environments/environment';
+
+interface StudentAttributes {
+  email: string,
+  course: string,
+  name: string,
+  googleId: string,
+  lastName: string,
+  comments: string,
+  team: string,
+  section: string,
+  key: string
+}
+
+interface StudentProfile {
+  googleId: string,
+  shortName: string,
+  email: string,
+  institute: string,
+  nationality: string,
+  gender: string,
+  moreInfo: string,
+  pictureKey: string,
+  modifiedDate: Date
+}
+
+interface StudentDetails {
+  student: StudentAttributes,
+  studentProfile: StudentProfile,
+  hasSection: boolean
+}
 
 /**
  * Instructor course student details page.
@@ -11,14 +45,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class InstructorCourseStudentDetailsPageComponent implements OnInit {
 
-  user: string = '';
+  private backendUrl: string = environment.backendUrl;
 
-  constructor(private route: ActivatedRoute) { }
+  user: string = '';
+  student?: StudentAttributes;
+  studentProfile?: StudentProfile;
+
+  constructor(private route: ActivatedRoute, private httpRequestService: HttpRequestService,
+    private statusMessageService: StatusMessageService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((queryParams: any) => {
       this.user = queryParams.user;
+      this.loadStudentDetails(queryParams.courseid, queryParams.studentemail);
     });
+  }
+
+  loadStudentDetails(courseid: string, studentemail: string): void {
+    const paramMap: { [key: string]: string } = { courseid, studentemail };
+    this.httpRequestService.get('/courses/students/details', paramMap).subscribe((resp: StudentDetails) => {
+      this.student = resp.student;
+      this.studentProfile = resp.studentProfile;
+      this.studentProfile.modifiedDate = new Date(this.studentProfile.modifiedDate);
+    }, (resp: ErrorMessageOutput) => {
+      this.statusMessageService.showErrorMessage(resp.error.message);
+    });
+  }
+
+  getPictureUrl(pictureKey: string): string {
+    if (!pictureKey) {
+        return "/assets/images/profile_picture_default.png";
+    }
+    return `${this.backendUrl}/students/profilePic?blob-key=${pictureKey}`;
   }
 
 }
