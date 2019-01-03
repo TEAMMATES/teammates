04dd247e3902ad1ba47fe49c3c3680552b1d2501
Tran Tien Dat <peter.trantiendat@gmail.com>
2019-01-03 18:51:17 +0700

    [#9255] Migrate Instructor Course Student Details page to Angular (#9285)

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -965,6 +965,8 @@ public final class Const {
         public static final String JOIN = "/join";
         public static final String TIMEZONE = "/timezone";
 
+        public static final String COURSE_STUDENT_DETAILS = "/courses/students/details";
+        public static final String STUDENT_PROFILE_PICTURE = "/students/profilePic";
     }
 
     @Deprecated
--- src/main/java/teammates/ui/newcontroller/ActionFactory.java
+++ src/main/java/teammates/ui/newcontroller/ActionFactory.java
@@ -46,6 +46,8 @@ public class ActionFactory {
         map(ResourceURIs.SESSIONS_STATS, GET, GetSessionResponseStatsAction.class);
         map(ResourceURIs.JOIN, GET, GetCourseJoinStatusAction.class);
         map(ResourceURIs.JOIN, PUT, JoinCourseAction.class);
+        map(ResourceURIs.COURSE_STUDENT_DETAILS, GET, GetCourseStudentDetailsAction.class);
+        map(ResourceURIs.STUDENT_PROFILE_PICTURE, GET, GetStudentProfilePictureAction.class);
     }
 
     private static void map(String uri, String method, Class<? extends Action> actionClass) {
