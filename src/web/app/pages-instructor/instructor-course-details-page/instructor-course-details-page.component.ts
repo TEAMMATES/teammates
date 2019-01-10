a8e83642c89089af845768ed6e2761936f95ed91
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2019-01-10 17:06:46 +0800

    update constructors and data objects used for course details

--- src/web/app/pages-instructor/instructor-course-details-page/instructor-course-details-page.component.ts
+++ src/web/app/pages-instructor/instructor-course-details-page/instructor-course-details-page.component.ts
@@ -1,5 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from '../../../environments/environment';
+import { HttpRequestService } from '../../../services/http-request.service';
+import { StatusMessageService } from '../../../services/status-message.service';
+import { ErrorMessageOutput } from '../../message-output';
+
+interface StudentListStudentData {
+  studentName: string;
+  studentEmail: string;
+  studentStatus: string;
+  photoUrl: string;
+}
+
+interface StudentListTeamData {
+  teamName: string;
+  students: StudentListStudentData[];
+}
+
+interface StudentListSectionData {
+  sectionName: string;
+  teams: StudentListTeamData[];
+}
+
+interface CourseAttributes {
+  id: string;
+  name: string;
+}
+
+interface CourseStats {
+  sectionsTotal: string;
+  teamsTotal: string;
+  studentsTotal: string;
+}
+
+interface CourseDetailsBundle {
+  course: CourseAttributes;
+  stats: CourseStats;
+}
+
+interface InstructorAttributes {
+  googleId: string;
+  name: string;
+  email: string;
+  displayedName: string;
+  isArchived: boolean;
+  isDisplayedToStudents: boolean;
+}
+
+interface CourseInfo {
+  courseDetails: CourseDetailsBundle;
+  currentInstructor: InstructorAttributes;
+  instructors: InstructorAttributes[];
+  studentListHtmlTableAsString: string;
+  sections: StudentListSectionData[];
+  hasSection: boolean;
+}
 
 /**
  * Instructor course details page.
@@ -12,6 +68,12 @@ import { ActivatedRoute } from '@angular/router';
 export class InstructorCourseDetailsPageComponent implements OnInit {
 
   user: string = '';
+  courseDetails? : CourseDetailsBundle;
+  currentInstructor? : InstructorAttributes;
+  instructors? : InstructorAttributes[];
+  sections? : StudentListSectionData[];
+
+  private backendUrl: string = environment.backendUrl;
 
   constructor(private route: ActivatedRoute) { }
 
