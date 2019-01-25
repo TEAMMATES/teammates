391f34b086f010e355858017bc76241a7ca484c9
tanhengyeow <E0032242@u.nus.edu>
2019-01-25 22:14:02 +0800

    Implemented POST /students/profilePic
    No tests for this yet, refer to comment brought over from v6.

--- src/main/java/teammates/ui/newcontroller/ActionFactory.java
+++ src/main/java/teammates/ui/newcontroller/ActionFactory.java
@@ -83,6 +83,7 @@ public class ActionFactory {
         map(ResourceURIs.STUDENT_COURSE, GET, StudentGetCourseDetailsAction.class);
         map(ResourceURIs.STUDENT_PROFILE, GET, GetStudentProfileAction.class);
         map(ResourceURIs.STUDENT_PROFILE_FORM_URL, POST, PostStudentProfileFormUrlAction.class);
+        map(ResourceURIs.STUDENT_PROFILE_PICTURE, POST, PostStudentProfilePictureAction.class);
         map(ResourceURIs.STUDENT_PROFILE_UPDATE, PUT, PutStudentProfileAction.class);
         map(ResourceURIs.STUDENT_PROFILE_PICTURE, GET, GetStudentProfilePictureAction.class);
         map(ResourceURIs.STUDENT_COURSES, GET, GetStudentCoursesAction.class);
