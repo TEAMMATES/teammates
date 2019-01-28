e461369bbf3aea26e644bb0b1a5ed38fd8878b6e
Sun Shengran <dale.sunshengran@gmail.com>
2019-01-28 18:54:10 +0800

    [#9255] Migrate instructor courses page to Angular (#9351)

--- src/main/java/teammates/ui/controller/InstructorFeedbackDeleteAllSoftDeletedSessionsAction.java
+++ src/main/java/teammates/ui/controller/InstructorFeedbackDeleteAllSoftDeletedSessionsAction.java
@@ -7,7 +7,6 @@ import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.StatusMessage;
 import teammates.common.util.StatusMessageColor;
-import teammates.ui.pagedata.InstructorCoursesPageData;
 
 /**
  * Action: Permanently delete all sessions from Recycle Bin for an instructor.
@@ -17,8 +16,7 @@ public class InstructorFeedbackDeleteAllSoftDeletedSessionsAction extends Action
     @Override
     public ActionResult execute() {
 
-        InstructorCoursesPageData data = new InstructorCoursesPageData(account, sessionToken);
-        List<InstructorAttributes> instructorList = logic.getInstructorsForGoogleId(data.account.googleId);
+        List<InstructorAttributes> instructorList = logic.getInstructorsForGoogleId(account.googleId);
 
         for (InstructorAttributes instructor : instructorList) {
             List<FeedbackSessionAttributes> feedbackSessionList =
