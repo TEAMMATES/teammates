b2eeb422e0de57dcd837fba99c6648e747008420
Rahul Rajesh <rahul.rajesh@hotmail.com>
2019-01-29 10:17:47 +0800

    Add imports to module.ts file

--- src/web/app/pages-help/instructor-help-page/instructor-help-page.module.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-page.module.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { InstructorHelpPageComponent } from './instructor-help-page.component';
+
+import {
+  InstructorHelpStudentsSectionComponent,
+} from './instructor-help-students-section/instructor-help-students-section.component';
 
 /**
  * Module for instructor help page.
@@ -8,9 +15,12 @@ import { InstructorHelpPageComponent } from './instructor-help-page.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    NgbModule,
   ],
   declarations: [
     InstructorHelpPageComponent,
+    InstructorHelpStudentsSectionComponent,
   ],
   exports: [
     InstructorHelpPageComponent,
