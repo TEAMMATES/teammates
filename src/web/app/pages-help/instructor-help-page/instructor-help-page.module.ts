70ba0b3cbba5b1fc275d5f57a996ffce04d943bc
Rahul Rajesh <rahul.rajesh@hotmail.com>
2019-01-31 07:10:59 +0800

    Add routerlink to getting-started page

--- src/web/app/pages-help/instructor-help-page/instructor-help-page.module.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { InstructorHelpPageComponent } from './instructor-help-page.component';
 
@@ -18,6 +19,7 @@ import {
     CommonModule,
     FormsModule,
     NgbModule,
+    RouterModule
   ],
   declarations: [
     InstructorHelpPageComponent,
