8793a61a07c4bb618a0a376e9cf31b91d140be57
Ayush Chattoraj <ayush.chattoraj@u.nus.edu>
2019-01-31 15:02:15 +0800

    WIP

---
+++ src/web/app/pages-help/instructor-help-page/scroll-spy.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, Injectable, Input, EventEmitter, Output, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+    selector: '[scrollSpy]'
+})
+export class ScrollSpyDirective {
+    @Input() public spiedTags = [];
+    @Output() public sectionChange = new EventEmitter<string>();
+    private currentSection: string;
+
+    constructor(private _el: ElementRef) {}
+
+    @HostListener('scroll', ['$event'])
+    onScroll(event: any) {
+        let currentSection: string;
+        const children = this._el.nativeElement.children;
+        const scrollTop = event.target.scrollTop;
+        const parentOffset = event.target.offsetTop;
+        for (let i = 0; i < children.length; i++) {
+            const element = children[i];
+            if (this.spiedTags.some(spiedTag => spiedTag === element.tagName)) {
+                if ((element.offsetTop - parentOffset) <= scrollTop) {
+                    currentSection = element.id;
+                }
+            }
+        }
+        console.log("Here 1" + currentSection + this.currentSection);
+        if (currentSection !== this.currentSection) {
+            console.log("Here 2");
+            this.currentSection = currentSection;
+            this.sectionChange.emit(this.currentSection);
+        }
+    }
+
+}
