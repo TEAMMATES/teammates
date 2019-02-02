1ca8631f7b1b45690b045bd2e0dd5ba7cbdf2b1c
Ayush Chattoraj <ayush.chattoraj@u.nus.edu>
2019-02-02 17:18:16 +0800

    Fix missing import statement

--- src/web/app/pages-help/instructor-help-page/instructor-help-courses-section/instructor-help-courses-section.component.spec.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-courses-section/instructor-help-courses-section.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InstructorHelpCoursesSectionComponent } from './instructor-help-courses-section.component';
 
 describe('InstructorHelpCoursesSectionComponent', () => {
@@ -9,6 +10,7 @@ describe('InstructorHelpCoursesSectionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [InstructorHelpCoursesSectionComponent],
+      imports: [NgbModule],
     })
     .compileComponents();
   }));
