79a6a4b857c6424ea898f52fab37a3aba11f9b5e
Monika <manuela.monika97@gmail.com>
2019-02-02 14:32:14 +0800

    Add content for getting started page

--- src/web/app/pages-help/instructor-help-page/instructor-help-getting-started/instructor-help-getting-started.component.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-getting-started/instructor-help-getting-started.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from "../../../../environments/environment";
 
 @Component({
   selector: 'tm-instructor-help-getting-started',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./instructor-help-getting-started.component.scss']
 })
 export class InstructorHelpGettingStartedComponent implements OnInit {
+
+  readonly supportEmail: string = environment.supportEmail;
 
   constructor() { }
 
