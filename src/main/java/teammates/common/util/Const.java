0c04c36b8fd3e22f82ae8b9bb9dd4d751d7fdb29
tanhengyeow <E0032242@u.nus.edu>
2019-02-08 18:30:36 +0800

    Implement POST /student/profileFormUrl + Test

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -978,6 +978,8 @@ public final class Const {
         public static final String STUDENT_COURSE = "/student/course";
         public static final String STUDENT_PROFILE_PICTURE = "/students/profilePic";
         public static final String STUDENT_PROFILE = "/student/profile";
+        public static final String STUDENT_PROFILE_FORM_URL = "/student/profileFormUrl";
+        public static final String STUDENT_PROFILE_UPLOAD = "/student/profileUpload";
         public static final String STUDENT_COURSES = "/student/courses";
         public static final String STUDENTS_AND_FEEDBACK_SESSION_DATA_SEARCH = "/studentsAndSessionData/search";
 
--- src/main/java/teammates/ui/webapi/action/ActionFactory.java
+++ src/main/java/teammates/ui/webapi/action/ActionFactory.java
@@ -91,6 +91,7 @@ public class ActionFactory {
         map(ResourceURIs.STUDENT_COURSE, GET, StudentGetCourseDetailsAction.class);
         map(ResourceURIs.STUDENT_PROFILE, GET, GetStudentProfileAction.class);
         map(ResourceURIs.STUDENT_PROFILE, PUT, PutStudentProfileAction.class);
+        map(ResourceURIs.STUDENT_PROFILE_FORM_URL, POST, PostStudentProfileFormUrlAction.class);
         map(ResourceURIs.STUDENT_PROFILE_PICTURE, GET, GetStudentProfilePictureAction.class);
         map(ResourceURIs.STUDENT_COURSES, GET, GetStudentCoursesAction.class);
         map(ResourceURIs.STUDENTS_AND_FEEDBACK_SESSION_DATA_SEARCH, GET, SearchStudentsAndFeedbackSessionDataAction.class);
