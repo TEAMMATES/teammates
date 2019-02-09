8d523afe51664164cfa55f67b4799e66d0070da7
junming403 <wjmfelix0403@gmail.com>
2019-02-09 13:00:58 +0800

    add email template

--- src/main/java/teammates/ui/webapi/action/LinkRecoveryAction.java
+++ src/main/java/teammates/ui/webapi/action/LinkRecoveryAction.java
@@ -1,8 +1,7 @@
 package teammates.ui.webapi.action;
 
 import teammates.common.util.Const;
-import teammates.logic.core.StudentsLogic;
-import teammates.storage.api.StudentsDb;
+import teammates.ui.webapi.output.ApiOutput;
 
 /**
  * Action specifically created for confirming email and sending recovery link.
@@ -21,6 +20,20 @@ public class ConfirmRestoreEmailAction extends Action{
     @Override
     public ActionResult execute() {
         String email = getNonNullRequestParamValue(Const.ParamsNames.RESTORE_EMAIL);
+        boolean hasStudentsWithRestoreEmail = !logic.getAllStudentForEmail(email).isEmpty();
 
+        if (hasStudentsWithRestoreEmail) {
+
+        } else {
+            return new JsonResult()
+        }
+
+    }
+
+    /**
+     * Output format for {@link GetFeedbackQuestionRecipientsAction}.
+     */
+    public static class ResponseRestoreStatus extends ApiOutput {
+        public ResponseRestoreStatus(String status)
     }
 }
