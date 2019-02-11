fce2d026153bec5506ef0bd09862318441738ea9
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2019-02-11 23:29:48 +0800

    adjust remind students feedback session logic

--- src/main/java/teammates/ui/webapi/action/RemindFeedbackSessionSubmissionAction.java
+++ src/main/java/teammates/ui/webapi/action/RemindFeedbackSessionSubmissionAction.java
@@ -39,14 +39,10 @@ public class RemindFeedbackSessionSubmissionAction extends Action {
                     + "as the feedback session is not open for submissions.", HttpStatus.SC_BAD_REQUEST);
         }
 
-        String usersToRemind = getRequestParamValue(Const.ParamsNames.SUBMISSION_REMIND_USERLIST);
+        String usersToRemind = getNonNullRequestParamValue(Const.ParamsNames.SUBMISSION_REMIND_USERLIST);
 
-        if (usersToRemind == null) {
-            taskQueuer.scheduleFeedbackSessionReminders(courseId, feedbackSessionName, userInfo.getId());
-        } else {
-            taskQueuer.scheduleFeedbackSessionRemindersForParticularUsers(courseId, feedbackSessionName,
+        taskQueuer.scheduleFeedbackSessionRemindersForParticularUsers(courseId, feedbackSessionName,
                     usersToRemind.split(","), userInfo.getId());
-        }
 
         return new JsonResult("Reminders sent");
     }
