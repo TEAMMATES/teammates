76d99c3840d089c24e6e885554e96aa9159bc0ca
Monika <manuela.monika97@gmail.com>
2019-02-11 12:21:03 +0800

    Remove unnecessary null check

--- src/web/app/pages-help/instructor-help-page/instructor-help-getting-started/instructor-help-getting-started.component.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-getting-started/instructor-help-getting-started.component.ts
@@ -23,7 +23,7 @@ export class InstructorHelpGettingStartedComponent implements OnInit {
    */
   jumpTo(target: string): boolean {
     const destination: Element | null = document.getElementById(target);
-    if (destination !== null) {
+    if (destination) {
       destination.scrollIntoView();
       // to prevent the navbar from covering the text
       window.scrollTo(0, window.pageYOffset - 50);
