5f04f3229d81df51c6eebab87e1ee575ba7d959a
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2019-02-12 00:56:04 +0800

    update test case for feedback session remind and resend results link

--- src/test/java/teammates/test/cases/webapi/RemindFeedbackSessionResultTest.java
+++ src/test/java/teammates/test/cases/webapi/RemindFeedbackSessionResultTest.java
@@ -1,8 +1,13 @@
 package teammates.test.cases.webapi;
 
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
+import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
+import teammates.ui.webapi.action.JsonResult;
 import teammates.ui.webapi.action.RemindFeedbackSessionResultAction;
 
 /**
@@ -23,13 +28,75 @@ public class RemindFeedbackSessionResultTest extends BaseActionTest<RemindFeedba
     @Test
     @Override
     protected void testExecute() throws Exception {
-        // TODO
+        InstructorAttributes instructor1ofCourse1 = typicalBundle.instructors.get("instructor1OfCourse1");
+        FeedbackSessionAttributes fs = typicalBundle.feedbackSessions.get("closedSession");
+        StudentAttributes studentToEmail = typicalBundle.students.get("student1InCourse1");
+
+        loginAsInstructor(instructor1ofCourse1.googleId);
+
+        ______TS("Unsuccessful case: Not enough parameters");
+        verifyHttpParameterFailure();
+        String[] paramsNoCourseId = new String[] {
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_RESEND_PUBLISHED_EMAIL_USER_LIST, studentToEmail.getEmail()
+        };
+        verifyHttpParameterFailure(paramsNoCourseId);
+        String[] paramsNoFeedback = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.SUBMISSION_RESEND_PUBLISHED_EMAIL_USER_LIST, studentToEmail.getEmail()
+        };
+        verifyHttpParameterFailure(paramsNoFeedback);
+        String[] paramsNoUsersToResendEmail = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName()
+        };
+        verifyHttpParameterFailure(paramsNoUsersToResendEmail);
+
+        ______TS("Unsuccessful case: Feedback session not published, warning message generated");
+
+        fs = typicalBundle.feedbackSessions.get("session1InCourse1");
+        String[] paramsFeedbackSessionNotPublshed = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_RESEND_PUBLISHED_EMAIL_USER_LIST, studentToEmail.getEmail()
+        };
+
+        RemindFeedbackSessionResultAction action = getAction(paramsFeedbackSessionNotPublshed);
+        JsonResult result = getJsonResult(action);
+
+        assertEquals(HttpStatus.SC_BAD_REQUEST, result.getStatusCode());
+        verifyNoTasksAdded(action);
+
+        ______TS("Successful case: Typical case");
+
+        fs = typicalBundle.feedbackSessions.get("closedSession");
+        String[] paramsTypical = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_RESEND_PUBLISHED_EMAIL_USER_LIST, studentToEmail.getEmail()
+        };
+
+        action = getAction(paramsTypical);
+        result = getJsonResult(action);
+
+        assertEquals(HttpStatus.SC_OK, result.getStatusCode());
+        verifySpecifiedTasksAdded(action,
+                Const.TaskQueue.FEEDBACK_SESSION_RESEND_PUBLISHED_EMAIL_QUEUE_NAME, 1);
     }
 
     @Test
     @Override
     protected void testAccessControl() throws Exception {
-        // TODO
+        FeedbackSessionAttributes fs = typicalBundle.feedbackSessions.get("closedSession");
+        StudentAttributes studentNotSubmitFeedback = typicalBundle.students.get("student1InCourse1");
+        String[] submissionParams = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_RESEND_PUBLISHED_EMAIL_USER_LIST, studentNotSubmitFeedback.getEmail()
+        };
+
+        verifyOnlyInstructorsOfTheSameCourseCanAccess(submissionParams);
+        verifyInaccessibleWithoutModifySessionPrivilege(submissionParams);
     }
 
 }
--- src/test/java/teammates/test/cases/webapi/RemindFeedbackSessionSubmissionActionTest.java
+++ src/test/java/teammates/test/cases/webapi/RemindFeedbackSessionSubmissionActionTest.java
@@ -1,8 +1,13 @@
 package teammates.test.cases.webapi;
 
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
+import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
+import teammates.ui.webapi.action.JsonResult;
 import teammates.ui.webapi.action.RemindFeedbackSessionSubmissionAction;
 
 /**
@@ -23,13 +28,74 @@ public class RemindFeedbackSessionSubmissionActionTest extends BaseActionTest<Re
     @Test
     @Override
     protected void testExecute() throws Exception {
-        // TODO
+        InstructorAttributes instructor1ofCourse1 = typicalBundle.instructors.get("instructor1OfCourse1");
+        FeedbackSessionAttributes fs = typicalBundle.feedbackSessions.get("session1InCourse1");
+        StudentAttributes studentNotSubmitFeedback = typicalBundle.students.get("student5InCourse1");
+
+        loginAsInstructor(instructor1ofCourse1.googleId);
+
+        ______TS("Unsuccessful case: Not enough parameters");
+        verifyHttpParameterFailure();
+        String[] paramsNoCourseId = new String[] {
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail(),
+        };
+        verifyHttpParameterFailure(paramsNoCourseId);
+        String[] paramsNoFeedback = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail(),
+        };
+        verifyHttpParameterFailure(paramsNoFeedback);
+        String[] paramsNoUsersToRemind = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+        };
+        verifyHttpParameterFailure(paramsNoUsersToRemind);
+
+        ______TS("Unsuccessful case: Feedback session not open, warning message generated");
+
+        fs = typicalBundle.feedbackSessions.get("awaiting.session");
+        String[] paramsFeedbackSessionNotOpen = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail()
+        };
+
+        RemindFeedbackSessionSubmissionAction action = getAction(paramsFeedbackSessionNotOpen);
+        JsonResult result = getJsonResult(action);
+
+        assertEquals(HttpStatus.SC_BAD_REQUEST, result.getStatusCode());
+
+        ______TS("Successful case: Typical case");
+
+        fs = typicalBundle.feedbackSessions.get("session1InCourse1");
+        String[] paramsTypical = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail()
+        };
+
+        action = getAction(paramsTypical);
+        result = getJsonResult(action);
+
+        assertEquals(HttpStatus.SC_OK, result.getStatusCode());
+        verifySpecifiedTasksAdded(action,
+                Const.TaskQueue.FEEDBACK_SESSION_REMIND_PARTICULAR_USERS_EMAIL_QUEUE_NAME, 1);
     }
 
     @Test
     @Override
     protected void testAccessControl() throws Exception {
-        // TODO
+        FeedbackSessionAttributes fs = typicalBundle.feedbackSessions.get("session1InCourse1");
+        StudentAttributes studentNotSubmitFeedback = typicalBundle.students.get("student5InCourse1");
+        String[] submissionParams = new String[] {
+                Const.ParamsNames.COURSE_ID, fs.getCourseId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+                Const.ParamsNames.SUBMISSION_REMIND_USERLIST, studentNotSubmitFeedback.getEmail()
+        };
+
+        verifyOnlyInstructorsOfTheSameCourseCanAccess(submissionParams);
+        verifyInaccessibleWithoutModifySessionPrivilege(submissionParams);
     }
 
 }
