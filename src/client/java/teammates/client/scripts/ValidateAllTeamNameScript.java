141f97271fd0ce9b0d4f3298dee617dbf6d3f92a
junming403 <wjmfelix0403@gmail.com>
2019-02-11 14:12:57 +0800

    add script for checking all team name

---
+++ src/client/java/teammates/client/scripts/ValidateAllTeamNameScript.java
@@ -0,0 +1,40 @@
+package teammates.client.scripts;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
+import teammates.storage.entity.CourseStudent;
+
+public class ValidateAllTeamNameScript extends DataMigrationEntitiesBaseScript<CourseStudent>{
+
+    @Override
+    protected Query<CourseStudent> getFilterQuery() {
+        return ofy().load().type(CourseStudent.class);
+    }
+
+    @Override
+    protected boolean isPreview() {
+        return true;
+    }
+
+    @Override
+    protected String getLastPositionOfCursor() {
+        return null;
+    }
+
+    @Override
+    protected int getCursorInformationPrintCycle() {
+        return 100;
+    }
+
+    @Override
+    protected boolean isMigrationNeeded(Key<CourseStudent> key) throws Exception {
+        CourseStudent student = ofy().load().key(key).now();
+
+        return student.getTeamName().equals(student.getEmail());
+    }
+
+    @Override
+    protected void migrateEntity(Key<CourseStudent> entity) throws Exception {
+        // no actual data migrations needed.
+    }
+}
