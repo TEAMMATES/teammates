ab54ac32f5d963f2aab6fef992f9c2e9f550abc8
junming403 <wjmfelix0403@gmail.com>
2019-02-13 22:08:32 +0800

    add test for downgrade function

--- src/test/java/teammates/test/cases/webapi/DowngradeAccountActionTest.java
+++ src/test/java/teammates/test/cases/webapi/DowngradeAccountActionTest.java
@@ -1,9 +1,13 @@
 package teammates.test.cases.webapi;
 
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.util.Const;
 import teammates.ui.webapi.action.DowngradeAccountAction;
+import teammates.ui.webapi.action.JsonResult;
+import teammates.ui.webapi.output.MessageOutput;
 
 /**
  * SUT: {@link DowngradeAccountAction}.
@@ -23,7 +27,27 @@ public class DowngradeAccountActionTest extends BaseActionTest<DowngradeAccountA
     @Override
     @Test
     protected void testExecute() {
-        // TODO
+        InstructorAttributes instructor1ofCourse1 = typicalBundle.instructors.get("instructor1OfCourse1");
+
+        loginAsAdmin();
+
+        ______TS("Not enough parameters");
+
+        verifyHttpParameterFailure();
+
+        ______TS("Typical case");
+
+        String[] params = {
+                Const.ParamsNames.INSTRUCTOR_ID, instructor1ofCourse1.getGoogleId(),
+        };
+
+        DowngradeAccountAction a = getAction(params);
+        JsonResult r = getJsonResult(a);
+
+        MessageOutput response = (MessageOutput) r.getOutput();
+
+        assertEquals(HttpStatus.SC_OK, r.getStatusCode());
+        assertEquals("Instructor account is successfully downgraded to student.", response.getMessage());
     }
 
     @Override
