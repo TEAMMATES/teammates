77f5a506c60d78c577c1e6f961296d9eefc50c56
junming403 <wjmfelix0403@gmail.com>
2019-02-14 14:49:02 +0800

    add test case for search good id

--- src/test/java/teammates/test/cases/webapi/SearchAccountsActionTest.java
+++ src/test/java/teammates/test/cases/webapi/SearchAccountsActionTest.java
@@ -2,8 +2,12 @@ package teammates.test.cases.webapi;
 
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.AccountAttributes;
+import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.util.Const;
+import teammates.ui.webapi.action.JsonResult;
 import teammates.ui.webapi.action.SearchAccountsAction;
+import teammates.ui.webapi.output.AdminSearchResultData;
 
 /**
  * SUT: {@link SearchAccountsAction}.
@@ -23,13 +27,27 @@ public class SearchAccountsActionTest extends BaseActionTest<SearchAccountsActio
     @Override
     @Test
     protected void testExecute() {
+        InstructorAttributes acc = typicalBundle.instructors.get("instructor1OfCourse1");
         loginAsAdmin();
 
         ______TS("Not enough parameters");
 
         verifyHttpParameterFailure();
 
-        ______TS("Typical case");
+        ______TS("Typical case: search google id");
+
+        String[] submissionParams = new String[] {
+                Const.ParamsNames.ADMIN_SEARCH_KEY, acc.getGoogleId(),
+        };
+
+        SearchAccountsAction a = getAction(submissionParams);
+        JsonResult result = getJsonResult(a);
+        AdminSearchResultData response = (AdminSearchResultData) result.getOutput();
+
+        assertTrue(response.getStudents().isEmpty());
+        assertEquals(1, response.getInstructors().size());
+
+
     }
 
     @Override
