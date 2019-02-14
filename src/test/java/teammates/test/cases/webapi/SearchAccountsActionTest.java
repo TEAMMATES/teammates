0241a6f31e6b8bd4e501b298b050d9b0405cbf63
junming403 <wjmfelix0403@gmail.com>
2019-02-14 15:51:25 +0800

    add tests for accountSearch action

--- src/test/java/teammates/test/cases/webapi/SearchAccountsActionTest.java
+++ src/test/java/teammates/test/cases/webapi/SearchAccountsActionTest.java
@@ -2,7 +2,7 @@ package teammates.test.cases.webapi;
 
 import org.testng.annotations.Test;
 
-import teammates.common.datatransfer.attributes.AccountAttributes;
+import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.util.Const;
 import teammates.ui.webapi.action.JsonResult;
@@ -13,6 +13,13 @@ import teammates.ui.webapi.output.AdminSearchResultData;
  * SUT: {@link SearchAccountsAction}.
  */
 public class SearchAccountsActionTest extends BaseActionTest<SearchAccountsAction> {
+
+    @Override
+    protected void prepareTestData() {
+        DataBundle dataBundle = getTypicalDataBundle();
+        removeAndRestoreDataBundle(dataBundle);
+        putDocuments(dataBundle);
+    }
 
     @Override
     protected String getActionUri() {
@@ -36,17 +43,64 @@ public class SearchAccountsActionTest extends BaseActionTest<SearchAccountsActio
 
         ______TS("Typical case: search google id");
 
-        String[] submissionParams = new String[] {
+        String[] googleIdParams = new String[] {
                 Const.ParamsNames.ADMIN_SEARCH_KEY, acc.getGoogleId(),
         };
 
-        SearchAccountsAction a = getAction(submissionParams);
+        SearchAccountsAction a = getAction(googleIdParams);
         JsonResult result = getJsonResult(a);
         AdminSearchResultData response = (AdminSearchResultData) result.getOutput();
 
         assertTrue(response.getStudents().isEmpty());
         assertEquals(1, response.getInstructors().size());
 
+        ______TS("Typical case: search course id");
+        String[] courseIdParams = new String[] {
+                Const.ParamsNames.ADMIN_SEARCH_KEY, acc.getCourseId(),
+        };
+
+        a = getAction(courseIdParams);
+        result = getJsonResult(a);
+        response = (AdminSearchResultData) result.getOutput();
+
+        assertEquals(5, response.getInstructors().size());
+        assertEquals(5, response.getStudents().size());
+
+        ______TS("Typical case: full text search accounts that contains 'Course2'");
+        String[] accNameParams = new String[] {
+                Const.ParamsNames.ADMIN_SEARCH_KEY, "Course2",
+        };
+
+        a = getAction(accNameParams);
+        result = getJsonResult(a);
+        response = (AdminSearchResultData) result.getOutput();
+
+        assertEquals(3, response.getInstructors().size());
+        assertEquals(2, response.getStudents().size());
+
+        ______TS("Typical case: search email");
+        String[] emailParams = new String[] {
+                Const.ParamsNames.ADMIN_SEARCH_KEY, "@course1.tmt",
+        };
+
+        a = getAction(emailParams);
+        result = getJsonResult(a);
+        response = (AdminSearchResultData) result.getOutput();
+
+        assertEquals(4, response.getInstructors().size());
+        assertEquals(0, response.getStudents().size());
+
+        ______TS("Typical case: search has no match");
+        String[] noMatchParams = new String[] {
+                Const.ParamsNames.ADMIN_SEARCH_KEY, "nomatch",
+        };
+
+        a = getAction(noMatchParams);
+        result = getJsonResult(a);
+        response = (AdminSearchResultData) result.getOutput();
+
+        assertEquals(0, response.getInstructors().size());
+        assertEquals(0, response.getStudents().size());
 
     }
 
