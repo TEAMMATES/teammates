17ec93fdd7292964947f6472b36e1591be581fbd
Jeremy Choo <ChooJeremy@Ymail.com>
2019-02-14 18:12:16 +0800

    Update documentation of BasicRequest

--- src/main/java/teammates/ui/webapi/request/BasicRequest.java
+++ src/main/java/teammates/ui/webapi/request/BasicRequest.java
@@ -3,7 +3,11 @@ package teammates.ui.webapi.request;
 import teammates.common.exception.InvalidHttpRequestBodyException;
 
 /**
- * The request body of a HTTP request.
+ * A BasicRequest represents the request body of a HTTP request. Within the HTTP request, it is expected that the
+ * body of the request would contain certain specific parameters in order to fulfill the requirements of the API.
+ * A BasicRequest specifies those requirements. They are then extracted out to external interfaces at
+ * web/types/api-output.ts from generateTypes. These BasicRequest are also used in the context of the API, wherein
+ * they are parsed in Action's getAndValidateRequestBody method to return the request object itself to the API.
  */
 public abstract class BasicRequest {
 
