0a0587b5d2b35c2559d6f1d43114c8ce39ecc78c
Amrut Prabhu <amrut08@gmail.com>
2019-02-15 00:31:43 +0800

    Update student profile and student course details
    Fix ReferenceError

--- src/web/types/gender.ts
+++ src/web/types/gender.ts
@@ -2,12 +2,28 @@
  * Represents the gender of a student.
  */
 export enum Gender {
+
+  /**
+   * Male gender.
+   */
   MALE,
+
+  /**
+   * Female gender.
+   */
   FEMALE,
+
+  /**
+   * Not specified gender.
+   */
   OTHER,
 }
+
 export namespace Gender {
 
+  /**
+   * Returns an array containing the values of Gender enum.
+   */
   export const enumValues: () => Gender[] = (): Gender[] => {
     const genders: Gender[] = [];
     genders.push(Gender.MALE);
