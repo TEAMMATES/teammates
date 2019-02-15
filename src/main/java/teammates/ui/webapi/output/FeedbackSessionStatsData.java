9af08b6fd2217b2d00a6af5bbbc6e023c4720979
Jiang Chunhui <jiangchunhui007@gmail.com>
2019-02-15 16:27:36 +0800

    add session stats DTO

---
+++ src/main/java/teammates/ui/webapi/output/FeedbackSessionStatsData.java
@@ -0,0 +1,20 @@
+package teammates.ui.webapi.output;
+
+public class FeedbackSessionStatsData extends ApiOutput {
+
+    private final int submittedTotal;
+    private final int expectedTotal;
+
+    public FeedbackSessionStatsData(int submittedTotal, int expectedTotal) {
+        this.submittedTotal = submittedTotal;
+        this.expectedTotal = expectedTotal;
+    }
+
+    public int getSubmittedTotal() {
+        return submittedTotal;
+    }
+
+    public int getExpectedTotal() {
+        return expectedTotal;
+    }
+}
