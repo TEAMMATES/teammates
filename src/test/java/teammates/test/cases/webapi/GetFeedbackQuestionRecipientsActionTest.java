c631721cbdf5436b041aec355033e7b5cffeba56
Jiang Chunhui <jiangchunhui007@gmail.com>
2019-02-16 21:41:27 +0800

    refactor getting parameters

--- src/test/java/teammates/test/cases/webapi/GetFeedbackQuestionRecipientsActionTest.java
+++ src/test/java/teammates/test/cases/webapi/GetFeedbackQuestionRecipientsActionTest.java
@@ -1,5 +1,7 @@
 package teammates.test.cases.webapi;
 
+import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
@@ -10,11 +12,28 @@ import teammates.common.util.Const;
 import teammates.common.util.StringHelper;
 import teammates.ui.webapi.action.GetFeedbackQuestionRecipientsAction;
 import teammates.ui.webapi.action.Intent;
+import teammates.ui.webapi.action.JsonResult;
+import teammates.ui.webapi.output.FeedbackQuestionRecipientsData;
 
 /**
  * SUT: {@link GetFeedbackQuestionRecipientsAction}.
  */
 public class GetFeedbackQuestionRecipientsActionTest extends BaseActionTest<GetFeedbackQuestionRecipientsAction> {
+
+    private FeedbackSessionAttributes firstSession;
+    private FeedbackSessionAttributes secondSession;
+    private StudentAttributes student1InCourse1;
+    private InstructorAttributes instructor1OfCourse1;
+
+    @Override
+    @BeforeMethod
+    public void beforeTestMethodSetup() {
+        super.beforeTestMethodSetup();
+        firstSession = typicalBundle.feedbackSessions.get("session1InCourse1");
+        secondSession = typicalBundle.feedbackSessions.get("session2InCourse1");
+        student1InCourse1 = typicalBundle.students.get("student1InCourse1");
+        instructor1OfCourse1 = typicalBundle.instructors.get("instructor1OfCourse1");
+    }
 
     @Override
     protected String getActionUri() {
@@ -30,107 +49,82 @@ public class GetFeedbackQuestionRecipientsActionTest extends BaseActionTest<GetF
     @Override
     protected void testExecute() throws Exception {
         // TODO
+    private String[] generateParameters(FeedbackSessionAttributes session, int questionNumber, Intent intent,
+                                        String regKey, String moderatedPerson, String previewPerson) {
+        FeedbackQuestionAttributes question = logic.getFeedbackQuestion(session.getFeedbackSessionName(),
+                session.getCourseId(), questionNumber);
+        return new String[] {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, question.getFeedbackQuestionId(),
+                Const.ParamsNames.INTENT, intent.toString(),
+                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, moderatedPerson,
+                Const.ParamsNames.PREVIEWAS, previewPerson,
+                Const.ParamsNames.REGKEY, regKey,
+        };
+    }
+
     }
 
     @Test
     @Override
     protected void testAccessControl() throws Exception {
-        FeedbackSessionAttributes firstSession = typicalBundle.feedbackSessions.get("session1InCourse1");
-        FeedbackSessionAttributes secondSession = typicalBundle.feedbackSessions.get("session2InCourse1");
-        StudentAttributes student1InCourse1 = typicalBundle.students.get("student1InCourse1");
-        InstructorAttributes instructor1OfCourse1 = typicalBundle.instructors.get("instructor1OfCourse1");
-        FeedbackQuestionAttributes instructorQuestion =
-                logic.getFeedbackQuestion(firstSession.getFeedbackSessionName(),
-                        firstSession.getCourseId(), 3);
-        FeedbackQuestionAttributes studentQuestion =
-                logic.getFeedbackQuestion(firstSession.getFeedbackSessionName(),
-                        firstSession.getCourseId(), 2);
-        FeedbackQuestionAttributes questionNotShownToModeratedInstructor =
-                logic.getFeedbackQuestion(secondSession.getFeedbackSessionName(),
-                        secondSession.getCourseId(), 1);
-
-        ______TS("Question not intended shown to instructor, moderated instructor should not be accessible");
-        String[] invalidModeratedInstructorSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, questionNotShownToModeratedInstructor.getFeedbackQuestionId(),
-                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, "idOfInstructor1OfCourse1",
-                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
-        };
-        verifyCannotAccess(invalidModeratedInstructorSubmissionParams);
 
         ______TS("Student intends to access instructor's question, should not be accessible");
-        String[] studentAccessInstructorQuestion = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, instructorQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
-        };
-        verifyCannotAccess(studentAccessInstructorQuestion);
+        String[] studentAccessInstructorQuestionParams = generateParameters(firstSession, 3,
+                Intent.STUDENT_SUBMISSION, "", "", "");
+        verifyCannotAccess(studentAccessInstructorQuestionParams);
 
         ______TS("Instructor intends to access student's question, should not be accessible");
-        String[] instructorAccessStudentQuestion = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, studentQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
-        };
-        verifyCannotAccess(instructorAccessStudentQuestion);
+        String[] instructorAccessStudentQuestionParams = generateParameters(firstSession, 2,
+                Intent.INSTRUCTOR_SUBMISSION, "", "", "");
+        verifyCannotAccess(instructorAccessStudentQuestionParams);
 
         ______TS("Instructor access instructor's question, should be accessible");
-        String[] instructorSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, instructorQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
-        };
+        String[] instructorSubmissionParams = generateParameters(firstSession, 3,
+                Intent.INSTRUCTOR_SUBMISSION, "", "", "");
         verifyOnlyInstructorsOfTheSameCourseCanAccess(instructorSubmissionParams);
         verifyInaccessibleWithoutModifyInstructorPrivilege(instructorSubmissionParams);
 
         ______TS("Student access student's question, should be accessible");
-        String[] studentSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, studentQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
-        };
+        String[] studentSubmissionParams = generateParameters(firstSession, 2,
+                Intent.STUDENT_SUBMISSION, "", "", "");
         verifyAccessibleForStudentsOfTheSameCourse(studentSubmissionParams);
 
         ______TS("Student in the same course without logging in, should be accessible");
         StudentAttributes unloggedStudent =
                 logic.getStudentForGoogleId(student1InCourse1.getCourse(), student1InCourse1.googleId);
-        String[] unregisteredStudentSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, studentQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
-                Const.ParamsNames.REGKEY, StringHelper.encrypt(unloggedStudent.getKey()),
-        };
+        String[] unregisteredStudentSubmissionParams = generateParameters(firstSession, 2,
+                Intent.STUDENT_SUBMISSION, StringHelper.encrypt(unloggedStudent.getKey()),
+                "", "");
         verifyAccessibleWithoutLogin(unregisteredStudentSubmissionParams);
 
+        ______TS("Question not intended shown to instructor, moderated instructor should not be accessible");
+        String[] invalidModeratedInstructorSubmissionParams = generateParameters(secondSession, 1,
+                Intent.INSTRUCTOR_SUBMISSION, "", instructor1OfCourse1.email, "");
+        verifyCannotAccess(invalidModeratedInstructorSubmissionParams);
+
         ______TS("Instructor moderates student's question, should be accessible if he has privilege");
-        String[] moderatedStudentSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, studentQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
-                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, student1InCourse1.email,
-        };
+        String[] moderatedStudentSubmissionParams = generateParameters(firstSession, 2,
+                Intent.STUDENT_SUBMISSION, "", student1InCourse1.email, "");
         verifyAccessibleForInstructorsOfTheSameCourse(moderatedStudentSubmissionParams);
         verifyInaccessibleWithoutModifyInstructorPrivilege(moderatedStudentSubmissionParams);
 
         ______TS("Instructor previews student's question, should be accessible if he has privilege");
-        String[] previewStudentSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, studentQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
-                Const.ParamsNames.PREVIEWAS, student1InCourse1.email,
-        };
+        String[] previewStudentSubmissionParams = generateParameters(firstSession, 2,
+                Intent.STUDENT_SUBMISSION, "", "", student1InCourse1.email);
         verifyAccessibleForInstructorsOfTheSameCourse(previewStudentSubmissionParams);
         verifyInaccessibleWithoutModifyInstructorPrivilege(previewStudentSubmissionParams);
 
         ______TS("Instructor moderates another instructor's question, "
                 + "should be accessible if he has privilege");
-        String[] moderatedInstructorSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, instructorQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
-                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, instructor1OfCourse1.email,
-        };
+        String[] moderatedInstructorSubmissionParams = generateParameters(firstSession, 3,
+                Intent.INSTRUCTOR_SUBMISSION, "", instructor1OfCourse1.email, "");
         verifyAccessibleForInstructorsOfTheSameCourse(moderatedInstructorSubmissionParams);
         verifyInaccessibleWithoutModifyInstructorPrivilege(moderatedInstructorSubmissionParams);
 
         ______TS("Instructor previews another instructor's question,"
                 + " should be accessible if he has privilege");
-        String[] previewInstructorSubmissionParams = new String[] {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, instructorQuestion.getFeedbackQuestionId(),
-                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
-                Const.ParamsNames.PREVIEWAS, instructor1OfCourse1.email,
-        };
+        String[] previewInstructorSubmissionParams = generateParameters(firstSession, 3,
+                Intent.INSTRUCTOR_SUBMISSION, "", "", instructor1OfCourse1.email);
         verifyAccessibleForInstructorsOfTheSameCourse(previewInstructorSubmissionParams);
         verifyInaccessibleWithoutModifyInstructorPrivilege(previewInstructorSubmissionParams);
     }
