e29b3472b0c300399f4a8b87dfade7a9378e1dad
tong72 <tong72@purdue.edu>
2019-02-17 18:47:05 -0500

    Fixed Coding Style Errors for GetTimeZonesActionTest.java

--- src/test/java/teammates/test/cases/webapi/GetTimeZonesActionTest.java
+++ src/test/java/teammates/test/cases/webapi/GetTimeZonesActionTest.java
@@ -1,7 +1,8 @@
 package teammates.test.cases.webapi;
 
 import org.apache.http.HttpStatus;
-import org.joda.time.*;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
 import org.testng.annotations.Test;
 
 import teammates.common.util.Const;
@@ -36,6 +37,16 @@ public class GetTimeZonesActionTest extends BaseActionTest<GetTimeZonesAction> {
 
         TimeZonesData output = (TimeZonesData) r.getOutput();
 
+        DateTimeZone nyTime = DateTimeZone.forID("America/New_York");
+        boolean standardOffsetNewYork = nyTime.isStandardOffset(Instant.now().getMillis());
+        boolean daylightSavingNewYork = !standardOffsetNewYork;
+        DateTimeZone sydTime = DateTimeZone.forID("Australia/Sydney");
+        boolean standardOffsetSydney = sydTime.isStandardOffset(Instant.now().getMillis());
+        boolean daylightSavingSydney = !standardOffsetSydney;
+        DateTimeZone ldnTime = DateTimeZone.forID("Europe/London");
+        boolean standardOffsetLondon = ldnTime.isStandardOffset(Instant.now().getMillis());
+        boolean daylightSavingLondon = !standardOffsetLondon;
+
         assertEquals(HttpStatus.SC_OK, r.getStatusCode());
         // This test does not check the timezone database used is the latest
         // Only check that the version number is returned, and some sample values for timezone offset
@@ -45,24 +56,19 @@ public class GetTimeZonesActionTest extends BaseActionTest<GetTimeZonesAction> {
          * e.g. New York observes DST, so the offset is not always UTC-05:00 the entire year.
          * e.g. timezones can change, like Caracas modifying their timezone. This affects the offset as well.
          */
-        DateTimeZone nyTime = DateTimeZone.forID("America/New_York");
-        boolean standardOffsetNY = nyTime.isStandardOffset(Instant.now().getMillis());
-        DateTimeZone sydTime = DateTimeZone.forID("Australia/Sydney");
-        boolean standardOffsetSYD = sydTime.isStandardOffset(Instant.now().getMillis());
-        DateTimeZone ldnTime = DateTimeZone.forID("Europe/London");
-        boolean standardOffsetLDN = ldnTime.isStandardOffset(Instant.now().getMillis());
+
         assertEquals(8 * 60 * 60, output.getOffsets().get("Asia/Singapore").intValue());
-        if (!standardOffsetNY) {    //Daylight Saving Time in New York
+        if (daylightSavingNewYork) {
             assertEquals(-4 * 60 * 60, output.getOffsets().get("America/New_York").intValue());
         } else {
             assertEquals(-5 * 60 * 60, output.getOffsets().get("America/New_York").intValue());
         }
-        if (!standardOffsetSYD) {
+        if (daylightSavingSydney) {
             assertEquals(11 * 60 * 60, output.getOffsets().get("Australia/Sydney").intValue());
         } else {
             assertEquals(10 * 60 * 60, output.getOffsets().get("Australia/Sydney").intValue());
         }
-        if (!standardOffsetLDN) {
+        if (daylightSavingLondon) {
             assertEquals(1, output.getOffsets().get("Europe/London").intValue());
         } else {
             assertEquals(0, output.getOffsets().get("Europe/London").intValue());
