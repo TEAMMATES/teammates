3eab9577656a4a946d8ee7df0ca47ff2c522ee8a
Amrut Prabhu <amrut08@gmail.com>
2019-02-18 23:37:19 +0800

    Reload recaptcha on submit

--- src/web/app/pages-help/recover-session-links/link-recovery-page.component.ts
+++ src/web/app/pages-help/recover-session-links/link-recovery-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReCaptcha2Component } from 'ngx-captcha';
 import { HttpRequestService } from '../../../services/http-request.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { MessageOutput } from '../../../types/api-output';
@@ -16,15 +17,15 @@ import { ErrorMessageOutput } from '../../error-message-output';
 export class LinkRecoveryPageComponent implements OnInit {
 
   // ngx-recaptcha2 element properties
-  captchaIsLoaded: boolean = false;
   captchaSuccess: boolean = false;
-  captchaIsExpired: boolean = false;
   captchaResponse?: string;
   size: 'compact' | 'normal' = 'normal';
   lang: string = 'en';
 
   formLinkRecovery!: FormGroup;
   readonly siteKey: string = '6LeZSZEUAAAAAO-xCzi314NoCgJILEH9qzFuer3P';
+
+  @ViewChild('captchaElem') captchaElem!: ReCaptcha2Component;
 
   constructor(private httpRequestService: HttpRequestService,
               private statusMessageService: StatusMessageService,
@@ -59,18 +60,27 @@ export class LinkRecoveryPageComponent implements OnInit {
         this.statusMessageService.showSuccessMessage(resp.message);
 
         // Reset input field and reCAPTCHA
-        this.handleReset();
+        (this.formLinkRecovery = this.formBuilder.group({
+          email: ['', Validators.required],
+          recaptcha: ['', Validators.required],
+        }));
+        this.reload();
       }, (response: ErrorMessageOutput) => {
         this.statusMessageService.showErrorMessage(response.error.message);
+
+        // Reset reCAPTCHA
+        (this.formLinkRecovery = this.formBuilder.group({
+          recaptcha: ['', Validators.required],
+        }));
+        this.reload();
       });
   }
 
   /**
-   * Handles reset of the reCAPTCHA.
+   * Reloads the reCAPTCHA widget.
    */
-  handleReset(): void {
-    this.captchaSuccess = false;
-    this.captchaResponse = undefined;
+  reload(): void {
+    this.captchaElem.reloadCaptcha();
   }
 
   /**
@@ -80,21 +90,5 @@ export class LinkRecoveryPageComponent implements OnInit {
   handleSuccess(captchaResponse: string): void {
     this.captchaSuccess = true;
     this.captchaResponse = captchaResponse;
-  }
-
-  /**
-   * Handles loading of the reCAPTCHA.
-   */
-  handleLoad(): void {
-    this.captchaIsLoaded = true;
-    this.captchaIsExpired = false;
-  }
-
-  /**
-   * Handles expiry of the reCAPTCHA.
-   */
-  handleExpire(): void {
-    this.captchaSuccess = false;
-    this.captchaIsExpired = true;
   }
 }
