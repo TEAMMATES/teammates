af08ed5718091b6bf934c311aa4cd0191b1365fa
junming403 <wjmfelix0403@gmail.com>
2019-02-19 02:32:23 +0800

    add provider for service tetsing

--- src/web/services/account.service.spec.ts
+++ src/web/services/account.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccountService } from './account.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+
 
 describe('AccountService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+    ],
+  }));
 
   it('should be created', () => {
     const service: AccountService = TestBed.get(AccountService);
--- src/web/services/student-profile.service.spec.ts
+++ src/web/services/student-profile.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StudentProfileService } from './student-profile.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('StudentProfileService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+    ],
+  }));
 
   it('should be created', () => {
     const service: StudentProfileService = TestBed.get(StudentProfileService);
