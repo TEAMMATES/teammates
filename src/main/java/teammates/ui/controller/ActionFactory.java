8993839fea05ccce646d65a90e51e5a8c7d77712
Wen Xin <e0052753@u.nus.edu>
2019-02-20 03:29:35 +0800

    [#9382] Migrate instructor student list page to Angular (#9374)

--- src/main/java/teammates/ui/controller/ActionFactory.java
+++ src/main/java/teammates/ui/controller/ActionFactory.java
@@ -28,8 +28,6 @@ public class ActionFactory {
         map(INSTRUCTOR_FEEDBACK_RESPONSE_COMMENT_ADD, InstructorFeedbackResponseCommentAddAction.class);
         map(INSTRUCTOR_FEEDBACK_RESPONSE_COMMENT_EDIT, InstructorFeedbackResponseCommentEditAction.class);
         map(INSTRUCTOR_FEEDBACK_RESPONSE_COMMENT_DELETE, InstructorFeedbackResponseCommentDeleteAction.class);
-        map(INSTRUCTOR_STUDENT_LIST_PAGE, InstructorStudentListPageAction.class);
-        map(INSTRUCTOR_STUDENT_LIST_AJAX_PAGE, InstructorStudentListAjaxPageAction.class);
         map(INSTRUCTOR_STUDENT_RECORDS_AJAX_PAGE, InstructorStudentRecordsAjaxPageAction.class);
 
         map(STUDENT_FEEDBACK_RESULTS_PAGE, StudentFeedbackResultsPageAction.class);
