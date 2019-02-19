8ba4c707b6e5e8e419bade97d6e4453f42ef994d
Wilson Kurniawan <wkurniawan.92@gmail.com>
2019-02-20 03:05:32 +0800

    Extract out DataBundleHelper to reduce duplication

--- src/test/java/teammates/test/cases/webapi/GetCourseStudentsActionTest.java
+++ src/test/java/teammates/test/cases/webapi/GetCourseStudentsActionTest.java
@@ -2,17 +2,16 @@ package teammates.test.cases.webapi;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
-import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
 import teammates.test.driver.AssertHelper;
+import teammates.test.driver.DataBundleHelper;
 import teammates.ui.webapi.action.GetCourseStudentsAction;
 import teammates.ui.webapi.action.GetCourseStudentsAction.CourseDetails;
 import teammates.ui.webapi.action.GetCourseStudentsAction.GetCourseStudentsActionResult;
@@ -178,78 +177,6 @@ public class GetCourseStudentsActionTest extends BaseActionTest<GetCourseStudent
         });
 
         return studentDetailsList;
-    }
-
-    static class DataBundleHelper {
-        private final DataBundle dataBundle;
-
-        DataBundleHelper(DataBundle dataBundle) {
-            this.dataBundle = dataBundle;
-        }
-
-        public List<CourseAttributes> getCoursesByInstructorGoogleId(String instructorGoogleId) {
-            return dataBundle.instructors.values().stream()
-                    .filter(instructorAttributes -> {
-                        String googleId = instructorAttributes.getGoogleId();
-                        if (googleId != null && googleId.equals(instructorGoogleId)) {
-                            return true;
-                        }
-                        return false;
-                    })
-                    .map(instructorAttributes -> getCourseById(instructorAttributes.getCourseId()))
-                    .collect(Collectors.toList());
-        }
-
-        public CourseAttributes getCourseById(String courseId) {
-            return dataBundle.courses.values()
-                    .stream()
-                    .filter(course -> course.getId().equals(courseId))
-                    .findFirst()
-                    .orElseGet(null);
-        }
-
-        public List<StudentAttributes> getStudentsForCourse(String courseId) {
-            return dataBundle.students.values()
-                    .stream()
-                    .filter(student -> student.course.equals(courseId))
-                    .collect(Collectors.toList());
-        }
-
-        public List<String> getSectionNamesForCourse(String courseId) {
-            List<String> sections = new ArrayList<>();
-
-            List<StudentAttributes> studentsOfCourse = getStudentsForCourse(courseId);
-            studentsOfCourse.forEach(student -> {
-                if (student.section != null && !sections.contains(student.section)) {
-                    sections.add(student.section);
-                }
-            });
-
-            return sections;
-        }
-
-        public List<String> getTeamsForSection(String courseId, String sectionName) {
-            List<String> teams = new ArrayList<>();
-
-            List<StudentAttributes> studentsOfCourse = getStudentsForCourse(courseId);
-            studentsOfCourse.forEach(student -> {
-                if (student.section != null && student.section.equals(sectionName) && student.team != null
-                        && !teams.contains(student.team)) {
-                    teams.add(student.team);
-                }
-            });
-
-            return teams;
-        }
-
-        public List<StudentAttributes> getStudentsForTeam(String courseId, String sectionName, String teamName) {
-            return dataBundle.students.values()
-                    .stream()
-                    .filter(student -> student.course.equals(courseId))
-                    .filter(student -> student.section.equals(sectionName))
-                    .filter(student -> student.team.equals(teamName))
-                    .collect(Collectors.toList());
-        }
     }
 
 }
