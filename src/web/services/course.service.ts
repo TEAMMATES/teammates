8764a7d2001a5713759bbf1d0304c57c9133d28f
Jeremy Choo <ChooJeremy@Ymail.com>
2019-02-23 00:12:53 +0800

    Update front end for sending reminder emails

--- src/web/services/course.service.ts
+++ src/web/services/course.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { JoinStatus } from '../types/api-output';
+import { JoinStatus, MessageOutput } from '../types/api-output';
 import { HttpRequestService } from './http-request.service';
 
 /**
@@ -23,5 +23,37 @@ export class CourseService {
       entitytype: entityType,
     };
     return this.httpRequestService.get('/join', paramMap);
+  }
+
+  /**
+   * Send reminder emails to everyone.
+   */
+  sendReminderEmail(courseId: string): Observable<MessageOutput> {
+    const paramMap: { [key: string]: string } = {
+      courseid: courseId,
+    };
+    return this.httpRequestService.get('/join/remind', paramMap);
+  }
+
+  /**
+   * Send reminder emails to a student.
+   */
+  sendReminderEmailToStudent(courseId: string, studentEmail: string): Observable<MessageOutput> {
+    const paramMap: { [key: string]: string } = {
+      courseid: courseId,
+      studentemail: studentEmail,
+    };
+    return this.httpRequestService.get('/join/remind', paramMap);
+  }
+
+  /**
+   * Send reminder emails to an instructor.
+   */
+  sendReminderEmailToInstructor(courseId: string, instructorEmail: string): Observable<MessageOutput> {
+    const paramMap: { [key: string]: string } = {
+      courseid: courseId,
+      instructoremail: instructorEmail,
+    };
+    return this.httpRequestService.get('/join/remind', paramMap);
   }
 }
