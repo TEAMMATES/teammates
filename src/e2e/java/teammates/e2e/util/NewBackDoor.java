d1e9092a8369851b869afd1fc74949596a8c739e
Jacob Li Peng Cheng <lipech@hotmail.com>
2019-02-25 21:42:43 +0800

    implement NewBackDoor methods to handle different calls to front end for testing

--- src/e2e/java/teammates/e2e/util/NewBackDoor.java
+++ src/e2e/java/teammates/e2e/util/NewBackDoor.java
@@ -3,6 +3,8 @@ package teammates.e2e.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.http.HttpEntity;
@@ -16,6 +18,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import teammates.common.util.Const;
+import teammates.common.util.SanitizationHelper;
 
 /**
  * Used to create API calls to the back-end without going through the UI.
@@ -38,13 +41,32 @@ public final class NewBackDoor {
     }
 
     /**
+     * Executes POST request with the given {@code relativeUrl}.
+     *
+     * @return The content of the HTTP response
+     */
+    public static String executePostRequest(String relativeUrl) {
+        return executeRequest(HttpPost.METHOD_NAME, relativeUrl);
+    }
+
+    /**
+     * Executes DELETE request with the given {@code relativeUrl}.
+     *
+     * @return The content of the HTTP response
+     */
+    public static String executeDeleteRequest(String relativeUrl) {
+        return executeRequest(HttpDelete.METHOD_NAME, relativeUrl);
+    }
+
+    /**
      * Executes HTTP request with the given {@code method} and {@code relativeUrl}.
      *
      * @return The content of the HTTP response
      */
-    private static String executeRequest(String method, String relativeUrl) {
+    public static String executeRequest(String method, String relativeUrl) {
         String url = TestProperties.TEAMMATES_URL + Const.ResourceURIs.URI_PREFIX + relativeUrl;
-
+        // http://localhost:8080/webapi/relativeUrl
+        // relativeUrl = backdoor?action=CreateFeedbackAction&params=information
         HttpRequestBase request;
         switch (method) {
         case HttpGet.METHOD_NAME:
@@ -79,6 +101,52 @@ public final class NewBackDoor {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Deletes an account from datastore.
+     */
+    public static String deleteAccount(String googleId) {
+        Map<String, String> params = createParamMap("DeleteStudentAction");
+
+        params.put(Const.ParamsNames.STUDENT_ID, googleId);
+        return executeDeleteRequest("/backdoor?".concat(encodeParameters(params)));
+    }
+
+    /**
+     * Deletes a feedback session from the datastore.
+     */
+    public static String deleteFeedbackSession(String feedbackSessionName, String courseId) {
+        Map<String, String> params = createParamMap("DeleteFeedbackSessionAction");
+
+        params.put(Const.ParamsNames.FEEDBACK_SESSION_NAME, feedbackSessionName);
+        params.put(Const.ParamsNames.COURSE_ID, courseId);
+        return executeDeleteRequest("/backdoor?".concat(encodeParameters(params)));
+    }
+
+    /**
+     * Gets a feedback session from the datastore.
+     */
+    public static String getFeedbackSession(String courseId, String feedbackSessionName) {
+        Map<String, String> params = createParamMap("GetFeedbackSessionAction");
+
+        params.put(Const.ParamsNames.FEEDBACK_SESSION_NAME, feedbackSessionName);
+        params.put(Const.ParamsNames.COURSE_ID, courseId);
+
+        return executePostRequest( "/backdoor?".concat(encodeParameters(params)));
+    }
+
+    private static Map<String, String> createParamMap(String operation) {
+        Map<String, String> map = new HashMap<>();
+        map.put(Const.ParamsNames.BACKDOOR_OPERATION, operation);
+
+        return map;
+    }
+
+    private static String encodeParameters(Map<String, String> map) {
+        StringBuilder dataStringBuilder = new StringBuilder();
+        map.forEach((key, value) -> dataStringBuilder.append(key + "=" + SanitizationHelper.sanitizeForUri(value) + "&"));
+        return dataStringBuilder.toString();
     }
 
     private static void addAuthKeys(HttpRequestBase request) {
