759e405011d2aa177a76f6b50adf8e45f089ea4b
Tran Tien Dat <peter.trantiendat@gmail.com>
2019-02-27 00:19:14 +0800

    Disable modal dialogs in example box

--- src/web/app/pages-help/instructor-help-page/example-box/example-box.component.ts
+++ src/web/app/pages-help/instructor-help-page/example-box/example-box.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+export class DisabledNgbModal {
+  open(_content: any): void {}
+}
 
 @Component({
   selector: 'tm-example-box',
   templateUrl: './example-box.component.html',
-  styleUrls: ['./example-box.component.scss']
+  styleUrls: ['./example-box.component.scss'],
+  providers: [{ provide: NgbModal, useClass: DisabledNgbModal }]
 })
 export class ExampleBoxComponent implements OnInit {
 
