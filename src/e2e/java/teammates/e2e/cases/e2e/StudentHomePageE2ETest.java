970cff16f82121411b849e59525b97fddcf6a6b3
Jacob Li Peng Cheng <lipech@hotmail.com>
2019-03-04 21:06:14 +0800

    reorganise test json file to remove unnecessary information and fix StudentHomeE2ETest cases

--- src/e2e/java/teammates/e2e/cases/e2e/StudentHomePageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/e2e/StudentHomePageE2ETest.java
@@ -1,23 +1,40 @@
 package teammates.e2e.cases.e2e;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.openqa.selenium.By;
-import org.testng.annotations.AfterClass;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import teammates.common.util.AppUrl;
-import teammates.common.util.Const;
-import teammates.e2e.pageobjects.StudentHomePage;
-import teammates.e2e.util.BackDoor;
+import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.e2e.util.TestProperties;
 
 /**
- * SUT: {@link Const.WebPageURIs#STUDENT_HOME_PAGE}
+ * SUT: {@link teammates.common.util.Const.WebPageURIs#STUDENT_HOME_PAGE}.
  */
 public class StudentHomePageE2ETest extends BaseE2ETestCase {
 
     @Override
     protected void prepareTestData() {
         testData = loadDataBundle("/StudentHomePageE2ETest.json");
+
+        String student1GoogleId = TestProperties.TEST_STUDENT1_ACCOUNT;
+        String student1Email = student1GoogleId + "@gmail.com";
+        testData.accounts.get("alice.tmms").googleId = student1GoogleId;
+        testData.accounts.get("alice.tmms").email = student1Email;
+
+        // This student has a registered account but yet to join course
+        testData.students.get("alice.tmms@SHomeUiT.CS2104").email = student1Email;
+
+        testData.students.get("alice.tmms@SHomeUiT.CS1101").googleId = student1GoogleId;
+        testData.students.get("alice.tmms@SHomeUiT.CS1101").email = student1Email;
+        testData.students.get("alice.tmms@SHomeUiT.CS4215").googleId = student1GoogleId;
+        testData.students.get("alice.tmms@SHomeUiT.CS4215").email = student1Email;
+        testData.students.get("alice.tmms@SHomeUiT.CS4221").googleId = student1GoogleId;
+        testData.students.get("alice.tmms@SHomeUiT.CS4221").email = student1Email;
+
         removeAndRestoreDataBundle(testData);
     }
 
@@ -30,16 +47,53 @@ public class StudentHomePageE2ETest extends BaseE2ETestCase {
 
         ______TS("login successfully");
 
-        StudentHomePage studentHome = getHomePage().clickStudentLogin()
-                                   .loginAsStudent(TestProperties.TEST_STUDENT1_ACCOUNT,
-                                                   TestProperties.TEST_STUDENT1_PASSWORD);
+        getHomePage().clickStudentLogin().loginAsStudent(
+                TestProperties.TEST_STUDENT1_ACCOUNT, TestProperties.TEST_STUDENT1_PASSWORD);
 
         browser.waitForPageLoad();
-        assertTrue(verifyFirstPanelFeedbackSessionName("[SHomeUiT.CS1101]: Programming Methodology"));
+
+        List<String> courseIds = getAllVisibleCourseIds();
+
+        for (int i = 0; i < courseIds.size(); i++) {
+            String courseId = courseIds.get(i);
+
+            assertTrue(verifyVisibleCourseToStudents(courseId, i));
+
+            String feedbackSessionNames = testData.feedbackSessions.entrySet().stream()
+                    .filter(feedbackSession -> courseId.equals(feedbackSession.getValue().getCourseId()))
+                    .map(x -> x.getValue().getFeedbackSessionName())
+                    .collect(Collectors.joining());
+
+            if (feedbackSessionNames.isEmpty()) {
+                assertFalse(verifyVisibleFeedbackSessionToStudents("empty", i));
+            } else {
+                assertTrue(verifyVisibleFeedbackSessionToStudents(feedbackSessionNames, i));
+            }
+        }
     }
 
-    private boolean verifyFirstPanelFeedbackSessionName(String message) {
-        return message.equals(browser.driver.findElement(By.cssSelector("div.card-header.bg-primary strong")).getText());
+    private List<WebElement> getStudentHomeCoursePanels() {
+        return browser.driver.findElements(By.cssSelector("div.card.bg-light"));
     }
 
+    private List<String> getAllVisibleCourseIds() {
+        List<String> courseIds = new ArrayList<>();
+
+        for (StudentAttributes student : testData.students.values()) {
+            if (student.googleId.equals(TestProperties.TEST_STUDENT1_ACCOUNT)) {
+                courseIds.add(student.getCourse());
+            }
+        }
+        return courseIds;
+    }
+
+    private boolean verifyVisibleCourseToStudents(String courseName, int index) {
+        return getStudentHomeCoursePanels().get(index).getText().contains(courseName);
+    }
+
+    private boolean verifyVisibleFeedbackSessionToStudents(String feedbackSessionName, int index) {
+        return getStudentHomeCoursePanels().get(index)
+                .findElement(By.cssSelector("div.table-responsive table.table tbody")).getText()
+                .contains(feedbackSessionName);
+    }
 }
