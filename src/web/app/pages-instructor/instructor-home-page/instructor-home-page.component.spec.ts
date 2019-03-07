3cd0f44f6e1f821b06c6431a9d6a6362352b42db
Monika <manuela.monika97@gmail.com>
2019-03-08 02:14:44 +0800

    Add test case: 1 course with 1 session

--- src/web/app/pages-instructor/instructor-home-page/instructor-home-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-home-page/instructor-home-page.component.spec.ts
@@ -1,6 +1,12 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  FeedbackSession, FeedbackSessionPublishStatus,
+  FeedbackSessionSubmissionStatus,
+  ResponseVisibleSetting,
+  SessionVisibleSetting,
+} from '../../../types/api-output';
 import { SessionsTableRowModel, SortBy, SortOrder } from '../../components/sessions-table/sessions-table-model';
 import { InstructorPrivilege } from '../../instructor-privilege';
 import { InstructorHomePageComponent } from './instructor-home-page.component';
@@ -51,6 +57,59 @@ describe('InstructorHomePageComponent', () => {
         canSubmitSessionInSections: true,
       },
       sessionsTableRowModels: [],
+      sessionsTableRowModelsSortBy: SortBy.NONE,
+      sessionsTableRowModelsSortOrder: SortOrder.ASC,
+      hasPopulated: true,
+      isAjaxSuccess: true,
+      isTabExpanded: true,
+    };
+    component.user = instructorName;
+    component.courseTabModels = [courseTabModels];
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with one course with one feedback session', () => {
+    const instructorName: string = '';
+    const feedbackSession: any = {
+      courseId: 'CS3281',
+      timeZone: 'Asia/Singapore',
+      feedbackSessionName: 'Feedback 1',
+      instructions: 'Answer all questions',
+      submissionStartTimestamp: 0,
+      submissionEndTimestamp: 1,
+      gracePeriod: 0,
+      sessionVisibleSetting: SessionVisibleSetting.AT_OPEN,
+      customSessionVisibleTimestamp: 0,
+      responseVisibleSetting: ResponseVisibleSetting.AT_VISIBLE,
+      customResponseVisibleTimestamp: 0,
+      submissionStatus: FeedbackSessionSubmissionStatus.CLOSED,
+      publishStatus: FeedbackSessionPublishStatus.NOT_PUBLISHED,
+      isClosingEmailEnabled: true,
+      isPublishedEmailEnabled: true,
+      createdAtTimestamp: 0,
+    };
+    const instructorPrivilege: any = {
+      canModifyCourse: true,
+      canModifySession: true,
+      canModifyStudent: true,
+      canSubmitSessionInSections: true,
+    };
+    const sessionsTableRowModels: any = {
+      feedbackSession,
+      instructorPrivilege,
+      responseRate: '0 / 6',
+      isLoadingResponseRate: false,
+    };
+    const courseTabModels: any = {
+      instructorPrivilege,
+      course: {
+        courseId: 'CS3281',
+        courseName: 'Thematic Systems',
+        creationDate: '26 Feb 23:59 PM',
+        timeZone: 'Asia/Singapore',
+      },
+      sessionsTableRowModels: [sessionsTableRowModels],
       sessionsTableRowModelsSortBy: SortBy.NONE,
       sessionsTableRowModelsSortOrder: SortOrder.ASC,
       hasPopulated: true,
