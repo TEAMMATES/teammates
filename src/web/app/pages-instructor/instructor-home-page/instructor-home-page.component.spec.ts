2e34dd57eeb523fa1f5d85949d60fe7a83698591
Monika <manuela.monika97@gmail.com>
2019-03-08 01:46:31 +0800

    Add instructor home snapshot test cases: default, 1 course no session

--- src/web/app/pages-instructor/instructor-home-page/instructor-home-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-home-page/instructor-home-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SessionsTableRowModel, SortBy, SortOrder } from '../../components/sessions-table/sessions-table-model';
+import { InstructorPrivilege } from '../../instructor-privilege';
 import { InstructorHomePageComponent } from './instructor-home-page.component';
 import { InstructorHomePageModule } from './instructor-home-page.module';
 
@@ -27,5 +29,37 @@ describe('InstructorHomePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with one course without feedback session', () => {
+    const instructorName: string = '';
+    const courseTabModels: any = {
+      course: {
+        courseId: 'CS3281',
+        courseName: 'Thematic Systems',
+        creationDate: '26 Feb 23:59 PM',
+        timeZone: 'Asia/Singapore',
+      },
+      instructorPrivilege: {
+        canModifyCourse: true,
+        canModifySession: true,
+        canModifyStudent: true,
+        canSubmitSessionInSections: true,
+      },
+      sessionsTableRowModels: [],
+      sessionsTableRowModelsSortBy: SortBy.NONE,
+      sessionsTableRowModelsSortOrder: SortOrder.ASC,
+      hasPopulated: true,
+      isAjaxSuccess: true,
+      isTabExpanded: true,
+    };
+    component.user = instructorName;
+    component.courseTabModels = [courseTabModels];
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 });
