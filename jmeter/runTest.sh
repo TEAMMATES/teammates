0e628e8cd3f9967d3f672b30f96f7aefe43d4821
Amrut Prabhu <amrut08@gmail.com>
2019-03-13 17:10:20 +0800

    Update bash script and gitignore

--- jmeter/runTest.sh
+++ jmeter/runTest.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
 FILE=$(echo $1 | cut -d'.' -f 1)
 
-jmeter -n -t $1 -l ./results/"$FILE"_result.jtl
+# TODO: generate sample test data and store it in datastore
+
+# org.apache.jmeter.protocol.http.control - HTTP Controls logging
+jmeter -n -t $1 -l ./results/"$FILE"_result.jtl -Lorg.apache.jmeter.protocol.http.control=DEBUG -j ./results/jmeter.log
+
+# TODO: parse test result data
