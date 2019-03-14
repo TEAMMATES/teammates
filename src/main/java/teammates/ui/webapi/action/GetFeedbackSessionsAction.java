d560036f535643a6ce0ec91346a77436dbdbc195
junming403 <wjmfelix0403@gmail.com>
2019-03-14 13:43:10 +0800

    consolidate GetFeedbackSessionAction api

--- src/main/java/teammates/ui/webapi/action/GetFeedbackSessionsAction.java
+++ src/main/java/teammates/ui/webapi/action/GetFeedbackSessionsAction.java
@@ -1,10 +1,13 @@
 package teammates.ui.webapi.action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
+import teammates.common.exception.UnauthorizedAccessException;
 import teammates.common.util.Const;
 import teammates.ui.webapi.output.FeedbackSessionsData;
 
@@ -20,12 +23,27 @@ public class GetFeedbackSessionsAction extends Action {
 
     @Override
     public void checkSpecificAccessControl() {
+        if (!(userInfo.isStudent || userInfo.isInstructor)) {
+            throw new UnauthorizedAccessException("Student or instructor privilege is required to access this resource.");
+        }
+
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
-        if (courseId == null) {
-            gateKeeper.verifyInstructorPrivileges(logic.getAccount(userInfo.getId()));
-        } else {
-            CourseAttributes courseAttributes = logic.getCourse(courseId);
-            gateKeeper.verifyAccessible(logic.getInstructorForGoogleId(courseId, userInfo.getId()), courseAttributes);
+        if (userInfo.isInstructor) {
+            if (courseId == null) {
+                gateKeeper.verifyInstructorPrivileges(logic.getAccount(userInfo.getId()));
+            } else {
+                CourseAttributes courseAttributes = logic.getCourse(courseId);
+                gateKeeper.verifyAccessible(logic.getInstructorForGoogleId(courseId, userInfo.getId()), courseAttributes);
+            }
+        }
+
+        if (userInfo.isStudent) {
+            if (courseId == null) {
+                gateKeeper.verifyStudentPrivileges(logic.getAccount(userInfo.getId()));
+            } else {
+                CourseAttributes courseAttributes = logic.getCourse(courseId);
+                gateKeeper.verifyAccessible(logic.getStudentForGoogleId(courseId, userInfo.getId()), courseAttributes);
+            }
         }
     }
 
@@ -35,14 +53,22 @@ public class GetFeedbackSessionsAction extends Action {
 
         List<FeedbackSessionAttributes> feedbackSessionAttributes;
         if (courseId == null) {
-            boolean isInRecycleBin = getBooleanRequestParamValue(Const.ParamsNames.IS_IN_RECYCLE_BIN);
+            if (userInfo.isInstructor) {
+                boolean isInRecycleBin = getBooleanRequestParamValue(Const.ParamsNames.IS_IN_RECYCLE_BIN);
 
-            List<InstructorAttributes> instructors = logic.getInstructorsForGoogleId(userInfo.getId(), true);
+                List<InstructorAttributes> instructors = logic.getInstructorsForGoogleId(userInfo.getId(), true);
 
-            if (isInRecycleBin) {
-                feedbackSessionAttributes = logic.getSoftDeletedFeedbackSessionsListForInstructors(instructors);
+                if (isInRecycleBin) {
+                    feedbackSessionAttributes = logic.getSoftDeletedFeedbackSessionsListForInstructors(instructors);
+                } else {
+                    feedbackSessionAttributes = logic.getFeedbackSessionsListForInstructor(instructors);
+                }
             } else {
-                feedbackSessionAttributes = logic.getFeedbackSessionsListForInstructor(instructors);
+                List<StudentAttributes> students = logic.getStudentsForGoogleId(userInfo.getId());
+                feedbackSessionAttributes = new ArrayList<>();
+                for (StudentAttributes student : students) {
+                    feedbackSessionAttributes.addAll(logic.getFeedbackSessionsForCourse(student.getCourse()));
+                }
             }
         } else {
             feedbackSessionAttributes = logic.getFeedbackSessionsForCourse(courseId);
