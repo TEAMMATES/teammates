c7cd490671d14d64a61321e02448790281e9bedc
Rahul Rajesh <rahul.rajesh@hotmail.com>
2019-03-15 00:01:26 +0800

    Modify action to fix failing tests

--- src/main/java/teammates/ui/webapi/action/GetStudentProfilePictureAction.java
+++ src/main/java/teammates/ui/webapi/action/GetStudentProfilePictureAction.java
@@ -1,11 +1,25 @@
 package teammates.ui.webapi.action;
 
+import org.apache.http.HttpStatus;
+
+import teammates.common.datatransfer.attributes.StudentAttributes;
+import teammates.common.datatransfer.attributes.StudentProfileAttributes;
+import teammates.common.exception.UnauthorizedAccessException;
 import teammates.common.util.Const;
 
 /**
  * Action: serves a profile picture that is stored in Google Cloud Storage.
  */
 public class GetStudentProfilePictureAction extends Action {
+
+    /** String indicating ACCESS is not given. */
+    public static final String UNAUTHORIZED_ACCESS = "You are not allowed to view this resource!";
+
+    /** String indicating student not found. */
+    public static final String STUDENT_NOT_FOUND = "No student found for given parameters";
+
+    /** String indicating profile picture not found. */
+    public static final String PROFILE_PIC_NOT_FOUND = "Student has no profile picture";
 
     @Override
     protected AuthType getMinAuthLevel() {
@@ -14,12 +28,46 @@ public class GetStudentProfilePictureAction extends Action {
 
     @Override
     public void checkSpecificAccessControl() {
-        // Profile picture is available to everyone with the blob key
+        String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
+        String studentEmail = getRequestParamValue(Const.ParamsNames.STUDENT_EMAIL);
+
+        //viewing someone else's photo
+        if (studentEmail != null && courseId != null) {
+            StudentAttributes student = logic.getStudentForEmail(courseId, studentEmail);
+
+            if (student == null) {
+                throw new UnauthorizedAccessException(UNAUTHORIZED_ACCESS);
+            }
+
+            gateKeeper.verifyAccessibleForCurrentUserAsInstructorOrTeamMemberOrAdmin(logic.getAccount(userInfo.id),
+                    courseId, student.getSection(), student.getEmail());
+        }
     }
 
     @Override
     public ActionResult execute() {
-        String blobKey = getNonNullRequestParamValue(Const.ParamsNames.BLOB_KEY);
-        return new ImageResult(blobKey);
+        String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
+        String studentEmail = getRequestParamValue(Const.ParamsNames.STUDENT_EMAIL);
+
+        StudentProfileAttributes studentProfile = null;
+
+        if (studentEmail == null || courseId == null) {
+            if (!userInfo.isStudent) {
+                return new JsonResult(STUDENT_NOT_FOUND, HttpStatus.SC_NOT_FOUND);
+            }
+            studentProfile = logic.getStudentProfile(userInfo.id);
+        } else {
+            StudentAttributes student = logic.getStudentForEmail(courseId, studentEmail);
+            if (student == null) {
+                return new JsonResult(STUDENT_NOT_FOUND, HttpStatus.SC_NOT_FOUND);
+            }
+            studentProfile = logic.getStudentProfile(student.googleId);
+        }
+
+        if (studentProfile == null || studentProfile.pictureKey.equals("")) {
+            return new JsonResult(PROFILE_PIC_NOT_FOUND, HttpStatus.SC_NOT_FOUND);
+        }
+
+        return new ImageResult(studentProfile.pictureKey);
     }
 }
