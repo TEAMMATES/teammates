26f951db619ed66701875011a6ea94f146881f8d
Amrut Prabhu <amrut08@gmail.com>
2019-03-15 20:42:20 +0800

    [#9412] Link Recovery: Provide a method for students to request submissions links to be resent (frontend) (#9447)
    * Add placeholder link recovery page
    
    * Add Link Recovery page to nav bar
    
    * Add link recovery email form
    
    Reduce form width
    
    * Add link recovery http request
    
    Reset email field if success
    
    Add error message for submit with empty input
    
    * Add email format check and error
    
    Disable submit button if invalid input
    
    * Add recaptcha widget with ngx-captcha
    
    Add CSP to backend WebPageServlet
    
    * Change to angular reactive form
    
    * Reload recaptcha on submit
    
    * Remove dynamic error messages
    
    * Change local captcha site key to config value
    
    * Add test file
    
    Import NgxCaptchaModule to fix test
    
    * Update CSP
    
    * Add recaptcha secret key as config value
    
    * Add reCAPTCHA response verification
    
    Change implementation using apache lib
    
    * Add recaptcha test keys
    
    * Add RecaptchaVerifierTest
    
    * Changes based on review
    
    * Add catch for exception
    
    * Remove reload function
    
    * Update catch
    
    * Remove tslint suppression
    
    * Add ApiRequest class
    
    * Refactor RecaptchaVerifier
    
    * Add negative test cases for RecaptchaVerifier
    
    * Minor frontend changes
    
    * Remove API dependency from test
    
    * Update exception handling
    
    * Use Logger to print stack trace
    
    * Change names to SessionLinksRecovery
    
    * Add reCAPTCHA docs
    
    * Remove test keys from config
    
    * Add captcha.md
    
    * Update RecaptchaVerifier and Logger
    
    * Update ApiRequest -> HttpRequest
    
    * Add http timeout
    
    * Remove unnecessary lines
    
    * Changes based on review
    
    * Update test cases
    
    * Minor text changes
    
    * Minor fixes
    
    * Add secret key as method parameter
    
    * Minor fixes

---
+++ src/main/java/teammates/common/util/HttpRequest.java
@@ -0,0 +1,50 @@
+package teammates.common.util;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * A utility class to execute an HTTP request and return the response.
+ */
+public final class HttpRequest {
+
+    private static final int TIMEOUT_IN_MS = 30000;
+
+    private HttpRequest() {
+        // Utility class
+        // Intentional private constructor to prevent instantiation.
+    }
+
+    /**
+     * Executes a HTTP GET request and returns the response string.
+     * @param uri The URI containing the request URL and request parameters.
+     * @return the HTTP response string after executing the GET request
+     */
+    public static String executeGetRequest(URI uri) throws IOException {
+        HttpUriRequest request = new HttpGet(uri);
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(TIMEOUT_IN_MS).build();
+
+        HttpResponse httpResponse = HttpClientBuilder.create()
+                                                     .setDefaultRequestConfig(requestConfig)
+                                                     .build()
+                                                     .execute(request);
+        HttpEntity entity = httpResponse.getEntity();
+        String response = EntityUtils.toString(entity, "UTF-8");
+
+        if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            return response;
+        } else {
+            throw new HttpResponseException(httpResponse.getStatusLine().getStatusCode(), response);
+        }
+    }
+}
