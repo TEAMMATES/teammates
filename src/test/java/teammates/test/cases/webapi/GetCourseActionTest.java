f917a477d335b0ba93eafd9fde6a8aa6c65bd8d9
Jeremy Choo <ChooJeremy@Ymail.com>
2019-03-15 21:40:59 +0800

    Updated tests to support students

--- src/test/java/teammates/test/cases/webapi/GetCourseActionTest.java
+++ src/test/java/teammates/test/cases/webapi/GetCourseActionTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
 import teammates.ui.webapi.action.GetCourseAction;
 import teammates.ui.webapi.action.JsonResult;
@@ -39,7 +40,7 @@ public class GetCourseActionTest extends BaseActionTest<GetCourseAction> {
 
         loginAsInstructor(instructor1OfCourse1.googleId);
 
-        ______TS("typical success case");
+        ______TS("typical success case for instructor");
 
         String[] params = {
                 Const.ParamsNames.COURSE_ID, instructor1OfCourse1.getCourseId(),
@@ -49,6 +50,25 @@ public class GetCourseActionTest extends BaseActionTest<GetCourseAction> {
 
         assertEquals(HttpStatus.SC_OK, response.getStatusCode());
         CourseData courseData = (CourseData) response.getOutput();
+
+        assertEquals(expectedCourse.getId(), courseData.getCourseId());
+        assertEquals(expectedCourse.getName(), courseData.getCourseName());
+        assertEquals(expectedCourse.getTimeZone().getId(), courseData.getTimeZone());
+
+        StudentAttributes student1OfCourse1 = typicalBundle.students.get("student1InCourse1");
+        expectedCourse = logic.getCourse(student1OfCourse1.getCourse());
+        loginAsStudent(student1OfCourse1.googleId);
+
+        ______TS("typical success case for student");
+
+        params = new String[] {
+                Const.ParamsNames.COURSE_ID, student1OfCourse1.getCourse(),
+        };
+        getCourseAction = getAction(params);
+        response = getJsonResult(getCourseAction);
+
+        assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+        courseData = (CourseData) response.getOutput();
 
         assertEquals(expectedCourse.getId(), courseData.getCourseId());
         assertEquals(expectedCourse.getName(), courseData.getCourseName());
@@ -63,6 +83,11 @@ public class GetCourseActionTest extends BaseActionTest<GetCourseAction> {
         ______TS("Not enough parameters");
 
         verifyHttpParameterFailure();
+
+        StudentAttributes student1OfCourse1 = typicalBundle.students.get("student1InCourse1");
+        loginAsStudent(student1OfCourse1.googleId);
+
+        verifyHttpParameterFailure();
     }
 
     @Test
@@ -70,6 +95,15 @@ public class GetCourseActionTest extends BaseActionTest<GetCourseAction> {
         InstructorAttributes instructor1OfCourse1 = typicalBundle.instructors.get("instructor1OfCourse1");
         loginAsInstructor(instructor1OfCourse1.googleId);
 
+        testNonExistentCourse();
+
+        StudentAttributes student1OfCourse1 = typicalBundle.students.get("student1InCourse1");
+        loginAsStudent(student1OfCourse1.googleId);
+
+        testNonExistentCourse();
+    }
+
+    private void testNonExistentCourse() {
         String[] params = {
                 Const.ParamsNames.COURSE_ID, "fake-course",
         };
