aba42f265ddf75536731324392109ef3bb65cbe1
Amrut Prabhu <amrut08@gmail.com>
2019-03-18 00:38:49 +0800

    Generate HTML report

--- src/test/jmeter/runTest.sh
+++ src/test/jmeter/runTest.sh
@@ -5,6 +5,9 @@ FILE=$(echo $1 | cut -d'.' -f 1)
 # TODO: generate sample test data and store it in datastore
 
 # org.apache.jmeter.protocol.http.control - HTTP Controls logging
+# TODO: overwrite instead of append
 jmeter -n -t $1 -l ./results/"$FILE"_result.jtl -Lorg.apache.jmeter.protocol.http.control=DEBUG -j ./results/jmeter.log
 
 # TODO: parse test result data
+
+jmeter -g ./results/"$FILE"_result.jtl -o ./reports/"$FILE"/ -j ./results/jmeter-report.log
