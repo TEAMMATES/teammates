09eaa5eccd149c1ecae3361cf41b64bc97353bcf
Amrut Prabhu <amrut08@gmail.com>
2019-03-20 03:20:24 +0800

    Rename and move test file

--- src/test/jmeter/runTest.sh
+++ src/test/jmeter/runTest.sh
@@ -5,7 +5,7 @@ FILE=$(echo $1 | cut -d'.' -f 1)
 # TODO: generate sample test data and store it in datastore
 
 # TODO: overwrite instead of append
-jmeter -n -t $1 -l ./results/"$FILE"_result.jtl -Lorg.apache.jmeter.protocol.http.control=DEBUG -j ./results/jmeter.log
+jmeter -n -t ./tests/$1 -l ./results/"$FILE"_result.jtl -Lorg.apache.jmeter.protocol.http.control=DEBUG -j ./results/jmeter.log
 
 # TODO: parse test result data
 
