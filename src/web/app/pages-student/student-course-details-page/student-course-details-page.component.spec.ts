29ff8ed90c983dba687838b2d9149faa25c1fbd6
Ayush Chattoraj <ayush.chattoraj@gmail.com>
2019-03-20 01:16:40 +0800

    Add populated test case
    Minor fix
    
    Fix lint errors

--- src/web/app/pages-student/student-course-details-page/student-course-details-page.component.spec.ts
+++ src/web/app/pages-student/student-course-details-page/student-course-details-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Gender } from '../../../types/gender';
 import { StudentCourseDetailsPageComponent } from './student-course-details-page.component';
 
 describe('StudentCourseDetailsPageComponent', () => {
@@ -32,14 +33,51 @@ describe('StudentCourseDetailsPageComponent', () => {
     expect(fixture).toMatchSnapshot();
   });
 
-  it('should snap with a single course', () => {
-    const courseAttributes = {
+  it('should snap with populated fields', () => {
+    const courseAttributes: any = {
       createdAt: '1552509888215',
       id: 'CS3281',
       name: 'Thematic Systems',
       timeZone: 'Asia/Singapore',
     };
+    const instructorDetails: any = [
+      {
+        name: 'Ayush',
+        email: 'ayush@ayush.com',
+      }, {
+        name: 'Byush',
+        email: 'byush@byush.com',
+      },
+    ];
+    const studentDetails: any = {
+      email: 'cyush@cyush.com',
+      course: 'CS3281',
+      name: 'Cyush',
+      googleId: 'cyush@gmail.com',
+      team: 'CSquad',
+      section: 'Section 1',
+    };
+    const teammateProfiles: any = [
+      {
+        shortName: 'ash',
+        email: 'ash@ash.com',
+        institute: 'NUS',
+        nationality: 'Indian',
+        gender: Gender.MALE,
+        pictureKey: 'something.jpg',
+      },
+      {
+        shortName: 'bsh',
+        email: 'bsh@bsh.com',
+        gender: Gender.OTHER,
+        pictureKey: 'something2.jpg',
+      },
+    ];
     component.course = courseAttributes;
+    component.instructorDetails = instructorDetails;
+    component.student = studentDetails;
+    component.teammateProfiles = teammateProfiles;
+
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
