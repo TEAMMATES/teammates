76c903219b85a772e92530c9ec4d3ca87d18c28d
Rahul Rajesh <rahul.rajesh@hotmail.com>
2019-03-19 15:56:54 +0800

    Add snapshot test for student list page

--- src/web/app/pages-instructor/student-list/student-list.component.spec.ts
+++ src/web/app/pages-instructor/student-list/student-list.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StudentListSectionData } from './student-list-section-data';
 import { StudentListComponent } from './student-list.component';
 
 describe('StudentListComponent', () => {
@@ -28,5 +29,145 @@ describe('StudentListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with table head set to hidden', () => {
+    component.isHideTableHead = true;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with some student list data', () => {
+    component.sections = [
+      {
+        students: [
+          {
+            name: 'tester',
+            team: '1',
+            email: 'tester@tester.com',
+            status: 'Yet to join',
+          },
+        ],
+        sectionName: '1',
+        isAllowedToViewStudentInSection: true,
+        isAllowedToModifyStudent: true,
+      },
+      {
+        students: [
+          {
+            name: 'Alice Betsy',
+            team: 'Team 1',
+            email: 'alice.b.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+          {
+            name: 'Benny Charles',
+            team: 'Team 1',
+            email: 'benny.c.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+          {
+            name: 'Danny Engrid',
+            team: 'Team 1',
+            email: 'danny.e.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+          {
+            name: 'Emma Farrell',
+            team: 'Team 1',
+            email: 'emma.f.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+        ],
+        sectionName: 'Tutorial Group 1',
+        isAllowedToViewStudentInSection: true,
+        isAllowedToModifyStudent: true,
+      },
+    ];
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with some student list data and some students to hide', () => {
+    component.sections = [
+      {
+        students: [
+          {
+            name: 'tester',
+            team: '1',
+            email: 'tester@tester.com',
+            status: 'Yet to join',
+          },
+        ],
+        sectionName: '1',
+        isAllowedToViewStudentInSection: true,
+        isAllowedToModifyStudent: true,
+      },
+      {
+        students: [
+          {
+            name: 'Alice Betsy',
+            team: 'Team 1',
+            email: 'alice.b.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+          {
+            name: 'Benny Charles',
+            team: 'Team 1',
+            email: 'benny.c.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+          {
+            name: 'Danny Engrid',
+            team: 'Team 1',
+            email: 'danny.e.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+          {
+            name: 'Emma Farrell',
+            team: 'Team 1',
+            email: 'emma.f.tmms@gmail.tmt',
+            status: 'Joined',
+          },
+        ],
+        sectionName: 'Tutorial Group 1',
+        isAllowedToViewStudentInSection: true,
+        isAllowedToModifyStudent: true,
+      },
+    ];
+
+    component.listOfStudentsToHide = [
+      'alice.b.tmms@gmail.tmt',
+      'tester@tester.com',
+    ];
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with some student list data with no sections', () => {
+    component.sections = [
+      {
+        students: [
+          {
+            name: 'tester',
+            team: '1',
+            email: 'tester@tester.com',
+            status: 'Yet to join',
+          },
+        ],
+        sectionName: 'None',
+        isAllowedToViewStudentInSection: true,
+        isAllowedToModifyStudent: true,
+      },
+    ];
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 });
