5c4e73a54d19dc938820d3bf9ff6a5ce42acf813
Jiang Chunhui <jiangchunhui007@gmail.com>
2019-03-20 13:16:46 +0800

    use create request dto

--- src/main/java/teammates/ui/webapi/request/FeedbackResponseCommentUpdateRequest.java
+++ src/main/java/teammates/ui/webapi/request/FeedbackResponseCommentUpdateRequest.java
@@ -1,39 +1,11 @@
 package teammates.ui.webapi.request;
 
-import javax.annotation.Nullable;
-
 /**
  * The update request of a feedback response comment.
  */
-public class FeedbackResponseCommentUpdateRequest extends BasicRequest {
-
-    private String commentText;
-    @Nullable
-    private String showCommentTo;
-    @Nullable
-    private String showGiverNameTo;
+public class FeedbackResponseCommentUpdateRequest extends FeedbackResponseCommentBasicRequest {
 
     public FeedbackResponseCommentUpdateRequest(String commentText, String showCommentTo, String showGiverNameTo) {
-        this.commentText = commentText;
-        this.showCommentTo = showCommentTo;
-        this.showGiverNameTo = showGiverNameTo;
+        super(commentText, showCommentTo, showGiverNameTo);
     }
-
-    public String getCommentText() {
-        return commentText;
-    }
-
-    public String getShowCommentTo() {
-        return showCommentTo;
-    }
-
-    public String getShowGiverNameTo() {
-        return showGiverNameTo;
-    }
-
-    @Override
-    public void validate() {
-        // TODO decide what to validate
-    }
-
 }
