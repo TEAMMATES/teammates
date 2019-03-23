424598d8064c2d0fbd43784e202774a73a0212e5
Wilson Kurniawan <wkurniawan.92@gmail.com>
2019-03-24 02:10:34 +0800

    Add snapshot tests for admin home page

--- src/web/app/pages-admin/admin-home-page/admin-home-page.component.spec.ts
+++ src/web/app/pages-admin/admin-home-page/admin-home-page.component.spec.ts
@@ -27,4 +27,65 @@ describe('AdminHomePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should snap with default view', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with some instructors details', () => {
+    component.instructorsConsolidated = [
+      {
+        name: 'Instructor A',
+        email: 'instructora@example.com',
+        institution: 'Sample Institution A',
+        status: 'PENDING',
+        joinLink: 'This should not be displayed',
+        message: 'This should not be displayed',
+      },
+      {
+        name: 'Instructor B',
+        email: 'instructorb@example.com',
+        institution: 'Sample Institution B',
+        status: 'SUCCESS',
+        joinLink: 'http://localhost:4200/web/join',
+        message: 'This should not be displayed',
+      },
+      {
+        name: 'Instructor C',
+        email: 'instructorc@example.com',
+        institution: 'Sample Institution C',
+        status: 'FAIL',
+        joinLink: 'This should not be displayed',
+        message: 'The instructor cannot be added for some reason',
+      },
+    ];
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with disabled adding instructor button if there are active requests', () => {
+    component.instructorsConsolidated = [
+      {
+        name: 'Instructor A',
+        email: 'instructora@example.com',
+        institution: 'Sample Institution A',
+        status: 'ADDING',
+        joinLink: 'This should not be displayed',
+        message: 'This should not be displayed',
+      },
+      {
+        name: 'Instructor B',
+        email: 'instructorb@example.com',
+        institution: 'Sample Institution B',
+        status: 'PENDING',
+        joinLink: 'This should not be displayed',
+        message: 'This should not be displayed',
+      },
+    ];
+    component.activeRequests = 1;
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
 });
