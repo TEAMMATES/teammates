6c90abad0c10e250ac4a8527f2cfbed2b1370415
Wilson Kurniawan <wkurniawan.92@gmail.com>
2019-03-24 03:00:55 +0800

    Add AdminHomePageE2ETest

--- src/e2e/java/teammates/e2e/pageobjects/AdminHomePage.java
+++ src/e2e/java/teammates/e2e/pageobjects/AdminHomePage.java
@@ -1,10 +1,34 @@
 package teammates.e2e.pageobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * Represents the admin home page of the website.
- * TODO: migrate with admin home page
  */
 public class AdminHomePage extends AppPage {
+
+    @FindBy(id = "instructor-details-single-line")
+    private WebElement detailsSingleLineTextBox;
+
+    @FindBy (id = "instructor-name")
+    private WebElement nameTextBox;
+
+    @FindBy (id = "instructor-email")
+    private WebElement emailTextBox;
+
+    @FindBy (id = "instructor-institution")
+    private WebElement institutionTextBox;
+
+    @FindBy (id = "add-instructor")
+    private WebElement submitButton;
+
+    @FindBy (id = "add-instructor-single-line")
+    private WebElement submitButtonDetailsSingleLineForm;
+
+    @FindBy (id = "add-all-instructors")
+    private WebElement addAllInstructorsButton;
 
     public AdminHomePage(Browser browser) {
         super(browser);
@@ -12,7 +36,43 @@ public class AdminHomePage extends AppPage {
 
     @Override
     protected boolean containsExpectedPageContents() {
-        // return getPageSource().contains("<h1>Add New Instructor</h1>");
-        return true;
+        return getPageSource().contains("Add New Instructor</h1>");
     }
+
+    public void queueInstructorForAdding(String name, String email, String institute) {
+        if (name != null) {
+            fillTextBox(nameTextBox, name);
+        }
+        if (email != null) {
+            fillTextBox(emailTextBox, email);
+        }
+        if (institute != null) {
+            fillTextBox(institutionTextBox, institute);
+        }
+
+        click(submitButton);
+    }
+
+    public void queueInstructorForAdding(String instructorDetails) {
+        if (instructorDetails != null) {
+            fillTextBox(detailsSingleLineTextBox, instructorDetails);
+        }
+        click(submitButtonDetailsSingleLineForm);
+    }
+
+    public void addAllInstructors() {
+        click(addAllInstructorsButton);
+        waitForElementToBeClickable(addAllInstructorsButton);
+    }
+
+    public String getMessageForInstructor(int i) {
+        By by = By.id("message-instructor-" + i);
+        waitForElementVisibility(by);
+        WebElement element = browser.driver.findElement(by);
+        if (element == null) {
+            return null;
+        }
+        return element.getText();
+    }
+
 }
