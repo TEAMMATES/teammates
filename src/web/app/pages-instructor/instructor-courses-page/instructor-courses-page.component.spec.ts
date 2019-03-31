140a92fb1a96f5b45859b5189c260c327b493f2f
Ayush Chattoraj <ayush.chattoraj@gmail.com>
2019-03-31 22:28:17 +0800

    Add more tests
    Fix lint error

--- src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.component.spec.ts
@@ -42,7 +42,7 @@ describe('InstructorCoursesPageComponent', () => {
   });
 
   it('should snap with all courses in course stats', () => {
-    let activeCourses = [
+    const activeCourses: any[] = [
       {
         id: 'CS3281',
         name: 'Modifiable Students and Courses',
@@ -70,9 +70,9 @@ describe('InstructorCoursesPageComponent', () => {
         createdAt: '2002-11-05T08:15:30+08:00',
         canModifyStudent: false,
         canModifyCourse: true,
-      }
+      },
     ];
-    let archivedCourses = [
+    const archivedCourses: any[] = [
       {
         id: 'CS2104',
         name: 'Can modify archived',
@@ -84,9 +84,9 @@ describe('InstructorCoursesPageComponent', () => {
         name: 'Cannot modify archived',
         createdAt: '2002-11-05T08:15:30+08:00',
         canModifyCourse: false,
-      }
+      },
     ];
-    let deleteCourses = [
+    const deleteCourses: any[] = [
       {
         id: 'CS1020',
         name: 'Can modify deleted',
@@ -100,28 +100,28 @@ describe('InstructorCoursesPageComponent', () => {
         createdAt: '2002-12-05T08:15:30+08:00',
         deletedAt: '2003-12-05T08:15:30+08:00',
         canModifyCourse: false,
-      }
+      },
     ];
-    let courseStats = {
-      'CS3281' : {
+    const courseStats: {[key: string]: { [key: string]: number }} = {
+      CS3281 : {
         sections: 1,
         teams: 1,
         students: 1,
         unregistered: 1,
       },
-      'CS3282': {
+      CS3282: {
         sections: 2,
         teams: 2,
         students: 2,
         unregistered: 2,
       },
-      'CS2103': {
+      CS2103: {
         sections: 3,
         teams: 3,
         students: 3,
         unregistered: 3,
       },
-      'CS2102': {
+      CS2102: {
         sections: 4,
         teams: 4,
         students: 4,
@@ -132,6 +132,139 @@ describe('InstructorCoursesPageComponent', () => {
     component.archivedCourses = archivedCourses;
     component.softDeletedCourses = deleteCourses;
     component.courseStats = courseStats;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap when it is undeletable and unrestorable', () => {
+    const activeCourses: any[] = [
+      {
+        id: 'CS3281',
+        name: 'Modifiable Students and Courses',
+        createdAt: '2018-11-05T08:15:30-05:00',
+        canModifyCourse: true,
+        canModifyStudent: true,
+      },
+      {
+        id: 'CS3282',
+        name: 'Nothing modifiable',
+        createdAt: '2019-02-02T08:15:30+08:00',
+        canModifyCourse: false,
+        canModifyStudent: false,
+      },
+      {
+        id: 'CS2103',
+        name: 'Modifiable Students',
+        createdAt: '2018-11-05T08:15:30+05:30',
+        canModifyStudent: true,
+        canModifyCourse: false,
+      },
+      {
+        id: 'CS2102',
+        name: 'Modifiable Course',
+        createdAt: '2002-11-05T08:15:30+08:00',
+        canModifyStudent: false,
+        canModifyCourse: true,
+      },
+    ];
+    const archivedCourses: any[] = [
+      {
+        id: 'CS2104',
+        name: 'Can modify archived',
+        createdAt: '2002-11-05T08:15:30+08:00',
+        canModifyCourse: true,
+      },
+      {
+        id: 'CS2106',
+        name: 'Cannot modify archived',
+        createdAt: '2002-11-05T08:15:30+08:00',
+        canModifyCourse: false,
+      },
+    ];
+    const deleteCourses: any[] = [
+      {
+        id: 'CS1020',
+        name: 'Can modify deleted',
+        createdAt: '2002-11-05T08:15:30+08:00',
+        deletedAt: '2003-11-05T08:15:30+08:00',
+        canModifyCourse: true,
+      },
+      {
+        id: 'CS2010',
+        name: 'Cannot modify deleted',
+        createdAt: '2002-12-05T08:15:30+08:00',
+        deletedAt: '2003-12-05T08:15:30+08:00',
+        canModifyCourse: false,
+      },
+    ];
+    const courseStats: {[key: string]: { [key: string]: number }} = {
+      CS3281 : {
+        sections: 1,
+        teams: 1,
+        students: 1,
+        unregistered: 1,
+      },
+      CS3282: {
+        sections: 2,
+        teams: 2,
+        students: 2,
+        unregistered: 2,
+      },
+      CS2103: {
+        sections: 3,
+        teams: 3,
+        students: 3,
+        unregistered: 3,
+      },
+      CS2102: {
+        sections: 4,
+        teams: 4,
+        students: 4,
+        unregistered: 4,
+      },
+    };
+    component.activeCourses = activeCourses;
+    component.archivedCourses = archivedCourses;
+    component.softDeletedCourses = deleteCourses;
+    component.courseStats = courseStats;
+    component.canDeleteAll = false;
+    component.canRestoreAll = false;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with no courses in course stats', () => {
+    const activeCourses: any[] = [
+      {
+        id: 'CS3281',
+        name: 'Modifiable Students and Courses',
+        createdAt: '2018-11-05T08:15:30-05:00',
+        canModifyCourse: true,
+        canModifyStudent: true,
+      },
+      {
+        id: 'CS3282',
+        name: 'Nothing modifiable',
+        createdAt: '2019-02-02T08:15:30+08:00',
+        canModifyCourse: false,
+        canModifyStudent: false,
+      },
+      {
+        id: 'CS2103',
+        name: 'Modifiable Students',
+        createdAt: '2018-11-05T08:15:30+05:30',
+        canModifyStudent: true,
+        canModifyCourse: false,
+      },
+      {
+        id: 'CS2102',
+        name: 'Modifiable Course',
+        createdAt: '2002-11-05T08:15:30+08:00',
+        canModifyStudent: false,
+        canModifyCourse: true,
+      },
+    ];
+    component.activeCourses = activeCourses;
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
