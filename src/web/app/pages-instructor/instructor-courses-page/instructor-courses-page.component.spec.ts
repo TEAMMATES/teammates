617efa015ed7e602cf9c2e2f998d350fb575d33d
Chattoraj Ayush <ayush.chattoraj@gmail.com>
2019-04-04 01:05:30 +0800

    [#9480] Add instructor courses snapshot (#9627)
    * Add snapshot with all courses in course stats
    
    * Add more tests
    
    Fix lint error
    
    * Update failing snapshot test
    
    Update failing snapshot test
    
    * Refactor href to use routerlink
    
    * Remove unnecessary cases
    
    * Reuse objects

--- src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.component.spec.ts
@@ -11,7 +11,69 @@ class AddCourseFormStubComponent {}
 describe('InstructorCoursesPageComponent', () => {
   let component: InstructorCoursesPageComponent;
   let fixture: ComponentFixture<InstructorCoursesPageComponent>;
+  const activeCourses: any[] = [
+    {
+      id: 'CS3281',
+      name: 'Modifiable Students and Courses',
+      createdAt: '2018-11-05T08:15:30',
+      canModifyCourse: true,
+      canModifyStudent: true,
+    },
+    {
+      id: 'CS3282',
+      name: 'Nothing modifiable',
+      createdAt: '2019-02-02T08:15:30',
+      canModifyCourse: false,
+      canModifyStudent: false,
+    },
+  ];
 
+  const archivedCourses: any[] = [
+    {
+      id: 'CS2104',
+      name: 'Can modify archived',
+      createdAt: '2002-11-05T08:15:30',
+      canModifyCourse: true,
+    },
+    {
+      id: 'CS2106',
+      name: 'Cannot modify archived',
+      createdAt: '2002-11-05T08:15:30',
+      canModifyCourse: false,
+    },
+  ];
+
+  const deletedCourses: any[] = [
+    {
+      id: 'CS1020',
+      name: 'Can modify deleted',
+      createdAt: '2002-11-05T08:15:30',
+      deletedAt: '2003-11-05T08:15:30',
+      canModifyCourse: true,
+    },
+    {
+      id: 'CS2010',
+      name: 'Cannot modify deleted',
+      createdAt: '2002-12-05T08:15:30',
+      deletedAt: '2003-12-05T08:15:30',
+      canModifyCourse: false,
+    },
+  ];
+
+  const courseStats: {[key: string]: { [key: string]: number }} = {
+    CS3281 : {
+      sections: 1,
+      teams: 1,
+      students: 1,
+      unregistered: 1,
+    },
+    CS3282: {
+      sections: 2,
+      teams: 2,
+      students: 2,
+      unregistered: 2,
+    },
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -35,5 +97,35 @@ describe('InstructorCoursesPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with all courses in course stats', () => {
+    component.activeCourses = activeCourses;
+    component.archivedCourses = archivedCourses;
+    component.softDeletedCourses = deletedCourses;
+    component.courseStats = courseStats;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap when it is undeletable and unrestorable', () => {
+    component.activeCourses = activeCourses;
+    component.archivedCourses = archivedCourses;
+    component.softDeletedCourses = deletedCourses;
+    component.courseStats = courseStats;
+    component.canDeleteAll = false;
+    component.canRestoreAll = false;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with no courses in course stats', () => {
+    component.activeCourses = activeCourses;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 });
