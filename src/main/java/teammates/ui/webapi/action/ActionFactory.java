1491e6911f5879dfea4d01cea1c27e3ccda32033
junming403 <wjmfelix0403@gmail.com>
2019-04-03 19:56:04 +0800

    migrate /accounts api to new endpoints

--- src/main/java/teammates/ui/webapi/action/ActionFactory.java
+++ src/main/java/teammates/ui/webapi/action/ActionFactory.java
@@ -40,7 +40,6 @@ public class ActionFactory {
         map(ResourceURIs.AUTH, GET, GetAuthInfoAction.class);
         map(ResourceURIs.ACCOUNTS_SEARCH, GET, SearchAccountsAction.class);
         map(ResourceURIs.ACCOUNT, GET, GetAccountAction.class);
-        map(ResourceURIs.ACCOUNTS, GET, GetAccountsAction.class);
         map(ResourceURIs.ACCOUNT, POST, CreateAccountAction.class);
         map(ResourceURIs.ACCOUNT, DELETE, DeleteAccountAction.class);
         map(ResourceURIs.ACCOUNT_DOWNGRADE, PUT, DowngradeAccountAction.class);
