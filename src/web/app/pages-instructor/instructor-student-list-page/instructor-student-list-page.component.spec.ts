5dc7bb40a944545a4f1fe6cf30255c19331d14d9
James Thompson <maleintheuk@hotmail.com>
2019-04-04 21:01:26 +0100

    Revert "[9606] add snapshots to component spec to check chevrons are correct"
    This reverts commit ab48aa5be31aa41156baf386b9d4617fa0212cb5.

--- src/web/app/pages-instructor/instructor-student-list-page/instructor-student-list-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-student-list-page/instructor-student-list-page.component.spec.ts
@@ -5,14 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InstructorStudentListPageComponent } from './instructor-student-list-page.component';
 
-const defaultCourse: object = {
-  courseId: 'CS3281',
-  courseName: 'Thematic Systems',
-  creationDate: '26 Feb 23:59 PM',
-  deletionDate: '',
-  timeZone: 'Asia/Singapore',
-};
-
 @Component({ selector: 'tm-student-list', template: '' })
 class StudentListStubComponent {
   @Input() courseId: string = '';
@@ -50,27 +42,5 @@ describe('InstructorStudentListPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should snap correctly when course isChecked=false', () => {
-    const courses: any = [{
-      ...defaultCourse,
-      isChecked: false,
-    }];
-
-    component.courses = courses;
-    fixture.detectChanges();
-    expect(fixture).toMatchSnapshot();
-  });
-
-  it('should snap correctly when course isChecked=true', () => {
-    const courses: any = [{
-      ...defaultCourse,
-      isChecked: true,
-    }];
-
-    component.courses = courses;
-    fixture.detectChanges();
-    expect(fixture).toMatchSnapshot();
   });
 });
