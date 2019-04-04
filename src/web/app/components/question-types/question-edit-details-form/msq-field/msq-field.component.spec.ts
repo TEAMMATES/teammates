3c4429142fda18ef6021d9bc81be44c92a1d433e
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2019-04-04 17:20:57 +0800

    Add snackbar module to msq field

--- src/web/app/components/question-types/question-edit-details-form/msq-field/msq-field.component.spec.ts
+++ src/web/app/components/question-types/question-edit-details-form/msq-field/msq-field.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material';
 import { MsqFieldComponent } from './msq-field.component';
 
 describe('MsqFieldComponent', () => {
@@ -12,6 +13,7 @@ describe('MsqFieldComponent', () => {
       declarations: [MsqFieldComponent],
       imports: [
         FormsModule,
+        MatSnackBarModule,
       ],
     })
     .compileComponents();
