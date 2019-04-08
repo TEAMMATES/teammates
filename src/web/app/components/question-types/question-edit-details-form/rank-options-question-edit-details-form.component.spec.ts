0d1e5d3f25d6424812db04babdc4f15390529915
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2019-04-08 22:53:43 +0800

    [#9382] Add support for rank options question (#9650)
    * Add instructor session edit page for rank options
    
    * Set up submission page
    
    * Update backend validation
    
    * Make minor fixes
    
    * Improve logic for submission page initialization
    
    * Tweak instructions section in submission page
    
    * Change indicator for isEnabled
    
    * Make cosmetic changes
    
    * Use filters instead of loops
    
    * Make Minor changes

---
+++ src/web/app/components/question-types/question-edit-details-form/rank-options-question-edit-details-form.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormsModule } from '@angular/forms';
+import { RankOptionsFieldComponent } from './rank-options-field/rank-options-field.component';
+import { RankOptionsQuestionEditDetailsFormComponent } from './rank-options-question-edit-details-form.component';
+
+describe('RankOptionsQuestionEditDetailsFormComponent', () => {
+  let component: RankOptionsQuestionEditDetailsFormComponent;
+  let fixture: ComponentFixture<RankOptionsQuestionEditDetailsFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        RankOptionsQuestionEditDetailsFormComponent,
+        RankOptionsFieldComponent],
+      imports: [
+        FormsModule,
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RankOptionsQuestionEditDetailsFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
