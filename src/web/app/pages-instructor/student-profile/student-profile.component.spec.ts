317cbd023bbafad397b7ff73bc36f9e53b2f677b
Rahul Rajesh <rahul.rajesh@hotmail.com>
2019-04-12 21:19:32 +0800

    Remove unnecessary import

--- src/web/app/pages-instructor/student-profile/student-profile.component.spec.ts
+++ src/web/app/pages-instructor/student-profile/student-profile.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { StudentProfileComponent } from './student-profile.component';
 
 describe('StudentProfileComponent', () => {
