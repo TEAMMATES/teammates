e54ef62b103c99b4761ef34654f2147278938713
Rahul Rajesh <rahul.rajesh@hotmail.com>
2019-04-17 15:30:03 +0800

    Update course details page to use new APIs

--- src/web/app/pages-student/student-course-details-page/student-course-details-page.component.ts
+++ src/web/app/pages-student/student-course-details-page/student-course-details-page.component.ts
@@ -1,47 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { CourseService } from '../../../services/course.service';
 import { HttpRequestService } from '../../../services/http-request.service';
 import { StatusMessageService } from '../../../services/status-message.service';
+import { StudentProfileService } from '../../../services/student-profile.service';
+import { StudentService } from '../../../services/student.service';
+import { Course, Instructors, Student, Students } from '../../../types/api-output';
 import { Gender } from '../../../types/gender';
 import { ErrorMessageOutput } from '../../error-message-output';
+import { Intent } from '../../Intent';
+import { StudentProfile } from '../../pages-instructor/student-profile/student-profile';
 
-interface StudentAttributes {
-  email: string;
-  course: string;
-  name: string;
-  googleId: string;
-  team: string;
-  section: string;
-}
-
-interface InstructorDetails {
-  name: string;
-  email: string;
-}
-
-interface CourseAttributes {
-  createdAt: string;
-  id: string;
-  name: string;
-  timeZone: string;
-}
-
-interface TeammateProfile {
-  shortName: string;
-  email: string;
-  institute: string;
-  nationality: string;
-  gender: Gender;
-  pictureKey: string;
-  photoUrl?: string;
-}
-
-interface StudentCourseDetails {
-  student: StudentAttributes;
-  course: CourseAttributes;
-  instructorDetails: InstructorDetails[];
-  teammateProfiles?: TeammateProfile[];
+interface StudentProfileWithPicture extends StudentProfile {
+  photoUrl: string;
 }
 
 /**
@@ -55,67 +27,113 @@ interface StudentCourseDetails {
 export class StudentCourseDetailsPageComponent implements OnInit {
   Gender: typeof Gender = Gender; // enum
   user: string = '';
-  student?: StudentAttributes;
-  course?: CourseAttributes;
-  instructorDetails?: InstructorDetails[];
-  teammateProfiles?: TeammateProfile[];
+  student?: Student;
+  course?: Course;
+  instructorDetails?: Instructors;
+  teammateProfiles: StudentProfileWithPicture[] = [];
 
-  constructor(private route: ActivatedRoute, private httpRequestService: HttpRequestService,
+  constructor(private route: ActivatedRoute,
+              private httpRequestService: HttpRequestService,
+              private studentProfileService: StudentProfileService,
+              private studentService: StudentService,
+              private courseService: CourseService,
               private statusMessageService: StatusMessageService) { }
 
+  /**
+   * Fetches relevant data to be displayed on page.
+   */
   ngOnInit(): void {
     this.route.queryParams.subscribe((queryParams: any) => {
       this.user = queryParams.user;
-      this.loadStudentDetails(queryParams.courseid, this.user);
+      this.loadStudent(queryParams.courseid);
+      this.loadCourse(queryParams.courseid);
+      this.loadInstructors(queryParams.courseid);
     });
   }
 
   /**
-   * Fetch the data to be displayed on the page.
+   * Loads the course details.
    * @param courseid: id of the course queried
-   * @param user: only used in admin masquerade mode, value should be student id
    */
-  loadStudentDetails(courseid: string, user: string): void {
-    const paramMap: { [key: string]: string } = { courseid, user };
-    this.httpRequestService.get('/student/course', paramMap).subscribe((resp: StudentCourseDetails) => {
-      this.student = resp.student;
-      this.instructorDetails = resp.instructorDetails;
-      this.course = resp.course;
-      this.teammateProfiles = resp.teammateProfiles;
-
-      if (!this.student) {
-        this.statusMessageService.showErrorMessage('Error retrieving student details');
-      }
-
-      if (!this.course) {
-        this.statusMessageService.showErrorMessage('Error retrieving course details');
-      }
-
-      if (!resp.instructorDetails) {
-        this.statusMessageService.showErrorMessage('Error retrieving instructor details');
-      }
-
-      if (!this.teammateProfiles) {
-        this.statusMessageService.showWarningMessage('You do not have any teammates yet.');
-      }
-
-      if (this.teammateProfiles && this.course && this.student) {
-        const courseId: string = this.course.id;
-        const studentEmail: string = this.student.email;
-        this.teammateProfiles.forEach((teammateProfile: TeammateProfile) => teammateProfile.photoUrl
-          = `${environment.backendUrl}/webapi/student/profilePic?courseid=${courseId}
-          &studentemail=${studentEmail}`);
-      }
-
-    }, (resp: ErrorMessageOutput) => {
-      this.statusMessageService.showErrorMessage(resp.error.message);
+  loadCourse(courseid: string): void {
+    this.courseService.getCourse(courseid).subscribe((course: Course) => {
+      this.course = course;
     });
+  }
+
+  /**
+   * Loads the students of the course.
+   * @param courseid: id of the course queried
+   */
+  loadStudent(courseid: string): void {
+    const paramMap: { [key: string]: string } = {
+      courseid,
+    };
+
+    this.httpRequestService.get('/student', paramMap)
+        .subscribe((student: Student) => {
+          this.student = student;
+          this.loadTeammates(courseid, student.teamName);
+        }, (resp: ErrorMessageOutput) => {
+          this.statusMessageService.showErrorMessage(resp.error.message);
+        });
+  }
+
+  /**
+   * Loads the teammates of the current student.
+   * @param courseid: id of the course queried
+   * @param teamName: team of current student
+   */
+  loadTeammates(courseid: string, teamName: string): void {
+    this.studentService.getStudentsFromCourseAndTeam(courseid, teamName)
+        .subscribe((students: Students) => {
+          students.students.forEach((student: Student) => {
+            this.studentProfileService.getStudentProfile(student.email, courseid)
+                  .subscribe((studentProfile: StudentProfile) => {
+                    const newPhotoUrl: string =
+              `${environment.backendUrl}/webapi/student/profilePic?courseid=${courseid}&studentemail=${student.email}`;
+
+                    const newTeammateProfile: StudentProfileWithPicture = {
+                      shortName: studentProfile.shortName,
+                      email: studentProfile.email,
+                      institute: studentProfile.institute,
+                      nationality: studentProfile.institute,
+                      gender: studentProfile.gender,
+                      moreInfo: studentProfile.moreInfo,
+                      pictureKey: studentProfile.pictureKey,
+                      photoUrl : newPhotoUrl,
+                    };
+                    this.teammateProfiles.push(newTeammateProfile);
+                  });
+          });
+
+        }, (resp: ErrorMessageOutput) => {
+          this.statusMessageService.showErrorMessage(resp.error.message);
+        });
+  }
+
+  /**
+   * Loads the instructors of the course.
+   * @param courseid: id of the course queried
+   */
+  loadInstructors(courseid: string): void {
+    const paramMap: { [key: string]: string } = {
+      courseid,
+      intent: Intent.FULL_DETAIL,
+    };
+
+    this.httpRequestService.get('/instructors', paramMap)
+        .subscribe((instructors: Instructors) => {
+          this.instructorDetails = instructors;
+        }, (resp: ErrorMessageOutput) => {
+          this.statusMessageService.showErrorMessage(resp.error.message);
+        });
   }
 
   /**
    * Sets the profile picture of a student as the default image
    */
-  setDefaultPic(teammateProfile: TeammateProfile): void {
+  setDefaultPic(teammateProfile: StudentProfileWithPicture): void {
     teammateProfile.photoUrl = '/assets/images/profile_picture_default.png';
   }
 }
