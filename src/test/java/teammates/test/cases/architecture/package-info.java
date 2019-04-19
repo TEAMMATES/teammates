d570536a50789e773281ee111fa00e63aff341fa
Wilson Kurniawan <wilson@visenze.com>
2019-04-20 02:21:41 +0800

    [#9386] Replace Macker with ArchUnit (#9615)
    * Change Macker to ArchUnit
    
    * Use CheckStyle IllegalImport for Date and Calendar
    
    * Migrate all Macker rules to ArchUnit

---
+++ src/test/java/teammates/test/cases/architecture/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Test cases for checking the system's architecture.
+ */
+package teammates.test.cases.architecture;
--- src/test/java/teammates/test/cases/datatransfer/StudentProfileAttributesTest.java
+++ src/test/java/teammates/test/cases/datatransfer/StudentProfileAttributesTest.java
@@ -374,28 +374,4 @@ public class StudentProfileAttributesTest extends BaseAttributesTest {
                 .build();
     }
 
-    /**
-     * SUT: {@link StudentProfileAttributes.Gender}.
-     */
-    public static class GenderTest {
-        @Test
-        public void testGetGenderEnumValue() {
-            // invalid values
-            assertEquals(StudentProfileAttributes.Gender.OTHER,
-                    StudentProfileAttributes.Gender.getGenderEnumValue(null));
-            assertEquals(StudentProfileAttributes.Gender.OTHER,
-                         StudentProfileAttributes.Gender.getGenderEnumValue("'\"'invalidGender"));
-            assertEquals(StudentProfileAttributes.Gender.OTHER,
-                         StudentProfileAttributes.Gender.getGenderEnumValue("invalidGender"));
-
-            // valid values
-            assertEquals(StudentProfileAttributes.Gender.MALE,
-                         StudentProfileAttributes.Gender.getGenderEnumValue("MALE"));
-            assertEquals(StudentProfileAttributes.Gender.FEMALE,
-                         StudentProfileAttributes.Gender.getGenderEnumValue("female"));
-            assertEquals(StudentProfileAttributes.Gender.OTHER,
-                         StudentProfileAttributes.Gender.getGenderEnumValue("oTheR"));
-        }
-    }
-
 }
