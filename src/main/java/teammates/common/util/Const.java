3a15f367c2990b92558afffdec873c1f8c2007a1
Wang Junming <905760127@qq.com>
2019-04-21 20:06:56 +0800

    [#9595] Migrate Get /accounts to RESTFul API  (#9641)
    * migrate /accounts api to new endpoints
    
    * migrate /courses to new endpoints
    
    * use merge to handle multipe requests
    
    * use forkjoin
    
    * minor fix
    
    * minor fix
    
    * minor fix

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -951,7 +951,6 @@ public final class Const {
         public static final String ERROR_REPORT = "/errorreport";
         public static final String AUTH = "/auth";
         public static final String ACCOUNT = "/account";
-        public static final String ACCOUNTS = "/accounts";
         public static final String ACCOUNTS_SEARCH = "/accounts/search";
         public static final String ACCOUNT_RESET = "/account/reset";
         public static final String ACCOUNT_DOWNGRADE = "/account/downgrade";
