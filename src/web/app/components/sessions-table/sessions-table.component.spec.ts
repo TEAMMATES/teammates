56e91cc353175283d8256a7d4d3dac22b35f5397
Monika Manuela Hengki <manuela.monika97@gmail.com>
2019-04-21 17:27:47 +0800

    [#9480] Add snapshot tests for sessions table component (#9664)
    * Add test case with no feedback session
    
    * Add test case for session table like in instructor home page
    
    * Add test case for session table like in sessions page

--- src/web/app/components/sessions-table/sessions-table.component.spec.ts
+++ src/web/app/components/sessions-table/sessions-table.component.spec.ts
@@ -1,5 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  FeedbackSession,
+  FeedbackSessionPublishStatus,
+  FeedbackSessionSubmissionStatus,
+  InstructorPrivilege,
+  ResponseVisibleSetting,
+  SessionVisibleSetting,
+} from '../../../types/api-output';
+import { SessionsTableColumn, SessionsTableRowModel, SortBy } from './sessions-table-model';
 import { SessionsTableComponent } from './sessions-table.component';
 import { SessionsTableModule } from './sessions-table.module';
 
@@ -22,5 +31,95 @@ describe('SessionsTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  const feedbackSession1: FeedbackSession = {
+    courseId: 'GOT',
+    timeZone: 'Asia/Singapore',
+    feedbackSessionName: 'Season 8 Review',
+    instructions: 'Fill up all',
+    submissionStartTimestamp: 1555232400,
+    submissionEndTimestamp: 1555332400,
+    gracePeriod: 0,
+    sessionVisibleSetting: SessionVisibleSetting.AT_OPEN,
+    responseVisibleSetting: ResponseVisibleSetting.AT_VISIBLE,
+    submissionStatus: FeedbackSessionSubmissionStatus.OPEN,
+    publishStatus: FeedbackSessionPublishStatus.NOT_PUBLISHED,
+    isClosingEmailEnabled: true,
+    isPublishedEmailEnabled: true,
+    createdAtTimestamp: 1554967204,
+  };
+
+  const feedbackSession2: FeedbackSession = {
+    courseId: 'GOT',
+    timeZone: 'Asia/Singapore',
+    feedbackSessionName: 'Season 7 Review',
+    instructions: 'Fill up all',
+    submissionStartTimestamp: 1554232400,
+    submissionEndTimestamp: 1554332400,
+    gracePeriod: 100,
+    sessionVisibleSetting: SessionVisibleSetting.AT_OPEN,
+    responseVisibleSetting: ResponseVisibleSetting.LATER,
+    submissionStatus: FeedbackSessionSubmissionStatus.CLOSED,
+    publishStatus: FeedbackSessionPublishStatus.PUBLISHED,
+    isClosingEmailEnabled: false,
+    isPublishedEmailEnabled: false,
+    createdAtTimestamp: 1554967204,
+  };
+
+  const instructorCanEverything: InstructorPrivilege = {
+    canModifyCourse: true,
+    canModifySession: true,
+    canModifyStudent: true,
+    canModifyInstructor: true,
+    canViewStudentInSections: true,
+    canModifySessionCommentsInSections: true,
+    canViewSessionInSections: true,
+    canSubmitSessionInSections: true,
+  };
+
+  const instructorCannotEverything: InstructorPrivilege = {
+    canModifyCourse: false,
+    canModifySession: false,
+    canModifyStudent: false,
+    canModifyInstructor: false,
+    canViewStudentInSections: false,
+    canModifySessionCommentsInSections: false,
+    canViewSessionInSections: false,
+    canSubmitSessionInSections: false,
+  };
+
+  const sessionTable1: SessionsTableRowModel = {
+    feedbackSession: feedbackSession1,
+    responseRate: '8 / 9',
+    isLoadingResponseRate: false,
+    instructorPrivilege: instructorCanEverything,
+  };
+
+  const sessionTable2: SessionsTableRowModel = {
+    feedbackSession: feedbackSession2,
+    responseRate: '',
+    isLoadingResponseRate: true,
+    instructorPrivilege: instructorCannotEverything,
+  };
+
+  it('should snap like in home page with 2 sessions sorted by start date', () => {
+    component.columnsToShow = [SessionsTableColumn.START_DATE, SessionsTableColumn.END_DATE];
+    component.sessionsTableRowModelsSortBy = SortBy.START_DATE;
+    component.sessionsTableRowModels = [sessionTable1, sessionTable2];
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap like in sessions page with 2 sessions sorted by session name', () => {
+    component.columnsToShow = [SessionsTableColumn.COURSE_ID];
+    component.sessionsTableRowModelsSortBy = SortBy.FEEDBACK_SESSION_NAME;
+    component.sessionsTableRowModels = [sessionTable1, sessionTable2];
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 });
