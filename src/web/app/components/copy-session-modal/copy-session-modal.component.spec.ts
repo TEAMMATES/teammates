9fc6b88567858e9850af1afca4edcfcb0f24a892
crizyli <liyufei6666@gmail.com>
2019-06-04 14:57:21 +0800

    add tests for copy-session-modal.component.spec.ts

--- src/web/app/components/copy-session-modal/copy-session-modal.component.spec.ts
+++ src/web/app/components/copy-session-modal/copy-session-modal.component.spec.ts
@@ -1,8 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { FormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CopySessionModalComponent } from './copy-session-modal.component';
+import {
+  Course,
+  FeedbackSession,
+  FeedbackSessionPublishStatus,
+  FeedbackSessionSubmissionStatus,
+  ResponseVisibleSetting,
+  SessionVisibleSetting
+} from "../../../types/api-output";
+import {By} from "@angular/platform-browser";
+import {DebugElement} from "@angular/core";
 
 describe('CopySessionModalComponent', () => {
   let component: CopySessionModalComponent;
@@ -18,7 +28,7 @@ describe('CopySessionModalComponent', () => {
         NgbActiveModal,
       ],
     })
-    .compileComponents();
+        .compileComponents();
   }));
 
   beforeEach(() => {
@@ -30,4 +40,108 @@ describe('CopySessionModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  const feedbackSessionToCopy: FeedbackSession = {
+    courseId: 'Test01',
+    timeZone: 'Asia/Singapore',
+    feedbackSessionName: 'Test session',
+    instructions: 'Answer all',
+    submissionStartTimestamp: 1555232400,
+    submissionEndTimestamp: 1555332400,
+    gracePeriod: 0,
+    sessionVisibleSetting: SessionVisibleSetting.AT_OPEN,
+    responseVisibleSetting: ResponseVisibleSetting.AT_VISIBLE,
+    submissionStatus: FeedbackSessionSubmissionStatus.OPEN,
+    publishStatus: FeedbackSessionPublishStatus.NOT_PUBLISHED,
+    isClosingEmailEnabled: true,
+    isPublishedEmailEnabled: true,
+    createdAtTimestamp: 1554967204,
+  };
+
+  const courseSessionIn: Course = {
+    courseId: 'Test01',
+    courseName: 'Sample Course 101',
+    creationDate: '13 Mar 2019',
+    deletionDate: 'Not Applicable',
+    timeZone: 'UTC',
+  };
+
+  const courseCopyTo: Course = {
+    courseId: 'Test02',
+    courseName: 'Sample Course 202',
+    creationDate: '14 Mar 2019',
+    deletionDate: 'Not Applicable',
+    timeZone: 'UTC',
+  };
+
+  it('should bind fields correctly', fakeAsync(() => {
+    component.newFeedbackSessionName = feedbackSessionToCopy.feedbackSessionName;
+    component.courseCandidates = [courseSessionIn, courseCopyTo];
+    component.sessionToCopyCourseId = courseSessionIn.courseId;
+    fixture.detectChanges();
+    tick();
+
+    const nameInput: any = fixture.debugElement.query(By.css('.form-control'));
+    const sessionInCourseSpan: any = fixture.debugElement.query(By.css('span.text-danger'));
+    const copyButton: any = fixture.debugElement.query(By.css('button.btn.btn-primary'));
+
+    expect(nameInput.nativeElement.value).toEqual('Test session');
+    expect(component.courseCandidates.length).toEqual(2);
+    expect(sessionInCourseSpan.nativeElement.textContent).toEqual('Test01');
+    expect(copyButton.nativeElement.disabled).toBeTruthy();
+  }));
+
+  it('should set the course to copy to', fakeAsync(() => {
+    component.newFeedbackSessionName = feedbackSessionToCopy.feedbackSessionName;
+    component.courseCandidates = [courseSessionIn, courseCopyTo];
+    component.sessionToCopyCourseId = courseSessionIn.courseId;
+    fixture.detectChanges();
+    tick();
+
+    const options: DebugElement[] = fixture.debugElement.queryAll(By.css('input[type="radio"]'));
+    const secondOption: any = options[1];
+    secondOption.triggerEventHandler('change', { target: secondOption.nativeElement });
+    fixture.detectChanges();
+    tick();
+
+    const nameInput: any = fixture.debugElement.query(By.css('.form-control'));
+    const sessionInCourseSpan: any = fixture.debugElement.query(By.css('span.text-danger'));
+    const copyButton: any = fixture.debugElement.query(By.css('button.btn.btn-primary'));
+
+    expect(nameInput.nativeElement.value).toEqual('Test session');
+    expect(component.courseCandidates.length).toEqual(2);
+    expect(sessionInCourseSpan.nativeElement.textContent).toEqual('Test01');
+    expect(copyButton.nativeElement.disabled).toBeFalsy();
+  }));
+
+  it('should snap with some session and courses candidates', fakeAsync(() => {
+    component.newFeedbackSessionName = feedbackSessionToCopy.feedbackSessionName;
+    component.courseCandidates = [courseSessionIn, courseCopyTo];
+    component.sessionToCopyCourseId = courseSessionIn.courseId;
+    fixture.detectChanges();
+    tick();
+
+    expect(fixture).toMatchSnapshot();
+  }));
+
+  it('should snap with course to copy to is set', fakeAsync(() => {
+    component.newFeedbackSessionName = feedbackSessionToCopy.feedbackSessionName;
+    component.courseCandidates = [courseSessionIn, courseCopyTo];
+    component.sessionToCopyCourseId = courseSessionIn.courseId;
+    fixture.detectChanges();
+    tick();
+
+    const options: DebugElement[] = fixture.debugElement.queryAll(By.css('input[type="radio"]'));
+    const secondOption: any = options[1];
+    secondOption.triggerEventHandler('change', { target: secondOption.nativeElement });
+    fixture.detectChanges();
+    tick();
+
+    expect(fixture).toMatchSnapshot();
+  }));
+
 });
