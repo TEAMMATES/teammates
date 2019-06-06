65d63a1a22305f2fd7c2f06fbc3616170690ba04
erjillsison <erjillsison@gmail.com>
2019-06-06 13:26:50 +0800

    Update error-report.component.spec.ts

--- src/web/app/components/error-report/error-report.component.spec.ts
+++ src/web/app/components/error-report/error-report.component.spec.ts
@@ -2,18 +2,23 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { ErrorReportComponent } from './error-report.component';
+import { HttpRequestService } from "../../../services/http-request.service";
+import { of, throwError } from "rxjs";
 
 describe('ErrorReportComponent', () => {
   let component: ErrorReportComponent;
   let fixture: ComponentFixture<ErrorReportComponent>;
+  let httpRequestService: HttpRequestService;
 
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [ErrorReportComponent],
       imports: [
         FormsModule,
         HttpClientTestingModule,
       ],
+      providers: [HttpRequestService],
     })
     .compileComponents();
   }));
@@ -21,6 +26,7 @@ describe('ErrorReportComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorReportComponent);
     component = fixture.componentInstance;
+    httpRequestService = TestBed.get(HttpRequestService);
     fixture.detectChanges();
   });
 
@@ -41,18 +47,82 @@ describe('ErrorReportComponent', () => {
 
     fixture.detectChanges();
 
-    const p = fixture.nativeElement.querySelector('p');
+    const p: any = fixture.nativeElement.querySelector('p');
     if (p) {
       expect(p.textContent).toContain(error);
     }
   });
 
+  it('should have the requestID form disabled ', () => {
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input.disabled).toBeTruthy();
+  });
+
+  it('should have pre-filled Subject input form', () => {
+    const input: any = fixture.nativeElement.querySelectorAll('input');
+
+    expect(component.subject).toEqual('User-submitted Error Report');
+    expect(input[1].value).toEqual(component.subject);
+  });
+
   it('should get user input from Subject form', () => {
-    const input = fixture.nativeElement.querySelectorAll('input');
+    const input: any = fixture.nativeElement.querySelectorAll('input');
 
     expect(component.subject).toEqual('User-submitted Error Report');
     input[1].value = 'testInput';
     input[1].dispatchEvent((new Event('input')));
     expect(component.subject).toEqual('testInput');
   });
+
+  it('should disable send report button and after successfully sending report', () => {
+    const button: any = fixture.nativeElement.querySelector('button');
+
+    expect(component.sendButtonEnabled).toBeTruthy();
+    expect(component.errorReportSubmitted).toBeFalsy();
+
+    spyOn(httpRequestService, 'post').and.returnValue(of(''));
+    button.click();
+
+    expect(component.sendButtonEnabled).toBeFalsy();
+    expect(component.errorReportSubmitted).toBeTruthy();
+  });
+
+  it('should not disable send report button after unsuccessfully sending report', () => {
+    const button: any = fixture.nativeElement.querySelector('button');
+
+    expect(component.sendButtonEnabled).toBeTruthy();
+    expect(component.errorReportSubmitted).toBeFalsy();
+
+    spyOn(httpRequestService, 'post').and.returnValue(throwError({
+      error: {
+        message: 'This is the error message',
+      },
+    }));
+    button.click();
+
+    expect(component.sendButtonEnabled).toBeTruthy();
+    expect(component.errorReportSubmitted).toBeFalsy();
+  });
+
+
+  it('should snap with default view', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with some content', () => {
+    component.content = 'some content';
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with disabled send report button after sending report', () => {
+
+    component.sendButtonEnabled = false;
+    component.errorReportSubmitted = true;
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
 });
