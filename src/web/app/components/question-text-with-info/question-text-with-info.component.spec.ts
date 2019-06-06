7f85085c0f8fe301b1d122a193a1140acf58747c
Pengyu Zhao <zpyfengyingwu@gmail.com>
2019-06-06 16:32:48 +0800

    add test cases for question-text-with-info

--- src/web/app/components/question-text-with-info/question-text-with-info.component.spec.ts
+++ src/web/app/components/question-text-with-info/question-text-with-info.component.spec.ts
@@ -5,6 +5,8 @@ import {
   QuestionAdditionalInfoModule,
 } from '../question-types/question-additional-info/question-additional-info.module';
 import { QuestionTextWithInfoComponent } from './question-text-with-info.component';
+import {By} from "@angular/platform-browser";
+import {FeedbackQuestionType} from "../../../types/api-output";
 
 describe('QuestionTextWithInfoComponent', () => {
   let component: QuestionTextWithInfoComponent;
@@ -29,5 +31,36 @@ describe('QuestionTextWithInfoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not show control link and question details for TEXT questions', () => {
+    const questionDetailControlLink = fixture.debugElement.query(By.css('a'));
+    const explicitQuestionDetail = fixture.debugElement.query(By.css('div'));
+    expect(questionDetailControlLink).toBeNull();
+    expect(explicitQuestionDetail).toBeNull();
+  });
+
+  it('should show control link when question type is not TEXT',() => {
+    component.questionDetails.questionType = FeedbackQuestionType.MCQ;
+    component.questionDetails.questionText = 'MCQ question details';
+    fixture.detectChanges();
+    const questionDetailControlLink = fixture.debugElement.query(By.css('a'));
+    const explicitQuestionDetail = fixture.debugElement.query(By.css('div'));
+    expect(questionDetailControlLink).not.toBeNull();
+    expect(explicitQuestionDetail).toBeNull();
+  });
+
+  it('should show question detail when click control link',() => {
+    component.questionDetails.questionType = FeedbackQuestionType.MCQ;
+    component.questionDetails.questionText = 'MCQ question details';
+    fixture.detectChanges();
+    const questionDetailControlLink = fixture.debugElement.query(By.css('a'));
+    questionDetailControlLink.nativeElement.click();
+    fixture.detectChanges();
+    const explicitQuestionDetail = fixture.debugElement.query(By.css('div'));
+    const mcqQuestionDetail = fixture.debugElement.query(By.css('tm-mcq-question-additional-info'));
+    expect(questionDetailControlLink).not.toBeNull();
+    expect(explicitQuestionDetail).not.toBeNull();
+    expect(mcqQuestionDetail).not.toBeNull();
   });
 });
