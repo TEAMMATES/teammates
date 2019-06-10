2bd38afe52a67317bdb24cec8065406d9c550bfa
Pengyu Zhao <zpyfengyingwu@gmail.com>
2019-06-10 14:52:25 +0800

    fix choice weights not shown in unidirectional-data-flow pattern.

--- src/web/app/components/question-types/question-edit-details-form/mcq-question-edit-details-form.component.ts
+++ src/web/app/components/question-types/question-edit-details-form/mcq-question-edit-details-form.component.ts
@@ -112,9 +112,11 @@ export class McqQuestionEditDetailsFormComponent
   triggerWeightsColumn(event: any): void {
     const fieldsToUpdate: any = {};
     if (!event.target.checked) {
+      this.model.hasAssignedWeights = false;
       fieldsToUpdate.mcqWeights = [];
       fieldsToUpdate.mcqOtherWeight = 0;
     } else {
+      this.model.hasAssignedWeights = true;
       fieldsToUpdate.mcqWeights = Array(this.model.numOfMcqChoices).fill(0);
     }
     this.triggerModelChangeBatch(fieldsToUpdate);
