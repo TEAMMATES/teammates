944ae4400aac9a87e46935a1e7ba86e29445fd95
Pengyu Zhao <zpyfengyingwu@gmail.com>
2019-06-13 15:17:09 +0800

    update test cases.

--- src/web/app/components/status-message/status-message.component.spec.ts
+++ src/web/app/components/status-message/status-message.component.spec.ts
@@ -37,32 +37,31 @@ describe('StatusMessageComponent', () => {
   it('should show all three messages', () => {
     component.messages = messages;
     fixture.detectChanges();
-    const statusMessages: HTMLElement[] = fixture.nativeElement.querySelectorAll('div.alert div');
-    expect(statusMessages.length).toEqual(3);
-    expect(statusMessages[0].innerHTML).toEqual('a');
-    expect(statusMessages[1].innerHTML).toEqual('b');
-    expect(statusMessages[2].innerHTML).toEqual('c');
+
     expect(fixture).toMatchSnapshot();
   });
 
   it('should delete a message after clicking button', () => {
     component.messages = messages;
     fixture.detectChanges();
-    const statusMessages: HTMLElement[] = fixture.nativeElement.querySelectorAll('div.alert div');
+
     const buttons: HTMLElement[] = fixture.nativeElement.querySelectorAll('div.alert button');
-    expect(statusMessages.length).toEqual(3);
-    expect(buttons.length).toEqual(3);
+
     buttons[0].click();
     fixture.detectChanges();
+
     const statusMessagesAfterClick: HTMLElement[] = fixture.nativeElement.querySelectorAll('div.alert div');
+
     expect(statusMessagesAfterClick.length).toEqual(2);
     expect(statusMessagesAfterClick[0].innerHTML).toEqual('b');
     expect(statusMessagesAfterClick[1].innerHTML).toEqual('c');
-    expect(fixture).toMatchSnapshot();
+
     buttons[1].click();
     buttons[2].click();
     fixture.detectChanges();
+
     const deleteAll: HTMLElement[] = fixture.nativeElement.querySelectorAll('div.alert div');
+
     expect(deleteAll.length).toEqual(0);
   });
 
