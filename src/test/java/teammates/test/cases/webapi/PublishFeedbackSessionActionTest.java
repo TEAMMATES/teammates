47dcbb922c26dfaf1838868b4aa1fdc0d4045a8a
erjillsison <erjillsison@gmail.com>
2019-06-14 11:30:03 +0800

    Add failure test cases

--- src/test/java/teammates/test/cases/webapi/PublishFeedbackSessionActionTest.java
+++ src/test/java/teammates/test/cases/webapi/PublishFeedbackSessionActionTest.java
@@ -30,7 +30,6 @@ public class PublishFeedbackSessionActionTest extends BaseActionTest<PublishFeed
     @Test
     @Override
     protected void testExecute() throws Exception {
-
         ______TS("Typical case");
 
         CourseAttributes course = typicalBundle.courses.get("typicalCourse1");
@@ -50,7 +49,7 @@ public class PublishFeedbackSessionActionTest extends BaseActionTest<PublishFeed
         assertEquals(feedbackSessionData.getPublishStatus(), FeedbackSessionPublishStatus.PUBLISHED);
 
         ______TS("Failure case: Session is already published");
-
+        // Attempt to publish the same session again.
         assertTrue(logic.getFeedbackSession(session.getFeedbackSessionName(), course.getId()).isPublished());
 
         result = getJsonResult(getAction(params));
@@ -58,6 +57,49 @@ public class PublishFeedbackSessionActionTest extends BaseActionTest<PublishFeed
 
         assertEquals(output.getMessage(), "Error publishing feedback session: Session has already been published.");
         assertEquals(result.getStatusCode(), HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void testExecute_invalidRequests_shouldFail() {
+        ______TS("non existent session name");
+
+        CourseAttributes course = typicalBundle.courses.get("typicalCourse1");
+        FeedbackSessionAttributes session = typicalBundle.feedbackSessions.get("session1InCourse1");
+        String randomSessionName = "randomName";
+        assertNotNull(course.getId(), session.getFeedbackSessionName());
+
+        String[] params = {
+                Const.ParamsNames.COURSE_ID, course.getId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, randomSessionName,
+        };
+
+        assertNull(logic.getFeedbackSession(randomSessionName, course.getId()));
+
+        PublishFeedbackSessionAction publishFeedbackSessionAction = getAction(params);
+        JsonResult result = getJsonResult(publishFeedbackSessionAction);
+        MessageOutput output = (MessageOutput) result.getOutput();
+
+        assertEquals(String.format("Trying to update a non-existent feedback session: %s/%s",
+                course.getId(), randomSessionName), output.getMessage());
+        assertEquals(result.getStatusCode(), HttpStatus.SC_INTERNAL_SERVER_ERROR);
+
+        ______TS("non existent course id");
+
+        String randomCourseId = "randomCourseId";
+
+        params = new String[] {
+                Const.ParamsNames.COURSE_ID, randomCourseId,
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, session.getFeedbackSessionName(),
+        };
+        assertNull(logic.getFeedbackSession(session.getFeedbackSessionName(), randomCourseId));
+
+        publishFeedbackSessionAction = getAction(params);
+        result = getJsonResult(publishFeedbackSessionAction);
+        output = (MessageOutput) result.getOutput();
+
+        assertEquals(String.format("Trying to update a non-existent feedback session: %s/%s",
+                randomCourseId, session.getFeedbackSessionName()), output.getMessage());
+        assertEquals(result.getStatusCode(), HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 
     @Test
