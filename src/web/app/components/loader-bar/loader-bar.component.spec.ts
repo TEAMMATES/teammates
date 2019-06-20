b072160bb9537e35050e840fe21cf0eb13e4cf43
erjillsison <erjillsison@gmail.com>
2019-06-20 15:07:03 +0800

    Update tests

--- src/web/app/components/loader-bar/loader-bar.component.spec.ts
+++ src/web/app/components/loader-bar/loader-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatProgressBarModule } from '@angular/material';
 
 import { LoaderBarComponent } from './loader-bar.component';
 
@@ -9,6 +10,7 @@ describe('LoaderBarComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LoaderBarComponent],
+      imports: [MatProgressBarModule],
     })
     .compileComponents();
   }));
