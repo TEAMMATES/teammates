ed0c32802a9545787dcdadd17342108038b98d21
crizyli <liyufei6666@gmail.com>
2019-06-26 14:04:30 +0800

    display related feedback responses in student all records page

--- src/web/app/pages-instructor/instructor-student-records-page/instructor-student-records-page.component.ts
+++ src/web/app/pages-instructor/instructor-student-records-page/instructor-student-records-page.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { HttpRequestService } from '../../../services/http-request.service';
 import { StatusMessageService } from '../../../services/status-message.service';
+import { QuestionOutput, ResponseOutput, SessionResults, Student } from '../../../types/api-output';
 import { ErrorMessageOutput } from '../../error-message-output';
+import { Intent } from '../../Intent';
 import { StudentProfile } from '../student-profile/student-profile';
 
 interface StudentRecords {
@@ -16,6 +18,7 @@ interface StudentRecords {
 
 interface Session {
   name: string;
+  questions: QuestionOutput[];
   isCollapsed: boolean;
 }
 
@@ -34,11 +37,12 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
   studentName: string = '';
   studentEmail: string = '';
   studentProfile?: StudentProfile ;
+  studentSection: string = '';
   sessions: Session[] = [];
   photoUrl: string = '';
 
   constructor(private route: ActivatedRoute, private httpRequestService: HttpRequestService,
-    private statusMessageService: StatusMessageService) { }
+              private statusMessageService: StatusMessageService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((queryParams: any) => {
@@ -57,18 +61,39 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
    */
   loadStudentRecords(courseid: string, studentemail: string): void {
     const paramMap: { [key: string]: string } = { courseid, studentemail };
+    this.httpRequestService.get('/student', paramMap).subscribe((resp: Student) => {
+      this.studentSection = resp.sectionName;
+    });
     this.httpRequestService.get('/students/records', paramMap).subscribe((resp: StudentRecords) => {
       this.courseId = resp.courseId;
       this.studentName = resp.studentName;
       this.studentEmail = resp.studentEmail;
       this.studentProfile = resp.studentProfile;
-      this.sessions = resp.sessionNames.map((sessionName: string) => ({ name: sessionName, isCollapsed: false }));
       if (!this.studentProfile) {
         this.statusMessageService.showWarningMessage(
                 'Normally, we would show the student\'s profile here. '
                 + 'However, either this student has not created a profile yet, '
                 + 'or you do not have access to view this student\'s profile.');
       }
+
+      resp.sessionNames.forEach((fsname: string) => {
+        const resultParamMap: { [key: string]: string } = {
+          courseid,
+          fsname,
+          frgroupbysection: this.studentSection,
+          intent: Intent.INSTRUCTOR_RESULT,
+        };
+        this.httpRequestService.get('/result', resultParamMap).subscribe((res: SessionResults) => {
+          res.questions.forEach((q: QuestionOutput) => {
+            q.allResponses = q.allResponses.filter((response: ResponseOutput) =>
+                response.giver === this.studentName || response.recipient === this.studentName);
+          });
+          const questions: QuestionOutput[] = res.questions.filter((q: QuestionOutput) => q.allResponses.length > 0);
+          this.sessions.push({ questions, name: fsname, isCollapsed: questions.length === 0 });
+        }, (res: ErrorMessageOutput) => {
+          this.statusMessageService.showErrorMessage(res.error.message);
+        });
+      });
     }, (resp: ErrorMessageOutput) => {
       this.statusMessageService.showErrorMessage(resp.error.message);
     });
