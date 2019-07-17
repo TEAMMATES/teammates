f1cf9c3ea36e1e1ffa444fb4812d80b797f36fa0
crizyli <liyufei6666@gmail.com>
2019-07-17 15:29:15 +0800

    remove old STUDENT_COURSES api

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -773,7 +773,6 @@ public final class Const {
         public static final String STUDENT_COURSE = "/student/course";
         public static final String STUDENT_PROFILE_PICTURE = "/student/profilePic";
         public static final String STUDENT_PROFILE = "/student/profile";
-        public static final String STUDENT_COURSES = "/student/courses";
         public static final String STUDENTS_CSV = "/students/csv";
         public static final String STUDENTS_AND_FEEDBACK_SESSION_DATA_SEARCH = "/studentsAndSessionData/search";
 
