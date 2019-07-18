5a54596ed5e23d7d8c6e620c771ee280cb8b0201
crizyli <liyufei6666@gmail.com>
2019-07-18 13:28:37 +0800

    sort feedback sessions

--- src/web/app/pages-instructor/instructor-student-records-page/instructor-student-records-page.component.ts
+++ src/web/app/pages-instructor/instructor-student-records-page/instructor-student-records-page.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../environments/environment';
-import { FeedbackSessionsService } from "../../../services/feedback-sessions.service";
+import { FeedbackSessionsService } from '../../../services/feedback-sessions.service';
 import { StatusMessageService } from '../../../services/status-message.service';
-import { StudentProfileService } from "../../../services/student-profile.service";
-import { StudentProfile, FeedbackSessions, FeedbackSession } from "../../../types/api-output";
+import { StudentProfileService } from '../../../services/student-profile.service';
+import { FeedbackSession, FeedbackSessions, StudentProfile } from '../../../types/api-output';
 import { ErrorMessageOutput } from '../../error-message-output';
 
 interface Session {
@@ -32,7 +32,7 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
   constructor(private route: ActivatedRoute,
               private statusMessageService: StatusMessageService,
               private studentProfileService: StudentProfileService,
-              private feedbackSessionsService: FeedbackSessionsService,) { }
+              private feedbackSessionsService: FeedbackSessionsService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((queryParams: any) => {
@@ -65,8 +65,13 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
     });
 
     this.feedbackSessionsService.getFeedbackSessionsForInstructor(courseid).subscribe((fss: FeedbackSessions) => {
-      this.sessions = fss.feedbackSessions.map((fs: FeedbackSession) => ({ name: fs.feedbackSessionName,
-        isCollapsed: false }));
+      this.sessions = fss.feedbackSessions.map((fs: FeedbackSession) => Object.assign({}, fs))
+          .sort((a: FeedbackSession, b: FeedbackSession) => (a.submissionEndTimestamp <
+              b.submissionEndTimestamp) ? 1 : (a.submissionEndTimestamp === b.submissionEndTimestamp) ?
+              ((a.submissionStartTimestamp < b.submissionStartTimestamp) ? 1 :
+                  (a.submissionStartTimestamp === b.submissionStartTimestamp) ? ((a.feedbackSessionName >
+                      b.feedbackSessionName) ? 1 : -1) : -1) : -1)
+          .map((fs: FeedbackSession) => ({ name: fs.feedbackSessionName, isCollapsed: false }));
     }, (resp: ErrorMessageOutput) => {
       this.statusMessageService.showErrorMessage(resp.error.message);
     });
