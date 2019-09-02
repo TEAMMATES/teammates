470278e16ed4a9da43e20a4bd341d5cbd16fb23a
nusjzx <nusjzx@gmail.com>
2019-09-02 12:40:35 +0800

    [#9795] prevent duplicate mcq options (#9852)
    * Prevent MCQ duplicate options
    
    * Prevent MSQ duplicate options
    
    * Add setter for MCQ and MSQ choices and prevent using deprecated methods

--- src/main/java/teammates/common/util/Const.java
+++ src/main/java/teammates/common/util/Const.java
@@ -268,6 +268,7 @@ public final class Const {
                 + " must be valid non-negative numbers with precision up to 2 decimal places.";
         public static final String MCQ_ERROR_EMPTY_MCQ_OPTION = "The Mcq options cannot be empty";
         public static final String MCQ_ERROR_OTHER_CONTENT_NOT_PROVIDED = "No text provided for other option";
+        public static final String MCQ_ERROR_DUPLICATE_MCQ_OPTION = "The Mcq options cannot be duplicate";
 
         // Msq
         public static final int MSQ_MIN_NUM_OF_CHOICES = 2;
@@ -298,6 +299,7 @@ public final class Const {
                 "The weights for the choices of a " + Const.FeedbackQuestionTypeNames.MSQ
                 + " must be valid numbers with precision up to 2 decimal places.";
         public static final String MSQ_ANSWER_NONE_OF_THE_ABOVE = "None of the above";
+        public static final String MSQ_ERROR_DUPLICATE_MSQ_OPTION = "The Msq options cannot be duplicate";
 
         // Numscale
         public static final String NUMSCALE_ERROR_MIN_MAX =
