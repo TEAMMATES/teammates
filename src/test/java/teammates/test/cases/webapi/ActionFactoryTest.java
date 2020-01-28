b808bd1a2594b321fbd4d48dee5a73da65b9a2d9
Joshua Wong <joshwonghc@gmail.com>
2020-01-28 14:53:31 +0800

    Add more tests for ActionFactory

--- src/test/java/teammates/test/cases/webapi/ActionFactoryTest.java
+++ src/test/java/teammates/test/cases/webapi/ActionFactoryTest.java
@@ -1,16 +1,13 @@
 package teammates.test.cases.webapi;
 
-import org.apache.http.HttpStatus;
-import org.testng.annotations.Test;
-
-import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
+import org.testng.annotations.Test;
 
-import teammates.test.cases.BaseTestCase;
+import teammates.common.exception.ActionMappingException;
 import teammates.common.util.Const;
 import teammates.test.driver.MockHttpServletRequest;
+import teammates.test.cases.BaseTestCase;
 import teammates.ui.webapi.action.Action;
 import teammates.ui.webapi.action.ActionFactory;
 import teammates.ui.webapi.action.GetAuthInfoAction;
@@ -25,7 +22,29 @@ public class ActionFactoryTest extends BaseTestCase {
 
         MockHttpServletRequest existingActionServletRequest = new MockHttpServletRequest(
             HttpGet.METHOD_NAME, Const.ResourceURIs.URI_PREFIX + Const.ResourceURIs.AUTH);
+        // Use this value for testing
+        existingActionServletRequest.addHeader("Backdoor-Key", "samplekey");
         Action existingAction = actionFactory.getAction(existingActionServletRequest, HttpGet.METHOD_NAME);
         assertTrue(existingAction instanceof GetAuthInfoAction);
+
+        ______TS("Action does not exist and ActionMappingException is thrown");
+
+        MockHttpServletRequest nonExistentActionServletRequest = new MockHttpServletRequest(
+            HttpGet.METHOD_NAME, Const.ResourceURIs.URI_PREFIX + "blahblahblah");
+        // Use this value for testing
+        nonExistentActionServletRequest.addHeader("Backdoor-Key", "samplekey");
+        assertThrows(ActionMappingException.class, () -> {
+            actionFactory.getAction(nonExistentActionServletRequest, HttpGet.METHOD_NAME);
+        });
+        
+        ______TS("Method does not on action exist and ActionMappingException is thrown");
+
+        MockHttpServletRequest nonExistentMethodOnActionServletRequest = new MockHttpServletRequest(
+            HttpGet.METHOD_NAME, Const.ResourceURIs.URI_PREFIX + Const.ResourceURIs.AUTH);
+        // Use this value for testing
+        nonExistentMethodOnActionServletRequest.addHeader("Backdoor-Key", "samplekey");
+        assertThrows(ActionMappingException.class, () -> {
+            actionFactory.getAction(nonExistentMethodOnActionServletRequest, HttpPost.METHOD_NAME);
+        });
     }
 }
