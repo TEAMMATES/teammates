a8207a162797f9363a8a731102cdd9a84109d260
Tiu Wee Han <deantiu56@gmail.com>
2020-02-13 21:51:04 +0800

    Add Instructor service tests

--- src/web/services/instructor.service.spec.ts
+++ src/web/services/instructor.service.spec.ts
@@ -1,22 +1,81 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { Instructor, InstructorPermissionRole, Instructors, JoinState } from '../types/api-output';
+import { HttpRequestService } from './http-request.service';
 import { InstructorService } from './instructor.service';
+import DoneCallback = jest.DoneCallback;
 
 describe('InstructorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule,
-    ],
-  }));
+  let spyHttpRequestService: any;
+  let service: InstructorService;
 
-  it('should be created', () => {
-    const service: InstructorService = TestBed.get(InstructorService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    spyHttpRequestService = {
+      get: jest.fn(),
+      post: jest.fn(),
+      put: jest.fn(),
+      delete: jest.fn(),
+    };
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: HttpRequestService, useValue: spyHttpRequestService },
+      ],
+    });
+    service = TestBed.get(InstructorService);
   });
 
   it('should be created', () => {
-    const service: InstructorService = TestBed.get(InstructorService);
-    expect(service.getInstructorsFromCourse('')).toBeTruthy();
+    expect(service).toBeTruthy();
+  });
+
+  it('should execute GET', () => {
+    service.getInstructorsFromCourse('CS3281');
+    const paramMap: { [key: string]: string } = {
+      courseid: 'CS3281',
+    };
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith('/instructors', paramMap);
+  });
+
+  it('should send the correct course id', (done: DoneCallback) => {
+    const mockInstructors: Instructors = {
+      instructors: [{
+        googleId: '',
+        courseId: 'CS3281',
+        email: '',
+        isDisplayedToStudents: true,
+        displayedToStudentsAs: '',
+        name: '',
+        role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_COOWNER,
+        joinState: JoinState.JOINED,
+      }, {
+        googleId: '',
+        courseId: 'CS3282',
+        email: '',
+        isDisplayedToStudents: true,
+        displayedToStudentsAs: '',
+        name: '',
+        role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_COOWNER,
+        joinState: JoinState.JOINED,
+      }],
+    };
+
+    spyHttpRequestService.get.mockImplementation((endpoint: string, paramMap: { [key: string]: string }) => {
+      expect(endpoint).toEqual('/instructors');
+      const courseid: string = paramMap.courseid;
+      return of<Instructors>({
+        instructors: mockInstructors.instructors.filter((instructor: Instructor) => instructor.courseId === courseid),
+      });
+    });
+
+    service.getInstructorsFromCourse('CS3281').subscribe((instructors: Instructors) => {
+      expect(instructors.instructors).toEqual(mockInstructors.instructors
+        .filter((instructor: Instructor) => instructor.courseId === 'CS3281'));
+      done();
+    });
   });
 });
