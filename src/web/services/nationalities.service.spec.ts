10c06b440c2efcdf1f0c3c8dd00e5d116d4fe701
Tiu Wee Han <deantiu56@gmail.com>
2020-02-13 08:28:26 +0800

    Update tests for NationalitiesService

--- src/web/services/nationalities.service.spec.ts
+++ src/web/services/nationalities.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NationalitiesService } from './nationalities.service';
 
 describe('NationalitiesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+    ],
+  }));
 
   it('should be created', () => {
     const service: NationalitiesService = TestBed.get(NationalitiesService);
