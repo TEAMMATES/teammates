bde95183c238ccd074420a928c90543a5bb49362
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-02-15 20:50:12 +0800

    Add MasqueradeModeService tests

--- src/web/services/masquerade-mode.service.spec.ts
+++ src/web/services/masquerade-mode.service.spec.ts
@@ -1,17 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { MasqueradeModeService } from './masquerade-mode.service';
 
 describe('MasqueradeModeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      RouterTestingModule,
-    ],
-  }));
+  let service: MasqueradeModeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        MasqueradeModeService,
+      ],
+    });
+    service = TestBed.get(MasqueradeModeService);
+  });
 
   it('should be created', () => {
-    const service: MasqueradeModeService = TestBed.get(MasqueradeModeService);
     expect(service).toBeTruthy();
+  });
+
+  it('getMasqueradeUser should be defined', () => {
+    expect(service.getMasqueradeUser()).toBeDefined();
+  });
+
+  it('setMasqueradeUser should set user', () => {
+    const testUser: string = 'test';
+    service.setMasqueradeUser(testUser);
+    expect(service.getMasqueradeUser()).toEqual(testUser);
+  });
+
+  it('isInMasqueradingMode should be false when no user', () => {
+    expect(service.isInMasqueradingMode()).toBe(false);
+  });
+
+  it('isInMasqueradingMode should be true when user set', () => {
+    const testUser: string = 'test';
+    service.setMasqueradeUser(testUser);
+    expect(service.isInMasqueradingMode()).toBe(true);
   });
 });
