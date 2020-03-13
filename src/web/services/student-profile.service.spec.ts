b32d3977654da75ae6a6de2737d022b4cc743f3a
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-03-13 12:40:43 +0800

    Draft Implementation

--- src/web/services/student-profile.service.spec.ts
+++ src/web/services/student-profile.service.spec.ts
@@ -2,16 +2,45 @@ import { TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { StudentProfileService } from './student-profile.service';
+import { HttpRequestService } from "./http-request.service";
+import { ResourceEndpoints } from "../types/api-endpoints";
 
 describe('StudentProfileService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule,
-    ],
-  }));
+  let spyHttpRequestService: any;
+  let service: StudentProfileService;
+
+  beforeEach(() => {
+    spyHttpRequestService = {
+      get: jest.fn(),
+      post: jest.fn(),
+      put: jest.fn(),
+      delete: jest.fn(),
+    };
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: HttpRequestService, useValue: spyHttpRequestService },
+      ],
+    });
+    service = TestBed.get(StudentProfileService);
+  });
 
   it('should be created', () => {
-    const service: StudentProfileService = TestBed.get(StudentProfileService);
     expect(service).toBeTruthy();
+  });
+
+  it('should execute GET on student profile picture endpoint', () => {
+    service.getProfilePicture();
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE_PICTURE, {}, 'blob');
+  });
+
+  it('should execute POST on student profile picture endpoint', () => {
+    // TODO
+    spyOn(FormData.prototype, "append");
+    const formData: {} = {};
+    service.postProfilePicture(formData);
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE_PICTURE, {}, formData);
   });
 });
