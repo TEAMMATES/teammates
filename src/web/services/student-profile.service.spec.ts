3247e19b441ae192b8986d65ba3df4c6e17f9b9a
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-03-15 12:42:01 +0800

    Add all test cases

--- src/web/services/student-profile.service.spec.ts
+++ src/web/services/student-profile.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ResourceEndpoints } from '../types/api-endpoints';
+import { StudentProfileUpdateRequest } from '../types/api-request';
+import { HttpRequestService } from './http-request.service';
 import { StudentProfileService } from './student-profile.service';
-import { HttpRequestService } from "./http-request.service";
-import { ResourceEndpoints } from "../types/api-endpoints";
 
 describe('StudentProfileService', () => {
   let spyHttpRequestService: any;
@@ -31,16 +32,54 @@ describe('StudentProfileService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should execute GET on student profile endpoint with params', () => {
+    const studentEmail: string = 'test@123.com';
+    const courseId: string = 'test-course';
+    const paramsMap: { [key: string]: string } = {
+      studentemail: studentEmail,
+      courseid: courseId,
+    };
+    service.getStudentProfile(studentEmail, courseId);
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE, paramsMap);
+  });
+
+  it('should execute GET on student profile endpoint without params', () => {
+    service.getStudentProfile();
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE);
+  });
+
   it('should execute GET on student profile picture endpoint', () => {
     service.getProfilePicture();
     expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE_PICTURE, {}, 'blob');
   });
 
+  it('should execute PUT on student profile endpoint', () => {
+    const googleId: string = '';
+    const requestBody: StudentProfileUpdateRequest = new class implements StudentProfileUpdateRequest {
+      email: string = '';
+      existingNationality: string = '';
+      gender: string = '';
+      institute: string = '';
+      moreInfo: string = '';
+      nationality: string = '';
+      shortName: string = '';
+    };
+    const paramsMap: { [key: string]: string } = {
+      googleid: googleId,
+    };
+    service.updateStudentProfile(googleId, requestBody);
+    expect(spyHttpRequestService.put).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE, paramsMap, requestBody);
+  });
+
   it('should execute POST on student profile picture endpoint', () => {
-    // TODO
-    spyOn(FormData.prototype, "append");
-    const formData: {} = {};
+    const formData: FormData = new FormData();
     service.postProfilePicture(formData);
-    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE_PICTURE, {}, formData);
+    expect(spyHttpRequestService.post).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE_PICTURE, {}, formData);
+  });
+
+  it('should execute DELETE on student profile picture endpoint', () => {
+    const paramsMap: { [key: string]: string } = {};
+    service.deleteProfilePicture(paramsMap);
+    expect(spyHttpRequestService.delete).toHaveBeenCalledWith(ResourceEndpoints.STUDENT_PROFILE_PICTURE, paramsMap);
   });
 });
