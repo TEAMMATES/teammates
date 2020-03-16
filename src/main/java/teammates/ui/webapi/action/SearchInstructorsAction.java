fe12a03d3a01dc976ab3147a88d2c4ae136b4545
Joshua Wong <joshwonghc@gmail.com>
2020-03-17 02:30:29 +0800

    Add institute and key to InstructorData

--- src/main/java/teammates/ui/webapi/action/SearchInstructorsAction.java
+++ src/main/java/teammates/ui/webapi/action/SearchInstructorsAction.java
@@ -2,6 +2,7 @@ package teammates.ui.webapi.action;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import teammates.common.datatransfer.attributes.AccountAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
