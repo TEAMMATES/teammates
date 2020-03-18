5f2d07e5a292c504440bba1b583758d865c6811b
Daekoon <daekoon_lim@hotmail.com>
2020-03-19 04:21:52 +0800

    Fix lint

--- src/main/java/teammates/common/datatransfer/attributes/EntityAttributes.java
+++ src/main/java/teammates/common/datatransfer/attributes/EntityAttributes.java
@@ -56,6 +56,21 @@ public abstract class EntityAttributes<E extends BaseEntity> {
     }
 
     /**
+     * Custom equals method that is able to handle null objects.
+     * @param first first object to be compared
+     * @param second second object to be compared
+     * @return true of both objects are null or equal, false otherwise.
+     */
+    protected boolean nullableDataEquals(Object first, Object second) {
+        if (first == null && second == null) {
+            return true;
+        } else if (first == null || second == null) {
+            return false;
+        }
+        return first.equals(second);
+    }
+
+    /**
      * Helper class to determine whether a field should be updated or not.
      *
      * <p>The class behaves like {@link java.util.Optional} but allows null value.
@@ -117,20 +132,5 @@ public abstract class EntityAttributes<E extends BaseEntity> {
                     : "UpdateOption.empty";
         }
 
-    }
-
-    /**
-     * Custom equals method that is able to handle null objects.
-     * @param first first object to be compared
-     * @param second second object to be compared
-     * @return true of both objects are null or equal, false otherwise.
-     */
-    protected boolean nullableDataEquals(Object first, Object second) {
-        if (first == null && second == null) {
-            return true;
-        } else if (first == null || second == null) {
-            return false;
-        }
-        return first.equals(second);
     }
 }
