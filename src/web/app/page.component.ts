61c606f8ad17999994b7625a8f746f1a162b29d2
Diana Ramos <1161209@isep.ipp.pt>
2020-05-06 14:12:06 +0100

    [#9836] Fixed bug where clicking on link of navbar doesn't close modal.

--- src/web/app/page.component.ts
+++ src/web/app/page.component.ts
@@ -118,6 +118,15 @@ export class PageComponent {
       this.isCollapsed = !this.isCollapsed;
     }
   }
+  
+  /**
+   * Method that checks if current page has active modals and close them.
+   */
+  closeModal(): void{
+    if (this.modalService.hasOpenModals()) {
+      this.modalService.dismissAll();
+    }
+  }
 }
 
 /**
