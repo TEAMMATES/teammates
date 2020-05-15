674cf3030e23e1e90225b6388460da4b6c239780
Chee Yuan <chong-1997@hotmail.com>
2020-05-15 16:16:57 +0800

    Remove unused import

--- src/test/java/teammates/test/cases/webapi/ConfirmFeedbackSessionSubmissionActionTest.java
+++ src/test/java/teammates/test/cases/webapi/ConfirmFeedbackSessionSubmissionActionTest.java
@@ -11,7 +11,6 @@ import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
-import teammates.common.exception.EntityNotFoundException;
 import teammates.common.exception.InvalidHttpParameterException;
 import teammates.common.util.Const;
 import teammates.common.util.EmailWrapper;
