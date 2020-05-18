bca68bf4740a606984663dbf5d6fad4189bfe2f6
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-05-18 21:49:11 +0800

    Add test dependencies

--- src/web/app/pages-help/instructor-help-page/instructor-help-questions-section/instructor-help-questions-section.component.spec.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-questions-section/instructor-help-questions-section.component.spec.ts
@@ -2,16 +2,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
+import { HttpRequestService } from '../../../../services/http-request.service';
+import { QuestionEditFormModule } from '../../../components/question-edit-form/question-edit-form.module';
+import { ExampleBoxComponent } from '../example-box/example-box.component';
 import { InstructorHelpQuestionsSectionComponent } from './instructor-help-questions-section.component';
 
 describe('InstructorHelpQuestionsSectionComponent', () => {
   let component: InstructorHelpQuestionsSectionComponent;
   let fixture: ComponentFixture<InstructorHelpQuestionsSectionComponent>;
+  let spyHttpRequestService: any;
 
   beforeEach(async(() => {
+    spyHttpRequestService = {
+      get: jest.fn(),
+      post: jest.fn(),
+      put: jest.fn(),
+      delete: jest.fn(),
+    };
     TestBed.configureTestingModule({
-      declarations: [InstructorHelpQuestionsSectionComponent],
-      imports: [NgbModule, RouterTestingModule, NgxPageScrollCoreModule],
+      declarations: [InstructorHelpQuestionsSectionComponent, ExampleBoxComponent],
+      imports: [NgbModule, RouterTestingModule, NgxPageScrollCoreModule, QuestionEditFormModule],
+      providers: [
+        { provide: HttpRequestService, useValue: spyHttpRequestService },
+      ],
     })
     .compileComponents();
   }));
