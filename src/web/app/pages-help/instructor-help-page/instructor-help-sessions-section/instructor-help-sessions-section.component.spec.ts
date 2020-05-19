0dff9922bf99e4aef5739388158d4fedacfe5a25
t-cheepeng <tcp97@hotmail.com>
2020-05-19 10:52:22 +0800

    Change import location of MatSnackBarModule

--- src/web/app/pages-help/instructor-help-page/instructor-help-sessions-section/instructor-help-sessions-section.component.spec.ts
+++ src/web/app/pages-help/instructor-help-page/instructor-help-sessions-section/instructor-help-sessions-section.component.spec.ts
@@ -1,26 +1,13 @@
-import { Component, Input } from "@angular/core";
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 
-import { TemplateSession } from '../../../../services/feedback-sessions.service';
-import { Course } from '../../../../types/api-output';
-import {
-  SessionEditFormMode,
-  SessionEditFormModel,
-} from '../../../components/session-edit-form/session-edit-form-model';
+import { SessionEditFormModule } from '../../../components/session-edit-form/session-edit-form.module';
+import { ExampleBoxComponent } from '../example-box/example-box.component';
 import { InstructorHelpSessionsSectionComponent } from './instructor-help-sessions-section.component';
-import { ExampleBoxComponent } from "../example-box/example-box.component";
-
-@Component({ selector: 'tm-session-edit-form', template: '' })
-class SessionEditFormStubComponent {
-  @Input() formMode: SessionEditFormMode = SessionEditFormMode.ADD;
-  @Input() model: SessionEditFormModel = {} as SessionEditFormModel;
-  @Input() courseCandidates: Course[] = [];
-  @Input() templateSessions: TemplateSession[] = [];
-}
 
 describe('InstructorHelpSessionsSectionComponent', () => {
   let component: InstructorHelpSessionsSectionComponent;
@@ -28,8 +15,8 @@ describe('InstructorHelpSessionsSectionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [InstructorHelpSessionsSectionComponent, ExampleBoxComponent, SessionEditFormStubComponent],
-      imports: [NgbModule, RouterTestingModule, NgxPageScrollCoreModule],
+      declarations: [InstructorHelpSessionsSectionComponent, ExampleBoxComponent],
+      imports: [NgbModule, RouterTestingModule, NgxPageScrollCoreModule, SessionEditFormModule, MatSnackBarModule],
     })
     .compileComponents();
   }));
