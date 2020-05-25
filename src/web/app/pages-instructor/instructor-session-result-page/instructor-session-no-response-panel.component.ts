97f7492e62958d2091c7a5bbfef8cabc5923499b
Derek-Hardy <derekzxj@outlook.com>
2020-05-25 19:37:46 +0800

    added remind-all button and fixed some typo and UI

--- src/web/app/pages-instructor/instructor-session-result-page/instructor-session-no-response-panel.component.ts
+++ src/web/app/pages-instructor/instructor-session-result-page/instructor-session-no-response-panel.component.ts
@@ -1,11 +1,26 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { forkJoin } from 'rxjs';
+import { FeedbackSessionsService } from '../../../services/feedback-sessions.service';
+import { StatusMessageService } from '../../../services/status-message.service';
+import { StudentService } from '../../../services/student.service';
 import {
   FeedbackSession, FeedbackSessionPublishStatus,
   FeedbackSessionSubmissionStatus,
+  FeedbackSessionSubmittedGiverSet,
   ResponseVisibleSetting,
   SessionVisibleSetting,
-  Student,
+  Student, Students,
 } from '../../../types/api-output';
+import {
+  SendRemindersToStudentModalComponent,
+} from '../../components/sessions-table/send-reminders-to-student-modal/send-reminders-to-student-modal.component';
+import {
+  StudentListInfoTableRowModel,
+} from '../../components/sessions-table/student-list-info-table/student-list-info-table-model';
+import {
+    ErrorMessageOutput
+} from '../../error-message-output';
 
 /**
  * Instructor sessions results page No Response Panel.
@@ -38,7 +53,12 @@ export class InstructorSessionNoResponsePanelComponent implements OnInit, OnChan
   isTabExpanded: boolean = false;
 
   noResponseStudentsInSection: Student[] = [];
-  constructor() { }
+
+  constructor(
+    private modalService: NgbModal,
+    private feedbackSessionsService: FeedbackSessionsService,
+    private statusMessageService: StatusMessageService,
+    private studentService: StudentService) { }
 
   ngOnInit(): void {
     this.filterStudentsBySection();
@@ -55,6 +75,49 @@ export class InstructorSessionNoResponsePanelComponent implements OnInit, OnChan
     } else {
       this.noResponseStudentsInSection = this.noResponseStudents;
     }
+  }
+
+  openSendReminderModal(event: any) {
+    event.stopPropagation();
+
+    const courseId: string = this.session.courseId;
+    const feedbackSessionName: string = this.session.feedbackSessionName;
+
+    const modalRef: NgbModalRef = this.modalService.open(SendRemindersToStudentModalComponent);
+    modalRef.componentInstance.courseId = courseId;
+    modalRef.componentInstance.feedbackSessionName = feedbackSessionName;
+
+    forkJoin(
+        this.studentService.getStudentsFromCourse({ courseId }),
+        this.feedbackSessionsService.getFeedbackSessionSubmittedGiverSet({ courseId, feedbackSessionName }))
+        .subscribe(
+            (result: any[]) => {
+              const students: Student[] = (result[0] as Students).students;
+              const giverSet: Set<string> = new Set((result[1] as FeedbackSessionSubmittedGiverSet).giverIdentifiers);
+              modalRef.componentInstance.studentListInfoTableRowModels
+                = students.map((student: Student) => ({
+                    email: student.email,
+                    name: student.name,
+                    teamName: student.teamName,
+                    sectionName: student.sectionName,
+
+                    hasSubmittedSession: giverSet.has(student.email),
+                    isSelected: true,
+                  } as StudentListInfoTableRowModel));
+
+                  modalRef.result.then((studentsToRemind: StudentListInfoTableRowModel[]) => {
+                    this.feedbackSessionsService.remindFeedbackSessionSubmissionForStudent(courseId, feedbackSessionName, {
+                        usersToRemind: studentsToRemind.map((m: StudentListInfoTableRowModel) => m.email),
+                        }).subscribe(() => {
+                          this.statusMessageService.showSuccessMessage(
+                            'Reminder e-mails have been sent out to those students and instructors. '
+                             + 'Please allow up to 1 hour for all the notification emails to be sent out.');
+
+                        }, (resp: ErrorMessageOutput) => { this.statusMessageService.showErrorMessage(resp.error.message); });
+                    }, () => {});
+
+              }, (resp: ErrorMessageOutput) => { this.statusMessageService.showErrorMessage(resp.error.message);}
+        );
   }
 
   /**
