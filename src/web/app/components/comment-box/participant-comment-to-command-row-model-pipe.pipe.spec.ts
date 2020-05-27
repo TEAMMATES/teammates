520984830897fd617dd92d60dcf3fdc1f4e1b748
Chee Yuan <chong-1997@hotmail.com>
2020-05-27 16:31:49 +0800

    Convert to use pipes

---
+++ src/web/app/components/comment-box/participant-comment-to-command-row-model-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ParticipantCommentToCommandRowModelPipePipe } from './participant-comment-to-command-row-model-pipe.pipe';
+
+describe('ParticipantCommentToCommandRowModelPipePipe', () => {
+  it('create an instance', () => {
+    const pipe: ParticipantCommentToCommandRowModelPipePipe = new ParticipantCommentToCommandRowModelPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
