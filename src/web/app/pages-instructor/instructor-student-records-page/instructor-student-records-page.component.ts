9876f6864d992b7e8fb02f8cd207d5fccb1dbe79
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-05-28 16:26:13 +0800

    Initial implementation from #9798

--- src/web/app/pages-instructor/instructor-student-records-page/instructor-student-records-page.component.ts
+++ src/web/app/pages-instructor/instructor-student-records-page/instructor-student-records-page.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { FeedbackSessionsService } from '../../../services/feedback-sessions.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { StudentProfileService } from '../../../services/student-profile.service';
-import { Gender, StudentProfile } from '../../../types/api-output';
+import { StudentService } from '../../../services/student.service';
+import {
+  FeedbackSession,
+  FeedbackSessions,
+  Gender,
+  QuestionOutput, ResponseOutput,
+  SessionResults, Student,
+  StudentProfile,
+} from '../../../types/api-output';
+import { Intent } from '../../../types/api-request';
 import { ErrorMessageOutput } from '../../error-message-output';
 
 interface Session {
   name: string;
   isCollapsed: boolean;
+  questions: QuestionOutput[];
 }
 
 /**
@@ -23,6 +34,7 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
 
   courseId: string = '';
   studentEmail: string = '';
+  studentSection: string = '';
 
   studentProfile: StudentProfile = {
     name: '',
@@ -38,7 +50,9 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private statusMessageService: StatusMessageService,
-              private studentProfileService: StudentProfileService) { }
+              private studentProfileService: StudentProfileService,
+              private feedbackSessionsService: FeedbackSessionsService,
+              private studentService: StudentService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((queryParams: any) => {
@@ -46,6 +60,7 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
       this.studentEmail = queryParams.studentemail;
 
       this.loadStudentRecords();
+      this.loadStudentResults();
       this.photoUrl
           = `${environment.backendUrl}/webapi/student/profilePic?`
             + `courseid=${this.courseId}&studentemail=${this.studentEmail}`;
@@ -56,10 +71,42 @@ export class InstructorStudentRecordsPageComponent implements OnInit {
    * Loads the student's records based on the given course ID and email.
    */
   loadStudentRecords(): void {
+    this.studentService.getStudent(this.courseId, this.studentEmail).subscribe((resp: Student) => {
+      this.studentSection = resp.sectionName;
+    });
     this.studentProfileService.getStudentProfile(this.studentEmail, this.courseId).subscribe((resp: StudentProfile) => {
       this.studentProfile = resp;
     }, (resp: ErrorMessageOutput) => {
       this.statusMessageService.showErrorMessage(resp.error.message);
+    });
+  }
+
+  /**
+   * Loads the student's feedback session results based on the given course ID and student name.
+   */
+  loadStudentResults(): void {
+    this.feedbackSessionsService.getFeedbackSessionsForInstructor(this.courseId).subscribe((resp: FeedbackSessions) => {
+      resp.feedbackSessions.forEach((feedbackSession: FeedbackSession) => {
+        this.feedbackSessionsService.getFeedbackSessionsResult({
+          courseId: this.courseId,
+          feedbackSessionName: feedbackSession.feedbackSessionName,
+          groupBySection: this.studentSection,
+          intent: Intent.INSTRUCTOR_RESULT,
+        }).subscribe((res: SessionResults) => {
+          res.questions.forEach((q: QuestionOutput) => {
+            q.allResponses = q.allResponses.filter((response: ResponseOutput) =>
+                response.giverEmail === this.studentProfile.email ||
+                response.recipientEmail === this.studentProfile.email);
+          });
+          this.sessions.push({
+            name: feedbackSession.feedbackSessionName,
+            isCollapsed: true,
+            questions: res.questions.filter((q: QuestionOutput) => q.allResponses.length > 0),
+          });
+        });
+      });
+    }, (errorMessageOutput: ErrorMessageOutput) => {
+      this.statusMessageService.showErrorMessage(errorMessageOutput.error.message);
     });
   }
 }
