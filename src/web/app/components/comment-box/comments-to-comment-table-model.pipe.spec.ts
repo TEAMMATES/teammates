6b63e13772c64c7640b116c5a3707ce53dbab61c
Chee Yuan <chong-1997@hotmail.com>
2020-05-29 16:47:08 +0800

    Add test

--- src/web/app/components/comment-box/comments-to-comment-table-model.pipe.spec.ts
+++ src/web/app/components/comment-box/comments-to-comment-table-model.pipe.spec.ts
@@ -1,35 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {
-  CommentVisibilityType,
-  FeedbackQuestionType,
-  ResponseOutput,
-} from '../../../types/api-output';
+import { CommentOutput } from '../../../types/api-output';
 import { CommentToCommentRowModelPipe } from './comment-to-comment-row-model.pipe';
 import { CommentsToCommentTableModelPipe } from './comments-to-comment-table-model.pipe';
 
 describe('CommentsToCommentTableModelPipe', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CommentsToCommentTableModelPipe],
-      providers: [CommentToCommentRowModelPipe],
-    });
-  });
-  it('converts response output to comment table model correctly', () => {
-    const pipe: ComponentFixture<CommentsToCommentTableModelPipe> = TestBed.createComponent(
-        CommentsToCommentTableModelPipe);
-    const response: ResponseOutput = {
-      responseId: 'responseId',
-      giver: 'giver',
-      relatedGiverEmail: 'relatedGiverEmail',
-      giverTeam: 'giverTeam',
-      giverEmail: 'giverEmail',
-      giverSection: 'giverSection',
-      recipient: 'recipient',
-      recipientTeam: 'recipientTeam',
-      recipientEmail: 'recipientEmail',
-      recipientSection: 'recipientSection',
-      responseDetails: { questionType: FeedbackQuestionType.MCQ },
-      participantComment: {
+  it('converts comments to comment table model correctly', () => {
+    const pipe: CommentsToCommentTableModelPipe = new CommentsToCommentTableModelPipe(
+        new CommentToCommentRowModelPipe());
+    const comments: CommentOutput[] = [
+      {
+        commentGiverName: 'commentGiverName',
+        lastEditorName: 'lastEditorName',
         commentGiver: 'commentGiver',
         lastEditorEmail: 'lastEditorEmail',
         feedbackResponseCommentId: 0,
@@ -39,21 +19,83 @@ describe('CommentsToCommentTableModelPipe', () => {
         isVisibilityFollowingFeedbackQuestion: false,
         showGiverNameTo: [],
         showCommentTo: [],
+      },
+      {
+        commentGiverName: 'commentGiverName2',
+        lastEditorName: 'lastEditorName2',
+        commentGiver: 'commentGiver2',
+        lastEditorEmail: 'lastEditorEmail2',
+        feedbackResponseCommentId: 1,
+        commentText: 'commentText2',
+        createdAt: 1,
+        lastEditedAt: 1,
+        isVisibilityFollowingFeedbackQuestion: true,
+        showGiverNameTo: [],
+        showCommentTo: [],
+      },
+    ];
+    expect(pipe.transform(comments, true, 'UTC')).toEqual({
+      commentRows: [{
+        timezone: 'UTC',
+        originalComment: {
+          commentGiverName: 'commentGiverName',
+          lastEditorName: 'lastEditorName',
+          commentGiver: 'commentGiver',
+          lastEditorEmail: 'lastEditorEmail',
+          feedbackResponseCommentId: 0,
+          commentText: 'commentText',
+          createdAt: 0,
+          lastEditedAt: 0,
+          isVisibilityFollowingFeedbackQuestion: false,
+          showGiverNameTo: [],
+          showCommentTo: [],
+        },
         commentGiverName: 'commentGiverName',
         lastEditorName: 'lastEditorName',
+        commentEditFormModel: {
+          commentText: 'commentText',
+          isUsingCustomVisibilities: false,
+          showCommentTo: [],
+          showGiverNameTo: [],
+        },
+        isEditing: false,
       },
-      instructorComments: [{
-        commentGiver: 'commentGiver';
-        lastEditorEmail: 'lastEditorEmail';
-        feedbackResponseCommentId: 0;
-        commentText: 'commentText';
-        createdAt: 0;
-        lastEditedAt: 0;
-        isVisibilityFollowingFeedbackQuestion: boolean;
-        showGiverNameTo: CommentVisibilityType[];
-        showCommentTo: CommentVisibilityType[];
+      {
+        timezone: 'UTC',
+        originalComment: {
+          commentGiverName: 'commentGiverName2',
+          lastEditorName: 'lastEditorName2',
+          commentGiver: 'commentGiver2',
+          lastEditorEmail: 'lastEditorEmail2',
+          feedbackResponseCommentId: 1,
+          commentText: 'commentText2',
+          createdAt: 1,
+          lastEditedAt: 1,
+          isVisibilityFollowingFeedbackQuestion: true,
+          showGiverNameTo: [],
+          showCommentTo: [],
+        },
+        commentGiverName: 'commentGiverName2',
+        lastEditorName: 'lastEditorName2',
+        commentEditFormModel: {
+          commentText: 'commentText2',
+          isUsingCustomVisibilities: false,
+          showCommentTo: [],
+          showGiverNameTo: [],
+        },
+        isEditing: false,
       }],
-    };
-    expect(pipe).toBeTruthy();
+      newCommentRow: {
+        commentEditFormModel: {
+          commentText: '',
+          isUsingCustomVisibilities: false,
+          showCommentTo: [],
+          showGiverNameTo: [],
+        },
+        isEditing: false,
+      },
+      isAddingNewComment: false,
+      isReadonly: true,
+    });
   });
 });
