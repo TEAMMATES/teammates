0da91d6f79a3f5c465a9c54c5f1a46ca72ec3c14
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-05-29 22:11:04 +0800

    Add constsum recipient question statistics

--- src/web/app/components/question-types/question-statistics/constsum-recipients-question-statistics.component.ts
+++ src/web/app/components/question-types/question-statistics/constsum-recipients-question-statistics.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { FeedbackConstantSumQuestionDetails, FeedbackConstantSumResponseDetails } from '../../../../types/api-output';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import {
+  FeedbackConstantSumQuestionDetails,
+  FeedbackConstantSumResponseDetails,
+  FeedbackParticipantType,
+} from '../../../../types/api-output';
 import { DEFAULT_CONSTSUM_OPTIONS_QUESTION_DETAILS } from '../../../../types/default-question-structs';
 import { QuestionStatistics } from './question-statistics';
 
@@ -12,10 +16,57 @@ import { QuestionStatistics } from './question-statistics';
   styleUrls: ['./constsum-recipients-question-statistics.component.scss'],
 })
 export class ConstsumRecipientsQuestionStatisticsComponent
-    extends QuestionStatistics<FeedbackConstantSumQuestionDetails, FeedbackConstantSumResponseDetails> {
+    extends QuestionStatistics<FeedbackConstantSumQuestionDetails, FeedbackConstantSumResponseDetails>
+    implements OnInit, OnChanges {
+
+  emailToTeamName: Record<string, string> = {};
+  emailToName: Record<string, string> = {};
+  pointsPerOption: Record<string, number[]> = {};
+  totalPointsPerOption: Record<string, number> = {};
+  averagePointsPerOption: Record<string, number> = {};
 
   constructor() {
     super(DEFAULT_CONSTSUM_OPTIONS_QUESTION_DETAILS());
+  }
+
+  ngOnInit(): void {
+    this.calculateStatistics();
+  }
+
+  ngOnChanges(): void {
+    this.calculateStatistics();
+  }
+
+  private calculateStatistics(): void {
+    this.emailToTeamName = {};
+    this.emailToName = {};
+    this.pointsPerOption = {};
+    this.totalPointsPerOption = {};
+    this.averagePointsPerOption = {};
+
+    const isRecipientTeam: boolean = this.recipientType === FeedbackParticipantType.TEAMS
+        || this.recipientType === FeedbackParticipantType.TEAMS_EXCLUDING_SELF;
+
+    for (const response of this.responses) {
+      const identifier: string = isRecipientTeam ? response.recipient : response.recipientEmail;
+
+      this.pointsPerOption[identifier] = this.pointsPerOption[identifier] || [];
+      this.pointsPerOption[identifier].push(response.responseDetails.answers[0]);
+
+      if (!this.emailToTeamName[identifier]) {
+        this.emailToTeamName[identifier] = isRecipientTeam ? '' : response.recipientTeam;
+      }
+      if (!this.emailToName[identifier]) {
+        this.emailToName[identifier] = response.recipient;
+      }
+    }
+
+    for (const recipient of Object.keys(this.pointsPerOption)) {
+      const answers: number[] = this.pointsPerOption[recipient];
+      const sum: number = answers.reduce((a: number, b: number) => a + b, 0);
+      this.totalPointsPerOption[recipient] = sum;
+      this.averagePointsPerOption[recipient] = +(answers.length === 0 ? 0 : sum / answers.length).toFixed(2);
+    }
   }
 
 }
