b8e023b0beaca818681b60dcc0a4089d81c758bb
t-cheepeng <tcp97@hotmail.com>
2020-06-09 13:49:59 +0800

    Switch to use guard clause

--- src/web/app/components/question-responses/gqr-rqg-view-responses/gqr-rqg-view-responses.component.ts
+++ src/web/app/components/question-responses/gqr-rqg-view-responses/gqr-rqg-view-responses.component.ts
@@ -95,36 +95,38 @@ export class GqrRqgViewResponsesComponent extends InstructorResponsesViewBase im
 
         const shouldDisplayBasedOnSection: boolean = this.feedbackResponsesService
             .isFeedbackResponsesDisplayedOnSection(response, this.section, this.sectionType);
-        if (shouldDisplayBasedOnSection) {
-          if (this.isGqr) {
-            this.teamsToUsers[response.giverTeam] = this.teamsToUsers[response.giverTeam] || [];
-            if (this.teamsToUsers[response.giverTeam].indexOf(response.giver) === -1) {
-              this.teamsToUsers[response.giverTeam].push(response.giver);
-              this.teamExpanded[response.giverTeam] = this.isExpandAll;
-            }
-            if (response.relatedGiverEmail) {
-              this.userToRelatedEmail[response.giver] = response.relatedGiverEmail;
-            }
+        if (!shouldDisplayBasedOnSection) {
+          continue;
+        }
 
-            this.userExpanded[response.giver] = this.isExpandAll;
-          } else {
-            if (!response.recipientTeam) {
-              // Recipient is team
-              this.teamsToUsers[response.recipient] = this.teamsToUsers[response.recipient] || [];
-              if (this.teamsToUsers[response.recipient].indexOf(response.recipient) === -1) {
-                this.teamsToUsers[response.recipient].push(response.recipient);
-                this.teamExpanded[response.recipient] = this.isExpandAll;
-              }
-              this.userExpanded[response.recipient] = this.isExpandAll;
-              continue;
-            }
-            this.teamsToUsers[response.recipientTeam] = this.teamsToUsers[response.recipientTeam] || [];
-            if (this.teamsToUsers[response.recipientTeam].indexOf(response.recipient) === -1) {
-              this.teamsToUsers[response.recipientTeam].push(response.recipient);
-              this.teamExpanded[response.recipientTeam] = this.isExpandAll;
+        if (this.isGqr) {
+          this.teamsToUsers[response.giverTeam] = this.teamsToUsers[response.giverTeam] || [];
+          if (this.teamsToUsers[response.giverTeam].indexOf(response.giver) === -1) {
+            this.teamsToUsers[response.giverTeam].push(response.giver);
+            this.teamExpanded[response.giverTeam] = this.isExpandAll;
+          }
+          if (response.relatedGiverEmail) {
+            this.userToRelatedEmail[response.giver] = response.relatedGiverEmail;
+          }
+
+          this.userExpanded[response.giver] = this.isExpandAll;
+        } else {
+          if (!response.recipientTeam) {
+            // Recipient is team
+            this.teamsToUsers[response.recipient] = this.teamsToUsers[response.recipient] || [];
+            if (this.teamsToUsers[response.recipient].indexOf(response.recipient) === -1) {
+              this.teamsToUsers[response.recipient].push(response.recipient);
+              this.teamExpanded[response.recipient] = this.isExpandAll;
             }
             this.userExpanded[response.recipient] = this.isExpandAll;
+            continue;
           }
+          this.teamsToUsers[response.recipientTeam] = this.teamsToUsers[response.recipientTeam] || [];
+          if (this.teamsToUsers[response.recipientTeam].indexOf(response.recipient) === -1) {
+            this.teamsToUsers[response.recipientTeam].push(response.recipient);
+            this.teamExpanded[response.recipientTeam] = this.isExpandAll;
+          }
+          this.userExpanded[response.recipient] = this.isExpandAll;
         }
       }
     }
