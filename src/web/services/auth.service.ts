c1026d3da40fcfd2d73ecc60ae36556973170d0b
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-06-22 08:26:28 +0800

    Add endpoint to check regkey validity

--- src/web/services/auth.service.ts
+++ src/web/services/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { ResourceEndpoints } from '../types/api-endpoints';
-import { AuthInfo } from '../types/api-output';
+import { AuthInfo, RegkeyValidity } from '../types/api-output';
+import { Intent } from '../types/api-request';
 import { HttpRequestService } from './http-request.service';
 
 /**
@@ -29,6 +30,14 @@ export class AuthService {
       params.nextUrl = nextUrl;
     }
     return this.httpRequestService.get(ResourceEndpoints.AUTH, params);
+  }
+
+  /**
+   * Gets the validity of the given registration key for user.
+   */
+  getAuthRegkeyValidity(key: string, intent: Intent): Observable<RegkeyValidity> {
+    const params: Record<string, string> = { key, intent };
+    return this.httpRequestService.get(ResourceEndpoints.AUTH_REGKEY, params);
   }
 
 }
