b7902d382bbed6a5222e18b3d4090655b9f783de
Chee Yuan <chong-1997@hotmail.com>
2020-07-04 17:42:43 +0800

    Add skeleton for new collapse

---
+++ src/web/app/components/teammates-common/collapse-anim.ts
@@ -0,0 +1,17 @@
+import { animate, AnimationTriggerMetadata, state, style, transition, trigger } from '@angular/animations';
+
+/**
+ * Directive for collapsing of *ngIf columns.
+ */
+export const collapseAnim: AnimationTriggerMetadata = trigger('collapseAnim', [
+  state('*', style({ 'overflow-y': 'hidden' })),
+  state('void', style({ 'overflow-y': 'hidden' })),
+  transition(':leave', [
+    style({ height: '*' }),
+    animate('300ms ease-in-out', style({ height: 0, opacity: 0 })),
+  ]),
+  transition(':enter', [
+    style({ height: '0' }),
+    animate('300ms ease-in-out', style({ height: '*', opacity: 1 })),
+  ]),
+]);
