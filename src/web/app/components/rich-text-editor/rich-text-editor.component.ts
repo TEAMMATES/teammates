b51f2889bc49f296738c088422186c91cf8da651
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-07-10 14:54:42 +0800

    Remove unnecessary print and fullscreen plugins and shorten toolbar

--- src/web/app/components/rich-text-editor/rich-text-editor.component.ts
+++ src/web/app/components/rich-text-editor/rich-text-editor.component.ts
@@ -43,39 +43,21 @@ export class RichTextEditorComponent implements OnInit {
       skin_url: '/tinymce/skins/ui/oxide',
       suffix: '.min',
       resize: false,
-      fontsize_formats: '8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 20pt 24pt 26pt 28pt 36pt 48pt 72pt',
-      font_formats: 'Andale Mono=andale mono,times;'
-          + 'Arial=arial,helvetica,sans-serif;'
-          + 'Arial Black=arial black,avant garde;'
-          + 'Book Antiqua=book antiqua,palatino;'
-          + 'Comic Sans MS=comic sans ms,sans-serif;'
-          + 'Courier New=courier new,courier;'
-          + 'Georgia=georgia,palatino;'
-          + 'Helvetica=helvetica;'
-          + 'Impact=impact,chicago;'
-          + 'Symbol=symbol;'
-          + 'Tahoma=tahoma,arial,helvetica,sans-serif;'
-          + 'Terminal=terminal,monaco;'
-          + 'Times New Roman=times new roman,times;'
-          + 'Trebuchet MS=trebuchet ms,geneva;'
-          + 'Verdana=verdana,geneva;'
-          + 'Webdings=webdings;'
-          + 'Wingdings=wingdings,zapf dingbats',
       relative_urls: false,
       convert_urls: false,
       remove_linebreaks: false,
       plugins: [
         'placeholder',
-        'advlist autolink lists link image charmap print hr anchor',
-        'searchreplace wordcount visualblocks visualchars code fullscreen',
+        'advlist autolink lists link image charmap hr anchor',
+        'searchreplace wordcount visualblocks visualchars code',
         'insertdatetime nonbreaking save table directionality',
         'emoticons paste textpattern',
       ],
+      menubar: false,
 
-      toolbar1: 'insertfile undo redo | styleselect | bold italic underline '
-          + '| alignleft aligncenter alignright alignjustify '
-          + '| bullist numlist outdent indent | link image',
-      toolbar2: 'print | forecolor backcolor | fontsizeselect fontselect | charmap emoticons | fullscreen',
+      toolbar1: 'styleselect | forecolor backcolor '
+          + '| bold italic underline | alignleft aligncenter alignright alignjustify '
+          + '| bullist numlist | link image charmap emoticons',
     };
   }
 
