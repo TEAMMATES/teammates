466572f314b20d07a68cb637c40d48a04ba0bd85
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-07-10 15:56:01 +0800

    Fix student short name/email wrongly hidden for student-instructor caller

--- src/main/java/teammates/ui/webapi/action/GetStudentProfileAction.java
+++ src/main/java/teammates/ui/webapi/action/GetStudentProfileAction.java
@@ -74,7 +74,7 @@ public class GetStudentProfileAction extends Action {
 
         StudentProfileData output = new StudentProfileData(name, studentProfile);
         // If student requesting and is not the student's own profile, hide some fields
-        if (userInfo.isStudent && !studentId.equals(userInfo.id)) {
+        if (userInfo.isStudent && !userInfo.isInstructor && !studentId.equals(userInfo.id)) {
             output.hideInformationWhenViewedByOtherStudent();
         }
 
