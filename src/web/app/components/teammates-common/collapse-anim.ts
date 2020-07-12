3ccfec993df200c65c047ba157091db3e64f9123
jtankw3 <j.tankw3@gmail.com>
2020-07-12 22:15:06 +0800

    Fix CollapseAnim bug causing element cut off

--- src/web/app/components/teammates-common/collapse-anim.ts
+++ src/web/app/components/teammates-common/collapse-anim.ts
@@ -1,17 +1,15 @@
-import { animate, AnimationTriggerMetadata, state, style, transition, trigger } from '@angular/animations';
+import { animate, AnimationTriggerMetadata, style, transition, trigger } from '@angular/animations';
 
 /**
  * Directive for collapsing of *ngIf columns.
  */
 export const collapseAnim: AnimationTriggerMetadata = trigger('collapseAnim', [
-  state('*', style({ overflow: 'hidden' })),
-  state('void', style({ overflow: 'hidden' })),
   transition(':leave', [
-    style({ height: '*' }),
+    style({ height: '*', overflow: 'hidden' }),
     animate('300ms ease-in-out', style({ height: 0, opacity: 0 })),
   ]),
   transition(':enter', [
-    style({ height: '0' }),
-    animate('300ms ease-in-out', style({ height: '*', opacity: 1 })),
+    style({ height: '0', overflow: 'hidden' }),
+    animate('300ms ease-in-out', style({ height: '*', opacity: 1, overflow: 'visible' })),
   ]),
 ]);
