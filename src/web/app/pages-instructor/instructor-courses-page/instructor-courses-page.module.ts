3e7bb102c5b8b89747d3fec9f500bf15695c8c68
t-cheepeng <tcp97@hotmail.com>
2020-07-25 20:58:40 +0800

    [#10243] Loading spinners for show buttons  (#10403)
    * Add spinner to instructor course page show
    
    * Update spinner on instructor session table
    
    Co-authored-by: Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>

--- src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.module.ts
+++ src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbDropdownModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { AjaxLoadingModule } from '../../components/ajax-loading/ajax-loading.module';
 import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
 import { AddCourseFormModule } from './add-course-form/add-course-form.module';
 import { InstructorCoursesPageComponent } from './instructor-courses-page.component';
@@ -33,6 +34,7 @@ const routes: Routes = [
     NgbTooltipModule,
     AddCourseFormModule,
     LoadingSpinnerModule,
+    AjaxLoadingModule,
   ],
 })
 export class InstructorCoursesPageModule { }
