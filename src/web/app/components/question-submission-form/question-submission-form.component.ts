c2ad78caff30e9823b59aef46b3fa711f09586cb
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2020-08-24 18:07:59 +0800

    [#10638] Add guard clause to recipient submission form model update #10639

--- src/web/app/components/question-submission-form/question-submission-form.component.ts
+++ src/web/app/components/question-submission-form/question-submission-form.component.ts
@@ -201,6 +201,7 @@ export class QuestionSubmissionFormComponent implements OnInit {
    * Updates validity of all responses in a question.
    */
   updateValidity(isValid: boolean): void {
+    if (this.model.recipientSubmissionForms.length === 0) { return; }
     const recipientSubmissionForms: FeedbackResponseRecipientSubmissionFormModel[] =
         this.model.recipientSubmissionForms.slice().map(
             (model: FeedbackResponseRecipientSubmissionFormModel) => Object.assign({}, model, { isValid }));
