32b88f3b1d0141c202917f2e70b0daf2e158ea0e
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-10-03 15:54:42 +0800

    Merge BaseTestCaseWithBackDoorApiAccess with BaseE2ETestCase

--- src/e2e/java/teammates/e2e/cases/BaseE2ETestCase.java
+++ src/e2e/java/teammates/e2e/cases/BaseE2ETestCase.java
@@ -11,26 +11,43 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import teammates.common.datatransfer.DataBundle;
+import teammates.common.datatransfer.attributes.AccountAttributes;
+import teammates.common.datatransfer.attributes.CourseAttributes;
+import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
+import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
+import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
+import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
+import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
+import teammates.common.datatransfer.attributes.StudentProfileAttributes;
+import teammates.common.exception.HttpRequestFailedException;
 import teammates.common.util.AppUrl;
 import teammates.common.util.Const;
 import teammates.common.util.ThreadHelper;
 import teammates.common.util.Url;
+import teammates.common.util.retry.MaximumRetriesExceededException;
+import teammates.common.util.retry.RetryManager;
+import teammates.common.util.retry.RetryableTaskReturns;
 import teammates.e2e.pageobjects.AdminHomePage;
 import teammates.e2e.pageobjects.AppPage;
 import teammates.e2e.pageobjects.Browser;
 import teammates.e2e.pageobjects.DevServerLoginPage;
 import teammates.e2e.pageobjects.HomePage;
+import teammates.e2e.util.BackDoor;
 import teammates.e2e.util.EmailAccount;
 import teammates.e2e.util.TestProperties;
+import teammates.test.BaseTestCaseWithDatastoreAccess;
 import teammates.test.FileHelper;
 
 /**
  * Base class for all browser tests.
  *
  * <p>This type of test has no knowledge of the workings of the application,
- * and can only communicate via the UI or via {@link teammates.e2e.util.BackDoor} to obtain/transmit data.
+ * and can only communicate via the UI or via {@link BackDoor} to obtain/transmit data.
  */
-public abstract class BaseE2ETestCase extends BaseTestCaseWithBackDoorApiAccess {
+public abstract class BaseE2ETestCase extends BaseTestCaseWithDatastoreAccess {
+
+    static final BackDoor BACKDOOR = BackDoor.getInstance();
 
     protected Browser browser;
     protected DataBundle testData;
@@ -212,6 +229,201 @@ public abstract class BaseE2ETestCase extends BaseTestCaseWithBackDoorApiAccess 
         } catch (Exception e) {
             fail("Failed to verify email sent:" + e);
         }
+    }
+
+    @Override
+    @SuppressWarnings("PMD.EmptyMethodInAbstractClassShouldBeAbstract")
+    public void setupObjectify() {
+        // Not necessary as BackDoor API is used instead
+    }
+
+    @Override
+    @SuppressWarnings("PMD.EmptyMethodInAbstractClassShouldBeAbstract")
+    public void tearDownObjectify() {
+        // Not necessary as BackDoor API is used instead
+    }
+
+    @Override
+    @SuppressWarnings("PMD.EmptyMethodInAbstractClassShouldBeAbstract")
+    public void setUpGae() {
+        // Not necessary as BackDoor API is used instead
+    }
+
+    @Override
+    @SuppressWarnings("PMD.EmptyMethodInAbstractClassShouldBeAbstract")
+    public void tearDownGae() {
+        // Not necessary as BackDoor API is used instead
+    }
+
+    @Override
+    protected RetryManager getPersistenceRetryManager() {
+        return new RetryManager(TestProperties.PERSISTENCE_RETRY_PERIOD_IN_S / 2);
+    }
+
+    protected AccountAttributes getAccount(String googleId) {
+        return BACKDOOR.getAccount(googleId);
+    }
+
+    @Override
+    protected AccountAttributes getAccount(AccountAttributes account) {
+        return getAccount(account.googleId);
+    }
+
+    @Override
+    @SuppressWarnings("PMD.EmptyMethodInAbstractClassShouldBeAbstract")
+    protected StudentProfileAttributes getStudentProfile(StudentProfileAttributes studentProfileAttributes) {
+        return null; // BACKDOOR.getStudentProfile(studentProfileAttributes.googleId);
+    }
+
+    protected AccountAttributes getAccountWithRetry(String googleId) throws MaximumRetriesExceededException {
+        return getPersistenceRetryManager().runUntilNotNull(new RetryableTaskReturns<AccountAttributes>("getAccount") {
+            @Override
+            public AccountAttributes run() {
+                return getAccount(googleId);
+            }
+        });
+    }
+
+    protected CourseAttributes getCourse(String courseId) {
+        return BACKDOOR.getCourse(courseId);
+    }
+
+    @Override
+    protected CourseAttributes getCourse(CourseAttributes course) {
+        return getCourse(course.getId());
+    }
+
+    protected CourseAttributes getArchivedCourse(String instructorId, String courseId) {
+        return BACKDOOR.getArchivedCourse(instructorId, courseId);
+    }
+
+    protected CourseAttributes getCourseWithRetry(String courseId) throws MaximumRetriesExceededException {
+        return getPersistenceRetryManager().runUntilNotNull(new RetryableTaskReturns<CourseAttributes>("getCourse") {
+            @Override
+            public CourseAttributes run() {
+                return getCourse(courseId);
+            }
+        });
+    }
+
+    protected FeedbackQuestionAttributes getFeedbackQuestion(String courseId, String feedbackSessionName, int qnNumber) {
+        return BACKDOOR.getFeedbackQuestion(courseId, feedbackSessionName, qnNumber);
+    }
+
+    @Override
+    protected FeedbackQuestionAttributes getFeedbackQuestion(FeedbackQuestionAttributes fq) {
+        return getFeedbackQuestion(fq.courseId, fq.feedbackSessionName, fq.questionNumber);
+    }
+
+    protected FeedbackQuestionAttributes getFeedbackQuestionWithRetry(
+            String courseId, String feedbackSessionName, int qnNumber)
+            throws MaximumRetriesExceededException {
+        return getPersistenceRetryManager().runUntilNotNull(new RetryableTaskReturns<FeedbackQuestionAttributes>(
+                "getFeedbackQuestion") {
+            @Override
+            public FeedbackQuestionAttributes run() {
+                return getFeedbackQuestion(courseId, feedbackSessionName, qnNumber);
+            }
+        });
+    }
+
+    protected FeedbackResponseCommentAttributes getFeedbackResponseComment(String feedbackResponseId) {
+        return BACKDOOR.getFeedbackResponseComment(feedbackResponseId);
+    }
+
+    @Override
+    protected FeedbackResponseCommentAttributes getFeedbackResponseComment(FeedbackResponseCommentAttributes frc) {
+        return getFeedbackResponseComment(frc.feedbackResponseId);
+    }
+
+    protected FeedbackResponseAttributes getFeedbackResponse(String feedbackQuestionId, String giver, String recipient) {
+        return BACKDOOR.getFeedbackResponse(feedbackQuestionId, giver, recipient);
+    }
+
+    @Override
+    protected FeedbackResponseAttributes getFeedbackResponse(FeedbackResponseAttributes fr) {
+        return getFeedbackResponse(fr.feedbackQuestionId, fr.giver, fr.recipient);
+    }
+
+    protected FeedbackSessionAttributes getFeedbackSession(String courseId, String feedbackSessionName) {
+        return BACKDOOR.getFeedbackSession(courseId, feedbackSessionName);
+    }
+
+    @Override
+    protected FeedbackSessionAttributes getFeedbackSession(FeedbackSessionAttributes fs) {
+        return getFeedbackSession(fs.getCourseId(), fs.getFeedbackSessionName());
+    }
+
+    protected FeedbackSessionAttributes getFeedbackSessionWithRetry(String courseId, String feedbackSessionName)
+            throws MaximumRetriesExceededException {
+        return getPersistenceRetryManager().runUntilNotNull(new RetryableTaskReturns<FeedbackSessionAttributes>(
+                "getFeedbackSession") {
+            @Override
+            public FeedbackSessionAttributes run() {
+                return getFeedbackSession(courseId, feedbackSessionName);
+            }
+        });
+    }
+
+    protected FeedbackSessionAttributes getSoftDeletedSession(String feedbackSessionName, String instructorId) {
+        return BACKDOOR.getSoftDeletedSession(feedbackSessionName, instructorId);
+    }
+
+    protected InstructorAttributes getInstructor(String courseId, String instructorEmail) {
+        return BACKDOOR.getInstructor(courseId, instructorEmail);
+    }
+
+    @Override
+    protected InstructorAttributes getInstructor(InstructorAttributes instructor) {
+        return getInstructor(instructor.courseId, instructor.email);
+    }
+
+    protected InstructorAttributes getInstructorWithRetry(String courseId, String instructorEmail)
+            throws MaximumRetriesExceededException {
+        return getPersistenceRetryManager().runUntilNotNull(new RetryableTaskReturns<InstructorAttributes>("getInstructor") {
+            @Override
+            public InstructorAttributes run() {
+                return getInstructor(courseId, instructorEmail);
+            }
+        });
+    }
+
+    protected String getKeyForInstructor(String courseId, String instructorEmail) {
+        return getInstructor(courseId, instructorEmail).getKey();
+    }
+
+    protected String getKeyForInstructorWithRetry(String courseId, String instructorEmail)
+            throws MaximumRetriesExceededException {
+        return getPersistenceRetryManager().runUntilSuccessful(new RetryableTaskReturns<String>("getKeyForInstructor") {
+            @Override
+            public String run() {
+                return getKeyForInstructor(courseId, instructorEmail);
+            }
+
+            @Override
+            public boolean isSuccessful(String result) {
+                return !result.startsWith(Const.StatusCodes.BACKDOOR_STATUS_FAILURE);
+            }
+        });
+    }
+
+    @Override
+    protected StudentAttributes getStudent(StudentAttributes student) {
+        return BACKDOOR.getStudent(student.course, student.email);
+    }
+
+    protected String getKeyForStudent(StudentAttributes student) {
+        return getStudent(student).getKey();
+    }
+
+    @Override
+    protected String doRemoveAndRestoreDataBundle(DataBundle testData) throws HttpRequestFailedException {
+        return BACKDOOR.removeAndRestoreDataBundle(testData);
+    }
+
+    @Override
+    protected String doPutDocuments(DataBundle testData) {
+        return BACKDOOR.putDocuments(testData);
     }
 
 }
