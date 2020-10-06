efc19a1464cb0aefa8c496807ec90c775b54282c
jtankw3 <j.tankw3@gmail.com>
2020-10-06 16:00:35 +0800

    Add InstructorCourseDetailsPageE2ETest

---
+++ src/e2e/java/teammates/e2e/cases/e2e/InstructorCourseDetailsPageE2ETest.java
@@ -0,0 +1,124 @@
+package teammates.e2e.cases.e2e;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.attributes.CourseAttributes;
+import teammates.common.datatransfer.attributes.InstructorAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
+import teammates.common.util.AppUrl;
+import teammates.common.util.Const;
+import teammates.e2e.pageobjects.InstructorCourseDetailsPage;
+import teammates.e2e.util.TestProperties;
+
+/**
+ * SUT: {@link Const.WebPageURIs#INSTRUCTOR_COURSE_DETAILS_PAGE}.
+ */
+public class InstructorCourseDetailsPageE2ETest extends BaseE2ETestCase {
+    private StudentAttributes student;
+    private CourseAttributes course;
+
+    private String fileName;
+
+    @Override
+    protected void prepareTestData() {
+        testData = loadDataBundle("/InstructorCourseDetailsPageE2ETest.json");
+        student = testData.students.get("charlie.tmms@ICDetailsE2eT.CS2104");
+        if (!TestProperties.isDevServer()) {
+            student.email = TestProperties.TEST_STUDENT1_ACCOUNT;
+        }
+
+        removeAndRestoreDataBundle(testData);
+        course = testData.courses.get("ICDetailsE2eT.CS2104");
+        fileName = "/" + course.getId() + "_studentList.csv";
+    }
+
+    @BeforeClass
+    public void classSetup() {
+        deleteDownloadsFile(fileName);
+    }
+
+    @Test
+    public void allTests() {
+        AppUrl detailsPageUrl = createUrl(Const.WebPageURIs.INSTRUCTOR_COURSE_DETAILS_PAGE)
+                .withUserId(testData.instructors.get("ICDetailsE2eT.instr").googleId)
+                .withCourseId(course.getId());
+        InstructorCourseDetailsPage detailsPage = loginAdminToPage(detailsPageUrl, InstructorCourseDetailsPage.class);
+
+        ______TS("verify loaded details");
+        InstructorAttributes[] instructors = { testData.instructors.get("ICDetailsE2eT.instr"),
+                testData.instructors.get("ICDetailsE2eT.instr2"),
+        };
+        StudentAttributes[] students = { testData.students.get("alice.tmms@ICDetailsE2eT.CS2104"),
+                testData.students.get("charlie.tmms@ICDetailsE2eT.CS2104"),
+                testData.students.get("benny.tmms@ICDetailsE2eT.CS2104"),
+                testData.students.get("danny.tmms@ICDetailsE2eT.CS2104"),
+        };
+
+        verifyCourseDetails(detailsPage, course, instructors, students);
+        detailsPage.verifyNumStudents(4);
+        detailsPage.verifyStudentDetails(students);
+
+        ______TS("send invite");
+        detailsPage.sendInvite(student);
+
+        detailsPage.verifyStatusMessage("An email has been sent to " + student.getEmail());
+        String expectedEmailSubject = "TEAMMATES: Invitation to join course ["
+                + course.getName() + "][" + course.getId() + "]";
+        verifyEmailSent(student.getEmail(), expectedEmailSubject);
+
+        ______TS("remind all students to join");
+        detailsPage.remindAllToJoin();
+
+        detailsPage.verifyStatusMessage("Emails have been sent to unregistered students.");
+        verifyEmailSent(student.getEmail(), expectedEmailSubject);
+
+        ______TS("download student list");
+        detailsPage.downloadStudentList();
+        String status = student.googleId.isEmpty() ? "Yet to Join" : "Joined";
+        String lastName = student.getName().split(" ")[1];
+        String[] studentInfo = { student.getTeam(), student.getName(), lastName, status, student.getEmail() };
+        List<String> expectedContent = Arrays.asList("Course ID," + course.getId(),
+                "Course Name," + course.getName(), String.join(",", studentInfo));
+        verifyDownloadedFile(fileName, expectedContent);
+
+        ______TS("delete student");
+        detailsPage.sortByName();
+        detailsPage.sortByStatus();
+        StudentAttributes[] studentsAfterDelete = { students[3], students[0], students[2] };
+        detailsPage.deleteStudent(student);
+
+        detailsPage.verifyStatusMessage("Student is successfully deleted from course \""
+                + course.getId() + "\"");
+        detailsPage.verifyNumStudents(3);
+        detailsPage.verifyStudentDetails(studentsAfterDelete);
+        verifyAbsentInDatastore(student);
+
+        ______TS("delete all students");
+        detailsPage.deleteAllStudents();
+
+        detailsPage.verifyStatusMessage("All the students have been removed from the course");
+        detailsPage.verifyNumStudents(0);
+        verifyAbsentInDatastore(studentsAfterDelete[0]);
+        verifyAbsentInDatastore(studentsAfterDelete[1]);
+        verifyAbsentInDatastore(studentsAfterDelete[2]);
+    }
+
+    private void verifyCourseDetails(InstructorCourseDetailsPage detailsPage, CourseAttributes course,
+                                     InstructorAttributes[] instructors, StudentAttributes[] students) {
+        Set<String> sections = new HashSet<>();
+        Set<String> teams = new HashSet<>();
+
+        for (StudentAttributes student : students) {
+            sections.add(student.getSection());
+            teams.add(student.getTeam());
+        }
+
+        detailsPage.verifyCourseDetails(course, instructors, sections.size(), teams.size(), students.length);
+    }
+}
