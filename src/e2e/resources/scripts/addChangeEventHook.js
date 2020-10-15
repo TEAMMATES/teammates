8f34791c9b7e7cae68841b9d50c333a4a195c0e9
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-10-16 01:26:11 +0800

    Remove unused items in page object classes

--- src/e2e/resources/scripts/addChangeEventHook.js
+++
@@ -1,6 +0,0 @@
-var seleniumArguments = arguments;
-seleniumArguments[0].addEventListener(seleniumArguments[1], function onchange() {
-  this.removeEventListener(seleniumArguments[1], onchange);
-  document.body.setAttribute(seleniumArguments[2], true);
-});
-document.body.setAttribute(seleniumArguments[2], false);
