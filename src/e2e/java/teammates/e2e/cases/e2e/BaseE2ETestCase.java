c19420d68c4d599b2a8216a6b32f05576a12a0f9
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-10-26 01:25:28 +0800

    Remove unnecessary GoogleLoginPage and login-related functions

--- src/e2e/java/teammates/e2e/cases/e2e/BaseE2ETestCase.java
+++ src/e2e/java/teammates/e2e/cases/e2e/BaseE2ETestCase.java
@@ -21,7 +21,6 @@ import teammates.e2e.pageobjects.AppPage;
 import teammates.e2e.pageobjects.Browser;
 import teammates.e2e.pageobjects.DevServerLoginPage;
 import teammates.e2e.pageobjects.HomePage;
-import teammates.e2e.pageobjects.LoginPage;
 import teammates.e2e.util.EmailAccount;
 import teammates.e2e.util.TestProperties;
 import teammates.test.FileHelper;
@@ -121,7 +120,6 @@ public abstract class BaseE2ETestCase extends BaseTestCaseWithBackDoorApiAccess 
 
         // In dev server, any username is acceptable as admin
         String adminUsername = "devserver.admin.account";
-        String adminPassword = "anypassword";
 
         String userId = url.get(Const.ParamsNames.USER_ID);
 
@@ -131,8 +129,8 @@ public abstract class BaseE2ETestCase extends BaseTestCaseWithBackDoorApiAccess 
             adminUsername = userId;
         }
 
-        LoginPage loginPage = AppPage.getNewPageInstance(browser, DevServerLoginPage.class);
-        loginPage.loginAsAdmin(adminUsername, adminPassword);
+        DevServerLoginPage loginPage = AppPage.getNewPageInstance(browser, DevServerLoginPage.class);
+        loginPage.loginAsAdmin(adminUsername);
 
         return AppPage.getNewPageInstance(browser, url, typeOfPage);
     }
