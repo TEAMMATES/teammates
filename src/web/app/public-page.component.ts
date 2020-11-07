c998e7de986a16a29320e1ae379ea8a5f05c2db5
Wilson Kurniawan <wkurniawan.92@gmail.com>
2020-11-07 20:50:45 +0800

    Add back authService call for public page

--- src/web/app/public-page.component.ts
+++ src/web/app/public-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '../environments/environment';
+import { AuthService } from '../services/auth.service';
 
 /**
  * Component for publicly available pages.
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
   selector: 'tm-public-page',
   template: '<tm-page [isValidUser]="true" [hideAuthInfo]="true"></tm-page>',
 })
-export class PublicPageComponent {}
+export class PublicPageComponent {
+
+  constructor(private route: ActivatedRoute,
+              private authService: AuthService) {
+    if (environment.maintenance) {
+      return;
+    }
+    this.route.queryParams.subscribe((queryParams: any) => {
+      this.authService.getAuthUser(queryParams.user).subscribe(() => {
+        // No need to do anything; this is necessary to get CSRF token
+      });
+    });
+  }
+
+}
