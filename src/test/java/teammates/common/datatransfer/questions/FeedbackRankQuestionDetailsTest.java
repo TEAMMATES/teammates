0132c3241ad6221c1ebcff27883737d5e3ed1d97
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2020-12-11 12:56:02 +0800

    Add unit tests for Feedback*QuestionDetails classes

---
+++ src/test/java/teammates/common/datatransfer/questions/FeedbackRankQuestionDetailsTest.java
@@ -0,0 +1,41 @@
+package teammates.common.datatransfer.questions;
+
+import org.testng.annotations.Test;
+
+import teammates.test.BaseTestCase;
+
+/**
+ * SUT: {@link FeedbackRankQuestionDetails}.
+ */
+public class FeedbackRankQuestionDetailsTest extends BaseTestCase {
+    @Test
+    public void testValidateMinOptionsToBeRanked_sameValidValues_shouldReturnTrue() {
+        int testValue = 100;
+        FeedbackRankQuestionDetails feedbackRankQuestionDetails = new FeedbackRankOptionsQuestionDetails();
+        feedbackRankQuestionDetails.setMinOptionsToBeRanked(testValue);
+        assertEquals(feedbackRankQuestionDetails.minOptionsToBeRanked, testValue);
+    }
+    @Test
+    public void testValidateMinOptionsToBeRanked_differentValidValues_shouldReturnTrue() {
+        int testValue = 100;
+        int testDiffValue = 101;
+        FeedbackRankQuestionDetails feedbackRankQuestionDetails = new FeedbackRankOptionsQuestionDetails();
+        feedbackRankQuestionDetails.setMinOptionsToBeRanked(testValue);
+        assertNotEquals(feedbackRankQuestionDetails.minOptionsToBeRanked, testDiffValue);
+    }
+    @Test
+    public void testValidateMaxOptionsToBeRanked_sameValidValues_shouldReturnTrue() {
+        int testValue = 100;
+        FeedbackRankQuestionDetails feedbackRankQuestionDetails = new FeedbackRankOptionsQuestionDetails();
+        feedbackRankQuestionDetails.setMaxOptionsToBeRanked(testValue);
+        assertEquals(feedbackRankQuestionDetails.maxOptionsToBeRanked, testValue);
+    }
+    @Test
+    public void testValidateMaxOptionsToBeRanked_differentValidValues_shouldReturnTrue() {
+        int testValue = 100;
+        int testDiffValue = 101;
+        FeedbackRankQuestionDetails feedbackRankQuestionDetails = new FeedbackRankOptionsQuestionDetails();
+        feedbackRankQuestionDetails.setMaxOptionsToBeRanked(testValue);
+        assertNotEquals(feedbackRankQuestionDetails.maxOptionsToBeRanked, testDiffValue);
+    }
+}
---
+++ src/test/java/teammates/common/datatransfer/questions/FeedbackTextResponseDetailsTest.java
@@ -0,0 +1,48 @@
+package teammates.common.datatransfer.questions;
+
+import org.testng.annotations.Test;
+
+import teammates.common.util.SanitizationHelper;
+import teammates.test.BaseTestCase;
+
+/**
+ * SUT: {@link FeedbackTextResponseDetails}.
+ */
+public class FeedbackTextResponseDetailsTest extends BaseTestCase {
+    @Test
+    public void testValidateResponseAnswer_sameValues_shouldReturnTrue() {
+        String testAnswer = "Hello World";
+        FeedbackTextResponseDetails feedbackTextResponseDetails = new FeedbackTextResponseDetails(testAnswer);
+        assertEquals(feedbackTextResponseDetails.getAnswer(), "Hello World");
+    }
+    @Test
+    public void testValidateResponseAnswer_differentValues_shouldReturnFalse() {
+        String testAnswer = "Hello World";
+        FeedbackTextResponseDetails feedbackTextResponseDetails = new FeedbackTextResponseDetails(testAnswer);
+        assertNotEquals(feedbackTextResponseDetails.getAnswer(), "World Hello");
+    }
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testValidateAttributesOfAnswer_nullValue_shouldThrowException() {
+        FeedbackTextResponseDetails feedbackTextResponseDetails = new FeedbackTextResponseDetails(null);
+        int test = feedbackTextResponseDetails.getAnswer().length();
+    }
+    @Test
+    public void testValidateAttributesOfAnswer_validValue_shouldThrowException() {
+        String testValue = "Hello World";
+        FeedbackTextResponseDetails feedbackTextResponseDetails = new FeedbackTextResponseDetails(testValue);
+        int test = feedbackTextResponseDetails.getAnswer().length();
+    }
+    @Test
+    public void testValidateSanitizedAnswerString_nullValues_shouldReturnTrue() {
+        FeedbackTextResponseDetails feedbackTextResponseDetails = new FeedbackTextResponseDetails(null);
+        assertEquals(feedbackTextResponseDetails.getAnswerString(),
+                SanitizationHelper.sanitizeForRichText(feedbackTextResponseDetails.getAnswer()));
+    }
+    @Test
+    public void testValidateSanitizedAnswerString_validValues_shouldReturnTrue() {
+        String testValue = "Hello World";
+        FeedbackTextResponseDetails feedbackTextResponseDetails = new FeedbackTextResponseDetails(testValue);
+        assertEquals(feedbackTextResponseDetails.getAnswerString(),
+                SanitizationHelper.sanitizeForRichText(feedbackTextResponseDetails.getAnswer()));
+    }
+}
