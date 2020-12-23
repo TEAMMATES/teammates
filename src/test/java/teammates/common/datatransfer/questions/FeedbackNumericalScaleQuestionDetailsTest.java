4da742870fd13500baba1d52a2759c2b29381fee
rrtheonlyone <rahul.rajesh.bhat@gmail.com>
2020-12-23 09:19:40 +0800

    Add failing test: validation does not take into account minimum value

--- src/test/java/teammates/common/datatransfer/questions/FeedbackNumericalScaleQuestionDetailsTest.java
+++ src/test/java/teammates/common/datatransfer/questions/FeedbackNumericalScaleQuestionDetailsTest.java
@@ -47,7 +47,7 @@ public class FeedbackNumericalScaleQuestionDetailsTest extends BaseTestCase {
         resp = new FeedbackNumericalScaleResponseDetails();
         resp.setAnswer(1.333);
         errors = numScaleQuestion.validateResponsesDetails(Arrays.asList(resp), 1);
-        assertTrue(errors.size() == 1);
+        assertEquals(1, errors.size());
         assertEquals("Please enter a valid value. The two nearest valid values are 1.3 and 1.4.", errors.get(0));
 
         ______TS("Test Val=2 and Step=0.1 does not trigger error");
@@ -68,40 +68,40 @@ public class FeedbackNumericalScaleQuestionDetailsTest extends BaseTestCase {
         resp = new FeedbackNumericalScaleResponseDetails();
         resp.setAnswer(1.333333);
         errors = numScaleQuestion.validateResponsesDetails(Arrays.asList(resp), 1);
-        assertTrue(errors.size() == 1);
+        assertEquals(1, errors.size());
         assertEquals("Please enter a valid value. The two nearest valid values are 1.33333 and 1.33334.",
                 errors.get(0));
 
-        numScaleQuestion.setStep(1);
-        numScaleQuestion.setMinScale(1);
+        numScaleQuestion.setStep(0.7);
+        numScaleQuestion.setMinScale(3);
         numScaleQuestion.setMaxScale(5);
 
         ______TS("Test Val=6 and Max=5,Step=1 triggers error");
         resp = new FeedbackNumericalScaleResponseDetails();
         resp.setAnswer(5.1);
         errors = numScaleQuestion.validateResponsesDetails(Arrays.asList(resp), 1);
-        assertTrue(errors.size() == 1);
-        assertEquals("5.1 is out of the range for Numerical-scale question.(min=1, max=5)", errors.get(0));
+        assertEquals(1, errors.size());
+        assertEquals("5.1 is out of the range for Numerical-scale question.(min=3, max=5)", errors.get(0));
 
-        ______TS("Test Val=0 and Max=5,Step=1 triggers error");
+        ______TS("Test Val=0 and Min=3,Step=1 triggers error");
         resp = new FeedbackNumericalScaleResponseDetails();
         resp.setAnswer(5.1);
         errors = numScaleQuestion.validateResponsesDetails(Arrays.asList(resp), 1);
-        assertTrue(errors.size() == 1);
-        assertEquals("5.1 is out of the range for Numerical-scale question.(min=1, max=5)", errors.get(0));
+        assertEquals(1, errors.size());
+        assertEquals("5.1 is out of the range for Numerical-scale question.(min=3, max=5)", errors.get(0));
 
         ______TS("Test Val=5.1 and Max=5,Step=1 triggers error");
         resp = new FeedbackNumericalScaleResponseDetails();
         resp.setAnswer(5.1);
         errors = numScaleQuestion.validateResponsesDetails(Arrays.asList(resp), 1);
         assertTrue(errors.size() == 1);
-        assertEquals("5.1 is out of the range for Numerical-scale question.(min=1, max=5)", errors.get(0));
+        assertEquals("5.1 is out of the range for Numerical-scale question.(min=3, max=5)", errors.get(0));
 
         FeedbackNumericalScaleResponseDetails correctResp = new FeedbackNumericalScaleResponseDetails();
-        correctResp.setAnswer(1);
+        correctResp.setAnswer(3);
 
         FeedbackNumericalScaleResponseDetails respInvalidStep = new FeedbackNumericalScaleResponseDetails();
-        respInvalidStep.setAnswer(1.1);
+        respInvalidStep.setAnswer(3.5);
 
         FeedbackNumericalScaleResponseDetails respInvalidRange = new FeedbackNumericalScaleResponseDetails();
         respInvalidRange.setAnswer(100);
@@ -109,9 +109,9 @@ public class FeedbackNumericalScaleQuestionDetailsTest extends BaseTestCase {
         ______TS("Test 1 correct + 2 wrong triggers right messages");
         errors = numScaleQuestion
                 .validateResponsesDetails(Arrays.asList(correctResp, respInvalidStep, respInvalidRange), 1);
-        assertTrue(errors.size() == 2);
-        assertEquals("Please enter a valid value. The two nearest valid values are 1.0 and 2.0.", errors.get(0));
-        assertEquals("100 is out of the range for Numerical-scale question.(min=1, max=5)", errors.get(1));
+        assertEquals(2, errors.size());
+        assertEquals("Please enter a valid value. The two nearest valid values are 3.0 and 3.7.", errors.get(0));
+        assertEquals("100 is out of the range for Numerical-scale question.(min=3, max=5)", errors.get(1));
     }
 
     @Test
@@ -126,20 +126,20 @@ public class FeedbackNumericalScaleQuestionDetailsTest extends BaseTestCase {
         numScaleQuestion.setMaxScale(1);
         numScaleQuestion.setMinScale(10);
         errors = numScaleQuestion.validateQuestionDetails();
-        assertTrue(errors.size() == 1);
+        assertEquals(1, errors.size());
         assertEquals(Const.FeedbackQuestion.NUMSCALE_ERROR_MIN_MAX, errors.get(0));
 
         ______TS("Test Step <= 0 error");
         numScaleQuestion.setMaxScale(100);
         numScaleQuestion.setStep(0);
         errors = numScaleQuestion.validateQuestionDetails();
-        assertTrue(errors.size() == 1);
+        assertEquals(1, errors.size());
         assertEquals(Const.FeedbackQuestion.NUMSCALE_ERROR_STEP, errors.get(0));
 
         ______TS("Test Step <= 0 + Min >= Max error");
         numScaleQuestion.setMinScale(100);
         errors = numScaleQuestion.validateQuestionDetails();
-        assertTrue(errors.size() == 2);
+        assertEquals(2, errors.size());
         assertEquals(Const.FeedbackQuestion.NUMSCALE_ERROR_MIN_MAX, errors.get(0));
         assertEquals(Const.FeedbackQuestion.NUMSCALE_ERROR_STEP, errors.get(1));
     }
