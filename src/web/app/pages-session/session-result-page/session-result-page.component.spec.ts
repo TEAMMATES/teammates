6b8842525ae8971f7372c90e026646db8cac0568
t-cheepeng <tcp97@hotmail.com>
2021-01-18 11:47:20 +0800

    Add snapshot test to session result component

--- src/web/app/pages-session/session-result-page/session-result-page.component.spec.ts
+++ src/web/app/pages-session/session-result-page/session-result-page.component.spec.ts
@@ -1,12 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  FeedbackSessionPublishStatus,
+  FeedbackSessionSubmissionStatus,
+  ResponseVisibleSetting,
+  SessionVisibleSetting,
+} from '../../../types/api-output';
 import { LoadingRetryModule } from '../../components/loading-retry/loading-retry.module';
 import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
 import { SingleStatisticsModule } from '../../components/question-responses/single-statistics/single-statistics.module';
-import {
-  StudentViewResponsesModule,
-} from '../../components/question-responses/student-view-responses/student-view-responses.module';
+import { StudentViewResponsesModule } from '../../components/question-responses/student-view-responses/student-view-responses.module';
 import { QuestionTextWithInfoModule } from '../../components/question-text-with-info/question-text-with-info.module';
 import { SessionResultPageComponent } from './session-result-page.component';
 
@@ -38,5 +42,60 @@ describe('SessionResultPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with session results are loading', () => {
+    component.isFeedbackSessionResultsLoading = true;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap when session results failed to load', () => {
+    component.isFeedbackSessionResultsLoading = false;
+    component.hasFeedbackSessionResultsLoadingFailed = true;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with user that is logged in and using session link', () => {
+    component.regKey = 'session-link-key';
+    component.loggedInUser = 'alice';
+    component.personName = 'alice';
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with user that is not logged in and using session link', () => {
+    component.regKey = 'session-link-key';
+    component.loggedInUser = '';
+    component.personName = 'alice';
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with an open feedback session with no questions', () => {
+    component.session = {
+      courseId: 'CS3281',
+      timeZone: 'UTC',
+      feedbackSessionName: 'Peer Review 1',
+      instructions: '',
+      submissionStartTimestamp: 1555232400,
+      submissionEndTimestamp: 1555233400,
+      gracePeriod: 0,
+      sessionVisibleSetting: SessionVisibleSetting.AT_OPEN,
+      responseVisibleSetting: ResponseVisibleSetting.AT_VISIBLE,
+      submissionStatus: FeedbackSessionSubmissionStatus.OPEN,
+      publishStatus: FeedbackSessionPublishStatus.NOT_PUBLISHED,
+      isClosingEmailEnabled: true,
+      isPublishedEmailEnabled: true,
+      createdAtTimestamp: 1555231400,
+    };
+    component.questions = [];
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 });
