86900e3a30883dc32de13f7cc614f8cd9f25d932
jianhandev <jianhanli.dev@gmail.com>
2021-01-25 10:11:46 +0800

    Tweak code to integrate data from backend

--- src/web/app/components/response-time-series-chart/response-time-series-chart.component.ts
+++ src/web/app/components/response-time-series-chart/response-time-series-chart.component.ts
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 import { BehaviorSubject, interval } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
 import { FeedbackResponseStatsService } from '../../../services/feedback-response-stats.service';
-import { FeedbackResponseStats } from '../../../types/api-output';
+import { FeedbackResponseRecord, FeedbackResponseRecords } from '../../../types/api-output';
 import { ErrorMessageOutput } from '../../error-message-output';
 import { ResponseTimeSeriesChartModel } from './response-time-series-chart.model';
 
@@ -29,7 +29,7 @@ export class ResponseTimeSeriesChartComponent implements OnInit {
 
   ngOnInit(): void {
     this.interval.pipe(
-        switchMap((value: number) => interval(value * 60 * 1000)),
+        switchMap((value: number) => interval(value * 1000)),
         tap(() => this.refresh()),
     ).subscribe();
   }
@@ -37,11 +37,9 @@ export class ResponseTimeSeriesChartComponent implements OnInit {
   refresh(): void {
     this.feedbackResponseStatsService.loadResponseStats(this.model.durationMinutes.toString(),
         this.model.intervalMinutes.toString())
-        .subscribe((data: FeedbackResponseStats) => {
-          const parsedData: any[] = this.parseData(data);
-          this.drawChart(parsedData, this.model.durationMinutes * 60 * 1000);
+        .subscribe((records: FeedbackResponseRecords) => {
+          this.drawChart(records.responseRecords, this.model.durationMinutes * 60 * 1000);
         }, (resp: ErrorMessageOutput) => {
-
         });
   }
 
@@ -60,7 +58,7 @@ export class ResponseTimeSeriesChartComponent implements OnInit {
     this.interval.next(newInterval);
   }
 
-  drawChart(data: object[], duration: number): void {
+  drawChart(data: FeedbackResponseRecord[], duration: number): void {
     const svg: any = d3.select('svg');
     const svgWidth: number = 800;
     const svgHeight: number = 400;
@@ -84,13 +82,14 @@ export class ResponseTimeSeriesChartComponent implements OnInit {
     const y: any = d3.scaleLinear()
         .rangeRound([height, 0]);
 
+    console.log(data);
     x.domain([Date.now() - duration, Date.now()]);
-    y.domain(d3.extent(data, (d: any) => d.resCount));
+    y.domain(d3.extent(data, (r: FeedbackResponseRecord) => r.count));
 
     const line: any = d3.line()
-        .defined((d: any) => d.timestamp >= Date.now() - duration && d.timestamp <= Date.now())
-        .x((d: any) => x(d.timestamp))
-        .y((d: any) => y(d.resCount));
+        .defined((r: FeedbackResponseRecord) => r.timestamp >= Date.now() - duration && r.timestamp <= Date.now())
+        .x((r: FeedbackResponseRecord) => x(r.timestamp))
+        .y((r: FeedbackResponseRecord) => y(r.count));
 
     container.append('g')
         .attr('transform', `translate(0,${height})`)
@@ -114,16 +113,5 @@ export class ResponseTimeSeriesChartComponent implements OnInit {
         .attr('stroke-linecap', 'round')
         .attr('stroke-width', 1.5)
         .attr('d', line);
-  }
-
-  parseData(data: any): any[] {
-    const arr: any[] = [];
-    for (const i of Object.keys(data.datapoints)) {
-      arr.push({
-        timestamp: new Date(i),
-        resCount: +data.datapoints[i],
-      });
-    }
-    return arr;
   }
 }
