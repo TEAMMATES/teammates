3f2f9b2cb5fb47d2e61c15c497951dcd10c52c22
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2021-02-11 17:59:32 +0800

    Enhance the script with binary search

--- src/client/java/teammates/client/scripts/ListActiveInstructors.java
+++ src/client/java/teammates/client/scripts/ListActiveInstructors.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import teammates.client.remoteapi.RemoteApiClient;
@@ -15,28 +16,30 @@ import teammates.storage.entity.FeedbackSession;
 public class ListActiveInstructors extends RemoteApiClient {
     @Override
     protected void doOperation() {
-        //2020/01/01
+        //2010/01/01
         int startDate = 1;
         int startMonth = 1;
         int startYear = 2020;
         //2021/02/01
         int endDate = 1;
-        int endMonth = 2;
+        int endMonth = 3;
         int endYear = 2021;
 
         LocalDate startPoint = LocalDate.of(startYear, startMonth, startDate);
         LocalDate endPoint = LocalDate.of(endYear, endMonth, endDate);
-        long startTime = startPoint.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
-        long endTime = endPoint.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
+        long startTimeInMilli = startPoint.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
+        long endTimeInMilli = endPoint.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
+        Instant startTime = Instant.ofEpochMilli(startTimeInMilli);
+        Instant endTime = Instant.ofEpochMilli(endTimeInMilli);
 
         Set<String> activeInstructorEmails = new HashSet<>();
-        ofy().load().type(FeedbackSession.class).forEach(feedbackSession -> {
-            Instant createdTime = feedbackSession.getCreatedTime();
-            //validate active period of instructors
-            if (createdTime.isAfter(Instant.ofEpochMilli(startTime))
-                    && createdTime.isBefore(Instant.ofEpochMilli(endTime))) {
-                activeInstructorEmails.add(feedbackSession.getCreatorEmail());
-            }
+        List<FeedbackSession> feedbackSessions = ofy().load().type(FeedbackSession.class).list();
+        int startIndex = getTimeIndex(feedbackSessions, startTime);
+        int endIndex = getTimeIndex(feedbackSessions, endTime);
+
+        List<FeedbackSession> desiredFeedbackSessions = feedbackSessions.subList(startIndex, endIndex + 1);
+        desiredFeedbackSessions.forEach(feedbackSession -> {
+            activeInstructorEmails.add(feedbackSession.getCreatorEmail());
         });
 
         StringBuilder results = new StringBuilder();
@@ -44,6 +47,34 @@ public class ListActiveInstructors extends RemoteApiClient {
             results.append(email).append(", \n");
         }
         System.out.println(results.toString());
+    }
+
+    /**
+     * Get the index of desired time with binary search.
+     * @param feedbackSessions All feedback sessions to search
+     * @param desiredTime The timestamp to search for
+     * @return timeIndex
+     */
+    public int getTimeIndex(List<FeedbackSession> feedbackSessions, Instant desiredTime) {
+        int mid = 0;
+        int start = 0;
+        int end = feedbackSessions.size() - 1;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            Instant currentTime = feedbackSessions.get(mid).getCreatedTime();
+
+            if (currentTime.equals(desiredTime)) {
+                start = mid;
+            } else if (currentTime.isAfter(desiredTime)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+
+        return mid;
     }
 
     public static void main(String[] args) throws Exception {
