37d1eede3975f2dc5431a54c3e442110aa1f9923
Derek-Hardy <derekzxj@outlook.com>
2021-02-19 14:38:46 +0800

    include localDatastoreHelper test config

--- src/test/java/teammates/test/BaseTestCaseWithObjectifyAccess.java
+++ src/test/java/teammates/test/BaseTestCaseWithObjectifyAccess.java
@@ -1,9 +1,15 @@
 package teammates.test;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.testing.LocalDatastoreHelper;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.util.Closeable;
@@ -15,23 +21,35 @@ import teammates.storage.api.OfyHelper;
  * registered for creation of Datastore Key objects used in defining parent-child relationships in entities.
  */
 public abstract class BaseTestCaseWithObjectifyAccess extends BaseTestCaseWithMinimalGaeEnvironment {
+    private static final double DB_CONSISTENCY = 1.0;
+    private static LocalDatastoreHelper localDatastoreHelper;
     private Closeable closeable;
+
+    @BeforeSuite
+    public void setLocalDatastoreHelper() throws IOException, InterruptedException {
+        localDatastoreHelper = LocalDatastoreHelper.create(DB_CONSISTENCY);
+        localDatastoreHelper.start();
+    }
 
     @BeforeClass
     public void setupObjectify() {
+        DatastoreOptions options = localDatastoreHelper.getOptions();
         ObjectifyService.init(new ObjectifyFactory(
-                DatastoreOptions.newBuilder()
-                        .setProjectId("tm-obj-v6-test")
-                        .build()
-                        .getService()
+                options.getService()
         ));
         OfyHelper.registerEntityClasses();
         closeable = ObjectifyService.begin();
     }
 
     @AfterClass
-    public void tearDownObjectify() {
+    public void tearDownObjectify() throws IOException {
         closeable.close();
+        localDatastoreHelper.reset();
+    }
+
+    @AfterSuite
+    public void tearDownLocalDatastoreHelper() throws InterruptedException, TimeoutException, IOException {
+        localDatastoreHelper.stop();
     }
 
 }
