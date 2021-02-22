808a43c0c8df573be132787ad3f821970b4294e5
teikjun <teikjunhci@gmail.com>
2021-02-22 23:20:25 +0800

    Fix lint issues

--- src/web/app/components/simple-modal/simple-modal.component.spec.ts
+++ src/web/app/components/simple-modal/simple-modal.component.spec.ts
@@ -1,8 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import { SimpleModalComponent } from './simple-modal.component';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProgressBarModule } from '../progress-bar/progress-bar.module';
-
+import { SimpleModalComponent } from './simple-modal.component';
 
 describe('SimpleModalComponent', () => {
   let component: SimpleModalComponent;
