ab7cd88f2eb01d148eaf0411f9463f2714507426
jianhandev <jianhanli.dev@gmail.com>
2021-02-28 12:44:46 +0800

    Add frontend tests for admin search page

--- src/web/app/pages-admin/admin-search-page/admin-search-page.component.spec.ts
+++ src/web/app/pages-admin/admin-search-page/admin-search-page.component.spec.ts
@@ -1,15 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModal, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { of, throwError } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { AccountService } from '../../../services/account.service';
+import { EmailGenerationService } from '../../../services/email-generation.service';
 import {
   FeedbackSessionsGroup, InstructorAccountSearchResult,
   SearchService, StudentAccountSearchResult,
 } from '../../../services/search.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { StudentService } from '../../../services/student.service';
+import { Email } from '../../../types/api-output';
 import { AdminSearchPageComponent } from './admin-search-page.component';
 
 const DEFAULT_FEEDBACK_SESSION_GROUP: FeedbackSessionsGroup = {
@@ -27,6 +30,7 @@ describe('AdminSearchPageComponent', () => {
   let searchService: SearchService;
   let studentService: StudentService;
   let statusMessageService: StatusMessageService;
+  let emailGenerationService: EmailGenerationService;
   let ngbModal: NgbModal;
 
   beforeEach(async(() => {
@@ -36,10 +40,11 @@ describe('AdminSearchPageComponent', () => {
         FormsModule,
         HttpClientTestingModule,
         NgbTooltipModule,
+        BrowserAnimationsModule,
       ],
       providers: [AccountService, SearchService, StatusMessageService, NgbModal],
     })
-    .compileComponents();
+        .compileComponents();
   }));
 
   beforeEach(() => {
@@ -49,12 +54,70 @@ describe('AdminSearchPageComponent', () => {
     searchService = TestBed.inject(SearchService);
     studentService = TestBed.inject(StudentService);
     statusMessageService = TestBed.inject(StatusMessageService);
+    emailGenerationService = TestBed.inject(EmailGenerationService);
     ngbModal = TestBed.inject(NgbModal);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should snap with default fields', (() => {
+    expect(fixture).toMatchSnapshot();
+  }));
+
+  it('should snap with a search key', () => {
+    component.searchQuery = 'TEST';
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with an expanded instructor table', () => {
+    component.instructors = [
+      {
+        name: 'tester',
+        email: 'tester@tester.com',
+        googleId: 'instructor-google-id',
+        courseId: 'test-exa.demo',
+        courseName: 'demo',
+        institute: 'institute',
+        courseJoinLink: 'course-join-link',
+        homePageLink: 'home-page-link',
+        manageAccountLink: 'manage-account-link',
+        showLinks: true,
+      },
+    ];
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with an expanded student table', () => {
+    component.students = [
+      {
+        name: 'Alice Betsy',
+        email: 'alice.b.tmms@gmail.tmt',
+        googleId: 'student-google-id',
+        courseId: 'test-exa.demo',
+        courseName: 'demo',
+        institute: 'institute',
+        courseJoinLink: 'course-join-link',
+        homePageLink: 'home-page-link',
+        manageAccountLink: 'manage-account-link',
+        showLinks: true,
+        section: 'section',
+        team: 'team',
+        comments: 'comments',
+        recordsPageLink: 'records-page-link',
+        openSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+        notOpenSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+        publishedSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      },
+    ];
+
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 
   it('should display error message for invalid input', () => {
@@ -65,9 +128,9 @@ describe('AdminSearchPageComponent', () => {
     }));
 
     const spyStatusMessageService: any = spyOn(statusMessageService, 'showErrorToast').and.callFake(
-      (args: string): void => {
-        expect(args).toEqual('This is the error message');
-      });
+        (args: string): void => {
+          expect(args).toEqual('This is the error message');
+        });
 
     const button: any = fixture.debugElement.nativeElement.querySelector('#search-button');
     button.click();
@@ -82,9 +145,9 @@ describe('AdminSearchPageComponent', () => {
     }));
 
     const spyStatusMessageService: any = spyOn(statusMessageService, 'showWarningToast').and.callFake(
-      (args: string): void => {
-        expect(args).toEqual('No results found.');
-      });
+        (args: string): void => {
+          expect(args).toEqual('No results found.');
+        });
 
     const button: any = fixture.debugElement.nativeElement.querySelector('#search-button');
     button.click();
@@ -267,9 +330,9 @@ describe('AdminSearchPageComponent', () => {
 
     spyOn(accountService, 'resetInstructorAccount').and.returnValue(of('Success'));
     const spyStatusMessageService: any = spyOn(statusMessageService, 'showSuccessToast').and.callFake(
-      (args: string): void => {
-        expect(args).toEqual('The instructor\'s Google ID has been reset.');
-      });
+        (args: string): void => {
+          expect(args).toEqual('The instructor\'s Google ID has been reset.');
+        });
 
     const link: any = fixture.debugElement.nativeElement.querySelector('#reset-instructor-id-0');
     link.click();
@@ -309,9 +372,9 @@ describe('AdminSearchPageComponent', () => {
     }));
 
     const spyStatusMessageService: any = spyOn(statusMessageService, 'showErrorToast').and.callFake(
-      (args: string): void => {
-        expect(args).toEqual('This is the error message');
-      });
+        (args: string): void => {
+          expect(args).toEqual('This is the error message');
+        });
 
     const link: any = fixture.debugElement.nativeElement.querySelector('#reset-instructor-id-0');
     link.click();
@@ -355,9 +418,9 @@ describe('AdminSearchPageComponent', () => {
     spyOn(accountService, 'resetStudentAccount').and.returnValue(of('success'));
 
     const spyStatusMessageService: any = spyOn(statusMessageService, 'showSuccessToast').and.callFake(
-      (args: string): void => {
-        expect(args).toEqual('The student\'s Google ID has been reset.');
-      });
+        (args: string): void => {
+          expect(args).toEqual('The student\'s Google ID has been reset.');
+        });
 
     const link: any = fixture.debugElement.nativeElement.querySelector('#reset-student-id-0');
     link.click();
@@ -405,9 +468,9 @@ describe('AdminSearchPageComponent', () => {
     }));
 
     const spyStatusMessageService: any = spyOn(statusMessageService, 'showErrorToast').and.callFake(
-      (args: string): void => {
-        expect(args).toEqual('This is the error message.');
-      });
+        (args: string): void => {
+          expect(args).toEqual('This is the error message.');
+        });
 
     const link: any = fixture.debugElement.nativeElement.querySelector('#reset-student-id-0');
     link.click();
@@ -555,4 +618,179 @@ describe('AdminSearchPageComponent', () => {
     expect(spyStatusMessageService).toBeCalled();
   });
 
+  it('should generate email when course join email button clicked', () => {
+    const studentResult: StudentAccountSearchResult = {
+      name: 'jack',
+      email: 'email@test.com',
+      googleId: 'googleId',
+      courseId: 'courseId',
+      courseName: 'courseName',
+      institute: 'institute',
+      courseJoinLink: 'courseJoinLink',
+      homePageLink: 'homePageLink',
+      manageAccountLink: 'manageAccountLink',
+      showLinks: true,
+      section: 'section',
+      team: 'team',
+      comments: 'comments',
+      recordsPageLink: 'recordsPageLink',
+      openSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      notOpenSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      publishedSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+    };
+    component.students = [studentResult];
+    fixture.detectChanges();
+
+    const spyEmailGenerationService: any = spyOn(emailGenerationService, 'getCourseJoinEmail').and.callFake(
+        (): Observable<Email> => of({
+          recipient: 'Jacky Chan',
+          subject: 'Course join email',
+          content: 'Course join email content',
+        }),
+    );
+
+    const sendButton: any = fixture.debugElement.nativeElement.querySelector('#send-course-join-button');
+    sendButton.click();
+
+    expect(spyEmailGenerationService).toBeCalled();
+  });
+
+  it('should show error message if fail to send course join email', () => {
+    const studentResult: StudentAccountSearchResult = {
+      name: 'name',
+      email: 'email',
+      googleId: 'googleId',
+      courseId: 'courseId',
+      courseName: 'courseName',
+      institute: 'institute',
+      courseJoinLink: 'courseJoinLink',
+      homePageLink: 'homePageLink',
+      manageAccountLink: 'manageAccountLink',
+      showLinks: true,
+      section: 'section',
+      team: 'team',
+      comments: 'comments',
+      recordsPageLink: 'recordsPageLink',
+      openSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      notOpenSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      publishedSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+    };
+    component.students = [studentResult];
+    fixture.detectChanges();
+
+    spyOn(emailGenerationService, 'getCourseJoinEmail').and.returnValue(throwError({
+      error: {
+        message: 'This is the error message.',
+      },
+    }));
+
+    const spyStatusMessageService: any = spyOn(statusMessageService, 'showErrorToast').and.callFake(
+        (args: string): void => {
+          expect(args).toEqual('This is the error message.');
+        });
+
+    const sendButton: any = fixture.debugElement.nativeElement.querySelector('#send-course-join-button');
+    sendButton.click();
+
+    expect(spyStatusMessageService).toBeCalled();
+  });
+
+  it('should generate email when send session reminder email button clicked', () => {
+    const studentResult: StudentAccountSearchResult = {
+      name: 'Jack Chan',
+      email: 'email@test.com',
+      googleId: 'googleId',
+      courseId: 'courseId',
+      courseName: 'courseName',
+      institute: 'institute',
+      courseJoinLink: 'courseJoinLink',
+      homePageLink: 'homePageLink',
+      manageAccountLink: 'manageAccountLink',
+      showLinks: true,
+      section: 'section',
+      team: 'team',
+      comments: 'comments',
+      recordsPageLink: 'recordsPageLink',
+      openSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      notOpenSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      publishedSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+    };
+    component.students = [studentResult];
+    fixture.detectChanges();
+
+    const spyEmailGenerationService: any = spyOn(emailGenerationService, 'getFeedbackSessionReminderEmail')
+        .and.callFake(
+            (): Observable<Email> => of({
+              recipient: 'Jacky Chan',
+              subject: 'Feedback reminder email',
+              content: 'Feedback reminder email content',
+            }),
+        );
+
+    const sendOpenSessionReminderButton: any = fixture.debugElement.nativeElement.querySelector('#send-open-session-reminder-button');
+    sendOpenSessionReminderButton.click();
+
+    expect(spyEmailGenerationService).toBeCalled();
+
+    const sendNotOpenSessionReminderButton: any = fixture.debugElement.nativeElement.querySelector('#send-not-open-session-reminder-button');
+    sendNotOpenSessionReminderButton.click();
+
+    expect(spyEmailGenerationService).toBeCalled();
+
+    const sendPublishedSessionReminderButton: any = fixture.debugElement.nativeElement.querySelector('#send-published-session-reminder-button');
+    sendPublishedSessionReminderButton.click();
+
+    expect(spyEmailGenerationService).toBeCalled();
+
+  });
+
+  it('should show error message if fail to send session reminder email', () => {
+    const studentResult: StudentAccountSearchResult = {
+      name: 'name',
+      email: 'email',
+      googleId: 'googleId',
+      courseId: 'courseId',
+      courseName: 'courseName',
+      institute: 'institute',
+      courseJoinLink: 'courseJoinLink',
+      homePageLink: 'homePageLink',
+      manageAccountLink: 'manageAccountLink',
+      showLinks: true,
+      section: 'section',
+      team: 'team',
+      comments: 'comments',
+      recordsPageLink: 'recordsPageLink',
+      openSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      notOpenSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+      publishedSessions: DEFAULT_FEEDBACK_SESSION_GROUP,
+    };
+    component.students = [studentResult];
+    fixture.detectChanges();
+
+    spyOn(emailGenerationService, 'getFeedbackSessionReminderEmail').and.returnValue(throwError({
+      error: {
+        message: 'This is the error message.',
+      },
+    }));
+
+    const spyStatusMessageService: any = spyOn(statusMessageService, 'showErrorToast').and.callFake(
+        (args: string): void => {
+          expect(args).toEqual('This is the error message.');
+        });
+
+    const sendOpenSessionReminderButton: any = fixture.debugElement.nativeElement.querySelector('#send-open-session-reminder-button');
+    sendOpenSessionReminderButton.click();
+
+    expect(spyStatusMessageService).toBeCalled();
+
+    const sendNotOpenSessionReminderButton: any = fixture.debugElement.nativeElement.querySelector('#send-not-open-session-reminder-button');
+    sendNotOpenSessionReminderButton.click();
+
+    expect(spyStatusMessageService).toBeCalled();
+
+    const sendPublishedSessionReminderButton: any = fixture.debugElement.nativeElement.querySelector('#send-published-session-reminder-button');
+    sendPublishedSessionReminderButton.click();
+
+    expect(spyStatusMessageService).toBeCalled();
+  });
 });
