d435c91867e8e9a62e9dfe5ca9cfdd66e806b936
halfwhole <41856541+halfwhole@users.noreply.github.com>
2021-03-03 10:03:31 +0800

    [#10950] Add logging endpoint for audit logs (#10985)
    * Add logging endpoint for audit logs
    
    * Remove commented-out logic and shift constants into Const.java
    
    * Change authentication level for audit logs tracking

---
+++ src/main/java/teammates/ui/webapi/CreateFeedbackSessionLogAction.java
@@ -0,0 +1,23 @@
+package teammates.ui.webapi;
+
+/**
+ * Action: creates a feedback session log for the purposes of tracking and auditing.
+ */
+class CreateFeedbackSessionLogAction extends BasicFeedbackSubmissionAction {
+
+    @Override
+    AuthType getMinAuthLevel() {
+        return AuthType.PUBLIC;
+    }
+
+    @Override
+    void checkSpecificAccessControl() {
+        // No specific access control restrictions on creating feedback session logs
+    }
+
+    @Override
+    JsonResult execute() {
+        // TODO: implement the logic for creating a feedback session log
+        return new JsonResult("Successful");
+    }
+}
