4242f231a9eaf5e95e076b5bcbb2ba32d77c0719
Adithya Narayan Rangarajan Sreenivasan <e0426343@u.nus.edu>
2021-03-04 17:05:05 +0800

    Fix test case for log service

--- src/web/services/log.service.spec.ts
+++ src/web/services/log.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LogService } from './log.service';
+import { HttpRequestService } from './http-request.service';
 
 describe('LogService', () => {
   let service: LogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpRequestService },
+      ],
+    });
     service = TestBed.inject(LogService);
   });
 
