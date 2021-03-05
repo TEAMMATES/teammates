c35f79cc897022eba16233a49cc33f5dc0cff934
dao ngoc hieu <53283766+daongochieu2810@users.noreply.github.com>
2021-03-05 22:21:55 +0800

    [#10979] Highlight today date and add today button (#10980)
    * Highlight today date and add today button
    
    * Move today button to footer
    
    * Fix hidden dates issue
    
    * Refactor code
    
    * Remove day template
    
    * Fix css style
    
    * Refactor template name
    
    Co-authored-by: Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>

--- src/web/app/components/session-edit-form/session-edit-form.component.ts
+++ src/web/app/components/session-edit-form/session-edit-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import moment from 'moment-timezone';
 import { TemplateSession } from '../../../services/feedback-sessions.service';
 import { SimpleModalService } from '../../../services/simple-modal.service';
@@ -106,7 +106,7 @@ export class SessionEditFormComponent implements OnInit {
   @Output()
   closeEditFormEvent: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private simpleModalService: SimpleModalService) { }
+  constructor(private simpleModalService: SimpleModalService, public calendar: NgbCalendar) { }
 
   ngOnInit(): void {
   }
