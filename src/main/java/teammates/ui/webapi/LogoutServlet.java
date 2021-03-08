1fa0eefc6f6460d891e98f0fe0ba80cfe61336aa
Wilson Kurniawan <wkurniawan.92@gmail.com>
2021-03-08 21:07:48 +0800

    [#11018] Implement Google OAuth 2.0 to replace Users API (#11073)

--- src/main/java/teammates/ui/webapi/LogoutServlet.java
+++ src/main/java/teammates/ui/webapi/LogoutServlet.java
@@ -2,26 +2,28 @@ package teammates.ui.webapi;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import teammates.logic.api.UserProvision;
 
 /**
  * Servlet that handles logout.
  */
 @SuppressWarnings("serial")
-public class LogoutServlet extends HttpServlet {
+public class LogoutServlet extends AuthServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.getSession().invalidate();
+
+        Cookie cookie = getLoginInvalidationCookie();
+        resp.addCookie(cookie);
+
         String frontendUrl = req.getParameter("frontendUrl");
         if (frontendUrl == null) {
             frontendUrl = "";
         }
-        String logoutUrl = new UserProvision().getLogoutUrl(frontendUrl + "/web");
-        resp.sendRedirect(logoutUrl);
+        resp.sendRedirect(frontendUrl + "/web");
     }
 
 }
