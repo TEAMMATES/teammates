c2b7c69c146b74d400d82f8d66d3cc09dde72502
Wilson Kurniawan <wkurniawan.92@gmail.com>
2021-03-12 01:23:05 +0800

    Remove single thread restriction for E2E tests in production server

--- src/e2e/java/teammates/e2e/cases/BaseE2ETestCase.java
+++ src/e2e/java/teammates/e2e/cases/BaseE2ETestCase.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
 
 import teammates.common.datatransfer.DataBundle;
 import teammates.common.datatransfer.UserInfoCookie;
@@ -48,18 +47,9 @@ import teammates.test.FileHelper;
 public abstract class BaseE2ETestCase extends BaseTestCaseWithDatastoreAccess {
 
     static final BackDoor BACKDOOR = BackDoor.getInstance();
-    private static Browser sharedBrowser;
 
     protected DataBundle testData;
     private Browser browser;
-
-    @BeforeSuite
-    protected void determineEnvironment(ITestContext context) {
-        if (!TestProperties.isDevServer()) {
-            // If testing against production server, run in single thread only
-            context.getSuite().getXmlSuite().setThreadCount(1);
-        }
-    }
 
     @BeforeClass
     public void baseClassSetup() throws Exception {
@@ -68,16 +58,7 @@ public abstract class BaseE2ETestCase extends BaseTestCaseWithDatastoreAccess {
     }
 
     protected void prepareBrowser() {
-        if (TestProperties.isDevServer()) {
-            browser = new Browser();
-        } else {
-            // As the tests are run in single thread, in order to reduce the time wasted on browser setup/teardown,
-            // use a single browser instance for all tests in the suite
-            if (sharedBrowser == null) {
-                sharedBrowser = new Browser();
-            }
-            browser = sharedBrowser;
-        }
+        browser = new Browser();
     }
 
     protected abstract void prepareTestData() throws Exception;
@@ -103,9 +84,6 @@ public abstract class BaseE2ETestCase extends BaseTestCaseWithDatastoreAccess {
 
     protected void releaseBrowser(boolean isSuccess) {
         if (browser == null) {
-            return;
-        }
-        if (!TestProperties.isDevServer()) {
             return;
         }
         if (isSuccess || TestProperties.CLOSE_BROWSER_ON_FAILURE) {
