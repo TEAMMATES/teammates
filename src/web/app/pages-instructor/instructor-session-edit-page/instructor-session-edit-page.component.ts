625d6ac9cf2c8ef74549dbb96a333602f6b86207
jfrairigh <jdfike0@gmail.com>
2021-03-12 00:51:02 -0600

    fixing revert part 2

--- src/web/app/pages-instructor/instructor-session-edit-page/instructor-session-edit-page.component.ts
+++ src/web/app/pages-instructor/instructor-session-edit-page/instructor-session-edit-page.component.ts
@@ -447,7 +447,8 @@ export class InstructorSessionEditPageComponent extends InstructorSessionBasePag
    */
   deleteExistingSessionHandler(): void {
     this.sessionEditFormModel.isDeleting = true;
-    this.feedbackSessionsService.moveSessionToRecycleBin(this.courseId, this.feedbackSessionName).pipe(finalize(() => this.sessionEditFormModel.isDeleting = false))
+    this.feedbackSessionsService.moveSessionToRecycleBin(this.courseId, this.feedbackSessionName)
+     .pipe(finalize(() => this.sessionEditFormModel.isDeleting = false))
      .subscribe(() => {
        this.navigationService.navigateWithSuccessMessage(this.router, '/web/instructor/sessions',
       'The feedback session has been deleted. You can restore it from the deleted sessions table below.');
