7a8d2e37c17af2a2367de5b12c231d4edbeede4e
Wilson Kurniawan <wkurniawan.92@gmail.com>
2021-03-29 12:52:02 +0800

    [#11020] Make search service optional and remove App Engine Search API usage (#11067)

---
+++ src/main/java/teammates/storage/search/SearchManagerFactory.java
@@ -0,0 +1,28 @@
+package teammates.storage.search;
+
+/**
+ * Factory that returns search manager implementation.
+ */
+public final class SearchManagerFactory {
+
+    private static SearchManager instance;
+
+    private SearchManagerFactory() {
+        // prevents initialization
+    }
+
+    public static SearchManager getSearchManager() {
+        return instance;
+    }
+
+    /**
+     * Registers the search service into the factory.
+     */
+    @SuppressWarnings("PMD.NonThreadSafeSingleton") // ok to ignore as method is only invoked at application startup
+    public static void registerSearchManager(SearchManager searchManager) {
+        if (instance == null) {
+            instance = searchManager;
+        }
+    }
+
+}
