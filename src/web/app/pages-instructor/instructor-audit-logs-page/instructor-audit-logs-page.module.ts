a26c595e7f863bffbdfde47e1b1d6bacf6145d32
t-cheepeng <tcp97@hotmail.com>
2021-03-29 17:28:54 +0800

    [#10969] Instructor Audit Logs Frontend (#11047)
    * Add method to fetch logs
    
    * Add logs results table
    
    * Add in disabled search button and optional params
    
    * Restrict datepicker to log retention period
    
    * Reword 30 day limit message
    
    * Add backend validation for session logs time param
    
    * Shift click event to select tag
    
    * Change click to ngModelChange and remove unnecessary checks
    
    * Hide navbar link for instructor logs page

--- src/web/app/pages-instructor/instructor-audit-logs-page/instructor-audit-logs-page.module.ts
+++ src/web/app/pages-instructor/instructor-audit-logs-page/instructor-audit-logs-page.module.ts
@@ -4,7 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbDatepickerModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
+import { PanelChevronModule } from '../../components/panel-chevron/panel-chevron.module';
 import { SessionEditFormModule } from '../../components/session-edit-form/session-edit-form.module';
+import { SortableTableModule } from '../../components/sortable-table/sortable-table.module';
 import { InstructorAuditLogsPageComponent } from './instructor-audit-logs-page.component';
 
 const routes: Routes = [
@@ -28,6 +30,8 @@ const routes: Routes = [
     SessionEditFormModule,
     NgbDropdownModule,
     LoadingSpinnerModule,
+    PanelChevronModule,
+    SortableTableModule,
   ],
 })
 export class InstructorAuditLogsPageModule { }
