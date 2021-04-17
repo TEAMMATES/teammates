e8f2a65f3b9bdd7f595c645fe7a29a4fbca3a203
Wilson Kurniawan <wkurniawan.92@gmail.com>
2021-04-18 07:58:42 +0800

    Replace DeadlineExceededException with self-maintained timer

--- src/main/java/teammates/common/util/RequestTracer.java
+++ src/main/java/teammates/common/util/RequestTracer.java
@@ -1,5 +1,8 @@
 package teammates.common.util;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.apache.commons.lang3.RandomStringUtils;
 
 /**
@@ -25,21 +28,47 @@ public final class RequestTracer {
     }
 
     /**
-     * Initializes the request with an ID.
+     * Returns the remaining time (in millis) until the current request times out.
      */
-    public static void init(String requestIdParam) {
+    public static long getRemainingTimeMillis() {
+        RequestTrace trace = THREAD_LOCAL.get();
+        if (trace == null) {
+            return -1L;
+        }
+        return trace.timeoutTimestamp - Instant.now().toEpochMilli();
+    }
+
+    /**
+     * Returns the remaining time (in millis) until the current request times out.
+     */
+    public static long getTimeElapsedMillis() {
+        RequestTrace trace = THREAD_LOCAL.get();
+        if (trace == null) {
+            return -1L;
+        }
+        return Instant.now().toEpochMilli() - trace.initTimestamp;
+    }
+
+    /**
+     * Initializes the request with an ID and the timeout value (in seconds).
+     */
+    public static void init(String requestIdParam, int timeoutInSeconds) {
         String requestId = requestIdParam;
         if (requestId == null) {
             requestId = RandomStringUtils.randomAlphanumeric(32);
         }
-        THREAD_LOCAL.set(new RequestTrace(requestId));
+        THREAD_LOCAL.set(new RequestTrace(requestId, timeoutInSeconds));
     }
 
     private static class RequestTrace {
         private final String requestId;
+        private final long initTimestamp;
+        private final long timeoutTimestamp;
 
-        private RequestTrace(String requestId) {
+        private RequestTrace(String requestId, int timeoutInSeconds) {
             this.requestId = requestId;
+            this.initTimestamp = Instant.now().toEpochMilli();
+            this.timeoutTimestamp = Instant.now().plus(timeoutInSeconds, ChronoUnit.SECONDS).toEpochMilli();
         }
     }
 
--- src/main/java/teammates/ui/webapi/RequestTraceFilter.java
+++ src/main/java/teammates/ui/webapi/RequestTraceFilter.java
@@ -1,15 +1,25 @@
 package teammates.ui.webapi;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpStatus;
+
+import teammates.common.exception.TeammatesException;
+import teammates.common.util.Logger;
 import teammates.common.util.RequestTracer;
 
 /**
@@ -17,23 +27,62 @@ import teammates.common.util.RequestTracer;
  */
 public class RequestTraceFilter implements Filter {
 
+    private static final Logger log = Logger.getLogger();
+
     @Override
     public void init(FilterConfig filterConfig) {
         // nothing to do
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException {
         HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
 
-        RequestTracer.init(request.getHeader("X-Cloud-Trace-Context"));
+        // The header X-AppEngine-QueueName cannot be spoofed as GAE will strip any user-sent X-AppEngine-QueueName headers.
+        // Reference: https://cloud.google.com/appengine/docs/standard/java/taskqueue/push/creating-handlers#reading_request_headers
+        boolean isRequestFromAppEngineQueue = request.getHeader("X-AppEngine-QueueName") != null;
 
-        chain.doFilter(req, resp);
+        // GAE will terminate an instance if any request exceeds 10 minutes.
+        // For GAE-invoked requests, we set the limit here minus a small grace period of 5 seconds
+        // to ensure that the 10 minutes limit will not be exceeded.
+        // For user-invoked requests, we keep the time limit at 1 minute (as how it was
+        // in the previous GAE runtime environment) in order to not let user wait for excessively long,
+        // as well as a reminder for us to keep optimizing our API response time.
+        int timeoutInSeconds = isRequestFromAppEngineQueue ? 10 * 60 - 5 : 60;
+
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        Future<Void> f = es.submit(() -> {
+            RequestTracer.init(request.getHeader("X-Cloud-Trace-Context"), timeoutInSeconds);
+            chain.doFilter(req, resp);
+            return null;
+        });
+
+        try {
+            f.get(timeoutInSeconds, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException e) {
+            log.severe(e.getClass().getSimpleName() + " caught by WebApiServlet: "
+                    + TeammatesException.toStringWithStackTrace(e));
+            throwError(response, HttpStatus.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (TimeoutException e) {
+            log.severe("TimeoutException caught by WebApiServlet: "
+                    + TeammatesException.toStringWithStackTrace(e));
+            throwError(response, HttpStatus.SC_GATEWAY_TIMEOUT,
+                    "The request exceeded the server timeout limit. Please try again later.");
+        } finally {
+            f.cancel(true);
+            es.shutdown();
+        }
     }
 
     @Override
     public void destroy() {
         // nothing to do
+    }
+
+    private void throwError(HttpServletResponse resp, int statusCode, String message) throws IOException {
+        JsonResult result = new JsonResult(message, statusCode);
+        result.send(resp);
     }
 
 }
