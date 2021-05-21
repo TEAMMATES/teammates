d4dc33a450ac25921a5ce311c3f40619a68d18ca
Adithya Narayan Rangarajan Sreenivasan <e0426343@u.nus.edu>
2021-05-21 23:13:21 +0800

    Add test for instructor creation

--- src/web/app/pages-instructor/instructor-course-edit-page/instructor-course-edit-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-course-edit-page/instructor-course-edit-page.component.spec.ts
@@ -1,21 +1,27 @@
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {FormsModule} from '@angular/forms';
-import {RouterTestingModule} from '@angular/router/testing';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {of} from 'rxjs';
-import {CourseService} from '../../../services/course.service';
-import {Course, Instructor, JoinState} from '../../../types/api-output';
-import {AjaxLoadingModule} from '../../components/ajax-loading/ajax-loading.module';
-import {LoadingRetryModule} from '../../components/loading-retry/loading-retry.module';
-import {LoadingSpinnerModule} from '../../components/loading-spinner/loading-spinner.module';
-import {TeammatesCommonModule} from '../../components/teammates-common/teammates-common.module';
-import {TeammatesRouterModule} from '../../components/teammates-router/teammates-router.module';
-import {CustomPrivilegeSettingPanelComponent,} from './custom-privilege-setting-panel/custom-privilege-setting-panel.component';
-import {InstructorCourseEditPageComponent} from './instructor-course-edit-page.component';
-import {InstructorEditPanelComponent,} from './instructor-edit-panel/instructor-edit-panel.component';
-import {ViewRolePrivilegesModalComponent} from './view-role-privileges-modal/view-role-privileges-modal.component';
-import {InstructorService} from "../../../services/instructor.service";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
+import { CourseService } from '../../../services/course.service';
+import { InstructorService } from '../../../services/instructor.service';
+import { Course, Instructor, InstructorPermissionRole, JoinState } from '../../../types/api-output';
+import { InstructorCreateRequest } from '../../../types/api-request';
+import { AjaxLoadingModule } from '../../components/ajax-loading/ajax-loading.module';
+import { LoadingRetryModule } from '../../components/loading-retry/loading-retry.module';
+import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
+import { SimpleModalModule } from '../../components/simple-modal/simple-modal.module';
+import { TeammatesCommonModule } from '../../components/teammates-common/teammates-common.module';
+import { TeammatesRouterModule } from '../../components/teammates-router/teammates-router.module';
+import { CustomPrivilegeSettingPanelComponent } from './custom-privilege-setting-panel/custom-privilege-setting-panel.component';
+import { InstructorCourseEditPageComponent } from './instructor-course-edit-page.component';
+import {
+    InstructorEditPanel,
+    InstructorEditPanelComponent,
+} from './instructor-edit-panel/instructor-edit-panel.component';
+import { ViewRolePrivilegesModalComponent } from './view-role-privileges-modal/view-role-privileges-modal.component';
 
 const testCourse: Course = {
   courseId: 'exampleId',
@@ -37,6 +43,41 @@ const testInstructor2: Instructor = {
   email: 'instructor2@gmail.com',
   joinState: JoinState.JOINED,
   name: 'Instructor 2',
+};
+
+const testInstructor3: Instructor = {
+  courseId: 'exampleId',
+  email: 'instructor3@gmail.com',
+  joinState: JoinState.NOT_JOINED,
+  name: 'Instructor 3',
+};
+
+const emptyInstructorPanel: InstructorEditPanel = {
+  googleId: '',
+  courseId: '',
+  email: '',
+  isDisplayedToStudents: true,
+  displayedToStudentsAs: '',
+  name: '',
+  role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_COOWNER,
+  joinState: JoinState.NOT_JOINED,
+
+  permission: {
+    privilege: {
+      canModifyCourse: true,
+      canModifySession: true,
+      canModifyStudent: true,
+      canModifyInstructor: true,
+      canViewStudentInSections: true,
+      canModifySessionCommentsInSections: true,
+      canViewSessionInSections: true,
+      canSubmitSessionInSections: true,
+    },
+    sectionLevel: [],
+  },
+
+  isEditing: true,
+  isSavingInstructorEdit: false,
 };
 
 describe('InstructorCourseEditPageComponent', () => {
@@ -63,6 +104,8 @@ describe('InstructorCourseEditPageComponent', () => {
         LoadingSpinnerModule,
         LoadingRetryModule,
         TeammatesRouterModule,
+        SimpleModalModule,
+        BrowserAnimationsModule,
       ],
     })
     .compileComponents();
@@ -146,7 +189,7 @@ describe('InstructorCourseEditPageComponent', () => {
     expect(component.isInstructorsLoading).toBeFalsy();
   });
 
-  it('should re-order if instructor is deleted', () => {
+  it('should not add instructor if CANCEL is requested', () => {
     component.course = testCourse;
     component.isCourseLoading = false;
     component.instructorDetailPanels = [
@@ -161,19 +204,86 @@ describe('InstructorCourseEditPageComponent', () => {
         editPanel: component.getInstructorEditPanelModel(testInstructor2),
       },
     ];
+    component.isAddingNewInstructor = true;
+    component.newInstructorPanel = component.getInstructorEditPanelModel(testInstructor3);
+    component.newInstructorPanel.isEditing = true;
     fixture.detectChanges();
 
-    component.deleteInstructor(1);
-    fixture.detectChanges();
-
-    const button: any = fixture.debugElement.nativeElement.querySelector('#btn-save-course');
+    const button: any = fixture.debugElement.nativeElement
+        .querySelector(`#btn-cancel-instructor-${ component.instructorDetailPanels.length + 1 }`);
     button.click();
 
+    expect(component.isAddingNewInstructor).toBeFalsy();
+  });
 
+  it('should add instructor details', () => {
+    spyOn(instructorService, 'createInstructor').and
+      .callFake(({ courseId, requestBody }: { courseId: string, requestBody: InstructorEditPanel }) => of({
+        courseId,
+        email: requestBody.email,
+        joinState: JoinState.NOT_JOINED,
+        name: requestBody.name,
+      }));
 
+    component.course = testCourse;
+    component.courseId = testCourse.courseId;
+    component.isCourseLoading = false;
+    component.instructorDetailPanels = [
+      {
+        originalInstructor: Object.assign({}, testInstructor1),
+        originalPanel: component.getInstructorEditPanelModel(testInstructor1),
+        editPanel: component.getInstructorEditPanelModel(testInstructor1),
+      },
+      {
+        originalInstructor: Object.assign({}, testInstructor2),
+        originalPanel: component.getInstructorEditPanelModel(testInstructor2),
+        editPanel: component.getInstructorEditPanelModel(testInstructor2),
+      },
+    ];
+    component.isAddingNewInstructor = true;
+    component.newInstructorPanel = component.getInstructorEditPanelModel(testInstructor3);
+    component.newInstructorPanel.isEditing = true;
+    fixture.detectChanges();
 
+    const button: any = fixture.debugElement.nativeElement
+        .querySelector(`#btn-save-instructor-${ component.instructorDetailPanels.length + 1 }`);
+    button.click();
 
+    expect(component.isAddingNewInstructor).toBeFalsy();
+    expect(component.isSavingNewInstructor).toBeFalsy();
+    expect(component.instructorDetailPanels.length).toBe(3);
+    expect(component.instructorDetailPanels[2].originalInstructor).toEqual(testInstructor3);
+    expect(component.newInstructorPanel).toEqual(emptyInstructorPanel);
+  });
 
+  it('should re-order if instructor is deleted', () => {
+    spyOn(instructorService, 'deleteInstructor').and.returnValue(of({}));
+
+    component.course = testCourse;
+    component.isCourseLoading = false;
+    component.instructorDetailPanels = [
+      {
+        originalInstructor: Object.assign({}, testInstructor1),
+        originalPanel: component.getInstructorEditPanelModel(testInstructor1),
+        editPanel: component.getInstructorEditPanelModel(testInstructor1),
+      },
+      {
+        originalInstructor: Object.assign({}, testInstructor2),
+        originalPanel: component.getInstructorEditPanelModel(testInstructor2),
+        editPanel: component.getInstructorEditPanelModel(testInstructor2),
+      },
+    ];
+
+    component.deleteInstructor(0);
+    fixture.detectChanges();
+
+    // using document instead of fixture as modal gets added into the dom outside the viewRef
+    const button: any = document.getElementsByClassName('modal-btn-ok').item(0);
+    button.click();
+    fixture.detectChanges();
+
+    expect(component.instructorDetailPanels.length).toBe(1);
+    expect(component.instructorDetailPanels[0].originalInstructor).toEqual(testInstructor2);
   });
 
   it('should snap with default fields', () => {
