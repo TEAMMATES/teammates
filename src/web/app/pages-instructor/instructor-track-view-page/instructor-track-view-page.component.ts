b46fc6dc5c7820b3e06f426f0f5f42838d7aac08
ypinhsuan <yenpinhsuan@gmail.com>
2021-05-27 12:00:59 +0800

    Add dialog for feedback session published more than 30 days ago

--- src/web/app/pages-instructor/instructor-track-view-page/instructor-track-view-page.component.ts
+++ src/web/app/pages-instructor/instructor-track-view-page/instructor-track-view-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateParserFormatter, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import moment from 'moment-timezone';
 import { forkJoin, Observable } from 'rxjs';
 import { concatMap, finalize, map, mergeAll } from 'rxjs/operators';
-import { FeedbackSessionsService } from 'src/web/services/feedback-sessions.service';
 import { CourseService } from '../../../services/course.service';
+import { FeedbackSessionsService } from '../../../services/feedback-sessions.service';
 import { LogService } from '../../../services/log.service';
+import { SimpleModalService } from '../../../services/simple-modal.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { StudentService } from '../../../services/student.service';
 import { LOCAL_DATE_TIME_FORMAT, TimeResolvingResult, TimezoneService } from '../../../services/timezone.service';
 import { ApiConst } from '../../../types/api-const';
+import { Intent } from '../../../types/api-request';
 import {
   Course,
   Courses,
@@ -17,6 +19,7 @@ import {
   FeedbackSessionLog,
   FeedbackSessionLogEntry,
   FeedbackSessionLogs, 
+  FeedbackSessionPublishStatus, 
   FeedbackSessions, 
   LogType,
   Student,
@@ -28,13 +31,14 @@ import { DateFormat } from '../../components/session-edit-form/session-edit-form
 import { TimeFormat } from '../../components/session-edit-form/time-picker/time-picker.component';
 import { ColumnData, SortableTableCellData } from '../../components/sortable-table/sortable-table.component';
 import { ErrorMessageOutput } from '../../error-message-output';
+import { SimpleModalType } from '../../components/simple-modal/simple-modal-type';
 
 /**
  * Model for searching of logs
  */
  interface SearchLogsFormModel {
   courseId: string;
-  sessionName: string;
+  feedbackSessionName: string;
   logsDateFrom: DateFormat;
   logsDateTo: DateFormat;
   logsTimeFrom: TimeFormat;
@@ -48,6 +52,7 @@ interface FeedbackSessionLogModel {
   courseId: string;
   feedbackSessionName: string;
   publishedTime: string;
+  publishStatus: boolean;
   logColumnsData: ColumnData[];
   logRowsData: SortableTableCellData[][];
 }
@@ -70,7 +75,7 @@ export class InstructorTrackViewPageComponent implements OnInit {
     logsDateTo: { year: 0, month: 0, day: 0 },
     logsTimeTo: { hour: 23, minute: 59 },
     courseId: '',
-    sessionName: '',
+    feedbackSessionName: '',
   };
   courses: Course[] = [];
   courseToFeedbackSession: Record<string, FeedbackSession[]> = {};
@@ -78,6 +83,7 @@ export class InstructorTrackViewPageComponent implements OnInit {
     courseId: '',
     feedbackSessionName: '',
     publishedTime: '',
+    publishStatus: false,
     logColumnsData: [],
     logRowsData: [],
   };
@@ -86,26 +92,18 @@ export class InstructorTrackViewPageComponent implements OnInit {
   isLoading: boolean = true;
   isSearching: boolean = false;
   hasResult: boolean = false;
+  publishedTime: number = 0;
+  isPublished: boolean = false;
 
   constructor(private courseService: CourseService,
     private feedbackSessionsService: FeedbackSessionsService,
     private logsService: LogService,
+    private simpleModalService: SimpleModalService,
     private statusMessageService: StatusMessageService,
     private studentService: StudentService,
     private timezoneService: TimezoneService) { }
 
   ngOnInit(): void {
-    const today: Date = new Date();
-
-    this.formModel.logsDateTo.year = today.getFullYear();
-    this.formModel.logsDateTo.month = today.getMonth() + 1;
-    this.formModel.logsDateTo.day = today.getDate();
-
-    const earliestSearchDate: Date = new Date(Date.now() - this.LOGS_RETENTION_PERIOD * 24 * 60 * 60 * 1000);
-    this.formModel.logsDateFrom.year = earliestSearchDate.getFullYear();
-    this.formModel.logsDateFrom.month = earliestSearchDate.getMonth() + 1;
-    this.formModel.logsDateFrom.day = earliestSearchDate.getDate();
-
     this.loadData();
   }
 
@@ -128,12 +126,62 @@ export class InstructorTrackViewPageComponent implements OnInit {
   }
 
   /**
+   * Get the selected feedback session
+   */
+  getFeedbackSession(): void {
+    this.isSearching = true;
+
+    // Get the publish status and time of feedback session
+    this.feedbackSessionsService
+      .getFeedbackSession({
+        courseId: this.formModel.courseId,
+        feedbackSessionName: this.formModel.feedbackSessionName,
+        intent: Intent.INSTRUCTOR_RESULT,
+      })
+      .subscribe((feedbackSession: FeedbackSession) => {
+        this.isPublished = feedbackSession.publishStatus === FeedbackSessionPublishStatus.PUBLISHED;
+        if (feedbackSession.resultVisibleFromTimestamp) {
+          this.publishedTime = feedbackSession.resultVisibleFromTimestamp;
+        }
+
+        if (!this.isPublished) {
+          // Feedback session is not published, do not need to search.
+          this.emptyResult();
+        } else if (this.publishedTime < Date.now() - this.LOGS_RETENTION_PERIOD * 24 * 60 * 60 * 1000) {
+          // Feedback session is published more than 30 days ago, open a dialog.
+          this.openModal();
+        } else {
+          // Published time of the feedback session is less than 30 days ago, update the form model to
+          // search only up till the publish date.
+          const publishedDate = new Date(this.publishedTime);
+          this.formModel.logsDateFrom = {
+            year: publishedDate.getFullYear(),
+            month: publishedDate.getMonth() + 1,
+            day: publishedDate.getDate(),
+          }
+          this.formModel.logsTimeFrom = {
+            hour: publishedDate.getHours(),
+            minute: publishedDate.getMinutes(),
+          }
+          this.search();
+        }
+      })
+  }
+
+  /**
    * Search for logs of student activity
    */
-  search(): void {
-    this.isSearching = true;
+  private search(): void {
     this.students = [];
     this.studentToLog = {};
+    
+    const today: Date = new Date();
+    this.formModel.logsDateTo = {
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      day: today.getDate(),
+    };
+
     const localDateTime: Observable<number>[] = [
       this.resolveLocalDateTime(this.formModel.logsDateFrom, this.formModel.logsTimeFrom, 'Search period from'),
       this.resolveLocalDateTime(this.formModel.logsDateTo, this.formModel.logsTimeTo, 'Search period until'),
@@ -146,7 +194,7 @@ export class InstructorTrackViewPageComponent implements OnInit {
                 courseId: this.formModel.courseId,
                 searchFrom: timestamp[0].toString(),
                 searchUntil: timestamp[1].toString(),
-                sessionName: this.formModel.sessionName,
+                sessionName: this.formModel.feedbackSessionName,
               });
             }),
             finalize(() => {
@@ -161,10 +209,52 @@ export class InstructorTrackViewPageComponent implements OnInit {
 
                 logs.feedbackSessionLogs[0].feedbackSessionLogEntries
                   .filter(entry => LogType[entry.feedbackSessionLogType.toString() as keyof typeof LogType] === LogType.FEEDBACK_SESSION_VIEW)
-                  .map((entry: FeedbackSessionLogEntry) => this.studentToLog[entry.studentData.email] = entry);
+                  .map((entry: FeedbackSessionLogEntry) => {
+                    if (!(entry.studentData.email in this.studentToLog) || this.studentToLog[entry.studentData.email].timestamp < entry.timestamp) {
+                      this.studentToLog[entry.studentData.email] = entry;
+                    }
+                  });
                 this.searchResult = this.toFeedbackSessionLogModel(logs.feedbackSessionLogs[0]);
               })
         }, (e: ErrorMessageOutput) => this.statusMessageService.showErrorToast(e.error.message));
+  }
+
+  /**
+   * Return an empty search result for feedback sessions that is not published
+   */
+  private emptyResult(): void {
+    this.searchResult = {
+      courseId: this.formModel.courseId,
+      feedbackSessionName: this.formModel.feedbackSessionName,
+      publishedTime: '',
+      publishStatus: false,
+      logColumnsData: [],
+      logRowsData: [], 
+    }
+    this.hasResult = true;
+    this.isSearching = false;
+  }
+
+  private openModal(): void {
+    const modalContent: string = 'Published date of selected feedback session is more than 30 days ago. Only activities within the last 30 days will be captured.';
+    const modalRef: NgbModalRef = this.simpleModalService.openConfirmationModal('Continue the operation?', SimpleModalType.NEUTRAL, modalContent);
+    modalRef.result.then(
+      () => {
+        // Allow 5 minute buffer
+        const earliestSearchDate: Date = new Date(Date.now() - this.LOGS_RETENTION_PERIOD * 24 * 60 * 60 * 1000 + 5 * 60 * 1000);
+        this.formModel.logsDateFrom = {
+          year: earliestSearchDate.getFullYear(),
+          month: earliestSearchDate.getMonth() + 1,
+          day: earliestSearchDate.getDate(),
+        };
+        this.formModel.logsTimeFrom = {
+          hour: earliestSearchDate.getHours(),
+          minute: earliestSearchDate.getMinutes(),
+        };
+        this.search();
+      },
+      () => { this.isSearching = false; }
+    );
   }
 
   private resolveLocalDateTime(date: DateFormat, time: TimeFormat, fieldName: string): Observable<number> {
@@ -183,8 +273,9 @@ export class InstructorTrackViewPageComponent implements OnInit {
   private toFeedbackSessionLogModel(log: FeedbackSessionLog): FeedbackSessionLogModel {
     return {
       courseId: this.formModel.courseId,
-      feedbackSessionName: this.formModel.sessionName,
-      publishedTime: '',
+      feedbackSessionName: this.formModel.feedbackSessionName,
+      publishedTime: this.timezoneService.formatToString(this.publishedTime, log.feedbackSessionData.timeZone, 'ddd, DD MMM, YYYY hh:mm:ss A'),
+      publishStatus: this.isPublished,
       logColumnsData: [
         { header: 'Status', sortBy: SortBy.LOG_DATE },
         { header: 'Name', sortBy: SortBy.GIVER_NAME },
@@ -199,7 +290,8 @@ export class InstructorTrackViewPageComponent implements OnInit {
             const entry = this.studentToLog[student.email];
             status = `Viewed last at ${this.timezoneService.formatToString(entry.timestamp, log.feedbackSessionData.timeZone, 'ddd, DD MMM, YYYY hh:mm:ss A')}`;
           } else {
-            status = 'Not viewed since ';
+            const timestamp: number = new Date().getTime();
+            status = `Not viewed since ${this.timezoneService.formatToString(this.publishedTime, log.feedbackSessionData.timeZone, 'ddd, DD MMM, YYYY hh:mm:ss A')}`;
           }
           return [
             { value: status },
