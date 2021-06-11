9ab27092c0b730623b90549432feff88262f1983
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-06-11 15:36:57 +0800

    Add tests for some methods in FeedbackRankRecipientsQuestionDetails class

--- src/test/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetailsTest.java
+++ src/test/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetailsTest.java
@@ -1,13 +1,28 @@
 package teammates.common.datatransfer.questions;
 
+import net.sf.saxon.trans.SymbolicName;
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
 import teammates.test.BaseTestCase;
 
 /**
  * SUT: {@link FeedbackRankRecipientsQuestionDetails}.
  */
 public class FeedbackRankRecipientsQuestionDetailsTest extends BaseTestCase {
+
+    @Test
+    public void testShouldChangesRequireResponseDeletion_shouldReturnFalse() {
+        FeedbackRankRecipientsQuestionDetails feedbackRankRecipientsQuestionDetails = new FeedbackRankRecipientsQuestionDetails();
+        FeedbackQuestionDetails newDetails = new FeedbackRankRecipientsQuestionDetails();
+        assertFalse(feedbackRankRecipientsQuestionDetails.shouldChangesRequireResponseDeletion(newDetails));
+    }
+
+    @Test
+    public void testValidateQuestionDetails_shouldReturnEmptyList() {
+        FeedbackRankRecipientsQuestionDetails feedbackRankRecipientsQuestionDetails = new FeedbackRankRecipientsQuestionDetails();
+        assertTrue(feedbackRankRecipientsQuestionDetails.validateQuestionDetails().isEmpty());
+    }
 
     @Test
     public void testIsInstructorCommentsOnResponsesAllowed_shouldReturnTrue() {
@@ -19,5 +34,12 @@ public class FeedbackRankRecipientsQuestionDetailsTest extends BaseTestCase {
     public void testIsFeedbackParticipantCommentsOnResponsesAllowed_shouldReturnFalse() {
         FeedbackQuestionDetails feedbackQuestionDetails = new FeedbackRankRecipientsQuestionDetails();
         assertFalse(feedbackQuestionDetails.isFeedbackParticipantCommentsOnResponsesAllowed());
+    }
+
+    @Test
+    public void testValidateGiverRecipientVisibility() {
+        FeedbackRankRecipientsQuestionDetails feedbackRankRecipientsQuestionDetails = new FeedbackRankRecipientsQuestionDetails();
+        FeedbackQuestionAttributes feedbackQuestionAttributes = FeedbackQuestionAttributes.builder().build();
+        assertEquals("", feedbackRankRecipientsQuestionDetails.validateGiverRecipientVisibility(feedbackQuestionAttributes));
     }
 }
