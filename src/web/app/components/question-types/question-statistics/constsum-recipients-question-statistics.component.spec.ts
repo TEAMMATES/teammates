d4de88475aa75fae555c0c49d82d34be060b5307
Jellybeano <lanyuxuan98@gmail.com>
2021-07-11 02:03:09 +0800

    Improve test coverage

--- src/web/app/components/question-types/question-statistics/constsum-recipients-question-statistics.component.spec.ts
+++ src/web/app/components/question-types/question-statistics/constsum-recipients-question-statistics.component.spec.ts
@@ -39,7 +39,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'bob@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [2, 8],
+        answers: [2],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -53,7 +53,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'bob@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [3, 7],
+        answers: [3],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -67,7 +67,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'bob@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [5, 5],
+        answers: [5],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -81,7 +81,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'bob@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [5, 5],
+        answers: [5],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -95,7 +95,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'emma@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [9, 1],
+        answers: [9],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -109,7 +109,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'emma@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [6, 4],
+        answers: [6],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -123,7 +123,7 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'emma@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [4, 6],
+        answers: [4],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
@@ -137,27 +137,94 @@ describe('ConstsumRecipientsQuestionStatisticsComponent', () => {
       recipientEmail: 'emma@gmail.com',
       recipientSection: '',
       responseDetails: {
-        answers: [7, 3],
+        answers: [7],
+        questionType: FeedbackQuestionType.CONSTSUM,
+      } as FeedbackConstantSumResponseDetails,
+    },
+    // Fred has self-review and is the only review
+    {
+      giver: 'Fred',
+      giverTeam: 'Team 3',
+      giverEmail: 'fred@gmail.com',
+      giverSection: '',
+      recipient: 'Fred',
+      recipientTeam: 'Team 3',
+      recipientEmail: 'fred@gmail.com',
+      recipientSection: '',
+      responseDetails: {
+        answers: [2],
+        questionType: FeedbackQuestionType.CONSTSUM,
+      } as FeedbackConstantSumResponseDetails,
+    },
+    // Henry has no self-review and has only one review
+    {
+      giver: 'Greg',
+      giverTeam: 'Team 3',
+      giverEmail: 'greg@gmail.com',
+      giverSection: '',
+      recipient: 'Henry',
+      recipientTeam: 'Team 3',
+      recipientEmail: 'henry@gmail.com',
+      recipientSection: '',
+      responseDetails: {
+        answers: [5],
+        questionType: FeedbackQuestionType.CONSTSUM,
+      } as FeedbackConstantSumResponseDetails,
+    },
+    // Greg has no self-review and has more than one review
+    {
+      giver: 'Fred',
+      giverTeam: 'Team 3',
+      giverEmail: 'fred@gmail.com',
+      giverSection: '',
+      recipient: 'Greg',
+      recipientTeam: 'Team 3',
+      recipientEmail: 'greg@gmail.com',
+      recipientSection: '',
+      responseDetails: {
+        answers: [7],
+        questionType: FeedbackQuestionType.CONSTSUM,
+      } as FeedbackConstantSumResponseDetails,
+    },
+    {
+      giver: 'Henry',
+      giverTeam: 'Team 3',
+      giverEmail: 'henry@gmail.com',
+      giverSection: '',
+      recipient: 'Greg',
+      recipientTeam: 'Team 3',
+      recipientEmail: 'greg@gmail.com',
+      recipientSection: '',
+      responseDetails: {
+        answers: [9],
         questionType: FeedbackQuestionType.CONSTSUM,
       } as FeedbackConstantSumResponseDetails,
     },
   ];
 
-  it('should calculate Average Excluding Self correctly', () => {
+  it('should calculate statistics correctly', () => {
 
     component.responses = responses;
 
     const expectedPointsPerOption: Record<string, number[]> = {
       'bob@gmail.com': [2, 3, 5, 5], 'emma@gmail.com': [4, 6, 7, 9],
+      'fred@gmail.com': [2], 'henry@gmail.com': [5],
+      'greg@gmail.com': [7, 9],
     };
     const expectedTotalPointsPerOption: Record<string, number> = {
       'bob@gmail.com': 15, 'emma@gmail.com': 26,
+      'fred@gmail.com': 2, 'henry@gmail.com': 5,
+      'greg@gmail.com': 16,
     };
     const expectedAveragePointsPerOption: Record<string, number> = {
       'bob@gmail.com': 3.75, 'emma@gmail.com': 6.5,
+      'fred@gmail.com': 2, 'henry@gmail.com': 5,
+      'greg@gmail.com': 8,
     };
     const expectedAveragePointsExcludingSelf: Record<string, number> = {
       'bob@gmail.com': 3.33, 'emma@gmail.com': 6.33,
+      'fred@gmail.com': 0, 'henry@gmail.com': 5,
+      'greg@gmail.com': 8,
     };
 
     component.calculateStatistics();
