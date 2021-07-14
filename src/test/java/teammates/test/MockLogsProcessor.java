7083bf7264209298d94e4515c106e3ae5868a1c6
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-07-14 19:14:33 +0800

    Add tests for new filters

--- src/test/java/teammates/test/MockLogsProcessor.java
+++ src/test/java/teammates/test/MockLogsProcessor.java
@@ -7,7 +7,6 @@ import java.util.function.Predicate;
 
 import com.google.logging.type.LogSeverity;
 
-import com.mailjet.client.resource.User;
 import teammates.common.datatransfer.ErrorLogEntry;
 import teammates.common.datatransfer.FeedbackSessionLogEntry;
 import teammates.common.datatransfer.GeneralLogEntry;
@@ -15,7 +14,6 @@ import teammates.common.datatransfer.GeneralLogEntry.SourceLocation;
 import teammates.common.datatransfer.QueryLogsResults;
 import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
-import teammates.common.util.LogEvent;
 import teammates.logic.api.LogsProcessor;
 
 /**
@@ -97,7 +95,7 @@ public class MockLogsProcessor extends LogsProcessor {
             SourceLocation sourceLocation, String exceptionClass, Integer pageSize, String pageToken) {
 
         Predicate<MockGeneralLogEntry> filterPredicate = getPredicate(severity, minSeverity, startTime, endTime, trace,
-                apiEndpoint, googleId, regkey, email, logEvent, exceptionClass);
+                apiEndpoint, googleId, regkey, email, logEvent, sourceLocation, exceptionClass);
 
         List<GeneralLogEntry> queryResults = new ArrayList<>();
         generalLogs.forEach(logEntry -> {
@@ -121,7 +119,7 @@ public class MockLogsProcessor extends LogsProcessor {
 
     private Predicate<MockGeneralLogEntry> getPredicate(String severity, String minSeverity, Instant startTime,
             Instant endTime, String trace, String apiEndpoint, String googleId, String regkey, String email,
-            String logEvent, String exceptionClass) {
+            String logEvent, SourceLocation sourceLocation, String exceptionClass) {
         assert startTime != null && endTime != null;
         return logEntry -> {
             boolean matchSeverity = true;
@@ -133,6 +131,7 @@ public class MockLogsProcessor extends LogsProcessor {
             boolean matchRegkey = true;
             boolean matchEmail = true;
             boolean matchLogEvent = true;
+            boolean matchSourceLocation = true;
             boolean matchExceptionClass = true;
 
             if (severity != null) {
@@ -145,28 +144,44 @@ public class MockLogsProcessor extends LogsProcessor {
                 matchTrace = logEntry.getTrace().equals(trace);
             }
             if (apiEndpoint != null) {
-                matchApiEndpoint = logEntry.getApiEndpoint().equals(apiEndpoint);
+                matchApiEndpoint = logEntry.getApiEndpoint() != null && logEntry.getApiEndpoint().equals(apiEndpoint);
             }
             if (googleId != null) {
-                matchGoogleId = logEntry.getUserInfo().googleId.equals(googleId);
+                matchGoogleId = logEntry.getUserInfo() != null && logEntry.getUserInfo().googleId != null
+                        && logEntry.getUserInfo().googleId.equals(googleId);
             }
             if (regkey != null) {
-                matchRegkey = logEntry.getUserInfo().regkey.equals(regkey);
+                matchRegkey = logEntry.getUserInfo() != null && logEntry.getUserInfo().regkey != null
+                        && logEntry.getUserInfo().regkey.equals(regkey);
             }
             if (email != null) {
-                matchEmail = logEntry.getUserInfo().email.equals(email);
+                matchEmail = logEntry.getUserInfo() != null && logEntry.getUserInfo().email != null
+                        && logEntry.getUserInfo().email.equals(email);
             }
             if (logEvent != null) {
-                matchLogEvent = logEntry.getLogEvent().equals(logEvent);
+                matchLogEvent = logEntry.getLogEvent() != null && logEntry.getLogEvent().equals(logEvent);
+            }
+            if (sourceLocation != null && sourceLocation.getFile() != null) {
+                matchSourceLocation = logEntry.getSourceLocation() != null
+                        && logEntry.getSourceLocation().getFile() != null
+                        && logEntry.getSourceLocation().getFile().equals(sourceLocation.getFile());
+                if (sourceLocation.getFunction() != null) {
+                    matchSourceLocation = matchSourceLocation && logEntry.getSourceLocation().getFunction() != null
+                            && logEntry.getSourceLocation().getFunction().equals(sourceLocation.getFunction());
+                }
             }
             if (exceptionClass != null) {
-                matchExceptionClass = logEntry.getExceptionClass().equals(exceptionClass);
+                matchExceptionClass = logEntry.getExceptionClass() != null
+                        && logEntry.getExceptionClass().equals(exceptionClass);
             }
             return matchSeverity && matchTimePeriod && matchTrace && matchApiEndpoint && matchGoogleId && matchRegkey
-                    && matchEmail && matchLogEvent && matchExceptionClass;
+                    && matchEmail && matchLogEvent && matchSourceLocation && matchExceptionClass;
         };
     }
 
+    /**
+     * Allows mocking of {@link GeneralLogEntry}.
+     */
     public static class MockGeneralLogEntry extends GeneralLogEntry {
         private final String apiEndpoint;
         private final UserInfo userInfo;
@@ -199,6 +214,9 @@ public class MockLogsProcessor extends LogsProcessor {
             return exceptionClass;
         }
 
+        /**
+         * Represents user information.
+         */
         public static class UserInfo {
             String googleId;
             String regkey;
@@ -208,6 +226,18 @@ public class MockLogsProcessor extends LogsProcessor {
                 this.googleId = google;
                 this.regkey = regkey;
                 this.email = email;
+            }
+
+            public String getGoogleId() {
+                return googleId;
+            }
+
+            public String getRegkey() {
+                return regkey;
+            }
+
+            public String getEmail() {
+                return email;
             }
         }
     }
--- src/test/java/teammates/ui/webapi/QueryLogsActionTest.java
+++ src/test/java/teammates/ui/webapi/QueryLogsActionTest.java
@@ -9,6 +9,8 @@ import org.testng.annotations.Test;
 import teammates.common.datatransfer.GeneralLogEntry;
 import teammates.common.exception.InvalidHttpParameterException;
 import teammates.common.util.Const;
+import teammates.common.util.LogEvent;
+import teammates.test.MockLogsProcessor.MockGeneralLogEntry;
 import teammates.ui.output.GeneralLogsData;
 
 /**
@@ -45,6 +47,9 @@ public class QueryLogsActionTest extends BaseActionTest<QueryLogsAction> {
         GeneralLogEntry.SourceLocation infoLogSourceLocation2 = new GeneralLogEntry.SourceLocation("file2", 2L, "func2");
         long infoLogTimestamp1 = endTimeForSuccessCases - 1000 * 60 - 1;
         long infoLogTimestamp2 = endTimeForSuccessCases - 1000 * 60 - 2;
+        String infoLogApiEndpoint = "info log apiEndpoint";
+        String infoLogGoogleId = "info log google id";
+
         String warningLogTrace1 = "warning log trace 1";
         String warningLogTrace2 = "warning log trace 2";
         String warningLogTextPayload1 = "warning log text palyload 1";
@@ -53,27 +58,30 @@ public class QueryLogsActionTest extends BaseActionTest<QueryLogsAction> {
         GeneralLogEntry.SourceLocation warningLogSourceLocation2 = new GeneralLogEntry.SourceLocation("file4", 4L, "func4");
         long warningLogTimestamp1 = endTimeForSuccessCases - 1000 * 60 - 3;
         long warningLogTimestamp2 = endTimeForSuccessCases - 1000 * 60 - 4;
-        String errorLogTrace1 = "error log trace 1";
-        String errorLogTrace2 = "error log trace 2";
+        String warningLogRegkey = "warning log regkey";
+        String warningLogEmail = "warning log email";
+
+        String errorLogTrace = "error log trace";
         String errorLogTextPayload1 = "error log text palyload 1";
         String errorLogTextPayload2 = "error log text palyload 2";
         GeneralLogEntry.SourceLocation errorLogSourceLocation1 = new GeneralLogEntry.SourceLocation("file5", 5L, "func5");
         GeneralLogEntry.SourceLocation errorLogSourceLocation2 = new GeneralLogEntry.SourceLocation("file6", 6L, "func6");
         long errorLogTimestamp1 = endTimeForSuccessCases - 1000 * 60 - 5;
         long errorLogTimestamp2 = endTimeForSuccessCases - 1000 * 60 - 6;
+        String errorLogExceptionClass = "error log exception class";
 
         mockLogsProcessor.insertInfoLog(infoLogTrace1, infoLogSourceLocation1, infoLogTimestamp1, infoLogTextPayload1,
-                null, null, null, null, null, null);
+                infoLogApiEndpoint, infoLogGoogleId, null, null, LogEvent.REQUEST_RECEIVED.toString(), null);
         mockLogsProcessor.insertInfoLog(infoLogTrace2, infoLogSourceLocation2, infoLogTimestamp2, infoLogTextPayload2,
-                null, null, null, null, null, null);
+                infoLogApiEndpoint, infoLogGoogleId, null, null, LogEvent.RESPONSE_DISPATCHED.toString(), null);
         mockLogsProcessor.insertWarningLog(warningLogTrace1, warningLogSourceLocation1, warningLogTimestamp1,
-                warningLogTextPayload1,null, null, null, null, null, null);
+                warningLogTextPayload1, null, null, warningLogRegkey, warningLogEmail, null, null);
         mockLogsProcessor.insertWarningLog(warningLogTrace2, warningLogSourceLocation2, warningLogTimestamp2,
-                warningLogTextPayload2,null, null, null, null, null, null);
-        mockLogsProcessor.insertGeneralErrorLog(errorLogTrace1, errorLogSourceLocation1, errorLogTimestamp1,
-                errorLogTextPayload1,null, null, null, null, null, null);
-        mockLogsProcessor.insertGeneralErrorLog(errorLogTrace2, errorLogSourceLocation2, errorLogTimestamp2,
-                errorLogTextPayload2,null, null, null, null, null, null);
+                warningLogTextPayload2, null, null, warningLogRegkey, warningLogEmail, null, null);
+        mockLogsProcessor.insertGeneralErrorLog(errorLogTrace, errorLogSourceLocation1, errorLogTimestamp1,
+                errorLogTextPayload1, null, null, null, null, null, errorLogExceptionClass);
+        mockLogsProcessor.insertGeneralErrorLog(errorLogTrace, errorLogSourceLocation2, errorLogTimestamp2,
+                errorLogTextPayload2, null, null, null, null, null, null);
 
         ______TS("Failure case: search end time is before search start time");
         String[] paramsInvalid1 = {
@@ -99,13 +107,13 @@ public class QueryLogsActionTest extends BaseActionTest<QueryLogsAction> {
         };
         assertThrows(InvalidHttpParameterException.class, () -> getJsonResult(getAction(paramsInvalid3)));
 
-        ______TS("Success case: all HTTP parameters are valid");
-        String[] paramsSuccessful1 = {
+        ______TS("Success case: all HTTP parameters are valid; filter by minimum severity level");
+        String[] paramsMinSeverity = {
                 Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
                 Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
                 Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
         };
-        actionOutput = getJsonResult(getAction(paramsSuccessful1));
+        actionOutput = getJsonResult(getAction(paramsMinSeverity));
         assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
 
         GeneralLogsData generalLogsData = (GeneralLogsData) actionOutput.getOutput();
@@ -146,15 +154,176 @@ public class QueryLogsActionTest extends BaseActionTest<QueryLogsAction> {
 
         assertEquals("stderr", entry5.getLogName());
         assertEquals("ERROR", entry5.getSeverity());
-        assertEquals(errorLogTrace1, entry5.getTrace());
+        assertEquals(errorLogTrace, entry5.getTrace());
         assertEquals(errorLogSourceLocation1, entry5.getSourceLocation());
         assertEquals(errorLogTimestamp1, entry5.getTimestamp());
 
         assertEquals("stderr", entry6.getLogName());
         assertEquals("ERROR", entry6.getSeverity());
-        assertEquals(errorLogTrace2, entry6.getTrace());
+        assertEquals(errorLogTrace, entry6.getTrace());
         assertEquals(errorLogSourceLocation2, entry6.getSourceLocation());
         assertEquals(errorLogTimestamp2, entry6.getTimestamp());
+
+        ______TS("Success case: filter by trace id");
+        String[] paramsTraceId = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_TRACE, errorLogTrace,
+        };
+        actionOutput = getJsonResult(getAction(paramsTraceId));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(2, logEntries.size());
+
+        GeneralLogEntry logEntryFilteredByTrace1 = logEntries.get(0);
+        GeneralLogEntry logEntryFilteredByTrace2 = logEntries.get(1);
+
+        assertEquals(errorLogTrace, logEntryFilteredByTrace1.getTrace());
+        assertEquals(errorLogTrace, logEntryFilteredByTrace2.getTrace());
+
+        ______TS("Success case: filter by apiEndpoint");
+        String[] paramsApiEndpoint = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_API_ENDPOINT, infoLogApiEndpoint,
+        };
+        actionOutput = getJsonResult(getAction(paramsApiEndpoint));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(2, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredByApiEndpoint1 = (MockGeneralLogEntry) logEntries.get(0);
+        MockGeneralLogEntry logEntryFilteredByApiEndpoint2 = (MockGeneralLogEntry) logEntries.get(1);
+
+        assertEquals(infoLogApiEndpoint, logEntryFilteredByApiEndpoint1.getApiEndpoint());
+        assertEquals(infoLogApiEndpoint, logEntryFilteredByApiEndpoint2.getApiEndpoint());
+
+        ______TS("Success case: filter by user info");
+        String[] paramsUserInfo1 = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.STUDENT_ID, infoLogGoogleId,
+        };
+        actionOutput = getJsonResult(getAction(paramsUserInfo1));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(2, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredByGoogleId1 = (MockGeneralLogEntry) logEntries.get(0);
+        MockGeneralLogEntry logEntryFilteredByGoogleId2 = (MockGeneralLogEntry) logEntries.get(1);
+
+        assertEquals(infoLogGoogleId, logEntryFilteredByGoogleId1.getUserInfo().getGoogleId());
+        assertEquals(infoLogGoogleId, logEntryFilteredByGoogleId2.getUserInfo().getGoogleId());
+
+        String[] paramsUserInfo2 = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.REGKEY, warningLogRegkey,
+                Const.ParamsNames.QUERY_LOGS_EMAIL, warningLogEmail,
+        };
+        actionOutput = getJsonResult(getAction(paramsUserInfo2));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(2, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredByRegkeyAndEmail1 = (MockGeneralLogEntry) logEntries.get(0);
+        MockGeneralLogEntry logEntryFilteredByRegkeyAndEmail2 = (MockGeneralLogEntry) logEntries.get(1);
+
+        assertEquals(warningLogRegkey, logEntryFilteredByRegkeyAndEmail1.getUserInfo().getRegkey());
+        assertEquals(warningLogEmail, logEntryFilteredByRegkeyAndEmail1.getUserInfo().getEmail());
+        assertEquals(warningLogRegkey, logEntryFilteredByRegkeyAndEmail2.getUserInfo().getRegkey());
+        assertEquals(warningLogEmail, logEntryFilteredByRegkeyAndEmail2.getUserInfo().getEmail());
+
+        ______TS("Success case: filter by log event");
+        String[] paramsLogEvent = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_EVENT, LogEvent.RESPONSE_DISPATCHED.toString(),
+        };
+        actionOutput = getJsonResult(getAction(paramsLogEvent));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(1, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredByLogEvent = (MockGeneralLogEntry) logEntries.get(0);
+        assertEquals(LogEvent.RESPONSE_DISPATCHED.toString(), logEntryFilteredByLogEvent.getLogEvent());
+
+        ______TS("Success case: filter by exception class");
+        String[] paramsExceptionClass = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_EXCEPTION_CLASS, errorLogExceptionClass,
+        };
+        actionOutput = getJsonResult(getAction(paramsExceptionClass));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(1, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredByExceptionClass = (MockGeneralLogEntry) logEntries.get(0);
+        assertEquals(errorLogExceptionClass, logEntryFilteredByExceptionClass.getExceptionClass());
+
+        ______TS("Success case: filter by source location");
+        String[] paramsSourceLocationFileOnly = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_SOURCE_LOCATION_FILE, errorLogSourceLocation1.getFile(),
+        };
+        actionOutput = getJsonResult(getAction(paramsSourceLocationFileOnly));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(1, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredBySourceLocationFile = (MockGeneralLogEntry) logEntries.get(0);
+        assertEquals(errorLogSourceLocation1.getFile(), logEntryFilteredBySourceLocationFile.getSourceLocation().getFile());
+
+        String[] paramsSourceLocationFileAndFunction = {
+                Const.ParamsNames.QUERY_LOGS_MIN_SEVERITY, severity,
+                Const.ParamsNames.QUERY_LOGS_STARTTIME, String.valueOf(startTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_ENDTIME, String.valueOf(endTimeForSuccessCases),
+                Const.ParamsNames.QUERY_LOGS_SOURCE_LOCATION_FILE, errorLogSourceLocation2.getFile(),
+                Const.ParamsNames.QUERY_LOGS_SOURCE_LOCATION_FUNCTION, errorLogSourceLocation2.getFunction(),
+        };
+        actionOutput = getJsonResult(getAction(paramsSourceLocationFileAndFunction));
+        assertEquals(HttpStatus.SC_OK, actionOutput.getStatusCode());
+
+        generalLogsData = (GeneralLogsData) actionOutput.getOutput();
+        logEntries = generalLogsData.getLogEntries();
+
+        assertEquals(1, logEntries.size());
+
+        MockGeneralLogEntry logEntryFilteredBySourceLocationFileAndFunction = (MockGeneralLogEntry) logEntries.get(0);
+        assertEquals(errorLogSourceLocation2.getFile(),
+                logEntryFilteredBySourceLocationFileAndFunction.getSourceLocation().getFile());
+        assertEquals(errorLogSourceLocation2.getFunction(),
+                logEntryFilteredBySourceLocationFileAndFunction.getSourceLocation().getFunction());
     }
 
     @Test
