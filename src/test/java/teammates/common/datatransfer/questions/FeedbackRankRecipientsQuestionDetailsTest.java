bb863da6a55758752856488683eac617156abccd
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-07-16 23:00:54 +0800

    Add test for FeedbackRankRecipientsQuestionDetails::validateResponseDetails

--- src/test/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetailsTest.java
+++ src/test/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetailsTest.java
@@ -1,5 +1,9 @@
 package teammates.common.datatransfer.questions;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
@@ -23,6 +27,62 @@ public class FeedbackRankRecipientsQuestionDetailsTest extends BaseTestCase {
         FeedbackRankRecipientsQuestionDetails feedbackRankRecipientsQuestionDetails =
                 new FeedbackRankRecipientsQuestionDetails();
         assertTrue(feedbackRankRecipientsQuestionDetails.validateQuestionDetails().isEmpty());
+    }
+
+    @Test
+    public void tesValidateResponsesDetails() {
+        FeedbackRankRecipientsQuestionDetails feedbackRankRecipientsQuestionDetails =
+                new FeedbackRankRecipientsQuestionDetails();
+        String duplicateErrorStr = "Duplicate rank %d in question";
+        String invalidErrorStr = "Invalid rank %d in question";
+        String maxOptionErrorStr = "You can rank at most %d options.";
+        String minOptionErrorStr = "You must rank at least %d options.";
+
+        ______TS("Failure: Duplicate ranks");
+        FeedbackRankRecipientsResponseDetails response1 = new FeedbackRankRecipientsResponseDetails();
+        FeedbackRankRecipientsResponseDetails response2 = new FeedbackRankRecipientsResponseDetails();
+        response1.setAnswer(1);
+        response2.setAnswer(1);
+        List<String> actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2), 2);
+        List<String> expectedErrors = Arrays.asList(String.format(duplicateErrorStr, response1.getAnswer()));
+        assertEquals(expectedErrors, actualErrors);
+
+        ______TS("Failure: Invalid ranks");
+        response1.setAnswer(3);
+        response2.setAnswer(-1);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2), 2);
+        expectedErrors = Arrays.asList(String.format(invalidErrorStr, response1.getAnswer()),
+                String.format(invalidErrorStr, response2.getAnswer()));
+        assertEquals(expectedErrors, actualErrors);
+
+        ______TS("Failure: Violate max and min options ranked");
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(2);
+        response1.setAnswer(3);
+        response2.setAnswer(1);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1), 3);
+        expectedErrors = Arrays.asList(
+                String.format(minOptionErrorStr, feedbackRankRecipientsQuestionDetails.minOptionsToBeRanked));
+        assertEquals(expectedErrors, actualErrors);
+
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(1);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2), 3);
+        expectedErrors = Arrays.asList(
+                String.format(maxOptionErrorStr, feedbackRankRecipientsQuestionDetails.maxOptionsToBeRanked));
+        assertEquals(expectedErrors, actualErrors);
+
+        ______TS("Success: valid responses");
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(2);
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(2);
+        response1.setAnswer(1);
+        response2.setAnswer(2);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2), 2);
+        expectedErrors = Collections.emptyList();
+        assertEquals(expectedErrors, actualErrors);
     }
 
     @Test
