336b81fcbe0274249dfc7b8380b997ab51fb2e50
ypinhsuan <yenpinhsuan@gmail.com>
2021-07-16 16:16:11 +0800

    Add api output

--- src/main/java/teammates/ui/webapi/ActionFactory.java
+++ src/main/java/teammates/ui/webapi/ActionFactory.java
@@ -22,12 +22,12 @@ import teammates.common.util.Const.TaskQueue;
  */
 public class ActionFactory {
 
-    protected static final Map<String, Map<String, Class<? extends Action>>> ACTION_MAPPINGS = new HashMap<>();
-
     private static final String GET = HttpGet.METHOD_NAME;
     private static final String POST = HttpPost.METHOD_NAME;
     private static final String PUT = HttpPut.METHOD_NAME;
     private static final String DELETE = HttpDelete.METHOD_NAME;
+
+    private static final Map<String, Map<String, Class<? extends Action>>> ACTION_MAPPINGS = new HashMap<>();
 
     static {
         map(ResourceURIs.DATABUNDLE, POST, PutDataBundleAction.class);
@@ -120,7 +120,6 @@ public class ActionFactory {
         map(ResourceURIs.SESSION_LOGS, POST, CreateFeedbackSessionLogAction.class);
         map(ResourceURIs.SESSION_LOGS, GET, GetFeedbackSessionLogsAction.class);
         map(ResourceURIs.LOGS, GET, QueryLogsAction.class);
-        map(ResourceURIs.ACTION_CLASS, GET, GetActionClassesAction.class);
 
         // Cron jobs; use GET request
         // Reference: https://cloud.google.com/appengine/docs/standard/java11/scheduling-jobs-with-cron-yaml
