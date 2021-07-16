36566016d1deb49fe41128ab81c9b8a44cf0b981
ypinhsuan <yenpinhsuan@gmail.com>
2021-07-16 15:38:03 +0800

    Add api endpoint for getting action class

---
+++ src/main/java/teammates/ui/webapi/GetActionClassesAction.java
@@ -0,0 +1,19 @@
+package teammates.ui.webapi;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import teammates.ui.output.ActionClasses;
+
+/**
+ * Retrieves a list of action class names.
+ */
+class GetActionClassesAction extends AdminOnlyAction {
+    @Override
+    ActionResult execute() {
+        List<String> actionClasses = ActionFactory.ACTION_MAPPINGS.values().stream()
+                .flatMap(map -> map.values().stream().map(actionClass -> actionClass.getSimpleName()))
+                .collect(Collectors.toList());
+        return new JsonResult(new ActionClasses(actionClasses));
+    }
+}
