ee54dc5f20d02eb0285faeeaa35b2fdd5347c214
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-07-22 12:07:12 +0800

    Fix imports

--- src/test/java/teammates/ui/webapi/GetActionClassesActionTest.java
+++ src/test/java/teammates/ui/webapi/GetActionClassesActionTest.java
@@ -1,6 +1,11 @@
 package teammates.ui.webapi;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.testng.annotations.Test;
 
