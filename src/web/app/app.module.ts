a9a77e1d8a21a5c8acf07467b6882199ff75c1a8
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-07-23 18:45:28 +0800

    Rename directory

--- src/web/app/app.module.ts
+++ src/web/app/app.module.ts
@@ -19,7 +19,7 @@ import { TeammatesRouterModule } from './components/teammates-router/teammates-r
 import { ToastModule } from './components/toast/toast.module';
 import { CustomUrlSerializer } from './custom-url-serializer';
 import { MaintenancePageComponent } from './maintenance-page.component';
-import { MaintainerPageComponent } from './page-maintainer/maintainer-page.component';
+import { MaintainerPageComponent } from './pages-maintainer/maintainer-page.component';
 import { ClickOutsideDirective, PageComponent } from './page.component';
 import { AdminPageComponent } from './pages-admin/admin-page.component';
 import { InstructorPageComponent } from './pages-instructor/instructor-page.component';
@@ -85,7 +85,7 @@ let routes: Routes = [
       {
         path: 'maintainer',
         component: MaintainerPageComponent,
-        loadChildren: () => import('./page-maintainer/maintainer-page.module').then((m: any) => m.MaintainerPageModule),
+        loadChildren: () => import('./pages-maintainer/maintainer-page.module').then((m: any) => m.MaintainerPageModule),
       },
       {
         path: '**',
