492403ada595c4b8b01d211e6daea1651dd2754c
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-07-23 12:23:34 +0800

    Improve test

--- src/test/java/teammates/ui/webapi/GetActionClassesActionTest.java
+++ src/test/java/teammates/ui/webapi/GetActionClassesActionTest.java
@@ -1,11 +1,9 @@
 package teammates.ui.webapi;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.testng.annotations.Test;
 
@@ -30,14 +28,107 @@ public class GetActionClassesActionTest extends BaseActionTest<GetActionClassesA
     @Test
     @Override
     protected void testExecute() throws Exception {
-        Set<String> actionClasses = new HashSet<>();
-        for (Map<String, Class<? extends Action>> map : ActionFactory.ACTION_MAPPINGS.values()) {
-            for (Class<? extends Action> action : map.values()) {
-                actionClasses.add(action.getSimpleName());
-            }
-        }
-        List<String> expectedActionClasses = new ArrayList<>(actionClasses);
-        Collections.sort(expectedActionClasses);
+        List<Class<? extends Action>> expectedActionClasses = Arrays.asList(
+                DeleteFeedbackResponseCommentAction.class,
+                CreateFeedbackResponseCommentAction.class,
+                GetFeedbackResponseCommentAction.class,
+                UpdateFeedbackResponseCommentAction.class,
+                RestoreFeedbackSessionAction.class,
+                BinFeedbackSessionAction.class,
+                GetCoursesAction.class,
+                GetRegkeyValidityAction.class,
+                PutDataBundleAction.class,
+                DeleteDataBundleAction.class,
+                CreateFeedbackSessionLogAction.class,
+                GetFeedbackSessionLogsAction.class,
+                GetInstructorPrivilegeAction.class,
+                UpdateInstructorPrivilegeAction.class,
+                GetFeedbackSessionsAction.class,
+                GenerateEmailAction.class,
+                GetFeedbackQuestionsAction.class,
+                GetOngoingSessionsAction.class,
+                GetStudentProfileAction.class,
+                UpdateStudentProfileAction.class,
+                GetNationalitiesAction.class,
+                AdminExceptionTestAction.class,
+                RemindFeedbackSessionResultAction.class,
+                DeleteInstructorAction.class,
+                CreateInstructorAction.class,
+                GetInstructorAction.class,
+                UpdateInstructorAction.class,
+                ArchiveCourseAction.class,
+                InstructorCourseJoinEmailWorkerAction.class,
+                DeleteStudentAction.class,
+                GetStudentAction.class,
+                UpdateStudentAction.class,
+                SearchStudentsAction.class,
+                FeedbackSessionRemindEmailWorkerAction.class,
+                DeleteStudentProfilePictureAction.class,
+                PostStudentProfilePictureAction.class,
+                GetStudentProfilePictureAction.class,
+                DeleteFeedbackSessionAction.class,
+                CreateFeedbackSessionAction.class,
+                GetFeedbackSessionAction.class,
+                UpdateFeedbackSessionAction.class,
+                FeedbackSessionClosingRemindersAction.class,
+                GetLocalDateTimeInfoAction.class,
+                GetTimeZonesAction.class,
+                FeedbackSessionRemindParticularUsersEmailWorkerAction.class,
+                GetFeedbackResponsesAction.class,
+                SubmitFeedbackResponsesAction.class,
+                FeedbackSessionPublishedEmailWorkerAction.class,
+                FeedbackSessionClosedRemindersAction.class,
+                SendErrorReportAction.class,
+                GetActionClassesAction.class,
+                UnpublishFeedbackSessionAction.class,
+                PublishFeedbackSessionAction.class,
+                GetSessionResultsAction.class,
+                GetHasResponsesAction.class,
+                DatastoreBackupAction.class,
+                RestoreCourseAction.class,
+                BinCourseAction.class,
+                DeleteAccountAction.class,
+                CreateAccountAction.class,
+                GetAccountAction.class,
+                FeedbackSessionPublishedRemindersAction.class,
+                QueryLogsAction.class,
+                SessionLinksRecoveryAction.class,
+                SendJoinReminderEmailAction.class,
+                RegenerateStudentCourseLinksAction.class,
+                CompileLogsAction.class,
+                GetAuthInfoAction.class,
+                GetFeedbackSessionSubmittedGiverSetAction.class,
+                GetCourseJoinStatusAction.class,
+                JoinCourseAction.class,
+                GetSessionResponseStatsAction.class,
+                DeleteCourseAction.class,
+                CreateCourseAction.class,
+                GetCourseAction.class,
+                UpdateCourseAction.class,
+                GetFeedbackQuestionRecipientsAction.class,
+                DowngradeAccountAction.class,
+                RemindFeedbackSessionSubmissionAction.class,
+                FeedbackSessionUnpublishedEmailWorkerAction.class,
+                SendEmailWorkerAction.class,
+                GetInstructorsAction.class,
+                PutDataBundleDocumentsAction.class,
+                FeedbackSessionResendPublishedEmailWorkerAction.class,
+                StudentCourseJoinEmailWorkerAction.class,
+                SearchInstructorsAction.class,
+                GetCourseSectionNamesAction.class,
+                ResetAccountAction.class,
+                FeedbackSessionOpeningRemindersAction.class,
+                DeleteStudentsAction.class,
+                GetStudentsAction.class,
+                EnrollStudentsAction.class,
+                DeleteFeedbackQuestionAction.class,
+                CreateFeedbackQuestionAction.class,
+                UpdateFeedbackQuestionAction.class
+        );
+        List<String> expectedActionClassesNames = expectedActionClasses.stream()
+                .map(Class::getSimpleName)
+                .sorted()
+                .collect(Collectors.toList());
 
         GetActionClassesAction action = getAction();
         action.execute();
@@ -46,7 +137,7 @@ public class GetActionClassesActionTest extends BaseActionTest<GetActionClassesA
         List<String> actualActionClasses = data.getActionClasses();
         Collections.sort(actualActionClasses);
 
-        assertEquals(expectedActionClasses, actualActionClasses);
+        assertEquals(expectedActionClassesNames, actualActionClasses);
     }
 
     @Override
