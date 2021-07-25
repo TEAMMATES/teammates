27ab5fcb1244bddfe000c3a98e6dcf716cdc8c67
Jellybeano <lanyuxuan98@gmail.com>
2021-07-26 03:19:33 +0800

    Migrate test data to JSON file

--- src/web/app/components/question-types/question-statistics/constsum-options-question-statistics.component.spec.ts
+++ src/web/app/components/question-types/question-statistics/constsum-options-question-statistics.component.spec.ts
@@ -1,10 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FeedbackQuestionType } from '../../../../types/api-request';
 
-import { FeedbackConstantSumResponseDetails } from '../../../../types/api-output';
 import { SortableTableModule } from '../../sortable-table/sortable-table.module';
 import { ConstsumOptionsQuestionStatisticsComponent } from './constsum-options-question-statistics.component';
-import { Response } from './question-statistics';
+import { default as responses } from './test-data/constsumOptionQuestionResponses.json';
 
 describe('ConstsumOptionsQuestionStatisticsComponent', () => {
   let component: ConstsumOptionsQuestionStatisticsComponent;
@@ -28,55 +26,10 @@ describe('ConstsumOptionsQuestionStatisticsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  const responses: Response<FeedbackConstantSumResponseDetails>[] = [
-    {
-      giver: 'Alice',
-      giverTeam: 'Team 1',
-      giverEmail: 'alice@gmail.com',
-      giverSection: '',
-      recipient: 'Alice',
-      recipientTeam: 'Team 1',
-      recipientEmail: 'alice@gmail.com',
-      recipientSection: '',
-      responseDetails: {
-        answers: [50, 50, 0],
-        questionType: FeedbackQuestionType.CONSTSUM,
-      } as FeedbackConstantSumResponseDetails,
-    },
-    {
-      giver: 'Bob',
-      giverTeam: 'Team 2',
-      giverEmail: 'bob@gmail.com',
-      giverSection: '',
-      recipient: 'Bob',
-      recipientTeam: 'Team 2',
-      recipientEmail: 'bob@gmail.com',
-      recipientSection: '',
-      responseDetails: {
-        answers: [30, 70, 0],
-        questionType: FeedbackQuestionType.CONSTSUM,
-      } as FeedbackConstantSumResponseDetails,
-    },
-    {
-      giver: 'Charles',
-      giverTeam: 'Team 1',
-      giverEmail: 'charles@gmail.com',
-      giverSection: '',
-      recipient: 'Charles',
-      recipientTeam: 'Team 1',
-      recipientEmail: 'charles@gmail.com',
-      recipientSection: '',
-      responseDetails: {
-        answers: [10, 90, 0],
-        questionType: FeedbackQuestionType.CONSTSUM,
-      } as FeedbackConstantSumResponseDetails,
-    },
-  ];
-
   it('should calculate statistics correctly', () => {
 
     component.question.constSumOptions = ['optionA', 'optionB', 'optionC'];
-    component.responses = responses;
+    component.responses = JSON.parse(JSON.stringify(responses.responses));
 
     component.calculateStatistics();
 
