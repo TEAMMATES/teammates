fa29a594ae27bb09bde023b3e35a58e4cbcf5179
ppris <49683462+pPris@users.noreply.github.com>
2021-07-26 17:47:07 +0800

    Add test to check that reminder is sent

--- src/test/java/teammates/logic/api/EmailGeneratorTest.java
+++ src/test/java/teammates/logic/api/EmailGeneratorTest.java
@@ -168,6 +168,12 @@ public class EmailGeneratorTest extends BaseLogicTest {
         verifyEmailReceivedCorrectly(emails, instructor1.email, subject,
                 "/sessionReminderEmailForInstructor.html", lineInEmailToInstructor);
 
+        InstructorAttributes instructorNotJoinedYet = instructorsLogic.getInstructorForEmail("idOfTypicalCourse1", "instructorNotYetJoinedCourse1@email.tmt");
+        String instructorReminderToJoinLine = "Note that you will need to join the course as an instructor";
+
+        // Verify that unregistered instructor gets reminder to join course
+        verifyEmailReceivedCorrectly(emails, instructorNotJoinedYet.email, subject, "/sessionReminderEmailForInstructorNotJoinedYet.html", instructorReminderToJoinLine);
+
         ______TS("feedback session closing alerts");
 
         emails = new EmailGenerator().generateFeedbackSessionClosingEmails(session);
