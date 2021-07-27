c3ab2ee55da499f43ad123cdf79ee0dae2bd6434
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2021-07-27 18:54:10 +0800

    Add more tests

--- src/test/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetailsTest.java
+++ src/test/java/teammates/common/datatransfer/questions/FeedbackRankRecipientsQuestionDetailsTest.java
@@ -33,14 +33,16 @@ public class FeedbackRankRecipientsQuestionDetailsTest extends BaseTestCase {
     public void tesValidateResponsesDetails() {
         FeedbackRankRecipientsQuestionDetails feedbackRankRecipientsQuestionDetails =
                 new FeedbackRankRecipientsQuestionDetails();
+        FeedbackRankRecipientsResponseDetails response1 = new FeedbackRankRecipientsResponseDetails();
+        FeedbackRankRecipientsResponseDetails response2 = new FeedbackRankRecipientsResponseDetails();
+        FeedbackRankRecipientsResponseDetails response3 = new FeedbackRankRecipientsResponseDetails();
+        FeedbackRankRecipientsResponseDetails response4 = new FeedbackRankRecipientsResponseDetails();
         String duplicateErrorStr = "Duplicate rank %d in question";
         String invalidErrorStr = "Invalid rank %d in question";
         String maxOptionErrorStr = "You can rank at most %d options.";
         String minOptionErrorStr = "You must rank at least %d options.";
 
         ______TS("Failure: Duplicate ranks");
-        FeedbackRankRecipientsResponseDetails response1 = new FeedbackRankRecipientsResponseDetails();
-        FeedbackRankRecipientsResponseDetails response2 = new FeedbackRankRecipientsResponseDetails();
         response1.setAnswer(1);
         response2.setAnswer(1);
         List<String> actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
@@ -72,6 +74,85 @@ public class FeedbackRankRecipientsQuestionDetailsTest extends BaseTestCase {
                 Arrays.asList(response1, response2), 3);
         expectedErrors = Arrays.asList(
                 String.format(maxOptionErrorStr, feedbackRankRecipientsQuestionDetails.maxOptionsToBeRanked));
+        assertEquals(expectedErrors, actualErrors);
+
+        ______TS("Failure: Mix of duplicate rank, invalid rank and violation of max and min options ranked");
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(4);
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(4);
+        response1.setAnswer(1);
+        response2.setAnswer(1);
+        response3.setAnswer(5);
+        response4.setAnswer(1);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2, response3, response4), 4);
+        expectedErrors = Arrays.asList(
+                String.format(duplicateErrorStr, response2.getAnswer()),
+                String.format(invalidErrorStr, response3.getAnswer()),
+                String.format(duplicateErrorStr, response4.getAnswer())
+        );
+        assertEquals(expectedErrors, actualErrors);
+
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(3);
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(1);
+        response1.setAnswer(1);
+        response2.setAnswer(1);
+        response3.setAnswer(2);
+        response4.setAnswer(3);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2, response3, response4), 4);
+        expectedErrors = Arrays.asList(
+                String.format(duplicateErrorStr, response2.getAnswer()),
+                String.format(maxOptionErrorStr, feedbackRankRecipientsQuestionDetails.getMaxOptionsToBeRanked())
+        );
+        assertEquals(expectedErrors, actualErrors);
+
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(3);
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(1);
+        response1.setAnswer(5);
+        response2.setAnswer(100);
+        response3.setAnswer(0);
+        response4.setAnswer(-999);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2, response3, response4), 4);
+        expectedErrors = Arrays.asList(
+                String.format(invalidErrorStr, response1.getAnswer()),
+                String.format(invalidErrorStr, response2.getAnswer()),
+                String.format(invalidErrorStr, response3.getAnswer()),
+                String.format(invalidErrorStr, response4.getAnswer()),
+                String.format(maxOptionErrorStr, feedbackRankRecipientsQuestionDetails.getMaxOptionsToBeRanked())
+        );
+        assertEquals(expectedErrors, actualErrors);
+
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(3);
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(1);
+        response1.setAnswer(-1);
+        response2.setAnswer(-1);
+        response3.setAnswer(5);
+        response4.setAnswer(1);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2, response3, response4), 3);
+        expectedErrors = Arrays.asList(
+                String.format(invalidErrorStr, response1.getAnswer()),
+                String.format(duplicateErrorStr, response2.getAnswer()),
+                String.format(invalidErrorStr, response3.getAnswer()),
+                String.format(maxOptionErrorStr, feedbackRankRecipientsQuestionDetails.getMaxOptionsToBeRanked())
+        );
+        assertEquals(expectedErrors, actualErrors);
+
+        feedbackRankRecipientsQuestionDetails.setMaxOptionsToBeRanked(5);
+        feedbackRankRecipientsQuestionDetails.setMinOptionsToBeRanked(5);
+        response1.setAnswer(-10);
+        response2.setAnswer(0);
+        response3.setAnswer(-10);
+        response4.setAnswer(1);
+        actualErrors = feedbackRankRecipientsQuestionDetails.validateResponsesDetails(
+                Arrays.asList(response1, response2, response3, response4), 3);
+        expectedErrors = Arrays.asList(
+                String.format(invalidErrorStr, response1.getAnswer()),
+                String.format(invalidErrorStr, response2.getAnswer()),
+                String.format(duplicateErrorStr, response3.getAnswer()),
+                String.format(minOptionErrorStr, feedbackRankRecipientsQuestionDetails.getMinOptionsToBeRanked())
+        );
         assertEquals(expectedErrors, actualErrors);
 
         ______TS("Success: valid responses");
