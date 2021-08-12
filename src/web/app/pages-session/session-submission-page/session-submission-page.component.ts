8052bdbd9503a242c3a86caaec4f059ea3f0ea31
Tan Jin <cjtanjin@gmail.com>
2021-08-12 22:16:05 +0800

    Fix linting errors

--- src/web/app/pages-session/session-submission-page/session-submission-page.component.ts
+++ src/web/app/pages-session/session-submission-page/session-submission-page.component.ts
@@ -224,7 +224,7 @@ export class SessionSubmissionPageComponent implements OnInit, AfterViewInit {
    * Scrolls to the question based on its given question id.
    */
   scrollToQuestion(): void {
-    let div = document.getElementById(this.moderatedQuestionId);
+    const div: HTMLElement | null = document.getElementById(this.moderatedQuestionId);
 
     // continue scrolling as long as the element to scroll to is yet to be found or not in view
     if (div == null || !(this.isInViewport(div))) {
