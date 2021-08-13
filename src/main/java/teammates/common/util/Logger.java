55e02f1e74a53c703dc672ba3caa9bce6ab2804b
Wilson Kurniawan <wkurniawan.92@gmail.com>
2021-08-13 22:16:26 +0800

    Include web version in logs

--- src/main/java/teammates/common/util/Logger.java
+++ src/main/java/teammates/common/util/Logger.java
@@ -76,6 +76,7 @@ public final class Logger {
         details.setRequestMethod(method);
         details.setRequestUrl(requestUrl);
         details.setUserAgent(request.getHeader("User-Agent"));
+        details.setWebVersion(request.getHeader("X-WEB-VERSION"));
         details.setRequestParams(HttpRequestHelper.getRequestParameters(request));
         details.setRequestHeaders(HttpRequestHelper.getRequestHeaders(request));
 
