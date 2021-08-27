2a0aced9ecefa07b67f95a8ed15f622f1ed17df0
Samuel Fang <samuelfangjw@gmail.com>
2021-08-27 22:28:21 +0800

    Add check for whether user already has account

--- src/web/app/user-create-account-page.component.ts
+++ src/web/app/user-create-account-page.component.ts
@@ -39,11 +39,18 @@ export class UserCreateAccountPageComponent implements OnInit {
 
       if (!this.key) {
         this.validUrl = false;
+        this.isLoading = false;
         return;
       }
 
       this.authService.getAuthUser().subscribe((resp: AuthInfo) => {
         this.userId = resp.user?.id || "";
+        
+        if (resp.user?.isInstructor) {
+          // User already has instructor account
+          this.navigationService.navigateByURL(this.router, `/web/instructor`);
+        }
+
         this.isLoading = false;
       });
     });
