30c85e6391e3c2c1acfe6890322d5c2906179bf1
Lye Yi Xian <lyeyx999@hotmail.com>
2021-09-01 11:40:44 +0800

    Use a constant map to check if we are going to output the additional message.

--- src/web/app/pages-session/session-result-page/session-result-page.component.ts
+++ src/web/app/pages-session/session-result-page/session-result-page.component.ts
@@ -13,6 +13,7 @@ import { StudentService } from '../../../services/student.service';
 import { TimezoneService } from '../../../services/timezone.service';
 import {
   AuthInfo,
+  FeedbackQuestionType,
   FeedbackSession, FeedbackSessionLogType,
   FeedbackSessionPublishStatus, FeedbackSessionSubmissionStatus,
   Instructor,
@@ -64,6 +65,9 @@ export class SessionResultPageComponent implements OnInit {
   visibilityRecipient: FeedbackVisibilityType = FeedbackVisibilityType.RECIPIENT;
 
   intent: Intent = Intent.STUDENT_RESULT;
+  RESPONSE_HIDDEN_QUESTIONS: FeedbackQuestionType[] = [
+    FeedbackQuestionType.CONTRIB
+  ]
 
   isFeedbackSessionResultsLoading: boolean = false;
   hasFeedbackSessionResultsLoadingFailed: boolean = false;
