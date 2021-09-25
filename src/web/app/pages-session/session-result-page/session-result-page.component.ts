05f4c7f13f3a427dae39246cbe087d74e5efbb64
rrtheonlyone <rahul.rajesh.bhat@gmail.com>
2021-09-25 18:42:54 +0800

    Remove isFeedbackSessionResultsEmpty

--- src/web/app/pages-session/session-result-page/session-result-page.component.ts
+++ src/web/app/pages-session/session-result-page/session-result-page.component.ts
@@ -71,7 +71,6 @@ export class SessionResultPageComponent implements OnInit {
 
   isFeedbackSessionResultsLoading: boolean = false;
   hasFeedbackSessionResultsLoadingFailed: boolean = false;
-  isFeedbackSessionResultsEmpty: boolean = false;
   retryAttempts: number = DEFAULT_NUMBER_OF_RETRY_ATTEMPTS;
 
   private backendUrl: string = environment.backendUrl;
@@ -212,9 +211,6 @@ export class SessionResultPageComponent implements OnInit {
             this.questions = sessionResults.questions.sort(
                 (a: QuestionOutput, b: QuestionOutput) =>
                     a.feedbackQuestion.questionNumber - b.feedbackQuestion.questionNumber);
-            if (this.questions.length === 0) {
-              this.isFeedbackSessionResultsEmpty = true;
-            }
           }, (resp: ErrorMessageOutput) => {
             this.handleError(resp);
           });
