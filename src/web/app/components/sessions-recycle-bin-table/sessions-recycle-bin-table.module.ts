4e1d881c2a4fa19ff4cc6a724a132914592bdfba
Xin Yee <codelearner19@gmail.com>
2021-09-26 19:07:39 +0800

    Reuse component ajax-loading instead of new spinner

--- src/web/app/components/sessions-recycle-bin-table/sessions-recycle-bin-table.module.ts
+++ src/web/app/components/sessions-recycle-bin-table/sessions-recycle-bin-table.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AjaxLoadingModule } from '../ajax-loading/ajax-loading.module';
 import { PanelChevronModule } from '../panel-chevron/panel-chevron.module';
 import { TeammatesCommonModule } from '../teammates-common/teammates-common.module';
 import { RecycleBinTableFormatDatePipe } from './recycle-bin-table-format-date.pipe';
@@ -15,6 +16,7 @@ import {
 @NgModule({
   declarations: [SessionsRecycleBinTableComponent, RecycleBinTableFormatDatePipe],
   imports: [
+    AjaxLoadingModule,
     CommonModule,
     NgbTooltipModule,
     TeammatesCommonModule,
