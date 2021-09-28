2117355a07bfac87dd935bc247abd464c3dfee9e
Xin Yee <codelearner19@gmail.com>
2021-09-28 20:55:47 +0800

    Refactor visibility-panel component modules and variables

--- src/web/app/components/visibility-panel/visibility-panel.component.ts
+++ src/web/app/components/visibility-panel/visibility-panel.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  FeedbackParticipantType,
+  FeedbackQuestionType,
+  FeedbackTextQuestionDetails,
+  FeedbackVisibilityType,
+  NumberOfEntitiesToGiveFeedbackToSetting,
+} from '../../../types/api-output';
+import { CommonVisibilitySetting } from '../../../services/feedback-questions.service';
+import { QuestionEditFormModel } from '../question-edit-form/question-edit-form-model';
+import { VisibilityControl } from '../../../types/visibility-control';
+//import { VisibilityStateMachine } from '../../../services/visibility-state-machine';
 
 @Component({
   selector: 'tm-visibility-panel',
@@ -7,9 +18,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisibilityPanelComponent implements OnInit {
 
+  //enum
+  FeedbackParticipantType: typeof FeedbackParticipantType = FeedbackParticipantType;
+  FeedbackQuestionType: typeof FeedbackQuestionType = FeedbackQuestionType;
+  NumberOfEntitiesToGiveFeedbackToSetting: typeof NumberOfEntitiesToGiveFeedbackToSetting =
+        NumberOfEntitiesToGiveFeedbackToSetting;
+  VisibilityControl: typeof VisibilityControl = VisibilityControl;
+  FeedbackVisibilityType: typeof FeedbackVisibilityType = FeedbackVisibilityType;
+  
+  @Input()
+  isCustomFeedbackVisibilitySettingAllowed: boolean = false;
+  
+  @Input()
+  model: QuestionEditFormModel = {
+                                     feedbackQuestionId: '',
+
+                                     questionNumber: 0,
+                                     questionBrief: '',
+                                     questionDescription: '',
+
+                                     isQuestionHasResponses: false,
+
+                                     questionType: FeedbackQuestionType.TEXT,
+                                     questionDetails: {
+                                       questionType: FeedbackQuestionType.TEXT,
+                                       questionText: '',
+                                     } as FeedbackTextQuestionDetails,
+
+                                     giverType: FeedbackParticipantType.STUDENTS,
+                                     recipientType: FeedbackParticipantType.STUDENTS,
+
+                                     numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.UNLIMITED,
+                                     customNumberOfEntitiesToGiveFeedbackTo: 1,
+
+                                     showResponsesTo: [],
+                                     showGiverNameTo: [],
+                                     showRecipientNameTo: [],
+
+                                     commonVisibilitySettingName: '',
+
+                                     isUsingOtherFeedbackPath: false,
+                                     isUsingOtherVisibilitySetting: false,
+                                     isDeleting: false,
+                                     isDuplicating: false,
+                                     isEditable: false,
+                                     isSaving: false,
+                                     isCollapsed: false,
+                                     isVisibilityChanged: false,
+                                     isFeedbackPathChanged: false,
+                                     isQuestionDetailsChanged: false,
+                                   };
+  
+  @Input()
+  VisibilityControls: VisibilityControl = VisibilityControl.SHOW_RESPONSE
+  
+  @Input()
+  FeedbackVisibilityTypes: FeedbackVisibilityType = FeedbackVisibilityType.STUDENTS
+  
+  @Input()
+  commonFeedbackVisibilitySettings: CommonVisibilitySetting[] = [];
+  
+  //@Input()
+  //visibilityStateMachine: VisibilityStateMachine;
+    
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
--- src/web/app/components/visibility-panel/visibility-panel.module.ts
+++ src/web/app/components/visibility-panel/visibility-panel.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgbDropdownModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { VisibilityPanelComponent } from './visibility-panel.component';
+import { VisibilityMessagesModule } from '../visibility-messages/visibility-messages.module';
+import { TeammatesCommonModule } from '../teammates-common/teammates-common.module';
 
 /**
  * Visibility panel module.
@@ -9,6 +12,10 @@ import { VisibilityPanelComponent } from './visibility-panel.component';
 @NgModule({
   imports: [
     CommonModule,
+    NgbDropdownModule,
+    NgbTooltipModule,
+    TeammatesCommonModule, 
+    VisibilityMessagesModule,
   ],
   declarations: [
     VisibilityPanelComponent,
