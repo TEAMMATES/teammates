e217e80fff72c50ab3a50e9088e3204f7ed38849
Xin Yee <codelearner19@gmail.com>
2021-10-12 01:07:23 +0800

    remove redundant code

--- src/web/app/components/view-results-panel/view-results-panel.component.spec.ts
+++ src/web/app/components/view-results-panel/view-results-panel.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ViewResultsPanelComponent } from './view-results-panel.component';\
+import { ViewResultsPanelComponent } from './view-results-panel.component';
 
 describe('ViewResultsPanelComponent', () => {
   let component: ViewResultsPanelComponent;
