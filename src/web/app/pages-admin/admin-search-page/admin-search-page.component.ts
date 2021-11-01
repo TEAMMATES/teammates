c4ba54b4a8fa0e9ac5d28167754190361339f116
Tan Jin <cjtanjin@gmail.com>
2021-11-01 12:32:11 +0800

    Fix merge conflicts

--- src/web/app/pages-admin/admin-search-page/admin-search-page.component.ts
+++ src/web/app/pages-admin/admin-search-page/admin-search-page.component.ts
@@ -15,7 +15,7 @@ import {
 import { SimpleModalService } from '../../../services/simple-modal.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { StudentService } from '../../../services/student.service';
-import { Email, MessageOutput, RegenerateStudentCourseLinks } from '../../../types/api-output';
+import { Email, MessageOutput, RegenerateKey } from '../../../types/api-output';
 import { SimpleModalType } from '../../components/simple-modal/simple-modal-type';
 import { collapseAnim } from '../../components/teammates-common/collapse-anim';
 import { ErrorMessageOutput } from '../../error-message-output';
