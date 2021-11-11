39e57aa0d1c71ce54ef3f752ed316b03d45a1453
xhasak <456221@mail.muni.cz>
2021-11-11 19:41:59 +0100

    [#10920] Fixed broken import link to ./config.template

--- src/web/environments/environment.ts
+++ src/web/environments/environment.ts
@@ -1,4 +1,4 @@
-import { config } from './config';
+import { config } from './config.template';
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
