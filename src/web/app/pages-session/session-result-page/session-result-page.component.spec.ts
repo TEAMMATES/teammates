6e40a8abe1a167954c1486157851910016b159fb
tjtanjin <cjtanjin@gmail.com>
2021-11-27 05:45:01 +0800

    Update component test for session result page

--- src/web/app/pages-session/session-result-page/session-result-page.component.spec.ts
+++ src/web/app/pages-session/session-result-page/session-result-page.component.spec.ts
@@ -681,7 +681,7 @@ describe('SessionResultPageComponent', () => {
     expect(navSpy.calls.count()).toEqual(1);
     expect(navSpy.calls.mostRecent().args[1]).toEqual('/web/front');
     expect(navSpy.calls.mostRecent().args[2]).toEqual(
-        `You are trying to access TEAMMATES using the Google account ${component.loggedInUser}, which
+        `You are trying to access TEAMMATES using the Google account user-id, which
         is not linked to this TEAMMATES account. If you used a different Google account to
         join/access TEAMMATES before, please use that Google account to access TEAMMATES. If you
         cannot remember which Google account you used before, please email us at
