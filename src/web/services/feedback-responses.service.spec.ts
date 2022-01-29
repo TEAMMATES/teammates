12d0eeef0c165fbac31bf2ac4e6701bfa87f69da
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2022-01-29 20:32:51 +0800

    Add unit test for #getFeedbackResponse

--- src/web/services/feedback-responses.service.spec.ts
+++ src/web/services/feedback-responses.service.spec.ts
@@ -1,5 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ResourceEndpoints } from '../types/api-const';
+import { Intent } from '../types/api-request';
 import { FeedbackResponsesService } from './feedback-responses.service';
 import { HttpRequestService } from './http-request.service';
 
@@ -27,5 +29,22 @@ describe('FeedbackResponsesService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call get when retrieving a feedback response', () => {
+    const dummyIntent: Intent = Intent.STUDENT_SUBMISSION;
+    const paramMap: Record<string, string> = {
+      questionid: '[auto-generated Datastore ID]',
+      intent: dummyIntent,
+      key: '[generated registration key]',
+      moderatedperson: '',
+    };
+    service.getFeedbackResponse({
+      questionId: paramMap.questionid,
+      intent: dummyIntent,
+      key: paramMap.key,
+      moderatedPerson: paramMap.moderatedperson,
+    });
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.RESPONSES, paramMap);
   });
 });
