99782c63f4c068ffc30873a516affc89c7c0bbf8
Samuel Fang <samuelfangjw@gmail.com>
2022-02-02 00:12:32 +0800

    Catch error when accountRequestAttributes is null

--- src/main/java/teammates/ui/webapi/CreateAccountRequestAction.java
+++ src/main/java/teammates/ui/webapi/CreateAccountRequestAction.java
@@ -1,9 +1,12 @@
 package teammates.ui.webapi;
 
+import org.apache.http.HttpStatus;
+
 import teammates.common.datatransfer.attributes.AccountRequestAttributes;
 import teammates.common.exception.EntityAlreadyExistsException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.EmailWrapper;
+import teammates.common.util.Logger;
 import teammates.ui.output.JoinLinkData;
 import teammates.ui.request.AccountCreateRequest;
 import teammates.ui.request.InvalidHttpRequestBodyException;
@@ -12,6 +15,8 @@ import teammates.ui.request.InvalidHttpRequestBodyException;
  * Creates a new account request.
  */
 class CreateAccountRequestAction extends AdminOnlyAction {
+
+    private static final Logger log = Logger.getLogger();
 
     @Override
     public JsonResult execute() throws InvalidHttpRequestBodyException, InvalidOperationException {
@@ -33,6 +38,13 @@ class CreateAccountRequestAction extends AdminOnlyAction {
         } catch (EntityAlreadyExistsException eaee) {
             // Use existing account request
             accountRequestAttributes = logic.getAccountRequest(instructorEmail, instructorInstitution);
+        }
+
+        if (accountRequestAttributes == null) {
+            String errorMessage = "Account Request for instructor with email " + instructorEmail
+                    + " and institute " + instructorInstitution + " could not be found";
+            log.severe("Unexpected error: " + errorMessage);
+            return new JsonResult(errorMessage, HttpStatus.SC_INTERNAL_SERVER_ERROR);
         }
 
         String joinLink = accountRequestAttributes.getRegistrationUrl();
