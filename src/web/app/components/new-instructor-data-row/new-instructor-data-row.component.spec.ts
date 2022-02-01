e49ba2535377054b3e40a29a6d4c0350473bd005
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2022-02-02 01:16:05 +0800

    Add NewInstructorDataRowComponent input unit tests

--- src/web/app/components/new-instructor-data-row/new-instructor-data-row.component.spec.ts
+++ src/web/app/components/new-instructor-data-row/new-instructor-data-row.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { InstructorData } from './instructor-data';
 
 import { NewInstructorDataRowComponent } from './new-instructor-data-row.component';
@@ -10,6 +11,9 @@ describe('NewInstructorDataRowComponent', () => {
   let expectedInstructorData: InstructorData;
   let expectedIndex: number;
   let expectedActiveRequests: number;
+
+  let addButtonDe: any;
+  let addButtonEl: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,9 +38,43 @@ describe('NewInstructorDataRowComponent', () => {
     component.index = expectedIndex;
     component.activeRequests = expectedActiveRequests;
     fixture.detectChanges();
+    addButtonDe = fixture.debugElement
+      .query(By.css(`#add-instructor-${expectedIndex}`));
+    addButtonEl = addButtonDe.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the instructor name received as input', () => {
+    const displayedInstructorName: string = fixture.debugElement
+      .query(By.css(`#instructor-${expectedIndex}-name`))
+      .nativeElement.textContent;
+    expect(displayedInstructorName).toEqual(expectedInstructorData.name);
+  });
+
+  it('should display the instructor email received as input', () => {
+    const displayedInstructorEmail: string = fixture.debugElement
+      .query(By.css(`#instructor-${expectedIndex}-email`))
+      .nativeElement.textContent;
+    expect(displayedInstructorEmail).toEqual(expectedInstructorData.email);
+  });
+
+  it('should display the instructor institution received as input', () => {
+    const displayedInstructorInstitution: string = fixture.debugElement
+      .query(By.css(`#instructor-${expectedIndex}-institution`))
+      .nativeElement.textContent;
+    expect(displayedInstructorInstitution).toEqual(expectedInstructorData.institution);
+  });
+
+  it('should have an add button that is not disabled when there are zero active requests as input', () => {
+    expect(addButtonEl.disabled).toBeFalsy();
+  });
+
+  it('should have an add button that is disabled when there are non-zero active requests as input', () => {
+    component.activeRequests = 1;
+    fixture.detectChanges();
+    expect(addButtonEl.disabled).toBeTruthy();
   });
 });
