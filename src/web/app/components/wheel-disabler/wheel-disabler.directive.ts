0ecf769efbb8e962ba8ceaa4d8b000369120d053
Howard Liu <howard@ixnet.work>
2022-02-17 21:31:41 +0800

    [#11487] Disable mouse wheel events for number inputs (#11542)
    * Disable mouse wheel events for number inputs
    
    - Prevent mouse wheel scroll from changing students' number inputs
    
    * Revert "Disable mouse wheel events for number inputs"
    
    This reverts commit 9429e87f4a7554e4b318e661e3851bf8b5ecc3e3.
    
    * Disable wheel for numeric inputs using directive
    
    - New WheelDisablerDirective and respective module is created
    - Updated snapshot for session-submission-page
    
    * Fix linting errors

---
+++ src/web/app/components/wheel-disabler/wheel-disabler.directive.ts
@@ -0,0 +1,18 @@
+import {
+    Directive, HostListener,
+  } from '@angular/core';
+
+/**
+ * Directive for loading spinner component
+ */
+@Directive({
+  selector: '[tmDisableWheel]',
+})
+export class WheelDisablerDirective {
+
+  @HostListener('wheel', ['$event'])
+  onWheel(e: Event): void {
+    e.preventDefault();
+  }
+
+}
