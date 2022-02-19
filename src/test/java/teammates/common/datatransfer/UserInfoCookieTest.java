6b595e6e69ef7b53420eb8d09e478eac823abbbe
Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>
2022-02-19 16:21:15 +0800

    Add UserInfo cookie test

---
+++ src/test/java/teammates/common/datatransfer/UserInfoCookieTest.java
@@ -0,0 +1,26 @@
+package teammates.common.datatransfer;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.testng.annotations.Test;
+
+import teammates.test.BaseTestCase;
+
+/**
+ * SUT: {@link UserInfoCookie}.
+ */
+public class UserInfoCookieTest extends BaseTestCase {
+    private UserInfoCookie uc = new UserInfoCookie("MockId");
+
+    @Test
+    public void testIsValid() {
+        ______TS("Cookie not expired");
+        uc.setExpiryTime(Instant.now().plus(1, ChronoUnit.DAYS));
+        assertTrue(uc.isValid());
+
+        ______TS("Cookie expired");
+        uc.setExpiryTime(Instant.now().minus(1, ChronoUnit.DAYS));
+        assertFalse(uc.isValid());
+    }
+}
