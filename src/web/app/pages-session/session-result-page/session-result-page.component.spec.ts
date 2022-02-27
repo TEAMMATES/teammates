136e3f84e8950269554f86a35f3a972b1a3c47cd
Howard Liu <howard@ixnet.work>
2022-02-27 23:04:42 +0800

    Fix snap test specs for previous change

--- src/web/app/pages-session/session-result-page/session-result-page.component.spec.ts
+++ src/web/app/pages-session/session-result-page/session-result-page.component.spec.ts
@@ -237,6 +237,9 @@ describe('SessionResultPageComponent', () => {
     feedbackSessionService = TestBed.inject(FeedbackSessionsService);
     logService = TestBed.inject(LogService);
     component = fixture.componentInstance;
+    // Set both loading flags to false initially for testing purposes only
+    component.isFeedbackSessionDetailsLoading = false;
+    component.isFeedbackSessionResultsLoading = false;
     fixture.detectChanges();
   });
 
@@ -248,13 +251,22 @@ describe('SessionResultPageComponent', () => {
     expect(fixture).toMatchSnapshot();
   });
 
-  it('should snap with session results are loading', () => {
+  it('should snap with session details and results are loading', () => {
+    component.isFeedbackSessionDetailsLoading = true;
+    component.isFeedbackSessionResultsLoading = true;
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with session details loaded and results are loading', () => {
+    component.isFeedbackSessionDetailsLoading = false;
     component.isFeedbackSessionResultsLoading = true;
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
 
   it('should snap when session results failed to load', () => {
+    component.isFeedbackSessionDetailsLoading = false;
     component.isFeedbackSessionResultsLoading = false;
     component.hasFeedbackSessionResultsLoadingFailed = true;
     fixture.detectChanges();
