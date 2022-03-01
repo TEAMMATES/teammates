fb60b831e920df67e20fb74b3f102e8098cdd7ee
Howard Liu <howard@ixnet.work>
2022-03-01 13:54:00 +0800

    [#11572] Notification Feature - Skeleton of DB (#11575)
    * Complete skeleton for notification db
    
    * Handle updatedAt, align with latest tech design
    
    * Add skeleton for notification logic
    
    * Add API to create notification, fix typo
    
    * Rename classes to plural form, add to LogicStarter

---
+++ src/main/java/teammates/logic/core/NotificationsLogic.java
@@ -0,0 +1,72 @@
+package teammates.logic.core;
+
+import teammates.common.datatransfer.attributes.NotificationAttributes;
+import teammates.common.exception.EntityAlreadyExistsException;
+import teammates.common.exception.EntityDoesNotExistException;
+import teammates.common.exception.InvalidParametersException;
+import teammates.storage.api.NotificationsDb;
+
+/**
+ * Handles the logic related to notifications.
+ */
+public final class NotificationsLogic {
+
+    private static final NotificationsLogic instance = new NotificationsLogic();
+
+    private final NotificationsDb notificationsDb = NotificationsDb.inst();
+
+    private NotificationsLogic() {
+        // prevent initialization
+    }
+
+    public static NotificationsLogic inst() {
+        return instance;
+    }
+
+    void initLogicDependencies() {
+        // No dependency to other logic class
+    }
+
+    /**
+     * Gets notification associated with the {@code notificationId}.
+     *
+     * @return null if no match found.
+     */
+    public NotificationAttributes getNotification(String notificationId) {
+        return notificationsDb.getNotification(notificationId);
+    }
+
+    /**
+     * Creates a notification.
+     *
+     * @return the created notification
+     * @throws InvalidParametersException if the notification is not valid
+     * @throws EntityAlreadyExistsException if the notification already exists in the database.
+     */
+    NotificationAttributes createNotification(NotificationAttributes notification)
+            throws InvalidParametersException, EntityAlreadyExistsException {
+        return notificationsDb.createEntity(notification);
+    }
+
+    /**
+     * Updates/Creates the notification using {@link NotificationAttributes.UpdateOptions}.
+     *
+     * @return updated notification
+     * @throws InvalidParametersException if attributes to update are not valid
+     * @throws EntityDoesNotExistException if notification cannot be found with given Id
+     */
+    public NotificationAttributes updateNotification(NotificationAttributes.UpdateOptions updateOptions)
+            throws InvalidParametersException, EntityDoesNotExistException {
+        return notificationsDb.updateNotification(updateOptions);
+    }
+
+    /**
+     * Deletes notification associated with the {@code notificationId}.
+     *
+     * <p>Fails silently if the notification doesn't exist.</p>
+     */
+    public void deleteNotification(String notificationId) {
+        notificationsDb.deleteNotification(notificationId);
+    }
+
+}
