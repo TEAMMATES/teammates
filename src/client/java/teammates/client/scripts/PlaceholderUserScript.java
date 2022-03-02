6fa0915c62e68252ebf3058f8d9589c94805fc86
Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>
2022-03-02 11:57:35 +0800

    Modify execScript to not crash if not run directly

---
+++ src/client/java/teammates/client/scripts/PlaceholderUserScript.java
@@ -0,0 +1,7 @@
+package teammates.client.scripts;
+
+public class PlaceholderUserScript {
+	public static void main(String[] args) {
+		System.out.println("Placeholder script has executed");
+	}	
+}
