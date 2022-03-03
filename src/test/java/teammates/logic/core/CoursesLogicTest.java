5f2d6eb8193824850fb98fd713db5eea4b55d4b2
moziliar <mozongran@gmail.com>
2022-03-03 22:15:10 +0800

    update: move typical course creation forward and revert session creation change

--- src/test/java/teammates/logic/core/CoursesLogicTest.java
+++ src/test/java/teammates/logic/core/CoursesLogicTest.java
@@ -13,7 +13,6 @@ import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
 import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
 import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
@@ -50,7 +49,7 @@ public class CoursesLogicTest extends BaseLogicTest {
 
     @Test
     public void testUpdateCourseCascade_shouldCascadeUpdateTimezoneOfFeedbackSessions() throws Exception {
-        CourseAttributes typicalCourse1 = dataBundle.courses.get("typicalCourse1");
+        var typicalCourse1 = dataBundle.courses.get("typicalCourse1");
         assertNotEquals("UTC", typicalCourse1.getTimeZone());
 
         coursesLogic.updateCourseCascade(
@@ -58,16 +57,18 @@ public class CoursesLogicTest extends BaseLogicTest {
                         .withTimezone("UTC")
                         .build());
 
-        List<FeedbackSessionAttributes> sessionsOfCourse = fsLogic.getFeedbackSessionsForCourse(typicalCourse1.getId());
+        ______TS("success: recover all sessions after course creation");
+        var sessionsOfCourse = fsLogic.getFeedbackSessionsForCourse(typicalCourse1.getId());
         assertFalse(sessionsOfCourse.isEmpty());
 
-        List<FeedbackSessionAttributes> sessionsWithinRecoveryRange = fsLogic.getFeedbackSessionsForCourseStartingAfter(
+        var sessionsWithinRecoveryRange = fsLogic.getFeedbackSessionsForCourseStartingAfter(
                 typicalCourse1.getId(), typicalCourse1.getCreatedAt());
-        assertEquals(sessionsWithinRecoveryRange.size(), sessionsOfCourse.size());
+        assertEquals(sessionsOfCourse.size(), sessionsWithinRecoveryRange.size());
 
-        List<FeedbackSessionAttributes> sessionsOutsideRecoveryRange = fsLogic.getFeedbackSessionsForCourseStartingAfter(
-                typicalCourse1.getId(), typicalCourse1.getCreatedAt().plus(Duration.ofDays(1)));
-        assertEquals(sessionsOutsideRecoveryRange.size(), sessionsOfCourse.size() - 1);
+        ______TS("success: recover some sessions some time after after course creation");
+        var sessionsOutsideRecoveryRange = fsLogic.getFeedbackSessionsForCourseStartingAfter(
+                typicalCourse1.getId(), typicalCourse1.getCreatedAt().plus(Duration.ofDays(30)));
+        assertEquals(sessionsOfCourse.size() - 1, sessionsOutsideRecoveryRange.size());
         assertTrue(sessionsOfCourse.stream().allMatch(s -> "UTC".equals(s.getTimeZone())));
     }
 
