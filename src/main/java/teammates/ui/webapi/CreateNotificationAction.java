7bbb515e24b6ac9569b2e25ad5a596cefc54e8f0
Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>
2022-03-04 18:06:57 +0800

    Add create notification action

---
+++ src/main/java/teammates/ui/webapi/CreateNotificationAction.java
@@ -0,0 +1,51 @@
+package teammates.ui.webapi;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.apache.http.HttpStatus;
+
+import teammates.common.datatransfer.attributes.NotificationAttributes;
+import teammates.common.exception.EntityAlreadyExistsException;
+import teammates.common.exception.InvalidParametersException;
+import teammates.common.util.Logger;
+import teammates.ui.request.CreateNotificationRequest;
+import teammates.ui.request.InvalidHttpRequestBodyException;
+
+/**
+ * Action: Creates a new notification banner
+ */
+public class CreateNotificationAction extends AdminOnlyAction {
+	private static final Logger log = Logger.getLogger();
+
+	@Override
+	public ActionResult execute() throws InvalidHttpRequestBodyException, InvalidOperationException {
+		CreateNotificationRequest notificationRequest = getAndValidateRequestBody(CreateNotificationRequest.class);
+
+		Instant startTime = Instant.ofEpochMilli(notificationRequest.getStartTimestamp());
+		Instant endTime = Instant.ofEpochMilli(notificationRequest.getEndTimestamp());
+
+		NotificationAttributes newNotification = NotificationAttributes.builder(UUID.randomUUID().toString())
+			.withStartTime(startTime)
+			.withEndTime(endTime)
+			.withType(notificationRequest.getNotificationType())
+			.withTargetUser(notificationRequest.getTargetUser())
+			.withTitle(notificationRequest.getTitle())
+			.withMessage(notificationRequest.getMessage())
+			.build();
+		;
+
+		try {
+			logic.createNotification(newNotification);
+			return new JsonResult("Notification has been created successfully", HttpStatus.SC_OK);
+		} catch (InvalidParametersException e) {
+			throw new InvalidHttpRequestBodyException(e);
+		} catch (EntityAlreadyExistsException e) {
+			// tell user entity already exists
+			// when will this happen - if UUID colldies?
+			// todo?  InvalidOperationException
+			log.severe("Unexpected error", e);
+			return new JsonResult(e.getMessage(), HttpStatus.SC_INTERNAL_SERVER_ERROR);
+		}
+	}
+}
