ebc8fe090b4fe27f76f2d1d655fd3843c9b55cbb
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2022-03-09 03:13:19 +0800

    Add deadline maps to FeedbackSessionUpdateRequest

--- src/main/java/teammates/ui/request/FeedbackSessionUpdateRequest.java
+++ src/main/java/teammates/ui/request/FeedbackSessionUpdateRequest.java
@@ -1,8 +1,44 @@
 package teammates.ui.request;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The update request of a feedback session.
  */
 public class FeedbackSessionUpdateRequest extends FeedbackSessionBasicRequest {
 
+    private Map<String, Long> studentDeadlines;
+    private Map<String, Long> instructorDeadlines;
+
+    /**
+     * Gets the deadlines for students.
+     */
+    public Map<String, Instant> getStudentDeadlines() {
+        Map<String, Instant> studentDeadlineInstants = new HashMap<>();
+        studentDeadlines.forEach((emailAddress, deadline) -> {
+            studentDeadlineInstants.put(emailAddress, Instant.ofEpochMilli(deadline));
+        });
+        return studentDeadlineInstants;
+    }
+
+    /**
+     * Gets the deadlines for instructors.
+     */
+    public Map<String, Instant> getInstructorDeadlines() {
+        Map<String, Instant> instructorDeadlineInstants = new HashMap<>();
+        instructorDeadlines.forEach((emailAddress, deadline) -> {
+            instructorDeadlineInstants.put(emailAddress, Instant.ofEpochMilli(deadline));
+        });
+        return instructorDeadlineInstants;
+    }
+
+    public void setStudentDeadlines(Map<String, Long> studentDeadlines) {
+        this.studentDeadlines = studentDeadlines;
+    }
+
+    public void setInstructorDeadlines(Map<String, Long> instructorDeadlines) {
+        this.instructorDeadlines = instructorDeadlines;
+    }
 }
