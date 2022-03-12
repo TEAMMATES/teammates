279cb8d625385f31cb5be3ebc48e3b75e7c45204
jianhandev <jianhandev@gmail.com>
2022-03-12 22:51:35 +0800

    Add frontend test for students batch delete

--- src/web/app/pages-instructor/instructor-course-details-page/instructor-course-details-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-course-details-page/instructor-course-details-page.component.spec.ts
@@ -1,7 +1,14 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
+import SpyInstance = jest.SpyInstance;
+import { SimpleModalService } from '../../../services/simple-modal.service';
+import { StatusMessageService } from '../../../services/status-message.service';
+import { StudentService } from '../../../services/student.service';
+import { createMockNgbModalRef } from '../../../test-helpers/mock-ngb-modal-ref';
 import { Course, Instructor, InstructorPermissionRole, JoinState, Student } from '../../../types/api-output';
+import { SimpleModalModule } from '../../components/simple-modal/simple-modal.module';
 import { StudentListRowModel } from '../../components/student-list/student-list.component';
 import { TeammatesCommonModule } from '../../components/teammates-common/teammates-common.module';
 import { InstructorCourseDetailsPageComponent } from './instructor-course-details-page.component';
@@ -25,9 +32,23 @@ const testStudent: Student = {
   courseId: 'CS101',
 };
 
+const testInstructor: Instructor = {
+  courseId: course.courseId,
+  joinState: JoinState.JOINED,
+  googleId: 'Hock',
+  name: 'Hock',
+  email: 'hock@gmail.com',
+  role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_COOWNER,
+  displayedToStudentsAs: 'Hock',
+  isDisplayedToStudents: false,
+};
+
 describe('InstructorCourseDetailsPageComponent', () => {
   let component: InstructorCourseDetailsPageComponent;
   let fixture: ComponentFixture<InstructorCourseDetailsPageComponent>;
+  let studentService: StudentService;
+  let simpleModalService: SimpleModalService;
+  let statusMessageService: StatusMessageService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -36,6 +57,7 @@ describe('InstructorCourseDetailsPageComponent', () => {
         TeammatesCommonModule,
         RouterTestingModule,
         InstructorCourseDetailsPageModule,
+        SimpleModalModule,
       ],
     })
     .compileComponents();
@@ -43,6 +65,9 @@ describe('InstructorCourseDetailsPageComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InstructorCourseDetailsPageComponent);
+    studentService = TestBed.inject(StudentService);
+    simpleModalService = TestBed.inject(SimpleModalService);
+    statusMessageService = TestBed.inject(StatusMessageService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -57,9 +82,9 @@ describe('InstructorCourseDetailsPageComponent', () => {
 
   it('should snap with a course with one co-owner and no students, and populated course student list', () => {
     const stats: any = {
-      sectionsTotal: 0,
-      teamsTotal: 0,
-      studentsTotal: 0,
+      numOfSections: 0,
+      numOfTeams: 0,
+      numOfStudents: 0,
     };
     const coOwner: Instructor = {
       courseId: course.courseId,
@@ -86,9 +111,9 @@ describe('InstructorCourseDetailsPageComponent', () => {
 
   it('should snap with a course with one co-owner and one student, and ajax failure', () => {
     const stats: any = {
-      sectionsTotal: 1,
-      teamsTotal: 1,
-      studentsTotal: 1,
+      numOfSections: 1,
+      numOfTeams: 1,
+      numOfStudents: 1,
     };
     const coOwner: Instructor = {
       courseId: course.courseId,
@@ -122,5 +147,95 @@ describe('InstructorCourseDetailsPageComponent', () => {
     component.isStudentsLoading = true;
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
+  });
+
+  it('should display confirmation modal if delete all students is requested', () => {
+    const stats: any = {
+      numOfSections: 1,
+      numOfTeams: 1,
+      numOfStudents: 1,
+    };
+    const courseDetails: any = {
+      course,
+      stats,
+    };
+    const studentListRowModel: StudentListRowModel = {
+      student: testStudent,
+      isAllowedToViewStudentInSection: true,
+      isAllowedToModifyStudent: true,
+    };
+    component.students = [studentListRowModel];
+    component.courseDetails = courseDetails;
+    component.instructors = [testInstructor];
+    component.isLoadingCsv = false;
+    component.isStudentsLoading = false;
+    fixture.detectChanges();
+
+    const promise: Promise<void> = Promise.resolve();
+
+    const spySimpleModalService: SpyInstance = jest.spyOn(simpleModalService, 'openConfirmationModal')
+        .mockReturnValue(createMockNgbModalRef({}, promise));
+
+    const deleteAllButton: any = fixture.debugElement.nativeElement.querySelector('#btn-delete-all');
+    deleteAllButton.click();
+    fixture.detectChanges();
+
+    expect(spySimpleModalService).toHaveBeenCalled();
+  });
+
+  it('should delete students in batches and show success message upon completion', () => {
+    const stats: any = {
+      numOfSections: 10,
+      numOfTeams: 10,
+      numOfStudents: 350,
+    };
+    const courseDetails: any = {
+      course,
+      stats,
+    };
+    component.courseDetails = courseDetails;
+    fixture.detectChanges();
+
+    const spyStudentService: SpyInstance = jest.spyOn(studentService, 'batchDeleteStudentsFromCourse')
+        .mockReturnValue(of({ message: 'Successful' }));
+
+    jest.spyOn(statusMessageService, 'showErrorToast')
+        .mockImplementation((args: string) => {
+          expect(args).toEqual('All the students have been removed from the course');
+        });
+
+    component.deleteAllStudentsFromCourse(course.courseId);
+
+    // given a limit of 100 students per call and 350 students,
+    // there should be four calls in total
+    expect(spyStudentService).toHaveBeenCalledTimes(4);
+  });
+
+  it('should show error message when delete fails', () => {
+    const stats: any = {
+      numOfSections: 1,
+      numOfTeams: 1,
+      numOfStudents: 1,
+    };
+    const courseDetails: any = {
+      course,
+      stats,
+    };
+    component.courseDetails = courseDetails;
+    fixture.detectChanges();
+
+    jest.spyOn(studentService, 'batchDeleteStudentsFromCourse').mockReturnValue(throwError({
+      error: {
+        message: 'This is the error message.',
+      },
+    }));
+
+    const spy: SpyInstance = jest.spyOn(statusMessageService, 'showErrorToast').mockImplementation((args: string) => {
+      expect(args).toEqual('This is the error message.');
+    });
+
+    component.deleteAllStudentsFromCourse(course.courseId);
+
+    expect(spy).toBeCalled();
   });
 });
