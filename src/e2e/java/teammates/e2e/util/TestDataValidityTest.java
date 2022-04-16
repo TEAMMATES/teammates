733e41bc905327195672d364a7786dec8545b52f
Zhao Jingjing <54243224+zhaojj2209@users.noreply.github.com>
2022-04-16 22:43:33 +0800

    [#11572] Notification Feature - E2E Tests for User Notifications Page (#11742)
    * Add instructor notifications page E2E tests
    
    * Add student notifications page E2E tests
    
    * Shorten notif test bundle names
    
    * Show timezone on user notifs page
    
    * Fix test data validity test

--- src/e2e/java/teammates/e2e/util/TestDataValidityTest.java
+++ src/e2e/java/teammates/e2e/util/TestDataValidityTest.java
@@ -209,7 +209,8 @@ public class TestDataValidityTest extends BaseTestCase {
                 .replace("Details", "Det")
                 .replace("Confirmation", "Conf")
                 .replace("Profile", "Prof")
-                .replace("Reminders", "Rem");
+                .replace("Reminders", "Rem")
+                .replace("Notifications", "Notifs");
 
         // Shorten question types
         shortenedTestPage = shortenedTestPage
