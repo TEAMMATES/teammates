2003616f906e3f27cd59f41e968ad3c5ed8a8dc8
Fergus Mok <fergusmok1@gmail.com>
2022-04-17 11:06:25 +0800

    Refactor submission status

--- src/web/app/pipes/session-submission-status.pipe.ts
+++ src/web/app/pipes/session-submission-status.pipe.ts
@@ -9,26 +9,27 @@ export class SubmissionStatusPipe implements PipeTransform {
    * Displays the submission status depending on student submissions and whether the session is open.
    */
   transform(isOpened: boolean, isWaitingToOpen: boolean, isSubmitted: boolean, hasOngoingExtension?: boolean): string {
-    if (isOpened && isSubmitted) {
-      if (hasOngoingExtension) {
-        return 'Submitted (with Extension)';
-      }
-      return 'Submitted';
-    }
-
-    if (!isSubmitted) {
-      if (hasOngoingExtension) {
-        return 'Pending (with Extension)';
-      }
-      if (isOpened) {
-        return 'Pending';
-      }
-    }
-
     if (isWaitingToOpen) {
       return 'Awaiting';
     }
 
-    return 'Closed';
+    if (!isOpened) {
+      return 'Closed';
+    }
+
+    let msg = '';
+    if (isOpened) {
+      if (isSubmitted) {
+        msg += 'Submitted';
+      } else {
+        msg += 'Pending';
+      }
+    }
+
+    if (hasOngoingExtension) {
+      msg += ' (with Extension)';
+    }
+
+    return msg;
   }
 }
