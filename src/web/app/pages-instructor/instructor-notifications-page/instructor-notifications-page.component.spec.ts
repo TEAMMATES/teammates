f5f8bda0e9dc96c6aef9ae6de4e2f54e8083e49f
Howard Liu <howard@ixnet.work>
2022-04-20 12:51:03 +0800

    [#11572] Notification feature - User frontend tests (#11741)

--- src/web/app/pages-instructor/instructor-notifications-page/instructor-notifications-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-notifications-page/instructor-notifications-page.component.spec.ts
@@ -29,4 +29,8 @@ describe('InstructorNotificationsPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
 });
