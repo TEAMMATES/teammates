975c9404f4f84bfa5be3ae86bd65902f04332828
xyliew25 <e0550381@u.nus.edu>
2022-06-01 19:48:46 +0800

    Add new options STUDENTS and TEAMS

--- src/web/services/feedback-questions.service.ts
+++ src/web/services/feedback-questions.service.ts
@@ -88,33 +88,33 @@ export class FeedbackQuestionsService {
       case FeedbackQuestionType.RUBRIC:
       case FeedbackQuestionType.CONSTSUM_OPTIONS:
         paths.set(FeedbackParticipantType.SELF,
-          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS_EXCLUDING_SELF,
-            FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
-            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS_EXCLUDING_SELF,
-            FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
+          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS,
+            FeedbackParticipantType.STUDENTS_EXCLUDING_SELF, FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
+            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS,
+            FeedbackParticipantType.TEAMS_EXCLUDING_SELF, FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
             FeedbackParticipantType.OWN_TEAM, FeedbackParticipantType.NONE]);
 
         paths.set(FeedbackParticipantType.STUDENTS,
-          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS_EXCLUDING_SELF,
-            FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
-            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS_EXCLUDING_SELF,
-            FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
+          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS,
+            FeedbackParticipantType.STUDENTS_EXCLUDING_SELF, FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
+            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS,
+            FeedbackParticipantType.TEAMS_EXCLUDING_SELF, FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
             FeedbackParticipantType.OWN_TEAM, FeedbackParticipantType.OWN_TEAM_MEMBERS,
             FeedbackParticipantType.OWN_TEAM_MEMBERS_INCLUDING_SELF,
             FeedbackParticipantType.NONE]);
 
         paths.set(FeedbackParticipantType.INSTRUCTORS,
-          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS_EXCLUDING_SELF,
+          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS,
             FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
-            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS_EXCLUDING_SELF,
+            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS,
             FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
             FeedbackParticipantType.OWN_TEAM, FeedbackParticipantType.NONE]);
 
         paths.set(FeedbackParticipantType.TEAMS,
-          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS_EXCLUDING_SELF,
-            FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
-            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS_EXCLUDING_SELF,
-            FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
+          [FeedbackParticipantType.SELF, FeedbackParticipantType.STUDENTS,
+            FeedbackParticipantType.STUDENTS_EXCLUDING_SELF, FeedbackParticipantType.STUDENTS_IN_SAME_SECTION,
+            FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.TEAMS,
+            FeedbackParticipantType.TEAMS_EXCLUDING_SELF, FeedbackParticipantType.TEAMS_IN_SAME_SECTION,
             FeedbackParticipantType.OWN_TEAM_MEMBERS_INCLUDING_SELF,
             FeedbackParticipantType.NONE]);
         break;
