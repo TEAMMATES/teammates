a4821210b3106061ca4163ff8c9a25528f78ca22
fans2619 <fans2619@gmail.com>
2022-06-06 01:26:41 +0800

    Add test for CopyInstructorsFromOtherCoursesModalComponent

--- src/web/app/pages-instructor/instructor-course-edit-page/copy-instructors-from-other-courses-modal/copy-instructors-from-other-courses-modal.component.spec.ts
+++ src/web/app/pages-instructor/instructor-course-edit-page/copy-instructors-from-other-courses-modal/copy-instructors-from-other-courses-modal.component.spec.ts
@@ -2,21 +2,16 @@ import { CommonModule } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { of } from 'rxjs';
-import { FeedbackQuestionsService } from '../../../../services/feedback-questions.service';
+import { InstructorService } from '../../../../services/instructor.service';
 import {
-  FeedbackMcqQuestionDetails,
-  FeedbackParticipantType,
-  FeedbackQuestion,
-  FeedbackQuestions,
-  FeedbackQuestionType,
-  FeedbackRankRecipientsQuestionDetails,
-  FeedbackTextQuestionDetails,
-  FeedbackVisibilityType,
-  NumberOfEntitiesToGiveFeedbackToSetting,
+  Instructor,
+  InstructorPermissionRole, Instructors,
+  JoinState,
 } from '../../../../types/api-output';
 import { SortBy, SortOrder } from '../../../../types/sort-properties';
+import { AjaxLoadingModule } from '../../../components/ajax-loading/ajax-loading.module';
 import { LoadingRetryModule } from '../../../components/loading-retry/loading-retry.module';
 import { LoadingSpinnerModule } from '../../../components/loading-spinner/loading-spinner.module';
 import { PanelChevronModule } from '../../../components/panel-chevron/panel-chevron.module';
@@ -26,105 +21,99 @@ import { CopyInstructorsFromOtherCoursesModalComponent } from './copy-instructor
 
 describe('CopyInstructorsFromOtherCoursesModalComponent', () => {
 
-  const testFeedbackQuestion1: FeedbackQuestion = {
-    feedbackQuestionId: 'feedback-question-1',
-    questionNumber: 1,
-    questionBrief: 'question brief',
-    questionDescription: 'description',
-    questionType: FeedbackQuestionType.TEXT,
-    questionDetails: {
-      questionType: FeedbackQuestionType.TEXT,
-      questionText: 'question text',
-    } as FeedbackTextQuestionDetails,
-    giverType: FeedbackParticipantType.STUDENTS,
-    recipientType: FeedbackParticipantType.INSTRUCTORS,
-    numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.UNLIMITED,
-    customNumberOfEntitiesToGiveFeedbackTo: 5,
-    showResponsesTo: [FeedbackVisibilityType.GIVER_TEAM_MEMBERS, FeedbackVisibilityType.INSTRUCTORS],
-    showGiverNameTo: [],
-    showRecipientNameTo: [],
+  const testInstructor1: Instructor = {
+    googleId: 'googleIdOfIns1',
+    courseId: 'FAN0001',
+    email: 'ins1@fan.tmt',
+    isDisplayedToStudents: true,
+    displayedToStudentsAs: 'abcdefg',
+    name: 'Instructor Cat',
+    role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_COOWNER,
+    joinState: JoinState.JOINED,
+    key: 'ujg2l5hj5l7jrfo28fyosfklh2892',
+    institute: 'A',
   };
 
-  const testFeedbackQuestion2: FeedbackQuestion = {
-    feedbackQuestionId: 'feedback-question-2',
-    questionNumber: 2,
-    questionBrief: 'question brief',
-    questionDescription: 'description',
-    questionType: FeedbackQuestionType.MCQ,
-    questionDetails: {
-      questionType: FeedbackQuestionType.MCQ,
-      questionText: 'question text',
-      mcqChoices: ['choice 1', 'choice 2', 'choice 3'],
-    } as FeedbackMcqQuestionDetails,
-    giverType: FeedbackParticipantType.STUDENTS,
-    recipientType: FeedbackParticipantType.INSTRUCTORS,
-    numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.UNLIMITED,
-    customNumberOfEntitiesToGiveFeedbackTo: 5,
-    showResponsesTo: [FeedbackVisibilityType.GIVER_TEAM_MEMBERS, FeedbackVisibilityType.INSTRUCTORS],
-    showGiverNameTo: [],
-    showRecipientNameTo: [],
+  const testInstructor2: Instructor = {
+    googleId: 'googleIdOfIns2',
+    courseId: 'FAN0001',
+    email: 'ins2@fan.tmt',
+    isDisplayedToStudents: false,
+    displayedToStudentsAs: '',
+    name: 'Instructor Dog',
+    role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_CUSTOM,
+    joinState: JoinState.JOINED,
+    key: '1343jbcl3iru2yct0897goji',
+    institute: 'A',
   };
 
-  const testFeedbackQuestion3: FeedbackQuestion = {
-    feedbackQuestionId: 'feedback-question-3',
-    questionNumber: 3,
-    questionBrief: 'question brief',
-    questionDescription: 'description',
-    questionType: FeedbackQuestionType.RANK_RECIPIENTS,
-    questionDetails: {
-      questionType: FeedbackQuestionType.RANK_RECIPIENTS,
-      questionText: 'question text',
-      minOptionsToBeRanked: 5,
-      maxOptionsToBeRanked: 5,
-      areDuplicatesAllowed: true,
-    } as FeedbackRankRecipientsQuestionDetails,
-    giverType: FeedbackParticipantType.STUDENTS,
-    recipientType: FeedbackParticipantType.INSTRUCTORS,
-    numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.UNLIMITED,
-    customNumberOfEntitiesToGiveFeedbackTo: 5,
-    showResponsesTo: [FeedbackVisibilityType.GIVER_TEAM_MEMBERS, FeedbackVisibilityType.INSTRUCTORS],
-    showGiverNameTo: [],
-    showRecipientNameTo: [],
+  const testInstructor3: Instructor = {
+    googleId: 'googleIdOfIns3',
+    courseId: 'FAN0002',
+    email: 'ins1@fan.tmt',
+    isDisplayedToStudents: true,
+    displayedToStudentsAs: 'QWQWQWQ',
+    name: 'King',
+    role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_MANAGER,
+    joinState: JoinState.NOT_JOINED,
+    key: 'pjkjbnc523iosdk2389nfbfib2',
+    institute: 'A',
   };
 
-  const testQuestionToCopyCandidate1: InstructorToCopyCandidateModel = {
-    instructor: testFeedbackQuestion1,
+  const testInstructor4: Instructor = {
+    googleId: 'googleIdOfIns4',
+    courseId: 'FAN0002',
+    email: 'ins4@fan.tmt',
+    isDisplayedToStudents: false,
+    displayedToStudentsAs: 'oldname',
+    name: 'Nothing',
+    role: InstructorPermissionRole.INSTRUCTOR_PERMISSION_ROLE_TUTOR,
+    joinState: JoinState.JOINED,
+    key: 'qdnjvbi47t928cjqnk1o8ry2o',
+    institute: 'A',
+  };
+
+  const testInstructorCandidate1: InstructorToCopyCandidateModel = {
+    instructor: testInstructor1,
     isSelected: false,
   };
 
-  const testQuestionToCopyCandidate2: InstructorToCopyCandidateModel = {
-    instructor: testFeedbackQuestion2,
+  const testInstructorCandidate2: InstructorToCopyCandidateModel = {
+    instructor: testInstructor2,
     isSelected: false,
   };
 
-  const testQuestionToCopyCandidate3: InstructorToCopyCandidateModel = {
-    instructor: testFeedbackQuestion3,
+  const testInstructorCandidate3: InstructorToCopyCandidateModel = {
+    instructor: testInstructor3,
     isSelected: false,
   };
 
-  const testFeedbackSessionTabModel1: CourseTabModel = {
-    courseId: 'testId1',
-    courseName: 'Test Session 1',
-    creationTimestamp: 1644458400, // Thursday, 10 February 2022 10:00:00 GMT+08:00
+  const testInstructorCandidate4: InstructorToCopyCandidateModel = {
+    instructor: testInstructor4,
+    isSelected: false,
+  };
+
+  const testCourseTab1: CourseTabModel = {
+    courseId: 'FAN0002',
+    courseName: 'Test Course 1',
+    creationTimestamp: 1658797215, // Tuesday, 26 July 2022 09:00:15 GMT+08:00
     isArchived: false,
     instructorCandidates: [],
     instructorCandidatesSortBy: SortBy.NONE,
     instructorCandidatesSortOrder: SortOrder.ASC,
-
     hasInstructorsLoaded: false,
     isTabExpanded: false,
     hasLoadingFailed: false,
   };
 
-  const testFeedbackSessionTabModel2: CourseTabModel = {
-    courseId: 'testId2',
-    courseName: 'Test Session 2',
-    creationTimestamp: 1645063200, // Thursday, 17 February 2022 10:00:00 GMT+08:00
+  const testCourseTab2: CourseTabModel = {
+    courseId: 'FAN0001',
+    courseName: 'Test Course 2',
+    creationTimestamp: 1645539742, // Tuesday, 22 February 2022 22:22:22 GMT+08:00
     isArchived: true,
     instructorCandidates: [],
     instructorCandidatesSortBy: SortBy.NONE,
     instructorCandidatesSortOrder: SortOrder.ASC,
-
     hasInstructorsLoaded: false,
     isTabExpanded: false,
     hasLoadingFailed: false,
@@ -132,7 +121,7 @@ describe('CopyInstructorsFromOtherCoursesModalComponent', () => {
 
   let component: CopyInstructorsFromOtherCoursesModalComponent;
   let fixture: ComponentFixture<CopyInstructorsFromOtherCoursesModalComponent>;
-  let feedbackQuestionsService: FeedbackQuestionsService;
+  let instructorService: InstructorService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -140,15 +129,17 @@ describe('CopyInstructorsFromOtherCoursesModalComponent', () => {
       imports: [
         CommonModule,
         FormsModule,
+        NgbTooltipModule,
         TeammatesCommonModule,
         PanelChevronModule,
         LoadingSpinnerModule,
         LoadingRetryModule,
+        AjaxLoadingModule,
         HttpClientTestingModule,
       ],
       providers: [
         NgbActiveModal,
-        FeedbackQuestionsService,
+        InstructorService,
       ],
     })
     .compileComponents();
@@ -156,7 +147,7 @@ describe('CopyInstructorsFromOtherCoursesModalComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CopyInstructorsFromOtherCoursesModalComponent);
-    feedbackQuestionsService = TestBed.inject(FeedbackQuestionsService);
+    instructorService = TestBed.inject(InstructorService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -169,91 +160,94 @@ describe('CopyInstructorsFromOtherCoursesModalComponent', () => {
     expect(fixture).toMatchSnapshot();
   });
 
-  it('should snap with feedback sessions', () => {
-    component.courses = [testFeedbackSessionTabModel1, testFeedbackSessionTabModel2];
+  it('should snap with courses', () => {
+    component.courses = [testCourseTab1, testCourseTab2];
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
 
-  it('should snap when feedback questions are loading', () => {
-    testFeedbackSessionTabModel1.isTabExpanded = true;
-    testFeedbackSessionTabModel1.hasInstructorsLoaded = false;
-    component.courses = [testFeedbackSessionTabModel1];
+  it('should snap when instructors are loading', () => {
+    testCourseTab1.isTabExpanded = true;
+    testCourseTab1.hasLoadingFailed = false;
+    testCourseTab1.hasInstructorsLoaded = false;
+    testCourseTab1.instructorCandidates = [];
+    component.courses = [testCourseTab1];
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
 
-  it('should snap when feedback questions are loaded', () => {
-    testFeedbackSessionTabModel1.isTabExpanded = true;
-    testFeedbackSessionTabModel1.hasInstructorsLoaded = true;
-    testFeedbackSessionTabModel1.instructorCandidates = [testQuestionToCopyCandidate1, testQuestionToCopyCandidate2];
-    component.courses = [testFeedbackSessionTabModel1];
+  it('should snap when instructors are loaded', () => {
+    testCourseTab1.isTabExpanded = true;
+    testCourseTab1.hasLoadingFailed = false;
+    testCourseTab1.hasInstructorsLoaded = true;
+    testCourseTab1.instructorCandidates = [testInstructorCandidate3, testInstructorCandidate4];
+    component.courses = [testCourseTab1];
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
 
   it('should snap when feedback questions failed to load', () => {
-    testFeedbackSessionTabModel1.isTabExpanded = true;
-    testFeedbackSessionTabModel1.hasLoadingFailed = true;
-    component.courses = [testFeedbackSessionTabModel1];
+    testCourseTab1.isTabExpanded = true;
+    testCourseTab1.hasLoadingFailed = true;
+    testCourseTab1.hasInstructorsLoaded = false;
+    testCourseTab1.instructorCandidates = [];
+    component.courses = [testCourseTab1];
     fixture.detectChanges();
     expect(fixture).toMatchSnapshot();
   });
 
-  it('should load feedback questions', () => {
-    const feedbackQuestions: FeedbackQuestions = {
-      questions: [testFeedbackQuestion1, testFeedbackQuestion2],
+  it('should load instructors', () => {
+    const instructors: Instructors = {
+      instructors: [testInstructor1, testInstructor2],
     };
-    jest.spyOn(feedbackQuestionsService, 'getFeedbackQuestions').mockReturnValue(of(feedbackQuestions));
-    component.courses = [testFeedbackSessionTabModel1];
+    jest.spyOn(instructorService, 'loadInstructors').mockReturnValue(of(instructors));
+    component.courses = [testCourseTab2];
 
-    component.loadInstructors(testFeedbackSessionTabModel1);
+    component.loadInstructors(testCourseTab2);
 
     expect(component.courses[0].hasInstructorsLoaded).toBeTruthy();
     expect(component.courses[0].hasLoadingFailed).toBeFalsy();
     expect(component.courses[0].instructorCandidates.length).toBe(2);
-    expect(component.courses[0].instructorCandidates[0].instructor.feedbackQuestionId)
-      .toBe(testFeedbackQuestion1.feedbackQuestionId);
-    expect(component.courses[0].instructorCandidates[1].instructor.feedbackQuestionId)
-      .toBe(testFeedbackQuestion2.feedbackQuestionId);
+    expect(component.courses[0].instructorCandidates[0].instructor.googleId)
+      .toBe(testInstructor1.googleId);
+    expect(component.courses[0].instructorCandidates[1].instructor.googleId)
+      .toBe(testInstructor2.googleId);
   });
 
-  it('should not allow copying when no questions are selected', () => {
-    testFeedbackSessionTabModel1.instructorCandidates = [testQuestionToCopyCandidate1, testQuestionToCopyCandidate2];
-    testFeedbackSessionTabModel2.instructorCandidates = [testQuestionToCopyCandidate3];
-    component.courses = [testFeedbackSessionTabModel1, testFeedbackSessionTabModel2];
+  it('should not allow copying when no instructors are selected', () => {
+    testCourseTab1.instructorCandidates = [testInstructorCandidate3, testInstructorCandidate4];
+    testCourseTab2.instructorCandidates = [testInstructorCandidate1, testInstructorCandidate2];
+    component.courses = [testCourseTab2, testCourseTab1];
     fixture.detectChanges();
 
-    const questions: FeedbackQuestion[] = component.getSelectedInstructors();
-    const button: any = fixture.debugElement.nativeElement.querySelector('#btn-confirm-copy-question');
+    const instructors: Instructor[] = component.getSelectedInstructors();
+    const button: any = fixture.debugElement.nativeElement.querySelector('#btn-confirm-copy-instructor');
 
     expect(component.isAnyInstructorCandidatesSelected).toBeFalsy();
-    expect(questions.length).toBe(0);
+    expect(instructors.length).toBe(0);
     expect(button.disabled).toBeTruthy();
   });
 
-  it('should copy selected questions', () => {
-    testQuestionToCopyCandidate1.isSelected = true;
-    testQuestionToCopyCandidate2.isSelected = true;
-    testQuestionToCopyCandidate3.isSelected = true;
-    testFeedbackSessionTabModel1.instructorCandidates = [testQuestionToCopyCandidate1, testQuestionToCopyCandidate2];
-    testFeedbackSessionTabModel2.instructorCandidates = [testQuestionToCopyCandidate3];
-    component.courses = [testFeedbackSessionTabModel1, testFeedbackSessionTabModel2];
+  it('should trigger copy button clicked event with selected instructors', () => {
+    testInstructorCandidate1.isSelected = true;
+    testInstructorCandidate3.isSelected = true;
+    testInstructorCandidate4.isSelected = true;
+    testCourseTab1.instructorCandidates = [testInstructorCandidate3, testInstructorCandidate4];
+    testCourseTab2.instructorCandidates = [testInstructorCandidate1, testInstructorCandidate2];
+    component.courses = [testCourseTab2, testCourseTab1];
     fixture.detectChanges();
 
-    jest.spyOn(component.activeModal, 'close').mockImplementation((questions: FeedbackQuestion[]) => {
-      expect(questions.length).toBe(3);
-      expect(questions[0].feedbackQuestionId).toBe(testFeedbackQuestion1.feedbackQuestionId);
-      expect(questions[1].feedbackQuestionId).toBe(testFeedbackQuestion2.feedbackQuestionId);
-      expect(questions[2].feedbackQuestionId).toBe(testFeedbackQuestion3.feedbackQuestionId);
-    });
+    const instructors: Instructor[] = [testInstructor1, testInstructor3, testInstructor4];
 
-    const button: any = fixture.debugElement.nativeElement.querySelector('#btn-confirm-copy-question');
+    jest.spyOn(component.copyClickedEvent, 'emit');
+
+    const button: any = fixture.debugElement.nativeElement.querySelector('#btn-confirm-copy-instructor');
 
     expect(component.isAnyInstructorCandidatesSelected).toBeTruthy();
     expect(button.disabled).toBeFalsy();
 
     button.click();
-    expect(component.activeModal.close).toHaveBeenCalledTimes(1);
+    expect(component.copyClickedEvent.emit).toHaveBeenCalledTimes(1);
+    expect(component.copyClickedEvent.emit).toHaveBeenCalledWith(instructors);
   });
 });
