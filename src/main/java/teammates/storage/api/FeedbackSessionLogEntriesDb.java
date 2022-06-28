4c52891ff33b53ea7b741d7dfec6cdd9b987714c
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2022-06-28 11:47:50 +0700

    Edit indexes and update fsl db query

--- src/main/java/teammates/storage/api/FeedbackSessionLogEntriesDb.java
+++ src/main/java/teammates/storage/api/FeedbackSessionLogEntriesDb.java
@@ -2,6 +2,7 @@ package teammates.storage.api;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.googlecode.objectify.Key;
@@ -37,19 +38,30 @@ public final class FeedbackSessionLogEntriesDb extends EntitiesDb<FeedbackSessio
     public List<FeedbackSessionLogEntryAttributes> getFeedbackSessionLogs(String courseId, String email,
                                                                           long startTime, long endTime, String fsName) {
         Query<FeedbackSessionLogEntry> query = load()
-                .filter("courseId =", courseId)
                 .filter("timestamp >=", startTime)
-                .filter("timestamp <=", endTime);
+                .filter("timestamp <=", endTime)
+                .filter("courseId =", courseId);
+
+        FeedbackSessionLogEntry entry = load().list().get(0);
+
+        if (entry.getTimestamp() >= startTime && entry.getTimestamp() <= endTime && entry.getCourseId().equals(courseId)) {
+            System.out.println("There is a match");
+        }
 
         if (email != null) {
             query = query.filter("studentEmail =", email);
+        } else {
+            System.out.println("Email is null");
         }
 
         if (fsName != null) {
             query = query.filter("feedbackSessionName =", fsName);
+        } else {
+            System.out.println("FsName is null");
         }
 
         List<FeedbackSessionLogEntry> entries = query.list();
+        System.out.println("The number of matched entries is: " + entries.size());
 
         return makeAttributes(entries);
     }
