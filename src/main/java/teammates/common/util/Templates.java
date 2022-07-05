ac6ba92c47311d33ae99950e8117b92e93587910
Tan Yi Guan <tanyg1@outlook.sg>
2022-07-05 16:12:39 +0800

    Fix linting errors

--- src/main/java/teammates/common/util/Templates.java
+++ src/main/java/teammates/common/util/Templates.java
@@ -52,7 +52,7 @@ public final class Templates {
                 FileHelper.readResourceFile("instructorEmailFragment-registrationKeyReset.html");
         public static final String USER_FEEDBACK_SESSION =
                 FileHelper.readResourceFile("userEmailTemplate-feedbackSession.html");
-        public static final String USER_FEEDBACK_SESSION_OPENING = 
+        public static final String USER_FEEDBACK_SESSION_OPENING =
                 FileHelper.readResourceFile("userEmailTemplate-feedbackSessionOpening.html");
         public static final String USER_FEEDBACK_SESSION_PUBLISHED =
                 FileHelper.readResourceFile("userEmailTemplate-feedbackSessionPublished.html");
