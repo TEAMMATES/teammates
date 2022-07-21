375afb219cd041bf39360e6763f5685a85b210ae
fans2619 <fans2619@gmail.com>
2022-07-22 01:15:50 +0800

    Move account request processing code to process-account-request-panel
    Move panel header to process-account-request-panel html

--- src/web/app/pages-admin/admin-requests-page/admin-requests-page.component.ts
+++ src/web/app/pages-admin/admin-requests-page/admin-requests-page.component.ts
@@ -3,24 +3,10 @@ import { finalize } from 'rxjs/operators';
 import { AccountService } from '../../../services/account.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { TimezoneService } from '../../../services/timezone.service';
-import { AccountRequest, AccountRequests, AccountRequestStatus } from '../../../types/api-output';
-import { AccountRequestUpdateRequest } from '../../../types/api-request';
+import { AccountRequest, AccountRequests } from '../../../types/api-output';
 import { DateFormat } from '../../components/datepicker/datepicker.component';
 import { collapseAnim } from '../../components/teammates-common/collapse-anim';
-import { removeAnim } from '../../components/teammates-common/remove-anim';
 import { ErrorMessageOutput } from '../../error-message-output';
-import {
-  EditedAccountRequestInfoModel,
-  ProcessAccountRequestPanelStatus,
-} from './process-account-request-panel/process-account-request-panel.component';
-
-export interface AccountRequestTab {
-  accountRequest: AccountRequest;
-  isTabExpanded: boolean;
-  panelStatus: ProcessAccountRequestPanelStatus;
-  isSavingChanges: boolean;
-  errorMessage: string;
-}
 
 interface FormQueryModel {
   fromDate: DateFormat;
@@ -34,15 +20,15 @@ interface FormQueryModel {
   selector: 'tm-admin-requests-page',
   templateUrl: './admin-requests-page.component.html',
   styleUrls: ['./admin-requests-page.component.scss'],
-  animations: [collapseAnim, removeAnim],
+  animations: [collapseAnim],
 })
 export class AdminRequestsPageComponent implements OnInit {
 
-  accountRequestPendingProcessingTabs: AccountRequestTab[] = [];
+  accountRequestsPendingProcessing: AccountRequest[] = [];
   hasAccountRequestsPendingProcessingLoadingFailed: boolean = false;
   isLoadingAccountRequestsPendingProcessing: boolean = false;
 
-  accountRequestWithinPeriodTabs: AccountRequestTab[] = [];
+  accountRequestsWithinPeriod: AccountRequest[] = [];
   hasAccountRequestsWithinPeriodLoadingFailed: boolean = false;
   isLoadingAccountRequestsWithinPeriod: boolean = false;
   hasQueried: boolean = false;
@@ -84,7 +70,7 @@ export class AdminRequestsPageComponent implements OnInit {
    * Loads all account requests pending processing.
    */
   loadAccountRequestsPendingProcessing(): void {
-    this.accountRequestPendingProcessingTabs = [];
+    this.accountRequestsPendingProcessing = [];
     this.hasAccountRequestsPendingProcessingLoadingFailed = false;
     this.isLoadingAccountRequestsPendingProcessing = true;
 
@@ -93,156 +79,13 @@ export class AdminRequestsPageComponent implements OnInit {
         this.isLoadingAccountRequestsPendingProcessing = false;
       }))
       .subscribe((resp: AccountRequests) => {
-        resp.accountRequests.forEach((ar: AccountRequest) => {
-          const accountRequestTab: AccountRequestTab = {
-            accountRequest: ar,
-            isTabExpanded: true,
-            panelStatus: ProcessAccountRequestPanelStatus.SUBMITTED,
-            isSavingChanges: false,
-            errorMessage: '',
-          };
-          this.accountRequestPendingProcessingTabs.push(accountRequestTab);
-        });
+        this.accountRequestsPendingProcessing = resp.accountRequests;
         // TODO: sort account requests
       }, (resp: ErrorMessageOutput) => {
-        this.accountRequestPendingProcessingTabs = [];
+        this.accountRequestsPendingProcessing = [];
         this.hasAccountRequestsPendingProcessingLoadingFailed = true;
         this.statusMessageService.showErrorToast(resp.error.message);
       });
-  }
-
-  /**
-   * Sets the panel status to EDITING.
-   */
-  editAccountRequest(accountRequestTab: AccountRequestTab): void {
-    accountRequestTab.errorMessage = '';
-    accountRequestTab.panelStatus = ProcessAccountRequestPanelStatus.EDITING;
-  }
-
-  /**
-   * Sets the panel status to SUBMITTED.
-   */
-  cancelEditAccountRequest(accountRequestTab: AccountRequestTab): void {
-    accountRequestTab.errorMessage = '';
-    accountRequestTab.panelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
-  }
-
-  /**
-   * Updates the account request in the tab.
-   */
-  saveAccountRequest(accountRequestTab: AccountRequestTab, editedInfo: EditedAccountRequestInfoModel): void {
-    accountRequestTab.isSavingChanges = true;
-    const accountRequest: AccountRequest = accountRequestTab.accountRequest;
-    const updateRequest: AccountRequestUpdateRequest = {
-      instructorName: editedInfo.editedName,
-      instructorEmail: editedInfo.editedEmail,
-      instructorInstitute: editedInfo.editedInstitute,
-    };
-    this.accountService.updateAccountRequest(accountRequest.email, accountRequest.institute, updateRequest)
-      .pipe(
-        finalize(() => {
-          accountRequestTab.isSavingChanges = false;
-      }))
-      .subscribe((resp: AccountRequest) => {
-        accountRequestTab.accountRequest = resp;
-        accountRequestTab.errorMessage = '';
-        accountRequestTab.panelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
-        this.statusMessageService.showSuccessToast('Account request successfully updated.');
-      }, (resp: ErrorMessageOutput) => {
-        accountRequestTab.errorMessage = resp.error.message;
-        this.statusMessageService.showErrorToast('Failed to update account request.');
-      });
-  }
-
-  /**
-   * Approves the account request in the tab.
-   */
-  approveAccountRequest(accountRequestTab: AccountRequestTab): void {
-    accountRequestTab.isSavingChanges = true;
-    const accountRequest: AccountRequest = accountRequestTab.accountRequest;
-    this.accountService.approveAccountRequest(accountRequest.email, accountRequest.institute)
-      .pipe(
-        finalize(() => {
-          accountRequestTab.isSavingChanges = false;
-        }))
-      .subscribe((resp: AccountRequest) => {
-        accountRequestTab.accountRequest = resp;
-        accountRequestTab.errorMessage = '';
-        accountRequestTab.panelStatus = ProcessAccountRequestPanelStatus.APPROVED;
-        this.statusMessageService.showSuccessToast('Account request successfully approved.');
-      }, (resp: ErrorMessageOutput) => {
-        accountRequestTab.errorMessage = resp.error.message;
-        this.statusMessageService.showErrorToast('Failed to approve account request.');
-      });
-  }
-
-  /**
-   * Rejects the account request in the tab.
-   */
-  rejectAccountRequest(accountRequestTab: AccountRequestTab): void {
-    accountRequestTab.isSavingChanges = true;
-    const accountRequest: AccountRequest = accountRequestTab.accountRequest;
-    this.accountService.rejectAccountRequest(accountRequest.email, accountRequest.institute)
-      .pipe(
-        finalize(() => {
-          accountRequestTab.isSavingChanges = false;
-        }))
-      .subscribe((resp: AccountRequest) => {
-        accountRequestTab.accountRequest = resp;
-        accountRequestTab.errorMessage = '';
-        accountRequestTab.panelStatus = ProcessAccountRequestPanelStatus.REJECTED;
-        this.statusMessageService.showSuccessToast('Account request successfully rejected.');
-      }, (resp: ErrorMessageOutput) => {
-        accountRequestTab.errorMessage = resp.error.message;
-        this.statusMessageService.showErrorToast('Failed to reject account request.');
-      });
-  }
-
-  /**
-   * Deletes the account request in the tab.
-   */
-  deleteAccountRequest(accountRequestTab: AccountRequestTab, index: number): void {
-    accountRequestTab.isSavingChanges = true;
-    const accountRequest: AccountRequest = accountRequestTab.accountRequest;
-    this.accountService.deleteAccountRequest(accountRequest.email, accountRequest.institute)
-      .subscribe(() => {
-        accountRequestTab.errorMessage = '';
-        this.accountRequestPendingProcessingTabs.splice(index, 1);
-        this.statusMessageService.showSuccessToast('Account request successfully deleted.');
-      }, (resp: ErrorMessageOutput) => {
-        accountRequestTab.isSavingChanges = false;
-        accountRequestTab.errorMessage = resp.error.message;
-        this.statusMessageService.showErrorToast('Failed to delete account request.');
-      });
-  }
-
-  /**
-   * Resets the account request in the tab.
-   */
-  resetAccountRequest(accountRequestTab: AccountRequestTab): void {
-    accountRequestTab.isSavingChanges = true;
-    const accountRequest: AccountRequest = accountRequestTab.accountRequest;
-    this.accountService.resetAccountRequest(accountRequest.email, accountRequest.institute)
-      .pipe(
-        finalize(() => {
-          accountRequestTab.isSavingChanges = false;
-        }))
-      .subscribe((resp: AccountRequest) => {
-        accountRequestTab.accountRequest = resp;
-        accountRequestTab.errorMessage = '';
-        accountRequestTab.panelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
-        this.statusMessageService.showSuccessToast('Account request successfully reset.');
-      }, (resp: ErrorMessageOutput) => {
-        accountRequestTab.errorMessage = resp.error.message;
-        this.statusMessageService.showErrorToast('Failed to reset account request.');
-      });
-  }
-
-  /**
-   * Toggles the specific account request card.
-   */
-  toggleCard(accountRequestTab: AccountRequestTab): void {
-    accountRequestTab.isTabExpanded = !accountRequestTab.isTabExpanded;
   }
 
   /**
@@ -259,7 +102,7 @@ export class AdminRequestsPageComponent implements OnInit {
     }
 
     this.hasQueried = true;
-    this.accountRequestWithinPeriodTabs = [];
+    this.accountRequestsWithinPeriod = [];
     this.hasAccountRequestsWithinPeriodLoadingFailed = false;
     this.isLoadingAccountRequestsWithinPeriod = true;
 
@@ -272,37 +115,13 @@ export class AdminRequestsPageComponent implements OnInit {
         this.isLoadingAccountRequestsWithinPeriod = false;
       }))
       .subscribe((resp: AccountRequests) => {
-        resp.accountRequests.forEach((ar: AccountRequest) => {
-          const accountRequestTab: AccountRequestTab = {
-            accountRequest: ar,
-            isTabExpanded: false,
-            panelStatus: this.getPanelStatusFromAccountRequestStatus(ar.status),
-            isSavingChanges: false,
-            errorMessage: '',
-          };
-          this.accountRequestWithinPeriodTabs.push(accountRequestTab);
-        });
+        this.accountRequestsWithinPeriod = resp.accountRequests;
         // TODO: sort account requests
       }, (resp: ErrorMessageOutput) => {
-        this.accountRequestWithinPeriodTabs = [];
+        this.accountRequestsWithinPeriod = [];
         this.hasAccountRequestsWithinPeriodLoadingFailed = true;
         this.statusMessageService.showErrorToast(resp.error.message);
       });
-  }
-
-  getPanelStatusFromAccountRequestStatus(arStatus : AccountRequestStatus): ProcessAccountRequestPanelStatus {
-    switch (arStatus) {
-      case AccountRequestStatus.SUBMITTED:
-        return ProcessAccountRequestPanelStatus.SUBMITTED;
-      case AccountRequestStatus.APPROVED:
-        return ProcessAccountRequestPanelStatus.APPROVED;
-      case AccountRequestStatus.REJECTED:
-        return ProcessAccountRequestPanelStatus.REJECTED;
-      case AccountRequestStatus.REGISTERED:
-        return ProcessAccountRequestPanelStatus.REGISTERED;
-      default:
-        return ProcessAccountRequestPanelStatus.UNDEFINED;
-    }
   }
 
 }
--- src/web/app/pages-admin/admin-requests-page/process-account-request-panel/process-account-request-panel.component.ts
+++ src/web/app/pages-admin/admin-requests-page/process-account-request-panel/process-account-request-panel.component.ts
@@ -1,6 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { AccountService } from '../../../../services/account.service';
+import { StatusMessageService } from '../../../../services/status-message.service';
 import { TimezoneService } from '../../../../services/timezone.service';
 import { AccountRequest, AccountRequestStatus } from '../../../../types/api-output';
+import { AccountRequestUpdateRequest } from '../../../../types/api-request';
+import { collapseAnim } from '../../../components/teammates-common/collapse-anim';
+import { ErrorMessageOutput } from '../../../error-message-output';
 
 export enum ProcessAccountRequestPanelStatus {
   SUBMITTED,
@@ -8,13 +14,7 @@ export enum ProcessAccountRequestPanelStatus {
   APPROVED,
   REJECTED,
   REGISTERED,
-  UNDEFINED, // TODO: this is only for old account requests with no status attribute
-}
-
-export interface EditedAccountRequestInfoModel {
-  editedName: string;
-  editedInstitute: string;
-  editedEmail: string;
+  UNDEFINED,
 }
 
 /**
@@ -24,6 +24,7 @@ export interface EditedAccountRequestInfoModel {
   selector: 'tm-process-account-request-panel',
   templateUrl: './process-account-request-panel.component.html',
   styleUrls: ['./process-account-request-panel.component.scss'],
+  animations: [collapseAnim],
 })
 export class ProcessAccountRequestPanelComponent implements OnInit {
 
@@ -31,86 +32,182 @@ export class ProcessAccountRequestPanelComponent implements OnInit {
   accountRequest!: AccountRequest;
 
   @Input()
+  index: number | undefined;
+
+  @Input()
+  isTabExpanded: boolean = true;
+
+  @Input()
+  showPanelHeader: boolean = true;
+
+  @Input()
+  panelHeaderColor: string = 'primary';
+
   panelStatus: ProcessAccountRequestPanelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
-
-  @Input()
   isSavingChanges: boolean = false;
-
-  @Input()
   errorMessage: string = '';
-
-  @Output()
-  editAccountRequestEvent: EventEmitter<void> = new EventEmitter();
-
-  @Output()
-  cancelEditAccountRequestEvent: EventEmitter<void> = new EventEmitter();
-
-  @Output()
-  saveAccountRequestEvent: EventEmitter<EditedAccountRequestInfoModel> = new EventEmitter();
-
-  @Output()
-  approveAccountRequestEvent: EventEmitter<void> = new EventEmitter();
-
-  @Output()
-  rejectAccountRequestEvent: EventEmitter<void> = new EventEmitter();
-
-  @Output()
-  deleteAccountRequestEvent: EventEmitter<void> = new EventEmitter();
-
-  @Output()
-  resetAccountRequestEvent: EventEmitter<void> = new EventEmitter();
-
-  // enums
-  ProcessAccountRequestPanelStatus: typeof ProcessAccountRequestPanelStatus = ProcessAccountRequestPanelStatus;
-  AccountRequestStatus: typeof AccountRequestStatus = AccountRequestStatus;
 
   editedName!: string;
   editedInstitute!: string;
   editedEmail!: string;
 
+  // enums
+  ProcessAccountRequestPanelStatus: typeof ProcessAccountRequestPanelStatus = ProcessAccountRequestPanelStatus;
+  AccountRequestStatus: typeof AccountRequestStatus = AccountRequestStatus;
+
   timezone: string = '';
 
-  constructor(private timezoneService: TimezoneService) {
+  constructor(private accountService: AccountService,
+              private statusMessageService: StatusMessageService,
+              private timezoneService: TimezoneService) {
   }
 
   ngOnInit(): void {
+    this.panelStatus = this.getPanelStatusFromAccountRequestStatus(this.accountRequest.status);
     this.timezone = this.timezoneService.guessTimezone() || 'UTC';
   }
 
   editAccountRequest(): void {
-    this.editAccountRequestEvent.emit();
     this.editedName = this.accountRequest!.name;
     this.editedInstitute = this.accountRequest!.institute;
     this.editedEmail = this.accountRequest!.email;
+
+    this.errorMessage = '';
+    this.panelStatus = ProcessAccountRequestPanelStatus.EDITING;
   }
 
   cancelEditAccountRequest(): void {
-    this.cancelEditAccountRequestEvent.emit();
+    this.errorMessage = '';
+    this.panelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
   }
 
+  /**
+   * Updates the account request.
+   */
   saveAccountRequest(): void {
-    const editedInfo: EditedAccountRequestInfoModel = {
-      editedName: this.editedName,
-      editedInstitute: this.editedInstitute,
-      editedEmail: this.editedEmail,
+    this.isSavingChanges = true;
+    const updateRequest: AccountRequestUpdateRequest = {
+      instructorName: this.editedName,
+      instructorEmail: this.editedEmail,
+      instructorInstitute: this.editedInstitute,
     };
-    this.saveAccountRequestEvent.emit(editedInfo);
+    this.accountService.updateAccountRequest(this.accountRequest.email, this.accountRequest.institute, updateRequest)
+      .pipe(
+        finalize(() => {
+          this.isSavingChanges = false;
+        }))
+      .subscribe((resp: AccountRequest) => {
+        this.accountRequest = resp;
+        this.errorMessage = '';
+        this.panelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
+        this.statusMessageService.showSuccessToast('Account request successfully updated.');
+      }, (resp: ErrorMessageOutput) => {
+        this.errorMessage = resp.error.message;
+        this.statusMessageService.showErrorToast('Failed to update account request.');
+      });
   }
 
+  /**
+   * Approves the account request.
+   */
   approveAccountRequest(): void {
-    this.approveAccountRequestEvent.emit();
+    this.isSavingChanges = true;
+    this.accountService.approveAccountRequest(this.accountRequest.email, this.accountRequest.institute)
+      .pipe(
+        finalize(() => {
+          this.isSavingChanges = false;
+        }))
+      .subscribe((resp: AccountRequest) => {
+        this.accountRequest = resp;
+        this.errorMessage = '';
+        this.panelStatus = ProcessAccountRequestPanelStatus.APPROVED;
+        this.statusMessageService.showSuccessToast('Account request successfully approved.');
+      }, (resp: ErrorMessageOutput) => {
+        this.errorMessage = resp.error.message;
+        this.statusMessageService.showErrorToast('Failed to approve account request.');
+      });
   }
 
+  /**
+   * Rejects the account request.
+   */
   rejectAccountRequest(): void {
-    this.rejectAccountRequestEvent.emit();
+    this.isSavingChanges = true;
+    this.accountService.rejectAccountRequest(this.accountRequest.email, this.accountRequest.institute)
+      .pipe(
+        finalize(() => {
+          this.isSavingChanges = false;
+        }))
+      .subscribe((resp: AccountRequest) => {
+        this.accountRequest = resp;
+        this.errorMessage = '';
+        this.panelStatus = ProcessAccountRequestPanelStatus.REJECTED;
+        this.statusMessageService.showSuccessToast('Account request successfully rejected.');
+      }, (resp: ErrorMessageOutput) => {
+        this.errorMessage = resp.error.message;
+        this.statusMessageService.showErrorToast('Failed to reject account request.');
+      });
   }
 
+  /**
+   * Deletes the account request.
+   */
   deleteAccountRequest(): void {
-    this.deleteAccountRequestEvent.emit();
+    this.isSavingChanges = true;
+    this.accountService.deleteAccountRequest(this.accountRequest.email, this.accountRequest.institute)
+      .pipe(
+        finalize(() => {
+          this.isSavingChanges = false;
+        }))
+      .subscribe(() => {
+        this.errorMessage = '';
+        this.panelStatus = ProcessAccountRequestPanelStatus.UNDEFINED;
+        this.statusMessageService.showSuccessToast('Account request successfully deleted.');
+      }, (resp: ErrorMessageOutput) => {
+        this.isSavingChanges = false;
+        this.errorMessage = resp.error.message;
+        this.statusMessageService.showErrorToast('Failed to delete account request.');
+      });
   }
 
+  /**
+   * Resets the account request.
+   */
   resetAccountRequest(): void {
-    this.resetAccountRequestEvent.emit();
+    this.isSavingChanges = true;
+    this.accountService.resetAccountRequest(this.accountRequest.email, this.accountRequest.institute)
+      .pipe(
+        finalize(() => {
+          this.isSavingChanges = false;
+        }))
+      .subscribe((resp: AccountRequest) => {
+        this.accountRequest = resp;
+        this.errorMessage = '';
+        this.panelStatus = ProcessAccountRequestPanelStatus.SUBMITTED;
+        this.statusMessageService.showSuccessToast('Account request successfully reset.');
+      }, (resp: ErrorMessageOutput) => {
+        this.errorMessage = resp.error.message;
+        this.statusMessageService.showErrorToast('Failed to reset account request.');
+      });
+  }
+
+  toggleCard(): void {
+    this.isTabExpanded = !this.isTabExpanded;
+  }
+
+  getPanelStatusFromAccountRequestStatus(arStatus : AccountRequestStatus): ProcessAccountRequestPanelStatus {
+    switch (arStatus) {
+      case AccountRequestStatus.SUBMITTED:
+        return ProcessAccountRequestPanelStatus.SUBMITTED;
+      case AccountRequestStatus.APPROVED:
+        return ProcessAccountRequestPanelStatus.APPROVED;
+      case AccountRequestStatus.REJECTED:
+        return ProcessAccountRequestPanelStatus.REJECTED;
+      case AccountRequestStatus.REGISTERED:
+        return ProcessAccountRequestPanelStatus.REGISTERED;
+      default:
+        return ProcessAccountRequestPanelStatus.UNDEFINED;
+    }
   }
 
 }
