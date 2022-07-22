f7334bed1e9af5a9f9c030c43a473743cdecd5e6
xyliew25 <e0550381@u.nus.edu>
2022-07-22 18:21:09 +0800

    Add LoginPageComponent test

--- src/web/app/login-page.component.spec.ts
+++ src/web/app/login-page.component.spec.ts
@@ -1,5 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NgxCaptchaModule } from 'ngx-captcha';
+import { AjaxLoadingModule } from './components/ajax-loading/ajax-loading.module';
+import { LoadingSpinnerModule } from './components/loading-spinner/loading-spinner.module';
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
@@ -9,7 +15,15 @@ describe('LoginPageComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
-      imports: [RouterTestingModule],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        LoadingSpinnerModule,
+        NgxCaptchaModule,
+        AjaxLoadingModule,
+        AngularFireModule.initializeApp({}),
+      ],
     })
         .compileComponents();
   }));
