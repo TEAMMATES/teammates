e6df6069abd9b3fd25c320d555cf68ccee9134f8
fans2619 <fans2619@gmail.com>
2022-07-24 16:39:01 +0800

    Remove user-facing account request form

--- src/web/app/pages-static/request-page/request-page.module.ts
+++ src/web/app/pages-static/request-page/request-page.module.ts
@@ -1,9 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { NgxCaptchaModule } from 'ngx-captcha';
-import { AjaxLoadingModule } from '../../components/ajax-loading/ajax-loading.module';
 import { RequestPageComponent } from './request-page.component';
 
 const routes: Routes = [
@@ -25,10 +22,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     RouterModule.forChild(routes),
-    NgxCaptchaModule,
-    AjaxLoadingModule,
   ],
 })
 export class RequestPageModule { }
