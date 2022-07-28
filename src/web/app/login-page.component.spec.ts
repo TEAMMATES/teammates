98426806c4cdd5fbe84071d28dd32ea2959f40ed
xyliew25 <e0550381@u.nus.edu>
2022-07-28 23:49:49 +0800

    Add frontend component tests

--- src/web/app/login-page.component.spec.ts
+++ src/web/app/login-page.component.spec.ts
@@ -4,6 +4,10 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxCaptchaModule } from 'ngx-captcha';
+import { AuthService } from '../services/auth.service';
+import { StatusMessageService } from '../services/status-message.service';
+import SpyInstance = jest.SpyInstance;
+import { throwError } from 'rxjs';
 import { AjaxLoadingModule } from './components/ajax-loading/ajax-loading.module';
 import { LoadingSpinnerModule } from './components/loading-spinner/loading-spinner.module';
 import { LoginPageComponent } from './login-page.component';
@@ -11,6 +15,8 @@ import { LoginPageComponent } from './login-page.component';
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
+  let authService: AuthService;
+  let statusMessageService: StatusMessageService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -24,6 +30,10 @@ describe('LoginPageComponent', () => {
         AjaxLoadingModule,
         AngularFireModule.initializeApp({}),
       ],
+      providers: [
+        AuthService,
+        StatusMessageService,
+      ],
     })
         .compileComponents();
   }));
@@ -31,10 +41,83 @@ describe('LoginPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    statusMessageService = TestBed.inject(StatusMessageService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show error toast if login with Google is unsuccessful', () => {
+    jest.spyOn(authService, 'isLogInWithEmailLink').mockResolvedValue(true);
+    window.localStorage.setItem('emailForSignIn', 'abc@gmail.com');
+    jest.spyOn(authService, 'logInWithEmailLink').mockRejectedValue(new Error());
+    const spy: SpyInstance = jest.spyOn(statusMessageService, 'showErrorToast')
+        .mockImplementation((args: string) => {
+          expect(args).toEqual('Login with email is unsuccessful. Please try again.');
+        });
+    fixture.detectChanges();
+    component.ngOnInit();
+    expect(spy).toBeCalled();
+  });
+
+  it('should show error toast if login with email is unsuccessful', () => {
+    jest.spyOn(authService, 'isLogInWithEmailLink').mockResolvedValue(false);
+    jest.spyOn(authService, 'getRedirectResult').mockRejectedValue(new Error());
+    const spy: SpyInstance = jest.spyOn(statusMessageService, 'showErrorToast')
+        .mockImplementation((args: string) => {
+          expect(args).toEqual('Login with Google is unsuccessful. Please try again.');
+        });
+    fixture.detectChanges();
+    component.ngOnInit();
+    expect(spy).toBeCalled();
+  });
+
+  it('should show error toast if cannot redirect to Google login page', () => {
+    jest.spyOn(authService, 'logInWithRedirect').mockRejectedValue(new Error());
+    const spy: SpyInstance = jest.spyOn(statusMessageService, 'showErrorToast')
+        .mockImplementation((args: string) => {
+          expect(args).toEqual('Cannot redirect to Google login page. Please try again.');
+        });
+    fixture.detectChanges();
+    component.logInWithGoogle();
+    expect(spy).toBeCalled();
+  });
+
+  it('should show error toast if login form is invalid', () => {
+    component.captchaResponse = '';
+    component.formLogin.setValue({
+      email: '',
+      recaptcha: '',
+    })
+    const spy: SpyInstance = jest.spyOn(statusMessageService, 'showErrorToast')
+        .mockImplementation((args: string) => {
+          expect(args).toEqual('Please enter a valid email address and click the reCAPTCHA before submitting.');
+        });
+    fixture.detectChanges();
+    component.logInWithEmail(component.formLogin);
+    expect(spy).toBeCalled();
+  });
+
+  it('should show error toast if cannot send login email', () => {
+    jest.spyOn(authService, 'sendLoginEmail').mockReturnValue(throwError({
+      error: {
+        message: 'This is the error message.',
+      },
+    }));
+    component.captchaResponse = 'captchaResponse';
+    component.formLogin.setValue({
+      email: 'abc@gmail.com',
+      recaptcha: 'recaptcha',
+    })
+    const spy: SpyInstance = jest.spyOn(statusMessageService, 'showErrorToast')
+        .mockImplementation((args: string) => {
+          expect(args).toEqual('This is the error message.');
+        });
+    fixture.detectChanges();
+    component.logInWithEmail(component.formLogin);
+    expect(spy).toBeCalled();
   });
 });
--- src/web/services/auth.service.spec.ts
+++ src/web/services/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { environment } from '../environments/environment';
 import { ResourceEndpoints } from '../types/api-const';
+import { Intent } from '../types/api-request';
 import { AuthService } from './auth.service';
 import { HttpRequestService } from './http-request.service';
 
@@ -13,6 +14,7 @@ describe('AuthService', () => {
   beforeEach(() => {
     spyHttpRequestService = {
       get: jest.fn(),
+      post: jest.fn(),
     };
     TestBed.configureTestingModule({
       providers: [
@@ -29,6 +31,28 @@ describe('AuthService', () => {
   it('should execute getAuthUser', () => {
     service.getAuthUser();
     expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.AUTH, { frontendUrl });
+  });
+
+  it('should execute getAuthRegkeyValidity', () => {
+    const key: string = 'key';
+    const intent: Intent = Intent.FULL_DETAIL;
+    const paramMap: Record<string, string> = { key, intent };
+    service.getAuthRegkeyValidity(key, intent);
+    expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.AUTH_REGKEY, paramMap);
+  });
+
+  it('should execute sendLoginEmail', () => {
+    const userEmail: string = 'abc@gmail.com';
+    const continueUrl: string = 'continueUrl';
+    const captchaResponse: string = 'captchaResponse';
+    const queryParam = { userEmail, continueUrl, captchaResponse };
+    const paramMap: Record<string, string> = {
+      useremail: queryParam.userEmail,
+      continueurl: queryParam.continueUrl,
+      captcharesponse: queryParam.captchaResponse,
+    };
+    service.sendLoginEmail(queryParam);
+    expect(spyHttpRequestService.post).toHaveBeenCalledWith(ResourceEndpoints.LOGIN_EMAIL, paramMap);
   });
 
 });
