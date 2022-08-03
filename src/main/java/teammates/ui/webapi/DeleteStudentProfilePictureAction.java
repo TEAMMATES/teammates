27a4e801a1fa0856344f33ebc15d459aedc0159c
Wilson Kurniawan <wkurniawan.92@gmail.com>
2022-08-03 11:55:23 +0800

    Remove trailing `@gmail.com` when checking for user ID equality

--- src/main/java/teammates/ui/webapi/DeleteStudentProfilePictureAction.java
+++ src/main/java/teammates/ui/webapi/DeleteStudentProfilePictureAction.java
@@ -18,7 +18,7 @@ class DeleteStudentProfilePictureAction extends Action {
             throw new UnauthorizedAccessException("Student privilege is required to update this resource.");
         }
         String googleId = getNonNullRequestParamValue(Const.ParamsNames.STUDENT_ID);
-        if (!userInfo.id.equals(googleId)) {
+        if (!userInfo.id.replaceFirst("@gmail.com$", "").equals(googleId.replaceFirst("@gmail.com$", ""))) {
             throw new UnauthorizedAccessException("You are not authorized to delete this student's profile.");
         }
     }
