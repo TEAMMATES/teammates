baccf9987c8ec9414db4f63b70e81e050e69c926
fans2619 <fans2619@gmail.com>
2022-08-03 19:26:15 +0800

    Revert additions in form-validator.ts as used in request form only

--- src/web/types/form-validator.ts
+++ src/web/types/form-validator.ts
@@ -33,30 +33,4 @@ export enum FormValidator {
    * Max length for the 'E-mail Address' field.
    */
   EMAIL_MAX_LENGTH = ApiConst.EMAIL_MAX_LENGTH,
-
-  /**
-   * Max length for the 'Institute' field.
-   */
-  INSTITUTE_NAME_MAX_LENGTH = ApiConst.INSTITUTE_NAME_MAX_LENGTH,
-
-  /**
-   * Max length for the 'Institute' field in the account request form.
-   */
-  ACCOUNT_REQUEST_INSTITUTE_NAME_MAX_LENGTH = ApiConst.ACCOUNT_REQUEST_INSTITUTE_NAME_MAX_LENGTH,
-
-  /**
-   * Max length for the 'Country' field in the account request form.
-   */
-  ACCOUNT_REQUEST_COUNTRY_NAME_MAX_LENGTH = ApiConst.ACCOUNT_REQUEST_COUNTRY_NAME_MAX_LENGTH,
-
-  /**
-   * Max length for the 'Home Page URL' field in the account request form.
-   */
-  ACCOUNT_REQUEST_HOME_PAGE_URL_MAX_LENGTH = ApiConst.ACCOUNT_REQUEST_HOME_PAGE_URL_MAX_LENGTH,
-
-  /**
-   * Max length for the 'Other Comments' field in the account request form.
-   */
-  ACCOUNT_REQUEST_COMMENTS_MAX_LENGTH = ApiConst.ACCOUNT_REQUEST_COMMENTS_MAX_LENGTH,
-
 }
