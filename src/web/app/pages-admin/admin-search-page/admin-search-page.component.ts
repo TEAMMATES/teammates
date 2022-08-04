060fb77e0daa3671f29e85fabc00f93b257f3972
fans2619 <fans2619@gmail.com>
2022-08-04 16:03:24 +0800

    Refine admin-search-page.component.ts

--- src/web/app/pages-admin/admin-search-page/admin-search-page.component.ts
+++ src/web/app/pages-admin/admin-search-page/admin-search-page.component.ts
@@ -17,7 +17,7 @@ import { SimpleModalService } from '../../../services/simple-modal.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { StudentService } from '../../../services/student.service';
 import { ApiConst } from '../../../types/api-const';
-import { AccountRequest, AccountRequestStatus, Email, MessageOutput, RegenerateKey } from '../../../types/api-output';
+import { AccountRequest, AccountRequestStatus, Email, RegenerateKey } from '../../../types/api-output';
 import { SimpleModalType } from '../../components/simple-modal/simple-modal-type';
 import { collapseAnim } from '../../components/teammates-common/collapse-anim';
 import { ErrorMessageOutput } from '../../error-message-output';
@@ -254,46 +254,9 @@ export class AdminSearchPageComponent {
     }, () => {});
   }
 
-  // TODO: this method is no longer used
-  resetAccountRequest(accountRequest: AccountRequestSearchResult): void {
-    const modalContent = `Are you sure you want to reset the account request for
-        <strong>${accountRequest.name}</strong> with email <strong>${accountRequest.email}</strong> from
-        <strong>${accountRequest.institute}</strong>?
-        An email with the account registration link will also be sent to the instructor.`;
-    const modalRef: NgbModalRef = this.simpleModalService.openConfirmationModal(
-        `Reset account request for <strong>${accountRequest.name}</strong>?`, SimpleModalType.WARNING, modalContent);
-
-    modalRef.result.then(() => {
-      this.accountService.resetAccountRequest(accountRequest.email, accountRequest.institute)
-        .subscribe(() => {
-          this.statusMessageService
-            .showSuccessToast(`Reset successful. An email has been sent to ${accountRequest.email}.`);
-          // accountRequest.status = '';
-        }, (resp: ErrorMessageOutput) => {
-          this.statusMessageService.showErrorToast(resp.error.message);
-        });
-    }, () => {});
-  }
-
-  // TODO: this method is no longer used
-  deleteAccountRequest(accountRequest: AccountRequestSearchResult): void {
-    const modalContent: string = `Are you sure you want to delete the account request for
-        <strong>${accountRequest.name}</strong> with email <strong>${accountRequest.email}</strong> from
-        <strong>${accountRequest.institute}</strong>?`;
-    const modalRef: NgbModalRef = this.simpleModalService.openConfirmationModal(
-        `Delete account request for <strong>${accountRequest.name}</strong>?`, SimpleModalType.WARNING, modalContent);
-
-    modalRef.result.then(() => {
-      this.accountService.deleteAccountRequest(accountRequest.email, accountRequest.institute)
-      .subscribe((resp: MessageOutput) => {
-        this.statusMessageService.showSuccessToast(resp.message);
-        this.accountRequests = this.accountRequests.filter((x: AccountRequestSearchResult) => x !== accountRequest);
-      }, (resp: ErrorMessageOutput) => {
-        this.statusMessageService.showErrorToast(resp.error.message);
-      });
-    }, () => {});
-  }
-
+  /**
+   * Opens a modal containing the panel for managing the account request.
+   */
   manageAccountRequest(accountRequest: AccountRequestSearchResult): void {
     this.isManageAccountRequestModalLoading = true;
     this.accountRequestManaged = undefined;
