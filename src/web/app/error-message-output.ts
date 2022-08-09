e334b7002ccf483e1e6308860b43572a21fc5f95
fans2619 <fans2619@gmail.com>
2022-08-09 18:21:35 +0800

    Remove AccountRequestCreateErrorResultsWrapper

--- src/web/app/error-message-output.ts
+++ src/web/app/error-message-output.ts
@@ -1,17 +1,9 @@
-import { AccountRequestCreateErrorResults, MessageOutput } from '../types/api-output';
+import { MessageOutput } from '../types/api-output';
 
 /**
  * Encapsulates error message from back-end API endpoints.
  */
 export interface ErrorMessageOutput {
   error: MessageOutput;
-  status: number;
-}
-
-/**
- * Encapsulates account request create error results from back-end API endpoint.
- */
-export interface AccountRequestCreateErrorResultsWrapper {
-  error: AccountRequestCreateErrorResults;
   status: number;
 }
