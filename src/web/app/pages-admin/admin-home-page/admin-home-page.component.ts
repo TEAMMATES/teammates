66ec25036f379bfa565dafb1a5eb96f9e7d23733
fans2619 <fans2619@gmail.com>
2022-08-09 19:01:31 +0800

    Remove pureInstitute and pureCountry in AccountRequestAttributes

--- src/web/app/pages-admin/admin-home-page/admin-home-page.component.ts
+++ src/web/app/pages-admin/admin-home-page/admin-home-page.component.ts
@@ -9,7 +9,7 @@ import { SimpleModalService } from '../../../services/simple-modal.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { Account, Accounts, Courses, JoinLink } from '../../../types/api-output';
 import { SimpleModalType } from '../../components/simple-modal/simple-modal-type';
-import { AccountRequestCreateErrorResultsWrapper, ErrorMessageOutput } from '../../error-message-output';
+import { ErrorMessageOutput } from '../../error-message-output';
 import { InstructorData, RegisteredInstructorAccountData } from './instructor-data';
 
 /**
@@ -124,19 +124,11 @@ export class AdminHomePageComponent {
           instructor.statusCode = 200;
           instructor.joinLink = resp.joinLink;
           this.activeRequests -= 1;
-        }, (resp: ErrorMessageOutput | AccountRequestCreateErrorResultsWrapper) => {
+        }, (resp: ErrorMessageOutput) => {
+          // resp should not be of type AccountRequestCreateErrorResultsWrapper for ADMIN_CREATE
           instructor.status = 'FAIL';
           instructor.statusCode = resp.status;
-          if ('message' in resp.error) {
-            // resp is ErrorMessageOutput
-            instructor.message = resp.error.message;
-          } else {
-            // resp is AccountRequestCreateErrorResultsWrapper
-            instructor.message = [resp.error.invalidNameMessage, resp.error.invalidEmailMessage,
-              resp.error.invalidInstituteMessage]
-              .filter(Boolean)
-              .join(' ');
-          }
+          instructor.message = resp.error.message;
           this.activeRequests -= 1;
         });
   }
