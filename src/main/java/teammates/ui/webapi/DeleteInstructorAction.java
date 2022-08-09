49ba4da1715e37744da10fc24d005359469e51f4
Matthew Lim <mklim@usc.edu>
2022-08-08 19:22:57 -0700

    Modified servlet behavior to reject admin deletion of the last instructor

--- src/main/java/teammates/ui/webapi/DeleteInstructorAction.java
+++ src/main/java/teammates/ui/webapi/DeleteInstructorAction.java
@@ -51,7 +51,7 @@ class DeleteInstructorAction extends Action {
         }
 
         // Deleting last instructor from the course is not allowed if you're not the admin
-        if (!userInfo.isAdmin && !hasAlternativeInstructor(courseId, instructor.getEmail())) {
+        if (!hasAlternativeInstructor(courseId, instructor.getEmail())) {
             throw new InvalidOperationException(
                     "The instructor you are trying to delete is the last instructor in the course. "
                     + "Deleting the last instructor from the course is not allowed.");
