1c9538878728b0a888a407ca1f9f4fc98ada35fb
Matthew Lim <mklim@usc.edu>
2022-08-13 16:53:11 -0700

    Updated commenting to make the purpose of a function more clear
    Co-authored-by: Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>

--- src/main/java/teammates/ui/webapi/DeleteInstructorAction.java
+++ src/main/java/teammates/ui/webapi/DeleteInstructorAction.java
@@ -50,7 +50,7 @@ class DeleteInstructorAction extends Action {
             return new JsonResult("Instructor is successfully deleted.");
         }
 
-        // Deleting last instructor from the course is not allowed if you're not the admin
+        // Deleting last instructor from the course is not allowed (even by admins)
         if (!hasAlternativeInstructor(courseId, instructor.getEmail())) {
             throw new InvalidOperationException(
                     "The instructor you are trying to delete is the last instructor in the course. "
