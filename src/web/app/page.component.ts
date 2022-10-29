79c2c8e74488e4304e9c61338b30b07633dc89b3
Danhui Chang <u7386105@anu.edu.au>
2022-10-29 10:38:56 +0800

    Add dark model into web to trigger

--- src/web/app/page.component.ts
+++ src/web/app/page.component.ts
@@ -139,6 +139,11 @@ export class PageComponent {
    * Method that checks if current page has active modals and close them.
    */
   closeModal(): void {
+    /**
+    * Method to toggle dark mode
+    */
+    toggleDarkTheme(): void{
+      document.body.classList.toggle('dark-theme');}
 
     if (this.ngbModal.hasOpenModals()) {
       this.ngbModal.dismissAll();
