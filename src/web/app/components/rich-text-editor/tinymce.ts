a167fdc1265452ce48eb7660451592f06ba62a5e
Wilson Kurniawan <wkurniawan.92@gmail.com>
2023-01-15 13:17:28 +0800

    Update tinymce library

--- src/web/app/components/rich-text-editor/tinymce.ts
+++ src/web/app/components/rich-text-editor/tinymce.ts
@@ -1,4 +1,4 @@
 /**
  * Base URL for tinymce libraries.
  */
-export const TINYMCE_BASE_URL: string = 'https://cdn.jsdelivr.net/npm/tinymce@5.10.5';
+export const TINYMCE_BASE_URL: string = 'https://cdn.jsdelivr.net/npm/tinymce@6.3.1';
