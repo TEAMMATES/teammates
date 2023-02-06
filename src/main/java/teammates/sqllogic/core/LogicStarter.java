f60b0166029ebeb6392f27b0427f6aadce468e8a
Samuel Fang <samuelfangjw@gmail.com>
2023-02-06 16:19:39 +0800

    [#12048] v9: Skeleton implementation (#12056)

---
+++ src/main/java/teammates/sqllogic/core/LogicStarter.java
@@ -0,0 +1,41 @@
+package teammates.sqllogic.core;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import teammates.common.util.Logger;
+import teammates.storage.sqlapi.CoursesDb;
+import teammates.storage.sqlapi.FeedbackSessionsDb;
+
+/**
+ * Setup in web.xml to register logic classes at application startup.
+ */
+public class LogicStarter implements ServletContextListener {
+
+    private static final Logger log = Logger.getLogger();
+
+    /**
+     * Registers dependencies between different logic classes.
+     */
+    public static void initializeDependencies() {
+        CoursesLogic coursesLogic = CoursesLogic.inst();
+        FeedbackSessionsLogic fsLogic = FeedbackSessionsLogic.inst();
+
+        coursesLogic.initLogicDependencies(CoursesDb.inst(), fsLogic);
+        fsLogic.initLogicDependencies(FeedbackSessionsDb.inst(), coursesLogic);
+
+        log.info("Initialized dependencies between logic classes");
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        // Invoked by Jetty at application startup.
+        initializeDependencies();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        // Nothing to do
+    }
+
+}
---
+++ src/main/java/teammates/storage/sqlapi/EntitiesDb.java
@@ -0,0 +1,52 @@
+
+package teammates.storage.sqlapi;
+
+import teammates.common.util.HibernateUtil;
+import teammates.common.util.JsonUtils;
+import teammates.common.util.Logger;
+import teammates.storage.sqlentity.BaseEntity;
+
+/**
+ * Base class for all classes performing CRUD operations against the database.
+ *
+ * @param <E> subclass of BaseEntity
+ */
+class EntitiesDb<E extends BaseEntity> {
+
+    static final String ERROR_CREATE_ENTITY_ALREADY_EXISTS = "Trying to create an entity that exists: %s";
+    static final String ERROR_UPDATE_NON_EXISTENT = "Trying to update non-existent Entity: ";
+
+    static final Logger log = Logger.getLogger();
+
+    /**
+     * Copy the state of the given object onto the persistent object with the same identifier.
+     * If there is no persistent instance currently associated with the session, it will be loaded.
+     */
+    E merge(E entity) {
+        assert entity != null;
+
+        E newEntity = HibernateUtil.getSessionFactory().getCurrentSession().merge(entity);
+        log.info("Entity saves: " + JsonUtils.toJson(entity));
+        return newEntity;
+    }
+
+    /**
+     * Associate {@code entity} with the persistence context.
+     */
+    void persist(E entity) {
+        assert entity != null;
+
+        HibernateUtil.getSessionFactory().getCurrentSession().persist(entity);
+        log.info("Entity persisted: " + JsonUtils.toJson(entity));
+    }
+
+    /**
+     * Deletes {@code entity} from persistence context.
+     */
+    void delete(E entity) {
+        assert entity != null;
+
+        HibernateUtil.getSessionFactory().getCurrentSession().remove(entity);
+        log.info("Entity deleted: " + JsonUtils.toJson(entity));
+    }
+}
