cd430a8e877e7181189e1c50d1885a01a1ff64c9
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2023-02-12 18:08:20 +0800

    Migrate GetCourseAction.java

--- src/main/java/teammates/ui/webapi/GetCourseAction.java
+++ src/main/java/teammates/ui/webapi/GetCourseAction.java
@@ -4,6 +4,7 @@ import teammates.common.datatransfer.InstructorPermissionSet;
 import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.util.Const;
+import teammates.storage.sqlentity.Course;
 import teammates.ui.output.CourseData;
 
 /**
@@ -24,15 +25,32 @@ class GetCourseAction extends Action {
 
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String entityType = getNonNullRequestParamValue(Const.ParamsNames.ENTITY_TYPE);
-        CourseAttributes course = logic.getCourse(courseId);
 
+        CourseAttributes courseAttributes = logic.getCourse(courseId);
+        if (!courseAttributes.isMigrated()) {
+            if (Const.EntityType.INSTRUCTOR.equals(entityType)) {
+                gateKeeper.verifyAccessible(getPossiblyUnregisteredInstructor(courseId), courseAttributes);
+                return;
+            }
+
+            if (Const.EntityType.STUDENT.equals(entityType)) {
+                gateKeeper.verifyAccessible(getPossiblyUnregisteredStudent(courseId), courseAttributes);
+                return;
+            }
+
+            throw new UnauthorizedAccessException("Student or instructor account is required to access this resource.");
+        } 
+
+        Course _course = sqlLogic.getCourse(courseId);
         if (Const.EntityType.INSTRUCTOR.equals(entityType)) {
-            gateKeeper.verifyAccessible(getPossiblyUnregisteredInstructor(courseId), course);
+            // TODO: Migrate once GateKeeper class is ready.
+            // gateKeeper.verifyAccessible(getPossiblyUnregisteredInstructor(courseId), courseAttributes);
             return;
         }
 
         if (Const.EntityType.STUDENT.equals(entityType)) {
-            gateKeeper.verifyAccessible(getPossiblyUnregisteredStudent(courseId), course);
+            // TODO: Migrate once GateKeeper class is ready.
+            // gateKeeper.verifyAccessible(getPossiblyUnregisteredStudent(courseId), courseAttributes);
             return;
         }
 
@@ -46,14 +64,35 @@ class GetCourseAction extends Action {
         if (courseAttributes == null) {
             throw new EntityNotFoundException("No course with id: " + courseId);
         }
-        CourseData output = new CourseData(courseAttributes);
+        if (!courseAttributes.isMigrated()) {
+            CourseData output = new CourseData(courseAttributes);
+            String entityType = getRequestParamValue(Const.ParamsNames.ENTITY_TYPE);
+            if (Const.EntityType.INSTRUCTOR.equals(entityType)) {
+                InstructorAttributes instructor = getPossiblyUnregisteredInstructor(courseId);
+                if (instructor != null) {
+                    InstructorPermissionSet privilege = constructInstructorPrivileges(instructor, null);
+                    output.setPrivileges(privilege);
+                }
+            } else if (Const.EntityType.STUDENT.equals(entityType)) {
+                output.hideInformationForStudent();
+            }
+            return new JsonResult(output);
+        } 
+
+        Course course = sqlLogic.getCourse(courseId);
+        if (course == null) {
+            throw new EntityNotFoundException("No course with id: " + courseId);
+        }
+        CourseData output = new CourseData(course);
+        
         String entityType = getRequestParamValue(Const.ParamsNames.ENTITY_TYPE);
         if (Const.EntityType.INSTRUCTOR.equals(entityType)) {
-            InstructorAttributes instructor = getPossiblyUnregisteredInstructor(courseId);
-            if (instructor != null) {
-                InstructorPermissionSet privilege = constructInstructorPrivileges(instructor, null);
-                output.setPrivileges(privilege);
-            }
+            // TODO: Migrate once Instructor class is ready.
+            // InstructorAttributes instructor = getPossiblyUnregisteredInstructor(courseId);
+            // if (instructor != null) {
+            //     InstructorPermissionSet privilege = constructInstructorPrivileges(instructor, null);
+            //     output.setPrivileges(privilege);
+            // }
         } else if (Const.EntityType.STUDENT.equals(entityType)) {
             output.hideInformationForStudent();
         }
