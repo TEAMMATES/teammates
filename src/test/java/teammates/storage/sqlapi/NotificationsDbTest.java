97d54b80ebfd20f07d000f318134c2b91a3f5c36
wuqirui <53338059+hhdqirui@users.noreply.github.com>
2023-02-15 12:56:19 +0800

    [#12048] Create SQL logic for CreateNotificationAction and add relevant tests for v9 migration (#12077)

---
+++ src/test/java/teammates/storage/sqlapi/NotificationsDbTest.java
@@ -0,0 +1,101 @@
+package teammates.storage.sqlapi;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.NotificationStyle;
+import teammates.common.datatransfer.NotificationTargetUser;
+import teammates.common.exception.EntityAlreadyExistsException;
+import teammates.common.exception.InvalidParametersException;
+import teammates.common.util.HibernateUtil;
+import teammates.storage.sqlentity.Notification;
+import teammates.test.BaseTestCase;
+
+/**
+ * SUT: {@code NotificationsDb}.
+ */
+public class NotificationsDbTest extends BaseTestCase {
+
+    private NotificationsDb notificationsDb = NotificationsDb.inst();
+
+    private Session session;
+
+    @BeforeMethod
+    public void setUp() {
+        session = mock(Session.class);
+        SessionFactory sessionFactory = mock(SessionFactory.class);
+        HibernateUtil.setSessionFactory(sessionFactory);
+        when(sessionFactory.getCurrentSession()).thenReturn(session);
+    }
+
+    @Test
+    public void testCreateNotification_success() throws EntityAlreadyExistsException, InvalidParametersException {
+        Notification newNotification = new Notification.NotificationBuilder()
+                .withStartTime(Instant.parse("2011-01-01T00:00:00Z"))
+                .withEndTime(Instant.parse("2099-01-01T00:00:00Z"))
+                .withStyle(NotificationStyle.DANGER)
+                .withTargetUser(NotificationTargetUser.GENERAL)
+                .withTitle("A deprecation note")
+                .withMessage("<p>Deprecation happens in three minutes</p>")
+                .build();
+
+        notificationsDb.createNotification(newNotification);
+
+        verify(session, times(1)).persist(newNotification);
+    }
+
+    @Test
+    public void testCreateNotification_invalidNonNullParameters_endTimeIsBeforeStartTime() {
+        Notification invalidNotification = new Notification.NotificationBuilder()
+                .withStartTime(Instant.parse("2011-02-01T00:00:00Z"))
+                .withEndTime(Instant.parse("2011-01-01T00:00:00Z"))
+                .withStyle(NotificationStyle.DANGER)
+                .withTargetUser(NotificationTargetUser.GENERAL)
+                .withTitle("A deprecation note")
+                .withMessage("<p>Deprecation happens in three minutes</p>")
+                .build();
+
+        assertThrows(InvalidParametersException.class, () -> notificationsDb.createNotification(invalidNotification));
+        verify(session, never()).persist(invalidNotification);
+    }
+
+    @Test
+    public void testCreateNotification_invalidNonNullParameters_emptyTitle() {
+        Notification invalidNotification = new Notification.NotificationBuilder()
+                .withStartTime(Instant.parse("2011-01-01T00:00:00Z"))
+                .withEndTime(Instant.parse("2099-01-01T00:00:00Z"))
+                .withStyle(NotificationStyle.DANGER)
+                .withTargetUser(NotificationTargetUser.GENERAL)
+                .withTitle("")
+                .withMessage("<p>Deprecation happens in three minutes</p>")
+                .build();
+
+        assertThrows(InvalidParametersException.class, () -> notificationsDb.createNotification(invalidNotification));
+        verify(session, never()).persist(invalidNotification);
+    }
+
+    @Test
+    public void testCreateNotification_invalidNonNullParameters_emptyMessage() {
+        Notification invalidNotification = new Notification.NotificationBuilder()
+                .withStartTime(Instant.parse("2011-01-01T00:00:00Z"))
+                .withEndTime(Instant.parse("2099-01-01T00:00:00Z"))
+                .withStyle(NotificationStyle.DANGER)
+                .withTargetUser(NotificationTargetUser.GENERAL)
+                .withTitle("A deprecation note")
+                .withMessage("")
+                .build();
+
+        assertThrows(InvalidParametersException.class, () -> notificationsDb.createNotification(invalidNotification));
+        verify(session, never()).persist(invalidNotification);
+    }
+}
