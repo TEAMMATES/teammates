abc1f93ff786e1c175523f9e3872fe74d8e72e61
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2023-02-20 18:48:40 +0800

    Update CalculateUsageStatisticsAction to include both old and new entities

--- src/main/java/teammates/ui/webapi/CalculateUsageStatisticsAction.java
+++ src/main/java/teammates/ui/webapi/CalculateUsageStatisticsAction.java
@@ -9,6 +9,7 @@ import teammates.common.exception.EntityAlreadyExistsException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Logger;
 import teammates.common.util.TimeHelper;
+import teammates.storage.sqlentity.UsageStatistics;
 
 /**
  * Gathers usage-related statistics (e.g. new created entities) in the past defined time period and store in the database.'
@@ -24,26 +25,27 @@ public class CalculateUsageStatisticsAction extends AdminOnlyAction {
         Instant startTime = endTime.minus(COLLECTION_TIME_PERIOD, ChronoUnit.MINUTES);
 
         UsageStatisticsAttributes entitiesStats = logic.calculateEntitiesStatisticsForTimeRange(startTime, endTime);
+        UsageStatistics sqlEntitiesStats = sqlLogic.calculateEntitiesStatisticsForTimeRange(startTime, endTime);
 
         int numEmailsSent = logsProcessor.getNumberOfLogsForEvent(startTime, endTime, LogEvent.EMAIL_SENT, "");
         int numSubmissions = logsProcessor.getNumberOfLogsForEvent(startTime, endTime, LogEvent.FEEDBACK_SESSION_AUDIT,
                 "jsonPayload.accessType=\"submission\"");
 
-        UsageStatisticsAttributes overallUsageStats = UsageStatisticsAttributes.builder(startTime, COLLECTION_TIME_PERIOD)
-                .withNumResponses(entitiesStats.getNumResponses())
-                .withNumCourses(entitiesStats.getNumCourses())
-                .withNumStudents(entitiesStats.getNumStudents())
-                .withNumInstructors(entitiesStats.getNumInstructors())
-                .withNumAccountRequests(entitiesStats.getNumAccountRequests())
-                .withNumEmails(numEmailsSent)
-                .withNumSubmissions(numSubmissions)
-                .build();
+        UsageStatistics overallUsageStats = new UsageStatistics(
+                startTime, COLLECTION_TIME_PERIOD,
+                entitiesStats.getNumResponses() + sqlEntitiesStats.getNumResponses(),
+                entitiesStats.getNumCourses() + sqlEntitiesStats.getNumCourses(),
+                entitiesStats.getNumStudents() + sqlEntitiesStats.getNumStudents(),
+                entitiesStats.getNumInstructors() + sqlEntitiesStats.getNumInstructors(),
+                entitiesStats.getNumAccountRequests() + sqlEntitiesStats.getNumAccountRequests(),
+                numEmailsSent, numSubmissions);
 
         try {
-            logic.createUsageStatistics(overallUsageStats);
+            sqlLogic.createUsageStatistics(overallUsageStats);
         } catch (InvalidParametersException | EntityAlreadyExistsException e) {
             log.severe("Unexpected error", e);
         }
+        
         return new JsonResult("Successful");
     }
 
