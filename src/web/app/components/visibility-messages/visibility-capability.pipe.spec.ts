1080de35931690b54c70b6c1834b3b541d0fa84e
agenell <agenell@kth.se>
2023-02-21 05:58:03 +0100

    fix: add manual branch coverage (#49)

--- src/web/app/components/visibility-messages/visibility-capability.pipe.spec.ts
+++ src/web/app/components/visibility-messages/visibility-capability.pipe.spec.ts
@@ -4,5 +4,8 @@ describe('VisibilityCapabilityPipe', () => {
   it('create an instance', () => {
     const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
     expect(pipe).toBeTruthy();
+    pipe.transform({SHOW_RESPONSE: true, SHOW_GIVER_NAME: true, SHOW_RECIPIENT_NAME: true});
+
+    console.log(pipe.b);
   });
 });
--- src/web/app/components/visibility-messages/visibility-capability.pipe.ts
+++ src/web/app/components/visibility-messages/visibility-capability.pipe.ts
@@ -9,6 +9,7 @@ import { VisibilityControl } from '../../../types/visibility-control';
 })
 export class VisibilityCapabilityPipe implements PipeTransform {
 
+  b: Record<number, boolean> = Object.fromEntries([...new Array(5)].map((_, i) => [i, false]));
   /**
    * Transforms a map of VisibilityControl to a capability description.
    *
@@ -19,16 +20,21 @@ export class VisibilityCapabilityPipe implements PipeTransform {
     let message: string = 'can see your response';
 
     if (controls.SHOW_RECIPIENT_NAME) {
+      this.b[0] = true;
       message += ', the name of the recipient';
 
       if (controls.SHOW_GIVER_NAME) {
+        this.b[1] = true;
         message += ', and your name';
       } else {
+        this.b[2] = true;
         message += ', but not your name';
       }
     } else if (controls.SHOW_GIVER_NAME) {
+      this.b[3] = true;
       message += ', and your name, but not the name of the recipient';
     } else {
+      this.b[4] = true;
       message += ', but not the name of the recipient, or your name';
     }
 
