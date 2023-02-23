07e7b374958ff656cd4aed3ec365780946b720c9
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2023-02-23 19:52:30 +0800

    Create DeadlineExtension entity

---
+++ src/main/java/teammates/storage/sqlentity/DeadlineExtension.java
@@ -0,0 +1,145 @@
+package teammates.storage.sqlentity;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import teammates.common.util.FieldValidator;
+
+/**
+ * Represents a deadline extension entity.
+ */
+@Entity
+@Table(name = "DeadlineExtensions")
+public class DeadlineExtension extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "sessionId", nullable = false)
+    private FeedbackSession feedbackSession;
+
+    @Column(nullable = false)
+    private Instant endTime;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    protected DeadlineExtension() {
+        // required by Hibernate
+    }
+
+    public DeadlineExtension(User user, FeedbackSession feedbackSession, Instant endTime) {
+        this.setUser(user);
+        this.setFeedbackSession(feedbackSession);
+        this.setEndTime(endTime);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public FeedbackSession getFeedbackSession() {
+        return feedbackSession;
+    }
+
+    public void setFeedbackSession(FeedbackSession feedbackSession) {
+        this.feedbackSession = feedbackSession;
+    }
+
+
+    public Instant getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Instant endTime) {
+        this.endTime = endTime;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "DeadlineExtension [id=" + id + ", user=" + user + ", feedbackSession=" + feedbackSession + ", endTime=" + endTime + 
+                ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.user.getCourse(), this.feedbackSession, this.user.getEmail());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        } else if (this == other) {
+            return true;
+        } else if (this.getClass() == other.getClass()) {
+            DeadlineExtension otherDe = (DeadlineExtension) other;
+            return Objects.equals(this.user.getCourse(), otherDe.user.getCourse())
+                    && Objects.equals(this.feedbackSession, otherDe.feedbackSession)
+                    && Objects.equals(this.user.getEmail(), otherDe.user.getEmail());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public List<String> getInvalidityInfo() {
+        List<String> errors = new ArrayList<>();
+
+        addNonEmptyError(FieldValidator.getInvalidityInfoForCourseId(user.getCourse().getId()), errors);
+        addNonEmptyError(FieldValidator.getInvalidityInfoForFeedbackSessionName(feedbackSession.getName()), errors);
+        addNonEmptyError(FieldValidator.getInvalidityInfoForEmail(user.getEmail()), errors);
+
+        return errors;
+    }
+}
