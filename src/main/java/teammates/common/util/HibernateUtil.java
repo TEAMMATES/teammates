83aeca02fbc5c3c4584fa2f10033797e861659bf
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2023-02-25 01:12:47 +0800

    Add DeadlineExtension to Annotated_Classes

--- src/main/java/teammates/common/util/HibernateUtil.java
+++ src/main/java/teammates/common/util/HibernateUtil.java
@@ -13,6 +13,7 @@ import teammates.storage.sqlentity.Account;
 import teammates.storage.sqlentity.AccountRequest;
 import teammates.storage.sqlentity.BaseEntity;
 import teammates.storage.sqlentity.Course;
+import teammates.storage.sqlentity.DeadlineExtension;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.storage.sqlentity.Instructor;
 import teammates.storage.sqlentity.Notification;
@@ -41,7 +42,8 @@ public final class HibernateUtil {
             Student.class,
             UsageStatistics.class,
             Section.class,
-            Team.class);
+            Team.class,
+            DeadlineExtension.class);
 
     private HibernateUtil() {
         // Utility class
