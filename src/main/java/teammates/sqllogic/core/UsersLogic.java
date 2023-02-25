b62fdc48ed4e2fa6ceecd37b87d4ee4003758d45
Dominic Lim <dominiclimkj3448@gmail.com>
2023-02-26 00:37:58 +0800

    Add get instructor and student actions

---
+++ src/main/java/teammates/sqllogic/core/UsersLogic.java
@@ -0,0 +1,53 @@
+package teammates.sqllogic.core;
+
+import teammates.storage.sqlapi.UsersDb;
+import teammates.storage.sqlentity.Instructor;
+import teammates.storage.sqlentity.Student;
+
+/**
+ * Handles operations related to user (instructor & student).
+ *
+ * @see User
+ * @see UsersDb
+ */
+public final class UsersLogic {
+    private static final UsersLogic instance = new UsersLogic();
+
+    private UsersDb usersDb;
+
+    private UsersLogic() {
+        // prevent initialization
+    }
+
+    public static UsersLogic inst() {
+        return instance;
+    }
+
+    void initLogicDependencies(UsersDb usersDb) {
+        this.usersDb = usersDb;
+    }
+
+    /**
+     * Gets instructor associated with {@code id}.
+     * 
+     * @param id    Id of Instructor.
+     * @return      Returns null if not found else Instructor.
+     */
+    public Instructor getInstructor(Integer id) {
+        assert id != null;
+
+        return usersDb.getInstructor(id);
+    }
+
+    /**
+     * Gets student associated with {@code id}.
+     * 
+     * @param id    Id of Student.
+     * @return      Returns null if not found else Student.
+     */
+    public Student getStudent(Integer id) {
+        assert id != null;
+
+        return usersDb.getStudent(id);
+    }
+}
