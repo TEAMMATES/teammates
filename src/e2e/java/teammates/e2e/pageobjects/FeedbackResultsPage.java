024351c36efe58469da5d04a721b071209fde67d
Jason Qiu <jason_qiu@hotmail.com>
2023-02-26 23:10:30 +0800

    [#12081] User-friendliness: Landmark questions in feedback sessions results page (#12129)
    * Make [more] button in feedback sessions result page focusable
    
    * Use headings for question number in feedback sessions result page
    
    * Edit snapshot tests
    
    * Change h3 to h2 tag and remove <b> tags
    
    * Change additional info from link to button

--- src/e2e/java/teammates/e2e/pageobjects/FeedbackResultsPage.java
+++ src/e2e/java/teammates/e2e/pageobjects/FeedbackResultsPage.java
@@ -400,7 +400,7 @@ public class FeedbackResultsPage extends AppPage {
     }
 
     private void showAdditionalInfo(int qnNumber) {
-        WebElement additionalInfoLink = getQuestionResponsesSection(qnNumber).findElement(By.id("additional-info-link"));
+        WebElement additionalInfoLink = getQuestionResponsesSection(qnNumber).findElement(By.id("additional-info-button"));
         if ("[more]".equals(additionalInfoLink.getText())) {
             click(additionalInfoLink);
             waitUntilAnimationFinish();
