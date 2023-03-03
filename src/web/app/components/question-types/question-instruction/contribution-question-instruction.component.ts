a863cd17711cb61377eada9ea8dcd147dda5ead3
Wei Qing <48304907+weiquu@users.noreply.github.com>
2023-03-03 12:56:16 +0800

    User-friendliness: Add labels and change "more info" button for team contribution questions (#12153)
    * Add labels and change button
    
    * Update tests

--- src/web/app/components/question-types/question-instruction/contribution-question-instruction.component.ts
+++ src/web/app/components/question-types/question-instruction/contribution-question-instruction.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
+import { SimpleModalService } from '../../../../services/simple-modal.service';
 import { FeedbackContributionQuestionDetails } from '../../../../types/api-output';
 import { DEFAULT_CONTRIBUTION_QUESTION_DETAILS } from '../../../../types/default-question-structs';
+import { SimpleModalType } from '../../simple-modal/simple-modal-type';
 
 /**
  * Instruction of contribution question.
@@ -18,4 +20,10 @@ export class ContributionQuestionInstructionComponent {
   @Input()
   numOfRecipients: number = 0;
 
+  constructor(private simpleModalService: SimpleModalService) {}
+
+  openHelpModal(modal: TemplateRef<any>): void {
+    const modalHeader: string = 'More info about the <code>Equal Share</code> scale';
+    this.simpleModalService.openInformationModal(modalHeader, SimpleModalType.NEUTRAL, modal);
+  }
 }
