696d47c2605269a19008e5ac2c9a3092c07f310b
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2023-03-06 19:29:46 +0800

    Update initializeDependencies

--- src/main/java/teammates/sqllogic/core/LogicStarter.java
+++ src/main/java/teammates/sqllogic/core/LogicStarter.java
@@ -5,9 +5,12 @@ import javax.servlet.ServletContextListener;
 
 import teammates.common.util.Logger;
 import teammates.storage.sqlapi.CoursesDb;
+import teammates.storage.sqlapi.DeadlineExtensionsDb;
+import teammates.storage.sqlapi.FeedbackQuestionsDb;
 import teammates.storage.sqlapi.FeedbackSessionsDb;
 import teammates.storage.sqlapi.NotificationsDb;
 import teammates.storage.sqlapi.UsageStatisticsDb;
+import teammates.storage.sqlapi.UsersDb;
 
 /**
  * Setup in web.xml to register logic classes at application startup.
@@ -21,16 +24,22 @@ public class LogicStarter implements ServletContextListener {
      */
     public static void initializeDependencies() {
         CoursesLogic coursesLogic = CoursesLogic.inst();
+        DeadlineExtensionsLogic deLogic = DeadlineExtensionsLogic.inst();
         FeedbackSessionsLogic fsLogic = FeedbackSessionsLogic.inst();
         FeedbackResponsesLogic frLogic = FeedbackResponsesLogic.inst();
         FeedbackQuestionsLogic fqLogic = FeedbackQuestionsLogic.inst();
         NotificationsLogic notificationsLogic = NotificationsLogic.inst();
         UsageStatisticsLogic usageStatisticsLogic = UsageStatisticsLogic.inst();
+        UsersLogic usersLogic = UsersLogic.inst();
 
         coursesLogic.initLogicDependencies(CoursesDb.inst(), fsLogic);
+        deLogic.initLogicDependencies(DeadlineExtensionsDb.inst());
         fsLogic.initLogicDependencies(FeedbackSessionsDb.inst(), coursesLogic, frLogic, fqLogic);
+        frLogic.initLogicDependencies();
+        fqLogic.initLogicDependencies(FeedbackQuestionsDb.inst());
         notificationsLogic.initLogicDependencies(NotificationsDb.inst());
         usageStatisticsLogic.initLogicDependencies(UsageStatisticsDb.inst());
+        usersLogic.initLogicDependencies(UsersDb.inst());
         log.info("Initialized dependencies between logic classes");
     }
 
