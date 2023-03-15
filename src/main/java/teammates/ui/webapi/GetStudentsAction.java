a909b01f91d389007b944e6f5f74c0eab8212d35
Dominic Lim <dominiclimkj3448@gmail.com>
2023-03-16 02:23:14 +0800

    Migrate get students action

--- src/main/java/teammates/ui/webapi/GetStudentsAction.java
+++ src/main/java/teammates/ui/webapi/GetStudentsAction.java
@@ -3,10 +3,13 @@ package teammates.ui.webapi;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
+import teammates.storage.sqlentity.Instructor;
+import teammates.storage.sqlentity.Student;
 import teammates.ui.output.StudentData;
 import teammates.ui.output.StudentsData;
 
@@ -24,16 +27,32 @@ class GetStudentsAction extends Action {
     void checkSpecificAccessControl() throws UnauthorizedAccessException {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String teamName = getRequestParamValue(Const.ParamsNames.TEAM_NAME);
-        if (teamName == null) {
-            // request to get all students of a course by instructor
-            InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.id);
-            gateKeeper.verifyAccessible(instructor, logic.getCourse(courseId),
-                    Const.InstructorPermissions.CAN_VIEW_STUDENT_IN_SECTIONS);
+
+        if (isCourseMigrated(courseId)) {
+            if (teamName == null) {
+                // request to get all students of a course by instructor
+                Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.id);
+                gateKeeper.verifyAccessible(instructor, sqlLogic.getCourse(courseId),
+                        Const.InstructorPermissions.CAN_VIEW_STUDENT_IN_SECTIONS);
+            } else {
+                // request to get team member by current student
+                Student student = sqlLogic.getStudentByGoogleId(courseId, userInfo.id);
+                if (student == null || !teamName.equals(student.getTeam().getName())) {
+                    throw new UnauthorizedAccessException("You are not part of the team");
+                }
+            }
         } else {
-            // request to get team member by current student
-            StudentAttributes student = logic.getStudentForGoogleId(courseId, userInfo.id);
-            if (student == null || !teamName.equals(student.getTeam())) {
-                throw new UnauthorizedAccessException("You are not part of the team");
+            if (teamName == null) {
+                // request to get all students of a course by instructor
+                InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.id);
+                gateKeeper.verifyAccessible(instructor, logic.getCourse(courseId),
+                        Const.InstructorPermissions.CAN_VIEW_STUDENT_IN_SECTIONS);
+            } else {
+                // request to get team member by current student
+                StudentAttributes student = logic.getStudentForGoogleId(courseId, userInfo.id);
+                if (student == null || !teamName.equals(student.getTeam())) {
+                    throw new UnauthorizedAccessException("You are not part of the team");
+                }
             }
         }
     }
@@ -42,34 +61,94 @@ class GetStudentsAction extends Action {
     public JsonResult execute() {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String teamName = getRequestParamValue(Const.ParamsNames.TEAM_NAME);
-        InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.id);
-        String privilegeName = Const.InstructorPermissions.CAN_VIEW_STUDENT_IN_SECTIONS;
-        boolean hasCoursePrivilege = instructor != null
-                && instructor.isAllowedForPrivilege(privilegeName);
-        boolean hasSectionPrivilege = instructor != null
-                && instructor.getSectionsWithPrivilege(privilegeName).size() != 0;
 
-        if (teamName == null && hasCoursePrivilege) {
-            // request to get all course students by instructor with course privilege
-            List<StudentAttributes> studentsForCourse = logic.getStudentsForCourse(courseId);
-            return new JsonResult(new StudentsData(studentsForCourse));
-        } else if (teamName == null && hasSectionPrivilege) {
-            // request to get students by instructor with section privilege
-            List<StudentAttributes> studentsForCourse = logic.getStudentsForCourse(courseId);
-            List<StudentAttributes> studentsToReturn = new LinkedList<>();
-            Set<String> sectionsWithViewPrivileges = instructor.getSectionsWithPrivilege(privilegeName).keySet();
-            studentsForCourse.forEach(student -> {
-                if (sectionsWithViewPrivileges.contains(student.getSection())) {
-                    studentsToReturn.add(student);
-                }
-            });
-            return new JsonResult(new StudentsData(studentsToReturn));
+        if (isCourseMigrated(courseId)) {
+            Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.id);
+            String privilegeName = Const.InstructorPermissions.CAN_VIEW_STUDENT_IN_SECTIONS;
+            boolean hasCoursePrivilege = instructor != null
+                    && instructor.isAllowedForPrivilege(privilegeName);
+            boolean hasSectionPrivilege = instructor != null
+                    && instructor.getSectionsWithPrivilege(privilegeName).size() != 0;
+
+            if (teamName == null && hasCoursePrivilege) {
+                // request to get all course students by instructor with course privilege
+                List<Student> studentsForCourse = sqlLogic.getStudentsForCourse(courseId);
+                StudentsData data = new StudentsData();
+                List<StudentData> studentDataList = studentsForCourse
+                        .stream()
+                        .map(StudentData::new)
+                        .collect(Collectors.toList());
+
+                data.setStudents(studentDataList);
+
+                return new JsonResult(data);
+            } else if (teamName == null && hasSectionPrivilege) {
+                // request to get students by instructor with section privilege
+                List<Student> studentsForCourse = sqlLogic.getStudentsForCourse(courseId);
+                List<Student> studentsToReturn = new LinkedList<>();
+                Set<String> sectionsWithViewPrivileges = instructor
+                        .getSectionsWithPrivilege(privilegeName).keySet();
+
+                studentsForCourse.forEach(student -> {
+                    if (sectionsWithViewPrivileges.contains(student.getTeam().getSection().getName())) {
+                        studentsToReturn.add(student);
+                    }
+                });
+
+                StudentsData data = new StudentsData();
+                List<StudentData> studentDataList = studentsToReturn
+                        .stream()
+                        .map(StudentData::new)
+                        .collect(Collectors.toList());
+
+                data.setStudents(studentDataList);
+
+                return new JsonResult(data);
+            } else {
+                // request to get team members by current student
+                List<Student> studentsForTeam = sqlLogic.getStudentsByTeamName(teamName, courseId);
+                StudentsData data = new StudentsData();
+                List<StudentData> studentDataList = studentsForTeam
+                        .stream()
+                        .map(StudentData::new)
+                        .collect(Collectors.toList());
+
+                data.setStudents(studentDataList);
+
+                data.getStudents().forEach(StudentData::hideInformationForStudent);
+
+                return new JsonResult(data);
+            }
         } else {
-            // request to get team members by current student
-            List<StudentAttributes> studentsForTeam = logic.getStudentsForTeam(teamName, courseId);
-            StudentsData studentsData = new StudentsData(studentsForTeam);
-            studentsData.getStudents().forEach(StudentData::hideInformationForStudent);
-            return new JsonResult(studentsData);
+            InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.id);
+            String privilegeName = Const.InstructorPermissions.CAN_VIEW_STUDENT_IN_SECTIONS;
+            boolean hasCoursePrivilege = instructor != null
+                    && instructor.isAllowedForPrivilege(privilegeName);
+            boolean hasSectionPrivilege = instructor != null
+                    && instructor.getSectionsWithPrivilege(privilegeName).size() != 0;
+    
+            if (teamName == null && hasCoursePrivilege) {
+                // request to get all course students by instructor with course privilege
+                List<StudentAttributes> studentsForCourse = logic.getStudentsForCourse(courseId);
+                return new JsonResult(new StudentsData(studentsForCourse));
+            } else if (teamName == null && hasSectionPrivilege) {
+                // request to get students by instructor with section privilege
+                List<StudentAttributes> studentsForCourse = logic.getStudentsForCourse(courseId);
+                List<StudentAttributes> studentsToReturn = new LinkedList<>();
+                Set<String> sectionsWithViewPrivileges = instructor.getSectionsWithPrivilege(privilegeName).keySet();
+                studentsForCourse.forEach(student -> {
+                    if (sectionsWithViewPrivileges.contains(student.getSection())) {
+                        studentsToReturn.add(student);
+                    }
+                });
+                return new JsonResult(new StudentsData(studentsToReturn));
+            } else {
+                // request to get team members by current student
+                List<StudentAttributes> studentsForTeam = logic.getStudentsForTeam(teamName, courseId);
+                StudentsData studentsData = new StudentsData(studentsForTeam);
+                studentsData.getStudents().forEach(StudentData::hideInformationForStudent);
+                return new JsonResult(studentsData);
+            }
         }
     }
 }
