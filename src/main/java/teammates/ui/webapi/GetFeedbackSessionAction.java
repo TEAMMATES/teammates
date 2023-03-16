f7b0b212712cc62ae4ce7cac439e82ebeb53f56a
Kevin Foong <kevin9foong@gmail.com>
2023-03-16 10:51:34 +0800

    Migrate GetFeedbackSessionAction

--- src/main/java/teammates/ui/webapi/GetFeedbackSessionAction.java
+++ src/main/java/teammates/ui/webapi/GetFeedbackSessionAction.java
@@ -4,6 +4,8 @@ import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
+import teammates.storage.sqlentity.FeedbackSession;
+import teammates.storage.sqlentity.Instructor;
 import teammates.ui.output.FeedbackSessionData;
 import teammates.ui.request.Intent;
 
@@ -21,24 +23,30 @@ class GetFeedbackSessionAction extends BasicFeedbackSubmissionAction {
     void checkSpecificAccessControl() throws UnauthorizedAccessException {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getNonNullRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
-        FeedbackSessionAttributes feedbackSession = getNonNullFeedbackSession(feedbackSessionName, courseId);
+        FeedbackSessionAttributes feedbackSessionAttributes = getNonNullFeedbackSession(feedbackSessionName, courseId);
+        FeedbackSession feedbackSession = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
         Intent intent = Intent.valueOf(getNonNullRequestParamValue(Const.ParamsNames.INTENT));
 
         switch (intent) {
         case STUDENT_SUBMISSION:
         case STUDENT_RESULT:
             StudentAttributes studentAttributes = getStudentOfCourseFromRequest(courseId);
-            checkAccessControlForStudentFeedbackSubmission(studentAttributes, feedbackSession);
+            checkAccessControlForStudentFeedbackSubmission(studentAttributes, feedbackSessionAttributes);
             break;
         case INSTRUCTOR_SUBMISSION:
         case INSTRUCTOR_RESULT:
             InstructorAttributes instructorAttributes = getInstructorOfCourseFromRequest(courseId);
-            checkAccessControlForInstructorFeedbackSubmission(instructorAttributes, feedbackSession);
+            checkAccessControlForInstructorFeedbackSubmission(instructorAttributes, feedbackSessionAttributes);
             break;
         case FULL_DETAIL:
             gateKeeper.verifyLoggedInUserPrivileges(userInfo);
-            gateKeeper.verifyAccessible(logic.getInstructorForGoogleId(courseId, userInfo.getId()),
+            if (!isCourseMigrated(courseId)) {
+                gateKeeper.verifyAccessible(logic.getInstructorForGoogleId(courseId, userInfo.getId()),
+                    feedbackSessionAttributes, Const.InstructorPermissions.CAN_VIEW_SESSION_IN_SECTIONS);
+            } else {
+                gateKeeper.verifyAccessible(sqlLogic.getInstructorByGoogleId(courseId, userInfo.getId()),
                     feedbackSession, Const.InstructorPermissions.CAN_VIEW_SESSION_IN_SECTIONS);
+            }
             break;
         default:
             throw new InvalidHttpParameterException("Unknown intent " + intent);
