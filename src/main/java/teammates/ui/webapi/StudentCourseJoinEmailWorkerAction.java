ef6e8c51e20367ef0ba9acb6d8f20185fb6cfba3
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2023-03-20 15:20:55 +0800

    Migrate StudentCourseJoinEmailWorkerAction with unit tests.

--- src/main/java/teammates/ui/webapi/StudentCourseJoinEmailWorkerAction.java
+++ src/main/java/teammates/ui/webapi/StudentCourseJoinEmailWorkerAction.java
@@ -3,31 +3,54 @@ package teammates.ui.webapi;
 import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const.ParamsNames;
+import teammates.storage.sqlentity.Course;
+import teammates.storage.sqlentity.Student;
 import teammates.common.util.EmailWrapper;
 
 /**
  * Task queue worker action: sends registration email for a student of a course.
  */
-class StudentCourseJoinEmailWorkerAction extends AdminOnlyAction {
+public class StudentCourseJoinEmailWorkerAction extends AdminOnlyAction {
 
     @Override
     public JsonResult execute() {
         String courseId = getNonNullRequestParamValue(ParamsNames.COURSE_ID);
-        CourseAttributes course = logic.getCourse(courseId);
+
+        if (!isCourseMigrated(courseId)) {
+            CourseAttributes course = logic.getCourse(courseId);
+            if (course == null) {
+                throw new EntityNotFoundException("Course with ID " + courseId + " does not exist!");
+            }
+
+            String studentEmail = getNonNullRequestParamValue(ParamsNames.STUDENT_EMAIL);
+            StudentAttributes student = logic.getStudentForEmail(courseId, studentEmail);
+            if (student == null) {
+                throw new EntityNotFoundException("Student does not exist.");
+            }
+
+            boolean isRejoin = getBooleanRequestParamValue(ParamsNames.IS_STUDENT_REJOINING);
+            EmailWrapper email = isRejoin
+                    ? emailGenerator.generateStudentCourseRejoinEmailAfterGoogleIdReset(course, student)
+                    : emailGenerator.generateStudentCourseJoinEmail(course, student);
+            emailSender.sendEmail(email);
+            return new JsonResult("Successful");
+        }
+
+        Course course = sqlLogic.getCourse(courseId);
         if (course == null) {
             throw new EntityNotFoundException("Course with ID " + courseId + " does not exist!");
         }
 
         String studentEmail = getNonNullRequestParamValue(ParamsNames.STUDENT_EMAIL);
-        StudentAttributes student = logic.getStudentForEmail(courseId, studentEmail);
+        Student student = sqlLogic.getStudentForEmail(courseId, studentEmail);
         if (student == null) {
             throw new EntityNotFoundException("Student does not exist.");
         }
 
         boolean isRejoin = getBooleanRequestParamValue(ParamsNames.IS_STUDENT_REJOINING);
         EmailWrapper email = isRejoin
-                ? emailGenerator.generateStudentCourseRejoinEmailAfterGoogleIdReset(course, student)
-                : emailGenerator.generateStudentCourseJoinEmail(course, student);
+                ? sqlEmailGenerator.generateStudentCourseRejoinEmailAfterGoogleIdReset(course, student)
+                : sqlEmailGenerator.generateStudentCourseJoinEmail(course, student);
         emailSender.sendEmail(email);
         return new JsonResult("Successful");
     }
