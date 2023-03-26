5ebe4797a75b043a461d90d65de95dfbd0059e66
Wilson Kurniawan <wkurniawan.92@gmail.com>
2023-03-27 00:22:13 +0800

    Add file to store Firebase credentials

--- src/main/java/teammates/logic/external/FirebaseAuthService.java
+++ src/main/java/teammates/logic/external/FirebaseAuthService.java
@@ -1,6 +1,11 @@
 package teammates.logic.external;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthErrorCode;
 import com.google.firebase.auth.FirebaseAuth;
@@ -8,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 
 import teammates.common.exception.AuthException;
+import teammates.common.util.FileHelper;
 import teammates.common.util.Logger;
 
 /**
@@ -21,9 +27,13 @@ public class FirebaseAuthService implements AuthService {
 
     public FirebaseAuthService() throws AuthException {
         try {
-            FirebaseApp.initializeApp();
+            InputStream firebaseCredentialsStream = FileHelper.getResourceAsStream("firebase-credentials.json");
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(firebaseCredentialsStream))
+                    .build();
+            FirebaseApp.initializeApp(options);
             log.info("Initialized FirebaseApp instance of name " + FirebaseApp.getInstance().getName());
-        } catch (IllegalStateException | IllegalArgumentException e) {
+        } catch (IOException | IllegalStateException | IllegalArgumentException e) {
             log.severe("Cannot initialize FirebaseApp: " + e.getMessage());
             throw new AuthException(e);
         }
