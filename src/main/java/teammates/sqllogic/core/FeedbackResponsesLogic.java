2220f6ab86fba8d34649daab125c4a4f80d6fc6d
Cedric Ong <cedricongjh@gmail.com>
2023-03-27 23:56:47 +0800

    create areThereResponsesForQuestion

--- src/main/java/teammates/sqllogic/core/FeedbackResponsesLogic.java
+++ src/main/java/teammates/sqllogic/core/FeedbackResponsesLogic.java
@@ -131,6 +131,13 @@ public final class FeedbackResponsesLogic {
     }
 
     /**
+     * Checks whether there are responses for a question.
+     */
+    public boolean areThereResponsesForQuestion(UUID questionId) {
+        return frDb.areThereResponsesForQuestion(questionId);
+    }
+
+    /**
      * Checks whether a giver has responded a session.
      */
     public boolean hasGiverRespondedForSession(String giver, String feedbackSessionName, String courseId) {
