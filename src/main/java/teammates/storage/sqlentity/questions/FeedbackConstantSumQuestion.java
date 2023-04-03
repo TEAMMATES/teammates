d0d4e710a9e2867342c59b586111d4ad259798fb
wuqirui <53338059+hhdqirui@users.noreply.github.com>
2023-04-03 16:11:34 +0800

    [#12048 ] Migrate UpdateFeedbackQuestionAction (#12318)
    * Migrate UpdateFeedbackQuestionAction
    
    * Remove redundant line
    
    * Update data bundle
    
    * Fix FeedbackSessionsLogicIT
    
    * Ignore old test
    
    * Update data bundle
    
    * Update data bundle
    
    * Update comment

--- src/main/java/teammates/storage/sqlentity/questions/FeedbackConstantSumQuestion.java
+++ src/main/java/teammates/storage/sqlentity/questions/FeedbackConstantSumQuestion.java
@@ -52,7 +52,12 @@ public class FeedbackConstantSumQuestion extends FeedbackQuestion {
                 this.getRecipientType(), this.getNumOfEntitiesToGiveFeedbackTo(), new ArrayList<>(this.getShowResponsesTo()),
                 new ArrayList<>(this.getShowGiverNameTo()), new ArrayList<>(this.getShowRecipientNameTo()),
                 new FeedbackConstantSumQuestionDetails(this.questionDetails.getQuestionText())
-            );
+        );
+    }
+
+    @Override
+    public void setQuestionDetails(FeedbackQuestionDetails questionDetails) {
+        this.questionDetails = (FeedbackConstantSumQuestionDetails) questionDetails;
     }
 
     @Override
