55997af3318093e3d1415b6a876f7f560267c166
Kei <keiwork111@gmail.com>
2023-04-21 17:43:48 +0800

    Add unit test for modified timestamps modal component

--- src/web/app/pages-instructor/modified-timestamps-modal/modified-timestamps-modal.component.spec.ts
+++ src/web/app/pages-instructor/modified-timestamps-modal/modified-timestamps-modal.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModifiedTimestampModalComponent } from './modified-timestamps-modal.component';
+import { TweakedTimestampData } from '../instructor-session-base-page.component';
 
 describe('ModifiedTimestampModalComponent', () => {
   let component: ModifiedTimestampModalComponent;
   let fixture: ComponentFixture<ModifiedTimestampModalComponent>;
 
+  const coursesOfModifiedSession: string[] = [
+    'u.gma-demo'
+  ];
+
+  const modifiedSessions: Record<string, TweakedTimestampData> = {
+    'Instructor Feedback Session': {
+      newTimestamp: {
+        submissionStartTimestamp: "23 Apr 2023 9:00 PM",
+        submissionEndTimestamp: "27 Apr 2023 11:59 PM",
+        sessionVisibleTimestamp: "On submission opening time",
+        responseVisibleTimestamp: "Not now (publish manually)",
+      },
+      oldTimestamp: {
+        submissionStartTimestamp:"22 Mar 2023 9:00 PM",
+        submissionEndTimestamp: "31 Mar 2023 11:59 PM",
+        sessionVisibleTimestamp: "On submission opening time",
+        responseVisibleTimestamp: "Not now (publish manually)",
+      }
+    }
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ModifiedTimestampModalComponent ]
+      declarations: [ ModifiedTimestampModalComponent ]
     })
     .compileComponents();
 
@@ -20,4 +42,25 @@ describe('ModifiedTimestampModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should snap with default fields', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should snap with coursesOfModifiedSession and modifiedSessions', () => {
+    component.coursesOfModifiedSession = coursesOfModifiedSession;
+    component.modifiedSessions = modifiedSessions;
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+
+  it('should snap with modifiedSessions', () => {
+    component.modifiedSessions = modifiedSessions;
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
 });
