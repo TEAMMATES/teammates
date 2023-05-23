37f713175e6279d63cdb1cbb2d22dfefd618b5a2
justincavalli <81125835+justincavalli@users.noreply.github.com>
2023-05-22 17:43:44 -0700

    remove trailing white space

--- src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-courses-page/instructor-courses-page.component.spec.ts
@@ -384,7 +384,7 @@ describe('InstructorCoursesPageComponent', () => {
     expect(component.activeCourses.length).toEqual(1);
     expect(component.activeCourses[0].course.courseId).toEqual('CS1231');
   });
-    
+
   it('should restore a soft deleted course', () => {
     component.softDeletedCourses = [courseModelCS1231];
     expect(component.softDeletedCourses.length).toEqual(1);
@@ -393,15 +393,15 @@ describe('InstructorCoursesPageComponent', () => {
       .mockReturnValue(of(courseModelCS1231));
     jest.spyOn(simpleModalService, 'openConfirmationModal')
       .mockReturnValue(createMockNgbModalRef());
-    
+
     component.onRestore('CS1231');
 
     expect(courseSpy).toHaveBeenCalledTimes(1);
     expect(courseSpy).toHaveBeenNthCalledWith(1, 'CS1231');
 
     expect(component.archivedCourses.length).toEqual(0);
-    expect(component.softDeletedCourses.length).toEqual(0);    
-  });
+    expect(component.softDeletedCourses.length).toEqual(0);
+});
 
   it('should soft delete a course', async () => {
     component.activeCourses = [courseModelCS1231];
