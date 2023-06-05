e17c33aa2f24ceb67e5551af3f2eaf98797f89c8
David Moreno-Bautista <71296308+damoba@users.noreply.github.com>
2023-06-05 07:59:07 -0400

    [#12099] Add 'course -> copy' option to the instructor home page (#12439)
    * Add course copy option to the instructor home page
    
    * Fix modified session not resetting
    
    * Refactor from scss to bootstrap
    
    ---------
    
    Co-authored-by: David Moreno-Bautista <71296308+davmoba4@users.noreply.github.com>
    Co-authored-by: Dominic Lim <46486515+domlimm@users.noreply.github.com>

--- src/web/app/pages-instructor/instructor-home-page/instructor-home-page.module.ts
+++ src/web/app/pages-instructor/instructor-home-page/instructor-home-page.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbCollapseModule, NgbDropdownModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { CopyCourseModalModule } from '../../components/copy-course-modal/copy-course-modal.module';
 import { LoadingRetryModule } from '../../components/loading-retry/loading-retry.module';
 import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
 import {
   ModifiedTimestampModalModule,
 } from '../../components/modified-timestamps-modal/modified-timestamps-module.module';
 import { PanelChevronModule } from '../../components/panel-chevron/panel-chevron.module';
+import { ProgressBarModule } from '../../components/progress-bar/progress-bar.module';
 import { SessionsTableModule } from '../../components/sessions-table/sessions-table.module';
 import { TeammatesRouterModule } from '../../components/teammates-router/teammates-router.module';
 import { InstructorHomePageComponent } from './instructor-home-page.component';
@@ -39,6 +41,8 @@ const routes: Routes = [
     LoadingRetryModule,
     PanelChevronModule,
     TeammatesRouterModule,
+    CopyCourseModalModule,
+    ProgressBarModule,
     ModifiedTimestampModalModule,
   ],
   exports: [
