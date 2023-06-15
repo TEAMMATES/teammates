62fbbf402d9c47ff03bd81d47ddd1c46c3a92e45
Zong Xun <zxun2000@gmail.com>
2023-06-15 15:35:11 +0800

    fix: ensure relevant panels open where needed

--- src/web/app/pages-student/student-home-page/student-home-page.module.ts
+++ src/web/app/pages-student/student-home-page/student-home-page.module.ts
@@ -7,11 +7,11 @@ import {
 } from '@ng-bootstrap/ng-bootstrap';
 import { LoadingRetryModule } from '../../components/loading-retry/loading-retry.module';
 import { LoadingSpinnerModule } from '../../components/loading-spinner/loading-spinner.module';
+import { PanelChevronModule } from '../../components/panel-chevron/panel-chevron.module';
 import { TeammatesCommonModule } from '../../components/teammates-common/teammates-common.module';
 import { TeammatesRouterModule } from '../../components/teammates-router/teammates-router.module';
 import { Pipes } from '../../pipes/pipes.module';
 import { StudentHomePageComponent } from './student-home-page.component';
-import { PanelChevronModule } from '../../components/panel-chevron/panel-chevron.module';
 
 const routes: Routes = [
   {
