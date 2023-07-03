7cca38b4d20616c7a7d68172d689ae20f7c2d5c6
renann.oliveira <renannpop11@gmail.com>
2023-07-03 13:05:12 -0300

    Adding button for Download Question Results

--- src/web/app/pages-instructor/instructor-session-result-page/instructor-session-result-question-view.component.spec.ts
+++ src/web/app/pages-instructor/instructor-session-result-page/instructor-session-result-question-view.component.spec.ts
@@ -12,10 +12,64 @@ import {
 } from '../../components/question-responses/single-statistics/single-statistics.module';
 import { QuestionTextWithInfoModule } from '../../components/question-text-with-info/question-text-with-info.module';
 import { InstructorSessionResultQuestionViewComponent } from './instructor-session-result-question-view.component';
+import { By } from '@angular/platform-browser';
 
 describe('InstructorSessionResultQuestionViewComponent', () => {
   let component: InstructorSessionResultQuestionViewComponent;
   let fixture: ComponentFixture<InstructorSessionResultQuestionViewComponent>;
+
+  // const testQuestions: Record<string, QuestionTabModel> = {
+  //   "question 1": {
+  //     question: {
+  //       feedbackQuestionId: "Q1",
+  //       questionNumber: 1,
+  //       questionBrief: "brief",
+  //       questionDescription: "desc",
+  //       questionDetails: {
+  //         questionText: "questionText",
+  //         questionType: FeedbackQuestionType.TEXT
+  //       },
+  //       questionType: FeedbackQuestionType.TEXT,
+  //       giverType: FeedbackParticipantType.STUDENTS, // testar se faz sentido trocar para professores um deses dois
+  //       recipientType: FeedbackParticipantType.STUDENTS,
+  //       numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM,
+  //       customNumberOfEntitiesToGiveFeedbackTo: 3,
+  //       showResponsesTo: [],
+  //       showGiverNameTo: [],
+  //       showRecipientNameTo: [],
+  //     },
+  //     responses: [],
+  //     statistics: "32",
+  //     hasPopulated: true,
+  //     errorMessage: "Error message",
+  //     isTabExpanded: false
+  //   },
+  //   "question 2": {
+  //     question: {
+  //       feedbackQuestionId: "Q2",
+  //       questionNumber: 1,
+  //       questionBrief: "brief",
+  //       questionDescription: "desc",
+  //       questionDetails: {
+  //         questionText: "questionText",
+  //         questionType: FeedbackQuestionType.TEXT
+  //       },
+  //       questionType: FeedbackQuestionType.TEXT,
+  //       giverType: FeedbackParticipantType.STUDENTS, // testar se faz sentido trocar para professores um deses dois
+  //       recipientType: FeedbackParticipantType.STUDENTS,
+  //       numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM,
+  //       customNumberOfEntitiesToGiveFeedbackTo: 3,
+  //       showResponsesTo: [],
+  //       showGiverNameTo: [],
+  //       showRecipientNameTo: [],
+  //     },
+  //     responses: [],
+  //     statistics: "89",
+  //     hasPopulated: true,
+  //     errorMessage: "Error message",
+  //     isTabExpanded: false
+  //   }
+  // }
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -41,5 +95,18 @@ describe('InstructorSessionResultQuestionViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('Verify the button for download question results', () => {
+    // expect(component).toBeTruthy();
+    fixture.detectChanges();
+    // component.questions = testQuestions
+
+    const downloadQuestionResultsSpy = jest.spyOn(component, 'triggerDownloadQuestionResult');
+    fixture.debugElement.nativeElement.query(By.css('.btn')).click();
+    // query(By.css(".card-header-btn-toolbar")).nativeElement.click();
+    // const teste = fixture.debugElement.nativeElement.debugElement
+    // console.log(teste)
+    expect(downloadQuestionResultsSpy).toHaveBeenCalledTimes(1);
   });
 });
