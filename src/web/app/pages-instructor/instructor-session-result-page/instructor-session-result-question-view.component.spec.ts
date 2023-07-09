cafd9b33594db0e43e84567012d3a95522c39cc9
renann.oliveira <renannpop11@gmail.com>
2023-07-09 17:23:20 -0300

    refactor test

--- src/web/app/pages-instructor/instructor-session-result-page/instructor-session-result-question-view.component.spec.ts
+++ src/web/app/pages-instructor/instructor-session-result-page/instructor-session-result-question-view.component.spec.ts
@@ -13,63 +13,66 @@ import {
 import { QuestionTextWithInfoModule } from '../../components/question-text-with-info/question-text-with-info.module';
 import { InstructorSessionResultQuestionViewComponent } from './instructor-session-result-question-view.component';
 import { By } from '@angular/platform-browser';
+// ../src/web/types/api-output
+import { FeedbackParticipantType, FeedbackQuestionType, NumberOfEntitiesToGiveFeedbackToSetting } from '../../../../../src/web/types/api-output';
+import { QuestionTabModel } from './instructor-session-result-page.component';
 
 describe('InstructorSessionResultQuestionViewComponent', () => {
   let component: InstructorSessionResultQuestionViewComponent;
   let fixture: ComponentFixture<InstructorSessionResultQuestionViewComponent>;
 
-  // const testQuestions: Record<string, QuestionTabModel> = {
-  //   "question 1": {
-  //     question: {
-  //       feedbackQuestionId: "Q1",
-  //       questionNumber: 1,
-  //       questionBrief: "brief",
-  //       questionDescription: "desc",
-  //       questionDetails: {
-  //         questionText: "questionText",
-  //         questionType: FeedbackQuestionType.TEXT
-  //       },
-  //       questionType: FeedbackQuestionType.TEXT,
-  //       giverType: FeedbackParticipantType.STUDENTS, // testar se faz sentido trocar para professores um deses dois
-  //       recipientType: FeedbackParticipantType.STUDENTS,
-  //       numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM,
-  //       customNumberOfEntitiesToGiveFeedbackTo: 3,
-  //       showResponsesTo: [],
-  //       showGiverNameTo: [],
-  //       showRecipientNameTo: [],
-  //     },
-  //     responses: [],
-  //     statistics: "32",
-  //     hasPopulated: true,
-  //     errorMessage: "Error message",
-  //     isTabExpanded: false
-  //   },
-  //   "question 2": {
-  //     question: {
-  //       feedbackQuestionId: "Q2",
-  //       questionNumber: 1,
-  //       questionBrief: "brief",
-  //       questionDescription: "desc",
-  //       questionDetails: {
-  //         questionText: "questionText",
-  //         questionType: FeedbackQuestionType.TEXT
-  //       },
-  //       questionType: FeedbackQuestionType.TEXT,
-  //       giverType: FeedbackParticipantType.STUDENTS, // testar se faz sentido trocar para professores um deses dois
-  //       recipientType: FeedbackParticipantType.STUDENTS,
-  //       numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM,
-  //       customNumberOfEntitiesToGiveFeedbackTo: 3,
-  //       showResponsesTo: [],
-  //       showGiverNameTo: [],
-  //       showRecipientNameTo: [],
-  //     },
-  //     responses: [],
-  //     statistics: "89",
-  //     hasPopulated: true,
-  //     errorMessage: "Error message",
-  //     isTabExpanded: false
-  //   }
-  // }
+  const testQuestions: Record<string, QuestionTabModel> = {
+    "question 1": {
+      question: {
+        feedbackQuestionId: "Q1",
+        questionNumber: 1,
+        questionBrief: "brief",
+        questionDescription: "desc",
+        questionDetails: {
+          questionText: "questionText",
+          questionType: FeedbackQuestionType.TEXT
+        },
+        questionType: FeedbackQuestionType.TEXT,
+        giverType: FeedbackParticipantType.STUDENTS, // testar se faz sentido trocar para professores um deses dois
+        recipientType: FeedbackParticipantType.STUDENTS,
+        numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM,
+        customNumberOfEntitiesToGiveFeedbackTo: 3,
+        showResponsesTo: [],
+        showGiverNameTo: [],
+        showRecipientNameTo: [],
+      },
+      responses: [],
+      statistics: "32",
+      hasPopulated: true,
+      errorMessage: "Error message",
+      isTabExpanded: false
+    },
+    "question 2": {
+      question: {
+        feedbackQuestionId: "Q2",
+        questionNumber: 2,
+        questionBrief: "brief",
+        questionDescription: "desc",
+        questionDetails: {
+          questionText: "questionText",
+          questionType: FeedbackQuestionType.TEXT
+        },
+        questionType: FeedbackQuestionType.TEXT,
+        giverType: FeedbackParticipantType.STUDENTS, // testar se faz sentido trocar para professores um deses dois
+        recipientType: FeedbackParticipantType.STUDENTS,
+        numberOfEntitiesToGiveFeedbackToSetting: NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM,
+        customNumberOfEntitiesToGiveFeedbackTo: 3,
+        showResponsesTo: [],
+        showGiverNameTo: [],
+        showRecipientNameTo: [],
+      },
+      responses: [],
+      statistics: "89",
+      hasPopulated: true,
+      errorMessage: "Error message",
+      isTabExpanded: false
+    }
+  }
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -90,23 +93,28 @@ describe('InstructorSessionResultQuestionViewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InstructorSessionResultQuestionViewComponent);
     component = fixture.componentInstance;
+    component.questions = testQuestions;
     fixture.detectChanges();
-  });
+  }); 
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('Verify the button for download question results', () => {
-    // expect(component).toBeTruthy();
+  it('Verify if buttons is rendered', () => {
     fixture.detectChanges();
-    // component.questions = testQuestions
+    const downloadQuestion1 = fixture.debugElement.queryAll(By.css('#btn-donwload-question'))[0].nativeElement
+    const downloadQuestion2 = fixture.debugElement.queryAll(By.css('#btn-donwload-question'))[1].nativeElement
+  
+    expect(downloadQuestion1).toBeTruthy();
+    expect(downloadQuestion2).toBeTruthy();
+  });
 
+  it('Verify the button trigger download', () => {
+    fixture.detectChanges();
     const downloadQuestionResultsSpy = jest.spyOn(component, 'triggerDownloadQuestionResult');
-    fixture.debugElement.nativeElement.query(By.css('.btn')).click();
-    // query(By.css(".card-header-btn-toolbar")).nativeElement.click();
-    // const teste = fixture.debugElement.nativeElement.debugElement
-    // console.log(teste)
+    fixture.debugElement.queryAll(By.css('#btn-donwload-question'))[0].nativeElement.click();
+  
     expect(downloadQuestionResultsSpy).toHaveBeenCalledTimes(1);
   });
 });
