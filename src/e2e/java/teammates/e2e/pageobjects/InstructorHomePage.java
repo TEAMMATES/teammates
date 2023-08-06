20a3219e27eed1e9e996edde63715b4e13af5de1
rexong <rexonghanwei200035@gmail.com>
2023-08-06 20:40:05 +0800

    Update E2E

--- src/e2e/java/teammates/e2e/pageobjects/InstructorHomePage.java
+++ src/e2e/java/teammates/e2e/pageobjects/InstructorHomePage.java
@@ -85,7 +85,7 @@ public class InstructorHomePage extends AppPage {
     public void sendReminderEmailToNonSubmitters(int courseTabIndex, int sessionIndex) {
         WebElement courseTab = getCourseTab(courseTabIndex);
         click(courseTab.findElement(By.className("btn-remind-" + sessionIndex)));
-        List<WebElement> remindSelectedButton = browser.driver.findElements(By.className("btn-remind-selected-" + sessionIndex));
+        List<WebElement> remindSelectedButtons = browser.driver.findElements(By.className("btn-remind-selected-" + sessionIndex));
         click(remindSelectedButtons.get(remindSelectedButtons.size() - 1));
         click(waitForElementPresence(By.id("btn-confirm-send-reminder")));
     }
