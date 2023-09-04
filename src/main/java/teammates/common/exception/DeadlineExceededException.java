725b7de91504118b0b766be7ce256dfa4d6b1aea
yyyiwei <87693878+yyyiwei@users.noreply.github.com>
2023-09-04 23:43:36 +0800

    Update DeadlineExceededException.java

--- src/main/java/teammates/common/exception/DeadlineExceededException.java
+++ src/main/java/teammates/common/exception/DeadlineExceededException.java
@@ -4,9 +4,4 @@ package teammates.common.exception;
  * Exception thrown when an operation is determined to have exceeded the time it is allowed to run.
  */
 public class DeadlineExceededException extends RuntimeException {
-
-    public DeadlineExceededException(String message) {
-        super(message);
-    }
-  
 }
