383cadffd51b5674bb40e6fad08bf6449a114310
Kenney <u7481157@anu.edu.au>
2023-10-08 18:36:19 +1100

    Added unit tests to CommentTableModal Component

--- src/web/app/components/comment-box/comment-table-modal/comment-table-modal.component.spec.ts
+++ src/web/app/components/comment-box/comment-table-modal/comment-table-modal.component.spec.ts
@@ -7,7 +7,8 @@ import { RichTextEditorModule } from '../../rich-text-editor/rich-text-editor.mo
 import { TeammatesCommonModule } from '../../teammates-common/teammates-common.module';
 import { CommentEditFormComponent } from '../comment-edit-form/comment-edit-form.component';
 import { CommentRowComponent } from '../comment-row/comment-row.component';
-import { CommentTableComponent } from '../comment-table/comment-table.component';
+import { CommentTableComponent, CommentTableModel } from '../comment-table/comment-table.component';
+import SpyInstance = jest.SpyInstance;
 import {
   CommentVisibilityControlNamePipe,
   CommentVisibilityTypeDescriptionPipe, CommentVisibilityTypeNamePipe, CommentVisibilityTypesJointNamePipe,
@@ -51,5 +52,63 @@ describe('CommentTableModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set isAddingNewComment to true in the model', () => {
+    component.model = {
+      commentRows: [],
+      newCommentRow: {
+        commentEditFormModel: {
+          commentText: '',
+          isUsingCustomVisibilities: false,
+          showCommentTo: [],
+          showGiverNameTo: [],
+        },
+        isEditing: true,
+      },
+      isAddingNewComment: false,
+      isReadOnly: false,
+    };
+    fixture.detectChanges();
+    component.ngOnChanges();
+    expect(component.model.isAddingNewComment).toBe(true);
+  });
+
+  it('should trigger an event to delete comment from comments table', () => {
+    const triggerDeleteCommentSpy: SpyInstance = jest.spyOn(component.deleteCommentEvent, 'emit');
+    component.triggerDeleteCommentEvent(1);
+    expect(triggerDeleteCommentSpy).toHaveBeenCalledWith(1);
+  });
+
+  it('should trigger an event to update comment from comments table', () => {
+    const triggerUpdateCommentSpy: SpyInstance = jest.spyOn(component.updateCommentEvent, 'emit');
+    component.triggerUpdateCommentEvent(0);
+    expect(triggerUpdateCommentSpy).toHaveBeenCalledWith(0);
+  });
+
+  it('Should trigger an event to add a new comment to the comments table', () => {
+    const triggerSaveNewCommentEventSpy: SpyInstance = jest.spyOn(component.saveNewCommentEvent, 'emit');
+    component.triggerSaveNewCommentEvent();
+    expect(triggerSaveNewCommentEventSpy).toHaveBeenCalled();  
+  });
+
+  it('Should trigger an event to change the model of the form' , () => {
+    const testModel:CommentTableModel = {
+      commentRows: [],
+      newCommentRow: {
+        commentEditFormModel: {
+          commentText: '',
+          isUsingCustomVisibilities: false,
+          showCommentTo: [],
+          showGiverNameTo: [],
+        },
+        isEditing: true,
+      },
+      isAddingNewComment: false,
+      isReadOnly: false,
+    };
+    const triggerModelChangeSpy: SpyInstance = jest.spyOn(component.modelChange, 'emit');
+    component.triggerModelChange(testModel);
+    expect(triggerModelChangeSpy).toBeCalledWith(testModel);
   });
 });
