83d102d30de2e980c4bca12382c8c0c0054f37cc
HarryLu <harrry.lu@tennentgroup.com.au>
2023-10-15 15:01:32 +1100

    spys assigned to a const  comments removed  added test for the ngOnInit() method

--- src/web/app/components/comment-box/comment-edit-form/comment-edit-form.component.spec.ts
+++ src/web/app/components/comment-box/comment-edit-form/comment-edit-form.component.spec.ts
@@ -1,16 +1,21 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CommentVisibilityType } from '../../../../types/api-output';
-import { CommentVisibilityControl } from '../../../../types/comment-visibility-control';
-import { RichTextEditorModule } from '../../rich-text-editor/rich-text-editor.module';
-import { TeammatesCommonModule } from '../../teammates-common/teammates-common.module';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {
+  CommentOutput,
+  CommentVisibilityType, FeedbackQuestionType,
+  FeedbackResponseDetails
+} from '../../../../types/api-output';
+import {CommentVisibilityControl} from '../../../../types/comment-visibility-control';
+import {RichTextEditorModule} from '../../rich-text-editor/rich-text-editor.module';
+import {TeammatesCommonModule} from '../../teammates-common/teammates-common.module';
 import {
   CommentVisibilityControlNamePipe,
-  CommentVisibilityTypeDescriptionPipe, CommentVisibilityTypeNamePipe,
+  CommentVisibilityTypeDescriptionPipe,
+  CommentVisibilityTypeNamePipe,
 } from '../comment-visibility-setting.pipe';
-import { CommentEditFormComponent } from './comment-edit-form.component';
+import {CommentEditFormComponent} from './comment-edit-form.component';
 
 describe('CommentEditFormComponent', () => {
   let component: CommentEditFormComponent;
@@ -45,7 +50,38 @@ describe('CommentEditFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // toggleVisibilityTable
+  describe('ngOnInit', () => {
+    it('should not modify the response when response is not provided', () => {
+      component.ngOnInit();
+      expect(component.response).toBeUndefined();
+    });
+
+    it('should remove anonymous hash from giver and recipient when response is provided', () => {
+      const feedbackResponseDetails : FeedbackResponseDetails = {
+        questionType: FeedbackQuestionType.CONSTSUM,
+      };
+      const commentOutputs: CommentOutput[] = [];
+      component.response = {
+        isMissingResponse: true,
+        responseId: 'string',
+        giver: 'Anonymous student 123',
+        giverTeam: 'string',
+        giverSection: 'string',
+        recipient: 'Anonymous instructor 456',
+        recipientTeam: 'string',
+        recipientSection: 'string',
+        responseDetails: feedbackResponseDetails,
+        instructorComments: commentOutputs
+      };
+      const response = component.response;
+
+      component.ngOnInit();
+
+      expect(response.giver).toEqual('Anonymous student');
+      expect(response.recipient).toEqual('Anonymous instructor');
+    });
+  });
+
   it('should initialize with the visibility table collapsed', () => {
     expect(component.isVisibilityTableExpanded).toBeFalsy();
   });
@@ -53,7 +89,6 @@ describe('CommentEditFormComponent', () => {
   it('should toggle visibility table from collapsed to expanded', () => {
     expect(component.isVisibilityTableExpanded).toBeFalsy();
     component.toggleVisibilityTable();
-    // After toggling, it should be expanded
     expect(component.isVisibilityTableExpanded).toBeTruthy();
   });
 
@@ -61,11 +96,9 @@ describe('CommentEditFormComponent', () => {
     component.isVisibilityTableExpanded = true;
     expect(component.isVisibilityTableExpanded).toBeTruthy();
     component.toggleVisibilityTable();
-    // After toggling, it should be collapsed
     expect(component.isVisibilityTableExpanded).toBeFalsy();
   });
 
-  // triggerModelChange
   it('should trigger model change with updated field', () => {
     const testField = 'commentText';
     const testData = 'Updated comment text';
@@ -74,14 +107,12 @@ describe('CommentEditFormComponent', () => {
 
     component.triggerModelChange(testField, testData);
 
-    // Verify that the model should be updated
     expect(emitSpy).toHaveBeenCalledWith({
       ...component.model,
       [testField]: testData,
     });
   });
 
-  // triggerModelChangeBatch
   it('should emit the updated model when triggerModelChangeBatch is called', () => {
     component.model = {
       commentText: 'Initial Comment',
@@ -102,55 +133,44 @@ describe('CommentEditFormComponent', () => {
 
     component.triggerModelChangeBatch(updatedModel);
 
-    // Check if the model is updated
     expect(modelChangeSpy).toHaveBeenCalledWith(updatedModel);
   });
 
-  // triggerCloseCommentBoxEvent
   it('should emit the closeCommentBoxEvent when triggerCloseCommentBoxEvent is called', () => {
     const closeCommentBoxEventSpy = jest.spyOn(component.closeCommentBoxEvent, 'emit');
     component.triggerCloseCommentBoxEvent();
-    // Check if the closeCommentBoxEvent was emitted
     expect(closeCommentBoxEventSpy).toHaveBeenCalled();
   });
 
-  // triggerSaveCommentEvent
   it('should emit the saveCommentEvent when triggerSaveCommentEvent is called', () => {
     const saveCommentEventSpy = jest.spyOn(component.saveCommentEvent, 'emit');
     component.triggerSaveCommentEvent();
-    // Check if the saveCommentEvent was emitted
     expect(saveCommentEventSpy).toHaveBeenCalled();
   });
 
-  // modifyVisibilityControl
   it('should allow and disallow visibility control and trigger model change', () => {
     const isAllowed = true;
     const visibilityType = CommentVisibilityType.GIVER;
     const visibilityControl = CommentVisibilityControl.SHOW_COMMENT;
 
-    jest.spyOn(component.visibilityStateMachine, 'allowToSee');
-    jest.spyOn(component.visibilityStateMachine, 'disallowToSee');
-    jest.spyOn(component, 'triggerModelChangeBatch');
+    const allowToSee = jest.spyOn(component.visibilityStateMachine, 'allowToSee');
+    const disallowToSee = jest.spyOn(component.visibilityStateMachine, 'disallowToSee');
+    const triggerModelChangeBatch = jest.spyOn(component, 'triggerModelChangeBatch');
 
     component.modifyVisibilityControl(isAllowed, visibilityType, visibilityControl);
 
-    // Check if the allowToSee method was called and disallowToSee was not called
-    expect(component.visibilityStateMachine.allowToSee).toHaveBeenCalledWith(visibilityType, visibilityControl);
-    expect(component.visibilityStateMachine.disallowToSee).not.toHaveBeenCalled();
-
-    // Check if triggerModelChangeBatch was called with the expected visibility types
-    expect(component.triggerModelChangeBatch).toHaveBeenCalledWith({
+    expect(allowToSee).toHaveBeenCalledWith(visibilityType, visibilityControl);
+    expect(disallowToSee).not.toHaveBeenCalled();
+    expect(triggerModelChangeBatch).toHaveBeenCalledWith({
       showCommentTo: [visibilityType],
       showGiverNameTo: [],
     });
 
     component.modifyVisibilityControl(false, visibilityType, visibilityControl);
 
-    // Check if the disallowToSee method was called and allowToSee was not called
     expect(component.visibilityStateMachine.disallowToSee).toHaveBeenCalledWith(visibilityType, visibilityControl);
     expect(component.visibilityStateMachine.allowToSee).toHaveBeenCalled();
 
-    // Check if triggerModelChangeBatch was called again with the updated visibility
     expect(component.triggerModelChangeBatch).toHaveBeenCalledWith({
       showCommentTo: [],
       showGiverNameTo: [],
