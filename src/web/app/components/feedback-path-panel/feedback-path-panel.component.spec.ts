34190897dee864058a031e91c773f410cdb8b9e9
u6714885 <u6714885@anu.edu.au>
2023-10-20 07:35:28 +1100

    add tests

--- src/web/app/components/feedback-path-panel/feedback-path-panel.component.spec.ts
+++ src/web/app/components/feedback-path-panel/feedback-path-panel.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { FeedbackParticipantType, NumberOfEntitiesToGiveFeedbackToSetting } from '../../../types/api-output';
 import { TeammatesCommonModule } from '../teammates-common/teammates-common.module';
 import { FeedbackPathPanelComponent } from './feedback-path-panel.component';
 
@@ -26,10 +27,59 @@ describe('FeedbackPathPanelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FeedbackPathPanelComponent);
     component = fixture.componentInstance;
+
+    component.customNumberOfEntitiesToGiveFeedbackTo.subscribe((value) => {
+      component.model.customNumberOfEntitiesToGiveFeedbackTo = value;
+    });
+    component.numberOfEntitiesToGiveFeedbackToSetting.subscribe((value) => {
+      component.model.numberOfEntitiesToGiveFeedbackToSetting = value;
+    });
+    component.customFeedbackPath.subscribe((value) => {
+      component.model.isUsingOtherFeedbackPath = value;
+    });
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggle sub menu status correctly', () => {
+    const menu = FeedbackParticipantType.STUDENTS;
+    component.toggleSubMenu(menu);
+    const firstToggle = component.isSubMenuOpen(menu);
+    component.toggleSubMenu(menu);
+    const secondToggle = component.isSubMenuOpen(menu);
+    expect(firstToggle).toBe(!secondToggle);
+  });
+
+  it('should reset menu', () => {
+    const menu = FeedbackParticipantType.STUDENTS;
+    component.toggleSubMenu(menu);
+    component.resetMenu();
+    const isMenuOpen = component.isSubMenuOpen(menu);
+    expect(isMenuOpen).toBe(false);
+  });
+
+  it('should trigger custom number of entities', () => {
+    const customNumber = 5;
+    component.triggerCustomNumberOfEntities(customNumber);
+    const result = component.model.customNumberOfEntitiesToGiveFeedbackTo;
+    expect(result).toEqual(customNumber);
+  });
+
+  it('should trigger number of entities setting', () => {
+    const setting = NumberOfEntitiesToGiveFeedbackToSetting.CUSTOM;
+    component.triggerNumberOfEntitiesSetting(setting);
+    const result = component.model.numberOfEntitiesToGiveFeedbackToSetting;
+     expect(result).toEqual(setting);
+  });
+
+  it('should trigger custom feedback path', () => {
+    component.triggerCustomFeedbackPath();
+    const result = component.model.isUsingOtherFeedbackPath;
+    expect(result).toBe(true);
+  });
+
 });
