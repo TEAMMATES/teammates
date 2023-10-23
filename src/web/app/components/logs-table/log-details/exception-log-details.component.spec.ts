9c8a95011988e28c3823b19257a23236835a31be
nikkixiong <140723684+nikkixiong@users.noreply.github.com>
2023-10-23 23:43:09 +0000

    Update exception-log-details.component.spec.ts

--- src/web/app/components/logs-table/log-details/exception-log-details.component.spec.ts
+++ src/web/app/components/logs-table/log-details/exception-log-details.component.spec.ts
@@ -1,112 +1,118 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { GeneralLogEntry, LogEvent, LogSeverity, ExceptionLogDetails } from '../../../../types/api-output';
 
+import { GeneralLogEntry, LogEvent, LogSeverity, ExceptionLogDetails } from '../../../../types/api-output';
 import { ExceptionLogDetailsComponent } from './exception-log-details.component';
 
 describe('ExceptionLogDetailsComponent', () => {
-  let component: ExceptionLogDetailsComponent;
-  let fixture: ComponentFixture<ExceptionLogDetailsComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ExceptionLogDetailsComponent],
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ExceptionLogDetailsComponent);
-    component = fixture.componentInstance;
+@@ -19,7 +19,109 @@ describe('ExceptionLogDetailsComponent', () => {
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  describe('Component Initialization', () => {
+    it('should instantiate the component', () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should create stack trace string', () => {
-    const details: ExceptionLogDetails = {
-      event: LogEvent.EXCEPTION_LOG,
-      exceptionClass: 'exceptionClass1',
-      exceptionClasses: ['exceptionClass1'],
-      exceptionStackTraces: [['exceptionStackTrace1']],
-      exceptionMessages: ['exceptionMessage1'],
-      loggerSourceLocation: { file: 'file', line: 1, function: 'function' },
-    };
+  describe('Stack Trace String Creation', () => {
+    // Check the logic in the createStackTraceString function
+    let exceptionDetails: ExceptionLogDetails;
+    let log: GeneralLogEntry;
 
-    const stackTrace = component.createStackTraceString(details);
-    expect(stackTrace).toContain('exceptionClass1: exceptionMessage1');
-    expect(stackTrace).toContain('at exceptionStackTrace');
+    beforeEach(() => {
+      exceptionDetails = {
+        event: LogEvent.EXCEPTION_LOG,
+        exceptionClass: '',
+        exceptionClasses: [],
+        exceptionStackTraces: [],
+        exceptionMessages: [],
+        loggerSourceLocation: { file: 'file', line: 1, function: 'function' },
+      };
+      log = {
+        severity: LogSeverity.INFO,
+        trace: 'trace_id_123',
+        insertId: 'unique_insert_id_456',
+        resourceIdentifier: {},
+        sourceLocation: { file: 'test_log.txt', line: 123, function: '' },
+        timestamp: Date.now(),
+        details: exceptionDetails,
+      };
+    });
+
+    it('should create a valid stack trace string', () => {
+      exceptionDetails.exceptionClasses = [''];
+      exceptionDetails.exceptionStackTraces = [['']];
+      exceptionDetails.exceptionMessages = ['message'];
+
+      component.log = { ...log, details: exceptionDetails };
+      expect(component.exceptionStackTrace).toContain(': message');
+      expect(component.exceptionStackTrace).toContain('        at ');
+    });
+
+    it('should return an empty string if the number of exceptionClasses and exceptionStackTraces is the same', () => {
+      exceptionDetails.exceptionClasses = [''];
+      exceptionDetails.exceptionStackTraces = [[''], ['']];
+      exceptionDetails.exceptionMessages = ['', ''];
+
+      component.log = { ...log, details: exceptionDetails };
+      expect(component.exceptionStackTrace).toBe('');
+    });
+
+    it('should return an empty string if the number of exceptionClasses and exceptionMessages '
+    + 'is different, when the number of exceptionClasses and exceptionStackTraces is the same', () => {
+      exceptionDetails.exceptionClasses = [''];
+      exceptionDetails.exceptionStackTraces = [['']];
+      exceptionDetails.exceptionMessages = ['', ''];
+
+      component.log = { ...log, details: exceptionDetails };
+      expect(component.exceptionStackTrace).toBe('');
+    });
   });
 
-  // Test for get log
-  it('should return a valid GeneralLogEntry', () => {
-    const log: GeneralLogEntry = {
-      severity: LogSeverity.INFO,
-      trace: 'test_trace',
-      insertId: '1',
-      resourceIdentifier: {},
-      sourceLocation: { file: 'log.txt', line: 42, function: 'test_function' },
-      timestamp: Date.now(),
-      details: undefined,
-    };
+  describe('Log Entry Manipulation', () => {
+    it('should assign and retrieve a valid GeneralLogEntry', () => {
+      const log: GeneralLogEntry = {
+        severity: LogSeverity.INFO,
+        trace: 'trace_id_123',
+        insertId: 'unique_insert_id_456',
+        resourceIdentifier: {},
+        sourceLocation: { file: 'test_log.txt', line: 123, function: '' },
+        timestamp: Date.now(),
+        details: undefined,
+      };
 
-    component.logValue = log;
-    expect(component.log).toEqual(log);
+      component.logValue = log;
+      expect(component.log).toEqual(log);
+    });
+
+    it('should clear exception exceptionDetails when exceptionStackTrace exists', () => {
+      const exceptionDetailsForParse: ExceptionLogDetails = {
+        event: LogEvent.EXCEPTION_LOG,
+        exceptionClass: '',
+        exceptionClasses: [''],
+        exceptionStackTraces: [['']],
+        exceptionMessages: [''],
+        loggerSourceLocation: { file: 'test_log.txt', line: 43, function: '' },
+      };
+
+      const log: GeneralLogEntry = {
+        ...exceptionDetailsForParse,
+        severity: LogSeverity.INFO,
+        trace: 'trace_id_123',
+        insertId: 'unique_insert_id_456',
+        resourceIdentifier: {},
+        sourceLocation: { file: 'test_log.txt', line: 123, function: '' },
+        timestamp: Date.now(),
+        details: exceptionDetailsForParse,
+      };
+
+      component.log = log;
+
+      expect(component.details.exceptionClasses).toBeUndefined();
+      expect(component.details.exceptionMessages).toBeUndefined();
+      expect(component.details.exceptionStackTraces).toBeUndefined();
+    });
   });
-
-  it('should clear details when exceptionStackTrace exists', () => {
-    const detailsForParse: ExceptionLogDetails = {
-      event: LogEvent.EXCEPTION_LOG,
-      exceptionClass: 'exceptionClass',
-      exceptionClasses: ['exceptionClass'],
-      exceptionStackTraces: [['exceptionStackTrace']],
-      exceptionMessages: ['exceptionMessage'],
-      loggerSourceLocation: { file: 'log.txt', line: 43, function: 'test_function' },
-    };
-
-    const log = {
-      severity: LogSeverity.INFO,
-      trace: 'test_trace',
-      insertId: '1',
-      resourceIdentifier: {},
-      sourceLocation: { file: 'log.txt', line: 42, function: 'test_function' },
-      timestamp: Date.now(),
-      details: detailsForParse,
-    };
-    component.log = log;
-
-    expect(component.details.exceptionClasses).toBeUndefined();
-    expect(component.details.exceptionMessages).toBeUndefined();
-    expect(component.details.exceptionStackTraces).toBeUndefined();
-  });
-
-  it('should return an empty string if exceptionClasses and exceptionStackTraces mismatch', () => {
-    const details: ExceptionLogDetails = {
-      event: LogEvent.EXCEPTION_LOG,
-      exceptionClass: 'exceptionClass',
-      exceptionClasses: ['exceptionClass'],
-      exceptionStackTraces: [['exceptionStackTrace'], ['another exceptionStackTrace']],
-      exceptionMessages: ['exceptionMessage', 'another exceptionMessage'],
-      loggerSourceLocation: { file: 'file', line: 1, function: 'function' },
-    };
-
-    const stackTrace = component.createStackTraceString(details);
-    expect(stackTrace).toBe('');
-  });
-
-  it('should return an empty string if exceptionMessages count mismatch with exceptionClasses', () => {
-    const details: ExceptionLogDetails = {
-      event: LogEvent.EXCEPTION_LOG,
-      exceptionClass: 'exceptionClass',
-      exceptionClasses: ['exceptionClass'],
-      exceptionStackTraces: [['exceptionStackTrace']],
-      exceptionMessages: ['exceptionMessage', 'another exceptionMessage'],
-      loggerSourceLocation: { file: 'file', line: 1, function: 'function' },
-    };
-
-    const stackTrace = component.createStackTraceString(details);
-    expect(stackTrace).toBe('');
-  });
-
 });
