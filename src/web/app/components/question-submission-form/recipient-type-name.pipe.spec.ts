620f796200eb3855b956020dbd11cf11f0eb8cad
Thomas <u7416757@anu.edu.au>
2023-10-27 11:33:06 +1100

    [#12588] fix some typo

--- src/web/app/components/question-submission-form/recipient-type-name.pipe.spec.ts
+++ src/web/app/components/question-submission-form/recipient-type-name.pipe.spec.ts
@@ -1,50 +1,8 @@
 import { RecipientTypeNamePipe } from './recipient-type-name.pipe';
-import { FeedbackParticipantType } from '../../../types/api-output';
 
 describe('RecipientTypeNamePipe', () => {
-  let pipe: RecipientTypeNamePipe;
-
-  beforeEach(() => {
-    pipe = new RecipientTypeNamePipe();
-  });
-
-  it('should create an instance', () => {
+  it('create an instance', () => {
+    const pipe: RecipientTypeNamePipe = new RecipientTypeNamePipe();
     expect(pipe).toBeTruthy();
-  });
-
-  it('should return "Team" for TEAMS', () => {
-    expect(pipe.transform(FeedbackParticipantType.TEAMS, FeedbackParticipantType.STUDENTS)).toEqual('Team');
-  });
-
-  it('should return "Student" for STUDENTS', () => {
-    expect(pipe.transform(FeedbackParticipantType.STUDENTS, FeedbackParticipantType.STUDENTS)).toEqual('Student');
-  });
-
-  it('should return "Instructor" for INSTRUCTORS', () => {
-    expect(pipe.transform(FeedbackParticipantType.INSTRUCTORS, FeedbackParticipantType.STUDENTS)).toEqual('Instructor');
-  });
-
-  // ... you can continue with the other FeedbackParticipantTypes
-
-  describe('for OWN_TEAM_MEMBERS and OWN_TEAM_MEMBERS_INCLUDING_SELF', () => {
-    it('should return "Student" when giverType is STUDENTS', () => {
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS, FeedbackParticipantType.STUDENTS)).toEqual('Student');
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS_INCLUDING_SELF, FeedbackParticipantType.STUDENTS)).toEqual('Student');
-    });
-
-    it('should return "Instructor" when giverType is INSTRUCTORS', () => {
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS, FeedbackParticipantType.INSTRUCTORS)).toEqual('Instructor');
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS_INCLUDING_SELF, FeedbackParticipantType.INSTRUCTORS)).toEqual('Instructor');
-    });
-
-    it('should return "Student" when giverType is TEAMS', () => {
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS, FeedbackParticipantType.TEAMS)).toEqual('Student');
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS_INCLUDING_SELF, FeedbackParticipantType.TEAMS)).toEqual('Student');
-    });
-
-    it('should return "Unknown" for any other giverType', () => {
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS, 'ANY_OTHER_TYPE' as FeedbackParticipantType)).toEqual('Unknown');
-      expect(pipe.transform(FeedbackParticipantType.OWN_TEAM_MEMBERS_INCLUDING_SELF, 'ANY_OTHER_TYPE' as FeedbackParticipantType)).toEqual('Unknown');
-    });
   });
 });
