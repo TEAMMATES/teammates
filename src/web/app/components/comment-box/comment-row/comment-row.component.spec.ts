101af472fb0bf2eb3110d0be8aa45428e62b02f1
Thomas <u7416757@anu.edu.au>
2023-10-29 12:01:51 +1100

    [#12588] Fix the error

--- src/web/app/components/comment-box/comment-row/comment-row.component.spec.ts
+++ src/web/app/components/comment-box/comment-row/comment-row.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FeedbackResponseCommentService } from '../../../../services/feedback-response-comment.service';
-import { CommentVisibilityType, FeedbackVisibilityType  } from '../../../../types/api-output';
+import { CommentVisibilityType, FeedbackVisibilityType } from '../../../../types/api-output';
 import { RichTextEditorModule } from '../../rich-text-editor/rich-text-editor.module';
 import { TeammatesCommonModule } from '../../teammates-common/teammates-common.module';
 import { CommentEditFormComponent } from '../comment-edit-form/comment-edit-form.component';
@@ -92,7 +92,7 @@ describe('CommentRowComponent', () => {
       expect(spyVisibilityStateMachine.allowAllApplicableTypesToSee).toHaveBeenCalled();
 
       expect(spyCommentService.getNewVisibilityStateMachine).toHaveBeenCalledWith(
-      component.questionShowResponsesTo
+      component.questionShowResponsesTo,
     );
     });
 
