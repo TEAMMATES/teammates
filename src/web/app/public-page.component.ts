8a87d1b491401399b22744419ff35b600f19cc29
u7452980 <u7452980@anu.edu.au>
2023-10-29 20:21:02 +1100

    public page component

--- src/web/app/public-page.component.ts
+++ src/web/app/public-page.component.ts
@@ -8,9 +8,50 @@ import { AuthService } from '../services/auth.service';
  */
 @Component({
   selector: 'tm-public-page',
-  template: '<tm-page [isValidUser]="true" [hideAuthInfo]="true"></tm-page>',
+  template: '<tm-page [navItems]="navItems" [isValidUser]="true" [hideAuthInfo]="true"></tm-page>',
 })
 export class PublicPageComponent {
+
+  navItems: any[] = [
+    {
+      url: '/web/instructor',
+      display: 'Home',
+    },
+    {
+      url: '/web/instructor/courses',
+      display: 'Courses',
+    },
+    {
+      url: '/web/instructor/sessions',
+      display: 'Sessions',
+    },
+    {
+      url: '/web/instructor/students',
+      display: 'Students',
+    },
+    {
+      url: '/web/instructor/search',
+      display: 'Search',
+    },
+    {
+      url: '/web/instructor/notifications',
+      display: 'Notifications',
+    },
+    {
+      display: 'Help',
+      children: [
+        {
+          url: '/web/instructor/getting-started',
+          display: 'Getting Started',
+        },
+        {
+          url: '/web/instructor/help',
+          display: 'Instructor Help',
+        },
+      ],
+    },
+  ];
+
   constructor(private route: ActivatedRoute,
               private authService: AuthService) {
     if (environment.maintenance) {
