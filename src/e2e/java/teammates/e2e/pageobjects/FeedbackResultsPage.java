fedc0ebee2b0578327c5f7a2dc7c9acfb157c7e6
Neo Wei Qing <ztolenstar@hotmail.com>
2023-11-28 17:32:18 +0800

    fix axe tests

--- src/e2e/java/teammates/e2e/pageobjects/FeedbackResultsPage.java
+++ src/e2e/java/teammates/e2e/pageobjects/FeedbackResultsPage.java
@@ -251,7 +251,7 @@ public class FeedbackResultsPage extends AppPage {
                                              List<FeedbackResponseAttributes> expectedResponses,
                                              boolean isGiverVisible) {
         List<WebElement> anonymousViews = getAllResponseViews(question.getQuestionNumber()).stream()
-                .filter(v -> isAnonymous(v.findElement(By.id("response-recipient")).getText()))
+                .filter(v -> isAnonymous(v.findElement(By.className("response-recipient")).getText()))
                 .collect(Collectors.toList());
         if (anonymousViews.isEmpty()) {
             fail("No anonymous views found");
@@ -309,7 +309,7 @@ public class FeedbackResultsPage extends AppPage {
 
     private boolean isAnyAnonymousResponseEqual(FeedbackQuestionAttributes question, WebElement responseView,
                                                 FeedbackResponseAttributes response) {
-        List<WebElement> giverNames = responseView.findElements(By.id("response-giver"));
+        List<WebElement> giverNames = responseView.findElements(By.className("response-giver"));
         List<WebElement> responseFields = getAllResponseFields(responseView);
         for (int i = 0; i < giverNames.size(); i++) {
             if (isAnonymous(giverNames.get(i).getText()) && isResponseEqual(question, responseFields.get(i), response)) {
@@ -468,13 +468,13 @@ public class FeedbackResultsPage extends AppPage {
     private WebElement getGivenResponseField(int questionNum, String receiver) {
         int recipientIndex = getGivenRecipientIndex(questionNum, receiver);
         return getQuestionResponsesSection(questionNum)
-                .findElements(By.cssSelector("#given-responses tm-single-response"))
+                .findElements(By.cssSelector(".given-responses tm-single-response"))
                 .get(recipientIndex);
     }
 
     private int getGivenRecipientIndex(int questionNum, String recipient) {
         List<WebElement> recipients = getQuestionResponsesSection(questionNum)
-                .findElements(By.cssSelector("#given-responses #response-recipient"));
+                .findElements(By.cssSelector(".given-responses .response-recipient"));
         for (int i = 0; i < recipients.size(); i++) {
             if (recipients.get(i).getText().split("To: ")[1].equals(recipient)) {
                 return i;
@@ -584,7 +584,7 @@ public class FeedbackResultsPage extends AppPage {
     }
 
     private String getCommentGiver(WebElement commentField) {
-        String commentGiverDescription = commentField.findElement(By.id("comment-giver-name")).getText();
+        String commentGiverDescription = commentField.findElement(By.className("comment-giver-name")).getText();
         return commentGiverDescription.split(" commented")[0];
     }
 
@@ -608,7 +608,7 @@ public class FeedbackResultsPage extends AppPage {
     }
 
     private int getGiverIndex(WebElement response, String giver) {
-        List<WebElement> givers = response.findElements(By.id("response-giver"));
+        List<WebElement> givers = response.findElements(By.className("response-giver"));
         for (int i = 0; i < givers.size(); i++) {
             if (givers.get(i).getText().contains(giver)) {
                 return i;
@@ -618,7 +618,7 @@ public class FeedbackResultsPage extends AppPage {
     }
 
     private int getRecipientIndex(int questionNum, String recipient) {
-        List<WebElement> recipients = getQuestionResponsesSection(questionNum).findElements(By.id("response-recipient"));
+        List<WebElement> recipients = getQuestionResponsesSection(questionNum).findElements(By.className("response-recipient"));
         for (int i = 0; i < recipients.size(); i++) {
             if (recipients.get(i).getText().split("To: ")[1].equals(recipient)) {
                 return i;
