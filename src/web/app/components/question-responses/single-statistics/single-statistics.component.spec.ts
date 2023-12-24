5478e21f5d414b265dc72d5416abd1c963041af7
PHILIP SARFO OWUSU <powusu4119@gmail.com>
2023-12-24 08:42:04 -0600

    Update single-statistics.component.spec.ts
    fix lint issues

--- src/web/app/components/question-responses/single-statistics/single-statistics.component.spec.ts
+++ src/web/app/components/question-responses/single-statistics/single-statistics.component.spec.ts
@@ -1,9 +1,9 @@
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
-import {QuestionStatisticsModule} from '../../question-types/question-statistics/question-statistics.module';
-import {SingleStatisticsComponent} from './single-statistics.component';
-import {FeedbackQuestionType, ResponseOutput} from "../../../../types/api-output";
-import {FeedbackResponsesService} from "../../../../services/feedback-responses.service";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FeedbackResponsesService } from '../../../../services/feedback-responses.service';
+import { FeedbackQuestionType, ResponseOutput } from '../../../../types/api-output';
+import { QuestionStatisticsModule } from '../../question-types/question-statistics/question-statistics.module';
+import { SingleStatisticsComponent } from './single-statistics.component';
 
 describe('SingleStatisticsComponent', () => {
   let component: SingleStatisticsComponent;
@@ -15,8 +15,8 @@ describe('SingleStatisticsComponent', () => {
       imports: [QuestionStatisticsModule, HttpClientTestingModule],
       providers: [{
         provide: FeedbackResponsesService,
-        useValue: { isFeedbackResponsesDisplayedOnSection: () => true }
-      }]
+        useValue: { isFeedbackResponsesDisplayedOnSection: () => true },
+      }],
     }).compileComponents();
   }));
 
@@ -26,49 +26,55 @@ describe('SingleStatisticsComponent', () => {
     fixture.detectChanges();
   });
 
+  /**
+   * Mock component responses list for all scenarios.
+   *
+   * @param isMissingResponse - is for setting true/false in the isMissingResponse field if missing response.
+   * @param questionType - is for updating feedback question type field
+   * @param recipient - is for updating recipient field
+   */
+  function getResponseOutput(isMissingResponse: boolean,
+                             questionType: FeedbackQuestionType,
+                             recipient: string = 'recipient'): ResponseOutput[] {
+    return [
+      {
+        isMissingResponse,
+        responseId: 'some_id',
+        giver: 'some_giver',
+        giverTeam: 'some_team',
+        giverSection: 'section_1',
+        recipient,
+        recipientTeam: 'recipient_1_team',
+        recipientSection: 'recipient_1_section',
+        responseDetails: {
+          questionType,
+        },
+        instructorComments: [],
+      },
+    ];
+  }
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should return empty responsesToUse when missing response & feedback question type is not contrib', () => {
     component.responses = getResponseOutput(true, FeedbackQuestionType.TEXT);
-    fixture.detectChanges()
-    expect(component.responsesToUse.length).toBe(0)
-  })
+    fixture.detectChanges();
+    expect(component.responsesToUse.length).toBe(0);
+  });
 
   it('should return empty when is using response to self & response recipient is not You', () => {
-    component.isStudent = true
+    component.isStudent = true;
     component.responses = getResponseOutput(false, FeedbackQuestionType.NUMSCALE);
     fixture.detectChanges();
-    expect(component.responsesToUse.length).toBe(0)
+    expect(component.responsesToUse.length).toBe(0);
   });
 
-  it('should return responses based on selected section when ngOnInit is called',  () => {
-    component.isStudent = false
+  it('should return responses based on selected section when ngOnInit is called', () => {
+    component.isStudent = false;
     component.responses = getResponseOutput(false, FeedbackQuestionType.CONTRIB, 'You');
-    component.ngOnInit()
-    expect(component.responsesToUse.length).toBe(1)
+    component.ngOnInit();
+    expect(component.responsesToUse.length).toBe(1);
   });
-
-  function getResponseOutput(isMissingResponse: boolean,
-                             questionType: FeedbackQuestionType,
-                             recipient: string = 'recipient'): ResponseOutput[] {
-    return [
-      {
-        isMissingResponse: isMissingResponse,
-        responseId: 'some_id',
-        giver: 'some_giver',
-        giverTeam: 'some_team',
-        giverSection: 'section_1',
-        recipient: recipient,
-        recipientTeam: 'recipient_1_team',
-        recipientSection: 'recipient_1_section',
-        responseDetails: {
-          questionType: questionType
-        },
-        instructorComments: []
-      }
-    ]
-  }
-
 });
