867211625778efe4e5fc7e962e301a81ed6dc47f
Dominic Berzin <berzindominic@gmail.com>
2024-01-29 15:14:50 +0800

    Add update comment logic

--- src/main/java/teammates/sqllogic/core/FeedbackResponseCommentsLogic.java
+++ src/main/java/teammates/sqllogic/core/FeedbackResponseCommentsLogic.java
@@ -1,11 +1,14 @@
 package teammates.sqllogic.core;
 
+import java.util.List;
 import java.util.UUID;
 
+import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
 import teammates.common.exception.EntityAlreadyExistsException;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.storage.sqlapi.FeedbackResponseCommentsDb;
+import teammates.storage.sqlentity.FeedbackResponse;
 import teammates.storage.sqlentity.FeedbackResponseComment;
 import teammates.ui.request.FeedbackResponseCommentUpdateRequest;
 
@@ -43,6 +46,21 @@ public final class FeedbackResponseCommentsLogic {
     public FeedbackResponseComment getFeedbackResponseComment(Long id) {
         return frcDb.getFeedbackResponseComment(id);
     }
+
+    public List<FeedbackResponseComment> getFeedbackResponseCommentForResponse(UUID feedbackResponseId) {
+        return frcDb.getFeedbackResponseCommentsForResponse(feedbackResponseId);
+    }
+
+        /**
+     * Gets all response comments for a response.
+     */
+    public List<FeedbackResponseComment> getFeedbackResponseCommentsForResponse(UUID feedbackResponseId) {
+        assert feedbackResponseId != null;
+
+        return frcDb.getFeedbackResponseCommentsForResponse(feedbackResponseId);
+    }
+
+
 
     /**
      * Gets the comment associated with the response.
@@ -90,11 +108,38 @@ public final class FeedbackResponseCommentsLogic {
     }
 
     /**
-     * Updates all email fields of feedback response comments.
+     * Updates a feedback response comment.
+     * @throws EntityDoesNotExistException if the comment does not exist
      */
+    public FeedbackResponseComment updateFeedbackResponseComment(Long frcId,
+            FeedbackResponseCommentAttributes updateRequest, String updaterEmail)
+            throws EntityDoesNotExistException {
+        FeedbackResponseComment comment = frcDb.getFeedbackResponseComment(frcId);
+        if (comment == null) {
+            throw new EntityDoesNotExistException("Trying to update a feedback response comment that does not exist.");
+        }
+
+        comment.setCommentText(updateRequest.getCommentText());
+        comment.setShowCommentTo(updateRequest.getShowCommentTo());
+        comment.setShowGiverNameTo(updateRequest.getShowGiverNameTo());
+        comment.setLastEditorEmail(updaterEmail);
+
+        return comment;
+    }
+
     public void updateFeedbackResponseCommentsEmails(String courseId, String oldEmail, String updatedEmail) {
         frcDb.updateGiverEmailOfFeedbackResponseComments(courseId, oldEmail, updatedEmail);
         frcDb.updateLastEditorEmailOfFeedbackResponseComments(courseId, oldEmail, updatedEmail);
+    }
+
+    public void updateFeedbackResponseCommentsForResponse(FeedbackResponse response)
+            throws InvalidParametersException, EntityDoesNotExistException {
+        List<FeedbackResponseComment> comments = getFeedbackResponseCommentForResponse(response.getId());
+        for (FeedbackResponseComment comment : comments) {
+            comment.setGiverSection(response.getGiverSection());
+            comment.setRecipientSection(response.getRecipientSection());
+            frcDb.updateFeedbackResponseComment(comment);
+        }
     }
 
 }
