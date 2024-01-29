2f59cbc6d4c6009695aa0c953c7f464d3d4a822f
Suneel Valmeti <suneelvalmeti@gmail.com>
2024-01-29 07:50:48 -0500

    Revert "Update InstructorSessionIndividualExtensionPageE2ETest.java"
    This reverts commit 77cd4c9898711c01e907f3fe5706225637861c73.

--- src/e2e/java/teammates/e2e/cases/InstructorSessionIndividualExtensionPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/InstructorSessionIndividualExtensionPageE2ETest.java
@@ -32,8 +32,6 @@ public class InstructorSessionIndividualExtensionPageE2ETest extends BaseE2ETest
     protected void prepareTestData() {
         testData = loadDataBundle("/InstructorSessionIndividualExtensionPageE2ETest.json");
         testEmail = TestProperties.TEST_EMAIL;
-        removeAndRestoreDataBundle(testData);
-        
         testData.students.get("alice.tmms@ISesIe.CS2104").setEmail(testEmail);
         instructor = testData.instructors.get("ISesIe.instructor1");
         course = testData.courses.get("course");
@@ -41,6 +39,7 @@ public class InstructorSessionIndividualExtensionPageE2ETest extends BaseE2ETest
         students = testData.students.values();
         instructors = testData.instructors.values();
 
+        removeAndRestoreDataBundle(testData);
     }
 
     @Test
