30d3c2a0bc835aa35fabd4e90b0dfd86c3eadf39
Dominic Berzin <berzindominic@gmail.com>
2024-02-02 10:51:42 +0800

    Fix lint

--- src/main/java/teammates/sqllogic/core/FeedbackResponseCommentsLogic.java
+++ src/main/java/teammates/sqllogic/core/FeedbackResponseCommentsLogic.java
@@ -3,7 +3,6 @@ package teammates.sqllogic.core;
 import java.util.List;
 import java.util.UUID;
 
-import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
 import teammates.common.exception.EntityAlreadyExistsException;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
