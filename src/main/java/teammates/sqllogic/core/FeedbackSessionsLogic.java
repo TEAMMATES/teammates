72f8f2dc7623d53f915ecacf88342328b7687a26
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2024-02-12 20:28:45 +0800

    update fs logic method

--- src/main/java/teammates/sqllogic/core/FeedbackSessionsLogic.java
+++ src/main/java/teammates/sqllogic/core/FeedbackSessionsLogic.java
@@ -284,9 +284,10 @@ public final class FeedbackSessionsLogic {
             return false;
         }
 
+        List<FeedbackQuestion> qns = fqLogic.getFeedbackQuestionsForSession(session);
         return isInstructor
-                ? fqLogic.hasFeedbackQuestionsForInstructors(session.getFeedbackQuestions(), false)
-                : fqLogic.hasFeedbackQuestionsForStudents(session.getFeedbackQuestions());
+                ? fqLogic.hasFeedbackQuestionsForInstructors(qns, false)
+                : fqLogic.hasFeedbackQuestionsForStudents(qns);
     }
 
     /**
