ae71d382ada31d1ec92164252510862d682f8061
Cedric Ong <cedricongjh@gmail.com>
2024-02-16 22:37:44 +0800

    Add test for getUsageStatisticsForTimeRange

--- src/it/java/teammates/it/storage/sqlapi/UsageStatisticsDbIT.java
+++ src/it/java/teammates/it/storage/sqlapi/UsageStatisticsDbIT.java
@@ -18,6 +18,36 @@ public class UsageStatisticsDbIT extends BaseTestCaseWithSqlDatabaseAccess {
     private final UsageStatisticsDb usageStatisticsDb = UsageStatisticsDb.inst();
 
     @Test
+    public void testGetUsageStatisticsForTimeRange() {
+        ______TS("returns empty array for no usageStatistics in time range");
+        Instant startTime = Instant.parse("2010-01-01T00:00:00Z");
+        List<UsageStatistics> actualUsageStatistics = usageStatisticsDb.getUsageStatisticsForTimeRange(
+                startTime, startTime.plus(1, ChronoUnit.DAYS));
+
+        assertEquals(actualUsageStatistics.size(), 0);
+
+        ______TS("returns correct number of usageStatistics in time range");
+        Instant startTimeOne = Instant.parse("2012-01-01T00:00:00Z");
+        UsageStatistics usageStatisticsOne = new UsageStatistics(
+                startTimeOne, 1, 0, 0, 0, 0, 0, 0, 0);
+
+        Instant startTimeTwo = Instant.parse("2012-01-02T00:00:00Z");
+        UsageStatistics usageStatisticsTwo = new UsageStatistics(
+                startTimeTwo, 1, 0, 0, 0, 0, 0, 0, 0);
+
+        usageStatisticsDb.createUsageStatistics(usageStatisticsOne);
+        usageStatisticsDb.createUsageStatistics(usageStatisticsTwo);
+
+        List<UsageStatistics> actulUsageStatisticsOne = usageStatisticsDb.getUsageStatisticsForTimeRange(
+                startTimeOne, startTimeOne.plus(1, ChronoUnit.DAYS));
+        assertEquals(actulUsageStatisticsOne.size(), 1);
+
+        List<UsageStatistics> actulUsageStatisticsTwo = usageStatisticsDb.getUsageStatisticsForTimeRange(
+                startTimeOne, startTimeOne.plus(2, ChronoUnit.DAYS));
+        assertEquals(actulUsageStatisticsTwo.size(), 2);
+    }
+
+    @Test
     public void testCreateUsageStatistics() {
         ______TS("success: create new usage statistics");
         Instant startTime = Instant.parse("2011-01-01T00:00:00Z");
