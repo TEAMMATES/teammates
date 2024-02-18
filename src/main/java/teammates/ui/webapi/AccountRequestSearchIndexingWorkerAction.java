28614f9498c0772405515185b746725f5800be96
Dominic Berzin <berzindominic@gmail.com>
2024-02-18 23:16:01 +0800

    feat: migrate account request search indexing worker action

--- src/main/java/teammates/ui/webapi/AccountRequestSearchIndexingWorkerAction.java
+++ src/main/java/teammates/ui/webapi/AccountRequestSearchIndexingWorkerAction.java
@@ -5,12 +5,14 @@ import org.apache.http.HttpStatus;
 import teammates.common.datatransfer.attributes.AccountRequestAttributes;
 import teammates.common.exception.SearchServiceException;
 import teammates.common.util.Const.ParamsNames;
+import teammates.storage.sqlentity.AccountRequest;
 
 /**
  * Task queue worker action: performs account request search indexing.
  */
 public class AccountRequestSearchIndexingWorkerAction extends AdminOnlyAction {
 
+    @SuppressWarnings("PMD.AvoidCatchingNPE")
     @Override
     public ActionResult execute() {
         String email = getNonNullRequestParamValue(ParamsNames.INSTRUCTOR_EMAIL);
@@ -18,12 +20,25 @@ public class AccountRequestSearchIndexingWorkerAction extends AdminOnlyAction {
 
         AccountRequestAttributes accountRequest = logic.getAccountRequest(email, institute);
 
+        //CHECKSTYLE.OFF:EmptyBlock
         try {
             logic.putAccountRequestDocument(accountRequest);
         } catch (SearchServiceException e) {
             // Set an arbitrary retry code outside of the range 200-299 to trigger automatic retry
             return new JsonResult("Failure", HttpStatus.SC_BAD_GATEWAY);
-        }
+        } catch (NullPointerException e) { }
+        //CHECKSTYLE.ON:EmptyBlock
+
+        AccountRequest accRequest = sqlLogic.getAccountRequest(email, institute);
+
+        //CHECKSTYLE.OFF:EmptyBlock
+        try {
+            sqlLogic.putAccountRequestDocument(accRequest);
+        } catch (SearchServiceException e) {
+            // Set an arbitrary retry code outside of the range 200-299 to trigger automatic retry
+            return new JsonResult("Failure", HttpStatus.SC_BAD_GATEWAY);
+        } catch (NullPointerException e) { }
+        //CHECKSTYLE.ON:EmptyBlock
 
         return new JsonResult("Successful");
     }
