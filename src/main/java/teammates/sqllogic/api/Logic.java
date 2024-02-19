31e44fa357ae7e49ce58858623cdf92e398191d4
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2024-02-20 00:38:05 +0800

    [#12048] Migrate FeedbackSessionOpeningSoonRemindersAction (#12740)
    * update logic
    
    * add it test for action
    
    * fix checkstyle errors

--- src/main/java/teammates/sqllogic/api/Logic.java
+++ src/main/java/teammates/sqllogic/api/Logic.java
@@ -1457,4 +1457,11 @@ public class Logic {
 
         return accountRequestLogic.searchAccountRequestsInWholeSystem(queryString);
     }
+
+    /**
+     * Returns a list of sessions that are going to open soon.
+     */
+    public List<FeedbackSession> getFeedbackSessionsOpeningWithinTimeLimit() {
+        return feedbackSessionsLogic.getFeedbackSessionsOpeningWithinTimeLimit();
+    }
 }
