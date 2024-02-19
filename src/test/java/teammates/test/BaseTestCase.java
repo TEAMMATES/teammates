901307a1b1c6d7e66840d400a69f2023be466637
Xenos F <git@xenosfio.com>
2024-02-19 13:01:15 +0800

    Add test for SqlSessionResultsBundle

--- src/test/java/teammates/test/BaseTestCase.java
+++ src/test/java/teammates/test/BaseTestCase.java
@@ -19,7 +19,9 @@ import teammates.common.datatransfer.InstructorPrivileges;
 import teammates.common.datatransfer.NotificationStyle;
 import teammates.common.datatransfer.NotificationTargetUser;
 import teammates.common.datatransfer.SqlDataBundle;
+import teammates.common.datatransfer.questions.FeedbackResponseDetails;
 import teammates.common.datatransfer.questions.FeedbackTextQuestionDetails;
+import teammates.common.datatransfer.questions.FeedbackTextResponseDetails;
 import teammates.common.util.Const;
 import teammates.common.util.FieldValidator;
 import teammates.common.util.JsonUtils;
@@ -27,6 +29,7 @@ import teammates.sqllogic.core.DataBundleLogic;
 import teammates.storage.sqlentity.Account;
 import teammates.storage.sqlentity.Course;
 import teammates.storage.sqlentity.FeedbackQuestion;
+import teammates.storage.sqlentity.FeedbackResponse;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.storage.sqlentity.Instructor;
 import teammates.storage.sqlentity.Notification;
@@ -168,6 +171,15 @@ public class BaseTestCase {
                 FeedbackParticipantType.SELF, FeedbackParticipantType.SELF, 1, new ArrayList<FeedbackParticipantType>(),
                 new ArrayList<FeedbackParticipantType>(), new ArrayList<FeedbackParticipantType>(),
                 new FeedbackTextQuestionDetails("test question text"));
+    }
+
+    protected FeedbackResponse getTypicalFeedbackResponseForQuestion(FeedbackQuestion question) {
+        return FeedbackResponse.makeResponse(question, "test-giver", getTypicalSection(), "test-recipient",
+                getTypicalSection(), getTypicalFeedbackResponseDetails());
+    }
+
+    protected FeedbackResponseDetails getTypicalFeedbackResponseDetails() {
+        return new FeedbackTextResponseDetails();
     }
 
     /**
