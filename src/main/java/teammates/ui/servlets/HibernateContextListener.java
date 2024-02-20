6689d713a574a7ed3e89e8c43362570a7f954da8
Fergus Mok <FergusMok1@gmail.com>
2024-02-20 22:52:00 +0800

    Add DataMigrationEntitiesBaseScriptSql and example Account Migration

--- src/main/java/teammates/ui/servlets/HibernateContextListener.java
+++ src/main/java/teammates/ui/servlets/HibernateContextListener.java
@@ -3,6 +3,7 @@ package teammates.ui.servlets;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import teammates.common.util.Config;
 import teammates.common.util.HibernateUtil;
 
 /**
@@ -13,13 +14,8 @@ public class HibernateContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent event) {
         // Invoked by Jetty at application startup.
-        // HibernateUtil.buildSessionFactory(Config.getDbConnectionUrl(), Config.APP_LOCALPOSTGRES_USERNAME,
-        //        Config.APP_LOCALPOSTGRES_PASSWORD);
-        
-        String connectionUrl = "jdbc:postgresql://35.240.141.62:5432/postgres";
-        String username = "postgres";
-        String password = "teammatesrocks";
-        HibernateUtil.buildSessionFactory(connectionUrl, username, password);
+        HibernateUtil.buildSessionFactory(Config.getDbConnectionUrl(), Config.APP_LOCALPOSTGRES_USERNAME,
+                Config.APP_LOCALPOSTGRES_PASSWORD);
     }
 
     @Override
