0668931ae77eb2dac7c40741a5d39db092721931
Xenos F <git@xenosfio.com>
2024-02-24 09:27:37 +0800

    Refactor no response text to const

--- src/main/java/teammates/storage/sqlentity/responses/FeedbackMissingResponse.java
+++ src/main/java/teammates/storage/sqlentity/responses/FeedbackMissingResponse.java
@@ -1,6 +1,7 @@
 package teammates.storage.sqlentity.responses;
 
 import teammates.common.datatransfer.questions.FeedbackTextResponseDetails;
+import teammates.common.util.Const;
 import teammates.storage.sqlentity.FeedbackQuestion;
 
 /**
@@ -19,7 +20,7 @@ public class FeedbackMissingResponse extends FeedbackTextResponse {
             FeedbackQuestion feedbackQuestion, String giver,
             String giverSectionName, String recipient, String recipientSectionName
     ) {
-        super(feedbackQuestion, giver, null, recipient, null, new FeedbackTextResponseDetails("No Response"));
+        super(feedbackQuestion, giver, null, recipient, null, new FeedbackTextResponseDetails(Const.MISSING_RESPONSE_TEXT));
         this.giverSectionName = giverSectionName;
         this.recipientSectionName = recipientSectionName;
     }
