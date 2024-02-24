057e512f416d9d39cfd5fd23e3fb2e4b54d98896
Kevin Foong <kevin9foong@gmail.com>
2024-02-24 21:50:22 +0800

    Add migration script for acc req

---
+++ src/client/java/teammates/client/scripts/sql/DataMigrationForAccountRequestSql.java
@@ -0,0 +1,69 @@
+package teammates.client.scripts.sql;
+
+import com.googlecode.objectify.cmd.Query;
+
+import teammates.storage.sqlentity.AccountRequest;
+
+public class DataMigrationForAccountRequestSql extends DataMigrationEntitiesBaseScriptSql<
+    teammates.storage.entity.AccountRequest,
+    AccountRequest
+> {
+
+    public static void main(String[] args) {
+        new DataMigrationForAccountRequestSql().doOperationRemotely();
+    }
+
+    @Override
+    protected Query<teammates.storage.entity.AccountRequest> getFilterQuery() {
+        // returns all AccountRequest entities
+        return ofy().load().type(teammates.storage.entity.AccountRequest.class);
+    }
+
+    /**
+     * Set to true to preview the migration without actually performing it.
+     */
+    @Override
+    protected boolean isPreview() {
+        return true;
+    }
+
+    /**
+     * Always returns true, as the migration is needed for all entities from Datastore to CloudSQL.
+     */
+    @Override
+    protected boolean isMigrationNeeded(teammates.storage.entity.AccountRequest accountRequest) {
+        return true;
+    }
+
+    @Override
+    protected void migrateEntity(teammates.storage.entity.AccountRequest oldEntity) throws Exception {
+        AccountRequest newEntity = new AccountRequest(
+            oldEntity.getEmail(),
+            oldEntity.getName(),
+            oldEntity.getInstitute()
+        );
+
+        // set registration key to the old value if exists
+        if (oldEntity.getRegistrationKey() != null) {
+            newEntity.setRegistrationKey(oldEntity.getRegistrationKey());
+        }
+
+        // set registeredAt to the old value if exists
+        if (oldEntity.getRegisteredAt() != null) {
+            newEntity.setRegisteredAt(oldEntity.getRegisteredAt());
+        }
+
+        // set createdAt to the old value if exists
+        if (oldEntity.getCreatedAt() != null) {
+            newEntity.setCreatedAt(oldEntity.getCreatedAt());
+        }
+
+        // for the updatedAt, we will let the db auto generate since this is the latest update time
+        // is during the migration
+
+        // for the id, we need to use the new UUID, since the old id is email + institute
+        // with % as delimiter
+
+        saveEntityDeferred(newEntity);
+    }
+}
