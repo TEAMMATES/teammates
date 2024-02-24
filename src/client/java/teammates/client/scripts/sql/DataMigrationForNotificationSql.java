e7f4c6cdd55aaad94a41e6f0529b5cb7a92a89cf
nicolascwy <25302138+NicolasCwy@users.noreply.github.com>
2024-02-24 15:33:01 +0800

    Add SQL notification migration script

---
+++ src/client/java/teammates/client/scripts/sql/DataMigrationForNotificationSql.java
@@ -0,0 +1,62 @@
+package teammates.client.scripts.sql;
+import java.util.UUID;
+
+import com.googlecode.objectify.cmd.Query;
+
+import teammates.storage.entity.Notification;
+
+public class DataMigrationForNotificationSql extends DataMigrationEntitiesBaseScriptSql<
+        teammates.storage.entity.Notification, teammates.storage.sqlentity.Notification> {
+
+  public static void main(String[] args) {
+    SeedDb seedDb = new SeedDb();
+    try {
+        seedDb.seedSetup();
+        DataMigrationForNotificationSql script = new DataMigrationForNotificationSql();
+        script.doOperation();
+    
+        seedDb.seedTearDown();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected Query<Notification> getFilterQuery() {
+    return ofy().load().type(teammates.storage.entity.Notification.class);
+  }
+
+  @Override
+  protected boolean isPreview() {
+    return true;
+  }
+
+  @Override
+  protected boolean isMigrationNeeded(Notification entity) {
+    return true;
+  }
+
+  @Override
+  protected void migrateEntity(Notification oldNotification) throws Exception {
+    teammates.storage.sqlentity.Notification newNotification = new teammates.storage.sqlentity.Notification(
+      oldNotification.getStartTime(),
+      oldNotification.getEndTime(),
+      oldNotification.getStyle(),
+      oldNotification.getTargetUser(),
+      oldNotification.getTitle(),
+      oldNotification.getMessage()
+      );
+
+    try {
+      UUID oldUuid = UUID.fromString(oldNotification.getNotificationId());
+      newNotification.setId(oldUuid);
+    } catch (Exception e) {
+      // Auto-generated UUID from entity is created
+    }
+
+    if (oldNotification.isShown()) {
+      newNotification.setShown();
+    }
+    saveEntityDeferred(newNotification);
+  }
+}
