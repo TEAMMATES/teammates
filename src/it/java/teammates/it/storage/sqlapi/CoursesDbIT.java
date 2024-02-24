a06280f6e51fe57fa6e1c3786ba54606664c4216
Dominic Berzin <berzindominic@gmail.com>
2024-02-24 14:40:27 +0800

    feat: add more coverage for methods in CoursesDb

--- src/it/java/teammates/it/storage/sqlapi/CoursesDbIT.java
+++ src/it/java/teammates/it/storage/sqlapi/CoursesDbIT.java
@@ -21,13 +21,32 @@ public class CoursesDbIT extends BaseTestCaseWithSqlDatabaseAccess {
     private final CoursesDb coursesDb = CoursesDb.inst();
 
     @Test
+    public void testGetCourse() throws Exception {
+        ______TS("failure: get course that does not exist");
+        Course actual = coursesDb.getCourse("non-existent-course-id");
+        assertNull(actual);
+
+        ______TS("failure: null assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getCourse(null));
+
+        ______TS("success: get course that already exists");
+        Course expected = getTypicalCourse();
+        coursesDb.createCourse(expected);
+
+        actual = coursesDb.getCourse(expected.getId());
+        verifyEquals(expected, actual);
+    }
+
+    @Test
     public void testCreateCourse() throws Exception {
         ______TS("success: create course that does not exist");
         Course course = getTypicalCourse();
         coursesDb.createCourse(course);
-
         Course actualCourse = coursesDb.getCourse("course-id");
         verifyEquals(course, actualCourse);
+
+        ______TS("failure: null course assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.createCourse(null));
 
         ______TS("failure: create course that already exist, execption thrown");
         Course identicalCourse = getTypicalCourse();
@@ -40,11 +59,12 @@ public class CoursesDbIT extends BaseTestCaseWithSqlDatabaseAccess {
     public void testUpdateCourse() throws Exception {
         ______TS("failure: update course that does not exist, exception thrown");
         Course course = getTypicalCourse();
-
         assertThrows(EntityDoesNotExistException.class, () -> coursesDb.updateCourse(course));
 
-        ______TS("success: update course that already exists");
+        ______TS("failure: null course assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.updateCourse(null));
 
+        ______TS("success: update course that already exists");
         coursesDb.createCourse(course);
         course.setName("new course name");
 
@@ -63,14 +83,74 @@ public class CoursesDbIT extends BaseTestCaseWithSqlDatabaseAccess {
     }
 
     @Test
+    public void testDeleteCourse() throws Exception {
+        ______TS("success: delete course that already exists");
+        Course course = getTypicalCourse();
+        coursesDb.createCourse(course);
+
+        coursesDb.deleteCourse(course);
+        Course actualCourse = coursesDb.getCourse(course.getId());
+        assertNull(actualCourse);
+    }
+
+    @Test
+    public void testCreateSection() throws Exception {
+        Course course = getTypicalCourse();
+        Section section = getTypicalSection();
+        coursesDb.createCourse(course);
+
+        ______TS("success: create section that does not exist");
+        coursesDb.createSection(section);
+        Section actualSection = coursesDb.getSectionByName(course.getId(), section.getName());
+        verifyEquals(section, actualSection);
+
+        ______TS("failure: null section assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.createSection(null));
+
+        ______TS("failure: invalid section details");
+        Section invalidSection = new Section(course, null);
+        assertThrows(InvalidParametersException.class, () -> coursesDb.createSection(invalidSection));
+
+        ______TS("failure: create section that already exist, execption thrown");
+        assertThrows(EntityAlreadyExistsException.class, () -> coursesDb.createSection(section));
+    }
+
+    @Test
+    public void testGetSectionByName() throws Exception {
+        Course course = getTypicalCourse();
+        Section section = getTypicalSection();
+        coursesDb.createCourse(course);
+        coursesDb.createSection(section);
+
+        ______TS("failure: null courseId assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getSectionByName(null, section.getName()));
+
+        ______TS("failure: null sectionName assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getSectionByName(course.getId(), null));
+
+        ______TS("success: get section that already exists");
+        Section actualSection = coursesDb.getSectionByName(course.getId(), section.getName());
+        verifyEquals(section, actualSection);
+
+        ______TS("failure: get section that does not exist");
+        Section nonExistentSection = coursesDb.getSectionByName(course.getId(), "non-existent-section-name");
+        assertNull(nonExistentSection);
+    }
+
+    @Test
     public void testGetSectionByCourseIdAndTeam() throws InvalidParametersException, EntityAlreadyExistsException {
         Course course = getTypicalCourse();
         Section section = new Section(course, "section-name");
         course.addSection(section);
         Team team = new Team(section, "team-name");
         section.addTeam(team);
-
         coursesDb.createCourse(course);
+
+        ______TS("failure: null courseId assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getSectionByCourseIdAndTeam(null, team.getName()));
+
+        ______TS("failure: null teamName assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getSectionByCourseIdAndTeam(course.getId(), null));
 
         ______TS("success: typical case");
         Section actualSection = coursesDb.getSectionByCourseIdAndTeam(course.getId(), team.getName());
@@ -91,10 +171,28 @@ public class CoursesDbIT extends BaseTestCaseWithSqlDatabaseAccess {
 
         coursesDb.createCourse(course);
 
+        ______TS("failure: null section assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getTeamsForSection(null));
+
         ______TS("success: typical case");
         List<Team> actualTeams = coursesDb.getTeamsForSection(section);
         assertEquals(expectedTeams.size(), actualTeams.size());
         assertTrue(expectedTeams.containsAll(actualTeams));
+    }
+
+    @Test
+    public void testDeleteSectionsByCourseId() throws Exception {
+        Course course = getTypicalCourse();
+        Section section = getTypicalSection();
+        coursesDb.createCourse(course);
+        coursesDb.createSection(section);
+
+        assertNotNull(coursesDb.getSectionByName(course.getId(), section.getName()));
+
+        ______TS("success: delete sections by course id");
+        coursesDb.deleteSectionsByCourseId(course.getId());
+        Section actualSection = coursesDb.getSectionByName(course.getId(), section.getName());
+        assertNull(actualSection);
     }
 
     @Test
@@ -119,9 +217,57 @@ public class CoursesDbIT extends BaseTestCaseWithSqlDatabaseAccess {
 
         coursesDb.createCourse(course);
 
+        ______TS("failure: null courseId assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getTeamsForCourse(null));
+
         ______TS("success: typical case");
         List<Team> actualTeams = coursesDb.getTeamsForCourse(course.getId());
         assertEquals(expectedTeams.size(), actualTeams.size());
         assertTrue(expectedTeams.containsAll(actualTeams));
+    }
+
+    @Test
+    public void testCreateTeam() throws Exception {
+        Course course = getTypicalCourse();
+        Section section = getTypicalSection();
+        Team team = new Team(section, "team-name1");
+        coursesDb.createCourse(course);
+        coursesDb.createSection(section);
+
+        assertNotNull(coursesDb.getSectionByName(course.getId(), section.getName()));
+
+        ______TS("failure: null team assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.createTeam(null));
+
+        ______TS("success: create team that does not exist");
+        coursesDb.createTeam(team);
+        Team actualTeam = coursesDb.getTeamByName(section.getId(), team.getName());
+        verifyEquals(team, actualTeam);
+
+        ______TS("failure: create team that already exist, execption thrown");
+        assertThrows(EntityAlreadyExistsException.class, () -> coursesDb.createTeam(team));
+    }
+
+    @Test
+    public void testGetTeamByName() throws Exception {
+        Course course = getTypicalCourse();
+        Section section = getTypicalSection();
+        Team team = new Team(section, "team-name1");
+        coursesDb.createCourse(course);
+        coursesDb.createSection(section);
+        coursesDb.createTeam(team);
+
+        Team actualTeam = coursesDb.getTeamByName(section.getId(), team.getName());
+        verifyEquals(team, actualTeam);
+
+        ______TS("failure: null sectionId assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getTeamByName(null, team.getName()));
+
+        ______TS("failure: null teamName assertion exception thrown");
+        assertThrows(AssertionError.class, () -> coursesDb.getTeamByName(section.getId(), null));
+
+        ______TS("success: null return");
+        Team nonExistentTeam = coursesDb.getTeamByName(section.getId(), "non-existent-team-name");
+        assertNull(nonExistentTeam);
     }
 }
