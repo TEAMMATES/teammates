d11d40c074cbe5ef666ea8f68e1312def454bbff
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2024-02-25 16:07:11 +0800

    migrate tests

--- src/e2e/java/teammates/e2e/cases/InstructorStudentRecordsPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/InstructorStudentRecordsPageE2ETest.java
@@ -2,6 +2,7 @@ package teammates.e2e.cases;
 
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.SqlDataBundle;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.AppUrl;
@@ -17,6 +18,9 @@ public class InstructorStudentRecordsPageE2ETest extends BaseE2ETestCase {
     protected void prepareTestData() {
         testData = loadDataBundle("/InstructorStudentRecordsPageE2ETest.json");
         removeAndRestoreDataBundle(testData);
+        
+        SqlDataBundle sqlTestData = loadSqlDataBundle("/InstructorStudentRecordsPageE2ETest_SqlEntities.json");
+        removeAndRestoreSqlDataBundle(sqlTestData);
     }
 
     @Test
