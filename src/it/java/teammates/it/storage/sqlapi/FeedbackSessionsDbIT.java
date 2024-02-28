b103dcd2e52f144e6e831a933c941b2041c7afa6
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-02-28 19:55:50 +0800

    Add SQL injection test for getFeedbackSessionEntitiesForCourse

--- src/it/java/teammates/it/storage/sqlapi/FeedbackSessionsDbIT.java
+++ src/it/java/teammates/it/storage/sqlapi/FeedbackSessionsDbIT.java
@@ -278,6 +278,26 @@ public class FeedbackSessionsDbIT extends BaseTestCaseWithSqlDatabaseAccess {
     }
 
     @Test
+    public void testGetFeedbackSessionEntitiesForCourse_sqlInjectionAttempt_shouldNotRunSqlInjectionQuery()
+            throws EntityAlreadyExistsException, InvalidParametersException, EntityDoesNotExistException {
+        Course course = createTypicalCourse();
+        coursesDb.createCourse(course);
+        FeedbackSession fs = createTypicalFeedbackSession(course);
+        fsDb.createFeedbackSession(fs);
+        List<FeedbackSession> sessions = fsDb.getFeedbackSessionEntitiesForCourse("course-id");
+        assertEquals(1, sessions.size());
+        // select f1_0.id,f1_0.course_id,f1_0.created_at,f1_0.creator_email,f1_0.deleted_at,f1_0.end_time,
+        // f1_0.grace_period,f1_0.instructions,f1_0.is_closed_email_sent,f1_0.is_closing_email_enabled,
+        // f1_0.is_closing_soon_email_sent,f1_0.is_open_email_sent,f1_0.is_opening_email_enabled,
+        // f1_0.is_opening_soon_email_sent,f1_0.is_published_email_enabled,f1_0.is_published_email_sent,f1_0.name,
+        // f1_0.results_visible_from_time,f1_0.session_visible_from_time,f1_0.start_time,f1_0.updated_at from
+        // feedback_sessions f1_0 join courses c1_0 on c1_0.id=f1_0.course_id where f1_0.course_id=?
+        String sqlInjectionCourseId = "course-id' OR 1 = 1 OR '' = '";
+        List<FeedbackSession> nonExistentSessions = fsDb.getFeedbackSessionEntitiesForCourse(sqlInjectionCourseId);
+        assertEquals(0, nonExistentSessions.size());
+    }
+
+    @Test
     public void testRestoreDeletedFeedbackSession_sqlInjectionAttemptIntoName_shouldNotRunSqlInjectionQuery()
             throws EntityAlreadyExistsException, InvalidParametersException, EntityDoesNotExistException {
         Course course = createTypicalCourse();
