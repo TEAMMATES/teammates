4d6689bf9f08d21076d5ea186812c0e810fc522f
Dominic Lim <dominiclimkj3448@gmail.com>
2024-03-09 23:53:20 +0800

    Fix lint

--- src/main/java/teammates/storage/sqlentity/Notification.java
+++ src/main/java/teammates/storage/sqlentity/Notification.java
@@ -14,6 +14,7 @@ import teammates.common.datatransfer.NotificationStyle;
 import teammates.common.datatransfer.NotificationTargetUser;
 import teammates.common.util.FieldValidator;
 import teammates.common.util.SanitizationHelper;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
