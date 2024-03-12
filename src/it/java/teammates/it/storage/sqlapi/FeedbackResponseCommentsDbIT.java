d4b9c046f3c6f54a4645a9f3f7419b72de35a35b
Xenos F <git@xenosfio.com>
2024-03-13 00:56:07 +0800

    Clear session when set up

--- src/it/java/teammates/it/storage/sqlapi/FeedbackResponseCommentsDbIT.java
+++ src/it/java/teammates/it/storage/sqlapi/FeedbackResponseCommentsDbIT.java
@@ -45,6 +45,7 @@ public class FeedbackResponseCommentsDbIT extends BaseTestCaseWithSqlDatabaseAcc
         super.setUp();
         persistDataBundle(testDataBundle);
         HibernateUtil.flushSession();
+        HibernateUtil.clearSession();
     }
 
     @Test
