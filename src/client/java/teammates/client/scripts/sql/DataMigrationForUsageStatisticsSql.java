95e88874001cbd1efe9be23c953b0aa0f226cfcc
Zhang Ziqing <zhangziqing9926@gmail.com>
2024-03-12 12:10:22 +0800

    Patch Usage Statistics

--- src/client/java/teammates/client/scripts/sql/DataMigrationForUsageStatisticsSql.java
+++ src/client/java/teammates/client/scripts/sql/DataMigrationForUsageStatisticsSql.java
@@ -1,5 +1,7 @@
 package teammates.client.scripts.sql;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import com.googlecode.objectify.cmd.Query;
@@ -10,12 +12,24 @@ import teammates.storage.sqlentity.UsageStatistics;
  * Data migration class for usage statistics.
  */
 public class DataMigrationForUsageStatisticsSql extends
-        DataMigrationEntitiesBaseScriptSql<
-            teammates.storage.entity.UsageStatistics,
-            UsageStatistics> {
+        DataMigrationEntitiesBaseScriptSql<teammates.storage.entity.UsageStatistics, UsageStatistics> {
+
+    // Set the default start time to resume the migration from.
+    // Set it to null if want to migrate all entities.
+    private static final String START_TIME_STRING = "2024-03-12 06:00:00.000 +0800";
+
+    private static final Instant START_TIME = parseStartTime(START_TIME_STRING);
 
     public static void main(String[] args) {
         new DataMigrationForUsageStatisticsSql().doOperationRemotely();
+    }
+
+    private static Instant parseStartTime(String startTimeString) {
+        if (startTimeString == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS Z");
+        return Instant.from(formatter.parse(startTimeString));
     }
 
     @Override
@@ -33,11 +47,16 @@ public class DataMigrationForUsageStatisticsSql extends
     }
 
     /**
-     * Always returns true, as the migration is needed for all entities from Datastore to CloudSQL .
+     * Always returns true, as the migration is needed for all entities from
+     * Datastore to CloudSQL .
      */
+    @SuppressWarnings("unused")
     @Override
     protected boolean isMigrationNeeded(teammates.storage.entity.UsageStatistics entity) {
-        return true;
+        if (START_TIME == null) {
+            return true;
+        }
+        return entity.getStartTime().isAfter(START_TIME);
     }
 
     @Override
@@ -51,8 +70,7 @@ public class DataMigrationForUsageStatisticsSql extends
                 oldEntity.getNumInstructors(),
                 oldEntity.getNumAccountRequests(),
                 oldEntity.getNumEmails(),
-                oldEntity.getNumSubmissions()
-        );
+                oldEntity.getNumSubmissions());
 
         try {
             UUID oldUuid = UUID.fromString(oldEntity.getId());
