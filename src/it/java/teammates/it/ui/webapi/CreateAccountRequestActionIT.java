b30217c87a69303f2d3faf3c5f06713405acf572
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-17 00:40:49 +0800

    Test execute with null arguments

--- src/it/java/teammates/it/ui/webapi/CreateAccountRequestActionIT.java
+++ src/it/java/teammates/it/ui/webapi/CreateAccountRequestActionIT.java
@@ -1,7 +1,15 @@
 package teammates.it.ui.webapi;
 
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.AccountRequestStatus;
 import teammates.common.util.Const;
+import teammates.storage.sqlentity.AccountRequest;
+import teammates.ui.output.AccountRequestData;
+import teammates.ui.request.AccountCreateRequest;
+import teammates.ui.request.InvalidHttpRequestBodyException;
 import teammates.ui.webapi.CreateAccountRequestAction;
+import teammates.ui.webapi.JsonResult;
 
 /**
  * SUT: {@link CreateAccountRequestAction}.
@@ -20,8 +28,34 @@ public class CreateAccountRequestActionIT extends BaseActionIT<CreateAccountRequ
 
     @Override
     protected void testExecute() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testExecute'");
+        // This is separated into different test methods.
+    }
+
+    @Test
+    void testExecute_nullEmail_throwsInvalidHttpRequestBodyException() {
+        AccountCreateRequest request = new AccountCreateRequest();
+        request.setInstructorName("Paul Atreides");
+        request.setInstructorInstitution("House Atreides");
+        InvalidHttpRequestBodyException ihrbException = verifyHttpRequestBodyFailure(request);
+        assertEquals("email cannot be null", ihrbException.getMessage());
+    }
+
+    @Test
+    void testExecute_nullName_throwsInvalidHttpRequestBodyException() {
+        AccountCreateRequest request = new AccountCreateRequest();
+        request.setInstructorEmail("kwisatz.haderach@atreides.org");
+        request.setInstructorInstitution("House Atreides");
+        InvalidHttpRequestBodyException ihrbException = verifyHttpRequestBodyFailure(request);
+        assertEquals("name cannot be null", ihrbException.getMessage());
+    }
+
+    @Test
+    void testExecute_nullInstitute_throwsInvalidHttpRequestBodyException() {
+        AccountCreateRequest request = new AccountCreateRequest();
+        request.setInstructorEmail("kwisatz.haderach@atreides.org");
+        request.setInstructorName("Paul Atreides");
+        InvalidHttpRequestBodyException ihrbException = verifyHttpRequestBodyFailure(request);
+        assertEquals("institute cannot be null", ihrbException.getMessage());
     }
 
     @Override
