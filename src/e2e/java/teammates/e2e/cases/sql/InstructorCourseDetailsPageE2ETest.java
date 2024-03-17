9abc6239dcd0fd19aa9034b12b8c1ffc8d016bb0
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-17 15:35:13 +0800

    Add teammates.e2e.cases.sql.InstructorCourseDetailsPageE2ETest

---
+++ src/e2e/java/teammates/e2e/cases/sql/InstructorCourseDetailsPageE2ETest.java
@@ -0,0 +1,21 @@
+package teammates.e2e.cases.sql;
+
+import teammates.common.util.Const;
+
+/**
+ * SUT: {@link Const.WebPageURIs#INSTRUCTOR_COURSE_DETAILS_PAGE}.
+ */
+public class InstructorCourseDetailsPageE2ETest extends BaseE2ETestCase {
+
+    @Override
+    protected void prepareTestData() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'prepareTestData'");
+    }
+
+    @Override
+    protected void testAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'testAll'");
+    }
+}
