4befeabe7299735b485c1f1a6f437253ccbd590d
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-18 01:48:12 +0800

    Check student links

--- src/e2e/java/teammates/e2e/cases/sql/InstructorCourseDetailsPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/sql/InstructorCourseDetailsPageE2ETest.java
@@ -10,6 +10,9 @@ import org.testng.annotations.Test;
 import teammates.common.util.AppUrl;
 import teammates.common.util.Const;
 import teammates.e2e.pageobjects.InstructorCourseDetailsPage;
+import teammates.e2e.pageobjects.InstructorCourseStudentDetailsEditPage;
+import teammates.e2e.pageobjects.InstructorCourseStudentDetailsViewPage;
+import teammates.e2e.pageobjects.InstructorStudentRecordsPage;
 import teammates.storage.sqlentity.Course;
 import teammates.storage.sqlentity.Instructor;
 import teammates.storage.sqlentity.Student;
@@ -53,5 +56,24 @@ public class InstructorCourseDetailsPageE2ETest extends BaseE2ETestCase {
         detailsPage.verifyCourseDetails(course, instructors, sectionNames.size(), teamNames.size(), students.size());
         detailsPage.verifyNumStudents(students.size());
         detailsPage.verifyStudentDetails(students);
+
+        ______TS("link: view student details page");
+        Student studentToView = testData.students.get("benny.tmms@ICDet.CS2104");
+        InstructorCourseStudentDetailsViewPage studentDetailsViewPage =
+                detailsPage.clickViewStudent(studentToView.getEmail());
+        studentDetailsViewPage.verifyIsCorrectPage(course.getId(), studentToView.getEmail());
+        studentDetailsViewPage.closeCurrentWindowAndSwitchToParentWindow();
+
+        ______TS("link: edit student details page");
+        InstructorCourseStudentDetailsEditPage studentDetailsEditPage =
+                detailsPage.clickEditStudent(studentToView.getEmail());
+        studentDetailsEditPage.verifyIsCorrectPage(course.getId(), studentToView.getEmail());
+        studentDetailsEditPage.closeCurrentWindowAndSwitchToParentWindow();
+
+        ______TS("link: view all records page");
+        InstructorStudentRecordsPage studentRecordsPage =
+                detailsPage.clickViewAllRecords(studentToView.getEmail());
+        studentRecordsPage.verifyIsCorrectPage(course.getId(), studentToView.getName());
+        studentRecordsPage.closeCurrentWindowAndSwitchToParentWindow();
     }
 }
