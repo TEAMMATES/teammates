db91cc753de1090f3f2cdedf53379ade5ee50861
Dominic Berzin <berzindominic@gmail.com>
2024-03-20 12:40:22 +0800

    create component for account request table

---
+++ src/web/app/components/account-requests-table/admin-reject-with-reason-modal/admin-reject-with-reason-modal-model.ts
@@ -0,0 +1,9 @@
+/**
+ * Result of {@link RejectWithReasonModalComponent}
+ */
+export interface RejectWithReasonModalComponentResult {
+    accountRequestName: string;
+    rejectionReasonTitle: string;
+    rejectionReasonBody: string;
+    isRejected: boolean;
+}
