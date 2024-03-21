b19e22c34824b6c5ec5db493f555525e4b92ad51
nicolascwy <25302138+NicolasCwy@users.noreply.github.com>
2024-03-21 20:05:37 +0800

    Cleanup

--- src/client/java/teammates/client/scripts/testdataconversion/toposort.py
+++ src/client/java/teammates/client/scripts/testdataconversion/toposort.py
@@ -1,43 +1,43 @@
 #Python program to print topological sorting of a DAG
 from collections import defaultdict
- 
+
 #Class to represent a graph
 class Graph:
     def __init__(self,vertices):
         self.graph = defaultdict(list) #dictionary containing adjacency List
         self.V = vertices #No. of vertices
- 
+
     # function to add an edge to graph
     def addEdge(self,u,v):
         self.graph[u].append(v)
- 
+
     # A recursive function used by topologicalSort
     def topologicalSortUtil(self,v,visited,stack):
- 
+
         # Mark the current node as visited.
         visited[v] = True
- 
+
         # Recur for all the vertices adjacent to this vertex
         for i in self.graph[v]:
             if visited[i] == False:
                 self.topologicalSortUtil(i,visited,stack)
- 
+
         # Push current vertex to stack which stores result
         stack.insert(0,v)
- 
+
     # The function to do Topological Sort. It uses recursive
     # topologicalSortUtil()
     def topologicalSort(self):
         # Mark all the vertices as not visited
         visited = [False]*self.V
         stack =[]
- 
+
         # Call the recursive helper function to store Topological
         # Sort starting from all vertices one by one
         for i in range(self.V):
             if visited[i] == False:
                 self.topologicalSortUtil(i,visited,stack)
- 
+
         # Print contents of stack
         return stack.copy()
 g = Graph(18)
@@ -81,6 +81,7 @@ lookup = {
     17: "usage_stats"
 }
 
+# (x, y) where x is dependent on y, y has to be created to be used
 dependencies = [
     (deadline_extension, user),
     (deadline_extension, feedback_session),
@@ -103,12 +104,6 @@ dependencies = [
 
 for start, end in dependencies:
     g.addEdge(start, end)
-# g.addEdge("Course", 2)
-# g.addEdge(5, 0)
-# g.addEdge(4, 0)
-# g.addEdge(4, 1)
-# g.addEdge(2, 3)
-# g.addEdge(1, 2)
- 
+
 print ("Following is a Topological Sort of the given graph")
 print(list(map(lambda x: lookup[x], g.topologicalSort())))
