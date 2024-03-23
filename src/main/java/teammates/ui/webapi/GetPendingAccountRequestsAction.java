ff961a2b355682b9ce4a3502c709a4e33ca11907
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2024-03-23 08:50:21 +0800

    fix checkstyle

--- src/main/java/teammates/ui/webapi/GetPendingAccountRequestsAction.java
+++ src/main/java/teammates/ui/webapi/GetPendingAccountRequestsAction.java
@@ -6,11 +6,17 @@ import teammates.storage.sqlentity.AccountRequest;
 import teammates.ui.output.AccountRequestData;
 import teammates.ui.output.AccountRequestsData;
 
+/**
+ * Action: Gets pending account requests.
+ */
 public class GetPendingAccountRequestsAction extends AdminOnlyAction {
     @Override
     public JsonResult execute() {
         List<AccountRequest> accountRequests = sqlLogic.getPendingAccountRequests();
-        List<AccountRequestData> accountRequestDatas = accountRequests.stream().map(ar -> new AccountRequestData(ar)).toList();
+        List<AccountRequestData> accountRequestDatas = accountRequests
+                .stream()
+                .map(ar -> new AccountRequestData(ar))
+                .toList();
 
         AccountRequestsData output = new AccountRequestsData();
         output.setAccountRequests(accountRequestDatas);
