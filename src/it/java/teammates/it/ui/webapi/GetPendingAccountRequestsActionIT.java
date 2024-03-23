887fb2358fe10e7f20611f35ec8cb45efe89b438
EuniceSim142 <77243938+EuniceSim142@users.noreply.github.com>
2024-03-23 09:19:27 +0800

    add it test

---
+++ src/it/java/teammates/it/ui/webapi/GetPendingAccountRequestsActionIT.java
@@ -0,0 +1,90 @@
+package teammates.it.ui.webapi;
+
+import java.util.List;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.AccountRequestStatus;
+import teammates.common.exception.EntityAlreadyExistsException;
+import teammates.common.exception.InvalidParametersException;
+import teammates.common.util.Const;
+import teammates.common.util.HibernateUtil;
+import teammates.storage.sqlentity.AccountRequest;
+import teammates.storage.sqlentity.Course;
+import teammates.ui.output.AccountRequestData;
+import teammates.ui.output.AccountRequestsData;
+import teammates.ui.webapi.GetPendingAccountRequestsAction;
+import teammates.ui.webapi.JsonResult;
+
+/**
+ * SUT: {@link GetPendingAccountRequestsAction}.
+ */
+public class GetPendingAccountRequestsActionIT extends BaseActionIT<GetPendingAccountRequestsAction> {
+
+    @Override
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        super.setUp();
+        persistDataBundle(typicalBundle);
+        HibernateUtil.flushSession();
+    }
+
+    @Override
+    protected String getActionUri() {
+        return Const.ResourceURIs.ACCOUNT_REQUESTS_PENDING;
+    }
+
+    @Override
+    protected String getRequestMethod() {
+        return GET;
+    }
+
+    @Override
+    @Test
+    public void testExecute() {
+        ______TS("No pending account requests initially");
+
+        GetPendingAccountRequestsAction action = getAction();
+        JsonResult result = getJsonResult(action);
+        AccountRequestsData data = (AccountRequestsData) result.getOutput();
+        List<AccountRequestData> arData = data.getAccountRequests();
+        
+        assertEquals(0, arData.size());
+
+        ______TS("Get 2 pending account requests, ignore 1 approved account request");
+        AccountRequest accountRequest1_approved = typicalBundle.accountRequests.get("instructor2");
+        accountRequest1_approved.setStatus(AccountRequestStatus.APPROVED);
+
+        AccountRequest accountRequest1 = typicalBundle.accountRequests.get("instructor1");
+        AccountRequest accountRequest2 = typicalBundle.accountRequests.get("instructor1OfCourse2");
+        accountRequest1.setStatus(AccountRequestStatus.PENDING);
+        accountRequest2.setStatus(AccountRequestStatus.PENDING);
+
+        action = getAction();
+        result = getJsonResult(action);
+        data = (AccountRequestsData) result.getOutput();
+        arData = data.getAccountRequests();
+
+        assertEquals(2, arData.size());
+
+        // account request 1 
+        assertEquals(arData.get(0).getEmail(), accountRequest1.getEmail());
+        assertEquals(arData.get(0).getInstitute(), accountRequest1.getInstitute());
+        assertEquals(arData.get(0).getName(), accountRequest1.getName());
+        assertEquals(arData.get(0).getRegistrationKey(), accountRequest1.getRegistrationKey());
+
+        // account request 2
+        assertEquals(arData.get(1).getEmail(), accountRequest2.getEmail());
+        assertEquals(arData.get(1).getInstitute(), accountRequest2.getInstitute());
+        assertEquals(arData.get(1).getName(), accountRequest2.getName());
+        assertEquals(arData.get(1).getRegistrationKey(), accountRequest2.getRegistrationKey());
+    }
+
+    @Override
+    @Test
+    public void testAccessControl() throws InvalidParametersException, EntityAlreadyExistsException {
+        Course course = typicalBundle.courses.get("course1");
+        verifyOnlyAdminCanAccess(course);
+    }
+}
