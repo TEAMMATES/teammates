3499d2a349de3b439ef01156ad5bf3a1f17511c7
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2024-03-24 22:45:45 -0700

    [#12588] Improve test code coverage of core components - ToastComponent (#12916)
    * add test cases
    
    * add test case for isTemplate()
    
    ---------
    
    Co-authored-by: Cedric Ong <67156011+cedricongjh@users.noreply.github.com>
    Co-authored-by: Dominic Lim <46486515+domlimm@users.noreply.github.com>

--- src/web/app/components/toast/toast.component.spec.ts
+++ src/web/app/components/toast/toast.component.spec.ts
@@ -1,7 +1,15 @@
+import { ElementRef, EmbeddedViewRef, Injector, TemplateRef } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastComponent } from './toast.component';
+
+class MockTemplateRef extends TemplateRef<any> {
+  override elementRef!: ElementRef<any>;
+  override createEmbeddedView(context: any, injector?: Injector | undefined): EmbeddedViewRef<any> {
+    throw new Error(`Method not implemented with context ${context} & injector ${injector}.`);
+  }
+}
 
 describe('ToastComponent', () => {
   let component: ToastComponent;
@@ -23,5 +31,29 @@ describe('ToastComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set autohide', () => {
+    component.toast = { message: 'Test message', autohide: false, classes: '' };
+    component.setAutohide(true);
+    expect(component.toast.autohide).toBe(true);
+  });
+
+  it('should remove toast', () => {
+    component.toast = { message: 'Test message', autohide: false, classes: '' };
+    jest.spyOn(component.toastChange, 'emit');
+    component.removeToast();
+    expect(component.toast).toBe(null);
+    expect(component.toastChange.emit).toHaveBeenCalledWith(null);
+  });
+
+  it('should return false if message is not a TemplateRef', () => {
+    component.toast = { message: 'Test message', autohide: false, classes: '' };
+    expect(component.isTemplate()).toBe(false);
+  });
+
+  it('should return true if message is a TemplateRef', () => {
+    component.toast = { message: new MockTemplateRef(), autohide: false, classes: '' };
+    expect(component.isTemplate()).toBe(true);
   });
 });
