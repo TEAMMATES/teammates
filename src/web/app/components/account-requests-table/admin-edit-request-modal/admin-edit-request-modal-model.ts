a7d977961f4d3d39b839c4e3fdbdb80b6c0bbb03
Dominic Berzin <berzindominic@gmail.com>
2024-03-25 13:08:42 +0800

    delete edit and reject modal components

--- src/web/app/components/account-requests-table/admin-edit-request-modal/admin-edit-request-modal-model.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Result of {@link EditRequestModalComponent}
- */
-export interface EditRequestModalComponentResult {
-    accountRequestName: string;
-    accountRequestEmail: string;
-    accountRequestInstitution: string;
-    accountRequestComment: string;
-}
