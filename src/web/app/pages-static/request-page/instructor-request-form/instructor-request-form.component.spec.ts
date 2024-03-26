d043af29ca1fe8f5a438ba68fcebcc191039b19e
Xenos F <git@xenosfio.com>
2024-03-27 03:17:07 +0800

    Add test cases for requestSubmissionEvent

--- src/web/app/pages-static/request-page/instructor-request-form/instructor-request-form.component.spec.ts
+++ src/web/app/pages-static/request-page/instructor-request-form/instructor-request-form.component.spec.ts
@@ -3,14 +3,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InstructorRequestFormComponent } from './instructor-request-form.component';
 import { InstructorRequestFormModel } from './instructor-request-form-model';
 import { By } from '@angular/platform-browser';
+import { first } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('InstructorRequestFormComponent', () => {
   let component: InstructorRequestFormComponent;
   let fixture: ComponentFixture<InstructorRequestFormComponent>;
+  let typicalModel: InstructorRequestFormModel = {
+    name: "John Doe",
+    institution: "Example Institution",
+    country: "Example Country",
+    email: "jd@example.edu",
+    homePage: "xyz.example.edu/john",
+    comments: "",
+  }; 
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [InstructorRequestFormComponent],
+      imports: [ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(InstructorRequestFormComponent);
     component = fixture.componentInstance;
@@ -22,32 +33,42 @@ describe('InstructorRequestFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should raise requestSubmissionEvent when submit', () => {
-    spyOn(component.requestSubmissionEvent, 'emit');
+  it('should raise requestSubmissionEvent when submit button is clicked', () => {
+    jest.spyOn(component.requestSubmissionEvent, 'emit');
 
     // Simulate form being submitted
-    let expectedModel: InstructorRequestFormModel = {
-      name: "John Doe",
-      institution: "Example Institution",
-      country: "Example Country",
-      email: "jd@example.edu",
-      homePage: "",
-      comments: "",
-    };
-    component.name.setValue(expectedModel.name);
-    component.institution.setValue(expectedModel.institution);
-    component.country.setValue(expectedModel.country);
-    component.email.setValue(expectedModel.email);
-    component.homePage.setValue(expectedModel.email);
-    component.comments.setValue(expectedModel.comments);
-    let btn = fixture.debugElement.query(By.css('btn[type="submit"]')).nativeElement;
-    btn.click();
-
+    fillFormWith(typicalModel);
+    let submitButton = fixture.debugElement.query(By.css('.btn.btn-primary'));
+    submitButton.triggerEventHandler('click');
+    submitButton.nativeElement.click();
     expect(component.requestSubmissionEvent.emit).toHaveBeenCalledTimes(1);
-
-    // TODO: test value
-    // let submissionModel: InstructorRequestFormModel | null;
-    // component.requestSubmissionEvent.pipe(first()).subscribe((data: InstructorRequestFormModel) => (submissionModel = data));
-    // expect(component.requestSubmissionEvent.emit).toHaveBeenCalledWith(expectedModel);
   });
+
+  it('should raise requestSubmissionEvent with the correct data when form is submitted', () => {
+    let actualModel: InstructorRequestFormModel | null = null;
+    // Listen for event
+    component.requestSubmissionEvent.pipe(first())
+        .subscribe((data: InstructorRequestFormModel) => (actualModel = data));
+
+    // Simulate form being submitted
+    fillFormWith(typicalModel);
+    component.onSubmit();
+
+    expect(actualModel).toBeTruthy();
+    expect(actualModel!.name).toBe(typicalModel.name);
+    expect(actualModel!.institution).toBe(typicalModel.institution);
+    expect(actualModel!.country).toBe(typicalModel.country);
+    expect(actualModel!.email).toBe(typicalModel.email);
+    expect(actualModel!.homePage).toBe(typicalModel.homePage);
+    expect(actualModel!.comments).toBe(typicalModel.comments);
+  });
+
+  function fillFormWith(data: InstructorRequestFormModel) {
+    component.name.setValue(data.name);
+    component.institution.setValue(data.institution);
+    component.country.setValue(data.country);
+    component.email.setValue(data.email);
+    component.homePage.setValue(data.homePage);
+    component.comments.setValue(data.comments);
+  }
 });
