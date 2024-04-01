1ac519f3b2977df03afb8d9357a4994d5cae5e09
mingyuanc <cmingyuan123@gmail.com>
2024-04-02 04:37:52 +0800

    Fix typo

--- src/e2e/java/teammates/e2e/cases/sql/FeedbackResultsPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/sql/FeedbackResultsPageE2ETest.java
@@ -95,10 +95,10 @@ public class FeedbackResultsPageE2ETest extends BaseE2ETestCase {
         ______TS("verify comments");
         verifyCommentDetails(2, testData.feedbackResponseComments.get("qn2Comment1"), student);
         verifyCommentDetails(2, testData.feedbackResponseComments.get("qn2Comment2"), student);
-        // Commented as only one student comment should be visible to students
-        // verifyCommentDetails(3, testData.feedbackResponseComments.get("qn3Comment1"),
-        // student);
         verifyCommentDetails(3, testData.feedbackResponseComments.get("qn3Comment1"), student);
+        // Commented as only one student comment should be visible to students
+        // verifyCommentDetails(3, testData.feedbackResponseComments.get("qn3Comment2"),
+        // student);
         verifyCommentDetails(4, testData.feedbackResponseComments.get("qn4Comment1"), student);
 
         ______TS("registered instructor: can access results");
