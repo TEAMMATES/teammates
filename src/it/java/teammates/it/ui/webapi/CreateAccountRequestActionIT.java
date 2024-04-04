649232441232cebcb5b88a45c80cbd3d2a6d44fa
domoberzin <berzindominic@gmail.com>
2024-04-05 00:39:40 +0800

    cleanup after create account requests test

--- src/it/java/teammates/it/ui/webapi/CreateAccountRequestActionIT.java
+++ src/it/java/teammates/it/ui/webapi/CreateAccountRequestActionIT.java
@@ -1,5 +1,8 @@
 package teammates.it.ui.webapi;
 
+import java.util.List;
+
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -221,4 +224,16 @@ public class CreateAccountRequestActionIT extends BaseActionIT<CreateAccountRequ
         verifyAccessibleWithoutLogin();
     }
 
+    @Override
+    @AfterMethod
+    protected void tearDown() {
+        HibernateUtil.beginTransaction();
+        List<AccountRequest> accountRequests = logic.getPendingAccountRequests();
+        for (AccountRequest ar : accountRequests) {
+            logic.deleteAccountRequest(ar.getEmail(), ar.getInstitute());
+        }
+        accountRequests = logic.getPendingAccountRequests();
+        HibernateUtil.commitTransaction();
+        assert accountRequests.isEmpty();
+    }
 }
