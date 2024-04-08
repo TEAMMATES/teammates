391dcb6829cf27f3387caedce8d1bedd43de67bc
Xenos F <git@xenosfio.com>
2024-04-08 11:39:24 +0800

    Add test for validation

---
+++ src/test/java/teammates/ui/request/AccountRequestRejectionRequestTest.java
@@ -0,0 +1,52 @@
+package teammates.ui.request;
+
+import org.testng.annotations.Test;
+
+import teammates.test.BaseTestCase;
+
+/**
+ * SUT: {@link AccountRequestRejectionRequest}.
+ */
+public class AccountRequestRejectionRequestTest extends BaseTestCase {
+
+    private static final String typicalTitle = "We are Unable to Create an Account for you";
+    private static final String typicalBody = new StringBuilder()
+            .append("<p>Hi, Example</p>\n")
+            .append("<p>Thanks for your interest in using TEAMMATES. ")
+            .append("We are unable to create a TEAMMATES instructor account for you.</p>\n\n")
+            .append("<p>\n")
+            .append("  <strong>Reason:</strong> The email address you provided ")
+            .append("is not an 'official' email address provided by your institution.<br />\n")
+            .append("  <strong>Remedy:</strong> ")
+            .append("Please re-submit an account request with your 'official' institution email address.\n")
+            .append("</p>\n\n")
+            .append("<p>If you need further clarification or would like to appeal this decision, ")
+            .append("please feel free to contact us at teammates@comp.nus.edu.sg.</p>\n")
+            .append("<p>Regards,<br />TEAMMATES Team.</p>\n")
+            .toString();
+
+    @Test
+    public void testValidate_withNonNullBodyAndNonNullTitle_shouldPass() throws Exception {
+        AccountRequestRejectionRequest request = new AccountRequestRejectionRequest(typicalTitle, typicalBody);
+        request.validate();
+    }
+
+    @Test
+    public void testValidate_withNullBodyAndNullTitle_shouldPass() throws Exception {
+        AccountRequestRejectionRequest request = new AccountRequestRejectionRequest(null, null);
+        request.validate();
+    }
+
+    @Test
+    public void testValidate_withNonNullBodyAndNullTitle_shouldFail() {
+        AccountRequestRejectionRequest request = new AccountRequestRejectionRequest(null, typicalBody);
+        assertThrows(InvalidHttpRequestBodyException.class, request::validate);
+    }
+
+
+    @Test
+    public void testValidate_withNullBodyAndNonNullTitle_shouldFail() {
+        AccountRequestRejectionRequest request = new AccountRequestRejectionRequest(typicalTitle, null);
+        assertThrows(InvalidHttpRequestBodyException.class, request::validate);
+    }
+}
