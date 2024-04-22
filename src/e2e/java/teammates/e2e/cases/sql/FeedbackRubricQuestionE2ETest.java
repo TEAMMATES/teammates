980a77a1407d5758ee25764283a0c728a484a9fb
YX Z <yxz@YXs-MacBook-Air-2.local>
2024-04-22 21:33:43 +0800

    Migrate all tests

--- src/e2e/java/teammates/e2e/cases/sql/FeedbackRubricQuestionE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/sql/FeedbackRubricQuestionE2ETest.java
@@ -6,15 +6,14 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
-import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
-import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
-import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.datatransfer.questions.FeedbackRubricQuestionDetails;
 import teammates.common.datatransfer.questions.FeedbackRubricResponseDetails;
-import teammates.common.util.Const;
 import teammates.e2e.pageobjects.FeedbackSubmitPage;
 import teammates.e2e.pageobjects.InstructorFeedbackEditPage;
 import teammates.storage.sqlentity.FeedbackQuestion;
+import teammates.storage.sqlentity.FeedbackResponse;
+import teammates.storage.sqlentity.Student;
+import teammates.storage.sqlentity.questions.FeedbackRubricQuestion;
 
 /**
  * SUT: {@link Const.WebPageURIs#INSTRUCTOR_SESSION_EDIT_PAGE}, {@link Const.WebPageURIs#SESSION_SUBMISSION_PAGE}
@@ -51,91 +50,96 @@ public class FeedbackRubricQuestionE2ETest extends BaseFeedbackQuestionE2ETest {
                 (FeedbackRubricQuestionDetails) loadedQuestion.getQuestionDetailsCopy();
         feedbackEditPage.verifyRubricQuestionDetails(1, questionDetails);
 
-        // ______TS("add new question");
-        // // add new question exactly like loaded question
-        // loadedQuestion.setQuestionNumber(2);
-        // feedbackEditPage.addRubricQuestion(loadedQuestion);
+        ______TS("add new question");
+        // add new question exactly like loaded question
+        FeedbackRubricQuestion newQuestion = new FeedbackRubricQuestion(
+                loadedQuestion.getFeedbackSession(), 2, loadedQuestion.getDescription(),
+                loadedQuestion.getGiverType(), loadedQuestion.getRecipientType(),
+                loadedQuestion.getNumOfEntitiesToGiveFeedbackTo(),
+                loadedQuestion.getShowResponsesTo(), loadedQuestion.getShowGiverNameTo(),
+                loadedQuestion.getShowRecipientNameTo(), questionDetails);
+        feedbackEditPage.addRubricQuestion(newQuestion);
 
-        // feedbackEditPage.verifyRubricQuestionDetails(2, questionDetails);
-        // verifyPresentInDatabase(loadedQuestion);
+        feedbackEditPage.verifyRubricQuestionDetails(2, questionDetails);
+        verifyPresentInDatabase(newQuestion);
 
-        // ______TS("copy question");
-        // FeedbackQuestionAttributes copiedQuestion = testData.feedbackQuestions.get("qn1ForSecondSession");
-        // questionDetails = (FeedbackRubricQuestionDetails) copiedQuestion.getQuestionDetailsCopy();
-        // feedbackEditPage.copyQuestion(copiedQuestion.getCourseId(),
-        //         copiedQuestion.getQuestionDetailsCopy().getQuestionText());
-        // copiedQuestion.setCourseId(course.getId());
-        // copiedQuestion.setFeedbackSessionName(feedbackSession.getFeedbackSessionName());
-        // copiedQuestion.setQuestionNumber(3);
+        ______TS("copy question");
+        FeedbackQuestion copiedQuestion = testData.feedbackQuestions.get("qn1ForSecondSession");
+        questionDetails = (FeedbackRubricQuestionDetails) copiedQuestion.getQuestionDetailsCopy();
+        feedbackEditPage.copyQuestion(copiedQuestion.getCourseId(),
+                copiedQuestion.getQuestionDetailsCopy().getQuestionText());
+        copiedQuestion.getCourse().setId(course.getId());
+        copiedQuestion.getFeedbackSession().setName(feedbackSession.getName());
+        copiedQuestion.setQuestionNumber(3);
 
-        // feedbackEditPage.verifyRubricQuestionDetails(3, questionDetails);
-        // verifyPresentInDatabase(copiedQuestion);
+        feedbackEditPage.verifyRubricQuestionDetails(3, questionDetails);
+        verifyPresentInDatabase(copiedQuestion);
 
-        // ______TS("edit question");
-        // // add a new choice
-        // questionDetails = (FeedbackRubricQuestionDetails) loadedQuestion.getQuestionDetailsCopy();
-        // List<String> choices = questionDetails.getRubricChoices();
-        // choices.add("Edited choice.");
-        // List<List<String>> descriptions = questionDetails.getRubricDescriptions();
-        // descriptions.get(0).add("Edit description.");
-        // descriptions.get(1).add("Edit description 1.");
-        // // edit existing descriptions
-        // descriptions.get(0).set(1, "Edit description 2");
-        // descriptions.get(1).set(0, "");
-        // // edit existing subquestion
-        // List<String> subQns = questionDetails.getRubricSubQuestions();
-        // subQns.set(0, "Edited subquestion.");
-        // // add a new subquestion
-        // subQns.add("Added subquestion.");
-        // descriptions.add(Arrays.asList("", "test", ""));
-        // // remove assigned weights
-        // questionDetails.setHasAssignedWeights(false);
-        // questionDetails.setRubricWeightsForEachCell(new ArrayList<>());
-        // loadedQuestion.setQuestionDetails(questionDetails);
-        // feedbackEditPage.editRubricQuestion(2, questionDetails);
-        // feedbackEditPage.waitForPageToLoad();
+        ______TS("edit question");
+        // add a new choice
+        questionDetails = (FeedbackRubricQuestionDetails) newQuestion.getQuestionDetailsCopy();
+        List<String> choices = questionDetails.getRubricChoices();
+        choices.add("Edited choice.");
+        List<List<String>> descriptions = questionDetails.getRubricDescriptions();
+        descriptions.get(0).add("Edit description.");
+        descriptions.get(1).add("Edit description 1.");
+        // edit existing descriptions
+        descriptions.get(0).set(1, "Edit description 2");
+        descriptions.get(1).set(0, "");
+        // edit existing subquestion
+        List<String> subQns = questionDetails.getRubricSubQuestions();
+        subQns.set(0, "Edited subquestion.");
+        // add a new subquestion
+        subQns.add("Added subquestion.");
+        descriptions.add(Arrays.asList("", "test", ""));
+        // remove assigned weights
+        questionDetails.setHasAssignedWeights(false);
+        questionDetails.setRubricWeightsForEachCell(new ArrayList<>());
+        newQuestion.setQuestionDetails(questionDetails);
+        feedbackEditPage.editRubricQuestion(2, questionDetails);
+        feedbackEditPage.waitForPageToLoad();
 
-        // feedbackEditPage.verifyRubricQuestionDetails(2, questionDetails);
-        // verifyPresentInDatabase(loadedQuestion);
+        feedbackEditPage.verifyRubricQuestionDetails(2, questionDetails);
+        verifyPresentInDatabase(newQuestion);
     }
 
     @Override
     protected void testSubmitPage() {
-        // FeedbackSubmitPage feedbackSubmitPage = loginToFeedbackSubmitPage();
+        FeedbackSubmitPage feedbackSubmitPage = loginToFeedbackSubmitPage();
+        FeedbackQuestion question = testData.feedbackQuestions.get("qn1ForFirstSession");
+        question.setQuestionNumber(1);
+        ______TS("verify loaded question");
+        Student receiver = testData.students.get("benny.tmms@FRubricQn.CS2104");
+        feedbackSubmitPage.verifyRubricQuestion(1, receiver.getName(),
+                (FeedbackRubricQuestionDetails) question.getQuestionDetailsCopy());
 
-        // ______TS("verify loaded question");
-        // FeedbackQuestionAttributes question = testData.feedbackQuestions.get("qn1ForFirstSession");
-        // StudentAttributes receiver = testData.students.get("benny.tmms@FRubricQn.CS2104");
-        // feedbackSubmitPage.verifyRubricQuestion(1, receiver.getName(),
-        //         (FeedbackRubricQuestionDetails) question.getQuestionDetailsCopy());
+        ______TS("submit response");
+        // Student giver = testData.students.get("
+        FeedbackResponse response = getResponse(question, receiver, Arrays.asList(1, 1));
+        feedbackSubmitPage.fillRubricResponse(1, receiver.getName(), response);
+        feedbackSubmitPage.clickSubmitQuestionButton(1);
 
-        // ______TS("submit response");
-        // String questionId = getFeedbackQuestion(question).getId();
-        // FeedbackResponseAttributes response = getResponse(questionId, receiver, Arrays.asList(1, 1));
-        // feedbackSubmitPage.fillRubricResponse(1, receiver.getName(), response);
-        // feedbackSubmitPage.clickSubmitQuestionButton(1);
+        verifyPresentInDatabase(response);
 
-        // verifyPresentInDatabase(response);
+        ______TS("check previous response");
+        feedbackSubmitPage = getFeedbackSubmitPage();
+        feedbackSubmitPage.verifyRubricResponse(1, receiver.getName(), response);
 
-        // ______TS("check previous response");
-        // feedbackSubmitPage = getFeedbackSubmitPage();
-        // feedbackSubmitPage.verifyRubricResponse(1, receiver.getName(), response);
+        ______TS("edit response");
+        response = getResponse(question, receiver, Arrays.asList(0, 0));
+        feedbackSubmitPage.fillRubricResponse(1, receiver.getName(), response);
+        feedbackSubmitPage.clickSubmitQuestionButton(1);
 
-        // ______TS("edit response");
-        // response = getResponse(questionId, receiver, Arrays.asList(0, 0));
-        // feedbackSubmitPage.fillRubricResponse(1, receiver.getName(), response);
-        // feedbackSubmitPage.clickSubmitQuestionButton(1);
-
-        // feedbackSubmitPage = getFeedbackSubmitPage();
-        // feedbackSubmitPage.verifyRubricResponse(1, receiver.getName(), response);
-        // verifyPresentInDatabase(response);
+        feedbackSubmitPage = getFeedbackSubmitPage();
+        feedbackSubmitPage.verifyRubricResponse(1, receiver.getName(), response);
+        verifyPresentInDatabase(response);
     }
 
-    private FeedbackResponseAttributes getResponse(String questionId, StudentAttributes receiver, List<Integer> answers) {
+    private FeedbackResponse getResponse(FeedbackQuestion question, Student receiver, List<Integer> answers) {
         FeedbackRubricResponseDetails details = new FeedbackRubricResponseDetails();
         details.setAnswer(answers);
-        return FeedbackResponseAttributes.builder(questionId, student.getEmail(), receiver.getEmail())
-                .withResponseDetails(details)
-                .build();
+
+        return FeedbackResponse.makeResponse(question, student.getEmail(), student.getSection(),
+                receiver.getEmail(), receiver.getSection(), details);
     }
 }
