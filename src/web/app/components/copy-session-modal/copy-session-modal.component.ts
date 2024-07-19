8b25ef17c8aedc28fe4227d76c89e71c76027c5d
Respirayson <raysonyeap4c@gmail.com>
2024-07-20 02:08:42 +0800

    Add logic to prepend Copy of when copying to same course id

--- src/web/app/components/copy-session-modal/copy-session-modal.component.ts
+++ src/web/app/components/copy-session-modal/copy-session-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Course } from '../../../types/api-output';
 import { FEEDBACK_SESSION_NAME_MAX_LENGTH } from '../../../types/field-validator';
@@ -11,7 +11,7 @@ import { FEEDBACK_SESSION_NAME_MAX_LENGTH } from '../../../types/field-validator
   templateUrl: './copy-session-modal.component.html',
   styleUrls: ['./copy-session-modal.component.scss'],
 })
-export class CopySessionModalComponent {
+export class CopySessionModalComponent implements OnInit {
 
   // const
   FEEDBACK_SESSION_NAME_MAX_LENGTH: number = FEEDBACK_SESSION_NAME_MAX_LENGTH;
@@ -24,8 +24,13 @@ export class CopySessionModalComponent {
 
   newFeedbackSessionName: string = '';
   copyToCourseSet: Set<string> = new Set<string>();
+  originalSessionName: string = '';
 
   constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void {
+    this.originalSessionName = this.newFeedbackSessionName;
+  }
 
   /**
    * Fires the copy event.
@@ -44,8 +49,14 @@ export class CopySessionModalComponent {
   select(courseId: string): void {
     if (this.copyToCourseSet.has(courseId)) {
       this.copyToCourseSet.delete(courseId);
+      if (courseId === this.sessionToCopyCourseId) {
+        this.newFeedbackSessionName = this.originalSessionName;
+      }
     } else {
       this.copyToCourseSet.add(courseId);
+      if (courseId === this.sessionToCopyCourseId) {
+        this.newFeedbackSessionName = `Copy of ${this.originalSessionName}`;
+      }
     }
   }
 }
