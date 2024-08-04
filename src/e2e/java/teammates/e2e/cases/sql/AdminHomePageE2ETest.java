9e15d2f38c4cf7200e0c098438ab8b193814b887
Andy Wang <128531452+Andy-W-Developer@users.noreply.github.com>
2024-08-04 21:47:30 +1200

    Fix inconsistent whitespacing

--- src/e2e/java/teammates/e2e/cases/sql/AdminHomePageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/sql/AdminHomePageE2ETest.java
@@ -23,23 +23,18 @@ public class AdminHomePageE2ETest extends BaseE2ETestCase {
         AdminHomePage homePage = loginAdminToPage(url, AdminHomePage.class);
 
         ______TS("Test adding instructors with both valid and invalid details");
-
         String name = "AHPUiT Instrúctör WithPlusInEmail";
         String email = "AHPUiT+++_.instr1!@gmail.tmt";
         String institute = "TEAMMATES Test Institute 1";
-
         homePage.queueInstructorForAdding(name, email, institute);
 
         String singleLineDetails = "Instructor With Invalid Email | invalidemail | TEAMMATES Test Institute 1";
-
         homePage.queueInstructorForAdding(singleLineDetails);
 
         homePage.reloadPage();
 
         ______TS("Verify that newly added instructor appears in account request table");
-
         homePage.verifyInstructorInAccountRequestTable(name, email, institute);
-
     }
 
 }
