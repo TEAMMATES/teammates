feadf321d20504f5457c33ab4d93e36506d64593
SD. <dai.sszzzz@gmail.com>
2024-10-18 09:23:43 +1100

    Add empty judgment and operation prompts

--- src/web/app/pages-instructor/instructor-course-enroll-page/instructor-course-enroll-page.component.ts
+++ src/web/app/pages-instructor/instructor-course-enroll-page/instructor-course-enroll-page.component.ts
@@ -666,6 +666,30 @@ export class InstructorCourseEnrollPageComponent implements OnInit {
 
     const existingStudentsData = existingStudentsHOTInstance.getData();
 
+
+    const validStudents: StudentEnrollRequest[] = existingStudentsData
+        .filter((row: string[]) => !row.every(cell => cell === null || cell === ''))
+        .map((row: string[]) => ({
+          section: row[0] ? row[0].trim() : '',
+          team: row[1] ? row[1].trim() : '',
+          name: row[2] ? row[2].trim() : '',
+          email: row[3] ? row[3].trim() : '',
+          comments: row[4] ? row[4].trim() : '',
+        }));
+
     newStudentsHOTInstance.loadData(existingStudentsData);
+
+    // judge if the existing student is empty
+    if (validStudents.length === 0) {
+      this.statusMessageService.showErrorToast('No valid existing students to copy.');
+      return;
+    }
+
+    if(this.isExistingStudentsPanelCollapsed) {
+      this.statusMessageService.showWarningToast('Please expand existing students.');
+    } else {
+      this.statusMessageService.showSuccessToast('Existing students copied successfully.');
+    }
+
   }
 }
