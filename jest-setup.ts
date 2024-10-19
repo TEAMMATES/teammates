8fd99434497b3a7f59e2e11f3b0ab468ec51b8ef
Zijian Yang <176981575+Zijian-Yang@users.noreply.github.com>
2024-10-19 15:03:35 +1100

    test: integrate jest-preset-angular for improved Angular testing compatibility and update related configurations

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,9 +1,22 @@
-require('core-js/es/reflect');
-require('core-js/proposals/reflect-metadata');
+import 'zone.js';
+import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
 require('@angular/localize/init');
 
-(window as any).IntersectionObserver = jest.fn(() => ({
-  observe: jest.fn(),
-  unobserve: jest.fn(),
-  disconnect: jest.fn(),
-}));
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  value: jest.fn(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  })),
+});
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  value: window.IntersectionObserver,
+});
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,29 @@
 module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      useESM: true,
+    },
+  },
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!@angular|rxjs|@ng-bootstrap|zone.js)'],
+  transform: {
+    '^.+\\.(ts|js|mjs|html|svg)$': ['jest-preset-angular', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      useESM: true,
+    }],
+  },
   collectCoverageFrom: [
     'src/web/app/**/*.ts',
     '!src/web/app/**/*.module.ts',
@@ -7,12 +32,4 @@ module.exports = {
   ],
   coverageDirectory: './coverage',
   coverageReporters: ['lcov', 'text-summary'],
-  setupFiles: [
-    './jest-setup.ts',
-  ],
-  moduleNameMapper: {
-    d3: '<rootDir>/node_modules/d3/dist/d3.min.js',
-    'lodash-es': 'lodash'
-  },
-  globalSetup: 'jest-preset-angular/global-setup',
 };
