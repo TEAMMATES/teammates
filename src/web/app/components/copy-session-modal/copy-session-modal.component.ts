869f32a843c3abe53aba1330adbc89c94bbea2cf
Ruofan Xu <u7660525@anu.edu.au>
2024-10-25 13:58:41 +1100

    test

--- src/web/app/components/copy-session-modal/copy-session-modal.component.ts
+++ src/web/app/components/copy-session-modal/copy-session-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Course } from '../../../types/api-output';
 import { FEEDBACK_SESSION_NAME_MAX_LENGTH } from '../../../types/field-validator';
+
 /**
 * Copy current session modal.
 */
@@ -11,40 +12,32 @@ templateUrl: './copy-session-modal.component.html',
 styleUrls: ['./copy-session-modal.component.scss'],
 })
 export class CopySessionModalComponent {
+
 // const
 FEEDBACK_SESSION_NAME_MAX_LENGTH: number = FEEDBACK_SESSION_NAME_MAX_LENGTH;
+
 @Input()
 courseCandidates: Course[] = [];
+
 @Input()
 sessionToCopyCourseId: string = '';
+
 newFeedbackSessionName: string = '';
 copyToCourseSet: Set<string> = new Set<string>();
 
-errorMessage: string = '';
-
 constructor(public activeModal: NgbActiveModal) {}
-
-  onNameChange(): void {
-    this.errorMessage = '';
-  }
 
   /**
    * Fires the copy event.
    */
   copy(): void {
-    if (this.newFeedbackSessionName.trim().length === 0) {
-      this.errorMessage = "The field \"Name for copied session\" should not be whitespace.";
-      return;
-    }
-
-    this.errorMessage = '';
-
     this.activeModal.close({
       newFeedbackSessionName: this.newFeedbackSessionName,
       sessionToCopyCourseId: this.sessionToCopyCourseId,
       copyToCourseList: Array.from(this.copyToCourseSet),
     });
   }
+
   /**
    * Toggles selection of course to copy to in set.
    */
