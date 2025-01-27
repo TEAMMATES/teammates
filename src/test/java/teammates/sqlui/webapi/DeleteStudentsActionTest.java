bee8c47ea9e570ea433ee1a5a86ac9e40085f501
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-01-28 04:07:48 +0800

    Add DeleteStudentsActionTest

---
+++ src/test/java/teammates/sqlui/webapi/DeleteStudentsActionTest.java
@@ -0,0 +1,118 @@
+package teammates.sqlui.webapi;
+
+import static org.mockito.Mockito.when;
+
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.InstructorPrivileges;
+import teammates.common.util.Const;
+import teammates.storage.sqlentity.Course;
+import teammates.storage.sqlentity.Instructor;
+import teammates.ui.output.MessageOutput;
+import teammates.ui.webapi.DeleteStudentsAction;
+
+/**
+ * SUT: {@link DeleteStudentsAction}.
+ */
+public class DeleteStudentsActionTest extends BaseActionTest<DeleteStudentsAction> {
+
+    String googleId = "user-googleId";
+    int deleteLimit = 3;
+
+    @Override
+    protected String getActionUri() {
+        return Const.ResourceURIs.STUDENTS;
+    }
+
+    @Override
+    protected String getRequestMethod() {
+        return DELETE;
+    }
+
+    @Test
+    void testExecute_deleteLimitedStudents_success() {
+        Course course = new Course("course-id", "name", Const.DEFAULT_TIME_ZONE, "institute");
+
+        when(mockLogic.getCourse(course.getId())).thenReturn(course);
+
+        String[] params = {
+                Const.ParamsNames.COURSE_ID, course.getId(),
+                Const.ParamsNames.LIMIT, String.valueOf(deleteLimit),
+        };
+
+        DeleteStudentsAction action = getAction(params);
+        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+
+        assertEquals("Successful", actionOutput.getMessage());
+    }
+
+    @Test
+    void testExecute_randomCourse_failSilently() {
+        when(mockLogic.getCourse("RANDOM_ID")).thenReturn(null);
+
+        String[] params = {
+                Const.ParamsNames.COURSE_ID, "RANDOM_ID",
+                Const.ParamsNames.LIMIT, String.valueOf(deleteLimit),
+        };
+
+        DeleteStudentsAction action = getAction(params);
+        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+
+        assertEquals("Successful", actionOutput.getMessage());
+    }
+
+    @Test
+    void testExecute_noParameters_throwsInvalidParametersException() {
+        verifyHttpParameterFailure();
+    }
+
+    @Test
+    void testSpecificAccessControl_instructorWithPermission_canAccess() {
+        Course course = new Course("course-id", "name", Const.DEFAULT_TIME_ZONE, "institute");
+        InstructorPrivileges instructorPrivileges = new InstructorPrivileges();
+        instructorPrivileges.updatePrivilege(Const.InstructorPermissions.CAN_MODIFY_STUDENT, true);
+        Instructor instructor = new Instructor(course, "name", "instructoremail@tm.tmt",
+                false, "", null, instructorPrivileges);
+
+        loginAsInstructor(googleId);
+        when(mockLogic.getCourse(course.getId())).thenReturn(course);
+        when(mockLogic.getInstructorByGoogleId(course.getId(), googleId)).thenReturn(instructor);
+
+        String[] params = {
+                Const.ParamsNames.COURSE_ID, course.getId(),
+        };
+
+        verifyCanAccess(params);
+    }
+
+    @Test
+    void testSpecificAccessControl_instructorWithInvalidPermission_cannotAccess() {
+        Course course = new Course("course-id", "name", Const.DEFAULT_TIME_ZONE, "institute");
+        InstructorPrivileges instructorPrivileges = new InstructorPrivileges();
+        instructorPrivileges.updatePrivilege(Const.InstructorPermissions.CAN_MODIFY_STUDENT, false);
+        Instructor instructor = new Instructor(course, "name", "instructoremail@tm.tmt",
+                false, "", null, instructorPrivileges);
+
+        loginAsInstructor(googleId);
+        when(mockLogic.getInstructorByGoogleId(course.getId(), googleId)).thenReturn(instructor);
+
+        String[] params = {
+                Const.ParamsNames.COURSE_ID, course.getId(),
+        };
+
+        verifyCannotAccess(params);
+    }
+
+    @Test
+    void testSpecificAccessControl_student_cannotAccess() {
+        String[] params = {
+                Const.ParamsNames.COURSE_ID, "course-id",
+        };
+
+        loginAsStudent(googleId);
+        verifyCannotAccess(params);
+
+        logoutUser();
+        verifyCannotAccess(params);
+    }
+}
