28080ff21b763cd6ab71aa9b61d8e1f41b67d6b2
peasantbird <bertrand.wong.xh@gmail.com>
2025-02-03 01:35:24 +0800

    Add tests for execute

---
+++ src/test/java/teammates/sqlui/webapi/DeleteAccountRequestActionTest.java
@@ -0,0 +1,82 @@
+package teammates.sqlui.webapi;
+
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.testng.annotations.Test;
+
+import teammates.common.datatransfer.AccountRequestStatus;
+import teammates.common.util.Const;
+import teammates.storage.sqlentity.AccountRequest;
+import teammates.ui.output.MessageOutput;
+import teammates.ui.webapi.DeleteAccountRequestAction;
+import teammates.ui.webapi.InvalidOperationException;
+
+/**
+ * SUT: {@link DeleteAccountRequestAction}.
+ */
+public class DeleteAccountRequestActionTest extends BaseActionTest<DeleteAccountRequestAction> {
+
+    @Override
+    protected String getActionUri() {
+        return Const.ResourceURIs.ACCOUNT_REQUEST;
+    }
+
+    @Override
+    protected String getRequestMethod() {
+        return DELETE;
+    }
+
+    @Test
+    void testExecute_existingAccountRequest_success() {
+        AccountRequest accountRequest = new AccountRequest("email", "name", "institute", AccountRequestStatus.PENDING,
+                "comments");
+        UUID id = accountRequest.getId();
+
+        when(mockLogic.getAccountRequest(id)).thenReturn(accountRequest);
+
+        String[] params = {
+                Const.ParamsNames.ACCOUNT_REQUEST_ID, id.toString(),
+        };
+
+        DeleteAccountRequestAction action = getAction(params);
+        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+
+        assertEquals("Account request successfully deleted.", actionOutput.getMessage());
+    }
+
+    @Test
+    void testExecute_registeredInstructor_throwsInvalidOperationException() {
+        AccountRequest accountRequest = new AccountRequest("email", "name", "institute",
+                AccountRequestStatus.REGISTERED, "comments");
+        accountRequest.setRegisteredAt(Instant.now());
+        UUID id = accountRequest.getId();
+
+        when(mockLogic.getAccountRequest(id)).thenReturn(accountRequest);
+
+        String[] params = {
+                Const.ParamsNames.ACCOUNT_REQUEST_ID, id.toString(),
+        };
+
+        InvalidOperationException ex = verifyInvalidOperation(params);
+        assertEquals("Account request of a registered instructor cannot be deleted.", ex.getMessage());
+    }
+
+    @Test
+    void testExecute_nonExistingAccountRequest_failSilently() {
+        UUID id = UUID.randomUUID();
+
+        when(mockLogic.getAccountRequest(id)).thenReturn(null);
+
+        String[] params = {
+                Const.ParamsNames.ACCOUNT_REQUEST_ID, id.toString(),
+        };
+
+        DeleteAccountRequestAction action = getAction(params);
+        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+
+        assertEquals("Account request successfully deleted.", actionOutput.getMessage());
+    }
+}
