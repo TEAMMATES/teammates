af0ebf7aeda6d9c76d9ddc53f9b53bd4abebee80
InfinityTwo <2poh.junkang@gmail.com>
2025-02-26 21:44:31 +0800

    Fix lint

--- src/test/java/teammates/sqlui/webapi/GetNotificationsActionTest.java
+++ src/test/java/teammates/sqlui/webapi/GetNotificationsActionTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.NotificationTargetUser;
