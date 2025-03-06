a8d0968e725aff8b8380f5eef68a1f5aca6df730
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-03-07 02:42:34 +0800

    Refine test class

--- src/test/java/teammates/sqlui/webapi/FeedbackSessionPublishedRemindersActionTest.java
+++ src/test/java/teammates/sqlui/webapi/FeedbackSessionPublishedRemindersActionTest.java
@@ -1,17 +1,20 @@
 package teammates.sqlui.webapi;
 
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.List;
 
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import teammates.common.exception.EntityDoesNotExistException;
-import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Const;
+import teammates.common.util.RequestTracer;
 import teammates.storage.sqlentity.Course;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.ui.output.MessageOutput;
@@ -22,8 +25,8 @@ import teammates.ui.webapi.FeedbackSessionPublishedRemindersAction;
  */
 public class FeedbackSessionPublishedRemindersActionTest extends BaseActionTest<FeedbackSessionPublishedRemindersAction> {
 
-    private Course course;
     private FeedbackSession session;
+    private FeedbackSession session2;
 
     @Override
     protected String getActionUri() {
@@ -37,76 +40,60 @@ public class FeedbackSessionPublishedRemindersActionTest extends BaseActionTest<
 
     @BeforeMethod
     public void setUp() {
-        course = new Course("course-id", "Course Name", Const.DEFAULT_TIME_ZONE, "institute");
-        session = new FeedbackSession(
-                "Session Name",
-                course,
-                "instructor1email@tm.tmt",
-                "Instructions",
-                Instant.now().minusSeconds(7200), // Start time 2 hours ago
-                Instant.now().minusSeconds(3600), // End time 1 hour ago
-                Instant.now().minusSeconds(7200), // Visible time 2 hours ago
-                Instant.now().plusSeconds(1800), // Results visible time in 30 minutes
-                Duration.ofMinutes(30),
-                false, false, true
-        );
+        Mockito.reset(mockLogic);
+
+        Course course = getTypicalCourse();
+        session = getTypicalFeedbackSessionForCourse(course);
+        session.setPublishedEmailEnabled(true);
+        session2 = getTypicalFeedbackSessionForCourse(course);
+        session2.setName("test-feedbacksession2");
+        session2.setPublishedEmailEnabled(true);
     }
 
     @Test
-    void testExecute_noPublishedSessions_noEmailsSent() {
-        when(mockLogic.getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent()).thenReturn(List.of());
+    void testExecute_allSessionsPublished_emailsSent() {
+        when(mockLogic.getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent())
+                .thenReturn(List.of(session, session2));
 
-        FeedbackSessionPublishedRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+        try (MockedStatic<RequestTracer> mockRequestTracer = mockStatic(RequestTracer.class)) {
+            FeedbackSessionPublishedRemindersAction action = getAction();
+            MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
 
-        verifyNoTasksAdded();
-        assertEquals("Successful", actionOutput.getMessage());
+            verify(mockLogic, times(1)).getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent();
+            mockRequestTracer.verify(RequestTracer::checkRemainingTime, times(2));
+            verifySpecifiedTasksAdded(Const.TaskQueue.FEEDBACK_SESSION_PUBLISHED_EMAIL_QUEUE_NAME, 2);
+            assertEquals("Successful", actionOutput.getMessage());
+        }
     }
 
     @Test
-    void testExecute_publishedSession_emailsSent() {
+    void testExecute_oneSessionPublished_emailsSent() {
         when(mockLogic.getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent()).thenReturn(List.of(session));
 
-        FeedbackSessionPublishedRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+        try (MockedStatic<RequestTracer> mockRequestTracer = mockStatic(RequestTracer.class)) {
+            FeedbackSessionPublishedRemindersAction action = getAction();
+            MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
 
-        verifySpecifiedTasksAdded(Const.TaskQueue.FEEDBACK_SESSION_PUBLISHED_EMAIL_QUEUE_NAME, 1);
-        assertEquals("Successful", actionOutput.getMessage());
+            verify(mockLogic, times(1)).getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent();
+            mockRequestTracer.verify(RequestTracer::checkRemainingTime, times(1));
+            verifySpecifiedTasksAdded(Const.TaskQueue.FEEDBACK_SESSION_PUBLISHED_EMAIL_QUEUE_NAME, 1);
+            assertEquals("Successful", actionOutput.getMessage());
+        }
     }
 
     @Test
-    void testExecute_sessionPublishedWithEmailsAlreadySent_noEmailsSent() {
-        session.setPublishedEmailSent(true);
+    void testExecute_noSessionsPublished_noEmailsSent() {
         when(mockLogic.getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent()).thenReturn(List.of());
 
-        FeedbackSessionPublishedRemindersAction action = getAction();
-        MessageOutput output = (MessageOutput) getJsonResult(action).getOutput();
+        try (MockedStatic<RequestTracer> mockRequestTracer = mockStatic(RequestTracer.class)) {
+            FeedbackSessionPublishedRemindersAction action = getAction();
+            MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
 
-        verifyNoTasksAdded();
-        assertEquals("Successful", output.getMessage());
-    }
-
-    @Test
-    void testExecute_publishedReminderDisabled_noEmailsSent() {
-        session.setPublishedEmailEnabled(false);
-        when(mockLogic.getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent()).thenReturn(List.of());
-
-        FeedbackSessionPublishedRemindersAction action = getAction();
-        MessageOutput output = (MessageOutput) getJsonResult(action).getOutput();
-
-        verifyNoTasksAdded();
-        assertEquals("Successful", output.getMessage());
-    }
-
-    @Test
-    void testExecute_unpublishedSession_noEmailsSent() throws InvalidParametersException, EntityDoesNotExistException {
-        when(mockLogic.unpublishFeedbackSession(session.getName(), course.getId())).thenReturn(session);
-
-        FeedbackSessionPublishedRemindersAction action = getAction();
-        MessageOutput output = (MessageOutput) getJsonResult(action).getOutput();
-
-        verifyNoTasksAdded();
-        assertEquals("Successful", output.getMessage());
+            verify(mockLogic, times(1)).getFeedbackSessionsWhichNeedAutomatedPublishedEmailsToBeSent();
+            mockRequestTracer.verify(RequestTracer::checkRemainingTime, never());
+            verifyNoTasksAdded();
+            assertEquals("Successful", actionOutput.getMessage());
+        }
     }
 
     @Test
@@ -117,19 +104,25 @@ public class FeedbackSessionPublishedRemindersActionTest extends BaseActionTest<
 
     @Test
     void testSpecificAccessControl_instructor_cannotAccess() {
-        loginAsInstructor("instructor-googleId");
+        loginAsInstructor(getTypicalInstructor().getGoogleId());
         verifyCannotAccess();
     }
 
     @Test
     void testSpecificAccessControl_student_cannotAccess() {
-        loginAsStudent("student-googleId");
+        loginAsStudent(getTypicalStudent().getGoogleId());
         verifyCannotAccess();
     }
 
     @Test
     void testSpecificAccessControl_loggedOut_cannotAccess() {
         logoutUser();
+        verifyCannotAccess();
+    }
+
+    @Test
+    void testSpecificAccessControl_unregistered_cannotAccess() {
+        loginAsUnregistered(getTypicalInstructor().getGoogleId());
         verifyCannotAccess();
     }
 }
