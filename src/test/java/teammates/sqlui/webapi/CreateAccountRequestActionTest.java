4ff1b4ed61a0eef907ad26efda67ef0645b26252
peasantbird <bertrand.wong.xh@gmail.com>
2025-03-10 12:29:18 +0800

    Add valid account create request test case

--- src/test/java/teammates/sqlui/webapi/CreateAccountRequestActionTest.java
+++ src/test/java/teammates/sqlui/webapi/CreateAccountRequestActionTest.java
@@ -9,6 +9,7 @@ import teammates.common.datatransfer.AccountRequestStatus;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Const;
 import teammates.storage.sqlentity.AccountRequest;
+import teammates.ui.output.AccountRequestData;
 import teammates.ui.request.AccountCreateRequest;
 import teammates.ui.request.InvalidHttpRequestBodyException;
 import teammates.ui.webapi.CreateAccountRequestAction;
@@ -18,9 +19,6 @@ import teammates.ui.webapi.CreateAccountRequestAction;
  */
 public class CreateAccountRequestActionTest extends BaseActionTest<CreateAccountRequestAction> {
 
-    private String instructorName = "JamesBond";
-    private String instructorEmail = "jamesbond89@gmail.tmt";
-    private String instructorInstitution = "TEAMMATES Test Institute 1";
     private AccountCreateRequest createRequest;
 
     @Override
@@ -67,21 +65,42 @@ public class CreateAccountRequestActionTest extends BaseActionTest<CreateAccount
 
     @Test
     void testExecute_validAccountCreateRequest_success() throws InvalidParametersException {
-        loginAsAdmin();
-        AccountRequest accountRequest = new AccountRequest(instructorEmail, instructorName, instructorInstitution, AccountRequestStatus.PENDING, null);
+        String instructorEmail = "jamesbond89@gmail.tmt";
+        String instructorName = "JamesBond";
+        String instructorInstitution = "TEAMMATES Test Institute 1";
+        String instructorComments = "comments";
+        AccountRequest accountRequest = new AccountRequest(instructorEmail, instructorName, instructorInstitution,
+                AccountRequestStatus.PENDING, instructorComments);
 
-        when(mockLogic.createAccountRequestWithTransaction(instructorName, instructorEmail, instructorInstitution, AccountRequestStatus.PENDING, null)).thenReturn(accountRequest);
+        when(mockLogic.createAccountRequestWithTransaction(instructorName, instructorEmail, instructorInstitution,
+                AccountRequestStatus.PENDING, instructorComments)).thenReturn(accountRequest);
 
+        CreateAccountRequestAction action = getAction(createRequest);
+        AccountRequestData output = (AccountRequestData) getJsonResult(action).getOutput();
+
+        verifyAccountRequestCreated(output, accountRequest);
         verifySpecifiedTasksAdded(Const.TaskQueue.SEARCH_INDEXING_QUEUE_NAME, 1);
+        verifyNumberOfEmailsSent(2);
     }
 
     private AccountCreateRequest getTypicalAccountCreateRequest() {
         AccountCreateRequest createRequest = new AccountCreateRequest();
 
-        createRequest.setInstructorName(instructorName);
-        createRequest.setInstructorEmail(instructorEmail);
-        createRequest.setInstructorInstitution(instructorInstitution);
+        createRequest.setInstructorEmail("  jamesbond89@gmail.tmt  ");
+        createRequest.setInstructorName("  JamesBond  ");
+        createRequest.setInstructorInstitution("  TEAMMATES Test Institute 1  ");
+        createRequest.setInstructorComments("  comments  ");
 
         return createRequest;
+    }
+
+    private void verifyAccountRequestCreated(AccountRequestData output, AccountRequest accountRequest) {
+        assertEquals(output.getId(), accountRequest.getId().toString());
+        assertEquals(output.getEmail(), accountRequest.getEmail());
+        assertEquals(output.getName(), accountRequest.getName());
+        assertEquals(output.getInstitute(), accountRequest.getInstitute());
+        assertEquals(output.getRegistrationKey(), accountRequest.getRegistrationKey());
+        assertEquals(output.getStatus(), accountRequest.getStatus());
+        assertEquals(output.getComments(), accountRequest.getComments());
     }
 }
