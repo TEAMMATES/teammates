7cdd2fde8a8b74ee4e1bcb528912f053810d779e
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-03-19 02:15:53 +0800

    Refine test class

--- src/test/java/teammates/sqlui/webapi/FeedbackSessionOpeningSoonRemindersActionTest.java
+++ src/test/java/teammates/sqlui/webapi/FeedbackSessionOpeningSoonRemindersActionTest.java
@@ -1,24 +1,25 @@
 package teammates.sqlui.webapi;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.util.Const;
-import teammates.common.util.EmailType;
 import teammates.common.util.EmailWrapper;
-import teammates.common.util.TaskWrapper;
-import teammates.storage.sqlentity.Course;
+import teammates.common.util.RequestTracer;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.ui.output.MessageOutput;
-import teammates.ui.request.SendEmailRequest;
 import teammates.ui.webapi.FeedbackSessionOpeningSoonRemindersAction;
 
 /**
@@ -27,8 +28,8 @@ import teammates.ui.webapi.FeedbackSessionOpeningSoonRemindersAction;
 public class FeedbackSessionOpeningSoonRemindersActionTest
         extends BaseActionTest<FeedbackSessionOpeningSoonRemindersAction> {
 
-    private Course course;
     private FeedbackSession session;
+    private FeedbackSession session2;
 
     @Override
     protected String getActionUri() {
@@ -42,144 +43,72 @@ public class FeedbackSessionOpeningSoonRemindersActionTest
 
     @BeforeMethod
     public void setUp() {
-        course = new Course("course-id", "Course Name", Const.DEFAULT_TIME_ZONE, "institute");
-        session = new FeedbackSession(
-                "Session Name",
-                course,
-                "instructor1email@tm.tmt",
-                "Instructions",
-                Instant.now().plus(Duration.ofHours(24)), // Starts in 24 hours
-                Instant.now().plus(Duration.ofHours(48)), // Ends in 48 hours
-                Instant.now(), // Visible now
-                Instant.now().plus(Duration.ofHours(48)), // Results visible in 48 hours
-                Duration.ofMinutes(30),
-                true, false, false
-        );
+        Mockito.reset(mockLogic, mockSqlEmailGenerator);
 
+        session = mock(FeedbackSession.class);
+        session2 = mock(FeedbackSession.class);
+        EmailWrapper mockEmail = mock(EmailWrapper.class);
+        EmailWrapper mockEmail2 = mock(EmailWrapper.class);
+
+        when(mockSqlEmailGenerator.generateFeedbackSessionOpeningSoonEmails(session)).thenReturn(List.of(mockEmail));
+        when(mockSqlEmailGenerator.generateFeedbackSessionOpeningSoonEmails(session2)).thenReturn(List.of(mockEmail2));
+    }
+
+    @Test
+    void testExecute_allSessionsOpeningSoon_emailsSent() {
+        when(mockLogic.getFeedbackSessionsOpeningWithinTimeLimit()).thenReturn(List.of(session, session2));
+
+        try (MockedStatic<RequestTracer> mockRequestTracer = mockStatic(RequestTracer.class)) {
+            FeedbackSessionOpeningSoonRemindersAction action = getAction();
+            MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+
+            verify(mockLogic, times(1)).getFeedbackSessionsOpeningWithinTimeLimit();
+            mockRequestTracer.verify(RequestTracer::checkRemainingTime, times(2));
+            verify(mockSqlEmailGenerator, times(1)).generateFeedbackSessionOpeningSoonEmails(session);
+            verify(mockSqlEmailGenerator, times(1)).generateFeedbackSessionOpeningSoonEmails(session2);
+            verify(session, times(1)).setOpeningSoonEmailSent(true);
+            verify(session2, times(1)).setOpeningSoonEmailSent(true);
+
+            verifySpecifiedTasksAdded(Const.TaskQueue.SEND_EMAIL_QUEUE_NAME, 2);
+            verifyNoMoreInteractions(mockLogic, mockSqlEmailGenerator, session, session2);
+            assertEquals("Successful", actionOutput.getMessage());
+        }
+    }
+
+    @Test
+    void testExecute_oneSessionOpeningSoon_emailsSent() {
         when(mockLogic.getFeedbackSessionsOpeningWithinTimeLimit()).thenReturn(List.of(session));
+
+        try (MockedStatic<RequestTracer> mockRequestTracer = mockStatic(RequestTracer.class)) {
+            FeedbackSessionOpeningSoonRemindersAction action = getAction();
+            MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+
+            verify(mockLogic, times(1)).getFeedbackSessionsOpeningWithinTimeLimit();
+            mockRequestTracer.verify(RequestTracer::checkRemainingTime, times(1));
+            verify(mockSqlEmailGenerator, times(1)).generateFeedbackSessionOpeningSoonEmails(session);
+            verify(session, times(1)).setOpeningSoonEmailSent(true);
+
+            verifySpecifiedTasksAdded(Const.TaskQueue.SEND_EMAIL_QUEUE_NAME, 1);
+            verifyNoMoreInteractions(mockLogic, mockSqlEmailGenerator, session, session2);
+            assertEquals("Successful", actionOutput.getMessage());
+        }
     }
 
     @Test
     void testExecute_noSessionsOpeningSoon_noEmailsSent() {
         when(mockLogic.getFeedbackSessionsOpeningWithinTimeLimit()).thenReturn(List.of());
 
-        FeedbackSessionOpeningSoonRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
+        try (MockedStatic<RequestTracer> mockRequestTracer = mockStatic(RequestTracer.class)) {
+            FeedbackSessionOpeningSoonRemindersAction action = getAction();
+            MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
 
-        verifyNoTasksAdded();
-        assertEquals("Successful", actionOutput.getMessage());
-    }
+            verify(mockLogic, times(1)).getFeedbackSessionsOpeningWithinTimeLimit();
+            mockRequestTracer.verify(RequestTracer::checkRemainingTime, never());
 
-    @Test
-    void testExecute_sessionOpeningSoon_emailsSent() {
-        int numRecipients = 3;
-        List<EmailWrapper> emails = IntStream.range(1, numRecipients + 1)
-                .mapToObj(i -> {
-                    EmailWrapper email = new EmailWrapper();
-                    email.setRecipient("recipient" + i + "@tm.tmt");
-                    email.setType(EmailType.FEEDBACK_OPENING_SOON);
-                    email.setSubjectFromType(course.getName(), session.getName());
-                    return email;
-                })
-                .collect(Collectors.toList());
-
-        when(mockSqlEmailGenerator.generateFeedbackSessionOpeningSoonEmails(session)).thenReturn(emails);
-
-        FeedbackSessionOpeningSoonRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
-
-        // Verify tasks added
-        verifySpecifiedTasksAdded(Const.TaskQueue.SEND_EMAIL_QUEUE_NAME, numRecipients);
-
-        List<TaskWrapper> tasksAdded = mockTaskQueuer.getTasksAdded();
-        String expectedSubject = String.format(
-                EmailType.FEEDBACK_OPENING_SOON.getSubject(),
-                course.getName(),
-                session.getName()
-        );
-        for (TaskWrapper task : tasksAdded) {
-            SendEmailRequest req = (SendEmailRequest) task.getRequestBody();
-            EmailWrapper emailSent = req.getEmail();
-            assertEquals(expectedSubject, emailSent.getSubject());
+            verifyNoTasksAdded();
+            verifyNoMoreInteractions(mockLogic, mockSqlEmailGenerator, session, session2);
+            assertEquals("Successful", actionOutput.getMessage());
         }
-
-        assertEquals("Successful", actionOutput.getMessage());
-    }
-
-    @Test
-    void testExecute_sessionOpeningSoonWithEmailsAlreadySent_noEmailsSent() {
-        session.setOpeningSoonEmailSent(true);
-
-        FeedbackSessionOpeningSoonRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
-
-        verifyNoTasksAdded();
-        assertEquals("Successful", actionOutput.getMessage());
-    }
-
-    @Test
-    void testExecute_openingSoonReminderDisabled_noEmailsSent() {
-        session.setOpeningEmailEnabled(false);
-
-        FeedbackSessionOpeningSoonRemindersAction action = getAction();
-        MessageOutput output = (MessageOutput) getJsonResult(action).getOutput();
-
-        verifyNoTasksAdded();
-        assertEquals("Successful", output.getMessage());
-    }
-
-    // Checks that no emails are sent when a session is modified to open in < 24 hours if it was initially > 24 hours.
-    @Test
-    void testExecute_sessionModifiedToOpenInLessThan24Hours_noEmailsSent() {
-        session.setStartTime(Instant.now().plusSeconds(3600)); // Starts in 1 hour
-
-        FeedbackSessionOpeningSoonRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
-
-        verifyNoTasksAdded();
-        assertEquals("Successful", actionOutput.getMessage());
-    }
-
-    // Checks that sessions previously completed but modified to open in 24 hours will resend the opening soon email.
-    @Test
-    void testExecute_completedSessionModifiedToOpenIn24Hours_emailsSent() {
-        session.setStartTime(Instant.now().minus(Duration.ofHours(2))); // Previously started
-        session.setEndTime(Instant.now().minus(Duration.ofHours(1))); // Already ended
-        session.setStartTime(Instant.now().plus(Duration.ofHours(24))); // Modified to open in 24 hours
-        session.setEndTime(Instant.now().plus(Duration.ofHours(48))); // Modified to end in 48 hours
-
-        int numRecipients = 3;
-        List<EmailWrapper> emails = IntStream.range(1, numRecipients + 1)
-                .mapToObj(i -> {
-                    EmailWrapper email = new EmailWrapper();
-                    email.setRecipient("recipient" + i + "@tm.tmt");
-                    email.setType(EmailType.FEEDBACK_OPENING_SOON);
-                    email.setSubjectFromType(course.getName(), session.getName());
-                    return email;
-                })
-                .collect(Collectors.toList());
-
-        when(mockSqlEmailGenerator.generateFeedbackSessionOpeningSoonEmails(session)).thenReturn(emails);
-
-        FeedbackSessionOpeningSoonRemindersAction action = getAction();
-        MessageOutput actionOutput = (MessageOutput) getJsonResult(action).getOutput();
-
-        // Verify tasks added
-        verifySpecifiedTasksAdded(Const.TaskQueue.SEND_EMAIL_QUEUE_NAME, numRecipients);
-
-        List<TaskWrapper> tasksAdded = mockTaskQueuer.getTasksAdded();
-        String expectedSubject = String.format(
-                EmailType.FEEDBACK_OPENING_SOON.getSubject(),
-                course.getName(),
-                session.getName()
-        );
-        for (TaskWrapper task : tasksAdded) {
-            SendEmailRequest req = (SendEmailRequest) task.getRequestBody();
-            EmailWrapper emailSent = req.getEmail();
-            assertEquals(expectedSubject, emailSent.getSubject());
-        }
-
-        assertEquals("Successful", actionOutput.getMessage());
     }
 
     @Test
