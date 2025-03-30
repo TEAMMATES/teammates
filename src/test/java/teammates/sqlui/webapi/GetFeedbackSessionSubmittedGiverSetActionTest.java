4d8ed2f4f9d55233a588b670834182f75249d502
peasantbird <bertrand.wong.xh@gmail.com>
2025-03-30 11:23:01 +0800

    Add tests

---
+++ src/test/java/teammates/sqlui/webapi/GetFeedbackSessionSubmittedGiverSetActionTest.java
@@ -0,0 +1,124 @@
+package teammates.sqlui.webapi;
+
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import teammates.common.util.Const;
+import teammates.storage.sqlentity.Course;
+import teammates.storage.sqlentity.FeedbackQuestion;
+import teammates.storage.sqlentity.FeedbackResponse;
+import teammates.storage.sqlentity.FeedbackSession;
+import teammates.storage.sqlentity.Instructor;
+import teammates.ui.output.FeedbackSessionSubmittedGiverSet;
+import teammates.ui.webapi.GetFeedbackSessionSubmittedGiverSetAction;
+import teammates.ui.webapi.JsonResult;
+
+/**
+ * SUT: {@link GetFeedbackSessionSubmittedGiverSetAction}.
+ */
+public class GetFeedbackSessionSubmittedGiverSetActionTest
+        extends BaseActionTest<GetFeedbackSessionSubmittedGiverSetAction> {
+
+    private Instructor typicalInstructor;
+    private Course typicalCourse;
+    private FeedbackSession typicalFeedbackSession;
+    private FeedbackResponse typicalFeedbackResponse;
+
+    @Override
+    protected String getActionUri() {
+        return Const.ResourceURIs.SESSION_SUBMITTED_GIVER_SET;
+    }
+
+    @Override
+    protected String getRequestMethod() {
+        return GET;
+    }
+
+    @BeforeMethod
+    void setUp() {
+        typicalInstructor = getTypicalInstructor();
+        typicalCourse = getTypicalCourse();
+        typicalFeedbackSession = getTypicalFeedbackSessionForCourse(typicalCourse);
+        FeedbackQuestion typicalFeedbackQuestion = getTypicalFeedbackQuestionForSession(typicalFeedbackSession);
+        typicalFeedbackResponse = getTypicalFeedbackResponseForQuestion(typicalFeedbackQuestion);
+    }
+
+    @Test
+    void testExecute_missingParameters_throwsInvalidHttpParameterException() {
+        verifyHttpParameterFailure();
+    }
+
+    @Test
+    void testExecute_missingCourseId_throwsInvalidHttpParameterException() {
+        String[] params = new String[] {
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, typicalFeedbackSession.getName(),
+        };
+
+        verifyHttpParameterFailure(params);
+    }
+
+    @Test
+    void testExecute_missingFeedbackSessionName_throwsInvalidHttpParameterException() {
+        String[] params = new String[] {
+                Const.ParamsNames.COURSE_ID, typicalCourse.getId(),
+        };
+
+        verifyHttpParameterFailure(params);
+    }
+
+    @Test
+    void testExecute_typicalCase_success() {
+        String[] params = new String[] {
+                Const.ParamsNames.COURSE_ID, typicalCourse.getId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, typicalFeedbackSession.getName(),
+        };
+
+        when(mockLogic.getGiverSetThatAnsweredFeedbackSession(typicalFeedbackSession.getName(), typicalCourse.getId()))
+                .thenReturn(new HashSet<>(Arrays.asList(typicalFeedbackResponse.getGiver())));
+
+        GetFeedbackSessionSubmittedGiverSetAction action = getAction(params);
+        JsonResult result = getJsonResult(action);
+        FeedbackSessionSubmittedGiverSet output = (FeedbackSessionSubmittedGiverSet) result.getOutput();
+
+        assertEquals(new HashSet<>(Arrays.asList(typicalFeedbackResponse.getGiver())), output.getGiverIdentifiers());
+    }
+
+    @Test
+    void testCheckSpecificAccessControl_typicalCase_success() {
+        String[] params = new String[] {
+                Const.ParamsNames.COURSE_ID, typicalCourse.getId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, typicalFeedbackSession.getName(),
+        };
+
+        when(mockLogic.getFeedbackSession(typicalFeedbackSession.getName(), typicalCourse.getId()))
+                .thenReturn(typicalFeedbackSession);
+        when(mockLogic.getInstructorByGoogleId(typicalCourse.getId(), typicalInstructor.getGoogleId()))
+                .thenReturn(typicalInstructor);
+
+        loginAsInstructor(typicalInstructor.getGoogleId());
+
+        verifyCanAccess(params);
+    }
+
+    @Test
+    void testCheckSpecificAccessControl_instructorWithoutAccess_throwsUnauthorizedAccessException() {
+        String[] params = new String[] {
+                Const.ParamsNames.COURSE_ID, typicalCourse.getId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, typicalFeedbackSession.getName(),
+        };
+
+        when(mockLogic.getFeedbackSession(typicalFeedbackSession.getName(), typicalCourse.getId()))
+                .thenReturn(typicalFeedbackSession);
+        when(mockLogic.getInstructorByGoogleId(typicalCourse.getId(), "instructor-not-in-course"))
+                .thenReturn(null);
+
+        loginAsInstructor("instructor-not-in-course");
+
+        verifyCannotAccess(params);
+    }
+}
