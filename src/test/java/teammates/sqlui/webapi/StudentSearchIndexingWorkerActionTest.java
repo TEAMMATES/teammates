bc7e7b40f4f5ac23a9a5cd2a55d9c4a3dc0f64f4
domoberzin <berzindominic@gmail.com>
2025-03-30 14:36:38 +0800

    fix: lint fixes

--- src/test/java/teammates/sqlui/webapi/StudentSearchIndexingWorkerActionTest.java
+++ src/test/java/teammates/sqlui/webapi/StudentSearchIndexingWorkerActionTest.java
@@ -1,10 +1,12 @@
 package teammates.sqlui.webapi;
 
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
 import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import teammates.common.exception.SearchServiceException;
@@ -16,11 +18,21 @@ import teammates.ui.output.MessageOutput;
 import teammates.ui.webapi.JsonResult;
 import teammates.ui.webapi.StudentSearchIndexingWorkerAction;
 
+/**
+ * SUT: {@link StudentSearchIndexingWorkerAction}.
+ */
 public class StudentSearchIndexingWorkerActionTest extends BaseActionTest<StudentSearchIndexingWorkerAction> {
-    
+
+    private Student typicalStudent;
+
     @Override
      String getActionUri() {
         return TaskQueue.STUDENT_SEARCH_INDEXING_WORKER_URL;
+    }
+
+    @BeforeMethod
+    void setUpMethod() {
+        typicalStudent = getTypicalStudent();
     }
 
     @Override
@@ -29,79 +41,73 @@ public class StudentSearchIndexingWorkerActionTest extends BaseActionTest<Studen
     }
 
     @Test
-    protected void textExecute_nullParams_throwsInvalidHttpParameterException() {
-        String[] params2 = {
-            Const.ParamsNames.COURSE_ID, null,
-            Const.ParamsNames.STUDENT_EMAIL, "email",
+    void textExecute_nullParams_throwsInvalidHttpParameterException() {
+        String[][] testCases = {
+                { Const.ParamsNames.COURSE_ID, null, Const.ParamsNames.STUDENT_EMAIL, "email" },
+                { Const.ParamsNames.COURSE_ID, "course-id", Const.ParamsNames.STUDENT_EMAIL, null },
         };
-        verifyHttpParameterFailure(params2);
 
-        String[] params3 = {
-            Const.ParamsNames.COURSE_ID, "course-id",
-            Const.ParamsNames.STUDENT_EMAIL, null,
-        };
-        verifyHttpParameterFailure(params3);
-
-
-        String[] params4 = {
-            Const.ParamsNames.COURSE_ID, "course-id",
-            Const.ParamsNames.STUDENT_EMAIL, null,
-        };
-        verifyHttpParameterFailure(params4);
+        for (String[] params : testCases) {
+            verifyHttpParameterFailure(params);
+        }
     }
-
 
     @Test
     void testExecute_typicalSuccessCase() throws Exception {
-        Student student = getTypicalStudent();
-
-
         String[] submissionParams = new String[] {
-            ParamsNames.COURSE_ID, student.getCourseId(),
-            ParamsNames.STUDENT_EMAIL, student.getEmail(),
+                ParamsNames.COURSE_ID, typicalStudent.getCourseId(),
+                ParamsNames.STUDENT_EMAIL, typicalStudent.getEmail(),
         };
 
-        when(mockLogic.getStudentForEmail(student.getCourseId(), student.getEmail())).thenReturn(student);
-        
+        when(mockLogic.getStudentForEmail(typicalStudent.getCourseId(), typicalStudent.getEmail()))
+                .thenReturn(typicalStudent);
+
         StudentSearchIndexingWorkerAction action = getAction(submissionParams);
         JsonResult res = getJsonResult(action);
 
-        assertEquals(HttpStatus.SC_ACCEPTED, res.getStatusCode());
+        assertEquals(HttpStatus.SC_OK, res.getStatusCode());
         assertEquals("Successful", ((MessageOutput) res.getOutput()).getMessage());
+
+        verify(mockLogic, times(1))
+                .getStudentForEmail(typicalStudent.getCourseId(), typicalStudent.getEmail());
+        verify(mockLogic, times(1)).putStudentDocument(typicalStudent);
     }
 
     @Test
     void testExecute_putDocument_throwsException() throws Exception {
-        Student student = getTypicalStudent();
-
-
         String[] submissionParams = new String[] {
-            ParamsNames.COURSE_ID, student.getCourseId(),
-            ParamsNames.STUDENT_EMAIL, student.getEmail(),
+                ParamsNames.COURSE_ID, typicalStudent.getCourseId(),
+                ParamsNames.STUDENT_EMAIL, typicalStudent.getEmail(),
         };
 
-        when(mockLogic.getStudentForEmail(student.getCourseId(), student.getEmail())).thenReturn(student);
+        when(mockLogic.getStudentForEmail(typicalStudent.getCourseId(),
+                typicalStudent.getEmail())).thenReturn(typicalStudent);
         doThrow(new SearchServiceException("Failure", HttpStatus.SC_BAD_GATEWAY))
-            .when(mockLogic).putStudentDocument(student);
-        
+                .when(mockLogic).putStudentDocument(typicalStudent);
+
         StudentSearchIndexingWorkerAction action = getAction(submissionParams);
         JsonResult res = getJsonResult(action, HttpStatus.SC_BAD_GATEWAY);
 
         assertEquals(HttpStatus.SC_BAD_GATEWAY, res.getStatusCode());
         assertEquals("Failure", ((MessageOutput) res.getOutput()).getMessage());
+
+        verify(mockLogic, times(1))
+                .getStudentForEmail(typicalStudent.getCourseId(), typicalStudent.getEmail());
+        verify(mockLogic, times(1)).putStudentDocument(typicalStudent);
     }
 
     @Test
     void testSpecificAccessControl_onlyAdmin_canAccess() {
+        logoutUser();
         verifyCannotAccess();
 
-        loginAsInstructor("user-googleId");
+        loginAsUnregistered("unregistered user");
         verifyCannotAccess();
 
-        loginAsStudent("user-googleId");
+        loginAsStudent(getTypicalStudent().getGoogleId());
         verifyCannotAccess();
 
-        loginAsMaintainer();
+        loginAsInstructor(getTypicalInstructor().getGoogleId());
         verifyCannotAccess();
 
         loginAsAdmin();
