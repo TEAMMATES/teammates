6f8fb0c6dd07a45df116e3e5454e8c3b75f189b6
peasantbird <bertrand.wong.xh@gmail.com>
2025-04-19 21:23:04 +0800

    Add more tests for access control

--- src/test/java/teammates/sqlui/webapi/SubmitFeedbackResponsesActionTest.java
+++ src/test/java/teammates/sqlui/webapi/SubmitFeedbackResponsesActionTest.java
@@ -3,6 +3,8 @@ package teammates.sqlui.webapi;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +18,7 @@ import teammates.storage.sqlentity.Course;
 import teammates.storage.sqlentity.FeedbackQuestion;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.storage.sqlentity.Instructor;
+import teammates.storage.sqlentity.Student;
 import teammates.ui.request.Intent;
 import teammates.ui.webapi.SubmitFeedbackResponsesAction;
 
@@ -24,6 +27,7 @@ import teammates.ui.webapi.SubmitFeedbackResponsesAction;
  */
 public class SubmitFeedbackResponsesActionTest extends BaseActionTest<SubmitFeedbackResponsesAction> {
 
+    private Student typicalStudent;
     private Instructor typicalInstructor;
     private Course typicalCourse;
     private FeedbackSession typicalFeedbackSession;
@@ -41,9 +45,11 @@ public class SubmitFeedbackResponsesActionTest extends BaseActionTest<SubmitFeed
 
     @BeforeMethod
     void setUp() {
+        typicalStudent = getTypicalStudent();
         typicalInstructor = getTypicalInstructor();
         typicalCourse = getTypicalCourse();
         typicalFeedbackSession = getTypicalFeedbackSessionForCourse(typicalCourse);
+        typicalFeedbackSession.setStartTime(Instant.now());
         typicalFeedbackQuestion = getTypicalFeedbackQuestionForSession(typicalFeedbackSession);
         typicalFeedbackQuestion.setShowGiverNameTo(List.of(FeedbackParticipantType.INSTRUCTORS));
         typicalFeedbackQuestion.setShowRecipientNameTo(List.of(FeedbackParticipantType.INSTRUCTORS));
@@ -58,17 +64,6 @@ public class SubmitFeedbackResponsesActionTest extends BaseActionTest<SubmitFeed
     @Test
     void testAccessControl_missingParameters_throwsInvalidHttpParameterException() {
         verifyHttpParameterFailureAcl();
-    }
-
-    @Test
-    void testAccessControl_noIntent_throwsInvalidHttpParameterException() {
-        String[] params = {
-                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString()
-        };
-
-        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
-
-        verifyHttpParameterFailureAcl(params);
     }
 
     @Test
@@ -94,6 +89,22 @@ public class SubmitFeedbackResponsesActionTest extends BaseActionTest<SubmitFeed
     }
 
     @Test
+    void testAccessControl_moderatedQuestionCannotBeSeenByInstructor_throwsUnauthorizedAccessException() {
+        typicalFeedbackQuestion.setShowGiverNameTo(new ArrayList<>());
+        typicalFeedbackQuestion.setShowRecipientNameTo(new ArrayList<>());
+        typicalFeedbackQuestion.setShowResponsesTo(new ArrayList<>());
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
+                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
+                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, typicalInstructor.getEmail()
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+
+        verifyCannotAccess(params);
+    }
+
+    @Test
     void testAccessControl_previewRequest_throwsUnauthorizedAccessException() {
         String[] params = {
                 Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
@@ -107,7 +118,76 @@ public class SubmitFeedbackResponsesActionTest extends BaseActionTest<SubmitFeed
     }
 
     @Test
-    void testAccessControl_typicalCase_canAccess() {
+    void testAccessControl_noIntent_throwsInvalidHttpParameterException() {
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString()
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+
+        verifyHttpParameterFailureAcl(params);
+    }
+
+    @Test
+    void testAccessControl_instructorResultIntent_throwsInvalidHttpParameterException() {
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
+                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_RESULT.toString()
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+
+        verifyHttpParameterFailureAcl(params);
+    }
+
+    @Test
+    void testAccessControl_studentResultIntent_throwsInvalidHttpParameterException() {
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
+                Const.ParamsNames.INTENT, Intent.STUDENT_RESULT.toString()
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+
+        verifyHttpParameterFailureAcl(params);
+    }
+
+    @Test
+    void testAccessControl_fullDetailIntent_throwsInvalidHttpParameterException() {
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
+                Const.ParamsNames.INTENT, Intent.FULL_DETAIL.toString()
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+
+        verifyHttpParameterFailureAcl(params);
+    }
+
+    @Test
+    void testAccessControl_moderationRequestForStudentSubmission_canAccess() {
+        typicalFeedbackQuestion.setGiverType(FeedbackParticipantType.STUDENTS);
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
+                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
+                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, typicalStudent.getEmail()
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+        when(mockLogic.getStudentForEmail(typicalCourse.getId(), typicalStudent.getEmail()))
+                .thenReturn(typicalStudent);
+        when(mockLogic.getInstructorByGoogleId(typicalCourse.getId(), typicalInstructor.getGoogleId()))
+                .thenReturn(typicalInstructor);
+
+        loginAsInstructor(typicalInstructor.getGoogleId());
+
+        verifyCanAccess(params);
+    }
+
+    // More tests here
+
+    @Test
+    void testAccessControl_moderationRequestForInstructorSubmission_canAccess() {
         String[] params = {
                 Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
                 Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
@@ -119,6 +199,24 @@ public class SubmitFeedbackResponsesActionTest extends BaseActionTest<SubmitFeed
                 .thenReturn(typicalInstructor);
         when(mockLogic.getInstructorByGoogleId(typicalCourse.getId(), typicalInstructor.getGoogleId()))
                 .thenReturn(typicalInstructor);
+
+        loginAsInstructor(typicalInstructor.getGoogleId());
+
+        verifyCanAccess(params);
+    }
+
+    @Test
+    void testAccessControl_() {
+        String[] params = {
+                Const.ParamsNames.FEEDBACK_QUESTION_ID, typicalFeedbackQuestion.getId().toString(),
+                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
+        };
+
+        when(mockLogic.getFeedbackQuestion(typicalFeedbackQuestion.getId())).thenReturn(typicalFeedbackQuestion);
+        when(mockLogic.getInstructorByGoogleId(typicalCourse.getId(), typicalInstructor.getGoogleId()))
+                .thenReturn(typicalInstructor);
+        when(mockLogic.getDeadlineForUser(typicalFeedbackSession, typicalInstructor))
+                .thenReturn(typicalFeedbackSession.getEndTime());
 
         loginAsInstructor(typicalInstructor.getGoogleId());
 
