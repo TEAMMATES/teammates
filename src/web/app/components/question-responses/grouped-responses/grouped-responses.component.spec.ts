609714fb80fec692ac86b8f512ce81829b46aa22
ctxsmith <ctxsmith@umich.edu>
2025-04-23 16:39:22 -0400

    lint fixes

--- src/web/app/components/question-responses/grouped-responses/grouped-responses.component.spec.ts
+++ src/web/app/components/question-responses/grouped-responses/grouped-responses.component.spec.ts
@@ -1,13 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { GroupedResponsesComponent } from './grouped-responses.component';
 import {
-  ResponseModerationButtonModule,
-} from '../../../pages-instructor/instructor-session-result-page/response-moderation-button/response-moderation-button.module';
-import { CommentBoxModule } from '../../comment-box/comment-box.module';
-import { QuestionTextWithInfoModule } from '../../question-text-with-info/question-text-with-info.module';
-import { TeammatesCommonModule } from '../../teammates-common/teammates-common.module';
-import { SingleResponseModule } from '../single-response/single-response.module';
-import {
   FeedbackSession,
   FeedbackSessionPublishStatus,
   FeedbackSessionSubmissionStatus,
@@ -19,7 +12,14 @@ import {
   FeedbackParticipantType,
   NumberOfEntitiesToGiveFeedbackToSetting,
 } from '../../../../types/api-output';
+import {
+  ResponseModerationButtonModule,
+} from '../../../pages-instructor/instructor-session-result-page/response-moderation-button/response-moderation-button.module';
+import { CommentBoxModule } from '../../comment-box/comment-box.module';
 import { CommentTableModel } from '../../comment-box/comment-table/comment-table.component';
+import { QuestionTextWithInfoModule } from '../../question-text-with-info/question-text-with-info.module';
+import { TeammatesCommonModule } from '../../teammates-common/teammates-common.module';
+import { SingleResponseModule } from '../single-response/single-response.module';
 
 describe('GroupedResponsesComponent', () => {
   let component: GroupedResponsesComponent;
