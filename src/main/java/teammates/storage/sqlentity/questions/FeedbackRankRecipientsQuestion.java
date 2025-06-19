9c2561a69ab396c45e8120fb372b1997e851a540
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-06-19 13:01:51 +0800

    [#12048] Migrate Tests for FeedbackRankRecipientQuestionE2ETest (#13346)
    * Migrate FeedbackRankRecipientQuestionE2ETest
    
    * Fix makeDeepCopy in FeedbackRankRecipientQuestion

--- src/main/java/teammates/storage/sqlentity/questions/FeedbackRankRecipientsQuestion.java
+++ src/main/java/teammates/storage/sqlentity/questions/FeedbackRankRecipientsQuestion.java
@@ -51,7 +51,7 @@ public class FeedbackRankRecipientsQuestion extends FeedbackQuestion {
                 newFeedbackSession, this.getQuestionNumber(), this.getDescription(), this.getGiverType(),
                 this.getRecipientType(), this.getNumOfEntitiesToGiveFeedbackTo(), new ArrayList<>(this.getShowResponsesTo()),
                 new ArrayList<>(this.getShowGiverNameTo()), new ArrayList<>(this.getShowRecipientNameTo()),
-                new FeedbackRankRecipientsQuestionDetails(this.questionDetails.getQuestionText())
+                this.questionDetails.getDeepCopy()
         );
     }
 
