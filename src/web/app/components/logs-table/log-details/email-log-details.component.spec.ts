21f2709d85700d11bf58ab685b74688d714ef471
Florian Viorel Tanely <izkx73@gmail.com>
2025-12-15 22:30:27 +0700

    Add unit tests for EmailLogDetailsComponent

--- src/web/app/components/logs-table/log-details/email-log-details.component.spec.ts
+++ src/web/app/components/logs-table/log-details/email-log-details.component.spec.ts
@@ -1,25 +1,77 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EmailLogDetailsComponent } from './email-log-details.component';
+import {
+  EmailSentLogDetails,
+  EmailType,
+  GeneralLogEntry,
+  LogEvent,
+  LogSeverity,
+} from '../../../../types/api-output';
 
 describe('EmailLogDetailsComponent', () => {
   let component: EmailLogDetailsComponent;
   let fixture: ComponentFixture<EmailLogDetailsComponent>;
 
+  const baseExpectedEmailContent = 'This is a test email content.';
+  const baseInitialLogDetails = {
+    event: LogEvent.EMAIL_SENT,
+    message: 'Test email log details message',
+    emailRecipient: 'Foo',
+    emailSubject: 'This is a test subject',
+    emailContent: baseExpectedEmailContent,
+    emailType: EmailType.LOGIN,
+    emailStatus: 1,
+    emailStatusMessage: 'This is a test email status message',
+  } as EmailSentLogDetails;
+  const baseExpectedLogDetails = {
+    ...baseInitialLogDetails,
+    emailContent: undefined,
+  } as EmailSentLogDetails;
+  const baseExpectedLogValue: GeneralLogEntry = {
+    severity: LogSeverity.DEFAULT,
+    trace: '0123456789abcdef',
+    insertId: '0123456789abcdef',
+    resourceIdentifier: {
+      module_id: 'mock',
+      version_id: '1-0-0',
+      project_id: 'mock-project',
+      zone: 'mock-zone-1',
+    },
+    sourceLocation: {
+      file: 'com.mock.Mock',
+      line: 100,
+      function: 'handleEmail',
+    },
+    timestamp: 1000,
+    message: 'Test email log message',
+    details: baseInitialLogDetails,
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [EmailLogDetailsComponent],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(EmailLogDetailsComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('log', baseExpectedLogValue);
     fixture.detectChanges();
-  });
+    fixture.whenStable();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the input log as log value', () => {
+    expect(component.logValue).toEqual(baseExpectedLogValue);
+  });
+
+  it('should move email content from log details to the component attribute', () => {
+    expect(component.details).toEqual(baseExpectedLogDetails);
+    expect(component.emailContent).toEqual(baseExpectedEmailContent);
   });
 });
