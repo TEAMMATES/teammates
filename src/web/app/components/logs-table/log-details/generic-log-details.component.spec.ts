980187525fe94eef05b0c2bf59d220abacc77215
Florian Viorel Tanely <izkx73@gmail.com>
2025-12-15 22:29:36 +0700

    Add unit tests for GenericLogDetailsComponent

--- src/web/app/components/logs-table/log-details/generic-log-details.component.spec.ts
+++ src/web/app/components/logs-table/log-details/generic-log-details.component.spec.ts
@@ -1,25 +1,56 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GenericLogDetailsComponent } from './generic-log-details.component';
+import {
+  GeneralLogEntry,
+  LogEvent,
+  LogSeverity,
+} from '../../../../types/api-output';
 
 describe('GenericLogDetailsComponent', () => {
   let component: GenericLogDetailsComponent;
   let fixture: ComponentFixture<GenericLogDetailsComponent>;
+  const expectedLogValue: GeneralLogEntry = {
+    severity: LogSeverity.DEFAULT,
+    trace: '0123456789abcdef',
+    insertId: '0123456789abcdef',
+    resourceIdentifier: {
+      module_id: 'mock',
+      version_id: '1-0-0',
+      project_id: 'mock-project',
+      zone: 'mock-zone-1',
+    },
+    sourceLocation: {
+      file: 'com.mock.Mock',
+      line: 100,
+      function: 'handle',
+    },
+    timestamp: 1000,
+    message: 'Test general log message',
+    details: {
+      event: LogEvent.DEFAULT_LOG,
+      message: 'Test general log details message',
+    },
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [GenericLogDetailsComponent],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(GenericLogDetailsComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('log', expectedLogValue);
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the input log as log value', () => {
+    expect(component.logValue).toEqual(expectedLogValue);
   });
 });
