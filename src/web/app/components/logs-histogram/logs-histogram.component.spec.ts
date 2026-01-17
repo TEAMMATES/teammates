4ea62e352a546dc4259a42842fc9a6010bf32d31
ekamsinghahuja <ekamsinghahuja123@gmail.com>
2026-01-18 00:47:30 +0530

    Add unit tests for LogsHistogramComponent

--- src/web/app/components/logs-histogram/logs-histogram.component.spec.ts
+++ src/web/app/components/logs-histogram/logs-histogram.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LogsHistogramComponent } from './logs-histogram.component';
 
-describe('LogsHistogramComponent', () => {
+fdescribe('LogsHistogramComponent', () => {
   let component: LogsHistogramComponent;
   let fixture: ComponentFixture<LogsHistogramComponent>;
 
@@ -21,5 +21,40 @@ describe('LogsHistogramComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call drawBars on ngOnChanges when chart exists', () => {
+    const drawBarsSpy = jest
+      .spyOn(component as any, 'drawBars');
+
+    component.ngOnChanges();
+
+    expect(drawBarsSpy).toHaveBeenCalled();
+  });
+
+  it('should not call drawBars on ngOnChanges when chart does not exist', () => {
+    const drawBarsSpy = jest
+      .spyOn(component as any, 'drawBars');
+
+    component['chart'] = null;
+    component.ngOnChanges();
+
+    expect(drawBarsSpy).not.toHaveBeenCalled();
+  });
+
+  it('should call createSvg on ngOnInit', () => {
+    const createSvgSpy = jest.spyOn(component as any, 'createSvg');
+
+    component.ngOnInit();
+
+    expect(createSvgSpy).toHaveBeenCalled();
+  });
+
+  it('should call drawBars on ngOnInit', () => {
+    const drawBarsSpy = jest.spyOn(component as any, 'drawBars');
+
+    component.ngOnInit();
+
+    expect(drawBarsSpy).toHaveBeenCalled();
   });
 });
