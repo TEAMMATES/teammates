fb421eb9d000f77581bff17c8f09bbbfaf349806
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-01-19 13:31:40 +0800

    Revert "Fixes sorting of students"
    This reverts commit ed6bff773a26eadbce465846a56b0de5bfbc01e7.

--- src/e2e/java/teammates/e2e/cases/sql/InstructorSearchPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/sql/InstructorSearchPageE2ETest.java
@@ -1,7 +1,5 @@
 package teammates.e2e.cases.sql;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,112 +20,100 @@ import teammates.storage.sqlentity.Student;
  */
 public class InstructorSearchPageE2ETest extends BaseE2ETestCase {
 
-        @Override
-        protected void prepareTestData() {
-                if (!TestProperties.INCLUDE_SEARCH_TESTS) {
-                        return;
-                }
-                testData = doRemoveAndRestoreDataBundle(
-                                loadSqlDataBundle("/InstructorSearchPageE2ETestSql.json"));
-                putDocuments(testData);
+    @Override
+    protected void prepareTestData() {
+        if (!TestProperties.INCLUDE_SEARCH_TESTS) {
+            return;
+        }
+        testData = doRemoveAndRestoreDataBundle(
+                loadSqlDataBundle("/InstructorSearchPageE2ETestSql.json"));
+        putDocuments(testData);
+    }
+
+    @Test
+    @Override
+    public void testAll() {
+        if (!TestProperties.INCLUDE_SEARCH_TESTS) {
+            return;
         }
 
-        @Test
-        @Override
-        public void testAll() {
-                if (!TestProperties.INCLUDE_SEARCH_TESTS) {
-                        return;
-                }
+        String instructorId = testData.accounts.get("instructor1OfCourse1").getGoogleId();
+        AppUrl searchPageUrl = createFrontendUrl(Const.WebPageURIs.INSTRUCTOR_SEARCH_PAGE);
 
-                String instructorId = testData.accounts.get("instructor1OfCourse1").getGoogleId();
-                AppUrl searchPageUrl = createFrontendUrl(Const.WebPageURIs.INSTRUCTOR_SEARCH_PAGE);
+        InstructorSearchPageSql searchPage = loginToPage(searchPageUrl, InstructorSearchPageSql.class, instructorId);
 
-                InstructorSearchPageSql searchPage = loginToPage(searchPageUrl, InstructorSearchPageSql.class,
-                                instructorId);
+        ______TS("cannot click search button if no search term is entered");
 
-                ______TS("cannot click search button if no search term is entered");
+        searchPage.search("");
 
-                searchPage.search("");
+        ______TS("search with no result");
 
-                ______TS("search with no result");
+        searchPage.search("thiswillnothitanything");
+        searchPage.verifyStatusMessage("No results found.");
 
-                searchPage.search("thiswillnothitanything");
-                searchPage.verifyStatusMessage("No results found.");
+        ______TS("search for students");
 
-                ______TS("search for students");
+        searchPage.search("student2");
 
-                searchPage.search("student2");
+        Course course1 = testData.courses.get("typicalCourse1");
+        Course course2 = testData.courses.get("typicalCourse2");
 
-                Course course1 = testData.courses.get("typicalCourse1");
-                Course course2 = testData.courses.get("typicalCourse2");
+        Student[] studentsInCourse1 = {
+        testData.students.get("student2InCourse1"),
+        testData.students.get("student2.2InCourse1"),
+        };
+        Student[] studentsInCourse2 = {
+                testData.students.get("student2InCourse2"),
+                testData.students.get("student2.2InCourse2"),
+        };
 
-                // The table is sorted by section > team > name (ascending) by default.
-                Student[] studentsInCourse1 = sortBySectionTeamName(
-                                testData.students.get("student2InCourse1"),
-                                testData.students.get("student2.2InCourse1"));
-                Student[] studentsInCourse2 = sortBySectionTeamName(
-                                testData.students.get("student2InCourse2"),
-                                testData.students.get("student2.2InCourse2"));
+        Map<String, Student[]> courseIdToStudents = new HashMap<>();
+        courseIdToStudents.put(course1.getId(), studentsInCourse1);
+        courseIdToStudents.put(course2.getId(), studentsInCourse2);
 
-                Map<String, Student[]> courseIdToStudents = new HashMap<>();
-                courseIdToStudents.put(course1.getId(), studentsInCourse1);
-                courseIdToStudents.put(course2.getId(), studentsInCourse2);
+        Map<String, Course> courseIdToCourse = new HashMap<>();
+        courseIdToCourse.put(course1.getId(), course1);
+        courseIdToCourse.put(course2.getId(), course2);
 
-                Map<String, Course> courseIdToCourse = new HashMap<>();
-                courseIdToCourse.put(course1.getId(), course1);
-                courseIdToCourse.put(course2.getId(), course2);
+        searchPage.verifyStudentDetails(courseIdToCourse, courseIdToStudents);
 
-                searchPage.verifyStudentDetails(courseIdToCourse, courseIdToStudents);
+        ______TS("link: view student details page");
 
-                ______TS("link: view student details page");
+        Student studentToView = testData.students.get("student2.2InCourse1");
+        String studentEmail = studentToView.getEmail();
 
-                Student studentToView = testData.students.get("student2.2InCourse1");
-                String studentEmail = studentToView.getEmail();
+        InstructorCourseStudentDetailsViewPageSql studentDetailsViewPage =
+                searchPage.clickViewStudent(course1, studentEmail);
+        studentDetailsViewPage.verifyIsCorrectPage(course1.getId(), studentEmail);
+        studentDetailsViewPage.closeCurrentWindowAndSwitchToParentWindow();
 
-                InstructorCourseStudentDetailsViewPageSql studentDetailsViewPage = searchPage.clickViewStudent(course1,
-                                studentEmail);
-                studentDetailsViewPage.verifyIsCorrectPage(course1.getId(), studentEmail);
-                studentDetailsViewPage.closeCurrentWindowAndSwitchToParentWindow();
+        ______TS("link: edit student details page");
 
-                ______TS("link: edit student details page");
+        InstructorCourseStudentDetailsEditPageSql studentDetailsEditPage =
+                searchPage.clickEditStudent(course1, studentEmail);
+        studentDetailsEditPage.verifyIsCorrectPage(course1.getId(), studentEmail);
+        studentDetailsEditPage.closeCurrentWindowAndSwitchToParentWindow();
 
-                InstructorCourseStudentDetailsEditPageSql studentDetailsEditPage = searchPage.clickEditStudent(course1,
-                                studentEmail);
-                studentDetailsEditPage.verifyIsCorrectPage(course1.getId(), studentEmail);
-                studentDetailsEditPage.closeCurrentWindowAndSwitchToParentWindow();
+        ______TS("link: view all records page");
 
-                ______TS("link: view all records page");
+        InstructorStudentRecordsPageSql studentRecordsPage =
+                searchPage.clickViewAllRecords(course1, studentEmail);
+        studentRecordsPage.verifyIsCorrectPage(course1.getId(), studentToView.getName());
+        studentRecordsPage.closeCurrentWindowAndSwitchToParentWindow();
 
-                InstructorStudentRecordsPageSql studentRecordsPage = searchPage.clickViewAllRecords(course1,
-                                studentEmail);
-                studentRecordsPage.verifyIsCorrectPage(course1.getId(), studentToView.getName());
-                studentRecordsPage.closeCurrentWindowAndSwitchToParentWindow();
+        ______TS("action: delete student");
 
-                ______TS("action: delete student");
+        Student studentToDelete = testData.students.get("student2InCourse2");
 
-                Student studentToDelete = testData.students.get("student2InCourse2");
+        searchPage.deleteStudent(course2, studentToDelete.getEmail());
 
-                searchPage.deleteStudent(course2, studentToDelete.getEmail());
+        Student[] studentsAfterDelete = {
+                testData.students.get("student2.2InCourse2"),
+        };
 
-                Student[] studentsAfterDelete = {
-                                testData.students.get("student2.2InCourse2"),
-                };
+        searchPage.verifyStudentDetails(course2, studentsAfterDelete);
+        verifyAbsentInDatabase(studentToDelete);
 
-                searchPage.verifyStudentDetails(course2, studentsAfterDelete);
-                verifyAbsentInDatabase(studentToDelete);
-
-        }
-
-        private Student[] sortBySectionTeamName(Student... students) {
-                return Arrays.stream(students)
-                                .sorted(Comparator
-                                                .comparing(Student::getSectionName,
-                                                                Comparator.nullsFirst(Comparator.naturalOrder()))
-                                                .thenComparing(Student::getTeamName,
-                                                                Comparator.nullsFirst(Comparator.naturalOrder()))
-                                                .thenComparing(Student::getName,
-                                                                Comparator.nullsFirst(Comparator.naturalOrder())))
-                                .toArray(Student[]::new);
-        }
+    }
 
 }
