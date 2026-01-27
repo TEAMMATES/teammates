476acd26c18b608a33489ed38d238a4e53ed02d4
YongJunXi <ernestyongjunxi@gmail.com>
2026-01-27 10:51:50 +0800

    Revert non-sql test to hard-coded version

--- src/e2e/java/teammates/e2e/cases/StudentNotificationsPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/StudentNotificationsPageE2ETest.java
@@ -1,6 +1,5 @@
 package teammates.e2e.cases;
 
-import java.time.Instant;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,7 +7,6 @@ import java.util.stream.Stream;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
-import teammates.common.datatransfer.NotificationTargetUser;
 import teammates.common.util.AppUrl;
 import teammates.common.util.Const;
 import teammates.e2e.pageobjects.StudentNotificationsPage;
@@ -40,13 +38,16 @@ public class StudentNotificationsPageE2ETest extends BaseE2ETestCase {
                 account.getGoogleId());
 
         ______TS("verify that only active notifications with correct target user are shown");
-        Notification[] notShownNotifications = sqlTestData.notifications.values().stream()
-                .filter(notification -> !shouldBeShown(notification))
-                .toArray(Notification[]::new);
+        Notification[] notShownNotifications = {
+                sqlTestData.notifications.get("notification3"),
+                sqlTestData.notifications.get("expiredNotification1"),
+        };
 
-        Notification[] shownNotifications = sqlTestData.notifications.values().stream()
-                .filter(notification -> shouldBeShown(notification))
-                .toArray(Notification[]::new);
+        Notification[] shownNotifications = {
+                sqlTestData.notifications.get("notification1"),
+                sqlTestData.notifications.get("notification2"),
+                sqlTestData.notifications.get("notification4"),
+        };
 
         ReadNotification[] readNotifications = sqlTestData.readNotifications.values().toArray(ReadNotification[]::new);
 
@@ -68,23 +69,6 @@ public class StudentNotificationsPageE2ETest extends BaseE2ETestCase {
 
         ______TS("notification banner is not visible");
         assertFalse(notificationsPage.isBannerVisible());
-    }
-
-    private boolean shouldBeShown(Notification notification) {
-        return isTargetedToStudents(notification.getTargetUser()) && !isExpired(notification);
-    }
-
-    private boolean isExpired(Notification notification) {
-        Instant now = Instant.now();
-        Instant endTime = notification.getEndTime();
-        if (endTime == null) {
-            return false;
-        }
-        return endTime.isBefore(now);
-    }
-
-    private boolean isTargetedToStudents(NotificationTargetUser targetUser) {
-        return targetUser == NotificationTargetUser.STUDENT || targetUser == NotificationTargetUser.GENERAL;
     }
 
     @AfterClass
