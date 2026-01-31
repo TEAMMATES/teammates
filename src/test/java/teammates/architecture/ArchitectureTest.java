a4feeb42543d4d0eb8b08b710b8424429f5ca608
AaronToh <aarontywei@gmail.com>
2026-01-31 17:00:56 +0800

    Fix lint

--- src/test/java/teammates/architecture/ArchitectureTest.java
+++ src/test/java/teammates/architecture/ArchitectureTest.java
@@ -525,7 +525,9 @@ public class ArchitectureTest {
                 .and().doNotHaveSimpleName("AccountRequestSearchManager")
                 .and().doNotHaveSimpleName("InstructorSearchManager")
                 .and().doNotHaveSimpleName("StudentSearchManager")
-                .and().doNotHaveSimpleName("AccountSearchManagerTest")
+                .and().doNotHaveSimpleName("AccountRequestSearchManagerTest")
+                .and().doNotHaveSimpleName("InstructorSearchManagerTest")
+                .and().doNotHaveSimpleName("StudentSearchManagerTest")
                 .should().accessClassesThat().resideInAPackage("org.apache.solr..")
                 .check(ALL_CLASSES);
     }
