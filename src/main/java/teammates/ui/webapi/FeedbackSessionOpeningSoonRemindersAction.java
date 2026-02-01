40a36a3c9f4c4319ccbc257d617cf91f396d0320
AaronToh <aarontywei@gmail.com>
2026-02-01 23:21:41 +0800

    Migrating FeedbackSessionOpeningSoonRemindersAction.java

--- src/main/java/teammates/ui/webapi/FeedbackSessionOpeningSoonRemindersAction.java
+++ src/main/java/teammates/ui/webapi/FeedbackSessionOpeningSoonRemindersAction.java
@@ -2,7 +2,6 @@ package teammates.ui.webapi;
 
 import java.util.List;
 
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.util.EmailWrapper;
 import teammates.common.util.Logger;
 import teammates.common.util.RequestTracer;
@@ -16,28 +15,6 @@ public class FeedbackSessionOpeningSoonRemindersAction extends AdminOnlyAction {
 
     @Override
     public JsonResult execute() {
-        List<FeedbackSessionAttributes> sessionAttributes = logic.getFeedbackSessionsOpeningWithinTimeLimit();
-        for (FeedbackSessionAttributes session : sessionAttributes) {
-            // If course has been migrated, use sql email logic instead.
-            if (isCourseMigrated(session.getCourseId())) {
-                continue;
-            }
-
-            RequestTracer.checkRemainingTime();
-            List<EmailWrapper> emailsToBeSent = emailGenerator.generateFeedbackSessionOpeningSoonEmails(session);
-            try {
-                taskQueuer.scheduleEmailsForSending(emailsToBeSent);
-
-                logic.updateFeedbackSession(
-                        FeedbackSessionAttributes
-                                .updateOptionsBuilder(session.getFeedbackSessionName(), session.getCourseId())
-                                .withSentOpeningSoonEmail(true)
-                                .build());
-            } catch (Exception e) {
-                log.severe("Unexpected error", e);
-            }
-        }
-
         List<FeedbackSession> sessions = sqlLogic.getFeedbackSessionsOpeningWithinTimeLimit();
         for (FeedbackSession session : sessions) {
             RequestTracer.checkRemainingTime();
