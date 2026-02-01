556d8095a70a697160d7fdfd8b4b729166a46a44
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-02-01 16:01:17 +0800

    Migrating SearchManagerFactory.java

--- src/main/java/teammates/storage/search/SearchManagerFactory.java
+++ src/main/java/teammates/storage/search/SearchManagerFactory.java
@@ -5,26 +5,11 @@ package teammates.storage.search;
  */
 public final class SearchManagerFactory {
 
-    private static InstructorSearchManager instructorInstance;
     private static StudentSearchManager studentInstance;
     private static AccountRequestSearchManager accountRequestInstance;
 
     private SearchManagerFactory() {
         // prevents initialization
-    }
-
-    public static InstructorSearchManager getInstructorSearchManager() {
-        return instructorInstance;
-    }
-
-    /**
-     * Registers the instructor search service into the factory.
-     */
-    @SuppressWarnings("PMD.NonThreadSafeSingleton") // ok to ignore as method is only invoked at application startup
-    public static void registerInstructorSearchManager(InstructorSearchManager instructorSearchManager) {
-        if (instructorInstance == null) {
-            instructorInstance = instructorSearchManager;
-        }
     }
 
     public static StudentSearchManager getStudentSearchManager() {
