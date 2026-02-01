06a7de3906f4fa8e6f9bb6f7ac64d840f5d3779d
AaronToh <aarontywei@gmail.com>
2026-02-01 23:14:55 +0800

    Migrating DeleteFeedbackResponseCommentAction.java

--- src/main/java/teammates/ui/webapi/DeleteFeedbackResponseCommentAction.java
+++ src/main/java/teammates/ui/webapi/DeleteFeedbackResponseCommentAction.java
@@ -1,12 +1,6 @@
 package teammates.ui.webapi;
 
 import teammates.common.datatransfer.FeedbackParticipantType;
-import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
-import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
-import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
-import teammates.common.datatransfer.attributes.InstructorAttributes;
-import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
 import teammates.storage.sqlentity.FeedbackQuestion;
 import teammates.storage.sqlentity.FeedbackResponse;
@@ -29,70 +23,12 @@ public class DeleteFeedbackResponseCommentAction extends BasicCommentSubmissionA
     @Override
     void checkSpecificAccessControl() throws UnauthorizedAccessException {
         long feedbackResponseCommentId = getLongRequestParamValue(Const.ParamsNames.FEEDBACK_RESPONSE_COMMENT_ID);
-        FeedbackResponseCommentAttributes frc = logic.getFeedbackResponseComment(feedbackResponseCommentId);
         FeedbackResponseComment comment = sqlLogic.getFeedbackResponseComment(feedbackResponseCommentId);
 
         String courseId;
-        if (frc != null) {
-            courseId = frc.getCourseId();
-        } else if (comment != null) {
+        if (comment != null) {
             courseId = comment.getFeedbackResponse().getFeedbackQuestion().getCourseId();
         } else {
-            return;
-        }
-
-        if (!isCourseMigrated(courseId)) {
-            FeedbackSessionAttributes session = getNonNullFeedbackSession(frc.getFeedbackSessionName(), frc.getCourseId());
-            FeedbackQuestionAttributes question = logic.getFeedbackQuestion(frc.getFeedbackQuestionId());
-
-            Intent intent = Intent.valueOf(getNonNullRequestParamValue(Const.ParamsNames.INTENT));
-
-            switch (intent) {
-            case STUDENT_SUBMISSION:
-                StudentAttributes student = getStudentOfCourseFromRequest(courseId);
-
-                gateKeeper.verifyAnswerableForStudent(question);
-                verifyInstructorCanSeeQuestionIfInModeration(question);
-                verifyNotPreview();
-
-                checkAccessControlForStudentFeedbackSubmission(student, session);
-                session = session.getCopyForStudent(student.getEmail());
-                verifySessionOpenExceptForModeration(session);
-                gateKeeper.verifyOwnership(frc,
-                        question.getGiverType() == FeedbackParticipantType.TEAMS
-                                ? student.getTeam() : student.getEmail());
-                break;
-            case INSTRUCTOR_SUBMISSION:
-                InstructorAttributes instructorAsFeedbackParticipant = getInstructorOfCourseFromRequest(courseId);
-
-                gateKeeper.verifyAnswerableForInstructor(question);
-                verifyInstructorCanSeeQuestionIfInModeration(question);
-                verifyNotPreview();
-
-                checkAccessControlForInstructorFeedbackSubmission(instructorAsFeedbackParticipant, session);
-                session = session.getCopyForInstructor(instructorAsFeedbackParticipant.getEmail());
-                verifySessionOpenExceptForModeration(session);
-                gateKeeper.verifyOwnership(frc, instructorAsFeedbackParticipant.getEmail());
-                break;
-            case INSTRUCTOR_RESULT:
-                gateKeeper.verifyLoggedInUserPrivileges(userInfo);
-                InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.getId());
-                if (instructor == null) {
-                    throw new UnauthorizedAccessException("Trying to access system using a non-existent instructor entity");
-                }
-                if (frc.getCommentGiver().equals(instructor.getEmail())) { // giver, allowed by default
-                    return;
-                }
-
-                FeedbackResponseAttributes response = logic.getFeedbackResponse(frc.getFeedbackResponseId());
-                gateKeeper.verifyAccessible(instructor, session, response.getGiverSection(),
-                        Const.InstructorPermissions.CAN_MODIFY_SESSION_COMMENT_IN_SECTIONS);
-                gateKeeper.verifyAccessible(instructor, session, response.getRecipientSection(),
-                        Const.InstructorPermissions.CAN_MODIFY_SESSION_COMMENT_IN_SECTIONS);
-                break;
-            default:
-                throw new InvalidHttpParameterException("Unknown intent " + intent);
-            }
             return;
         }
 
@@ -153,25 +89,15 @@ public class DeleteFeedbackResponseCommentAction extends BasicCommentSubmissionA
     public JsonResult execute() {
         long feedbackResponseCommentId = getLongRequestParamValue(Const.ParamsNames.FEEDBACK_RESPONSE_COMMENT_ID);
 
-        FeedbackResponseCommentAttributes frc = logic.getFeedbackResponseComment(feedbackResponseCommentId);
         FeedbackResponseComment comment = sqlLogic.getFeedbackResponseComment(feedbackResponseCommentId);
 
         JsonResult successfulJsonResult = new JsonResult("Successfully deleted feedback response comment.");
 
-        String courseId;
-        if (frc != null) {
-            courseId = frc.getCourseId();
-        } else if (comment != null) {
-            courseId = comment.getFeedbackResponse().getFeedbackQuestion().getCourseId();
-        } else {
+        if (comment == null) {
             return successfulJsonResult;
         }
 
-        if (isCourseMigrated(courseId)) {
-            sqlLogic.deleteFeedbackResponseComment(feedbackResponseCommentId);
-        } else {
-            logic.deleteFeedbackResponseComment(feedbackResponseCommentId);
-        }
+        sqlLogic.deleteFeedbackResponseComment(feedbackResponseCommentId);
 
         return successfulJsonResult;
     }
