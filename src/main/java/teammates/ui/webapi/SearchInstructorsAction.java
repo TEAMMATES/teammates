3cc5b38e001cfc6dc1dc82eb34391d48d9fe7687
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-02-01 18:23:06 +0800

    Remove unnecessary import

--- src/main/java/teammates/ui/webapi/SearchInstructorsAction.java
+++ src/main/java/teammates/ui/webapi/SearchInstructorsAction.java
@@ -3,7 +3,6 @@ package teammates.ui.webapi;
 import java.util.ArrayList;
 import java.util.List;
 
-import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.exception.SearchServiceException;
 import teammates.common.util.Const;
 import teammates.storage.sqlentity.Instructor;
