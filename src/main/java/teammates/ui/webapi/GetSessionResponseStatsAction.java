cc3964d4e4f9be356f8cbeeec8c2a5f394c6ecc9
AaronToh <aarontywei@gmail.com>
2026-02-01 23:31:14 +0800

    Migrating GetSessionResponseStatsAction.java

--- src/main/java/teammates/ui/webapi/GetSessionResponseStatsAction.java
+++ src/main/java/teammates/ui/webapi/GetSessionResponseStatsAction.java
@@ -1,7 +1,5 @@
 package teammates.ui.webapi;
 
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
-import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.util.Const;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.storage.sqlentity.Instructor;
@@ -25,15 +23,9 @@ public class GetSessionResponseStatsAction extends Action {
 
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getNonNullRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
-        if (isCourseMigrated(courseId)) {
-            FeedbackSession fs = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
-            Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.getId());
-            gateKeeper.verifyAccessible(instructor, fs);
-        } else {
-            FeedbackSessionAttributes fsa = getNonNullFeedbackSession(feedbackSessionName, courseId);
-            InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.getId());
-            gateKeeper.verifyAccessible(instructor, fsa);
-        }
+        FeedbackSession fs = getNonNullFeedbackSession(feedbackSessionName, courseId);
+        Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.getId());
+        gateKeeper.verifyAccessible(instructor, fs);
     }
 
     @Override
@@ -41,19 +33,11 @@ public class GetSessionResponseStatsAction extends Action {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getNonNullRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
 
-        if (isCourseMigrated(courseId)) {
-            FeedbackSession fsa = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
-            int expectedTotal = sqlLogic.getExpectedTotalSubmission(fsa);
-            int actualTotal = sqlLogic.getActualTotalSubmission(fsa);
-            FeedbackSessionStatsData output = new FeedbackSessionStatsData(actualTotal, expectedTotal);
-            return new JsonResult(output);
-        } else {
-            FeedbackSessionAttributes fsa = getNonNullFeedbackSession(feedbackSessionName, courseId);
-            int expectedTotal = logic.getExpectedTotalSubmission(fsa);
-            int actualTotal = logic.getActualTotalSubmission(fsa);
-            FeedbackSessionStatsData output = new FeedbackSessionStatsData(actualTotal, expectedTotal);
-            return new JsonResult(output);
-        }
+        FeedbackSession fsa = getNonNullFeedbackSession(feedbackSessionName, courseId);
+        int expectedTotal = sqlLogic.getExpectedTotalSubmission(fsa);
+        int actualTotal = sqlLogic.getActualTotalSubmission(fsa);
+        FeedbackSessionStatsData output = new FeedbackSessionStatsData(actualTotal, expectedTotal);
+        return new JsonResult(output);
     }
 
 }
