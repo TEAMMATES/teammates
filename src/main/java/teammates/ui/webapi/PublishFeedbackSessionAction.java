8a405e7b9badc39bf93610f9cc44eafdcc5a3f2b
AaronToh <aarontywei@gmail.com>
2026-02-01 23:32:00 +0800

    Migrating PublishFeedbackSessionAction.java

--- src/main/java/teammates/ui/webapi/PublishFeedbackSessionAction.java
+++ src/main/java/teammates/ui/webapi/PublishFeedbackSessionAction.java
@@ -2,8 +2,6 @@ package teammates.ui.webapi;
 
 import org.apache.http.HttpStatus;
 
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
-import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.util.Const;
@@ -29,18 +27,8 @@ public class PublishFeedbackSessionAction extends Action {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getNonNullRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
 
-        if (!isCourseMigrated(courseId)) {
-            InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.getId());
-            FeedbackSessionAttributes feedbackSession = getNonNullFeedbackSession(feedbackSessionName, courseId);
-
-            gateKeeper.verifyAccessible(instructor, feedbackSession,
-                    Const.InstructorPermissions.CAN_MODIFY_SESSION);
-
-            return;
-        }
-
         Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.getId());
-        FeedbackSession feedbackSession = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
+        FeedbackSession feedbackSession = getNonNullFeedbackSession(feedbackSessionName, courseId);
         gateKeeper.verifyAccessible(instructor, feedbackSession,
                 Const.InstructorPermissions.CAN_MODIFY_SESSION);
     }
@@ -50,11 +38,7 @@ public class PublishFeedbackSessionAction extends Action {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String feedbackSessionName = getNonNullRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
 
-        if (!isCourseMigrated(courseId)) {
-            return publishOldFeedbackSession(courseId, feedbackSessionName);
-        }
-
-        FeedbackSession feedbackSession = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
+        FeedbackSession feedbackSession = getNonNullFeedbackSession(feedbackSessionName, courseId);
         if (feedbackSession.isPublished()) {
             // If feedback session was already published to begin with, return early
             return new JsonResult(new FeedbackSessionData(feedbackSession));
@@ -65,31 +49,6 @@ public class PublishFeedbackSessionAction extends Action {
 
             if (publishFeedbackSession.isPublishedEmailEnabled()) {
                 taskQueuer.scheduleFeedbackSessionPublishedEmail(courseId, feedbackSessionName);
-            }
-
-            return new JsonResult(new FeedbackSessionData(publishFeedbackSession));
-        } catch (EntityDoesNotExistException e) {
-            throw new EntityNotFoundException(e);
-        } catch (InvalidParametersException e) {
-            // There should not be any invalid parameter here
-            log.severe("Unexpected error", e);
-            return new JsonResult(e.getMessage(), HttpStatus.SC_INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    private JsonResult publishOldFeedbackSession(String courseId, String feedbackSessionName) {
-        FeedbackSessionAttributes feedbackSession = getNonNullFeedbackSession(feedbackSessionName, courseId);
-        if (feedbackSession.isPublished()) {
-            // If feedback session was already published to begin with, return early
-            return new JsonResult(new FeedbackSessionData(feedbackSession));
-        }
-
-        try {
-            FeedbackSessionAttributes publishFeedbackSession = logic.publishFeedbackSession(feedbackSessionName, courseId);
-
-            if (publishFeedbackSession.isPublishedEmailEnabled()) {
-                taskQueuer.scheduleFeedbackSessionPublishedEmail(publishFeedbackSession.getCourseId(),
-                        publishFeedbackSession.getFeedbackSessionName());
             }
 
             return new JsonResult(new FeedbackSessionData(publishFeedbackSession));
