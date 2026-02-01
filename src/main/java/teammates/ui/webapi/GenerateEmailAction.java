e54ca497689a312fc8921f925b4e5415a516461c
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-02-01 18:55:58 +0800

    Migrating GenerateEmailAction.java

--- src/main/java/teammates/ui/webapi/GenerateEmailAction.java
+++ src/main/java/teammates/ui/webapi/GenerateEmailAction.java
@@ -3,9 +3,6 @@ package teammates.ui.webapi;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import teammates.common.datatransfer.attributes.CourseAttributes;
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
-import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.EmailType;
 import teammates.common.util.EmailWrapper;
@@ -22,39 +19,6 @@ public class GenerateEmailAction extends AdminOnlyAction {
     @Override
     public JsonResult execute() {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
-
-        if (!isCourseMigrated(courseId)) {
-            CourseAttributes course = logic.getCourse(courseId);
-            if (course == null) {
-                throw new EntityNotFoundException("Course with ID " + courseId + " does not exist!");
-            }
-
-            String studentEmail = getNonNullRequestParamValue(Const.ParamsNames.STUDENT_EMAIL);
-            StudentAttributes student = logic.getStudentForEmail(courseId, studentEmail);
-            if (student == null) {
-                throw new EntityNotFoundException("Student does not exist.");
-            }
-
-            String emailType = getNonNullRequestParamValue(Const.ParamsNames.EMAIL_TYPE);
-            String feedbackSessionName = getRequestParamValue(Const.ParamsNames.FEEDBACK_SESSION_NAME);
-
-            EmailWrapper email;
-
-            if (emailType.equals(EmailType.STUDENT_COURSE_JOIN.name())) {
-                email = emailGenerator.generateStudentCourseJoinEmail(course, student);
-            } else if (emailType.equals(EmailType.FEEDBACK_SESSION_REMINDER.name())) {
-                if (feedbackSessionName == null) {
-                    throw new InvalidHttpParameterException("Feedback session name not specified");
-                }
-                FeedbackSessionAttributes feedbackSession = getNonNullFeedbackSession(feedbackSessionName, courseId);
-                email = emailGenerator.generateFeedbackSessionReminderEmails(
-                        feedbackSession, Collections.singletonList(student), new ArrayList<>(), null).get(0);
-            } else {
-                throw new InvalidHttpParameterException("Email type " + emailType + " not accepted");
-            }
-
-            return new JsonResult(new EmailData(email));
-        }
 
         Course course = sqlLogic.getCourse(courseId);
         if (course == null) {
