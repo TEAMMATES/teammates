92b216a69093d85cb5b27047a8919c827e64f3ae
Florian Viorel Tanely <izkx73@gmail.com>
2026-02-03 01:25:17 +0800

    Migrating GetDeadlineExtensionAction.java

--- src/main/java/teammates/ui/webapi/GetDeadlineExtensionAction.java
+++ src/main/java/teammates/ui/webapi/GetDeadlineExtensionAction.java
@@ -2,7 +2,6 @@ package teammates.ui.webapi;
 
 import java.time.Instant;
 
-import teammates.common.datatransfer.attributes.DeadlineExtensionAttributes;
 import teammates.common.util.Config;
 import teammates.common.util.Const;
 import teammates.storage.sqlentity.FeedbackSession;
@@ -33,40 +32,24 @@ public class GetDeadlineExtensionAction extends Action {
         String userEmail = getNonNullRequestParamValue(Const.ParamsNames.USER_EMAIL);
         boolean isInstructor = Boolean.parseBoolean(getNonNullRequestParamValue(Const.ParamsNames.IS_INSTRUCTOR));
 
-        if (isCourseMigrated(courseId)) {
-            FeedbackSession feedbackSession = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
-            User user = isInstructor
-                    ? sqlLogic.getInstructorForEmail(courseId, userEmail)
-                    : sqlLogic.getStudentForEmail(courseId, userEmail);
+        FeedbackSession feedbackSession = getNonNullSqlFeedbackSession(feedbackSessionName, courseId);
+        User user = isInstructor
+                ? sqlLogic.getInstructorForEmail(courseId, userEmail)
+                : sqlLogic.getStudentForEmail(courseId, userEmail);
 
-            Instant deadlineExtensionEndTime = sqlLogic.getExtendedDeadlineForUser(feedbackSession, user);
+        Instant deadlineExtensionEndTime = sqlLogic.getExtendedDeadlineForUser(feedbackSession, user);
 
-            if (deadlineExtensionEndTime == null) {
-                throw new EntityNotFoundException(
-                        "Deadline extension for course id: " + courseId
-                        + " and feedback session name: " + feedbackSessionName
-                        + " and " + (isInstructor ? "instructor" : "student")
-                        + " email: " + userEmail
-                        + " not found.");
-            }
-
-            // set sentClosingSoonEmail as false by default since it is removed.
-            return new JsonResult(new DeadlineExtensionData(courseId, feedbackSessionName,
-                    userEmail, isInstructor, false, deadlineExtensionEndTime));
-        } else {
-            DeadlineExtensionAttributes deadlineExtension =
-                    logic.getDeadlineExtension(courseId, feedbackSessionName, userEmail, isInstructor);
-
-            if (deadlineExtension == null) {
-                throw new EntityNotFoundException(
-                        "Deadline extension for course id: " + courseId
-                        + " and feedback session name: " + feedbackSessionName
-                        + " and " + (isInstructor ? "instructor" : "student")
-                        + " email: " + userEmail
-                        + " not found.");
-            }
-
-            return new JsonResult(new DeadlineExtensionData(deadlineExtension));
+        if (deadlineExtensionEndTime == null) {
+            throw new EntityNotFoundException(
+                    "Deadline extension for course id: " + courseId
+                    + " and feedback session name: " + feedbackSessionName
+                    + " and " + (isInstructor ? "instructor" : "student")
+                    + " email: " + userEmail
+                    + " not found.");
         }
+
+        // set sentClosingSoonEmail as false by default since it is removed.
+        return new JsonResult(new DeadlineExtensionData(courseId, feedbackSessionName,
+                userEmail, isInstructor, false, deadlineExtensionEndTime));
     }
 }
