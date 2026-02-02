59bec2f05ffa597938dca205e9f1c45b72954cf4
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-02-02 17:17:05 +0800

    Migrating UpdateStudentAction.java

--- src/main/java/teammates/ui/webapi/UpdateStudentAction.java
+++ src/main/java/teammates/ui/webapi/UpdateStudentAction.java
@@ -2,8 +2,6 @@ package teammates.ui.webapi;
 
 import java.util.Arrays;
 
-import teammates.common.datatransfer.attributes.InstructorAttributes;
-import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.exception.EnrollException;
 import teammates.common.exception.EntityAlreadyExistsException;
 import teammates.common.exception.EntityDoesNotExistException;
@@ -58,25 +56,15 @@ public class UpdateStudentAction extends Action {
         }
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
 
-        if (isCourseMigrated(courseId)) {
-            Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.getId());
-            gateKeeper.verifyAccessible(
-                    instructor, sqlLogic.getCourse(courseId), Const.InstructorPermissions.CAN_MODIFY_STUDENT);
-        } else {
-            InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, userInfo.id);
-            gateKeeper.verifyAccessible(
-                    instructor, logic.getCourse(courseId), Const.InstructorPermissions.CAN_MODIFY_STUDENT);
-        }
+        Instructor instructor = sqlLogic.getInstructorByGoogleId(courseId, userInfo.id);
+        gateKeeper.verifyAccessible(
+                instructor, sqlLogic.getCourse(courseId), Const.InstructorPermissions.CAN_MODIFY_STUDENT);
     }
 
     @Override
     public JsonResult execute() throws InvalidHttpRequestBodyException, InvalidOperationException {
         String courseId = getNonNullRequestParamValue(Const.ParamsNames.COURSE_ID);
         String studentEmail = getNonNullRequestParamValue(Const.ParamsNames.STUDENT_EMAIL);
-
-        if (!isCourseMigrated(courseId)) {
-            return executeWithDatastore(courseId, studentEmail);
-        }
 
         Student existingStudent = sqlLogic.getStudentForEmail(courseId, studentEmail);
         if (existingStudent == null) {
@@ -122,79 +110,16 @@ public class UpdateStudentAction extends Action {
         return new JsonResult(SUCCESSFUL_UPDATE);
     }
 
-    private JsonResult executeWithDatastore(String courseId, String studentEmail)
-            throws InvalidHttpRequestBodyException, InvalidOperationException {
-        StudentAttributes student = logic.getStudentForEmail(courseId, studentEmail);
-        if (student == null) {
-            throw new EntityNotFoundException(STUDENT_NOT_FOUND_FOR_EDIT);
-        }
-
-        StudentUpdateRequest updateRequest = getAndValidateRequestBody(StudentUpdateRequest.class);
-        StudentAttributes studentToUpdate = StudentAttributes.builder(courseId, updateRequest.getEmail())
-                .withName(updateRequest.getName())
-                .withSectionName(updateRequest.getSection())
-                .withTeamName(updateRequest.getTeam())
-                .withComment(updateRequest.getComments())
-                .build();
-
-        try {
-            //we swap out email before we validate
-            //TODO: this is duct tape at the moment, need to refactor how we do the validation
-            String newEmail = studentToUpdate.getEmail();
-            studentToUpdate.setEmail(student.getEmail());
-            logic.validateSectionsAndTeams(Arrays.asList(studentToUpdate), student.getCourse());
-            studentToUpdate.setEmail(newEmail);
-
-            StudentAttributes updatedStudent = logic.updateStudentCascade(
-                    StudentAttributes.updateOptionsBuilder(courseId, studentEmail)
-                            .withName(updateRequest.getName())
-                            .withNewEmail(updateRequest.getEmail())
-                            .withTeamName(updateRequest.getTeam())
-                            .withSectionName(updateRequest.getSection())
-                            .withComment(updateRequest.getComments())
-                            .build());
-            taskQueuer.scheduleStudentForSearchIndexing(updatedStudent.getCourse(), updatedStudent.getEmail());
-
-            if (!student.getEmail().equals(updateRequest.getEmail())) {
-                logic.resetStudentGoogleId(updateRequest.getEmail(), courseId);
-
-                if (updateRequest.getIsSessionSummarySendEmail()) {
-                    boolean emailSent = sendEmail(courseId, updateRequest.getEmail());
-                    String statusMessage = emailSent ? SUCCESSFUL_UPDATE_WITH_EMAIL
-                            : SUCCESSFUL_UPDATE_BUT_EMAIL_FAILED;
-                    return new JsonResult(statusMessage);
-                }
-            }
-        } catch (EnrollException e) {
-            throw new InvalidOperationException(e);
-        } catch (InvalidParametersException e) {
-            throw new InvalidHttpRequestBodyException(e);
-        } catch (EntityDoesNotExistException ednee) {
-            throw new EntityNotFoundException(ednee);
-        } catch (EntityAlreadyExistsException e) {
-            throw new InvalidOperationException("Trying to update to an email that is already in use", e);
-        }
-
-        return new JsonResult(SUCCESSFUL_UPDATE);
-    }
-
     /**
      * Sends the feedback session summary as an email.
      *
      * @return The true if email was sent successfully or false otherwise.
      */
     private boolean sendEmail(String courseId, String studentEmail) {
-        if (isCourseMigrated(courseId)) {
-            EmailWrapper email = sqlEmailGenerator.generateFeedbackSessionSummaryOfCourse(
-                    courseId, studentEmail, EmailType.STUDENT_EMAIL_CHANGED);
-            EmailSendingStatus status = emailSender.sendEmail(email);
-            return status.isSuccess();
-        } else {
-            EmailWrapper email = emailGenerator.generateFeedbackSessionSummaryOfCourse(
-                    courseId, studentEmail, EmailType.STUDENT_EMAIL_CHANGED);
-            EmailSendingStatus status = emailSender.sendEmail(email);
-            return status.isSuccess();
-        }
+        EmailWrapper email = sqlEmailGenerator.generateFeedbackSessionSummaryOfCourse(
+                courseId, studentEmail, EmailType.STUDENT_EMAIL_CHANGED);
+        EmailSendingStatus status = emailSender.sendEmail(email);
+        return status.isSuccess();
     }
 
 }
