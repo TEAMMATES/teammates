b9a3fe199a8c1d63bec890a0d0f584012ffbcdb2
Sai <saibhagatsr@gmail.com>
2026-02-05 10:23:12 +0800

    Fix lint

--- src/e2e/java/teammates/e2e/pageobjects/InstructorFeedbackResultsPageSql.java
+++ src/e2e/java/teammates/e2e/pageobjects/InstructorFeedbackResultsPageSql.java
@@ -27,12 +27,12 @@ import teammates.common.datatransfer.questions.FeedbackRubricQuestionDetails;
 import teammates.common.datatransfer.questions.FeedbackRubricResponseDetails;
 import teammates.e2e.util.TestProperties;
 import teammates.storage.sqlentity.FeedbackQuestion;
-import teammates.test.ThreadHelper;
 import teammates.storage.sqlentity.FeedbackResponse;
 import teammates.storage.sqlentity.FeedbackResponseComment;
 import teammates.storage.sqlentity.FeedbackSession;
 import teammates.storage.sqlentity.Instructor;
 import teammates.storage.sqlentity.Student;
+import teammates.test.ThreadHelper;
 
 /**
  * Represents the "Results" page for Instructors.
