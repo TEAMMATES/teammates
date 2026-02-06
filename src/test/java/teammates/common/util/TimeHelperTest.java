cd21a013ae499adcd75f3d8dd644f5f253e2c3be
ming yuan <mingyuanc.work@gmail.com>
2026-02-07 00:58:57 +0800

    Fix TimeHelperTest for JDK 25 time parsing changes
    Update test to reflect changes in time parsing format due to JDK 25.

--- src/test/java/teammates/common/util/TimeHelperTest.java
+++ src/test/java/teammates/common/util/TimeHelperTest.java
@@ -37,6 +37,8 @@ public class TimeHelperTest extends BaseTestCase {
         instant = LocalDateTime.of(2015, Month.NOVEMBER, 30, 16, 0).atZone(ZoneId.of(zoneId)).toInstant();
 
         // TODO: Remove this alternation once a workaround is found
+        // this alteration is required as jdk25 changes its time parsing format
+        // previously "z" is parsed as SGT now its GMT +08:00
         String expected = "Mon, 30 Nov 2015, 04:00 PM (SGT|GMT\\+08:00)";
         String actual = TimeHelper.formatInstant(instant, zoneId, DATETIME_DISPLAY_FORMAT);
         assertTrue(actual.matches(expected));
