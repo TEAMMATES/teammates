3cd6d94d0e2f5def369ba276928a3ed735f4213f
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-02-08 15:45:02 +0800

    Revert "Migrating UserProvision.java"
    This reverts commit 34b8a2cfe97e215f28390a1e78cec229da5607bc.

--- src/main/java/teammates/logic/api/UserProvision.java
+++ src/main/java/teammates/logic/api/UserProvision.java
@@ -4,6 +4,8 @@ import teammates.common.datatransfer.UserInfo;
 import teammates.common.datatransfer.UserInfoCookie;
 import teammates.common.util.Config;
 import teammates.common.util.HibernateUtil;
+import teammates.logic.core.InstructorsLogic;
+import teammates.logic.core.StudentsLogic;
 import teammates.sqllogic.core.UsersLogic;
 
 /**
@@ -14,6 +16,8 @@ public class UserProvision {
     private static final UserProvision instance = new UserProvision();
 
     private final UsersLogic usersLogic = UsersLogic.inst();
+    private final InstructorsLogic instructorsLogic = InstructorsLogic.inst();
+    private final StudentsLogic studentsLogic = StudentsLogic.inst();
 
     @SuppressWarnings("PMD.UnnecessaryConstructor")
     public UserProvision() {
@@ -37,8 +41,10 @@ public class UserProvision {
 
         String userId = user.id;
         user.isAdmin = Config.APP_ADMINS.contains(userId);
-        user.isInstructor = usersLogic.isInstructorInAnyCourse(userId);
-        user.isStudent = usersLogic.isStudentInAnyCourse(userId);
+        user.isInstructor = usersLogic.isInstructorInAnyCourse(userId)
+                || instructorsLogic.isInstructorInAnyCourse(userId);
+        user.isStudent = usersLogic.isStudentInAnyCourse(userId)
+                || studentsLogic.isStudentInAnyCourse(userId);
         user.isMaintainer = Config.APP_MAINTAINERS.contains(user.getId());
         return user;
     }
@@ -71,8 +77,10 @@ public class UserProvision {
     public UserInfo getMasqueradeUser(String googleId) {
         UserInfo userInfo = new UserInfo(googleId);
         userInfo.isAdmin = false;
-        userInfo.isInstructor = usersLogic.isInstructorInAnyCourse(googleId);
-        userInfo.isStudent = usersLogic.isInstructorInAnyCourse(googleId);
+        userInfo.isInstructor = usersLogic.isInstructorInAnyCourse(googleId)
+                || instructorsLogic.isInstructorInAnyCourse(googleId);
+        userInfo.isStudent = usersLogic.isInstructorInAnyCourse(googleId)
+                || studentsLogic.isStudentInAnyCourse(googleId);
         userInfo.isMaintainer = Config.APP_MAINTAINERS.contains(googleId);
         return userInfo;
     }
