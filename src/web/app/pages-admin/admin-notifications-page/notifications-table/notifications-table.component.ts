3b3b0f127482bdb26d9df2645240f673140364fb
AlanRQiu <97566592+AlanRQiu@users.noreply.github.com>
2026-03-08 03:50:02 -0700

    feat: implemented notifications table component to iomplement sortabletablecomponent

--- src/web/app/pages-admin/admin-notifications-page/notifications-table/notifications-table.component.ts
+++ src/web/app/pages-admin/admin-notifications-page/notifications-table/notifications-table.component.ts
@@ -1,5 +1,5 @@
 import { NgClass, NgIf, NgFor } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { NotificationsTableHeaderColorScheme, NotificationsTableRowModel } from './notifications-table-model';
 import { SimpleModalService } from '../../../../services/simple-modal.service';
@@ -10,85 +10,152 @@ import { FormatDateBriefPipe } from '../../../components/teammates-common/format
 import { FormatDateDetailPipe } from '../../../components/teammates-common/format-date-detail.pipe';
 import { NotificationStyleClassPipe } from '../../../components/teammates-common/notification-style-class.pipe';
 import {
-  NotificationStyleDescriptionPipe,
+    NotificationStyleDescriptionPipe,
 } from '../../../components/teammates-common/notification-style-description.pipe';
 
+// Import types from sortable-table
+import { SortableTableComponent, ColumnData, SortableTableCellData, SortableTableHeaderColorScheme } from '../../../components/sortable-table/sortable-table.component';
+
 @Component({
-  selector: 'tm-notifications-table',
-  templateUrl: './notifications-table.component.html',
-  styleUrls: ['./notifications-table.component.scss'],
-  imports: [
-    NgClass,
-    NgIf,
-    NgFor,
-    NgbTooltip,
-    FormatDateDetailPipe,
-    FormatDateBriefPipe,
-    NotificationStyleDescriptionPipe,
-    NotificationStyleClassPipe,
-  ],
+    selector: 'tm-notifications-table',
+    templateUrl: './notifications-table.component.html',
+    styleUrls: ['./notifications-table.component.scss'],
+    standalone: true,
+    imports: [
+        NgClass,
+        NgIf,
+        NgFor,
+        NgbTooltip,
+        FormatDateDetailPipe,
+        FormatDateBriefPipe,
+        NotificationStyleDescriptionPipe,
+        NotificationStyleClassPipe,
+        SortableTableComponent,
+    ],
 })
-export class NotificationsTableComponent {
-  SortBy = SortBy;
-  SortOrder = SortOrder;
-  NotificationsTableHeaderColorScheme = NotificationsTableHeaderColorScheme;
+export class NotificationsTableComponent implements OnChanges {
+    SortBy = SortBy;
+    SortOrder = SortOrder;
+    NotificationsTableHeaderColorScheme = NotificationsTableHeaderColorScheme;
 
-  @Input()
-  guessTimezone = 'UTC';
+    @Input()
+    guessTimezone = 'UTC';
 
-  @Input()
-  notificationsTableRowModels: NotificationsTableRowModel[] = [];
+    @Input()
+    notificationsTableRowModels: NotificationsTableRowModel[] = [];
 
-  @Input()
-  notificationsTableRowModelsSortBy = SortBy.NOTIFICATION_CREATE_TIME;
+    @Input()
+    notificationsTableRowModelsSortBy = SortBy.NOTIFICATION_CREATE_TIME;
 
-  @Input()
-  notificationsTableRowModelsSortOrder = SortOrder.DESC;
+    @Input()
+    notificationsTableRowModelsSortOrder = SortOrder.DESC;
 
-  @Input()
-  headerColorScheme = NotificationsTableHeaderColorScheme.BLUE;
+    @Input()
+    headerColorScheme = NotificationsTableHeaderColorScheme.BLUE;
 
-  @Output()
-  sortNotificationsTableRowModelsEvent: EventEmitter<SortBy> = new EventEmitter();
+    @Output()
+    sortNotificationsTableRowModelsEvent: EventEmitter<{ sortBy: SortBy, sortOrder: SortOrder }> = new EventEmitter();
 
-  @Output()
-  deleteNotificationEvent: EventEmitter<string> = new EventEmitter();
+    @Output()
+    deleteNotificationEvent: EventEmitter<string> = new EventEmitter();
 
-  @Output()
-  loadNotificationEditFormEvent: EventEmitter<Notification> = new EventEmitter();
+    @Output()
+    loadNotificationEditFormEvent: EventEmitter<Notification> = new EventEmitter();
 
-  constructor(private simpleModalService: SimpleModalService) { }
-
-  /**
-   * Sorts the list of feedback session row.
-   */
-  sortNotificationsTableRowModels(by: SortBy): void {
-    this.sortNotificationsTableRowModelsEvent.emit(by);
-  }
-
-  getAriaSort(by: SortBy): string {
-    if (by !== this.notificationsTableRowModelsSortBy) {
-      return 'none';
+    get tableSortBy(): SortBy {
+        return this.notificationsTableRowModelsSortBy;
     }
-    return this.notificationsTableRowModelsSortOrder === SortOrder.ASC ? 'ascending' : 'descending';
-  }
+    get tableSortOrder(): SortOrder {
+        return this.notificationsTableRowModelsSortOrder;
+    }
+    get tableHeaderColorScheme(): SortableTableHeaderColorScheme {
+        return this.headerColorScheme as unknown as SortableTableHeaderColorScheme;
+    }
 
-  /**
-   * Deletes a notification based on its ID.
-   */
-  deleteNotification(notificationId: string, title: string): void {
-    const modalRef = this.simpleModalService.openConfirmationModal(
-      'Confirm your action',
-      SimpleModalType.DANGER,
-      `Do you want to delete this notification (titled "${title}") permanently? This action will not be reversible.`,
-    );
-    modalRef.result.then(() => this.deleteNotificationEvent.emit(notificationId));
-  }
+    columnsData: ColumnData[] = [
+        {
+            header: 'Title',
+            sortBy: SortBy.NOTIFICATION_TITLE,
+            alignment: 'start',
+        },
+        {
+            header: 'Message',
+            alignment: 'start',
+        },
+        {
+            header: 'Start Time',
+            sortBy: SortBy.NOTIFICATION_START_TIME,
+            alignment: 'center',
+        },
+        {
+            header: 'Created At',
+            sortBy: SortBy.NOTIFICATION_CREATE_TIME,
+            alignment: 'center',
+        },
+        {
+            header: 'Actions',
+            alignment: 'center',
+        },
+    ];
 
-  /**
-   * Loads the notification edit form.
-   */
-  loadNotificationEditForm(notification: Notification): void {
-    this.loadNotificationEditFormEvent.emit(notification);
-  }
+    rowsData: SortableTableCellData[][] = [];
+
+    constructor(private simpleModalService: SimpleModalService) { }
+
+    ngOnChanges(_changes: SimpleChanges): void {
+        this.rowsData = this.notificationsTableRowModels.map((rowModel) => [
+            {
+                value: rowModel.notification.title,
+                displayValue: rowModel.notification.title,
+            },
+            {
+                value: rowModel.notification.message,
+                displayValue: rowModel.notification.message,
+            },
+            {
+                value: rowModel.notification.startTimestamp,
+                displayValue: this.formatDate(rowModel.notification.startTimestamp),
+            },
+            {
+                value: rowModel.notification.createdAt,
+                displayValue: this.formatDate(rowModel.notification.createdAt),
+            },
+            {
+                value: rowModel.notification.notificationId,
+                displayValue: 'Actions',
+            },
+        ]);
+    }
+
+    formatDate(timestamp: number): string {
+        return new Date(timestamp).toLocaleString('en-US', { timeZone: this.guessTimezone || 'UTC' });
+    }
+
+    /**
+     * Handles sort event from sortable-table.
+     */
+    sortNotificationsTableRowModels(event: { sortBy: SortBy, sortOrder: SortOrder }): void {
+        this.notificationsTableRowModelsSortBy = event.sortBy;
+        this.notificationsTableRowModelsSortOrder = event.sortOrder;
+        this.ngOnChanges({});
+    }
+
+    /**
+     * Deletes a notification based on its ID.
+     */
+    deleteNotification(notificationId: string, title: string): void {
+        const modalRef = this.simpleModalService.openConfirmationModal(
+            'Confirm your action',
+            SimpleModalType.DANGER,
+            `Do you want to delete this notification (titled "${title}") permanently? This action will not be reversible.`,
+        );
+        modalRef.result.then(() => this.deleteNotificationEvent.emit(notificationId));
+    }
+
+    /**
+     * Loads the notification edit form.
+     */
+    loadNotificationEditForm(notification: Notification): void {
+        this.loadNotificationEditFormEvent.emit(notification);
+    }
 }
