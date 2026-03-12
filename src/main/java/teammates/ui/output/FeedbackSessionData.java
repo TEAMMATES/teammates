fae859c55d597c3224e05c43dd6d45d6483f89cb
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2026-03-12 13:28:40 +0800

    Remove legacy *Attributes constructors in ui/output

--- src/main/java/teammates/ui/output/FeedbackSessionData.java
+++ src/main/java/teammates/ui/output/FeedbackSessionData.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 import jakarta.annotation.Nullable;
 
 import teammates.common.datatransfer.InstructorPermissionSet;
-import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
 import teammates.common.util.Const;
 import teammates.common.util.TimeHelper;
 import teammates.storage.sqlentity.DeadlineExtension;
@@ -18,7 +17,7 @@ import teammates.storage.sqlentity.Instructor;
 import teammates.storage.sqlentity.Student;
 
 /**
- * The API output format of {@link FeedbackSessionAttributes}.
+ * The API output format of {@link FeedbackSession}.
  */
 public class FeedbackSessionData extends ApiOutput {
 
@@ -62,93 +61,6 @@ public class FeedbackSessionData extends ApiOutput {
 
     private Map<String, Long> studentDeadlines;
     private Map<String, Long> instructorDeadlines;
-
-    public FeedbackSessionData(FeedbackSessionAttributes feedbackSessionAttributes) {
-        String timeZone = feedbackSessionAttributes.getTimeZone();
-        this.feedbackSessionId = null;
-        this.courseId = feedbackSessionAttributes.getCourseId();
-        this.timeZone = timeZone;
-        this.feedbackSessionName = feedbackSessionAttributes.getFeedbackSessionName();
-        this.instructions = feedbackSessionAttributes.getInstructions();
-        this.submissionStartTimestamp = TimeHelper.getMidnightAdjustedInstantBasedOnZone(
-                feedbackSessionAttributes.getStartTime(), timeZone, true).toEpochMilli();
-        this.submissionEndTimestamp = TimeHelper.getMidnightAdjustedInstantBasedOnZone(
-                feedbackSessionAttributes.getEndTime(), timeZone, true).toEpochMilli();
-        this.submissionEndWithExtensionTimestamp = TimeHelper.getMidnightAdjustedInstantBasedOnZone(
-                feedbackSessionAttributes.getDeadline(), timeZone, true).toEpochMilli();
-        this.gracePeriod = feedbackSessionAttributes.getGracePeriodMinutes();
-
-        Instant sessionVisibleTime = feedbackSessionAttributes.getSessionVisibleFromTime();
-        this.sessionVisibleFromTimestamp = TimeHelper.getMidnightAdjustedInstantBasedOnZone(
-                sessionVisibleTime, timeZone, true).toEpochMilli();
-        if (sessionVisibleTime.equals(Const.TIME_REPRESENTS_FOLLOW_OPENING)) {
-            this.sessionVisibleSetting = SessionVisibleSetting.AT_OPEN;
-        } else {
-            this.sessionVisibleSetting = SessionVisibleSetting.CUSTOM;
-            this.customSessionVisibleTimestamp = this.sessionVisibleFromTimestamp;
-        }
-
-        Instant responseVisibleTime = feedbackSessionAttributes.getResultsVisibleFromTime();
-        this.resultVisibleFromTimestamp = TimeHelper.getMidnightAdjustedInstantBasedOnZone(
-                responseVisibleTime, timeZone, true).toEpochMilli();
-        if (responseVisibleTime.equals(Const.TIME_REPRESENTS_FOLLOW_VISIBLE)) {
-            this.responseVisibleSetting = ResponseVisibleSetting.AT_VISIBLE;
-        } else if (responseVisibleTime.equals(Const.TIME_REPRESENTS_LATER)) {
-            this.responseVisibleSetting = ResponseVisibleSetting.LATER;
-        } else {
-            this.responseVisibleSetting = ResponseVisibleSetting.CUSTOM;
-            this.customResponseVisibleTimestamp = this.resultVisibleFromTimestamp;
-        }
-
-        if (!feedbackSessionAttributes.isVisible()) {
-            this.submissionStatus = FeedbackSessionSubmissionStatus.NOT_VISIBLE;
-        }
-        if (feedbackSessionAttributes.isVisible() && !feedbackSessionAttributes.isOpened()) {
-            this.submissionStatus = FeedbackSessionSubmissionStatus.VISIBLE_NOT_OPEN;
-        }
-        if (feedbackSessionAttributes.isOpened()) {
-            this.submissionStatus = FeedbackSessionSubmissionStatus.OPEN;
-        }
-        if (feedbackSessionAttributes.isInGracePeriod()) {
-            this.submissionStatus = FeedbackSessionSubmissionStatus.GRACE_PERIOD;
-        }
-        if (feedbackSessionAttributes.isClosed()) {
-            this.submissionStatus = FeedbackSessionSubmissionStatus.CLOSED;
-        }
-
-        if (feedbackSessionAttributes.isPublished()) {
-            this.publishStatus = FeedbackSessionPublishStatus.PUBLISHED;
-        } else {
-            this.publishStatus = FeedbackSessionPublishStatus.NOT_PUBLISHED;
-        }
-
-        this.isClosingSoonEmailEnabled = feedbackSessionAttributes.isClosingSoonEmailEnabled();
-        this.isPublishedEmailEnabled = feedbackSessionAttributes.isPublishedEmailEnabled();
-
-        this.createdAtTimestamp = feedbackSessionAttributes.getCreatedTime().toEpochMilli();
-        if (feedbackSessionAttributes.getDeletedTime() == null) {
-            this.deletedAtTimestamp = null;
-        } else {
-            this.deletedAtTimestamp = feedbackSessionAttributes.getDeletedTime().toEpochMilli();
-        }
-
-        String userEmail = feedbackSessionAttributes.getUserEmail();
-        this.studentDeadlines = feedbackSessionAttributes.getStudentDeadlines()
-                .entrySet()
-                .stream()
-                .filter(entry -> userEmail == null || userEmail.equals(entry.getKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, entry ->
-                        TimeHelper.getMidnightAdjustedInstantBasedOnZone(entry.getValue(), timeZone, true)
-                                .toEpochMilli()));
-
-        this.instructorDeadlines = feedbackSessionAttributes.getInstructorDeadlines()
-                .entrySet()
-                .stream()
-                .filter(entry -> userEmail == null || userEmail.equals(entry.getKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, entry ->
-                        TimeHelper.getMidnightAdjustedInstantBasedOnZone(entry.getValue(), timeZone, true)
-                                .toEpochMilli()));
-    }
 
     public FeedbackSessionData(FeedbackSession feedbackSession) {
         assert feedbackSession != null;
