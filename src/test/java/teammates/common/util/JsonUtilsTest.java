daeed49fa36b1539ec83416a7f06d86701d6895a
YongJunXi <ernestyongjunxi@gmail.com>
2026-03-12 20:56:12 +0800

    Update JsonUtilsTest

--- src/test/java/teammates/common/util/JsonUtilsTest.java
+++ src/test/java/teammates/common/util/JsonUtilsTest.java
@@ -6,10 +6,14 @@ import java.util.List;
 import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.FeedbackParticipantType;
-import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
-import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
+import teammates.common.datatransfer.questions.FeedbackResponseDetails;
 import teammates.common.datatransfer.questions.FeedbackTextQuestionDetails;
 import teammates.common.datatransfer.questions.FeedbackTextResponseDetails;
+import teammates.storage.sqlentity.Course;
+import teammates.storage.sqlentity.FeedbackQuestion;
+import teammates.storage.sqlentity.FeedbackResponse;
+import teammates.storage.sqlentity.FeedbackSession;
+import teammates.storage.sqlentity.Section;
 import teammates.test.BaseTestCase;
 
 /**
@@ -25,18 +29,21 @@ public class JsonUtilsTest extends BaseTestCase {
         participants.add(FeedbackParticipantType.OWN_TEAM_MEMBERS);
         participants.add(FeedbackParticipantType.RECEIVER);
 
-        FeedbackQuestionAttributes fqa = FeedbackQuestionAttributes.builder()
-                .withCourseId("testingCourse")
-                .withFeedbackSessionName("testFeedbackSession")
-                .withGiverType(FeedbackParticipantType.INSTRUCTORS)
-                .withRecipientType(FeedbackParticipantType.SELF)
-                .withNumberOfEntitiesToGiveFeedbackTo(Const.MAX_POSSIBLE_RECIPIENTS)
-                .withQuestionNumber(1)
-                .withQuestionDetails(questionDetails)
-                .withShowGiverNameTo(participants)
-                .withShowRecipientNameTo(participants)
-                .withShowResponsesTo(participants)
-                .build();
+        Course course = getTypicalCourse();
+        course.setId("testingCourse");
+
+        FeedbackSession fs = getTypicalFeedbackSessionForCourse(course);
+        fs.setName("testFeedbackSession");
+
+        FeedbackQuestion fq = getTypicalFeedbackQuestionForSession(fs);
+        fq.setGiverType(FeedbackParticipantType.INSTRUCTORS);
+        fq.setRecipientType(FeedbackParticipantType.SELF);
+        fq.setNumOfEntitiesToGiveFeedbackTo(Const.MAX_POSSIBLE_RECIPIENTS);
+        fq.setQuestionNumber(1);
+        fq.setQuestionDetails(questionDetails);
+        fq.setShowGiverNameTo(participants);
+        fq.setShowRecipientNameTo(participants);
+        fq.setShowResponsesTo(participants);
 
         assertEquals("{\n"
                 + "  \"feedbackSessionName\": \"testFeedbackSession\",\n"
@@ -59,7 +66,7 @@ public class JsonUtilsTest extends BaseTestCase {
                 + "  \"showRecipientNameTo\": [\n"
                 + "    \"RECEIVER\"\n"
                 + "  ]\n"
-                + "}", JsonUtils.toJson(fqa));
+                + "}", JsonUtils.toJson(fq));
 
         assertEquals("{\"feedbackSessionName\":\"testFeedbackSession\","
                 + "\"courseId\":\"testingCourse\",\"questionDetails\":{\"shouldAllowRichText\":true,\"questionType\":"
@@ -68,20 +75,29 @@ public class JsonUtilsTest extends BaseTestCase {
                 + "\"giverType\":\"INSTRUCTORS\",\"recipientType\":\"SELF\",\"numberOfEntitiesToGiveFeedbackTo\":-100,"
                 + "\"showResponsesTo\":[\"RECEIVER\"],\"showGiverNameTo\":[\"RECEIVER\"],"
                 + "\"showRecipientNameTo\":[\"RECEIVER\"]}",
-                JsonUtils.toCompactJson(fqa));
+                JsonUtils.toCompactJson(fq));
     }
 
     @Test
     public void testFeedbackResponseDetailsAdaptor_withComposedResponseDetails_shouldSerializeToConcreteClass() {
-        FeedbackResponseAttributes fra =
-                FeedbackResponseAttributes.builder(
-                        "questionId", "giver@email.com", "recipient@email.com")
-                .withFeedbackSessionName("Session1")
-                .withCourseId("CS3281")
-                .withGiverSection("giverSection")
-                .withRecipientSection("recipientSection")
-                .withResponseDetails(new FeedbackTextResponseDetails("My answer"))
-                .build();
+        Course course = getTypicalCourse();
+        course.setId("CS3281");
+
+        FeedbackSession fs = getTypicalFeedbackSessionForCourse(course);
+        fs.setName("Session1");
+
+        FeedbackQuestion fq = getTypicalFeedbackQuestionForSession(fs);
+
+        Section giverSection = getTypicalSection();
+        giverSection.setName("giverSection");
+
+        Section recipientSection = getTypicalSection();
+        recipientSection.setName("recipientSection");
+
+        FeedbackResponseDetails frd = new FeedbackTextResponseDetails("My answer");
+        FeedbackResponse fr = FeedbackResponse.makeResponse(fq, "giver@email.com",
+                giverSection, "recipient@email.com",
+                recipientSection, frd);
 
         assertEquals("{\n"
                 + "  \"feedbackQuestionId\": \"questionId\",\n"
@@ -95,13 +111,13 @@ public class JsonUtilsTest extends BaseTestCase {
                 + "  },\n"
                 + "  \"giverSection\": \"giverSection\",\n"
                 + "  \"recipientSection\": \"recipientSection\"\n"
-                + "}", JsonUtils.toJson(fra));
+                + "}", JsonUtils.toJson(fr));
 
         assertEquals("{\"feedbackQuestionId\":\"questionId\",\"giver\":\"giver@email.com\","
                 + "\"recipient\":\"recipient@email.com\",\"feedbackSessionName\":\"Session1\","
                 + "\"courseId\":\"CS3281\",\"responseDetails\":{\"answer\":\"My answer\","
                 + "\"questionType\":\"TEXT\"},\"giverSection\":\"giverSection\","
                 + "\"recipientSection\":\"recipientSection\"}",
-                JsonUtils.toCompactJson(fra));
+                JsonUtils.toCompactJson(fr));
     }
 }
