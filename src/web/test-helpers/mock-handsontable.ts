d5d57e534d6ade33c34edea939bc1d5af527563b
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-03-14 19:04:37 +0800

    Create new tm-data-grid component

--- src/web/test-helpers/mock-handsontable.ts
+++ src/web/test-helpers/mock-handsontable.ts
@@ -1,9 +1,24 @@
-import { NgModule } from '@angular/core';
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, Input, NgModule } from '@angular/core';
 
 /**
  * This package is ESM only since v14.3, which causes issues when importing this module.
  * As a workaround, we mock the module here until we can update the testing framework to support ESM properly.
  */
-export class HotTableRegisterer {}
 
-export const HotTableModule = NgModule({})(class HotTableModule {});
+export const NON_COMMERCIAL_LICENSE = 'non-commercial-and-evaluation';
+
+@Component({
+  selector: 'hot-table',
+  template: '',
+  standalone: true,
+})
+export class HotTableComponent {
+  @Input() settings: any;
+  @Input() data: any;
+  hotInstance = null;
+}
+
+export const HotTableModule = NgModule({ imports: [HotTableComponent], exports: [HotTableComponent] })(
+  class HotTableModule {},
+);
