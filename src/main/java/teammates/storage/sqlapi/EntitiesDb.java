fc0728f76d59269bb024e5607a72e237678a82be
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-03-14 23:05:21 +0800

    Remove logging from entities db

--- src/main/java/teammates/storage/sqlapi/EntitiesDb.java
+++ src/main/java/teammates/storage/sqlapi/EntitiesDb.java
@@ -2,16 +2,12 @@
 package teammates.storage.sqlapi;
 
 import teammates.common.util.HibernateUtil;
-import teammates.common.util.Logger;
 import teammates.storage.sqlentity.BaseEntity;
 
 /**
  * Base class for all classes performing CRUD operations against the database.
  */
 class EntitiesDb {
-
-    static final Logger log = Logger.getLogger();
-
     /**
      * Copy the state of the given object onto the persistent object with the same identifier.
      * If there is no persistent instance currently associated with the session, it will be loaded.
@@ -20,7 +16,6 @@ class EntitiesDb {
         assert entity != null;
 
         T newEntity = HibernateUtil.merge(entity);
-        log.info("Entity saved: " + newEntity.toString());
         return newEntity;
     }
 
@@ -31,7 +26,6 @@ class EntitiesDb {
         assert entity != null;
 
         HibernateUtil.persist(entity);
-        log.info("Entity persisted: " + entity.toString());
     }
 
     /**
@@ -41,6 +35,5 @@ class EntitiesDb {
         assert entity != null;
 
         HibernateUtil.remove(entity);
-        log.info("Entity deleted: " + entity.toString());
     }
 }
