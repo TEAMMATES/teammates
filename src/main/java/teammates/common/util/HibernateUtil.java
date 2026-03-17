35815643c6232c3058d7764f3fa150961e50b78d
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-03-17 08:23:44 +0800

    Reduce maximum pool size for Hibernate HikariCP configuration

--- src/main/java/teammates/common/util/HibernateUtil.java
+++ src/main/java/teammates/common/util/HibernateUtil.java
@@ -122,14 +122,9 @@ public final class HibernateUtil {
                 .setProperty("show_sql", "true")
                 .setProperty("hibernate.current_session_context_class", "thread")
                 .setProperty("hibernate.hikari.minimumIdle", "10")
-                .setProperty("hibernate.hikari.maximumPoolSize", "30")
+                .setProperty("hibernate.hikari.maximumPoolSize", "10")
                 .setProperty("hibernate.hikari.idleTimeout", "300000")
                 .setProperty("hibernate.hikari.connectionTimeout", "30000")
-                // Uncomment only during migration for optimized batch-insertion, batch-update, and batch-fetch.
-                // .setProperty("hibernate.jdbc.batch_size", "50")
-                // .setProperty("hibernate.order_updates", "true")
-                // .setProperty("hibernate.batch_versioned_data", "true")
-                // .setProperty("hibernate.jdbc.fetch_size", "50")
                 .addPackage("teammates.storage.sqlentity");
 
         if (Config.IS_DEV_SERVER) {
