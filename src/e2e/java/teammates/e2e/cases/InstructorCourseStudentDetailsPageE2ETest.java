6105419dc2631cba59e010f7612fc449ef91b77d
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-03-17 09:00:31 +0800

    Remove redundant read only test

--- src/e2e/java/teammates/e2e/cases/InstructorCourseStudentDetailsPageE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/InstructorCourseStudentDetailsPageE2ETest.java
@@ -12,7 +12,6 @@ import teammates.storage.sqlentity.Student;
  */
 public class InstructorCourseStudentDetailsPageE2ETest extends BaseE2ETestCase {
     private Student student;
-    private Student otherStudent;
 
     @Override
     protected void prepareTestData() {
@@ -20,7 +19,6 @@ public class InstructorCourseStudentDetailsPageE2ETest extends BaseE2ETestCase {
                         loadDataBundle("/InstructorCourseStudentDetailsPageE2ETestSql.json"));
 
         student = testData.students.get("ICSDet.jose.tmms");
-        otherStudent = testData.students.get("ICSDet.benny.c");
     }
 
     @Test
@@ -33,12 +31,6 @@ public class InstructorCourseStudentDetailsPageE2ETest extends BaseE2ETestCase {
                         testData.instructors.get("ICSDet.instr").getGoogleId());
 
         viewPage.verifyStudentDetails(student);
-
-        ______TS("verify loaded details - another student");
-        viewPageUrl = getStudentDetailsViewPageUrl(otherStudent.getEmail());
-        viewPage = getNewPageInstance(viewPageUrl, InstructorCourseStudentDetailsViewPageSql.class);
-
-        viewPage.verifyStudentDetails(otherStudent);
     }
 
     private AppUrl getStudentDetailsViewPageUrl(String studentEmail) {
