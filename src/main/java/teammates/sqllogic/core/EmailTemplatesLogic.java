a88f3ea9b4ccea70bc895ec8d8276d24552171f8
WeeJean <e1354206@u.nus.edu>
2026-03-24 12:10:07 +0800

    Implement EmailTemplatesLogic for business rules

---
+++ src/main/java/teammates/sqllogic/core/EmailTemplatesLogic.java
@@ -0,0 +1,58 @@
+package teammates.sqllogic.core;
+
+import teammates.common.exception.InvalidParametersException;
+import teammates.storage.sqlapi.EmailTemplatesDb;
+import teammates.storage.sqlentity.EmailTemplate;
+
+/**
+ * Handles the logic related to email templates.
+ */
+public final class EmailTemplatesLogic {
+
+    private static final EmailTemplatesLogic instance = new EmailTemplatesLogic();
+
+    private EmailTemplatesDb emailTemplatesDb;
+
+    private EmailTemplatesLogic() {
+        // prevent initialization
+    }
+
+    public static EmailTemplatesLogic inst() {
+        return instance;
+    }
+
+    /**
+     * Initialise dependencies for {@code EmailTemplatesLogic} object.
+     */
+    public void initLogicDependencies(EmailTemplatesDb emailTemplatesDb) {
+        this.emailTemplatesDb = emailTemplatesDb;
+    }
+
+    /**
+     * Gets the email template associated with {@code templateKey},
+     * or {@code null} if no custom template has been saved for that key.
+     */
+    public EmailTemplate getEmailTemplate(String templateKey) {
+        assert templateKey != null;
+        return emailTemplatesDb.getEmailTemplate(templateKey);
+    }
+
+    /**
+     * Creates or updates the email template for the given key.
+     *
+     * @return the persisted email template.
+     * @throws InvalidParametersException if the template is not valid.
+     */
+    public EmailTemplate upsertEmailTemplate(EmailTemplate emailTemplate) throws InvalidParametersException {
+        assert emailTemplate != null;
+        return emailTemplatesDb.upsertEmailTemplate(emailTemplate);
+    }
+
+    /**
+     * Deletes a custom email template, reverting to the static file fallback.
+     * Does nothing if {@code emailTemplate} is {@code null}.
+     */
+    public void deleteEmailTemplate(EmailTemplate emailTemplate) {
+        emailTemplatesDb.deleteEmailTemplate(emailTemplate);
+    }
+}
