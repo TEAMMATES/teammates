671d053cb471f73b325a2c8be6c08a6fa7a7bad0
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-04-01 17:15:20 +0900

    [#13634] Remove Admin/Maintainer Logs (#13633)
    Co-authored-by: Dhira Tengara <dhira.pt@gmail.com>

--- src/web/app/components/logs-histogram/logs-histogram-model.ts
+++
@@ -1,9 +0,0 @@
-import { SourceLocation } from '../../../types/api-output';
-
-/**
- * The model for a histogram data.
- */
-export interface LogsHistogramDataModel {
-  sourceLocation: SourceLocation;
-  numberOfTimes: number;
-}
