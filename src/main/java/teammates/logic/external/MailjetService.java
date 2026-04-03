e2c423823412bbfc42039c9f0f3fa85283617974
DuckyGuy <42895782+DuckyGuy@users.noreply.github.com>
2026-04-03 01:17:43 -0700

    [#13682] Remove parseToEmail from EmailSenderService interface (#13685)

--- src/main/java/teammates/logic/external/MailjetService.java
+++ src/main/java/teammates/logic/external/MailjetService.java
@@ -27,11 +27,7 @@ import teammates.common.util.HtmlHelper;
  */
 public class MailjetService implements EmailSenderService {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public MailjetRequest parseToEmail(EmailWrapper wrapper) {
+    MailjetRequest parseToEmail(EmailWrapper wrapper) {
         MailjetRequest request = new MailjetRequest(Email.resource);
         request.property(Email.FROMEMAIL, wrapper.getSenderEmail());
         if (wrapper.getSenderName() != null && !wrapper.getSenderName().isEmpty()) {
