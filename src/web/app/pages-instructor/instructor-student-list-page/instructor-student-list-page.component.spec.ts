28cf9e7e49e354ff6d7c52bffd684b13cfa95b0c
Victor Almeida <victor_almeida@exati.com.br>
2026-04-03 16:02:38 -0300

    Removing BrowserAnimationsModule from tests

--- src/web/app/pages-instructor/instructor-student-list-page/instructor-student-list-page.component.spec.ts
+++ src/web/app/pages-instructor/instructor-student-list-page/instructor-student-list-page.component.spec.ts
@@ -1,7 +1,6 @@
 import { HttpStatusCode, provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { throwError } from 'rxjs';
 import { CourseTab, InstructorStudentListPageComponent } from './instructor-student-list-page.component';
@@ -75,9 +74,6 @@ describe('InstructorStudentListPageComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        BrowserAnimationsModule,
-      ],
       providers: [
         provideRouter([]),
         provideHttpClient(),
