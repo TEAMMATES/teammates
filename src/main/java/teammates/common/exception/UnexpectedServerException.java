f8de9a89986915deba675a0187ef003607670691
Afroj Mulani <mulaniafroj26@gmail.com>
2026-04-07 02:07:13 +0530

    Add documentation comment for UnexpectedServerException
    Added a comment to clarify the purpose of the exception.

--- src/main/java/teammates/common/exception/UnexpectedServerException.java
+++ src/main/java/teammates/common/exception/UnexpectedServerException.java
@@ -1,4 +1,5 @@
 package teammates.common.exception;
+
 /**
  * Exception thrown when an unexpected server-side error occurs.
  * This is used for centralized error handling and maps to HTTP 500 responses.
