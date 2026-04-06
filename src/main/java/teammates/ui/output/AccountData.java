feea4bbf90858644fb86c989f7fff9e7f46ac88f
Samuel <60355570+samuelfangjw@users.noreply.github.com>
2026-04-06 21:05:02 +0800

    [#13721] Remove unused readNotifications field from AccountData (#13734)

--- src/main/java/teammates/ui/output/AccountData.java
+++ src/main/java/teammates/ui/output/AccountData.java
@@ -1,8 +1,6 @@
 package teammates.ui.output;
 
-import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import teammates.storage.sqlentity.Account;
 
@@ -15,19 +13,12 @@ public class AccountData extends ApiOutput {
     private final String googleId;
     private final String name;
     private final String email;
-    private final Map<String, Long> readNotifications;
 
     public AccountData(Account account) {
         this.accountId = account.getId();
         this.googleId = account.getGoogleId();
         this.name = account.getName();
         this.email = account.getEmail();
-        this.readNotifications = account.getReadNotifications()
-                .stream()
-                .collect(Collectors.toMap(
-                        readNotification -> readNotification.getNotification().getId().toString(),
-                        readNotification ->
-                                readNotification.getNotification().getEndTime().toEpochMilli()));
     }
 
     public UUID getAccountId() {
@@ -44,10 +35,6 @@ public class AccountData extends ApiOutput {
 
     public String getName() {
         return name;
-    }
-
-    public Map<String, Long> getReadNotifications() {
-        return this.readNotifications;
     }
 
 }
