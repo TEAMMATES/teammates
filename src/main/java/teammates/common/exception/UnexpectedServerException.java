57f216ffc1b7eb67decac8c429d53e3ba948dc72
Afroj Mulani <mulaniafroj26@gmail.com>
2026-04-07 01:52:51 +0530

    Change UnexpectedServerException to extend RuntimeException

--- src/main/java/teammates/common/exception/UnexpectedServerException.java
+++ src/main/java/teammates/common/exception/UnexpectedServerException.java
@@ -3,7 +3,7 @@ package teammates.common.exception;
  * Exception thrown when an unexpected server-side error occurs.
  * This is used for centralized error handling and maps to HTTP 500 responses.
  */
-public class UnexpectedServerException extends Exception {
+public class UnexpectedServerException extends RuntimeException {
 
     public UnexpectedServerException(String message) {
         super(message);
