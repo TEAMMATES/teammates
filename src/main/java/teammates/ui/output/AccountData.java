103f9780f05bede5213e6967f3c3eb96de9c9739
Florian Viorel Tanely <izkx73@gmail.com>
2026-04-09 13:54:05 +0800

    Extract DTO: Remove googleId from AccountData
    (Selective directory cherry-pick from c0d2039412ae1cb064e7f866accff3f040e99434)

--- src/main/java/teammates/ui/output/AccountData.java
+++ src/main/java/teammates/ui/output/AccountData.java
@@ -10,13 +10,11 @@ import teammates.storage.sqlentity.Account;
 public class AccountData extends ApiOutput {
 
     private final UUID accountId;
-    private final String googleId;
     private final String name;
     private final String email;
 
     public AccountData(Account account) {
         this.accountId = account.getId();
-        this.googleId = account.getGoogleId();
         this.name = account.getName();
         this.email = account.getEmail();
     }
@@ -27,10 +25,6 @@ public class AccountData extends ApiOutput {
 
     public String getEmail() {
         return email;
-    }
-
-    public String getGoogleId() {
-        return googleId;
     }
 
     public String getName() {
