3aa8be55d17102df3270de9ba5f0fdb5b859329a
Florian Viorel Tanely <izkx73@gmail.com>
2026-04-10 22:31:13 +0800

    Fix lint in GetOngoingSessionsAction

--- src/main/java/teammates/ui/webapi/GetOngoingSessionsAction.java
+++ src/main/java/teammates/ui/webapi/GetOngoingSessionsAction.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import teammates.common.util.Const;
