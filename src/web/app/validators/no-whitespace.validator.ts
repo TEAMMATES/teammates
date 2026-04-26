7a743c50753e8f6219e739dfb660d9f901a6076d
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-04-27 05:56:58 +0800

    Fix lint

--- src/web/app/validators/no-whitespace.validator.ts
+++ src/web/app/validators/no-whitespace.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 /**
- *
+ * Validator that checks if the control value consists of only whitespace characters.
  */
 export function noWhitespaceValidator(control: AbstractControl): ValidationErrors | null {
   const value = control.value;
