db1adfa02dda6dbb1ed92000714a205193ba6920
Victor Henrique <87485590+viictor1@users.noreply.github.com>
2026-04-26 05:40:20 -0300

    [#13597] Migrate student pages and notification banner away from Angular's Animations package (#13790)

--- src/web/app/components/notification-banner/notification-banner.component.ts
+++ src/web/app/components/notification-banner/notification-banner.component.ts
@@ -1,10 +1,10 @@
 import { NgClass } from '@angular/common';
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ChangeDetectorRef } from '@angular/core';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 import { NotificationService } from '../../../services/notification.service';
 import { StatusMessageService } from '../../../services/status-message.service';
 import { Notification, Notifications, NotificationTargetUser } from '../../../types/api-output';
 import { ErrorMessageOutput } from '../../error-message-output';
-import { collapseAnim } from '../teammates-common/collapse-anim';
 import { NotificationStyleClassPipe } from '../teammates-common/notification-style-class.pipe';
 
 /**
@@ -14,10 +14,10 @@ import { NotificationStyleClassPipe } from '../teammates-common/notification-sty
   selector: 'tm-notification-banner',
   templateUrl: './notification-banner.component.html',
   styleUrls: ['./notification-banner.component.scss'],
-  animations: [collapseAnim],
   imports: [
     NgClass,
     NotificationStyleClassPipe,
+    NgbCollapse,
 ],
 })
 export class NotificationBannerComponent implements OnInit, OnChanges {
@@ -28,11 +28,12 @@ export class NotificationBannerComponent implements OnInit, OnChanges {
   @Input()
   notificationTargetUser: NotificationTargetUser = NotificationTargetUser.GENERAL;
 
-  isShown: boolean = true;
+  isShown: boolean = false;
   notifications: Notification[] = [];
 
   constructor(private notificationService: NotificationService,
-              private statusMessageService: StatusMessageService) { }
+              private statusMessageService: StatusMessageService,
+              private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     if (this.notificationTargetUser !== NotificationTargetUser.GENERAL) {
@@ -51,6 +52,10 @@ export class NotificationBannerComponent implements OnInit, OnChanges {
     this.notificationService.getUnreadNotificationsForTargetUser(this.notificationTargetUser)
       .subscribe((response: Notifications) => {
         this.notifications = response.notifications;
+        if (this.notifications.length > 0) {
+          this.cdr.detectChanges();
+          this.isShown = true;
+        }
       });
   }
 
