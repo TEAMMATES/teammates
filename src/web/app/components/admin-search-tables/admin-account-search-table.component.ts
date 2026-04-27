696f64ef481e5a86d571c64db6f0ebcf879fd665
Isabel Leong <lleong@andrew.cmu.edu>
2026-04-27 11:57:54 -0400

    fix(admin-search): reorder reused modal imports for lint

--- src/web/app/components/admin-search-tables/admin-account-search-table.component.ts
+++ src/web/app/components/admin-search-tables/admin-account-search-table.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { NgbModalRef, NgbModal, NgbTooltip, NgbDropdown, NgbDropdownToggle, NgbDropdownMenu } from '@ng-bootstrap/ng-bootstrap';
 import { AccountRequestTableRowModel } from './account-search-table-model';
+import { AccountService } from '../../../services/account.service';
+import { SimpleModalService } from '../../../services/simple-modal.service';
+import { StatusMessageService } from '../../../services/status-message.service';
+import { AccountRequest, MessageOutput } from '../../../types/api-output';
+import { AccountRequestUpdateRequest } from '../../../types/api-request';
+import { ErrorMessageOutput } from '../../error-message-output';
+import { SearchTermsHighlighterPipe } from '../../pipes/search-terms-highlighter.pipe';
 import { EditRequestModalComponentResult } from '../account-requests-table/admin-edit-request-modal/admin-edit-request-modal-model';
 import { EditRequestModalComponent } from '../account-requests-table/admin-edit-request-modal/admin-edit-request-modal.component';
 import {
@@ -9,13 +16,6 @@ import {
 import {
   RejectWithReasonModalComponent,
 } from '../account-requests-table/admin-reject-with-reason-modal/admin-reject-with-reason-modal.component';
-import { AccountService } from '../../../services/account.service';
-import { SimpleModalService } from '../../../services/simple-modal.service';
-import { StatusMessageService } from '../../../services/status-message.service';
-import { AccountRequest, MessageOutput } from '../../../types/api-output';
-import { AccountRequestUpdateRequest } from '../../../types/api-request';
-import { ErrorMessageOutput } from '../../error-message-output';
-import { SearchTermsHighlighterPipe } from '../../pipes/search-terms-highlighter.pipe';
 import { AjaxLoadingComponent } from '../ajax-loading/ajax-loading.component';
 import { SimpleModalType } from '../simple-modal/simple-modal-type';
 import { collapseAnim } from '../teammates-common/collapse-anim';
