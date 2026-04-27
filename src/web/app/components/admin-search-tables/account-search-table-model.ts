10409cdd48baa4cd6ea41537351eeda413c207d7
alexbock2 <alexandrabock14@gmail.com>
2026-04-27 15:10:07 -0400

    reuse existing AccountRequestTableRowModel

--- src/web/app/components/admin-search-tables/account-search-table-model.ts
+++ src/web/app/components/admin-search-tables/account-search-table-model.ts
@@ -1,17 +1,1 @@
-import { AccountRequestStatus } from '../../../types/api-output';
-
-/**
- * Model for the row entries in the account requests table.
- */
-export interface AccountRequestTableRowModel {
-    id: string;
-    name: string;
-    email: string;
-    status: AccountRequestStatus;
-    instituteAndCountry: string;
-    createdAtText: string;
-    registeredAtText: string;
-    comments: string;
-    registrationLink: string;
-    showLinks: boolean;
-}
+export type { AccountRequestTableRowModel } from '../account-requests-table/account-request-table-model';
