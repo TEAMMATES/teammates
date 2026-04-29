2f82f5b36d8f2a69a46faaa996523a3cf12fee27
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-04-29 20:55:27 +0800

    Update stylelint config

---
+++ stylelint.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "no-empty-source": null,
+    "selector-pseudo-element-no-unknown": [
+      true,
+      {
+        ignorePseudoElements: ["ng-deep"],
+      },
+    ],
+  },
+};
