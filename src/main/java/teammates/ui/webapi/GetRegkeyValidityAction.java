177d0f283e02790b48d8eddd358c1a5d019c432f
maitriupadhyay03-cell <maitriupadhyay03@gmail.com>
2026-05-10 18:38:02 +0530

    Refactor GetRegkeyValidityAction to extend PublicAction

--- src/main/java/teammates/ui/webapi/GetRegkeyValidityAction.java
+++ src/main/java/teammates/ui/webapi/GetRegkeyValidityAction.java
@@ -9,59 +9,49 @@ import teammates.ui.request.Intent;
 
 /**
  * Action: checks whether the provided registration key is valid for the logged in user.
- *
- * <p>This does not log in or log out the user.
- */
-public class GetRegkeyValidityAction extends Action {
+     *
+     * <p>This does not log in or log out the user.
+     */
+public class GetRegkeyValidityAction extends PublicAction {
 
     @Override
-    public AuthType getMinAuthLevel() {
-        return AuthType.PUBLIC;
-    }
+        public JsonResult execute() {
+                    String regKey = getNonNullRequestParamValue(Const.ParamsNames.REGKEY);
+                    Intent intent = Intent.valueOf(getNonNullRequestParamValue(Const.ParamsNames.INTENT));
 
-    @Override
-    void checkSpecificAccessControl() {
-        // Regkey information is available to everyone
-    }
+            boolean isValid = false;
+                    String googleId = null;
 
-    @Override
-    public JsonResult execute() {
-        Intent intent = Intent.valueOf(getNonNullRequestParamValue(Const.ParamsNames.INTENT));
-        String regKey = getNonNullRequestParamValue(Const.ParamsNames.REGKEY);
-
-        boolean isValid = false;
-        String googleId = null;
-
-        if (intent == Intent.STUDENT_SUBMISSION || intent == Intent.STUDENT_RESULT) {
-            Student student = logic.getStudentByRegistrationKey(regKey);
-            if (student != null) {
-                isValid = true;
-                googleId = student.getGoogleId();
+            if (intent == Intent.STUDENT_SUBMISSION || intent == Intent.STUDENT_RESULT) {
+                            Student student = logic.getStudentByRegistrationKey(regKey);
+                            if (student != null) {
+                                                isValid = true;
+                                                googleId = student.getGoogleId();
+                            }
+            } else if (intent == Intent.INSTRUCTOR_SUBMISSION || intent == Intent.INSTRUCTOR_RESULT) {
+                            Instructor instructor = logic.getInstructorByRegistrationKey(regKey);
+                            if (instructor != null) {
+                                                isValid = true;
+                                                googleId = instructor.getGoogleId();
+                            }
             }
-        } else if (intent == Intent.INSTRUCTOR_SUBMISSION || intent == Intent.INSTRUCTOR_RESULT) {
-            Instructor instructor = logic.getInstructorByRegistrationKey(regKey);
-            if (instructor != null) {
-                isValid = true;
-                googleId = instructor.getGoogleId();
+
+            boolean isUsed = false;
+                    boolean isAllowedAccess = false;
+
+            if (isValid) {
+                            if (StringHelper.isEmpty(googleId)) {
+                                                // If registration key has not been used, always allow access
+                                isAllowedAccess = true;
+                            } else {
+                                                isUsed = true;
+                                                // If the registration key has been used to register, the logged in user needs to match
+                                // Block access to not logged in user or mismatched user
+                                isAllowedAccess = userInfo != null && googleId.equals(userInfo.id);
+                            }
             }
+
+            return new JsonResult(new RegkeyValidityData(isValid, isUsed, isAllowedAccess));
         }
-
-        boolean isUsed = false;
-        boolean isAllowedAccess = false;
-
-        if (isValid) {
-            if (StringHelper.isEmpty(googleId)) {
-                // If registration key has not been used, always allow access
-                isAllowedAccess = true;
-            } else {
-                isUsed = true;
-                // If the registration key has been used to register, the logged in user needs to match
-                // Block access to not logged in user and mismatched user
-                isAllowedAccess = userInfo != null && googleId.equals(userInfo.id);
-            }
-        }
-
-        return new JsonResult(new RegkeyValidityData(isValid, isUsed, isAllowedAccess));
-    }
 
 }
