6f0d33c6891b6f0617c977a34a75e08035dffa57
Youssef <youssefmani17@gmail.com>
2026-05-10 22:53:05 +0100

    fix: address reviewer feedback for EmailSenderTest - Mocked Config to init EmailSender with EmptyEmailService - Renamed EmailSenderTest to EmailSenderServiceTest in logic.external

--- src/test/java/teammates/logic/api/EmailSenderTest.java
+++ src/test/java/teammates/logic/api/EmailSenderTest.java
@@ -19,6 +19,13 @@ import teammates.test.BaseTestCase;
  */
 public class EmailSenderTest extends BaseTestCase {
 
+    private static void stubConfigForEmptyEmailService(MockedStatic<Config> mockConfig) {
+        mockConfig.when(Config::isUsingSendgrid).thenReturn(false);
+        mockConfig.when(Config::isUsingMailgun).thenReturn(false);
+        mockConfig.when(Config::isUsingMailjet).thenReturn(false);
+        mockConfig.when(Config::isUsingSmtp).thenReturn(false);
+    }
+
     private EmailWrapper getEmailToTestDomain() {
         EmailWrapper message = new EmailWrapper();
         message.setRecipient("student" + Const.TEST_EMAIL_DOMAIN);
@@ -31,6 +38,7 @@ public class EmailSenderTest extends BaseTestCase {
     public void testSendEmail_allowEmailsToTestDomainFalse_blocksTestRecipient() {
         try (MockedStatic<Config> mockConfig = mockStatic(Config.class,
                 Mockito.withSettings().defaultAnswer(Answers.CALLS_REAL_METHODS))) {
+            stubConfigForEmptyEmailService(mockConfig);
             mockConfig.when(Config::isAllowSendingEmailsToTestDomain).thenReturn(false);
             EmailSender emailSender = new EmailSender();
             EmailSendingStatus status = emailSender.sendEmail(getEmailToTestDomain());
@@ -43,6 +51,7 @@ public class EmailSenderTest extends BaseTestCase {
     public void testSendEmail_allowEmailsToTestDomainTrue_doesNotBlockTestRecipient() {
         try (MockedStatic<Config> mockConfig = mockStatic(Config.class,
                 Mockito.withSettings().defaultAnswer(Answers.CALLS_REAL_METHODS))) {
+            stubConfigForEmptyEmailService(mockConfig);
             mockConfig.when(Config::isAllowSendingEmailsToTestDomain).thenReturn(true);
             EmailSender emailSender = new EmailSender();
             EmailSendingStatus status = emailSender.sendEmail(getEmailToTestDomain());
