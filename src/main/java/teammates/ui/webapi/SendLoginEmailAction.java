2849d2cd601f97ce9c7edd4e0a32645a0e25e661
maitriupadhyay03-cell <maitriupadhyay03@gmail.com>
2026-05-10 18:48:22 +0530

    Refactor SendLoginEmailAction to extend PublicAction

--- src/main/java/teammates/ui/webapi/SendLoginEmailAction.java
+++ src/main/java/teammates/ui/webapi/SendLoginEmailAction.java
@@ -14,53 +14,40 @@ import teammates.ui.request.InvalidHttpRequestBodyException;
 
 /**
  * Sends a login email.
- */
-public class SendLoginEmailAction extends Action {
+     */
+public class SendLoginEmailAction extends PublicAction {
 
     @Override
-    AuthType getMinAuthLevel() {
-        return AuthType.PUBLIC;
-    }
+        public JsonResult execute()
+                throws InvalidHttpRequestBodyException, InvalidOperationException {
+                            String userEmail = getNonNullRequestParamValue(Const.ParamsNames.USER_EMAIL);
 
-    @Override
-    void checkSpecificAccessControl() throws UnauthorizedAccessException {
-        // Sending of login emails can be requested by anyone
-    }
+            if (!userEmail.matches(REGEX_EMAIL)) {
+                            throw new InvalidHttpParameterException("Invalid email address: " + userEmail);
+            }
 
-    @Override
-    public JsonResult execute() throws InvalidHttpRequestBodyException, InvalidOperationException {
-        if (!authProxy.isLoginEmailEnabled()) {
-            throw new InvalidOperationException("Login using email link is not supported");
-        }
+            if (StringHelper.isEmpty(userEmail)) {
+                            return new JsonResult(new SendLoginEmailResponseData(false, "An error occurred. "
+                                                                                                     + "The email could not be generated."));
+            }
 
-        String userEmail = getNonNullRequestParamValue(Const.ParamsNames.USER_EMAIL);
-        if (!StringHelper.isMatching(userEmail, REGEX_EMAIL)) {
-            throw new InvalidHttpParameterException("Invalid email address: " + userEmail);
-        }
+            String continueUrl = getNonNullRequestParamValue(Const.ParamsNames.CONTINUE_URL);
+                            String loginLink = authProxy.generateLoginLink(userEmail, continueUrl);
+                            if (loginLink == null) {
+                                            return new JsonResult(new SendLoginEmailResponseData(false, "An error occurred. "
+                                                                                                                     + "The email could not be generated."));
+                            }
 
-        String userCaptchaResponse = getRequestParamValue(Const.ParamsNames.USER_CAPTCHA_RESPONSE);
-        if (!recaptchaVerifier.isVerificationSuccessful(userCaptchaResponse)) {
-            return new JsonResult(new SendLoginEmailResponseData(false, "ReCAPTCHA verification "
-                    + "failed. Please try again."));
-        }
+            EmailWrapper loginEmail = emailGenerator.generateLoginEmail(userEmail, loginLink);
+                            EmailSendingStatus status = emailSender.sendEmail(loginEmail);
 
-        String continueUrl = getNonNullRequestParamValue(Const.ParamsNames.CONTINUE_URL);
-        String loginLink = authProxy.generateLoginLink(userEmail, continueUrl);
-        if (loginLink == null) {
-            return new JsonResult(new SendLoginEmailResponseData(false, "An error occurred. "
-                    + "The email could not be generated."));
-        }
-
-        EmailWrapper loginEmail = emailGenerator.generateLoginEmail(userEmail, loginLink);
-        EmailSendingStatus status = emailSender.sendEmail(loginEmail);
-
-        if (status.isSuccess()) {
-            return new JsonResult(new SendLoginEmailResponseData(true,
-                    "The login link has been sent to the specified email address: " + userEmail));
-        } else {
-            return new JsonResult(new SendLoginEmailResponseData(false, "An error occurred. "
-                    + "The email could not be sent."));
-        }
-    }
+            if (status.isSuccess()) {
+                            return new JsonResult(new SendLoginEmailResponseData(true,
+                                                                                                     "The login link has been sent to the specified email address: " + userEmail));
+            } else {
+                            return new JsonResult(new SendLoginEmailResponseData(false, "An error occurred. "
+                                                                                                     + "The email could not be sent."));
+            }
+                }
 
 }
