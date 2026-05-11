996583fe103caec785073c8ebac421639cdcf23e
YongJunXi <ernestyongjunxi@gmail.com>
2026-05-11 12:12:44 +0800

    Remove unused firebase methods in front-end

--- src/web/app/page.component.ts
+++ src/web/app/page.component.ts
@@ -7,7 +7,6 @@ import { fromEvent, merge, Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import uaParser from 'ua-parser-js';
 import { environment } from '../environments/environment';
-import { AuthService } from '../services/auth.service';
 import { StatusMessageService } from '../services/status-message.service';
 import { NotificationTargetUser } from '../types/api-output';
 import { LoaderBarComponent } from './components/loader-bar/loader-bar.component';
@@ -112,7 +111,6 @@ export class PageComponent {
     private ngbModal: NgbModal,
     location: Location,
     private statusMessageService: StatusMessageService,
-    private authService: AuthService,
   ) {
     this.checkBrowserVersion();
     this.router.events.subscribe((val: any) => {
@@ -187,12 +185,6 @@ export class PageComponent {
   }
 
   logout(): void {
-    if (environment.firebaseConfig?.projectId) {
-      this.authService.logout().then(() => {
-        window.location.href = this.logoutUrl;
-      });
-    } else {
-      window.location.href = this.logoutUrl;
-    }
+    window.location.href = this.logoutUrl;
   }
 }
