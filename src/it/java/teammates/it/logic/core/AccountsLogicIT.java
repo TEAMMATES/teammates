1d02cd437477e1cae8cb3fe22e2e66e6743e2626
YongJunXi <ernestyongjunxi@gmail.com>
2026-05-20 01:47:50 +0800

    Fix lint

--- src/it/java/teammates/it/logic/core/AccountsLogicIT.java
+++ src/it/java/teammates/it/logic/core/AccountsLogicIT.java
@@ -1,7 +1,6 @@
 package teammates.it.logic.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.testng.annotations.BeforeMethod;
