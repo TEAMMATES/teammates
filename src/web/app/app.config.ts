cd48d8726b8fe592c32078391ca778ec87e4a5d2
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-05-19 10:40:09 +0800

    Remove service worker and pwa configuration

--- src/web/app/app.config.ts
+++ src/web/app/app.config.ts
@@ -4,11 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { provideProtractorTestingSupport } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgbDropdownModule, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 import routes from './app.routes';
-import { environment } from '../environments/environment';
 import { customUrlSerializerProvider } from './providers/custom-url-serializer';
 
 export const appConfig: ApplicationConfig = {
@@ -16,10 +14,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(
       NgbDropdownModule,
       NgxPageScrollCoreModule.forRoot(),
-      ServiceWorkerModule.register('ngsw-worker.js', {
-        enabled: environment.production,
-        registrationStrategy: 'registerImmediately',
-      }),
       FormsModule,
       NgbDatepickerModule,
     ),
