1ee39daf812fc4cc44ed92660bd6bf62934bfa5a
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-05-20 13:53:09 +0800

    Add vitest coverage

--- src/web/test-helpers/vitest.config.ts
+++ src/web/test-helpers/vitest.config.ts
@@ -8,5 +8,11 @@ export default defineConfig({
       'jest-preset-angular/build/serializers/ng-snapshot',
       'jest-preset-angular/build/serializers/no-ng-attributes',
     ],
+    coverage: {
+      include: ['src/web/app/**/*.ts', 'src/web/environments/**/*.ts', 'src/web/services/**/*.ts'],
+      exclude: ['src/web/app/**/*.module.ts'],
+      reportsDirectory: './coverage',
+      reporter: ['lcov', 'text-summary'],
+    },
   },
 });
