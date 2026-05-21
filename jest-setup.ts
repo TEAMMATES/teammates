9e8133f2710f954e8f09fc8cef3c9b6551d87333
rsp777 <54711053+rsp777@users.noreply.github.com>
2026-05-21 21:56:33 +0530

    [#13944]Remove shown property from Notification entity and fixed the tests

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,6 +1,7 @@
 require('@angular/localize/init');
 
-(window as any).IntersectionObserver = jest.fn(() => ({
+// @ts-ignore
+window.IntersectionObserver = jest.fn(() => ({
   observe: jest.fn(),
   unobserve: jest.fn(),
   disconnect: jest.fn(),
