1435a850b740909f52dc74ecd9639e826b8b1c3c
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-05-23 02:19:48 +0800

    Remove invalid usages of GetFeedbackResponseCommentAction

--- src/main/java/teammates/ui/webapi/GetFeedbackResponseCommentAction.java
+++ src/main/java/teammates/ui/webapi/GetFeedbackResponseCommentAction.java
@@ -2,99 +2,30 @@ package teammates.ui.webapi;
 
 import java.util.UUID;
 
-import org.apache.http.HttpStatus;
-
 import teammates.common.util.Const;
-import teammates.storage.entity.FeedbackQuestion;
-import teammates.storage.entity.FeedbackResponse;
 import teammates.storage.entity.FeedbackResponseComment;
-import teammates.storage.entity.FeedbackSession;
-import teammates.storage.entity.Instructor;
-import teammates.storage.entity.Student;
-import teammates.ui.exception.EntityNotFoundException;
-import teammates.ui.exception.InvalidHttpParameterException;
 import teammates.ui.exception.UnauthorizedAccessException;
 import teammates.ui.output.FeedbackResponseCommentData;
-import teammates.ui.request.Intent;
 
 /**
  * Get all the comments given by the user for a response.
  */
 public class GetFeedbackResponseCommentAction extends BasicCommentSubmissionAction {
-    // TODO: check if this is used anywhere.
     @Override
     AuthType getMinAuthLevel() {
-        return AuthType.REG_KEY;
+        return AuthType.ALL_ACCESS;
     }
 
     @Override
     void checkSpecificAccessControl() throws UnauthorizedAccessException {
-        UUID feedbackResponseId = getUuidRequestParamValue(Const.ParamsNames.FEEDBACK_RESPONSE_ID);
-
-        FeedbackResponse feedbackResponse = null;
-
-        feedbackResponse = logic.getFeedbackResponse(feedbackResponseId);
-
-        if (feedbackResponse == null) {
-            throw new EntityNotFoundException("The feedback response does not exist.");
-        }
-
-        String courseId = feedbackResponse.getFeedbackQuestion().getCourseId();
-        FeedbackQuestion feedbackQuestion = feedbackResponse.getFeedbackQuestion();
-        FeedbackSession feedbackSession =
-                getNonNullFeedbackSession(feedbackQuestion.getFeedbackSession().getName(),
-                        courseId);
-
-        verifyInstructorCanSeeQuestionIfInModeration(feedbackQuestion);
-        verifyNotPreview();
-
-        Intent intent = Intent.valueOf(getNonNullRequestParamValue(Const.ParamsNames.INTENT));
-        switch (intent) {
-        case STUDENT_SUBMISSION:
-            gateKeeper.verifyAnswerableForStudent(feedbackQuestion);
-            Student student = getStudentOfCourseForSubmission(courseId);
-            checkAccessControlForStudentFeedbackSubmission(student, feedbackSession);
-            break;
-        case INSTRUCTOR_SUBMISSION:
-            gateKeeper.verifyAnswerableForInstructor(feedbackQuestion);
-            Instructor instructor = getInstructorOfCourseForSubmission(courseId);
-            checkAccessControlForInstructorFeedbackSubmission(instructor, feedbackSession);
-            break;
-        default:
-            throw new InvalidHttpParameterException("Unknown intent " + intent);
-        }
+        throw new UnauthorizedAccessException("Backdoor only API.");
     }
 
     @Override
     public JsonResult execute() {
         UUID feedbackResponseId = getUuidRequestParamValue(Const.ParamsNames.FEEDBACK_RESPONSE_ID);
-
-        FeedbackResponse feedbackResponse = null;
-
-        feedbackResponse = logic.getFeedbackResponse(feedbackResponseId);
-
-        if (feedbackResponse == null) {
-            throw new EntityNotFoundException("The feedback response does not exist.");
-        }
-
-        Intent intent = Intent.valueOf(getNonNullRequestParamValue(Const.ParamsNames.INTENT));
-
-        switch (intent) {
-        case STUDENT_SUBMISSION:
-        case INSTRUCTOR_SUBMISSION:
-            FeedbackResponseComment comment =
-                    logic.getFeedbackResponseCommentForResponseFromParticipant(feedbackResponseId);
-            if (comment == null) {
-                FeedbackResponse fr = logic.getFeedbackResponse(feedbackResponseId);
-                if (fr == null) {
-                    throw new EntityNotFoundException("The feedback response does not exist.");
-                }
-                return new JsonResult("Comment not found", HttpStatus.SC_NO_CONTENT);
-            }
-
-            return new JsonResult(new FeedbackResponseCommentData(comment));
-        default:
-            throw new InvalidHttpParameterException("Unknown intent " + intent);
-        }
+        FeedbackResponseComment comment =
+                logic.getFeedbackResponseCommentForResponseFromParticipant(feedbackResponseId);
+        return new JsonResult(new FeedbackResponseCommentData(comment));
     }
 }
