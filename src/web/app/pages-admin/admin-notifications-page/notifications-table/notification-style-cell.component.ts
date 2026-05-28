5f5b81dc2d6c2730e260cd42df09c442dd53e8a9
javieryeow <javieryeow@gmail.com>
2026-05-28 23:54:00 +0800

    fix: admin notifications style cell

--- src/web/app/pages-admin/admin-notifications-page/notifications-table/notification-style-cell.component.ts
+++ src/web/app/pages-admin/admin-notifications-page/notifications-table/notification-style-cell.component.ts
@@ -1,13 +1,11 @@
-import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { NotificationStyle } from '../../../../types/api-output';
-import { NotificationStyleClassPipe } from '../../../components/teammates-common/notification-style-class.pipe';
 import { NotificationStyleDescriptionPipe } from '../../../components/teammates-common/notification-style-description.pipe';
 
 @Component({
   selector: 'tm-notification-style-cell',
   templateUrl: './notification-style-cell.component.html',
-  imports: [NgClass, NotificationStyleClassPipe, NotificationStyleDescriptionPipe],
+  imports: [NotificationStyleDescriptionPipe],
 })
 export class NotificationStyleCellComponent {
   @Input() style: NotificationStyle = NotificationStyle.SUCCESS;
