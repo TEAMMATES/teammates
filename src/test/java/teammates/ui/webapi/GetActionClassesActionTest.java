5156d7d4366a2487cc26d2f85d4b36d6d756888a
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-06-03 10:36:33 +0800

    Fix test

--- src/test/java/teammates/ui/webapi/GetActionClassesActionTest.java
+++ src/test/java/teammates/ui/webapi/GetActionClassesActionTest.java
@@ -126,7 +126,8 @@ public class GetActionClassesActionTest extends BaseActionTest<GetActionClassesA
                 GetReadNotificationsAction.class,
                 PutDataBundleAction.class,
                 DeleteDataBundleAction.class,
-                GetDeadlineExtensionsAction.class
+                GetDeadlineExtensionsAction.class,
+                GetSessionSubmissionDataAction.class
         );
         List<String> expectedActionClassesNames = expectedActionClasses.stream()
                 .map(Class::getSimpleName)
