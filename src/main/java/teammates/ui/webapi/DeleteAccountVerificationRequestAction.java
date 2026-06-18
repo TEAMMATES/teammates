cdb56c4a46b0611d060f564986930f4058982ba0
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-06-18 18:15:31 +0800

    Clean up delete actions

--- src/main/java/teammates/ui/webapi/DeleteAccountVerificationRequestAction.java
+++ src/main/java/teammates/ui/webapi/DeleteAccountVerificationRequestAction.java
@@ -3,7 +3,6 @@ package teammates.ui.webapi;
 import java.util.UUID;
 
 import teammates.common.util.Const;
-import teammates.storage.entity.AccountVerificationRequest;
 import teammates.ui.exception.InvalidOperationException;
 
 /**
@@ -14,15 +13,6 @@ public class DeleteAccountVerificationRequestAction extends AdminOnlyAction {
     @Override
     public JsonResult execute() throws InvalidOperationException {
         UUID id = getUuidRequestParamValue(Const.ParamsNames.ACCOUNT_VERIFICATION_REQUEST_ID);
-
-        AccountVerificationRequest toDelete = logic.getAccountVerificationRequest(id);
-
-        if (toDelete != null && toDelete.getCreatedDemoCourseAt() != null) {
-            // TODO: remove this check
-            // instructor is already registered and cannot be deleted
-            throw new InvalidOperationException(
-                    "Account verification request of a registered instructor cannot be deleted.");
-        }
 
         logic.deleteAccountVerificationRequest(id);
 
--- src/main/java/teammates/ui/webapi/DeleteStudentAction.java
+++ src/main/java/teammates/ui/webapi/DeleteStudentAction.java
@@ -14,17 +14,7 @@ public class DeleteStudentAction extends LoggedInAction {
     @Override
     void checkSpecificAccessControl() throws UnauthorizedAccessException {
         UUID userId = getUuidRequestParamValue(Const.ParamsNames.USER_ID);
-        Student student = logic.getStudent(userId);
-        if (student == null) {
-            throw new EntityNotFoundException("Student with user ID " + userId + " does not exist.");
-        }
-
-        if (requestContext.isAdmin()) {
-            return;
-        }
-
-        gateKeeper.verifyInstructorHasPrivilege(requestContext, student.getCourseId(),
-                Const.InstructorPermissions.CAN_MODIFY_STUDENT);
+        gateKeeper.verifyCanModifyStudent(requestContext, userId);
     }
 
     @Override
