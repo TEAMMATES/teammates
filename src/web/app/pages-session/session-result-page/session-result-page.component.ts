c3b03cd9c6614ba8e40e64e50ef05fde193fdd25
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2026-06-19 13:19:23 +0800

    [#13918] Update session pages loggedInUser to accountEmail (#14274)

--- src/web/app/pages-session/session-result-page/session-result-page.component.ts
+++ src/web/app/pages-session/session-result-page/session-result-page.component.ts
@@ -92,7 +92,7 @@ export class SessionResultPageComponent implements OnInit {
   @Input() key = '';
   @Input() previewAs = '';
   @Input() entityType = 'student';
-  loggedInUser = '';
+  accountEmail = '';
   visibilityRecipient: FeedbackVisibilityType = FeedbackVisibilityType.RECIPIENT;
 
   isPreviewHintExpanded = false;
@@ -126,7 +126,7 @@ export class SessionResultPageComponent implements OnInit {
       next: (auth: AuthInfo) => {
         const isPreview = !!(auth.user && this.previewAs);
         if (auth.user) {
-          this.loggedInUser = auth.user.id;
+          this.accountEmail = auth.user.accountEmail;
         }
         // prevent having both key and previewas parameters in URL
         if (this.key && isPreview) {
@@ -138,7 +138,7 @@ export class SessionResultPageComponent implements OnInit {
             next: (resp: RegkeyValidity) => {
               if (resp.isAllowedAccess) {
                 if (resp.isUsed) {
-                  // The logged in user matches the registration key; redirect to the logged in URL
+                  // The signed in user matches the registration key; redirect to the signed in URL
 
                   this.navigationService.navigateByURL(
                     `/web/${this.entityType}/sessions/${this.feedbackSessionId}/result`,
@@ -149,15 +149,11 @@ export class SessionResultPageComponent implements OnInit {
                 }
               } else if (resp.isValid) {
                 // At this point, registration key must already be used, otherwise access would be granted
-                if (this.loggedInUser) {
-                  // Registration key belongs to another user who is not the logged in user
+                if (this.accountEmail) {
+                  // Registration key belongs to another user who is not the signed in user
                   this.navigationService.navigateWithErrorMessage(
                     '/web/front',
-                    `You are trying to access TEAMMATES using the Google account ${this.loggedInUser}, which
-                        is not linked to this TEAMMATES account. If you used a different Google account to
-                        join/access TEAMMATES before, please use that Google account to access TEAMMATES. If you
-                        cannot remember which Google account you used before, please email us at
-                        ${environment.supportEmail} for help.`,
+                    `You are signed in as ${this.accountEmail}, but this course is linked to a different TEAMMATES account. If you used a different account to join/access TEAMMATES before, please use that account to access TEAMMATES. If you cannot remember which account you used before, please email us at ${environment.supportEmail} for help.`,
                   );
                 } else {
                   // There is no logged in user for a valid, used registration key, redirect to login page
@@ -178,8 +174,8 @@ export class SessionResultPageComponent implements OnInit {
               );
             },
           });
-        } else if (this.loggedInUser) {
-          // Load information based on logged in user
+        } else if (this.accountEmail) {
+          // Load information based on signed in user
           // This will also cover preview cases
           this.loadFeedbackSession();
         } else {
--- src/web/app/pages-session/session-submission-page/session-submission-page.component.ts
+++ src/web/app/pages-session/session-submission-page/session-submission-page.component.ts
@@ -109,7 +109,7 @@ export class SessionSubmissionPageComponent implements OnInit {
 
   courseId = '';
   feedbackSessionName = '';
-  loggedInUser = '';
+  accountEmail = '';
 
   // the name of the person involved
   // (e.g. the student name for unregistered student, the name of instructor being moderated)
@@ -174,14 +174,14 @@ export class SessionSubmissionPageComponent implements OnInit {
       next: (auth: AuthInfo) => {
         const isPreviewOrModeration = !!(auth.user && (this.moderatedPerson || this.previewAs));
         if (auth.user) {
-          this.loggedInUser = auth.user.id;
+          this.accountEmail = auth.user.accountEmail;
         }
         if (this.key && !isPreviewOrModeration) {
           this.authService.getAuthRegkeyValidity(this.key, this.intent).subscribe({
             next: (resp: RegkeyValidity) => {
               if (resp.isAllowedAccess) {
                 if (resp.isUsed) {
-                  // The logged in user matches the registration key; redirect to the logged in URL
+                  // The signed in user matches the registration key; redirect to the signed in URL
                   this.navigationService.navigateByURL(
                     `/web/${this.entityType}/sessions/${this.feedbackSessionId}/submission`,
                   );
@@ -191,15 +191,11 @@ export class SessionSubmissionPageComponent implements OnInit {
                 }
               } else if (resp.isValid) {
                 // At this point, registration key must already be used, otherwise access would be granted
-                if (this.loggedInUser) {
-                  // Registration key belongs to another user who is not the logged in user
+                if (this.accountEmail) {
+                  // Registration key belongs to another user who is not the signed in user
                   this.navigationService.navigateWithErrorMessage(
                     '/web/front',
-                    `You are trying to access TEAMMATES using the Google account ${this.loggedInUser}, which
-                        is not linked to this TEAMMATES account. If you used a different Google account to
-                        join/access TEAMMATES before, please use that Google account to access TEAMMATES. If you
-                        cannot remember which Google account you used before, please email us at
-                        ${environment.supportEmail} for help.`,
+                    `You are signed in as ${this.accountEmail}, but this course is linked to a different TEAMMATES account. If you used a different account to join/access TEAMMATES before, please use that account to access TEAMMATES. If you cannot remember which account you used before, please email us at ${environment.supportEmail} for help.`,
                   );
                 } else {
                   this.loadFeedbackSession(true, auth);
@@ -219,8 +215,8 @@ export class SessionSubmissionPageComponent implements OnInit {
               );
             },
           });
-        } else if (this.loggedInUser) {
-          // Load information based on logged in user
+        } else if (this.accountEmail) {
+          // Load information based on signed in user
           // This will also cover moderation/preview cases
           this.loadFeedbackSession(false, auth);
         } else {
@@ -404,7 +400,7 @@ export class SessionSubmissionPageComponent implements OnInit {
               {
                 onClosed: () =>
                   this.navigationService.navigateByURL(
-                    this.loggedInUser ? `/web/${this.entityType}/home` : '/web/front/home',
+                    this.accountEmail ? `/web/${this.entityType}/home` : '/web/front/home',
                   ),
               },
               { backdrop: 'static' },
@@ -421,7 +417,7 @@ export class SessionSubmissionPageComponent implements OnInit {
                 {
                   onClosed: () =>
                     this.navigationService.navigateByURL(
-                      this.loggedInUser ? `/web/${this.entityType}/home` : '/web/front/home',
+                      this.accountEmail ? `/web/${this.entityType}/home` : '/web/front/home',
                     ),
                 },
                 { backdrop: 'static' },
