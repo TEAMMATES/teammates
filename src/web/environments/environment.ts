158fdd6dc646342341b548ce3fd3ef7029b1d475
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2026-06-25 17:11:45 +0800

    [#14346] Add frontendUrl in ConfigData (#14349)

--- src/web/environments/environment.ts
+++ src/web/environments/environment.ts
@@ -14,7 +14,6 @@ export const environment: Environment = {
   version: APPLICATION_VERSION,
   production: false,
   backendUrl: 'http://localhost:8080',
-  frontendUrl: 'http://localhost:4200',
   withCredentials: true,
 };
 
