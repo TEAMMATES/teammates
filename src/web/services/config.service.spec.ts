c5f8385de0819b89976ed5447579ea500d0f7fbd
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2026-07-21 07:09:41 +0800

    [#14393] Cache config in frontend (#14395)

--- src/web/services/config.service.spec.ts
+++ src/web/services/config.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { ConfigService } from './config.service';
 import { createMockHttpRequestService, MockHttpRequestService } from '../test-helpers/mock-http-request';
 import { HttpRequestService } from './http-request.service';
 import { ResourceEndpoints } from '../types/api-const';
+import { Config } from '../types/api-output';
 
 describe('ConfigService', () => {
   let service: ConfigService;
   let spyHttpRequestService: MockHttpRequestService;
+  const mockConfig: Config = {
+    loginMethods: [],
+    frontendUrl: '',
+  };
 
   beforeEach(() => {
     spyHttpRequestService = createMockHttpRequestService();
@@ -21,8 +27,19 @@ describe('ConfigService', () => {
   });
 
   it('should execute GET on config endpoint', () => {
-    spyHttpRequestService.get.mockReturnValue({});
+    spyHttpRequestService.get.mockReturnValue(of(mockConfig));
     service.getConfig();
     expect(spyHttpRequestService.get).toHaveBeenCalledWith(ResourceEndpoints.CONFIG);
+  });
+
+  it('should return cached config if already fetched', () => {
+    spyHttpRequestService.get.mockReturnValue(of(mockConfig));
+
+    service.getConfig().subscribe();
+    service.getConfig().subscribe((cachedConfig: Config) => {
+      expect(cachedConfig).toEqual(mockConfig);
+    });
+
+    expect(spyHttpRequestService.get).toHaveBeenCalledOnce();
   });
 });
