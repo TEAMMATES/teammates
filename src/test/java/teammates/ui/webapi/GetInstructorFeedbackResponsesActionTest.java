ce0aa7dc6a7b32d181780fb056c113bf248dbeb6
YongJunXi <ernestyongjunxi@gmail.com>
2026-07-21 17:08:13 +0800

    Add negative test cases

--- src/test/java/teammates/ui/webapi/GetInstructorFeedbackResponsesActionTest.java
+++ src/test/java/teammates/ui/webapi/GetInstructorFeedbackResponsesActionTest.java
@@ -2,10 +2,14 @@ package teammates.ui.webapi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.UUID;
+
 import org.testng.annotations.Test;
 
 import teammates.common.util.Const;
 import teammates.test.GroupNames;
+import teammates.ui.exception.EntityNotFoundException;
+import teammates.ui.exception.UnauthorizedAccessException;
 import teammates.ui.output.FeedbackResponsesData;
 
 /**
@@ -13,6 +17,9 @@ import teammates.ui.output.FeedbackResponsesData;
  */
 public class GetInstructorFeedbackResponsesActionTest
         extends BaseActionTest<GetInstructorFeedbackResponsesAction, FeedbackResponsesData> {
+
+    private static final String NON_EXISTENT_FEEDBACK_QUESTION_ID =
+            UUID.fromString("00000000-0000-4000-8000-000000000102").toString();
 
     @Test(groups = GroupNames.ACTION)
     public void getInstructorFeedbackResponsesAction_validQuestion_returnsResponsesFromInstructor() {
@@ -36,5 +43,59 @@ public class GetInstructorFeedbackResponsesActionTest
 
         assertEquals(1, result.getResponses().size());
         assertEquals(response.id(), result.getResponses().get(0).getFeedbackResponseId());
+    }
+
+    @Test(groups = GroupNames.ACTION)
+    public void getInstructorFeedbackResponsesAction_questionDoesNotExist_throwsEntityNotFoundException() {
+        var account = given.account("account");
+        persistGivenData(given);
+
+        RequestContext request = new RequestContext()
+                .withParam(Const.ParamsNames.FEEDBACK_QUESTION_ID, NON_EXISTENT_FEEDBACK_QUESTION_ID)
+                .withAccountAuth(account.id());
+
+        assertActionThrows(EntityNotFoundException.class, request);
+    }
+
+    @Test(groups = GroupNames.ACTION)
+    public void getInstructorFeedbackResponsesAction_userNotLoggedIn_throwsUnauthorizedAccessException() {
+        var session = given.feedbackSession("session", fs -> fs.defaultCourse().opened());
+        var question = given.feedbackQuestion("question", q -> q.feedbackSession(session.alias()).instructorsToStudents());
+        persistGivenData(given);
+
+        RequestContext request = new RequestContext()
+                .withParam(Const.ParamsNames.FEEDBACK_QUESTION_ID, question.id().toString());
+
+        assertActionThrows(UnauthorizedAccessException.class, request);
+    }
+
+    @Test(groups = GroupNames.ACTION)
+    public void getInstructorFeedbackResponsesAction_studentAccessesInstructorEndpoint_throwsUnauthorizedAccessException() {
+        var account = given.account("account");
+        given.student("student", s -> s.defaultCourse().account(account.alias()));
+        var session = given.feedbackSession("session", fs -> fs.defaultCourse().opened());
+        var question = given.feedbackQuestion("question", q -> q.feedbackSession(session.alias()).instructorsToStudents());
+        persistGivenData(given);
+
+        RequestContext request = new RequestContext()
+                .withParam(Const.ParamsNames.FEEDBACK_QUESTION_ID, question.id().toString())
+                .withAccountAuth(account.id());
+
+        assertActionThrows(UnauthorizedAccessException.class, request);
+    }
+
+    @Test(groups = GroupNames.ACTION)
+    public void getInstructorFeedbackResponsesAction_instructorWithoutSubmitPrivilege_throwsUnauthorizedAccessException() {
+        var account = given.account("account");
+        given.instructor("instructor", i -> i.defaultCourse().account(account.alias()).noPrivileges());
+        var session = given.feedbackSession("session", fs -> fs.defaultCourse().opened());
+        var question = given.feedbackQuestion("question", q -> q.feedbackSession(session.alias()).instructorsToStudents());
+        persistGivenData(given);
+
+        RequestContext request = new RequestContext()
+                .withParam(Const.ParamsNames.FEEDBACK_QUESTION_ID, question.id().toString())
+                .withAccountAuth(account.id());
+
+        assertActionThrows(UnauthorizedAccessException.class, request);
     }
 }
